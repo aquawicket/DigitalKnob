ce620c647e1854ee17f996007736f62fa47d21d4
aquawicket <aquawicket@hotmail.com>
2016-07-14 05:08:08 -0700

    update

--- DKPlugins/DKCef/cefchild/cefchild.cpp
+++ DKPlugins/DKCef/cefchild/cefchild.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-	CefMainArgs main_args;
+	CefMainArgs main_args(argc, argv);
 
 	// Execute the secondary process.
 	return CefExecuteProcess(main_args, NULL, NULL);
