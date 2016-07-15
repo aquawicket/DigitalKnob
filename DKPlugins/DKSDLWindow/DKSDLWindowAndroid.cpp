d0a5f8931f70742092b812cb1e2b73d8435ffd82
aquawicket <aquawicket@hotmail.com>
2016-07-14 20:50:34 -0700

    update

--- DKPlugins/DKSDLWindow/DKSDLWindowAndroid.cpp
+++ DKPlugins/DKSDLWindow/DKSDLWindowAndroid.cpp
@@ -1,10 +1,12 @@
 #ifdef ANDROID
 #include "stdafx.h"
 #include "DKSDLWindowAndroid.h"
+#include "DKAndroid"
 #include "SDL.h"
 #include "SDL_main.h"
 
 extern "C" {
+	extern jint JNICALL JNI_OnLoad2(JavaVM* vm, void* reserved);
 	extern void SDL_Android_Init(JNIEnv* env, jclass cls);
 	extern void Java_org_libsdl_app_SDLActivity_onNativeDropFile(JNIEnv* env, jclass jcls, jstring filename);
 	extern JNIEXPORT void JNICALL Java_org_libsdl_app_SDLActivity_onNativeResize(JNIEnv* env, jclass jcls, jint width, jint height, jint format, jfloat rate);
@@ -82,6 +84,7 @@ void* DKSDLWindowAndroid::onInitSDL(void* data)
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 	
     /* This interface could expand with ABI negotiation, callbacks, etc. */
+	JNI_OnLoad2(thejvm, null);
     SDL_Android_Init(jd.env, jd.cls);
 	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengles");
     SDL_SetMainReady();
