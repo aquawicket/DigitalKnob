aa0395cd6b15406d8616c1e1850b234e3d0936ce
aquawicket <aquawicket@hotmail.com>
2016-07-14 18:26:58 -0700

    update

--- DKPlugins/DKSDLAudio/DKSDLAudio.cpp
+++ DKPlugins/DKSDLAudio/DKSDLAudio.cpp
@@ -70,8 +70,8 @@ void DKSDLAudio::Callback(void *userdata, Uint8 *stream, int len)
 {
 	if(audio_len ==0){ return; }
 
-	len = ((unsigned int)len > audio_len ? audio_len : (unsigned int)len );
-	//SDL_memcpy (stream, audio_pos, len); 					// simply copy from one buffer into the other
+	len = (len > audio_len ? audio_len : len );
+	SDL_memcpy(stream, audio_pos, len); 					// simply copy from one buffer into the other
 	SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);// mix from one buffer into another
 	audio_pos += len;
 	audio_len -= len;
