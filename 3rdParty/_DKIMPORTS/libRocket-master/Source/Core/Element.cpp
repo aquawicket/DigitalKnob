9ce0146f6052af7ab7264a6830e55e0de0cb2b45
aquawicket <aquawicket@hotmail.com>
2016-07-20 23:21:50 -0700

    update

--- 3rdParty/_DKIMPORTS/libRocket-master/Source/Core/Element.cpp
+++ 3rdParty/_DKIMPORTS/libRocket-master/Source/Core/Element.cpp
@@ -396,7 +396,6 @@ void Element::SetContentBox(const Vector2f& _content_offset, const Vector2f& _co
 		DirtyOffset();
 	}
 
-	/*
 	//TEMPORARY FIX
 	for(int i=0; i<GetNumChildren(); ++i){
 		Rocket::Core::Element* child = GetChild(i);
@@ -417,7 +416,6 @@ void Element::SetContentBox(const Vector2f& _content_offset, const Vector2f& _co
 	if(overflow_y == 2 && content_box.y > GetBox().GetSize().y){
 		GetElementScroll()->EnableScrollbar(ElementScroll::VERTICAL, GetBox().GetSize().y);
 	}
-	*/
 }
 
 // Sets the box describing the size of the element.
