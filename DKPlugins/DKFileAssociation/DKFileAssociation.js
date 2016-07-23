94c9396b40df6d00438118d17f143e2907434ef0
aquawicket <aquawicket@hotmail.com>
2016-07-23 02:49:20 -0700

    update

---
+++ DKPlugins/DKFileAssociation/DKFileAssociation.js
@@ -0,0 +1,18 @@
+/////////////////////////////////
+function DKFileAssociation_Init()
+{
+	//DKCreate("DKFileAssociation/DKFileAssociation.html");
+	//DKAddEvent("DKFileAssociation_item", "click", DKFileAssociation_OnEvent);
+}
+
+////////////////////////////////
+function DKFileAssociation_End()
+{
+	//DKClose("DKFileAssociation/DKFileAssociation.html");
+}
+
+////////////////////////////////////////
+function DKFileAssociation_OnEvent(event)
+{
+	//if(DK_Id(event,"DKFileAssociation_item")){ }
+}
