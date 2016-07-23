77642e6c16662a093651db3527ef3add3ab1e3bc
aquawicket <aquawicket@hotmail.com>
2016-07-23 13:46:19 -0700

    update

---
+++ DKPlugins/DKBlog/DKBlog.js
@@ -0,0 +1,18 @@
+
+///////////////////////
+function DKBlog_Init()
+{
+	//DKLog("DKBlog_Init() \n");
+}
+
+/////////////////////
+function DKBlog_End()
+{
+	
+}
+
+//////////////////////////////
+function DKBlog_OnEvent(event)
+{
+	
+}
