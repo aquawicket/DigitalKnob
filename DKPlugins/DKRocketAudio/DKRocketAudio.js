5212af223e0f47c83c114e25315b640b0b497bfc
aquawicket <aquawicket@hotmail.com>
2016-07-23 00:19:53 -0700

    update

---
+++ DKPlugins/DKRocketAudio/DKRocketAudio.js
@@ -0,0 +1,17 @@
+/////////////////////////////
+function DKRocketAudio_Init()
+{
+	DKCreate("DKRocketAudio/DKRocketAudio.html");
+}
+
+////////////////////////////
+function DKRocketAudio_End()
+{
+	DKClose("DKRocketAudio/DKRocketAudio.html");
+}
+
+/////////////////////////////////////
+function DKRocketAudio_OnEvent(event)
+{
+	
+}
