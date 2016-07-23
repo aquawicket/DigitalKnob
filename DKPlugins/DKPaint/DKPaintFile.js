915c759c94f23f6a7b466295ec88fcb3d7c6d515
aquawicket <aquawicket@hotmail.com>
2016-07-23 02:39:51 -0700

    update

---
+++ DKPlugins/DKPaint/DKPaintFile.js
@@ -0,0 +1,55 @@
+/////////////////////////////
+function DKPaintFile_Init()
+{
+	DKCreate("DKPaint/DKPaintFile.html,DKPaint.html");
+	DKAddEvent("GLOBAL", "mousedown", DKPaintFile_OnEvent);
+	DKAddEvent("DKPaintFile_Open", "click", DKPaintFile_OnEvent);
+	DKAddEvent("DKPaintFile_Save", "click", DKPaintFile_OnEvent);
+	DKAddEvent("DKPaintFile_Save As", "click", DKPaintFile_OnEvent);
+}
+
+////////////////////////////
+function DKPaintFile_End()
+{
+	DKRemoveEvent("GLOBAL", "mousedown", DKPaintFile_OnEvent);
+	DKClose("DKPaint/DKPaintFile.html");
+}
+
+/////////////////////////////////////
+function DKPaintFile_OnEvent(event)
+{
+	if(DK_Id(event,"DKPaintFile_Open")){
+		DKPaintFile_Open();
+	}
+	if(DK_Id(event,"DKPaintFile_Save")){
+		DKPaintFile_Save();
+	}
+	if(DK_Id(event,"DKPaintFile_Save As")){
+		DKPaintFile_SaveAs();
+	}
+	
+	if(DK_Id(event, "GLOBAL")){
+		if(DKWidget_IsChildOf(DKWidget_GetHoverElement(), "DKPaintFile.html")){
+			return;
+		}
+	}
+	DKClose("DKPaint/DKPaintFile.js");
+}
+
+/////////////////////////////
+function DKPaintFile_Open()
+{
+	DKLog("DKPaintFile_Open \n", DKDEBUG);
+}
+
+/////////////////////////////
+function DKPaintFile_Save()
+{
+	DKLog("DKPaintFile_Save \n", DKDEBUG);
+}
+
+//////////////////////////////
+function DKPaintFile_SaveAs()
+{
+	DKLog("DKPaintFile_SaveAs \n", DKDEBUG);
+}
---
+++ DKPlugins/DKPaint/DKPaintMenu.js
@@ -0,0 +1,57 @@
+/////////////////////////////
+function DKPaintMenu_Init()
+{
+	DKCreate("DKPaint/DKPaintMenu.html");
+	DKWidget_SetProperty("DKPaintMenu.html","top",DKWindow_GetMouseY()+"px");
+	DKWidget_SetProperty("DKPaintMenu.html","left",DKWindow_GetMouseX()+"px");
+	DKAddEvent("GLOBAL", "mousedown", DKPaintMenu_OnEvent);
+	DKAddEvent("DKPaintMenu_Cut", "click", DKPaintMenu_OnEvent);
+	DKAddEvent("DKPaintMenu_Copy", "click", DKPaintMenu_OnEvent);
+	DKAddEvent("DKPaintMenu_Paste", "click", DKPaintMenu_OnEvent);
+}
+
+////////////////////////////
+function DKPaintMenu_End()
+{
+	DKRemoveEvent("GLOBAL", "mousedown", DKPaintMenu_OnEvent);
+	DKClose("DKPaint/DKPaintMenu.html");
+}
+
+/////////////////////////////////////
+function DKPaintMenu_OnEvent(event)
+{
+	if(DK_Id(event,"DKPaintMenu_Cut")){
+		DKPaintMenu_Cut();
+	}
+	if(DK_Id(event,"DKPaintMenu_Copy")){
+		DKPaintMenu_Copy();
+	}
+	if(DK_Id(event,"DKPaintMenu_Paste")){
+		DKPaintMenu_Paste();
+	}
+	
+	if(DK_Id(event, "GLOBAL")){
+		if(DKWidget_IsChildOf(DKWidget_GetHoverElement(), "DKPaintMenu.html")){
+			return;
+		}
+	}
+	DKClose("DKPaint/DKPaintMenu.js");
+}
+
+////////////////////////////
+function DKPaintMenu_Cut()
+{
+	//TODO
+}
+
+/////////////////////////////
+function DKPaintMenu_Copy()
+{
+	//TODO
+}
+
+//////////////////////////////
+function DKPaintMenu_Paste()
+{
+	//TODO
+}
