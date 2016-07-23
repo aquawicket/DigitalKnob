f3a799a66aa761ed0dfcf3c26730c282b07d20e2
aquawicket <aquawicket@hotmail.com>
2016-07-23 02:22:43 -0700

    update

--- DKPlugins/DKRocketVideo/DKRocketVideo.js
+++ DKPlugins/DKRocketVideo/DKRocketVideo.js
@@ -1,99 +1,99 @@
 /////////////////////////////
-function DKRocketAudio_Init()
+function DKRocketVideo_Init()
 {
-	DKCreate("DKRocketAudio/DKRocketAudio.html");
-	DKAddEvent("DKRocketAudio_playpause", "click", DKRocketAudio_OnEvent);
-	DKAddEvent("DKRocketAudio_position", "click", DKRocketAudio_OnEvent);
-	DKAddEvent("DKRocketAudio_positiontrack", "click", DKRocketAudio_OnEvent);
-	DKAddEvent("DKRocketAudio_speaker", "click", DKRocketAudio_OnEvent);
-	DKAddEvent("DKRocketAudio_volume", "click", DKRocketAudio_OnEvent);
-	DKAddEvent("DKRocketAudio_volumetrack", "click", DKRocketAudio_OnEvent);
+	DKCreate("DKRocketVideo/DKRocketVideo.html");
+	DKAddEvent("DKRocketVideo_playpause", "click", DKRocketVideo_OnEvent);
+	DKAddEvent("DKRocketVideo_position", "click", DKRocketVideo_OnEvent);
+	DKAddEvent("DKRocketVideo_positiontrack", "click", DKRocketVideo_OnEvent);
+	DKAddEvent("DKRocketVideo_speaker", "click", DKRocketVideo_OnEvent);
+	DKAddEvent("DKRocketVideo_volume", "click", DKRocketVideo_OnEvent);
+	DKAddEvent("DKRocketVideo_volumetrack", "click", DKRocketVideo_OnEvent);
 }
 
 ////////////////////////////
-function DKRocketAudio_End()
+function DKRocketVideo_End()
 {
-	DKClose("DKRocketAudio/DKRocketAudio.html");
+	DKClose("DKRocketVideo/DKRocketVideo.html");
 }
 
 /////////////////////////////////////
-function DKRocketAudio_OnEvent(event)
+function DKRocketVideo_OnEvent(event)
 {
-	if(DK_Id(event, "DKRocketAudio_playpause")){
-		DKRocketAudio_playpause();
+	if(DK_Id(event, "DKRocketVideo_playpause")){
+		DKRocketVideo_playpause();
 	}
-	if(DK_Id(event, "DKRocketAudio_position")){
-		DKRocketAudio_position();
+	if(DK_Id(event, "DKRocketVideo_position")){
+		DKRocketVideo_position();
 	}
-	if(DK_Id(event, "DKRocketAudio_positiontrack")){
-		DKRocketAudio_positiontrack();
+	if(DK_Id(event, "DKRocketVideo_positiontrack")){
+		DKRocketVideo_positiontrack();
 	}
-	if(DK_Id(event, "DKRocketAudio_speaker", "click")){
-		DKRocketAudio_speaker();
+	if(DK_Id(event, "DKRocketVideo_speaker", "click")){
+		DKRocketVideo_speaker();
 	}
-	if(DK_Id(event, "DKRocketAudio_volume", "click")){
-		DKRocketAudio_volume();
+	if(DK_Id(event, "DKRocketVideo_volume", "click")){
+		DKRocketVideo_volume();
 	}
-	if(DK_Id(event, "DKRocketAudio_volumetrack", "click")){
-		DKRocketAudio_volumetrack();
+	if(DK_Id(event, "DKRocketVideo_volumetrack", "click")){
+		DKRocketVideo_volumetrack();
 	}
 }
 
 //////////////////////////////////
-function DKRocketAudio_playpause()
+function DKRocketVideo_playpause()
 {
-	//DKLog("DKRocketAudio_playpause() \n",DKDEBUG);
+	//DKLog("DKRocketVideo_playpause() \n",DKDEBUG);
 	
 	//toggle play/pause
-	var src = DKWidget_GetAttribute("DKRocketAudio_playpause", "src");
-	//DKLog("DKRocketAudio_playpause(): src="+src+"\n", DKDEBUG);
+	var src = DKWidget_GetAttribute("DKRocketVideo_playpause", "src");
+	//DKLog("DKRocketVideo_playpause(): src="+src+"\n", DKDEBUG);
 	
 	if(src.indexOf("play.png") != -1){
-		DKWidget_SetAttribute("DKRocketAudio_playpause", "src", "DKRocketAudio/pause.png");
+		DKWidget_SetAttribute("DKRocketVideo_playpause", "src", "DKRocketVideo/pause.png");
 	}
 	else{
-		DKWidget_SetAttribute("DKRocketAudio_playpause", "src", "DKRocketAudio/play.png");
+		DKWidget_SetAttribute("DKRocketVideo_playpause", "src", "DKRocketVideo/play.png");
 	}
 }
 
 /////////////////////////////////
-function DKRocketAudio_position()
+function DKRocketVideo_position()
 {
-	//DKLog("DKRocketAudio_position() \n",DKDEBUG);
+	//DKLog("DKRocketVideo_position() \n",DKDEBUG);
 }
 
 //////////////////////////////////////
-function DKRocketAudio_positiontrack()
+function DKRocketVideo_positiontrack()
 {
-	//DKLog("DKRocketAudio_positiontrack() \n",DKDEBUG);
+	//DKLog("DKRocketVideo_positiontrack() \n",DKDEBUG);
 }
 
 ////////////////////////////////
-function DKRocketAudio_speaker()
+function DKRocketVideo_speaker()
 {
-	//DKLog("DKRocketAudio_speaker() \n",DKDEBUG);
+	//DKLog("DKRocketVideo_speaker() \n",DKDEBUG);
 	
 	//toggle volume/mute
-	var src = DKWidget_GetAttribute("DKRocketAudio_speaker", "src");
-	DKLog("DKRocketAudio_speaker(): src="+src+"\n", DKDEBUG);
+	var src = DKWidget_GetAttribute("DKRocketVideo_speaker", "src");
+	DKLog("DKRocketVideo_speaker(): src="+src+"\n", DKDEBUG);
 	
 	if(src.indexOf("mute.png") != -1){
-		DKWidget_SetAttribute("DKRocketAudio_speaker", "src", "DKRocketAudio/volume3.png");
+		DKWidget_SetAttribute("DKRocketVideo_speaker", "src", "DKRocketVideo/volume3.png");
 	}
 	else{
-		DKWidget_SetAttribute("DKRocketAudio_speaker", "src", "DKRocketAudio/mute.png");
+		DKWidget_SetAttribute("DKRocketVideo_speaker", "src", "DKRocketVideo/mute.png");
 	}
 }
 
 ///////////////////////////////
-function DKRocketAudio_volume()
+function DKRocketVideo_volume()
 {
-	//DKLog("DKRocketAudio_volume() \n",DKDEBUG);
+	//DKLog("DKRocketVideo_volume() \n",DKDEBUG);
 }
 
 ////////////////////////////////////
-function DKRocketAudio_volumetrack()
+function DKRocketVideo_volumetrack()
 {
-	//DKLog("DKRocketAudio_volumetrack() \n",DKDEBUG);
+	//DKLog("DKRocketVideo_volumetrack() \n",DKDEBUG);
 }
 
