e1e57b7ad975d88a1729d45007ffca877d96564b
aquawicket <aquawicket@hotmail.com>
2016-08-01 01:56:05 -0700

    update

--- DKPlugins/DKConsole/DKConsole.js
+++ DKPlugins/DKConsole/DKConsole.js
@@ -3,6 +3,8 @@ function DKConsole_Init()
 {
 	DKCreate("DKConsole/DKConsole.html");
 	DKAddEvent("DKConsoleClose", "click", DKConsole_OnEvent);
+	DKAddEvent("GLOBAL", "notify", DKConsole_OnEvent);
+	
 	DKWidget_CreateElement("DKConsole.html", "a", "TODO");
 	DKWidget_CreateElement("DKConsole.html", "a", "TODO");
 	DKWidget_CreateElement("DKConsole.html", "a", "TODO");
@@ -23,7 +25,7 @@ function DKConsole_OnEvent(event)
 		DKClose("DKConsole/DKConsole.js");
 	}
 	
-	if(DK_Type(event, "DKNotify")){
+	if(DK_Type(event, "notify")){
 		DKConsole_Notify(DKWidget_GetValue(event));
 	}
 }
@@ -32,7 +34,7 @@ function DKConsole_OnEvent(event)
 /////////////////////////////////
 function DKConsole_Notify(string)
 {
-	string = string.replace("\n", "");
-	DKWidget_SetAttribute("DKConsoleText", "value", DKWidget_GetAttribute("DKConsoleText", "value")+string+"\n");
-	DKWidget_SetAttribute("DKConsoleText", "scrollTop", DKWidget_GetAttribute("DKConsoleText", "scrollHeight"));
+	//string = string.replace("\n", "");
+	//DKWidget_SetAttribute("DKConsoleText", "value", DKWidget_GetAttribute("DKConsoleText", "value")+string+"\n");
+	//DKWidget_SetAttribute("DKConsoleText", "scrollTop", DKWidget_GetAttribute("DKConsoleText", "scrollHeight"));
 }
