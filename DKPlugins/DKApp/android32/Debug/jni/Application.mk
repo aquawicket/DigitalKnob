2b9c0b95dbaf3c222ab48fd4c434f2ca18cb3833
aquawicket <aquawicket@hotmail.com>
2016-08-05 05:25:24 -0700

    update

--- DKPlugins/DKApp/android32/Debug/jni/Application.mk
+++ DKPlugins/DKApp/android32/Debug/jni/Application.mk
@@ -3,7 +3,7 @@ APP_BUILD_SCRIPT := $(call my-dir)/Android.mk
 
 APP_OPTIM := debug
 
-APP_PLATFORM 	:= android-15
+APP_PLATFORM 	:= android-19
 APP_STL 		:= gnustl_static
 APP_CPPFLAGS 	:= -fexceptions -frtti
 APP_ABI 		:= armeabi-v7a
