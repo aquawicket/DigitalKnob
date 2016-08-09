2a16ccb9f3e9bc182b06fb2258413ff75e8f7bba
aquawicket <aquawicket@hotmail.com>
2016-08-09 11:27:11 -0700

    update

---
+++ DKApps/DigitalKnob.com/assets/Panel0.js
@@ -0,0 +1,18 @@
+//////////////////////
+function Panel0_Init()
+{
+	DKCreate("Panel0.html");
+}
+
+/////////////////////
+function Panel0_End()
+{
+	DKClose("Panel0.html");
+}
+
+//////////////////////////////
+function Panel0_OnEvent(event)
+{
+	//DKLog("Panel0_OnEvent("+event+") \n", DKDEBUG);
+
+}
--- DKApps/DigitalKnob.com/assets/User.js
+++ DKApps/DigitalKnob.com/assets/User.js
@@ -1,3 +1,3 @@
 DKCreate("DKWindow");
 DKCreate("DKRocket")
-DKCreate("Panel0.html");
+DKCreate("Panel0.js");
