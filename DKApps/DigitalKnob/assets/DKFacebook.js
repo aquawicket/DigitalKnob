4915da86e3b00af2677ec736b478269c90163b4a
aquawicket <aquawicket@hotmail.com>
2016-08-10 00:40:12 -0700

    update

--- DKApps/DigitalKnob/assets/DKFacebook.js
+++ DKApps/DigitalKnob/assets/DKFacebook.js
@@ -1,19 +1,23 @@
-////////////////////////
+//////////////////////////
 function DKFacebook_Init()
 {
 	DKCreate("DKFacebook.html,Panel0_content");
 	//DKAddEvent("GLOBAL", "mousedown", DKFacebook_OnEvent);
+	DKAddEvent("DKFacebook_download", "click", DKFacebook_OnEvent);
 }
 
-///////////////////////
+/////////////////////////
 function DKFacebook_End()
 {
-	DKRemoveEvent("GLOBAL", "mousedown", DKFacebook_OnEvent);
+	//DKRemoveEvent("GLOBAL", "mousedown", DKFacebook_OnEvent);
 	DKClose("DKFacebook.html");
 }
 
-////////////////////////////////
+//////////////////////////////////
 function DKFacebook_OnEvent(event)
 {
-	
+	if(DK_Id(event, "DKFacebook_download")){
+		DKLog("Download.... TODO \n");
+		window.location = "http://Digitalknob.com/Download/FacebookLite.apk";  //FIXME - We need a DKWindow command for this
+	}
 }
