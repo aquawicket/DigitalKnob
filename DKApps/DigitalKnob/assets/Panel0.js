3d5268fd94a14a11411b66318c07171400873649
aquawicket <aquawicket@hotmail.com>
2016-08-09 21:31:50 -0700

    update

--- DKApps/DigitalKnob/assets/Panel0.js
+++ DKApps/DigitalKnob/assets/Panel0.js
@@ -4,6 +4,8 @@ function Panel0_Init()
 	DKCreate("Panel0.html");
 	//DKCreate("Content.html,Panel0_content");
 	DKCreate("OsInfo.js");
+	
+	DKAddEvent("AppsMenu", "click", Panel0_OnEvent);
 }
 
 /////////////////////
@@ -16,5 +18,7 @@ function Panel0_End()
 function Panel0_OnEvent(event)
 {
 	//DKLog("Panel0_OnEvent("+event+") \n", DKDEBUG);
-
+	if(DK_Id(event, "AppsMenu")){
+		DKLog("AppsMenu"); //TODO
+	}
 }
