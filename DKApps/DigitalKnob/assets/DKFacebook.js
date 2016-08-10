3953a4a1779b955ab8cd6ed395062baab9659ccd
aquawicket <aquawicket@hotmail.com>
2016-08-10 01:34:24 -0700

    update

--- DKApps/DigitalKnob/assets/DKFacebook.js
+++ DKApps/DigitalKnob/assets/DKFacebook.js
@@ -17,7 +17,6 @@ function DKFacebook_End()
 function DKFacebook_OnEvent(event)
 {
 	if(DK_Id(event, "DKFacebook_download")){
-		DKLog("Download.... TODO \n");
 		window.location = "http://Digitalknob.com/Download/DKFacebook.apk";  //FIXME - We need a DKWindow command for this
 	}
 }
