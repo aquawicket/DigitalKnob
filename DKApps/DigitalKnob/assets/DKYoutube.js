881b5345f141c86bb3b1f3664b02b2d53367cdd8
aquawicket <aquawicket@hotmail.com>
2016-08-11 15:05:09 -0700

    update

--- DKApps/DigitalKnob/assets/DKYoutube.js
+++ DKApps/DigitalKnob/assets/DKYoutube.js
@@ -1,19 +1,22 @@
-////////////////////////
+//////////////////////////
 function DKYoutube_Init()
 {
 	DKCreate("DKYoutube.html,Panel0_content");
 	//DKAddEvent("GLOBAL", "mousedown", DKYoutube_OnEvent);
+	DKAddEvent("DKYoutube_download", "click", DKYoutube_OnEvent);
 }
 
-///////////////////////
+/////////////////////////
 function DKYoutube_End()
 {
-	DKRemoveEvent("GLOBAL", "mousedown", DKYoutube_OnEvent);
+	//DKRemoveEvent("GLOBAL", "mousedown", DKYoutube_OnEvent);
 	DKClose("DKYoutube.html");
 }
 
-////////////////////////////////
+//////////////////////////////////
 function DKYoutube_OnEvent(event)
 {
-	
+	if(DK_Id(event, "DKYoutube_download")){
+		window.location = "http://Digitalknob.com/Download/DKYoutube.apk";  //FIXME - We need a DKWindow command for this
+	}
 }
