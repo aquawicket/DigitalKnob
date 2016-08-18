3cbfb1ee054cddad98122667b44eab953a317ebe
aquawicket <aquawicket@hotmail.com>
2016-08-17 20:12:24 -0700

    update

--- DKPlugins/DKApp/android32/Debug/src/digitalknob/dkapp/DK.java
+++ DKPlugins/DKApp/android32/Debug/src/digitalknob/dkapp/DK.java
@@ -15,10 +15,10 @@ public class DK extends Activity
 		System.loadLibrary("DKAndroid");
 	}
 
-	public static native void CallCppFunction(String data);
 	public native void initJNIBridge();
 	public static native void exitJNIBridge();
-
+	public static native void CallCppFunction(String data);
+	
 	//Main Android Activity life cycle
 	@Override
 	protected void onCreate(Bundle app) {
@@ -26,17 +26,30 @@ public class DK extends Activity
 		super.onCreate(app);
 		//initJNIBridge(); // Calls C++ function to store object for C++ to Java bridge
 
-		Intent splash;
-		splash = new Intent(this, SplashActivity.class);
-		startActivity(splash);
+		Intent splashActivity;
+		splashActivity = new Intent(this, SplashActivity.class);
+		startActivity(splashActivity);
 
-		final Intent webView;
-		webView = new Intent(this, WebviewActivity.class);
+		/*
+		final Intent webviewActivity;
+		webviewActivity = new Intent(this, WebviewActivity.class);
 		Handler handler = new Handler();
 		handler.postDelayed(new Runnable() {
 			@Override
 			public void run(){
-				startActivity(webView);
+				startActivity(webviewActivity);
+				finish();
+			}
+		}, 0);
+		*/
+
+		final Intent sdlActivity;
+		sdlActivity = new Intent(this, SDLActivity.class);
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run(){
+				startActivity(sdlActivity);
 				finish();
 			}
 		}, 0);
