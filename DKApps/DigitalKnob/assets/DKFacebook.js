b7e22a5919007aeab3aaf9c2e3b687966507a562
aquawicket <aquawicket@hotmail.com>
2016-08-22 21:27:04 -0700

    update

--- DKApps/DigitalKnob/assets/DKFacebook.js
+++ DKApps/DigitalKnob/assets/DKFacebook.js
@@ -1,9 +1,44 @@
+var os = DK_GetOS();
+var win32_download = "";
+var win64_download = "";
+var mac_download = "";
+var linux_download = "";
+var ios_download = "";
+var android_download = "http://digitalknob.com/Download/DKFacebook.apk";
+
 //////////////////////////
 function DKFacebook_Init()
 {
 	DKCreate("DKFacebook.html,Panel0_content");
 	//DKAddEvent("GLOBAL", "mousedown", DKFacebook_OnEvent);
 	DKAddEvent("DKFacebook_download", "click", DKFacebook_OnEvent);
+	
+	//Adjust Download Link
+	if(os.indexOf("Win32") != -1 && win32_download){
+		DKWidget_SetInnerHtml("DKFacebook_download", "Download for Windows 32");
+	}
+	else if(os.indexOf("Win64") != -1 && win32_download){
+		DKWidget_SetInnerHtml("DKFacebook_download", "Download for Windows 64");
+	}
+	else if(os.indexOf("Android") != -1 && android_download){
+		DKWidget_SetInnerHtml("DKFacebook_download", "Download for Android");
+	}
+	else if(os.indexOf("Mac") != -1){
+		DKWidget_SetInnerHtml("DKFacebook_download", "Download for Mac");
+	}
+	else if(os.indexOf("iOS") != -1){
+		DKWidget_SetInnerHtml("DKFacebook_download", "Download for iOS");
+	}
+	else{
+		DKWidget_RemoveElement("DKFacebook_download");
+		var id = DKWidget_CreateElement("DKFacebook.html","div","notavailable");
+		DKWidget_SetProperty(id, "position", "absolute");
+		DKWidget_SetProperty(id, "top", "60rem");
+		DKWidget_SetProperty(id, "left", "170rem");
+		DKWidget_SetProperty(id, "width", "200rem");
+		DKWidget_SetProperty(id, "color", "red");
+		DKWidget_SetInnerHtml(id, "Not available for "+os);
+	}
 }
 
 /////////////////////////
