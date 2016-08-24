808d2bff93442cf9b75212087c36d1bd4af2d559
aquawicket <aquawicket@hotmail.com>
2016-08-24 14:55:05 -0700

    update

--- DKApps/DigitalKnob/assets/AppsMenu.js
+++ DKApps/DigitalKnob/assets/AppsMenu.js
@@ -24,6 +24,13 @@ function AppsMenu_OnEvent(event)
 		
 		DKCreate("DKApp.js", function(){ 
 			DKApp_UpdateApp("DKFacebook");
+			DKApp_UpdateDescription("If you want to use facebook on your phone, but don't want all of the extra stuff that comes with it, give this a try. It fast, free and simple. <br /> \
+* Super fast <br /> \
+* No notifications <br /> \
+* No data sharing <br /> \
+* No location sharing <br /> \
+* No running processes <br /> \
+* Small, Fast, simple and free <br />");
 		});
 	}
 
