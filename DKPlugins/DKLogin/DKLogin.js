db489bc8853c0fcd8f369f8ca1bf7148c4aea4bc
aquawicket <aquawicket@hotmail.com>
2016-08-29 08:23:16 -0700

    update

--- DKPlugins/DKLogin/DKLogin.js
+++ DKPlugins/DKLogin/DKLogin.js
@@ -1,9 +1,10 @@
 ///////////////////////
 function DKLogin_Init()
 {
-	DKCreate("DKLogin/DKFacebook.js");
+	
 	DKCreate("DKLogin/DKLogin.html");
 	DKAddEvent("DKLoginFacebook", "click", DKLogin_OnEvent);
+	
 }
 
 //////////////////////
@@ -17,33 +18,32 @@ function DKLogin_OnEvent(event)
 {
 	if(DK_Id(event, "DKLoginFacebook")){
 		DKLog("DKLogin_OnEvent(): DKLoginFacebook \n");
-		DKFacebookLogin(DKLogin_FBconnect);
-		/*
-		DKFacebook_Login();
-		DKFacebook_Query('/me', "id", DKLogin_fbresponse);
-		DKFacebook_Query('/me', "name", DKLogin_fbresponse);
-		*/
+		DKCreate("DKLogin/DKFacebook.js", function(){
+			DKFacebookLogin(DKLogin_OnConnect);
+		});
 	}
 }
 
 ////////////////////////////////////////////
-function DKLogin_fbresponse(param, response)
+function DKLogin_FBresponse(param, response)
 {
-	/*
 	if(param == "id"){
-		DKLog("ID: "+response, DKDEBUG);
+		DKLog("ID: "+response);
 	}
 	if(param == "name"){
-		DKLog("Name: "+response, DKDEBUG);
+		DKLog("Name: "+response);
 		DKWidget_SetInnerHtml("DKLoginStatus", response+" logged in.");
 	}
 	
 	DKLog("Facebook response: "+response, DKDEBUG);
-	*/
 }
 
-////////////////////////////
-function DKLogin_FBconnect()
+/////////////////////////////////
+function DKLogin_OnConnect(value)
 {
 	DKLog("DKLogin_FBconnect() \n");
+	if(value.status === 'connected'){
+		DKFacebook_Query('/me', "name", DKLogin_FBresponse);
+		DKFacebook_Query('/me', "id", DKLogin_FBresponse);
+	}
 }
