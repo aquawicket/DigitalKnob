80060cf91d284cfa34d510f21bb9f9d89fa3fab1
aquawicket <aquawicket@hotmail.com>
2016-08-29 08:50:30 -0700

    update

--- DKPlugins/DKLogin/DKLogin.js
+++ DKPlugins/DKLogin/DKLogin.js
@@ -1,10 +1,11 @@
+var FACEBOOK_ID;
+var FACEBOOK_NAME;
+
 ///////////////////////
 function DKLogin_Init()
 {
-	
 	DKCreate("DKLogin/DKLogin.html");
 	DKAddEvent("DKLoginFacebook", "click", DKLogin_OnEvent);
-	
 }
 
 //////////////////////
@@ -24,20 +25,6 @@ function DKLogin_OnEvent(event)
 	}
 }
 
-////////////////////////////////////////////
-function DKLogin_FBresponse(param, response)
-{
-	if(param == "id"){
-		DKLog("ID: "+response);
-	}
-	if(param == "name"){
-		DKLog("Name: "+response);
-		DKWidget_SetInnerHtml("DKLoginStatus", response+" logged in.");
-	}
-	
-	DKLog("Facebook response: "+response, DKDEBUG);
-}
-
 /////////////////////////////////
 function DKLogin_OnConnect(value)
 {
@@ -45,5 +32,26 @@ function DKLogin_OnConnect(value)
 	if(value.status === 'connected'){
 		DKFacebook_Query('/me', "name", DKLogin_FBresponse);
 		DKFacebook_Query('/me', "id", DKLogin_FBresponse);
+		DKWidget_Hide("DKLoginFacebook");
+	}
+}
+
+////////////////////////////////////////////
+function DKLogin_FBresponse(param, response)
+{
+	//DKLog("DKLogin_FBresponse("+param+","+response+")", DKDEBUG);
+	
+	if(param == "name"){
+		FACEBOOK_NAME = response;
+		
+		var id = DKWidget_CreateElement("DKLogin.html", "div", "fbname");
+		DKWidget_SetProperty(id, "font-size", "18rem");
+		DKWidget_SetInnerHtml(id, FACEBOOK_NAME);
+	}
+	if(param == "id"){
+		FACEBOOK_ID = response;
+		
+		var id = DKWidget_CreateElement("DKLogin.html", "img", "fbpic");
+		DKWidget_SetAttribute(id, "src", "http://graph.facebook.com/"+FACEBOOK_ID+"/picture");
 	}
 }
