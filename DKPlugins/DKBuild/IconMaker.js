67787b575addb0671e8fb159ed99edaf2ec6df1c
aquawicket <aquawicket@hotmail.com>
2016-08-31 06:41:18 -0700

    update

---
+++ DKPlugins/DKBuild/IconMaker.js
@@ -0,0 +1,15 @@
+var IMAGEMAGICK = "C:/digitalknob/3rdParty/ImageMagick-7.0.2-10-portable-Q16-x86/";
+
+/////////////////////////
+function IconMaker_Init()
+{
+	IMAGEMAGICK = DKFile_GetShortName(IMAGEMAGICK);
+	DKLog("IMAGEMAGICK="+IMAGEMAGICK+"\n");
+}
+
+///////////////////////////
+function IconMaker_Create()
+{
+	DKLog("Creating Icons...\n", DKINFO);
+	DKLog("TODO\n", DKINFO);
+}
