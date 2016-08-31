7084c4ba0dddb433faebeb5fd56627e68b8b42b5
aquawicket <aquawicket@hotmail.com>
2016-08-31 11:42:54 -0700

    update

--- DKPlugins/DKBuild/IconMaker.js
+++ DKPlugins/DKBuild/IconMaker.js
@@ -17,7 +17,7 @@ function IconMaker_Create(AppPath)
 	DK_Execute(IMAGEMAGICK+"convert "+AppPath+"/icons/icon.png -define icon:auto-resize=256,128,64,48,32,16 "+AppPath+"/icons/windows/icon.ico");
 	
 	//Create Mac Icons
-	if(DK_GetOS == "Mac"){}
+	if(DK_GetOS == "Mac"){
 		DKFile_MkDir(AppPath+"/icons/mac");
 		DK_Execute("sips -z 16 16 "+AppPath+"/icons/icon.png --out "+AppPath+"/icons/mac/icon_16x16.png");
 		DK_Execute("sips -z 32 32 "+AppPath+"/icons/icon.png --out "+AppPath+"/icons/mac/icon_16x16@2x.png");
