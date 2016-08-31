0359d9b6c936a1d1dc2bb46f08b6b1a3fafbd7c0
aquawicket <aquawicket@hotmail.com>
2016-08-31 07:19:14 -0700

    update

--- DKPlugins/DKBuild/IconMaker.js
+++ DKPlugins/DKBuild/IconMaker.js
@@ -13,5 +13,12 @@ function IconMaker_Create(AppPath)
 	DKLog("IconMaker_Create("+AppPath+"+) \n", DKINFO);
 	
 	//Create Android Icons
+	DK_Execute(IMAGEMAGICK+"convert "+AppPath+"/icons/icon.png -resize 72x72 "+AppPath+"/icons/android/drawable-hdpi/icon.png");
+	DK_Execute(IMAGEMAGICK+"convert "+AppPath+"/icons/icon.png -resize 36x36 "+AppPath+"/icons/android/drawable-ldpi/icon.png");
+	DK_Execute(IMAGEMAGICK+"convert "+AppPath+"/icons/icon.png -resize 48x48 "+AppPath+"/icons/android/drawable-mdpi/icon.png");
+	DK_Execute(IMAGEMAGICK+"convert "+AppPath+"/icons/icon.png -resize 96x96 "+AppPath+"/icons/android/drawable-xhdpi/icon.png");
 	DK_Execute(IMAGEMAGICK+"convert "+AppPath+"/icons/icon.png -resize 144x144 "+AppPath+"/icons/android/drawable-xxhdpi/icon.png");
+	
+	//Create Windows Icon
+	DK_Execute(IMAGEMAGICK+"convert "+AppPath+"/icons/icon.png -define icon:auto-resize=256,128,64,48,32,16 "+AppPath+"/icons/windows/icon.ico");
 }
