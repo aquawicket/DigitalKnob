bf2493536b2ce47480ec9c9e963e71e50bf67596
aquawicket <aquawicket@hotmail.com>
2016-08-31 05:06:57 -0700

    update

--- DKApps/DigitalKnob/assets/Digitalknob/AppsMenu.js
+++ DKApps/DigitalKnob/assets/Digitalknob/AppsMenu.js
@@ -4,10 +4,48 @@ function AppsMenu_Init()
 	DKCreate("Digitalknob/AppsMenu.html");
 	DKAddEvent("GLOBAL", "mousedown", AppsMenu_OnEvent);
 	
-	AppsMenu_AddApp("DKDatabase");
-	AppsMenu_AddApp("DKFacebook");
-	AppsMenu_AddApp("DKSDLOS");
-	AppsMenu_AddApp("DKYoutube");
+	AppsMenu_GetApps();
+}
+
+///////////////////////////
+function AppsMenu_GetApps()
+{
+	var AppList = ["DKDatabase", "DKFacebook", "DKSDLOS", "DKYoutube"];
+	for(var i=0; i<AppList.length; i++){
+		AppsMenu_AddApp(AppList[i]);
+	}
+}
+
+//////////////////////////////////////
+function AppsMenu_GetDescription(name)
+{
+	if(name == "DKDatabase"){
+		return "A simple MySql database client. <br />";
+	}
+	
+	if(name == "DKFacebook"){
+		return "If you want to use facebook on your phone, but don't want all of the extra stuff that comes with it, give this a try. It fast, free and simple. <br /> \
+* Super fast <br /> \
+* No notifications <br /> \
+* No data sharing <br /> \
+* No location sharing <br /> \
+* No running processes <br /> \
+* Small, Fast, simple and free <br /><br />";
+	}
+	
+	if(name == "DKSDLOS"){
+		return "Experimental OS";
+	}
+	
+	if(name == "DKYoutube"){
+		return "Introducing, DKYoutube for Android. <br /> \
+Don't you just hate how youtube music stops playing when you turn off your screen. You want to listen to music on youtube, but you want to turn my screen off to save battery!. Try DKYoutube. <br /> \
+* Super small file size <br /> \
+* Faster speeds than the youtube app <br /> \
+* More secure with less data and location sharing <br /> \
+* Turn off your screen, and the music keeps playing <br /> \
+* Small, Fast, simple and free <br /><br />";
+	}
 }
 
 ///////////////////////
@@ -42,35 +80,8 @@ function AppsMenu_OnEvent(event)
 		DKClose("Digitalknob/DKApp.js");
 		DKCreate("Digitalknob/DKApp.js", function(){
 			DKApp_UpdateApp(id);
-			
-			////////DKApp_UpdateDescription() /////////////
-			if(id == "DKDatabase"){
-				DKApp_UpdateDescription("A simple MySql database client. <br />");
-			}
-			if(id == "DKFacebook"){
-				DKApp_UpdateDescription("If you want to use facebook on your phone, but don't want all of the extra stuff that comes with it, give this a try. It fast, free and simple. <br /> \
-* Super fast <br /> \
-* No notifications <br /> \
-* No data sharing <br /> \
-* No location sharing <br /> \
-* No running processes <br /> \
-* Small, Fast, simple and free <br /><br />");
-			}
-			if(id == "DKSDLOS"){
-				DKApp_UpdateDescription("Experimental OS");
-			}
-			if(id == "DKYoutube"){
-				DKApp_UpdateDescription("Introducing, DKYoutube for Android. <br /> \
-Don't you just hate how youtube music stops playing when you turn off your screen. You want to listen to music on youtube, but you want to turn my screen off to save battery!. Try DKYoutube. <br /> \
-* Super small file size <br /> \
-* Faster speeds than the youtube app <br /> \
-* More secure with less data and location sharing <br /> \
-* Turn off your screen, and the music keeps playing <br /> \
-* Small, Fast, simple and free <br /><br />");
-			}
-			//////////////////////////////////////////////	
-		});
-		
+			DKApp_UpdateDescription(AppsMenu_GetDescription(id));
+		});		
 	}
 	
 	if(DK_Id(event, "GLOBAL")){
