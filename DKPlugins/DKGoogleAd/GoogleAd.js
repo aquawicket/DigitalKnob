1bb62a1da977c8ee55341392736c6020e6de8f5c
aquawicket <aquawicket@hotmail.com>
2016-09-02 02:31:49 -0700

    update

---
+++ DKPlugins/DKGoogleAd/GoogleAd.js
@@ -0,0 +1,52 @@
+////////////////////////
+function GoogleAd_Init()
+{
+
+}
+
+//////////////////////////
+function GoogleAd_End()
+{
+/
+}
+
+////////////////////////////////
+function GoogleAd_OnEvent(event)
+{
+/
+}
+
+/////////////////////////////////////////
+function GoogleAd_CreateAd(width, height)
+{
+	// container is where you want the ad to be inserted
+	var id = DKWidget_CreateElement("Digitalknob.html", "div", "DKAdd");
+	DKWidget_SetProperty(id, "position", "absolute");
+	DKWidget_SetProperty(id, "width", width);
+	DKWidget_SetProperty(id, "height", height);
+	DKWidget_SetProperty(id, "text-align", "center");
+	
+	var px_width = document.getElementById(id).clientWidth;
+	var px_height = document.getElementById(id).clientHeight;
+	
+	px_width = Math.min(px_width, 1200);
+	px_height = Math.min(px_height, 1200);
+	window.google_ad_client = "ca-pub-4839505278360003";
+	window.google_ad_slot = "8269654670";
+	window.google_ad_width = px_width;
+	window.google_ad_height = px_height;
+
+	var container = document.getElementById(id);
+	var w = document.write;
+	document.write = function (content) {
+		container.innerHTML = content;
+		document.write = w;	
+	};
+
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'http://pagead2.googlesyndication.com/pagead/show_ads.js';
+	document.body.appendChild(script);
+
+	return id;
+}
