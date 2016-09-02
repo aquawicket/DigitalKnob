fde9c69ed247e4e292c025fd695f896fbe03e898
aquawicket <aquawicket@hotmail.com>
2016-09-02 01:54:07 -0700

    update

--- DKApps/DigitalKnob/assets/Digitalknob/Digitalknob.js
+++ DKApps/DigitalKnob/assets/Digitalknob/Digitalknob.js
@@ -9,6 +9,12 @@ function Digitalknob_Init()
 	DKAddEvent("Image", "click", Digitalknob_OnEvent);
 	DKAddEvent("AppsMenu", "click", Digitalknob_OnEvent);
 	DKAddEvent("Digitalknob_login", "click", Digitalknob_OnEvent);
+	
+	var width = DKWindow_GetWidth();
+	DKLog("width = "+width);
+	var intWidth = Math.min(parseInt(width), 1200);
+	DKLog("intWidth = "+intWidth);
+	Digitalknob_GoogleAd(intWidth, 100);
 }
 
 //////////////////////////
@@ -34,4 +40,32 @@ function Digitalknob_OnEvent(event)
 			DKFrame_Widget("DKLogin.html");
 		});
 	}
+}
+
+////////////////////////////////////////////
+function Digitalknob_GoogleAd(width, height)
+{
+	window.google_ad_client = "ca-pub-4839505278360003";
+	window.google_ad_slot = "8269654670";
+	window.google_ad_width = width;
+	window.google_ad_height = height;
+
+	// container is where you want the ad to be inserted
+	var id = DKWidget_CreateElement("Digitalknob.html", "div", "DKAdd");
+	DKWidget_SetProperty(id, "position", "absolute");
+	DKWidget_SetProperty(id, "bottom", "0px");
+	DKWidget_SetProperty(id, "width", "100%");
+	DKWidget_SetProperty(id, "height", "100px");
+	
+	var container = document.getElementById(id);
+	var w = document.write;
+	document.write = function (content) {
+		container.innerHTML = content;
+		document.write = w;	
+	};
+
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'http://pagead2.googlesyndication.com/pagead/show_ads.js';
+	document.body.appendChild(script);		
 }
