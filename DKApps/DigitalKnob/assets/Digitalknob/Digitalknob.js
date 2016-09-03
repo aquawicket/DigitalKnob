cf7257ac36b819b8f3e12800e0ac8c63ebe92566
aquawicket <aquawicket@hotmail.com>
2016-09-02 17:47:11 -0700

    update

--- DKApps/DigitalKnob/assets/Digitalknob/Digitalknob.js
+++ DKApps/DigitalKnob/assets/Digitalknob/Digitalknob.js
@@ -1,4 +1,4 @@
-//////////////////////
+///////////////////////////
 function Digitalknob_Init()
 {
 	DKCreate("DKFrame/DKFrame.js", function(){});
@@ -15,10 +15,6 @@ function Digitalknob_Init()
 		var id = DKGoogleAd_CreateAd("Digitalknob.html", "100%", "100rem");
 		DKWidget_SetProperty(id, "bottom", "0rem");
 	});
-	
-	//var width = DKWindow_GetWidth();
-	//var id = Digitalknob_GoogleAd("100%", "100rem");
-	//DKWidget_SetProperty(id, "bottom", "0rem");
 }
 
 //////////////////////////
@@ -27,7 +23,7 @@ function Digitalknob_End()
 	DKClose("Digitalknob/Digitalknob.html");
 }
 
-//////////////////////////////
+///////////////////////////////////
 function Digitalknob_OnEvent(event)
 {
 	//DKLog("Digitalknob_OnEvent("+event+") \n", DKDEBUG);
@@ -44,39 +40,4 @@ function Digitalknob_OnEvent(event)
 			DKFrame_Widget("DKLogin.html");
 		});
 	}
-}
-
-////////////////////////////////////////////
-function Digitalknob_GoogleAd(width, height)
-{
-	// container is where you want the ad to be inserted
-	var id = DKWidget_CreateElement("Digitalknob.html", "div", "DKAdd");
-	DKWidget_SetProperty(id, "position", "absolute");
-	DKWidget_SetProperty(id, "width", width);
-	DKWidget_SetProperty(id, "height", height);
-	DKWidget_SetProperty(id, "text-align", "center");
-	
-	var px_width = document.getElementById(id).clientWidth;
-	var px_height = document.getElementById(id).clientHeight;
-	
-	px_width = Math.min(px_width, 1200);
-	px_height = Math.min(px_height, 1200);
-	window.google_ad_client = "ca-pub-4839505278360003";
-	window.google_ad_slot = "8269654670";
-	window.google_ad_width = px_width;
-	window.google_ad_height = px_height;
-
-	var container = document.getElementById(id);
-	var w = document.write;
-	document.write = function (content) {
-		container.innerHTML = content;
-		document.write = w;	
-	};
-
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = 'http://pagead2.googlesyndication.com/pagead/show_ads.js';
-	document.body.appendChild(script);
-
-	return id;
 }
