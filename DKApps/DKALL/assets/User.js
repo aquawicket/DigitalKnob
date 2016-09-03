7b70d245c13e24377a42e0a3b5dc310245a31b50
aquawicket <aquawicket@hotmail.com>
2016-09-03 00:50:17 -0700

    update

--- DKApps/DKALL/assets/User.js
+++ DKApps/DKALL/assets/User.js
@@ -1,6 +1,1 @@
-DKCreate("DKOSGWindow");
-DKCreate("DKOSGRocket");
-DKCreate("DKDev/DKDev.js");
-//DKCreate("DKUpdate"); 
-//DKCreate("yourfile.js"); //or
-//DKCreate("yourfile.html");
+//Do nothing
