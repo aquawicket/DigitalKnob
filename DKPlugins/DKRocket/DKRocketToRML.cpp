c290df94b7e6b7c346d2c3d2c796f113d72c4f3a
aquawicket <aquawicket@hotmail.com>
2016-09-07 14:56:56 -0700

    update

--- DKPlugins/DKRocket/DKRocketToRML.cpp
+++ DKPlugins/DKRocket/DKRocketToRML.cpp
@@ -113,7 +113,11 @@ bool DKRocketToRML::PostProcess(Rocket::Core::Element* element)
 		DKString iLeft = toString(iframes[i]->GetAbsoluteLeft());
 		DKString iWidth = toString(iframes[i]->GetClientWidth());
 		DKString iHeight = toString(iframes[i]->GetClientHeight());
-		DKString url = iframes[i]->GetAttribute("src")->Get<Rocket::Core::String>().CString();
+
+		DKString url = "";
+		if(iframes[i]->GetAttribute("src")){
+			 url = iframes[i]->GetAttribute("src")->Get<Rocket::Core::String>().CString();
+		}
 		//DKLog("DKCef Calculated: top:"+iTop+" left:"+iLeft+" width:"+iWidth+" height:"+iHeight+" \n", DKINFO);
 		
 		//TODO
