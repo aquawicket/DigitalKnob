ba5fd0121b99fb08db2191bdef3edf9cd16cd6e2
aquawicket <aquawicket@hotmail.com>
2016-10-03 16:27:22 -0700

    update

--- DKPlugins/DKRocket/DKRocket.js
+++ DKPlugins/DKRocket/DKRocket.js
@@ -1,5 +1,45 @@
-//var window = new Object();
 DKLog("*************  DKRocket.js *******************\n");
+//DKCreate("DKWidget");
 
-var navigator = new Object();
-navigator.userAgent = "Rocket/1.3.1";
+var navigator = new Navigator();
+var document = new Document();
+var window = new Window();
+
+///////////////////
+function Document()
+{
+	this.body = new Element("body");
+	this.getElementById = function(id){ return new Element(id); }
+	this.hasFocus = function(){ return true;}
+}
+
+/////////////////////
+function Element(id){
+	this.id = id;
+	//this.parentNode = DKWidget_GetParent(id);
+}
+
+////////////////////
+function Navigator()
+{
+	this.appCodeName = "Rocket";
+	this.appName = "Rocket";
+	this.appVersion = "Rocket/1.3.1";
+	this.cookieEnabled = false;
+	//this.geolocation;
+	this.javaEnabled = function(){	return false; }
+	this.language = "en-US";
+	this.onLine = false;
+	this.platform = DK_GetOS();
+	this.product = "Rocket";
+	this.userAgent = "Rocket/1.3.1";
+}
+
+/////////////////
+function Window()
+{
+	//this.closed = false;
+	//this.defaultStatus;
+	this.document = document;
+	//this.frameElement = new Element();
+}
