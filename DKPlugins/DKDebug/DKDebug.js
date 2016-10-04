94dc5b45a16988f5b453da1344f2a4421134ff86
aquawicket <aquawicket@hotmail.com>
2016-10-04 12:47:06 -0700

    update

--- DKPlugins/DKDebug/DKDebug.js
+++ DKPlugins/DKDebug/DKDebug.js
@@ -1,19 +1,19 @@
-/////////////////////////
+///////////////////////
 function DKDebug_Init()
 {
-	DKAddEvent("GLOBAL", "keydown", DKDebug_OnEvent);
+	DKAddEvent("GLOBAL", "keypress", DKDebug_OnEvent);
 }
 
 //////////////////////
 function DKDebug_End()
 {
-
+	DKRemoveEvent("GLOBAL", "keypress", DKDebug_OnEvent);
 }
 
 ///////////////////////////////
 function DKDebug_OnEvent(event)
 {
-	if(DK_Type(event, "click")){
-		//Function();
+	if(DK_Type(event, "keypress")){
+		DKLog("Unicode CHARACTER code: "+DKWidget_GetValue(event)+"\n");
 	}
 }
