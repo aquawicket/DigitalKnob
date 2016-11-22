2ca084933d57af2999cb0e90ef42860ae96ddb38
aquawicket <aquawicket@hotmail.com>
2016-11-22 14:16:16 -0800

    update

--- DKPlugins/DKCef/DKCefApp.h
+++ DKPlugins/DKCef/DKCefApp.h
@@ -3,15 +3,45 @@
 #define DKCefApp_H
 #include <include/cef_app.h>
 
+
+///////////////////////////////////////
+class MyV8Handler : public CefV8Handler
+{
+public:
+	MyV8Handler() {}
+	virtual bool Execute(const CefString& name, CefRefPtr<CefV8Value> object, const CefV8ValueList& arguments, 
+						CefRefPtr<CefV8Value>& retval, CefString& exception) OVERRIDE {
+		DKLog("MyV8Handler::Execute() \n", DKDEBUG);
+		if (name == "myfunc") {
+			// Return my string value.
+			retval = CefV8Value::CreateString("My Value!");
+			return true;
+		}
+
+		// Function does not exist.
+		return false;
+	}
+
+	// Provide the reference counting implementation for this class.
+	IMPLEMENT_REFCOUNTING(MyV8Handler);
+};
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
 class DKCefApp : public CefApp, public CefBrowserProcessHandler, public CefRenderProcessHandler
 {
 public:
 	DKCefApp(){}
+
+	CefRefPtr<CefV8Handler> handler;
+
 	virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() OVERRIDE { return this; }
 	virtual CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() OVERRIDE { return this; }
 
-	void OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line){
+	virtual void OnContextInitialized();
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	virtual void OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line)
+	{
 		//DKLog("DKCefApp::OnBeforeCommandLineProcessing() \n", DKDEBUG);
 		command_line->AppendSwitchWithValue("enable-system-flash", "1");
 		command_line->AppendSwitchWithValue("allow-file-access-from-files", "1");
@@ -24,29 +54,19 @@ public:
 		//command_line->AppendSwitchWithValue("enable-begin-frame-scheduling", "1"); //Breaks Popups
 	}
 
-	void OnContextInitialized();
-
-	// FIXME: OnContextCreated never called
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	virtual void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) OVERRIDE
 	{
 		DKLog("DKCefApp::OnContextCreated() \n", DKDEBUG);
-		// Retrieve the context's window object.
-		CefRefPtr<CefV8Value> object = context->GetGlobal();
-		//CefRefPtr<CefV8Handler> handler = (CefV8Handler*)this;
-		//CefRefPtr<CefV8Value> func = CefV8Value::CreateFunction("myfunc", handler);
+		CefRefPtr<CefV8Value> object = context->GetGlobal(); // Retrieve the context's window object.
+		handler = new MyV8Handler();
+		CefRefPtr<CefV8Value> func = CefV8Value::CreateFunction("myfunc", handler);
 		//object->SetValue("register", CefV8Value::CreateFunction("register", handler), V8_PROPERTY_ATTRIBUTE_NONE);
-	}
-
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	bool OnProcessMessageReceived(CefRefPtr< CefBrowser > browser, CefProcessId source_process, CefRefPtr<CefProcessMessage> message)
-	{
-		DKLog("DKCefApp::OnProcessMessageReceived() \n", DKDEBUG);
-		return true;
 	}
 
 	IMPLEMENT_REFCOUNTING(DKCefApp);
 };
+
 
 
 #endif //DKCefApp_H
