4a7029025977800b5df7c04b7c2d1e491f73d672
aquawicket <aquawicket@hotmail.com>
2016-11-30 19:01:14 -0800

    update

--- DKPlugins/DKDebug/Input.js
+++ DKPlugins/DKDebug/Input.js
@@ -3,7 +3,7 @@
 function Input_Init()
 {
 	DKCreate("DKDebug/Input.html");
-	//DKAddEvent("Input_Button", "click", Input_OnEvent);
+	DKAddEvent("Input_Text", "keydown", Input_OnEvent);
 }
 
 ////////////////////
@@ -15,7 +15,16 @@ function Input_End()
 /////////////////////////////
 function Input_OnEvent(event)
 {
-	//if(DK_Id(event, "Input_Button")){
-	//	Input_CreateReport();
-	//}
+	if(DK_Id(event, "Input_Text")){
+		var key = DKWidget_GetValue(event);
+		//DKLog("Input_Text: key="+key+"\n");
+		if(key != 72){ return; }
+		Input_Run(DKWidget_GetValue("Input_Text"))
+	}
+}
+
+///////////////////////////
+function Input_Run(command)
+{
+	DK_RunJavascript(command);
 }
