b72f4b2f294e2102ce65754f6369c703a4ec025a
aquawicket <aquawicket@hotmail.com>
2016-11-30 17:56:21 -0800

    update

--- DKApps/DKIDE/assets/User.js
+++ DKApps/DKIDE/assets/User.js
@@ -32,6 +32,7 @@ else{
 	DKCreate("DKDev/DKDev.js", function(){});
 	DKCreate("DKDev/DKMenuRight.js", function(){});
 	DKWidget_SetProperty("DKMenuRight.html","right","0rem");
+	
 	/*
 	if(DK_GetBrowser() != "CEF"){ 
 		DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
