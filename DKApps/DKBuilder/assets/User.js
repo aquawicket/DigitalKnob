838c06d256c27b8a93bfd64690c3493fd5498268
aquawicket <aquawicket@hotmail.com>
2016-12-08 01:40:07 -0800

    update

--- DKApps/DKBuilder/assets/User.js
+++ DKApps/DKBuilder/assets/User.js
@@ -1,3 +1,8 @@
+var DKERROR = 1;     //Red
+var DKWARN = 2;      //Yellow
+var DKINFO = 3;      //White
+var DKDEBUG = 4;     //Blue
+
 DKCreate("DKBuild/DKBuild.js");
 //DKCreate("DKDebug/DKDebug.js"); //TODO for console apps
 //DKCreate("DKUpdate");
