495e26eafb59e0d5d6f33e1d2057f8d1a4d44d67
aquawicket <aquawicket@hotmail.com>
2016-12-11 15:15:38 -0800

    update

--- DKPlugins/DK/DK.js
+++ DKPlugins/DK/DK.js
@@ -9,25 +9,25 @@ var LOG_SHOW = ""; //comma seperated
 var LOG_HIDE = ""; //comma seperated
 
 // Dummy functions only implemented in c++
-function DK_DoFrame(){ /*DKLog("DK_ClearEvents(): not available for javascript", DKERROR); return;*/ }
-function EventLoop(){ /*DKLog("DK_ClearEvents(): not available for javascript", DKERROR); return;*/ }
+function DK_DoFrame(){ /*DKLog("DK_ClearEvents(): not available for "+DK_GetBrowser()+"\n", DKWARN);*/ }
+function EventLoop(){ /*DKLog("DK_ClearEvents(): not available for "+DK_GetBrowser()+"\n", DKWARN);*/ }
 EventLoop.run = function(){};
 
 if(DK_GetBrowser() != "CEF"){
-	function DK_ClearEvents(){DKLog("DK_ClearEvents(): not available for javascript", DKERROR); return; }
-	function DKRocket_Reload(){DKLog("DKRocket_Reload(): not available for javascript", DKERROR); return; }
-	function DK_CallFunc(var1, var2, var3){ DKLog("DK_CallFunc(): not available for javascript", DKERROR); return ""; }
-	function DK_Queue(var1, var2, var3){ DKLog("DK_Queue(): not available for javascript", DKERROR); }
-	function DK_LeftClick(){DKLog("DK_LeftClick(): not available for javascript", DKERROR); return; }
-	function DK_RightClick(){DKLog("DK_RightClick(): not available for javascript", DKERROR); return; }
-	function DK_SetCursorPos(){ DKLog("DK_SetCursorPos(): not available for javascript", DKERROR); return; }
-	function DKHook_GetWindows(){DKLog("DKHook_GetWindows(): not available for javascript", DKERROR); return; }
-	function DK_TestCrash(){DKLog("DK_TestCrash(): not available for javascript", DKERROR); return; }
-	function DK_LogGuiConsole(){DKLog("DK_LogGuiConsole(): not available for javascript", DKERROR); return; }
-	function DK_GetScreenWidth(){DKLog("DK_GetScreenWidth(): not available for javascript", DKERROR); return; }
-	function DK_GetScreenHeight(){DKLog("DK_GetScreenHeight(): not available for javascript", DKERROR); return; }
-	function DK_System(){DKLog("DK_System(): not available for javascript", DKERROR); return; }
-	function DK_PrintFunctions(){DKLog("DK_PrintFunctions(): not available for javascript", DKERROR); return; }
+	function DK_ClearEvents(){DKLog("DK_ClearEvents(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DKRocket_Reload(){DKLog("DKRocket_Reload(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_CallFunc(var1, var2, var3){ DKLog("DK_CallFunc(): not available for "+DK_GetBrowser()+"\n", DKWARN); return ""; }
+	function DK_Queue(var1, var2, var3){ DKLog("DK_Queue(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_LeftClick(){DKLog("DK_LeftClick(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_RightClick(){DKLog("DK_RightClick(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_SetCursorPos(){ DKLog("DK_SetCursorPos(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DKHook_GetWindows(){DKLog("DKHook_GetWindows(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_TestCrash(){DKLog("DK_TestCrash(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_LogGuiConsole(){DKLog("DK_LogGuiConsole(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_GetScreenWidth(){DKLog("DK_GetScreenWidth(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_GetScreenHeight(){DKLog("DK_GetScreenHeight(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_System(){DKLog("DK_System(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
+	function DK_PrintFunctions(){DKLog("DK_PrintFunctions(): not available for "+DK_GetBrowser()+"\n", DKWARN); }
 }
 
 var DKERROR = 1;     //Red
