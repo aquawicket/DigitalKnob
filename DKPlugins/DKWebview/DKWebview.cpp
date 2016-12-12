698c8de4b8c3422bc23803467f4f4b1291b62c10
aquawicket <aquawicket@hotmail.com>
2016-12-11 21:17:48 -0800

    update

---
+++ DKPlugins/DKWebview/DKWebview.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#ifdef ANDROID
+#include "DKAndroid.h"
+#include "DKApp.h"
+#include "DKClass.h"
+
+
+//////////////////////
+void DKWebView::init()
+{
+	DKLog("DKWebview::init()\n", DKDEBUG);
+}
+
+
+#endif //ANDROID
---
+++ DKPlugins/DKWebview/DKWebview.h
@@ -0,0 +1,17 @@
+#ifdef ANDROID
+
+#pragma once
+#ifndef DKWebview_H
+#define DKWebview_H
+
+///////////////
+class DKWebView
+{
+public:
+	static void init();
+};
+
+
+
+#endif
+#endif
