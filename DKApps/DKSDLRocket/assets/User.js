819d760b57c8eacfa342a6ad1fe6942ab77cee73
aquawicket <aquawicket@hotmail.com>
2016-12-12 19:52:40 -0800

    update

--- DKApps/DKSDLRocket/assets/User.js
+++ DKApps/DKSDLRocket/assets/User.js
@@ -11,3 +11,15 @@ DKWidget_SetProperty("DKNotepad.html", "width", "100%");
 DKWidget_SetProperty("DKNotepad.html", "height", "100%");
 
 
+/////////////////////////
+//ANDROID back key = exit
+DKAddEvent("GLOBAL", "keydown", User_OnEvent);
+function User_OnEvent(event)  //Duktape
+{
+	if(DK_Type(event, "keydown")){
+		if(DKWidget_GetValue(event) == "4"){ //Exit for ANDROID
+		    DK_Exit();
+		}
+	}
+}
+//////////////////////////
