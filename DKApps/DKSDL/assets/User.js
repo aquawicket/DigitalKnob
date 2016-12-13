72127d620e592668ba707bc15229f49270a35126
aquawicket <aquawicket@hotmail.com>
2016-12-12 20:02:01 -0800

    update

--- DKApps/DKSDL/assets/User.js
+++ DKApps/DKSDL/assets/User.js
@@ -1,1 +1,14 @@
 DKCreate("DKSDLWindow");
+
+/////////////////////////
+//ANDROID back key = exit
+DKAddEvent("GLOBAL", "keydown", User_OnEvent);
+function User_OnEvent(event)  //Duktape
+{
+	if(DK_Type(event, "keydown")){
+		if(DKWidget_GetValue(event) == "4"){ //Exit for ANDROID
+		    DK_Exit();
+		}
+	}
+}
+//////////////////////////
