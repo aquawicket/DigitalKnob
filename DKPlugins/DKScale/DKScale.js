56fec241b93638a6c31a2e5a635f57d5ee422130
aquawicket <aquawicket@hotmail.com>
2016-12-14 20:03:49 -0800

    update

--- DKPlugins/DKScale/DKScale.js
+++ DKPlugins/DKScale/DKScale.js
@@ -24,18 +24,18 @@ function DKScale_OnEvent(event)
 ////////////////////////
 function DKScale_Resize()
 {
+    DKLog("DKWindow_GetPixelRatio() = "+DKWindow_GetPixelRatio()+"\n", DKINFO);
 	//Set the scale
 	if((parseInt(DKWindow_GetHeight()) > parseInt(DKWindow_GetWidth())) && (parseFloat(DKWindow_GetPixelRatio()) > 1)){
 		var pixel_ratio = DKWindow_GetPixelRatio();
-		DKLog("DKWindow_GetPixelRatio() = "+pixel_ratio+"\n", DKINFO);
 		if(pixel_ratio > 1.0){//= Math.min(2, pixel_ratio);
 			pixel_ratio = 2.0;
 			DKWidget_SetProperty("html", "font-size", pixel_ratio+"px");
-			DKLog("DKScale_Resize(): scale set to "+pixel_ratio+"px\n", DKDEBUG);
+			DKLog("DKScale_Resize(): scale set to "+pixel_ratio+"px\n", DKINFO);
 		}
 	}
 	else{
 		DKWidget_SetProperty("html", "font-size", "1px");
-		DKLog("DKScale_Resize(): scale set to 1px\n", DKDEBUG);
+		DKLog("DKScale_Resize(): scale set to 1px\n", DKINFO);
 	}
 }
