61dbe35e93d48c50abefde385e099c93ad0e9d18
aquawicket <aquawicket@hotmail.com>
2017-01-09 19:20:04 -0800

    commit from git

--- DKApps/DKSDLCef/assets/User.js
+++ DKApps/DKSDLCef/assets/User.js
@@ -6,16 +6,18 @@ if(DK_GetBrowser() == "Rocket"){
 }
 
 DKCreate("DKDebug/DKDebug.js", function(){});
+DKWidget_SetProperty("body","background-color","blue");
+
 
 var assets = DKAssets_LocalAssets();
 var url = "http://www.google.com";
 var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
 DKWidget_SetAttribute(iframe, "src", url);
 DKWidget_SetProperty(iframe, "position", "absolute");
-DKWidget_SetProperty(iframe, "top", "0rem");
-DKWidget_SetProperty(iframe, "left", "0rem");
-DKWidget_SetProperty(iframe, "width", "100%");
-DKWidget_SetProperty(iframe, "bottom", "0rem");
+DKWidget_SetProperty(iframe, "top", "20rem");
+DKWidget_SetProperty(iframe, "left", "20rem");
+DKWidget_SetProperty(iframe, "right", "20rem");
+DKWidget_SetProperty(iframe, "bottom", "20rem");
 var currentBrowser = DKCef_GetCurrentBrowser(iframe);
 DKCef_SetUrl(iframe, url, currentBrowser);
 DKCef_SetFocus(iframe);
