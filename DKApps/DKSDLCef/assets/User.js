1702e4d44edb1506e952867b06952e5f56f23703
aquawicket <aquawicket@hotmail.com>
2017-01-10 14:30:08 -0800

    commit from git

--- DKApps/DKSDLCef/assets/User.js
+++ DKApps/DKSDLCef/assets/User.js
@@ -1,23 +1,8 @@
-
-if(DK_GetBrowser() == "Rocket"){
-	DKCreate("DKWindow");
-	DKCreate("DKRocket");
-	DKCreate("DKWidget");
-}
-
+DKCreate("DKWindow");
 DKCreate("DKDebug/DKDebug.js", function(){});
-DKWidget_SetProperty("body","background-color","blue");
-
-
 var assets = DKAssets_LocalAssets();
 var url = "http://www.google.com";
-var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
-DKWidget_SetAttribute(iframe, "src", url);
-DKWidget_SetProperty(iframe, "position", "absolute");
-DKWidget_SetProperty(iframe, "top", "20rem");
-DKWidget_SetProperty(iframe, "left", "20rem");
-DKWidget_SetProperty(iframe, "right", "20rem");
-DKWidget_SetProperty(iframe, "bottom", "20rem");
-var currentBrowser = DKCef_GetCurrentBrowser(iframe);
-DKCef_SetUrl(iframe, url, currentBrowser);
-DKCef_SetFocus(iframe);
+DKCreate("DKCef,CefSDL,0,0,400,400,"+url);
+var currentBrowser = DKCef_GetCurrentBrowser("CefSDL");
+DKCef_SetUrl("CefSDL", url, currentBrowser);
+DKCef_SetFocus("CefSDL");
