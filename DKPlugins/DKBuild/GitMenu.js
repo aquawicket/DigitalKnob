d5cfb0b49a7d5bb9b4433559840c0b33b1225250
aquawicket <aquawicket@hotmail.com>
2017-01-10 14:06:41 -0800

    commit from git

---
+++ DKPlugins/DKBuild/GitMenu.js
@@ -0,0 +1,37 @@
+///////////////////////
+function GitMenu_Init()
+{
+	DKCreate("DKBuild/GitMenu.html");
+	DKWidget_SetProperty("GitMenu.html","top",DKWindow_GetMouseY()+"px");
+	DKWidget_SetProperty("GitMenu.html","left",DKWindow_GetMouseX()+"px");
+	DKAddEvent("GLOBAL", "mousedown", GitMenu_OnEvent);
+	DKAddEvent("Git Update", "click", GitMenu_OnEvent);
+	DKAddEvent("Git Commit", "click", GitMenu_OnEvent);
+}
+
+//////////////////////
+function GitMenu_End()
+{
+	DKRemoveEvent("GLOBAL", "mousedown", GitMenu_OnEvent);
+	DKClose("DKBuild/GitMenu.html");
+}
+
+///////////////////////////////
+function GitMenu_OnEvent(event)
+{
+	DKLog("GitMenu_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
+	
+	if(DK_Id(event,"Git Update")){
+		DKThread_DKQueue("GitUpdate","DKBuild_GitUpdate();");
+	}
+	if(DK_Id(event,"Git Commit")){
+		DKThread_DKQueue("GitCommit","DKBuild_GitCommit();");
+	}
+	
+	if(DK_Id(event, "GLOBAL")){
+		if(DKWidget_IsChildOf(DKWidget_GetHoverElement(), "GitMenu.html")){
+			return;
+		}
+	}
+	DKClose("DKBuild/GitMenu.js");
+}
