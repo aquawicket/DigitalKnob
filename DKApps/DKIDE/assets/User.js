0037262c6f52303893e397d70492e94fdf03b55b
aquawicket <aquawicket@hotmail.com>
2017-01-13 02:09:52 -0800

    commit from git

--- DKApps/DKIDE/assets/User.js
+++ DKApps/DKIDE/assets/User.js
@@ -1,5 +1,8 @@
 if(DK_GetOS() == "Win32" || DK_GetOS() == "Win64"){
-	var USE_CEF = true;
+	var USE_CEF = 1;
+}
+if(DK_GetOS() == "Android"){
+	var USE_Webview = 1;
 }
 
 if(DK_GetBrowser() == "Rocket"){
@@ -11,6 +14,21 @@ if(DK_GetBrowser() == "Rocket"){
 
 DKCreate("DKDebug/DKDebug.js", function(){});
 
+////////////////////////////
+function User_OnEvent(event)  //Duktape
+{
+	DKLog("User_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
+	
+	if(DK_Type(event, "DKCef_OnQueueNewBrowser")){
+		var currentBrowser = DKCef_GetCurrentBrowser("DKCef_frame");
+		DKCef_SetUrl("DKCef_frame", DKWidget_GetValue(event), currentBrowser);
+	}
+	if(DK_Type(event, "keydown")){
+		if(DKWidget_GetValue(event) == "4"){ //Exit for ANDROID
+		    DK_Exit();
+		}
+	}
+}
 
 if(DK_GetBrowser() == "Rocket" && USE_CEF){
 	var assets = DKAssets_LocalAssets();
@@ -26,13 +44,19 @@ if(DK_GetBrowser() == "Rocket" && USE_CEF){
 	DKCef_SetUrl(iframe, url, currentBrowser);
 	DKCef_SetFocus(iframe);
 	
+	DKAddEvent("GLOBAL", "DKCef_OnQueueNewBrowser", User_OnEvent);
 	/*
 	DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
 		var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
 	});
 	*/
 }
-else{
+else if(DK_GetBrowser() == "Rocket" && USE_Webview){ //Duktape
+	var assets = DKAssets_LocalAssets();
+	var url = "file:///"+assets+"/index.html";
+	DKAddEvent("GLOBAL", "keydown", User_OnEvent);
+}
+else{  //Duktape or V8 or Webview
 	DKWidget_SetProperty("body","background-color","grey");
 	DKCreate("DKScale/DKScale.js", function(){});
 	DKCreate("DKBuild/DKBuild.js", function(){});
