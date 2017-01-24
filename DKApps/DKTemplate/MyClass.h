79056ae8b97f1b657fbb63f75cb0f09773f749ae
aquawicket <aquawicket@hotmail.com>
2017-01-24 01:14:39 -0800

    commit from git

--- DKApps/DKTemplate/MyClass.h
+++ DKApps/DKTemplate/MyClass.h
@@ -5,37 +5,37 @@ class MyClass
 public:
 	MyClass()
 	{
-		MyFuncs::RegisterFunc("MyClass::GetNumber", &MyClass::GetNumber, this);
-		MyFuncs::RegisterFunc("MyClass::GetString", &MyClass::GetString, this);
+		MyFuncs::RegisterFunc("MyClass::GetNumber",  &MyClass::GetNumber,  this);
+		MyFuncs::RegisterFunc("MyClass::GetString",  &MyClass::GetString,  this);
 		MyFuncs::RegisterFunc("MyClass::EditNumber", &MyClass::EditNumber, this);
 		MyFuncs::RegisterFunc("MyClass::EditString", &MyClass::EditString, this);
 	}
 
 	void GetNumber(void* input, void*& output)
 	{
-		int val = 1234;
-		output = static_cast<void*>(new int(val));             //WORKS, val eats memory
-		//output = static_cast<void*>(&val);                   //val goes out of scope
+		int var = 1234;
+		output = static_cast<void*>(new int(var));             //WORKS, var eats memory
+		//output = static_cast<void*>(&var);                   //ERROR, var goes out of scope
 	}
 	
 	void GetString(void* input, void*& output)
 	{
-		std::string val = "Get test";
-		output = static_cast<void*>(new std::string(val));     //WORKS, val eats memory
-		//output = static_cast<void*>(&val);                   //val goes out of scope
+		std::string var = "Get test";
+		output = static_cast<void*>(new std::string(var));     //WORKS, var eats memory
+		//output = static_cast<void*>(&var);                   //ERROR, var goes out of scope
 	}
 	
 	void EditNumber(void* input, void*& output)
 	{
-		int val = *static_cast<int*>(input);                   //val gets 4321 OK
-		//output = static_cast<void*>(new int(val));           //WORKS, val eats memory
-		output = static_cast<void*>(&val);                     //val goes out of scope
+		int var = *static_cast<int*>(input);                   //WORKS, var gets 4321 OK
+		output = static_cast<void*>(new int(var));             //WORKS, var eats memory
+		//output = static_cast<void*>(&var);                   //ERROR, var goes out of scope
 	}
 	
 	void EditString(void* input, void*& output)
 	{
-		std::string val = *static_cast<std::string*>(input);   //val gets "Edit test" OK
-		//output = static_cast<void*>(new std::string(val));   //WORKS, val eats memory
-		output = static_cast<void*>(&val);                     //val goes out of scope
+		std::string var = *static_cast<std::string*>(input);   //WORKS, var gets "Edit test" OK
+		output = static_cast<void*>(new std::string(var));     //WORKS, var eats memory
+		//output = static_cast<void*>(&var);                   //ERROR, var goes out of scope
 	}
 };
