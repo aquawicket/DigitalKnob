89f6b556a55d7d8bf43de60deda6fc018f14b472
aquawicket <aquawicket@hotmail.com>
2017-01-24 04:13:26 -0800

    commit from git

--- DKPlugins/DKSDLWindow/DKSDLWindow.cpp
+++ DKPlugins/DKSDLWindow/DKSDLWindow.cpp
@@ -113,7 +113,7 @@ void DKSDLWindow::Init()
 
 	DKString result;
 	sdlren = NULL;
-	if(!has(sdl_renderer, "SOFTWARE")){
+	if(!same(sdl_renderer, "SOFTWARE")){
 		result = "SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC";
 		sdlren = SDL_CreateRenderer(sdlwin, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	}
