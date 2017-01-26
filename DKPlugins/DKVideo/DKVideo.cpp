2cd086dc6b27f4dbd8510910534f7c6f50aeb8bc
aquawicket <aquawicket@hotmail.com>
2017-01-25 18:16:21 -0800

    commit from git

--- DKPlugins/DKVideo/DKVideo.cpp
+++ DKPlugins/DKVideo/DKVideo.cpp
@@ -11,6 +11,6 @@ void DKVideo::Init()
 //////////////////////////////////
 void DKVideo::Play(DKString& file)
 {
-	DKClass::CallFunc("DKOSGVideo::Play", static_cast<void*>(&file));
-	DKClass::CallFunc("DKSDLVideo::Play", static_cast<void*>(&file));
+	DKClass::CallFunc("DKOSGVideo::Play", &file, NULL);
+	DKClass::CallFunc("DKSDLVideo::Play", &file, NULL);
 }
