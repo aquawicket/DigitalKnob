16743e30caa45e378be820088ebe7cdb2b887791
dkuser <dkuser@digitalknob.com>
2017-01-29 14:01:18 -0800

    commit from git

--- DKPlugins/DKDebug/DKDebug.js
+++ DKPlugins/DKDebug/DKDebug.js
@@ -88,11 +88,12 @@ function DKDebug_KeyToChar(key)
 /////////////////////////////
 function DKDebug_RestartApp()
 {
+if(DK_GetBrowser() != "Rocket"){
 	//DKFrame_CloseAll();
 	DKWidget_SetInnerHtml("body", "");
-	DKClose("DKDebug.js");
+	//DKClose("DKDebug.js");
 	DK_Reload();
-	
+}
 }
 
 //////////////////////////////
