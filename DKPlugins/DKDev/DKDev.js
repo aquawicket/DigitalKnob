a4bfd04b7afa54747b15524262815c9b9c9c39d7
dkuser <dkuser@digitalknob.com>
2017-01-29 14:44:20 -0800

    commit from git

--- DKPlugins/DKDev/DKDev.js
+++ DKPlugins/DKDev/DKDev.js
@@ -32,7 +32,7 @@ function DKDev_End()
 	DKRemoveEvent("GLOBAL", "SaveHtmlFiles", DKDev_OnEvent);
 	DKRemoveEvent("GLOBAL", "OpenFile", DKDev_OnEvent);
 	DKRemoveEvent("GLOBAL", "FileChoice", DKDev_OnEvent);
-	DKClose("DKDev/DKDev.html");
+	DKClose("DKDev.html");
 }
 
 ///////////////////
