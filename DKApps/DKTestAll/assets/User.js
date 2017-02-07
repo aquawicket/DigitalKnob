3eed618d57a6991d67a082b877a224db1a07dd73
dkuser <dkuser@digitalknob.com>
2017-02-07 05:26:10 -0800

    commit from git

--- DKApps/DKTestAll/assets/User.js
+++ DKApps/DKTestAll/assets/User.js
@@ -4,7 +4,6 @@ var welcome = "\n";
 welcome = "######################### \n";
 welcome += "####### DKTestAll ####### \n";
 welcome += "######################### \n";
-
 DKLog(welcome, DKINFO);
 
 //TestJavascript
@@ -12,11 +11,7 @@ DKCreate("TestJavascript.js");
 DKClose("TestJavascript.js");
 
 //TestWindow
-//DK_GetKey();
-//DKLog("Press any key run  TestWindow.js\n", DKINFO);
 DKCreate("TestWindow.js");
 
 //TestRocket
-//DK_GetKey();
-//DKLog("Press any key run  TestRocket.js\n", DKINFO);
 DKCreate("TestRocket.js");
