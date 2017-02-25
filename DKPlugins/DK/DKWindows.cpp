dac486946fe4b046123a4cda72f5e735ac126a6f
dkuser <dkuser@digitalknob.com>
2017-02-25 14:34:18 -0800

    commit from git

--- DKPlugins/DK/DKWindows.cpp
+++ DKPlugins/DK/DKWindows.cpp
@@ -6,6 +6,7 @@
 #include "conio.h"
 #include <mmdeviceapi.h>
 #include <endpointvolume.h>
+#include <psapi.h>
 
 
 //////////////////////////////////////////////////
@@ -460,7 +461,45 @@ bool DKWindows::GetLastError(DKString& error)
 //////////////////////////////////////////////
 bool DKWindows::GetProcessList(DKString& list)
 {
-	list = "this,is,a,test";
+	//list = "this,is,a,test";
+
+	// Get the list of process identifiers.
+    DWORD aProcesses[1024], cbNeeded, cProcesses;
+    unsigned int i;
+    if(!EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded)){ return 0; }
+
+
+    // Calculate how many process identifiers were returned.
+    cProcesses = cbNeeded / sizeof(DWORD);
+
+    // Print the name and process identifier for each process.
+    for(i = 0; i < cProcesses; i++){
+        if(aProcesses[i] != 0){
+			TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
+			
+			//Get a handle to the process.
+			HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,FALSE, aProcesses[i]);
+
+			//Get the process name.
+			if(NULL != hProcess){
+				HMODULE hMod;
+				DWORD cbNeeded;
+
+				if(EnumProcessModules(hProcess, &hMod, sizeof(hMod),  &cbNeeded)){
+					GetModuleBaseName( hProcess, hMod, szProcessName, sizeof(szProcessName)/sizeof(TCHAR) );
+				}
+			}
+
+			// Print the process name and identifier.
+			//tprintf( TEXT("%s  (PID: %u)\n"), szProcessName, processID );
+			list += toString(szProcessName);
+			list += ",";
+
+			//Release the handle to the process.
+			CloseHandle(hProcess);
+        }
+    }
+
 	return true;
 }
 
