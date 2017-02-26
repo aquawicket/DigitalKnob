975718c47405fdcb2a498057f2a5ee09f6f4fed5
dkuser <dkuser@digitalknob.com>
2017-02-25 17:30:50 -0800

    commit from git

---
+++ DKPlugins/DKBuild/DKSolution.js
@@ -0,0 +1,201 @@
+var event_type;
+var event_id;
+var event_data1;
+var event_data2;
+var aPath;
+var rPath;
+
+////////////////////////////
+function DKSolution_Init()
+{	
+	DKCreate("DKFile/DKSolution.html");
+	DKAddEvent("DKSolution.html", "GetFile", DKSolution_OnEvent);
+	DKAddEvent("DKSolutionCancel", "click", DKSolution_OnEvent);
+	DKAddEvent("DKSolutionOK", "click", DKSolution_OnEvent);
+	DKAddEvent("DKSolutionUp", "click", DKSolution_OnEvent);
+	
+	aPath = "";
+	rPath = "";
+	var drives = DKFile_GetDrives(); //TODO
+	DKLog(drives+"\n", DKDEBUG);
+}
+
+///////////////////////////
+function DKSolution_End()
+{
+	DKRemoveEvent("DKSolution.html", "GetFile", DKSolution_OnEvent);
+	DKRemoveEvent("DKSolutionCancel", "click", DKSolution_OnEvent);
+	DKRemoveEvent("DKSolutionOK", "click", DKSolution_OnEvent);
+	DKRemoveEvent("DKSolutionUp", "click", DKSolution_OnEvent);
+	DKClose("DKSolution.html");
+}
+
+////////////////////////////////////
+function DKSolution_OnEvent(event)
+{	
+	DKLog("DKSolution_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
+
+	if(DK_IdLike(event, "DKSolutionDrive")){
+		DKSolution_OpenFolder(DKWidget_GetValue(event));
+	}
+	if(DK_IdLike(event, "DKSolutionFolder")){
+		DKLog("DKSolutionFolder", DKINFO);
+		DKSolution_OpenFolder(DKWidget_GetValue(event));
+	}
+	if(DK_IdLike(event, "DKSolutionFile")){
+		DKSolution_OpenFile(DKWidget_GetValue(event));
+	}
+
+	if(DK_Id(event, "DKSolutionUp")){
+		var up = DKWidget_GetValue("DKSolutionPath")+"/..";
+		DKLog(up+"\n", DKDEBUG);
+		DKSolution_OpenFolder(up);
+	}
+	
+	if(DK_Id(event, "DKSolutionOK")){
+		if(rPath && event_data2 == "relative"){
+			DKSendEvent(event_id, event_type, rPath);
+		}
+		else if(aPath && event_data2 == "absolute"){
+			DKSendEvent(event_id, event_type, aPath);
+		}
+		else{
+			//DKLog("DKSolution::ProcessEvent(): return_path_type incorrect. \n", DKERROR);
+		}
+		
+		DKFrame_Close("DKSolution.html");
+		return;
+	}
+	
+	if(DK_Id(event, "DKSolutionCancel")){
+		DKFrame_Close("DKSolution.html");
+		return;
+	}
+	
+	if(DK_Type(event, "GetFile")){
+		var params = DKWidget_GetValue(event).split(",");
+		event_id = params[0];
+		event_type = params[1];
+		event_data1 = params[2];
+		event_data2 = params[3];
+		DKLog("event_type:"+event_type+"\n", DKINFO);
+		DKLog("event_id:"+event_id+"\n", DKINFO);
+		DKLog("event_data1:"+event_data1+"\n", DKINFO);
+		DKLog("event_data2:"+event_data2+"\n", DKINFO);
+	
+		DKSolution_UpdatePath(event_data1);
+	}
+}
+
+//////////////////////////////////////
+function DKSolution_OpenFolder(path)
+{
+	DKLog("DKSolution_OpenFolder("+path+") \n", DKINFO);
+	if(DKSolution_UpdatePath(path)){
+		return true;
+	}
+	return false;
+}
+
+////////////////////////////////////
+function DKSolution_OpenFile(path)
+{
+	DKLog("DKSolution_OpenFile("+path+") \n", DKDEBUG);
+	if(DK_GetOS() == "Android"){
+		aPath = path;
+	}
+	else{
+		aPath = DKFile_GetAbsolutePath(path);
+	}
+	DKLog("aPath:"+aPath+"\n", DKDEBUG);
+	var assets = DKAssets_LocalAssets();
+	//DKLog("assets:"+assets+"\n", DKDEBUG);
+	rPath = DKFile_GetRelativePath(aPath, assets);
+	DKLog("rPath:"+rPath+"\n", DKDEBUG);
+	DKWidget_SetValue("DKSolutionPath",aPath);
+}
+
+//////////////////////////////////////
+function DKSolution_UpdatePath(path)
+{
+	//if(!path){ return false; }
+	DKLog("DKSolution_UpdatePath("+path+") \n", DKDEBUG);
+	if(DK_GetOS() == "Android"){
+		aPath = path;
+	}
+	else{
+		aPath = DKFile_GetAbsolutePath(path);
+	}
+	DKLog("aPath:"+aPath+"\n", DKDEBUG);
+	//var assets = DKAssets_LocalAssets();
+	//DKLog("assets:"+assets+"\n", DKDEBUG);
+	rPath = DKFile_GetRelativePath(aPath, path);
+	DKLog("rPath:"+rPath+"\n", DKDEBUG);
+	
+	var temp = DKFile_DirectoryContents(aPath);
+	var files = temp.split(",");
+
+	DKWidget_SetInnerHtml("DKSolutionMenu", ""); //Clear it
+	DKWidget_SetInnerHtml("DKSolutionMenu2", ""); //Clear it
+
+	for(var d=0; d<files.length; d++){
+		if(DKFile_IsDirectory(aPath+"/"+files[d])){ //Folders
+			var element2 = DKWidget_CreateElement("DKSolutionMenu2", "option", "DKSolutionFolder");
+			var value = aPath+"/"+files[d];
+			DKWidget_SetAttribute(element2,"value", value);
+			DKWidget_SetProperty(element2, "white-space", "nowrap");
+			DKAddEvent(element2, "click", DKSolution_OnEvent);
+			DKWidget_SetProperty(element2, "padding-left", "17px");
+			DKWidget_SetInnerHtml(element2,files[d]);
+			DKWidget_SetProperty(element2, "background-image", "url(\"DKFile/folder.png\")");
+			DKWidget_SetProperty(element2, "background-repeat", "no-repeat");
+		}
+	}
+
+	for(var f=0; f<files.length; f++){
+		if(!DKFile_IsDirectory(aPath+"/"+files[f])){ //Files
+			var element3 = DKWidget_CreateElement("DKSolutionMenu2", "option", "DKSolutionFile");
+			var value = aPath+"/"+files[f];
+			DKWidget_SetAttribute(element3, "value", value);
+			DKWidget_SetProperty(element3, "white-space", "nowrap");
+			DKWidget_SetProperty(element3, "padding-left", "17px");
+			DKWidget_SetProperty(element3, "background-repeat", "no-repeat");
+			DKWidget_SetInnerHtml(element3,files[f]);
+			DKAddEvent(element3, "click", DKSolution_OnEvent);
+
+			var extension = DKFile_GetExtention(files[f]);
+			if((extension == ".png") || (extension == ".jpeg") || (extension == ".jpg") || 
+				(extension == ".bmp") || (extension == ".tiff") || (extension == ".tif") || 
+				(extension == ".gif") || (extension == ".tga") || (extension == ".ico")
+				){
+				DKWidget_SetProperty(element3, "background-image", "url(\"DKFile/picture.png\")");
+			}
+
+			else if((extension == ".osg") || (extension == ".osgb") || (extension == ".osgt") ||
+				(extension == ".3dm") || (extension == ".3ds") || (extension == ".ac") ||
+				(extension == ".ascii") || (extension == ".blend")  || (extension == ".bvh") ||
+				(extension == ".c4d") || (extension == ".dae") || (extension == ".dds") ||
+				(extension == ".dgn") || (extension == ".dwg") || (extension == ".dxf") ||
+				(extension == ".fbx") || (extension == ".lwo") || (extension == ".lws") ||
+				(extension == ".ma") || (extension == ".max") || (extension == ".mb") ||
+				(extension == ".mesh") || (extension == ".mtl") || (extension == ".obj") ||
+				(extension == ".pov") || (extension == ".skp") || (extension == ".stl") ||
+				(extension == ".ztl")
+			){
+				DKWidget_SetProperty(element3, "background-image", "url(\"DKFile/cube.png\")");
+			}
+
+			else if((extension == ".html") || (extension == ".htm")){
+				DKWidget_SetProperty(element3, "background-image", "url(\"DKFile/html.png\")");
+			}
+
+			else{
+				DKWidget_SetProperty(element3, "background-image", "url(\"DKFile/file.png\")");
+			}
+		}
+	}
+	
+	DKWidget_SetValue("DKSolutionPath", aPath);
+	return true;
+}
+
