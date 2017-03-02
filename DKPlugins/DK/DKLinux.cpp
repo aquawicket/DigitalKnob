5e6a14743054eab310655c8665c43de3b74807bb
dkuser <dkuser@digitalknob.com>
2017-03-01 20:51:59 -0800

    commit from git

--- DKPlugins/DK/DKLinux.cpp
+++ DKPlugins/DK/DKLinux.cpp
@@ -28,12 +28,9 @@ bool DKLinux::GetMousePos(int& x, int& y)
  
 	if(XQueryPointer(dpy, root, &ret_root, &ret_child, &root_x, &root_y, &win_x, &win_y, &mask)){
 		// original version
-		//    printf("root loc: %4d,%4d win loc: %3d,%3d mask: 0x%08X\n",
-		//           root_x, root_y, win_x, win_y, mask);
- 
+		//    printf("root loc: %4d,%4d win loc: %3d,%3d mask: 0x%08X\n", root_x, root_y, win_x, win_y, mask);
 		// This returns in -geometry format
 		// I added \n so it actually shows something so people who test it know it works.
-		//printf("+%d+%d\n", root_x, root_y);
 		x = root_x;
 		y = root_y;
 		return true;
@@ -48,6 +45,42 @@ bool DKLinux::GetMousePos(int& x, int& y)
 bool DKLinux::Run(const DKString& command)
 {
 	//https://cboard.cprogramming.com/linux-programming/79686-linux-equivalent-win32-shellexecute.html
+	//http://www.linuxquestions.org/questions/programming-9/is-there-something-like-shellexecute-in-linux-213725/
+	
+	/*
+	// sh_cmd() - executes a command in the background
+	// returns TRUE is command was executed  (not the result of the command though..)
+	//NO GLOBALS
+	static gint sh_cmd (gchar * path, gchar * cmd, gchar * args)
+	{
+		gchar     cmd_line[256];
+		gchar   **argv;
+		gint      argp;
+		gint      rc = 0;
+
+		if (cmd == NULL)
+			return FALSE;
+
+		if (cmd[0] == '\0')
+			return FALSE;
+
+		if (path != NULL)
+			chdir (path);
+
+		snprintf (cmd_line, sizeof (cmd_line), "%s %s", cmd, args);
+
+		rc = g_shell_parse_argv (cmd_line, &argp, &argv, NULL);
+		if(!rc){
+			g_strfreev (argv);
+			return rc;
+		}
+
+		rc = g_spawn_async (path, argv, NULL, G_SPAWN_STDOUT_TO_DEV_NULL | G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, NULL);
+		g_strfreev (argv);
+		return rc;
+	}
+	*/
+
 	return false;
 }
 
