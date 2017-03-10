52f1b3f988391ba4a60ef400fd2186786b37e537
dkuser <dkuser@digitalknob.com>
2017-03-10 13:40:47 -0800

    commit from git

--- DKPlugins/DK/DKMac.h
+++ DKPlugins/DK/DKMac.h
@@ -11,14 +11,7 @@ class DKMac
 public:
 	static bool SetMainThreadNow(unsigned long int& id);
 	static bool GetThreadId(unsigned long int& id);
-	static bool GetMousePos(int& x, int& y){
-		//NSPoint mouseLoc;
-		//mouseLoc = [NSEvent mouseLocation]; //get current mouse position
-		//NSLog(@"Mouse location: %f %f", mouseLoc.x, mouseLoc.y);
-		x = 0;//mouseLoc.x;
-		y = 0;//mouseLoc.y;
-		return true;
-	}
+	static bool GetMousePos(int& x, int& y);
 };
 
 #endif //DKMac_H
