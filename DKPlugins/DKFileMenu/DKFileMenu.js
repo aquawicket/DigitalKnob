ccee310840019c6f89aaf47fc96149fdc919036a
dkuser <dkuser@digitalknob.com>
2017-04-08 02:41:03 -0700

    commit from git

---
+++ DKPlugins/DKFileMenu/DKFileMenu.js
@@ -0,0 +1,20 @@
+//TODO
+
+///////////////////////
+function DKFileMenu_Init()
+{
+
+}
+
+///////////////////////
+function DKFileMenu_End()
+{
+
+}
+
+///////////////////////////////
+function DKFileMenu_OnEvent(event)
+{
+	DKLog("DKFileMenu_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKINFO);
+	
+}
