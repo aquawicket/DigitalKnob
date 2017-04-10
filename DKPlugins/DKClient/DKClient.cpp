800c497dba4e96d837558dad1c37657db6be8f69
dkuser <dkuser@digitalknob.com>
2017-04-09 19:05:18 -0700

    commit from git

--- DKPlugins/DKClient/DKClient.cpp
+++ DKPlugins/DKClient/DKClient.cpp
@@ -20,16 +20,16 @@ void DKClient::Connect(const DKString address)
 {
 	if(address.empty()){ return; }
 
-	try{
+	//try{
 		c->stop();
 		tcp::resolver resolver(thread->io_service);
 		c->start(resolver.resolve(tcp::resolver::query(address.c_str(), "8087")));
 		_address = address;
-	}
-	catch(boost::system::system_error& e){
-		std::cerr << e.what() << '\n';
-		return;
-	}
+	//}
+	//catch(boost::system::system_error& e){
+	//	std::cerr << e.what() << '\n';
+	//	return;
+	//}
 }
 
 ///////////////////////////////////////////
@@ -37,12 +37,12 @@ void DKClient::Send(const DKString& string)
 {
 	if(!c->socket_.is_open()){ 
 		Connect(_address);
+		if(!c->socket_.is_open()){ 
+		  DKLog("DKClient::Send(): could not find open socket\n", DKWARN);
+		  return; 
+		}
 	}
-	
-	if(!c->socket_.is_open()){ 
-		DKLog("DKClient::Send(): could not find open socket\n", DKWARN);
-		return; 
-	}
+
 	const char* z = string.c_str();
 	size_t request_length = std::strlen(z);
 	boost::asio::write(c->socket_, boost::asio::buffer(z, request_length));
