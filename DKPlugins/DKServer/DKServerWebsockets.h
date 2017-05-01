90c88b7bbd60002307a4d4e5a56aed3c1e8944bf
dkuser <dkuser@digitalknob.com>
2017-05-01 04:50:19 -0700

    commit from git

--- DKPlugins/DKServer/DKServerWebsockets.h
+++ DKPlugins/DKServer/DKServerWebsockets.h
@@ -1,8 +1,11 @@
+//http://usualcarrot.com/libwebsockets-simple-websocket-server
+//https://barkingbogart.wordpress.com/2014/10/07/test-drive-libwebsockets-library-a-simple-server/
+
 #pragma once
 #ifndef DKServerWebsockets_H
 #define DKServerWebsockets_H
-
 #include <libwebsockets.h>
+
 
 ///////////////////////////////////////////
 class DKServerWebsockets : public DKObjectT<DKServerWebsockets>
