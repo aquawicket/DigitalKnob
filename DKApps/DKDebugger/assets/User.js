51a3bf89b7dfbf1a02745013fcb194943ce3e313
dkuser <dkuser@digitalknob.com>
2017-05-10 15:01:00 -0700

    commit from git

--- DKApps/DKDebugger/assets/User.js
+++ DKApps/DKDebugger/assets/User.js
@@ -3,21 +3,16 @@ var url = "http://127.0.0.1:2393";
 var width = 800;
 var height = 600;
 
-/*
 DKCreate("DKCef,Cef,0,0,"+width+","+height+","+url);
 DK_SetFramerate(5);
 DKCreate("DKDebug/DKDebug.js");
-*/
 
+DKAddEvent("GLOBAL", "keypress", User_OnEvent);
 ////////////////////////////
 function User_OnEvent(event)  //Duktape
 {
 	DKLog("User_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKDEBUG);
 	
-	if(DK_Type(event, "resize")){
-		DK_CallFunc("CefSDL::OnResize", "0,0,"+String(DKWindow_GetWidth())+","+String(DKWindow_GetHeight()));
-	}
-
 	if(DK_Type(event, "keypress")){ //press a key to run this after a DevTools context menu is open, it will find 'Save'
 		DKCef_RunJavascript("CefSDL", "var x = document.getElementsByClassName('soft-context-menu');");
 		DKCef_RunJavascript("CefSDL", "var shadow_root = x[0].shadowRoot;");
@@ -27,19 +22,9 @@ function User_OnEvent(event)  //Duktape
 		DKCef_RunJavascript("CefSDL", "console.log(save.innerHTML);");
 		
 		//FIXME - we can get the element of the save button, but we cannot add an event.
-		//DKCef_RunJavascript("CefSDL", "save.onclick = function(){alert('save clicked.')};");  ///grrrrr
+		DKCef_RunJavascript("CefSDL", "save.onclick = function(){alert('save clicked.')};");  ///grrrrr
 		//DKCef_RunJavascript("CefSDL", "save.addEventListener('focus', function(event){ event.preventDefault(); console.log('click'); });");
 	}
 }
 
-DKLog("Creating SDL -> CEF -> GUI \n", DKINFO);
-DKCreate("DKWindow");
-var width = DKWindow_GetWidth();
-var height = DKWindow_GetHeight();
-DKCreate("DKCef,CefSDL,0,0,"+width+","+height+","+url);
-var currentBrowser = DKCef_GetCurrentBrowser("CefSDL");
-DKCef_SetUrl("CefSDL", url, currentBrowser);
-DKCef_SetFocus("CefSDL");
-DKAddEvent("GLOBAL", "resize", User_OnEvent);
-DKAddEvent("GLOBAL", "keypress", User_OnEvent);
 
