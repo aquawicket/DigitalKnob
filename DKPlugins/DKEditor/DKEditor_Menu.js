24fd0c6a004b373e758770be0a4ab8262c20aeba
dkuser <dkuser@digitalknob.com>
2017-05-21 16:32:02 -0700

    commit from git

--- DKPlugins/DKEditor/DKEditor_Menu.js
+++ DKPlugins/DKEditor/DKEditor_Menu.js
@@ -8,6 +8,7 @@ function DKEditor_Menu_Init()
 	DKAddEvent("GLOBAL", "click", DKEditor_Menu_OnEvent);
 	DKAddEvent("GLOBAL", "contextmenu", DKEditor_Menu_OnEvent);
 	DKAddEvent("DKEditor_Menu_Assets", "click", DKEditor_Menu_OnEvent);
+	DKAddEvent("DKEditor_Menu_DevTools", "click", DKEditor_Menu_OnEvent);
 }
 
 ////////////////////////////
@@ -24,12 +25,15 @@ function DKEditor_Menu_OnEvent(event)
 	//DKLog("DKEditor_Menu_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKINFO);
 	
 	if(DK_Id(event, "DKEditor_Menu_Assets")){
-		//DKLog("Assets", DKINFO);
 		DKCreate("DKBuild/DKSolution.js", function(){
 			var frame = DKFrame_Widget("DKSolution.html");
 			DKSolution_UpdatePath(DKAssets_LocalAssets());
 			DKWidget_SetProperty("DKSolution.html", "height", "300rem");
 		});
+	}
+	
+	if(DK_Id(event, "DKEditor_Menu_DevTools")){
+		//DKCef_ShowDevTools("Cef", 0);
 	}
 
 	DKClose("DKEditor/DKEditor_Menu.js");
