579f250a3f663108181560d06df805dadda0a981
dkuser <dkuser@digitalknob.com>
2017-05-21 15:28:12 -0700

    commit from git

--- DKPlugins/DKEditor/DKEditor.js
+++ DKPlugins/DKEditor/DKEditor.js
@@ -2,16 +2,21 @@
 function DKEditor_Init()
 {
 	DKCreate("DKEditor/DKEditor.html");
+	DKAddEvent("DKEditor_edit", "click", DKEditor_OnEvent);
 }
 
-////////////////////
-function DKDev_End()
+///////////////////////
+function DKEditor_End()
 {
 	DKClose("DKEditor/DKEditor.html");
 }
 
-/////////////////////////////
-function DKDev_OnEvent(event)
+////////////////////////////////
+function DKEditor_OnEvent(event)
 {
 	//DKLog("DKEditor_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKINFO);
+	
+	if(DK_Id(event, "DKEditor_edit")){
+		DKCreate("DKEditor/DKEditor_Menu.js", function(){});
+	}
 }
--- DKPlugins/DKEditor/DKEditor_Menu.js
+++ DKPlugins/DKEditor/DKEditor_Menu.js
@@ -5,14 +5,12 @@ function DKEditor_Menu_Init()
 	DKCreate("DKEditor/DKEditor_Menu.html");
 	DKWidget_SetProperty("DKEditor_Menu.html","top",DKWindow_GetMouseY()+"rem");
 	DKWidget_SetProperty("DKEditor_Menu.html","left",DKWindow_GetMouseX()+"rem");
-	DKAddEvent("GLOBAL", "mousedown", DKEditor_Menu_OnEvent);
 	DKAddEvent("DKEditor_Menu_Item1", "click", DKEditor_Menu_OnEvent);
 }
 
 /////////////////////////
 function DKEditor_Menu_End()
 {
-	DKRemoveEvent("GLOBAL", "mousedown", DKEditor_Menu_OnEvent);
 	DKRemoveEvent("DKEditor_Menu_Item11", "click", DKEditor_Menu_OnEvent);
 	DKClose("DKEditor/DKEditor_Menu.html");
 }
@@ -20,8 +18,11 @@ function DKEditor_Menu_End()
 //////////////////////////////////
 function DKEditor_Menu_OnEvent(event)
 {
-	DKLog("DKEditor_Menu_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKINFO);
+	//DKLog("DKEditor_Menu_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKINFO);
 	
+	if(DK_Id(event, "DKEditor_Menu_Item1")){
+		DKLog("Item1", DKINFO);
+	}
 
 	DKClose("DKEditor/DKEditor_Menu.js");
 }
