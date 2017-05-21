2cb3b24a5f598c8c7ec1a724d5be2f3fcf52ef21
dkuser <dkuser@digitalknob.com>
2017-05-21 16:23:57 -0700

    commit from git

--- DKPlugins/DKEditor/DKEditor_Menu.js
+++ DKPlugins/DKEditor/DKEditor_Menu.js
@@ -5,12 +5,15 @@ function DKEditor_Menu_Init()
 	DKCreate("DKEditor/DKEditor_Menu.html");
 	DKWidget_SetProperty("DKEditor_Menu.html","top",DKWindow_GetMouseY()+"rem");
 	DKWidget_SetProperty("DKEditor_Menu.html","left",DKWindow_GetMouseX()+"rem");
+	DKAddEvent("GLOBAL", "click", DKEditor_Menu_OnEvent);
+	DKAddEvent("GLOBAL", "contextmenu", DKEditor_Menu_OnEvent);
 	DKAddEvent("DKEditor_Menu_Assets", "click", DKEditor_Menu_OnEvent);
 }
 
 ////////////////////////////
 function DKEditor_Menu_End()
 {
+	DKRemoveEvent("GLOBAL", "click", DKEditor_Menu_OnEvent);
 	DKRemoveEvent("DKEditor_Menu_Assets", "click", DKEditor_Menu_OnEvent);
 	DKClose("DKEditor/DKEditor_Menu.html");
 }
