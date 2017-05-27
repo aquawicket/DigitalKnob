f20de166eca4e456964576f2a2998769ab2f7595
dkuser <dkuser@digitalknob.com>
2017-05-26 22:16:13 -0700

    commit from git

--- DKApps/DKTemplate/assets/User.js
+++ DKApps/DKTemplate/assets/User.js
@@ -3,7 +3,7 @@ var USE_ROCKET = 0;
 var USE_CEF = 1;
 var USE_WEBVIEW = 1;
 var DKApp_url = "file:///"+DKAssets_LocalAssets()+"/index.html";
-//var DKApp_url = "http://google.com";
+//var DKApp_url = "https://us.letgo.com/en";
 
 //Validate settings
 if(DK_GetOS() == "Android" || DK_GetOS() == "iOS"){ USE_CEF = 0; }
