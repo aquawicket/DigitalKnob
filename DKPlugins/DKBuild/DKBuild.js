09dc145af66bd6d4d93fe65c5dc6d9547bef13ce
dkuser <dkuser@digitalknob.com>
2017-06-14 21:32:01 -0700

    commit from git

--- DKPlugins/DKBuild/DKBuild.js
+++ DKPlugins/DKBuild/DKBuild.js
@@ -383,7 +383,7 @@ function DKBuild_SvnCommit()
 ////////////////////////////
 function DKBuild_GitUpdate()
 {
-	if(DK_GetBrowser() != "CEF" || DK_GetBrowser() != "Rocket"){
+	if(DK_GetBrowser() != "CEF" && DK_GetBrowser() != "Rocket"){
 		return;
 	}
 	
@@ -420,7 +420,7 @@ function DKBuild_GitUpdate()
 ////////////////////////////
 function DKBuild_GitCommit()
 {
-	if(DK_GetBrowser() != "CEF" || DK_GetBrowser() != "Rocket"){
+	if(DK_GetBrowser() != "CEF" && DK_GetBrowser() != "Rocket"){
 		return;
 	}
 	
