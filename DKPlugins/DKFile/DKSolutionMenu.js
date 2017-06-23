a63c0123ae053ded95bceebedea43e0eb836c296
dkuser <dkuser@digitalknob.com>
2017-06-22 22:38:28 -0700

    commit from git

--- DKPlugins/DKFile/DKSolutionMenu.js
+++ DKPlugins/DKFile/DKSolutionMenu.js
@@ -31,7 +31,7 @@ function DKSolutionMenu_End()
 //////////////////////////////////////
 function DKSolutionMenu_OnEvent(event)
 {
-	DKLog("DKSolutionMenu_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+	//DKLog("DKSolutionMenu_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
 	
 	if(DK_Id(event,"DKSolutionMenu_Open")){
 		DKSolutionMenu_Open();
