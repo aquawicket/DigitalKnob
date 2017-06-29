cefe84867a17cf4a38bdf753e6a5fdbd775d0ee4
dkuser <dkuser@digitalknob.com>
2017-06-29 00:41:37 -0700

    commit from git

---
+++ DKPlugins/DKNotepad/DKNotepadView.js
@@ -0,0 +1,35 @@
+/////////////////////////////
+function DKNotepadView_Init()
+{
+	//DKLog("DKNotepadView_Init()\n");
+	
+	DKCreate("DKNotepad/DKNotepadView.html,DKNotepad/DKNotepad.html");
+	DKAddEvent("GLOBAL", "mousedown", DKNotepadView_OnEvent);
+	DKAddEvent("DKNotepadView_StatusBar", "click", DKNotepadView_OnEvent);
+}
+
+////////////////////////////
+function DKNotepadView_End()
+{
+	//DKLog("DKNotepadView_End()\n");
+	
+	DKRemoveEvents(DKNotepadView_OnEvent);
+	DKClose("DKNotepad/DKNotepadView.html");
+}
+
+/////////////////////////////////////
+function DKNotepadView_OnEvent(event)
+{
+	//DKLog("DKNotepadView_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+	
+	if(DK_Id(event,"DKNotepadView_StatusBar")){
+		DKLog("DKNotepadView_StatusBar\n");
+	}
+	
+	if(DK_Id(event, "GLOBAL")){
+		if(DKWidget_IsChildOf(DKWidget_GetHoverElement(), "DKNotepad/DKNotepadView.html")){
+			return;
+		}
+	}
+	DKClose("DKNotepad/DKNotepadView.js");
+}
