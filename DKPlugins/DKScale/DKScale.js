1f981be36747f1b1d9eedc1b4cc798181b2d529f
dkuser <dkuser@digitalknob.com>
2017-07-06 20:16:15 -0700

    commit from git

--- DKPlugins/DKScale/DKScale.js
+++ DKPlugins/DKScale/DKScale.js
@@ -1,19 +1,19 @@
 var pixel_ratio = 1.0;
 
-//////////////////////
+///////////////////////
 function DKScale_Init()
 {
 	DKAddEvent("GLOBAL", "resize", DKScale_OnEvent);
 	DKScale_Resize();
 }
 
-/////////////////////
+//////////////////////
 function DKScale_End()
 {
 	DKRemoveEvents(DKScale_OnEvent);
 }
 
-//////////////////////////////
+///////////////////////////////
 function DKScale_OnEvent(event)
 {
 	//DKLog("DKScale_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
@@ -23,14 +23,18 @@ function DKScale_OnEvent(event)
 	}
 }
 
-////////////////////////
+/////////////////////////
 function DKScale_Resize()
 {
-    //DKLog("DKWindow_GetPixelRatio() = "+DKWindow_GetPixelRatio()+"\n");
+    DKLog("DKWindow_GetPixelRatio() = "+DKWindow_GetPixelRatio()+"\n");
+	DKLog("DKWindow_GetWidth() = "+DKWindow_GetWidth()+"\n");
+	DKLog("DKWindow_GetHeight() = "+DKWindow_GetHeight()+"\n");
+	
 	if(pixel_ratio == DKWindow_GetPixelRatio()){ return true; } //nothing to be done
 	pixel_ratio = DKWindow_GetPixelRatio();
 
 	//Set the scale
+	/*
 	if((parseInt(DKWindow_GetHeight()) > parseInt(DKWindow_GetWidth()))){
 		if(pixel_ratio > 1.0){//= Math.min(2, pixel_ratio);
 			var ratio = 2.0;
@@ -42,4 +46,5 @@ function DKScale_Resize()
 		DKWidget_SetProperty("html", "font-size", "1px");
 		DKLog("DKScale_Resize(): scale set to 1px\n");
 	}
+	*/
 }
