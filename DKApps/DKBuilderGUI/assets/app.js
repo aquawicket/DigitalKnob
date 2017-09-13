0b1c5b3a87e5a0d1140fb56397708299323ae486
dkuser <dkuser@digitalknob.com>
2017-09-13 02:19:25 -0700

    commit from git

--- DKApps/DKBuilderGUI/assets/app.js
+++ DKApps/DKBuilderGUI/assets/app.js
@@ -3,7 +3,7 @@ var USE_WEBVIEW = 1; //Android, iOS?
 var USE_SDL = 0;     //Use with caution
 var USE_ROCKET = 0;  //Use with caution
 var DKApp_url = "file:///"+DKAssets_LocalAssets()+"index.html";
-//var DKApp_url = "http://digitalknob.com/MyApp";
+//var DKApp_url = "http://digitalknob.com/DKBuilderGUI";
 
 DKCreate("init.js", function(){});
 
@@ -11,7 +11,7 @@ DKCreate("init.js", function(){});
 ///////////////////////
 function app_LoadPage()
 {
-	DKLog("User_LoadPage()\n");
+	DKLog("app_LoadPage()\n");
 	
 	DKWidget_SetProperty("body", "background-color", "grey");
 	DKCreate("DKWindow/DKWindow.js", function(){
