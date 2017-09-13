02d8f98c67a7dc7ab26ed83ffc3f3ec5e3fbaead
dkuser <dkuser@digitalknob.com>
2017-09-13 01:45:38 -0700

    commit from git

---
+++ DKPlugins/_DKIMPORT/assets/app.js
@@ -0,0 +1,29 @@
+var USE_CEF = 1;     //Desktop
+var USE_WEBVIEW = 1; //Android, iOS?
+var USE_SDL = 0;     //Use with caution
+var USE_ROCKET = 0;  //Use with caution
+var DKApp_url = "file:///"+DKAssets_LocalAssets()+"index.html";
+//var DKApp_url = "http://digitalknob.com/DKTemplate";
+
+DKCreate("Init.js", function(){});
+
+
+///////////////////////
+function App_LoadPage()
+{
+	DKLog("App_LoadPage()\n");
+	
+	DKCreate("DKWindow/DKWindow.js", function(){
+	DKCreate("DKScale/DKScale.js", function(){
+	DKCreate("DKGui/DKFrame.js", function(){
+	DKCreate("DKGui/DKMenu.js", function(){
+	DKCreate("DKDebug/DKDebug.js", function(){
+	DKCreate("DKEditor/DKEditor.js", function(){
+		
+	});
+	});
+	});
+	});
+	});
+	});
+}
