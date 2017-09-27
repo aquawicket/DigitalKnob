06b5a0adf99217e1310611e9528ba5c3255fc6d4
dkuser <dkuser@digitalknob.com>
2017-09-27 13:08:44 -0700

    commit from git

--- DKPlugins/DKVncServer/DKVncServer.cpp
+++ DKPlugins/DKVncServer/DKVncServer.cpp
@@ -139,6 +139,7 @@ static void newframebuffer(rfbScreenInfoPtr screen, int width, int height)
 /////////////////////////////////////////////////////////////////////
 static void mouseevent(int buttonMask, int x, int y, rfbClientPtr cl)
 {
+	//DKUtil::SetMousePos(x, y);
 	if(buttonMask){
 		DKLog("mouseevent(): buttonMask="+toString(buttonMask)+" x="+toString(x)+" y="+toString(y)+"\n", DKINFO);
 	}
