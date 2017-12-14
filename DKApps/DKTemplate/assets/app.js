422e762cf6905ace32a6ce64bec779837bb4a333
dkuser <dkuser@digitalknob.com>
2017-12-13 23:17:17 -0800

    commit from git

--- DKApps/DKTemplate/assets/app.js
+++ DKApps/DKTemplate/assets/app.js
@@ -5,6 +5,7 @@ var USE_ROCKET = 0;  //Use with caution
 var DKApp_url = "file:///"+DKAssets_LocalAssets()+"index.html";
 //var DKApp_url = "http://digitalknob.com/DKTemplate";
 
+
 DKCreate("DK/init.js", function(){});
 
 ///////////////////////
