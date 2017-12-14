1d56e88c70b99d024de4ebb569e22b0b34be151c
dkuser <dkuser@digitalknob.com>
2017-12-14 00:19:18 -0800

    commit from git

--- DKApps/DKTemplate/assets/app.js
+++ DKApps/DKTemplate/assets/app.js
@@ -1,6 +1,6 @@
 var USE_CEF = 1;     //Desktop
 var USE_WEBVIEW = 1; //Android, iOS?
-var USE_SDL = 1;     //Use with caution
+var USE_SDL = 0;     //Use with caution
 var USE_ROCKET = 0;  //Use with caution
 var DKApp_url = "file:///"+DKAssets_LocalAssets()+"index.html";
 //var DKApp_url = "http://digitalknob.com/DKTemplate";
@@ -19,7 +19,6 @@ function app_LoadPage()
 	DKCreate("DKGui/DKMenu.js", function(){
 	DKCreate("DKDebug/DKDebug.js", function(){
 	DKCreate("DKEditor/DKEditor.js", function(){
-	DKCreate("DKSDLText"); //SDL fps counter
 	//DKCreate("DKHook/DKHook.js", function(){
 		//DKFrame_Widget("DKHook/DKHook.html");
 	//});		
