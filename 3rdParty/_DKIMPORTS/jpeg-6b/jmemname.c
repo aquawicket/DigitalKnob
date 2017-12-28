966f789cd3b4d9dc565ec074eb2d4edaec6e5c29
dkuser <dkuser@digitalknob.com>
2017-12-28 08:52:14 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/jpeg-6b/jmemname.c
+++ 3rdParty/_DKIMPORTS/jpeg-6b/jmemname.c
@@ -13,7 +13,9 @@
  */
 
 #define JPEG_INTERNALS
-#include <unistd.h>
+#ifndef WIN32           //ADDED BY DIGITALKNOB
+#include <unistd.h>     //ADDED BY DIGITALKNOB
+#endif                  //ADDED BY DIGITALKNOB
 #include "jinclude.h"
 #include "jpeglib.h"
 #include "jmemsys.h"		/* import the system-dependent declarations */
