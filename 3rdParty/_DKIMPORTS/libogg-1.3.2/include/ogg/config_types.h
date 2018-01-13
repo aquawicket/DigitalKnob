2996555b887be1ff322050b76f55775449c22797
dkuser <dkuser@digitalknob.com>
2018-01-13 13:37:19 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/libogg-1.3.2/include/ogg/config_types.h
+++ 3rdParty/_DKIMPORTS/libogg-1.3.2/include/ogg/config_types.h
@@ -2,9 +2,9 @@
 #define __CONFIG_TYPES_H__
 
 /* these are filled in by configure */
-#define INCLUDE_INTTYPES_H @INCLUDE_INTTYPES_H@
-#define INCLUDE_STDINT_H @INCLUDE_STDINT_H@
-#define INCLUDE_SYS_TYPES_H @INCLUDE_SYS_TYPES_H@
+#define INCLUDE_INTTYPES_H TRUE
+#define INCLUDE_STDINT_H
+#define INCLUDE_SYS_TYPES_H
 
 #if INCLUDE_INTTYPES_H
 #  include <inttypes.h>
@@ -16,10 +16,10 @@
 #  include <sys/types.h>
 #endif
 
-typedef @SIZE16@ ogg_int16_t;
-typedef @USIZE16@ ogg_uint16_t;
-typedef @SIZE32@ ogg_int32_t;
-typedef @USIZE32@ ogg_uint32_t;
-typedef @SIZE64@ ogg_int64_t;
+typedef int16_t ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
+typedef int32_t ogg_int32_t;
+typedef uint32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
 
 #endif
