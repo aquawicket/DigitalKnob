0670ea339b328c6ca36e848072451559a3e39e29
dkuser <dkuser@digitalknob.com>
2018-01-12 16:55:08 -0800

    commit from git

--- DKPlugins/DK/DKLinux.cpp
+++ DKPlugins/DK/DKLinux.cpp
@@ -155,42 +155,120 @@ bool DKLinux::GetVolume(float& volume)
 /////////////////////////////////////////////////
 bool DKLinux::VirtualMemory(float& virtualMemory)
 {
-	//TODO
-	return false;
+	#include "sys/types.h"
+	#include "sys/sysinfo.h"
+	struct sysinfo memInfo;
+	sysinfo (&memInfo);
+	long long totalVirtualMem = memInfo.totalram;
+	//Add other values in next statement to avoid int overflow on right hand side...
+	totalVirtualMem += memInfo.totalswap;
+	totalVirtualMem *= memInfo.mem_unit;
+	virtualMemory = totalVirtualMem;
+	return true;
 }
 
 /////////////////////////////////////////////////////
 bool DKLinux::VirtualMemoryUsed(float& virtualMemory)
 {
-	//TODO
-	return false;
+	long long virtualMemUsed = memInfo.totalram - memInfo.freeram;
+	//Add other values in next statement to avoid int overflow on right hand side...
+	virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
+	virtualMemUsed *= memInfo.mem_unit;
+	virtualMemory = virtualMemUsed;
+	return true;
 }
 
 //////////////////////////////////////////////////////////
 bool DKLinux::VirtualMemoryUsedByApp(float& virtualMemory)
 {
 	//TODO
+	/*
+	#include "stdlib.h"
+	#include "stdio.h"
+	#include "string.h"
+
+	int parseLine(char* line){
+	    // This assumes that a digit will be found and the line ends in " Kb".
+	    int i = strlen(line);
+	    const char* p = line;
+	    while (*p <'0' || *p > '9') p++;
+	    line[i-3] = '\0';
+	    i = atoi(p);
+	    return i;
+	}
+
+	int getValue(){ //Note: this value is in KB!
+		FILE* file = fopen("/proc/self/status", "r");
+		int result = -1;
+		char line[128];
+
+		while (fgets(line, 128, file) != NULL){
+		    if (strncmp(line, "VmSize:", 7) == 0){
+		        result = parseLine(line);
+		        break;
+		    }
+		}
+		fclose(file);
+		return result;
+	}
+	*/
 	return false;
 }
 
 ///////////////////////////////////////////////////
 bool DKLinux::PhysicalMemory(float& physicalMemory)
 {
-	//TODO
-	return false;
+	long long totalPhysMem = memInfo.totalram;
+	//Multiply in next statement to avoid int overflow on right hand side...
+	totalPhysMem *= memInfo.mem_unit;
+	physicalMemory = totalPhysMem;
+	return true;
 }
 
 ///////////////////////////////////////////////////////
 bool DKLinux::PhysicalMemoryUsed(float& physicalMemory)
 {
-	//TODO
-	return false;
+	long long physMemUsed = memInfo.totalram - memInfo.freeram;
+	//Multiply in next statement to avoid int overflow on right hand side...
+	physMemUsed *= memInfo.mem_unit;
+	physicalMemory = physMemUsed;
+	return true;
 }
 
 ////////////////////////////////////////////////////////////
 bool DKLinux::PhysicalMemoryUsedByApp(float& physicalMemory)
 {
 	//TODO
+	/*
+	#include "stdlib.h"
+	#include "stdio.h"
+	#include "string.h"
+
+	int parseLine(char* line){
+		// This assumes that a digit will be found and the line ends in " Kb".
+		int i = strlen(line);
+		const char* p = line;
+		while (*p <'0' || *p > '9') p++;
+		line[i-3] = '\0';
+		i = atoi(p);
+		return i;
+	}
+
+	int getValue(){ //Note: this value is in KB!
+		FILE* file = fopen("/proc/self/status", "r");
+		int result = -1;
+		char line[128];
+
+		while (fgets(line, 128, file) != NULL){
+			if (strncmp(line, "VmRSS:", 6) == 0){
+				result = parseLine(line);
+				break;
+			}
+		}
+		fclose(file);
+		return result;
+	}
+	*/
 	return false;
 }
 
@@ -205,6 +283,48 @@ void DKLinux::CpuInit()
 bool DKLinux::CpuUsed(int& cpu)
 {
 	//TODO
+	/*
+	#include "stdlib.h"
+	#include "stdio.h"
+	#include "string.h"
+
+	static unsigned long long lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
+
+	void init(){
+		FILE* file = fopen("/proc/stat", "r");
+		fscanf(file, "cpu %llu %llu %llu %llu", &lastTotalUser, &lastTotalUserLow, &lastTotalSys, &lastTotalIdle);
+		fclose(file);
+	}
+
+	double getCurrentValue(){
+		double percent;
+		FILE* file;
+		unsigned long long totalUser, totalUserLow, totalSys, totalIdle, total;
+
+		file = fopen("/proc/stat", "r");
+		fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow, &totalSys, &totalIdle);
+		fclose(file);
+
+		if(totalUser < lastTotalUser || totalUserLow < lastTotalUserLow || totalSys < lastTotalSys || totalIdle < lastTotalIdle){
+			//Overflow detection. Just skip this value.
+			percent = -1.0;
+		}
+		else{
+			total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) + (totalSys - lastTotalSys);
+			percent = total;
+			total += (totalIdle - lastTotalIdle);
+			percent /= total;
+			percent *= 100;
+		}
+
+		lastTotalUser = totalUser;
+		lastTotalUserLow = totalUserLow;
+		lastTotalSys = totalSys;
+		lastTotalIdle = totalIdle;
+
+		return percent;
+	}
+	*/
 	return false;
 }
 
@@ -212,6 +332,56 @@ bool DKLinux::CpuUsed(int& cpu)
 bool DKLinux::CpuUsedByApp(int& cpu)
 {
 	//TODO
+	/*
+	#include "stdlib.h"
+	#include "stdio.h"
+	#include "string.h"
+	#include "sys/times.h"
+	#include "sys/vtimes.h"
+
+	static clock_t lastCPU, lastSysCPU, lastUserCPU;
+	static int numProcessors;
+
+	void init(){
+		FILE* file;
+		struct tms timeSample;
+		char line[128];
+
+		lastCPU = times(&timeSample);
+		lastSysCPU = timeSample.tms_stime;
+		lastUserCPU = timeSample.tms_utime;
+
+		file = fopen("/proc/cpuinfo", "r");
+		numProcessors = 0;
+		while(fgets(line, 128, file) != NULL){
+			if(strncmp(line, "processor", 9) == 0) numProcessors++;
+		}
+		fclose(file);
+	}
+
+	double getCurrentValue(){
+		struct tms timeSample;
+		clock_t now;
+		double percent;
+
+		now = times(&timeSample);
+		if(now <= lastCPU || timeSample.tms_stime < lastSysCPU || timeSample.tms_utime < lastUserCPU){
+			//Overflow detection. Just skip this value.
+			percent = -1.0;
+		}
+		else{
+			percent = (timeSample.tms_stime - lastSysCPU) + (timeSample.tms_utime - lastUserCPU);
+			percent /= (now - lastCPU);
+			percent /= numProcessors;
+			percent *= 100;
+		}
+		lastCPU = now;
+		lastSysCPU = timeSample.tms_stime;
+		lastUserCPU = timeSample.tms_utime;
+
+		return percent;
+	}
+	*/
 	return false;
 }
 
