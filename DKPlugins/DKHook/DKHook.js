255128f15af0dc62e9d738a42def93e74380491d
dkuser <dkuser@digitalknob.com>
2018-01-14 20:34:48 -0800

    commit from git

--- DKPlugins/DKHook/DKHook.js
+++ DKPlugins/DKHook/DKHook.js
@@ -3,12 +3,12 @@ function DKHook_Init()
 {
 	DKCreate("DKHook");
 
-	DKAddEvent("GLOBAL", "mousemove", DKHook_OnEvent);
-	DKAddEvent("GLOBAL", "mousedown", DKHook_OnEvent);
-	DKAddEvent("GLOBAL", "mouseup", DKHook_OnEvent);
-	DKAddEvent("GLOBAL", "keypress", DKHook_OnEvent);
-	DKAddEvent("GLOBAL", "keydown", DKHook_OnEvent);
-	DKAddEvent("GLOBAL", "keyup", DKHook_OnEvent);
+	DKAddEvent("window", "mousemove", DKHook_OnEvent);
+	DKAddEvent("window", "mousedown", DKHook_OnEvent);
+	DKAddEvent("window", "mouseup", DKHook_OnEvent);
+	DKAddEvent("window", "keypress", DKHook_OnEvent);
+	DKAddEvent("window", "keydown", DKHook_OnEvent);
+	DKAddEvent("window", "keyup", DKHook_OnEvent);
 }
 
 /////////////////////
