ecca3bbd08242ee850cd8d7e9426fafd3cd50ca8
dkuser <dkuser@digitalknob.com>
2018-01-19 11:01:26 -0800

    commit from git

--- DKPlugins/DKOcr/DKOcr.cpp
+++ DKPlugins/DKOcr/DKOcr.cpp
@@ -1,8 +1,8 @@
 #include "DK/stdafx.h"
 #include "DKOcr/DKOcr.h"
 
-//#include "api/baseapi.h"
-//#include "src/allheaders.h"
+#include "api/baseapi.h"
+#include "src/allheaders.h"
 
 ///////////////////
 void DKOcr::Init()
