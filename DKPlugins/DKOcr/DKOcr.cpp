9d8081e10c7337b75a2c07ad8d64e91d76dd5c41
dkuser <dkuser@digitalknob.com>
2018-01-19 11:47:06 -0800

    commit from git

--- DKPlugins/DKOcr/DKOcr.cpp
+++ DKPlugins/DKOcr/DKOcr.cpp
@@ -1,16 +1,36 @@
 #include "DK/stdafx.h"
 #include "DKOcr/DKOcr.h"
-
+#include "DK/DKFile.h"
 #include "api/baseapi.h"
 #include "src/allheaders.h"
 
-///////////////////
+//////////////////
 void DKOcr::Init()
 {
+	char* outText;
 
+	tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+	// Initialize tesseract-ocr with English, without specifying tessdata path
+	if (api->Init(NULL, "eng")){
+		DKLog("Could not initialize tesseract.\n", DKERROR);
+		return;
+	}
+
+	// Open input image with leptonica library
+	DKString file = DKFile::local_assets+"test.tif";
+	Pix *image = pixRead(file.c_str());
+	api->SetImage(image);
+	// Get OCR result
+	outText = api->GetUTF8Text();
+	DKLog("OCR output:\n"+toString(outText)+"\n", DKINFO);
+
+	// Destroy used object and release memory
+	api->End();
+	delete [] outText;
+	pixDestroy(&image);
 }
 
-//////////////////
+/////////////////
 void DKOcr::End()
 {
 
