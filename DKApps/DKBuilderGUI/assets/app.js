768eed62c64a136f48b842b913fa162df0543307
dkuser <dkuser@digitalknob.com>
2018-01-21 22:28:43 -0800

    commit from git

--- DKApps/DKBuilderGUI/assets/app.js
+++ DKApps/DKBuilderGUI/assets/app.js
@@ -1,5 +1,5 @@
 var USE_CEF = 0;     //Desktop
-var USE_WEBVIEW = 1; //Android, iOS?
+var USE_WEBVIEW = 0; //Android, iOS?
 var USE_SDL = 1;     //Use with caution
 var USE_ROCKET = 1;  //Use with caution
 var DKApp_url = "file:///"+DKAssets_LocalAssets()+"index.html";
