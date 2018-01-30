d06fe1139ca865908df4e1e9fc0da9ad8a69c8f3
dkuser <dkuser@digitalknob.com>
2018-01-30 14:41:48 -0800

    commit from git

--- DKPlugins/DKOcr/DKOcr.js
+++ DKPlugins/DKOcr/DKOcr.js
@@ -3,10 +3,13 @@ function DKOcr_Init()
 {
 	DKLog("DKOcr_Init()\n");
 	
+	DKCreate("DKOcr");
 	DKCreate("DKOcr/DKOcr.html");
 	DKCreate("DKNotePad/DKNotepad.js", function(){
 		DKWidget_AppendChild("DKOcr/DKOcr.html", "DKNotepad/DKNotepad.html");
 	});
+	
+	DKAddEvent("GLOBAL", "OpenFile", DKOcr_OnEvent);
 }
 
 ////////////////////
@@ -21,4 +24,16 @@ function DKOcr_End()
 function DKOcr_OnEvent(event)
 {
 	DKLog("DKOcr_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+	
+	if(DK_Type(event, "OpenFile")){
+		var file = DK_GetValue(event);
+		DKLog("OpenFile: "+file+" \n");
+		var text;
+		if(text = DKOcr_ImageToText(file)){
+			DKWidget_SetAttribute("DKNotepad_Text", "value", text);
+		}
+		else{
+			DKNotepad_Open(file);
+		}
+	}
 }
