87dded3024b410fcc50477eeff94052068690e1c
dkuser <dkuser@digitalknob.com>
2018-02-04 13:47:58 -0800

    commit from git

--- DKPlugins/DKDuktape/DKJS.cpp
+++ DKPlugins/DKDuktape/DKJS.cpp
@@ -146,7 +146,7 @@ int DKJS::_DKCreate(duk_context* ctx)
 	DKObject* obj = DKCreate(data);
 	if(!obj){
 		duk_push_string(ctx, "");
-		DKLog("DKJS::_DKCreate("+data+"): obj invalid \n", DKERROR);
+		DKLog("DKJS::_DKCreate("+data+"): obj invalid \n", DKWARN);
 		return 0;
 	}
 
