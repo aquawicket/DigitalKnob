f4374658cdbda3ea9cc5b817a9506ae7bea04787
dkuser <dkuser@digitalknob.com>
2018-02-10 13:03:29 -0800

    commit from git

---
+++ DKPlugins/DKGui/TestGui.js
@@ -0,0 +1,17 @@
+///////////////////////
+function TestGui_Init()
+{
+	DKLog("TestGui_Init()\n");
+}
+
+//////////////////////
+function TestGui_End()
+{
+	DKLog("TestGui_End()\n");
+}
+
+///////////////////////////////
+function TestGui_OnEvent(event)
+{
+	DKLog("TestGui_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+}
