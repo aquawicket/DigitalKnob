07369e939d8c18e897d48fa18cf53b4782395e8f
dkuser <dkuser@digitalknob.com>
2018-02-10 13:00:36 -0800

    commit from git

---
+++ DKPlugins/DKGui/DKDropdown.js
@@ -0,0 +1,17 @@
+//////////////////////////
+function DKDropdown_Init()
+{
+	DKLog("DKDropdown_Init()\n");
+}
+
+/////////////////////////
+function DKDropdown_End()
+{
+	DKLog("DKDropdown_End()\n");
+}
+
+//////////////////////////////////
+function DKDropdown_OnEvent(event)
+{
+	DKLog("DKDropdown_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+}
--- DKPlugins/DKGui/DKMenu.js
+++ DKPlugins/DKGui/DKMenu.js
@@ -5,13 +5,13 @@ function DKMenu_Init()
 	//DKLog("DKMenu_Init()\n");
 }
 
-//////////////////////
+/////////////////////
 function DKMenu_End()
 {
 	//DKLog("DKMenu_End()\n");
 }
 
-///////////////////////////////
+//////////////////////////////
 function DKMenu_OnEvent(event)
 {
 	//DKLog("DKMenu_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
