2395169a32b0ce23418de430fe8208eab373b457
dkuser <dkuser@digitalknob.com>
2018-02-10 16:56:47 -0800

    commit from git

--- DKPlugins/DKGui/scrolltest.js
+++ DKPlugins/DKGui/scrolltest.js
@@ -1,8 +1,8 @@
 //////////////////////////
 function scrolltest_Init()
 {
-	DKCreate("DKGui/scrolltest.html");
-	//DKCreate("DKGui/scrolltest1.html", "DKGui/scrolltest.html");
+	DKCreate("DKGui/scrolltest.html", function(){});
+	//DKCreate("DKGui/scrolltest1.html", "DKGui/scrolltest.html", function(){});
 	//DKCreate("DKGui/scrolltest2.html", "DKGui/scrolltest.html");
 	//DKCreate("DKMessage/DKMessage.js", function(){});
 	DKAddEvent("bottomdiv", "mousedown", scrolltest_OnEvent);
