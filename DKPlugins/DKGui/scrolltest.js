4432a5b9fc6fc83da8788e5cf08139c8aa2a781e
dkuser <dkuser@digitalknob.com>
2018-02-22 11:22:44 -0800

    commit from git

--- DKPlugins/DKGui/scrolltest.js
+++ DKPlugins/DKGui/scrolltest.js
@@ -2,11 +2,12 @@
 function scrolltest_Init()
 {
 	DKCreate("DKGui/scrolltest.html", function(){
-	//DKCreate("DKGui/scrolltest1.html, DKGui/scrolltest.html", function(){
-	//DKCreate("DKGui/scrolltest2.html, DKGui/scrolltest.html", function(){
-		
-	//});		
-	//});
+	DKCreate("DKGui/scrolltest1.html", function(){
+	DKCreate("DKGui/scrolltest2.html", function(){
+		DKWidget_AppendChild("DKGui/scrolltest.html", "DKGui/scrolltest1.html");
+		DKWidget_AppendChild("DKGui/scrolltest.html", "DKGui/scrolltest2.html");
+	});		
+	});
 	});
 
 	//DKCreate("DKMessage/DKMessage.js", function(){});
