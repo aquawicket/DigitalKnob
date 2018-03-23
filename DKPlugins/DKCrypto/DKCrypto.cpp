4dd6c2871fef35d347af31ed44e998ff0aad33b3
dkuser <dkuser@digitalknob.com>
2018-03-23 14:44:06 -0700

    commit from git

---
+++ DKPlugins/DKCrypto/DKCrypto.cpp
@@ -0,0 +1,14 @@
+#include "DK/stdafx.h"
+#include "DKCrypto/DKCrypto.h"
+
+/////////////////////
+bool DKCrypto::Init()
+{
+	return true;
+}
+
+////////////////////
+bool DKCrypto::End()
+{
+	return true;
+}
