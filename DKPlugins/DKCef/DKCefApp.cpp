d136a8455343fb60b944f65f803bf1203bcc5f82
dkuser <dkuser@digitalknob.com>
2018-03-26 23:21:42 -0700

    commit from git

--- DKPlugins/DKCef/DKCefApp.cpp
+++ DKPlugins/DKCef/DKCefApp.cpp
@@ -272,3 +272,177 @@ void DKCefV8Handler::SetBrowser(CefRefPtr<CefBrowser> _browser)
 {
 	browser = _browser;
 }
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+bool DKCefApp::SendEvent(const DKString& id, const DKString& type, const DKString& value)
+{
+	/*
+	if(id.empty()){ return false; }
+	if(same(id,"DKLog")){ return false; }
+	if(type.empty()){ return false; }
+	if(same(type,"second")){ return false; }
+	DKLog("DKCefApp::SendEvent("+id+","+type+")", DKINFO);
+
+	DKString string = "DKSendEvent(\""+id+"\",\""+type+"\",\""+value+"\");";
+	CefRefPtr<CefFrame> frame = DKV8::_browser->GetMainFrame();
+	frame->ExecuteJavaScript(string.c_str(), frame->GetURL(), 0);
+	return true;
+	*/
+	return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void DKCefApp::OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line)
+{
+#ifndef DKCefChild
+	//printf("DKCefApp::OnBeforeCommandLineProcessing()\n");
+	if(same(DKV8::enable_system_flash, "ON")){
+		command_line->AppendSwitchWithValue("enable-system-flash", "1");
+	}
+	//if(same(DKV8::allow_file_access_from_files, "ON")){
+	command_line->AppendSwitchWithValue("allow-file-access-from-files", "1");
+	//}
+	if(same(DKV8::disable_gpu, "ON")){
+		command_line->AppendSwitchWithValue("disable-gpu", "1");
+	}
+	if(same(DKV8::disable_gpu_compositing, "ON")){
+		command_line->AppendSwitchWithValue("disable-gpu-compositing", "1");
+	}
+	if(same(DKV8::ignore_gpu_blacklist, "ON")){
+		command_line->AppendSwitchWithValue("ignore-gpu-blacklist", "1");
+	}
+	if(!DKV8::remote_debugging_port.empty()){
+		command_line->AppendSwitchWithValue("remote-debugging-port", DKV8::remote_debugging_port.c_str());
+	}
+	if(same(DKV8::disable_web_security, "ON")){
+		command_line->AppendSwitchWithValue("disable-web-security", "1");
+	}
+	if(!same(DKV8::no_proxy_server, "OFF")){
+		command_line->AppendSwitchWithValue("no-proxy-server", "1");
+	}
+	if(same(DKV8::enable_webgl, "ON")){
+		command_line->AppendSwitchWithValue("enable-webgl", "1");
+	}
+	if(same(DKV8::sandbox, "OFF")){
+		command_line->AppendSwitchWithValue("no-sandbox", "1");
+	}
+	if(same(DKV8::renderer_process_limit, "ON")){
+		command_line->AppendSwitchWithValue("renderer-process-limit", "1");
+	}
+	if(same(DKV8::enable_begin_frame_scheduling, "ON")){
+		command_line->AppendSwitchWithValue("enable-begin-frame-scheduling", "1"); //Breaks Popups
+	}
+	if(same(DKV8::enable_gpu, "ON")){
+		command_line->AppendSwitchWithValue("enable-gpu", "1");
+	}
+#ifdef LINUX
+	command_line->AppendSwitchWithValue("ppapi-flash-version", "25.0.0.127");
+	command_line->AppendSwitchWithValue("ppapi-flash-path", "/usr/lib/pepperflashplugin-nonfree/libpepflashplayer.so");
+#endif
+
+#endif //!DKCefChild
+
+	DKV8::v8handler = new DKCefV8Handler();
+}
+
+//////////////////////////////////////////////////////////////
+void DKCefApp::OnBrowserCreated(CefRefPtr<CefBrowser> browser) 
+{
+	//printf("DKCefApp::OnBrowserCreated()\n");
+	DKV8::_browser = browser;
+	DKV8::v8handler->SetBrowser(browser);
+	CefRefPtr<CefProcessMessage> msg = CefProcessMessage::Create("GetFunctions");
+	CefRefPtr<CefListValue> args = msg->GetArgumentList(); // Retrieve the argument list object.
+	browser->SendProcessMessage(PID_BROWSER, msg);
+}
+
+/////////////////////////////////////
+void DKCefApp::OnContextInitialized()
+{
+	CEF_REQUIRE_UI_THREAD();
+	//printf("DKCefApp::OnContextInitialized()\n");
+	CefRefreshWebPlugins();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void DKCefApp::OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context)
+{
+	DKLog(" DKCefApp::OnContextCreated\n", DKINFO);
+	DKV8::ctx = context->GetGlobal();
+
+	/*
+	std::map<std::string, bool>::iterator it = DKV8::funcs2.begin();
+	while(it != DKV8::funcs2.end()){
+	if(!it->second){
+	CefRefPtr<CefV8Value> value = CefV8Value::CreateFunction(it->first.c_str(), DKV8::v8handler);
+	if(DKV8::ctx->SetValue(it->first.c_str(), value, V8_PROPERTY_ATTRIBUTE_NONE)){
+	it->second = true;
+	DKLog("DKCefApp::OnContextCreated(): registered: "+it->first+"\n", DKINFO);
+	}
+	}
+	it++;
+	}
+	*/
+
+	for(unsigned int i=0; i<DKV8::funcs.size(); i++){
+		CefRefPtr<CefV8Value> value = CefV8Value::CreateFunction(DKV8::funcs[i].c_str(), DKV8::v8handler);
+		if(DKV8::ctx->SetValue(DKV8::funcs[i].c_str(), value, V8_PROPERTY_ATTRIBUTE_NONE)){
+			DKLog("DKCefApp::OnContextCreated(): registered: "+DKV8::funcs[i]+"\n", DKINFO);
+		}
+	}
+
+	//DKEvent::AddSendEventFunc(&DKCefApp::SendEvent, this);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool DKCefApp::OnProcessMessageReceived(CefRefPtr<CefBrowser> browser, CefProcessId source_process, CefRefPtr<CefProcessMessage> message) 
+{
+	//printf("DKCefApp::OnProcessMessageReceived()\n");
+	if(!DKV8::v8handler){
+		printf("DKCefApp::OnProcessMessageReceived(): v8handler invalid\n");
+		return false;
+	}
+
+	/*
+	if(message->GetName() == "GetFunctions"){
+	//printf("DKCefApp::OnProcessMessageReceived(GetFunctions)\n");
+	CefRefPtr<CefListValue> args = message->GetArgumentList();
+	for(unsigned int i=0; i<args->GetSize(); i++){
+	CefString string = args->GetString(i);
+	DKV8::funcs.push_back(std::string(string));
+	}
+	}
+	if(message->GetName() == "AttachFunction"){
+	//printf("DKCefApp::OnProcessMessageReceived(AttachFunction)\n");
+	CefRefPtr<CefListValue> args = message->GetArgumentList();
+	CefString func = args->GetString(0);
+	DKV8::funcs.push_back(std::string(func));
+
+	CefRefPtr<CefV8Value> value = CefV8Value::CreateFunction(func.c_str(), DKV8::v8handler);
+	DKV8::ctx->SetValue(func.c_str(), value, V8_PROPERTY_ATTRIBUTE_NONE);
+	//printf("registered: %s\n", func.c_str());
+	}
+	if(message->GetName() == "retval"){
+	//printf("DKCefApp::OnProcessMessageReceived(retval)\n");
+	CefRefPtr<CefListValue> retval = message->GetArgumentList();
+
+	if(retval->GetType(0) == VTYPE_STRING){
+	//printf("retval = %s\n", std::string(retval->GetString(0)).c_str());
+	//v8handler->_retval->SetString(0, retval->GetString(0));
+	}
+	if(retval->GetType(0) == VTYPE_INT){
+	//printf("retval = %d\n", retval->GetInt(0));
+	//v8handler->_retval->SetInt(0, retval->GetInt(0));
+	}
+	if(retval->GetType(0) == VTYPE_BOOL){
+	//printf("retval = %d\n", retval->GetBool(0));
+	//v8handler->_retval->SetBool(0, retval->GetBool(0));
+	}	
+	}
+	*/
+	return true;
+}
