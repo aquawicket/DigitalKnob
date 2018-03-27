bcd4117c651c4ddeeda783d5b3d579b66c1371ac
dkuser <dkuser@digitalknob.com>
2018-03-26 21:35:30 -0700

    commit from git

--- DKPlugins/DKAudio/DKAudio.h
+++ DKPlugins/DKAudio/DKAudio.h
@@ -11,17 +11,17 @@ public:
 	bool Init();
 	bool End();
 	
+	static bool GetDuration(int& duration);
+	static bool GetTime(int& time);
 	static bool GetVolume(int& volume);
 	static bool Mute();
 	static bool OpenMusic(DKString& file);
 	static bool Pause(DKString& file);
 	static bool PlaySound(const DKString& file);
 	static bool Resume(DKString& file);
+	static bool SetTime(int& seconds);
 	static bool SetVolume(int& volume);
 	static bool UnMute();
-	static bool SetTime(int& seconds);
-	static bool GetTime(int& time);
-	static bool GetDuration(int& duration);
 };
 
 REGISTER_OBJECT(DKAudio, true);
