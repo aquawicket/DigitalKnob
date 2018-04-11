bebe8b1c615237e163b1e335e36a32959040434c
dkuser <dkuser@digitalknob.com>
2018-04-11 15:35:40 -0700

    commit from git

--- DKPlugins/DKScreenRecorder/DKScreenRecorder.cpp
+++ DKPlugins/DKScreenRecorder/DKScreenRecorder.cpp
@@ -89,7 +89,8 @@ bool DKScreenRecorder::End()
 bool DKScreenRecorder::Record(const DKString& file)
 {
 	//OpenCV
-	videoWriter.open(file.c_str(), cv::VideoWriter::fourcc('M','J','P','G'), fps, cvSize(desktopWidth, desktopHeight), true);
+	//videoWriter.open(file.c_str(), cv::VideoWriter::fourcc('M','J','P','G'), fps, cvSize(desktopWidth, desktopHeight), true);
+	videoWriter.open(file.c_str(), CV_FOURCC_DEFAULT, fps, cvSize(desktopWidth, desktopHeight), true);
 	if(!videoWriter.isOpened()){
 		DKLog("DKScreenRecorder::Init(): Could not open the output video for write\n", DKWARN);
 		return false;
