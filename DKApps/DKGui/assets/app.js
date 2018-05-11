774ab7e6dfd2d275fe1704e33ac6181e73d3ebf6
dkuser <dkuser@digitalknob.com>
2018-05-10 23:08:45 -0700

    commit from git

---
+++ DKApps/DKGui/assets/app.js
@@ -0,0 +1,37 @@
+var USE_CEF     = 1; //Desktop
+var USE_WEBVIEW = 0; //TODO: Android, iOS
+var USE_SDL     = 0; //Use with caution
+var USE_ROCKET  = 0; //Use with caution
+var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"index.html";
+//var DKApp_url = "http://digitalknob.com/DKOS/index.html";
+
+DKCreate("DK/init.js", function(){});
+
+//////////////////////////
+function app_LoadPlugins()
+{
+	DKCreate("DKSDLText"); //SDL fps counter
+	DKCreate("DKTray/DKTray.js", function(){});
+	DKCreate("DKDebug/DKDebug.js", function(){});
+}
+
+///////////////////////
+function app_LoadPage()
+{
+	DKLog("app_LoadPage()\n");
+	
+	DKCreate("DK/DKBrowser.css");
+	DKCreate("DKWindow/DKWindow.js", function(){
+	DKCreate("DKScale/DKScale.js", function(){
+	DKCreate("DKGui/DKFrame.js", function(){
+	DKCreate("DKGui/DKMenu.js", function(){
+	DKCreate("DKDebug/DKDebug.js", function(){
+	DKCreate("DKGui/DKOS.js", function(){
+		//TODO - move DKOS into DKGui
+	});
+	});
+	});
+	});
+	});
+	});
+}
