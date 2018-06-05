6723d2139cf48e4e2ff153be21cca3bdf609db01
dkuser <dkuser@digitalknob.com>
2018-06-05 00:22:25 -0700

    commit from git

---
+++ DKPlugins/DKGui/DKTaskbar/README.txt
@@ -0,0 +1,1 @@
+//NOTE: This is only here to start working on sub-plugins, or recursive plugins.
---
+++ DKPlugins/DKGui/DKTaskbar/Taskbar.js
@@ -0,0 +1,56 @@
+var pos;
+var animation;
+///////////////////////
+function Taskbar_Init()
+{
+	//DKCreate("DKGui/Taskbar.html,DKGui/DKOS.html");
+	DKCreate("DKGui/Taskbar.html");
+	DKCreate("DKGui/scale.js", function(){});
+	DKCreate("DKGui/Clock.js", function(){});
+	if(DK_GetBrowser() == "Rocket"){
+		DKCreate("DKDebug/BugReport.js", function(){});
+		DKWidget_AppendChild("DKGui/Taskbar.html","DKDebug/BugReport.html"); //reparent
+	}
+	DKAddEvent("start", "click", Taskbar_OnEvent);
+	DKAddEvent("test_animate", "click", Taskbar_OnEvent);
+}
+
+//////////////////////
+function Taskbar_End()
+{
+	DKRemoveEvents(Taskbar_OnEvent);
+	DKClose("DKGui/Taskbar.html");
+	DKClose("DKGui/scale.js");
+	DKClose("DKGui/Clock.js");
+	DKClose("DKDebug/BugReport.js");
+}
+
+///////////////////////////////
+function Taskbar_OnEvent(event)
+{
+	DKLog("Taskbar_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+	
+	if(DK_Id(event, "start")){
+		DKCreate("DKGui/TaskbarMenu.js", function(){});
+	}
+	if(DK_Id(event, "test_animate")){
+		//DKLog("Taskbar_OnEvent(): animate\n");
+		pos = -45;
+		animation = setInterval(Taskbar_Animate, 15);
+		//EventLoop.run();
+	}
+}
+
+//////////////////////////
+function Taskbar_Animate()
+{
+	//DKLog("Taskbar_Animate(): frame()\n");
+    if(pos == 0){
+        clearInterval(animation);
+    } 
+	else{
+        pos++; 
+		DKWidget_SetProperty("DKGui/Taskbar.html","bottom",pos+"rem");
+		DK_DoFrame();
+    }
+}
