b6d8502c93b0dcc34804baafb3aefba259b38651
dkuser <dkuser@digitalknob.com>
2018-06-09 17:58:41 -0700

    commit from git

--- DKPlugins/DKBuild/DKBuild.js
+++ DKPlugins/DKBuild/DKBuild.js
@@ -123,46 +123,46 @@ function DKBuild_InstallSvn()
 }
 
 //////////////////////////////
-function DKBuild_ValidateGit()
-{
-	if(DK_GetBrowser() != "Rocket"){ return; }
-	DKLog("Looking for GIT \n");
-	//DKLog(GIT+"\n");
-	if(!DKFile_Exists(GIT)){
-		DKLog("Please install GIT \n");
-		DKBuild_InstallGit();
-	}
-	DKLog("Found GIT \n");
-	if(DK_GetOS() == "Mac"){
-		GIT = "git";
-	}
-}
+//function DKBuild_ValidateGit()
+//{
+//	if(DK_GetBrowser() != "Rocket"){ return; }
+//	DKLog("Looking for GIT \n");
+//	//DKLog(GIT+"\n");
+//	if(!DKFile_Exists(GIT)){
+//		DKLog("Please install GIT \n");
+//		DKBuild_InstallGit();
+//	}
+//	DKLog("Found GIT \n");
+//	if(DK_GetOS() == "Mac"){
+//		GIT = "git";
+//	}
+//}
 
 /////////////////////////////
-function DKBuild_InstallGit()
-{
-	if(DK_GetBrowser() != "Rocket"){ return; }
-	DKLog("Installing Git \n");
-	var assets = DKAssets_LocalAssets();
-	
-	if(DK_GetOS() == "Win32"){
-		DKCurl_Download("http://DigitalKnob.com/Download/Tools/Git-2.11.0-32-bit.exe", assets);
-		DK_System(assets+"/Git-2.11.0-32-bit.exe");
-	}
-	else if(DK_GetOS() == "Win64"){
-		DKCurl_Download("http://DigitalKnob.com/Download/Tools/Git-2.11.0-64-bit.exe", assets);
-		DK_System(assets+"/Git-2.11.0-64-bit.exe");
-	}
-	else if(DK_GetOS() == "Mac"){
-		//TODO
-	}
-	else if(DK_GetOS() == "Linux"){
-		DK_Execute("sudo apt-get install git");
-	}
-	else{
-		DKLog("ERROR: unrecognied HOST OS: "+DK_GetOS(), DKINFO);
-	}
-}
+//function DKBuild_InstallGit()
+//{
+//	if(DK_GetBrowser() != "Rocket"){ return; }
+//	DKLog("Installing Git \n");
+//	var assets = DKAssets_LocalAssets();
+//	
+//	if(DK_GetOS() == "Win32"){
+//		DKCurl_Download("http://DigitalKnob.com/Download/Tools/Git-2.11.0-32-bit.exe", assets);
+//		DK_System(assets+"/Git-2.11.0-32-bit.exe");
+//	}
+//	else if(DK_GetOS() == "Win64"){
+//		DKCurl_Download("http://DigitalKnob.com/Download/Tools/Git-2.11.0-64-bit.exe", assets);
+//		DK_System(assets+"/Git-2.11.0-64-bit.exe");
+//	}
+//	else if(DK_GetOS() == "Mac"){
+//		//TODO
+//	}
+//	else if(DK_GetOS() == "Linux"){
+//		DK_Execute("sudo apt-get install git");
+//	}
+//	else{
+//		DKLog("ERROR: unrecognied HOST OS: "+DK_GetOS(), DKINFO);
+//	}
+//}
 
 ////////////////////////////////
 function DKBuild_ValidateCmake()
@@ -393,85 +393,85 @@ function DKBuild_SvnCommit()
 }
 
 ////////////////////////////
-function DKBuild_GitUpdate()
-{
-	if(DK_GetBrowser() != "CEF" && DK_GetBrowser() != "Rocket"){
-		return;
-	}
-	
-	DKLog("Git Update DigitalKnob... \n");
-	DK_Execute(GIT +" clone https://github.com/aquawicket/DigitalKnob.git "+DKPATH+"/DK");
-	DKFile_ChDir(DKPATH+"/DK");
-	DK_Execute(GIT +" checkout -- .");
-	DK_Execute(GIT +" pull origin master");
-	
-	//Multipe user folders
-	var contents = DKFile_DirectoryContents(DKPATH);
-	var files = contents.split(",");
-	for(var i=0; i<files.length; i++){ //DKLog("files["+i+"] = "+files[i]+"\n");
-		DKFile_ChDir(DKPATH);
-		if(DKFile_IsDirectory(files[i])){ continue; }
-		var url = DKFile_GetSetting(files[i], "[MYGIT]");
-		if(url){ //DKLog("url = "+url+"\n");
-			var folder = files[i].replace(".txt",""); //DKLog("folder = "+folder+"\n");
-			DKLog("Git Update "+folder+"... \n");
-			DK_Execute(GIT +" clone "+url+" "+DKPATH+"/"+folder);
-			DK_Execute(GIT +" checkout -- .");
-			DK_Execute(GIT +" pull origin master");
-		}
-	}
-	
-	if(DKAvailable("DKAudio")){
-		DKCreate("DKAudio");
-	}
-	if(DKValid("DKAudioJS,DKAudioJS0")){
-		DKAudio_PlaySound("DKBuild/ding.wav");
-	}
-}
+//function DKBuild_GitUpdate()
+//{
+//	if(DK_GetBrowser() != "CEF" && DK_GetBrowser() != "Rocket"){
+//		return;
+//	}
+//	
+//	DKLog("Git Update DigitalKnob... \n");
+//	DK_Execute(GIT +" clone https://github.com/aquawicket/DigitalKnob.git "+DKPATH+"/DK");
+//	DKFile_ChDir(DKPATH+"/DK");
+//	DK_Execute(GIT +" checkout -- .");
+//	DK_Execute(GIT +" pull origin master");
+//	
+//	//Multipe user folders
+//	var contents = DKFile_DirectoryContents(DKPATH);
+//	var files = contents.split(",");
+//	for(var i=0; i<files.length; i++){ //DKLog("files["+i+"] = "+files[i]+"\n");
+//		DKFile_ChDir(DKPATH);
+//		if(DKFile_IsDirectory(files[i])){ continue; }
+//		var url = DKFile_GetSetting(files[i], "[MYGIT]");
+//		if(url){ //DKLog("url = "+url+"\n");
+//			var folder = files[i].replace(".txt",""); //DKLog("folder = "+folder+"\n");
+//			DKLog("Git Update "+folder+"... \n");
+//			DK_Execute(GIT +" clone "+url+" "+DKPATH+"/"+folder);
+//			DK_Execute(GIT +" checkout -- .");
+//			DK_Execute(GIT +" pull origin master");
+//		}
+//	}
+//	
+//	if(DKAvailable("DKAudio")){
+//		DKCreate("DKAudio");
+//	}
+//	if(DKValid("DKAudioJS,DKAudioJS0")){
+//		DKAudio_PlaySound("DKBuild/ding.wav");
+//	}
+//}
 
 ////////////////////////////
-function DKBuild_GitCommit()
-{
-	if(DK_GetBrowser() != "CEF" && DK_GetBrowser() != "Rocket"){
-		return;
-	}
-	
-	DKLog("Git Commit DigitalKnob... \n");
-	DKFile_ChDir(DKPATH+"/DK");
-	DK_Execute(GIT +" init");
-	DK_Execute(GIT +" config user.name \"dkuser\"");
-	DK_Execute(GIT +" config user.email \"dkuser@digitalknob.com\"");
-	DK_Execute(GIT +" commit -a -m \"commit from git\"");
-	DK_Execute(GIT +" config credential.helper store"); //store credentials 
-	DK_Execute(GIT +" push");
-	
-	//Multipe user folders
-	var contents = DKFile_DirectoryContents(DKPATH);
-	var files = contents.split(",");
-	for(var i=0; i<files.length; i++){ //DKLog("files["+i+"] = "+files[i]+"\n");
-		DKFile_ChDir(DKPATH);
-		if(DKFile_IsDirectory(files[i])){ continue; }
-		var url = DKFile_GetSetting(files[i], "[MYGIT]");
-		if(url){ //DKLog("url = "+url+"\n");
-			var folder = files[i].replace(".txt",""); //DKLog("folder = "+folder+"\n");
-			DKLog("Git Commit "+folder+"... \n");
-			DKFile_ChDir(DKPATH+"/"+folder);
-			DK_Execute(GIT +" init");
-			DK_Execute(GIT +" config user.name \"dkuser\"");
-			DK_Execute(GIT +" config user.email \"dkuser@digitalknob.com\"");
-			DK_Execute(GIT +" commit -a -m \"commit from git\"");
-			DK_Execute(GIT +" config credential.helper store"); //store credentials 
-			DK_Execute(GIT +" push");
-		}
-	}
-	
-	if(DKAvailable("DKAudio")){
-		DKCreate("DKAudio");
-	}
-	if(DKValid("DKAudioJS,DKAudioJS0")){
-		DKAudio_PlaySound("DKBuild/ding.wav");
-	}
-}
+//function DKBuild_GitCommit()
+//{
+//	if(DK_GetBrowser() != "CEF" && DK_GetBrowser() != "Rocket"){
+//		return;
+//	}
+//	
+//	DKLog("Git Commit DigitalKnob... \n");
+//	DKFile_ChDir(DKPATH+"/DK");
+//	DK_Execute(GIT +" init");
+//	DK_Execute(GIT +" config user.name \"dkuser\"");
+//	DK_Execute(GIT +" config user.email \"dkuser@digitalknob.com\"");
+//	DK_Execute(GIT +" commit -a -m \"commit from git\"");
+//	DK_Execute(GIT +" config credential.helper store"); //store credentials 
+//	DK_Execute(GIT +" push");
+//	
+//	//Multipe user folders
+//	var contents = DKFile_DirectoryContents(DKPATH);
+//	var files = contents.split(",");
+//	for(var i=0; i<files.length; i++){ //DKLog("files["+i+"] = "+files[i]+"\n");
+//		DKFile_ChDir(DKPATH);
+//		if(DKFile_IsDirectory(files[i])){ continue; }
+//		var url = DKFile_GetSetting(files[i], "[MYGIT]");
+//		if(url){ //DKLog("url = "+url+"\n");
+//			var folder = files[i].replace(".txt",""); //DKLog("folder = "+folder+"\n");
+//			DKLog("Git Commit "+folder+"... \n");
+//			DKFile_ChDir(DKPATH+"/"+folder);
+//			DK_Execute(GIT +" init");
+//			DK_Execute(GIT +" config user.name \"dkuser\"");
+//			DK_Execute(GIT +" config user.email \"dkuser@digitalknob.com\"");
+//			DK_Execute(GIT +" commit -a -m \"commit from git\"");
+//			DK_Execute(GIT +" config credential.helper store"); //store credentials 
+//			DK_Execute(GIT +" push");
+//		}
+//	}
+//	
+//	if(DKAvailable("DKAudio")){
+//		DKCreate("DKAudio");
+//	}
+//	if(DKValid("DKAudioJS,DKAudioJS0")){
+//		DKAudio_PlaySound("DKBuild/ding.wav");
+//	}
+//}
 
 /////////////////////////////////
 function DKBuild_GitCredentials()
