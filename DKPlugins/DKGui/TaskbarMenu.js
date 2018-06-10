f0180681f6c0be83c6bcb4cb95b102087a9099c9
dkuser <dkuser@digitalknob.com>
2018-06-09 17:46:39 -0700

    commit from git

--- DKPlugins/DKGui/TaskbarMenu.js
+++ DKPlugins/DKGui/TaskbarMenu.js
@@ -87,9 +87,13 @@ function TaskbarMenu_OnEvent(event)
 	}
 	
 	if(DK_Id(event,"Git")){
-		DKCreate("DKBuild/GitMenu.js", function(){
-			DKMenu_ValidatePosition("DKBuild/GitMenu.html");
-			DKWidget_SetProperty("DKBuild/GitMenu.html","top",DKWindow_GetMouseY()-30+"px");
+		//DKCreate("DKBuild/GitMenu.js", function(){
+		//	DKMenu_ValidatePosition("DKBuild/GitMenu.html");
+		//	DKWidget_SetProperty("DKBuild/GitMenu.html","top",DKWindow_GetMouseY()-30+"px");
+		//});
+		DKCreate("DKGit/GitMenu.js", function(){
+			DKMenu_ValidatePosition("DKGit/GitMenu.html");
+			DKWidget_SetProperty("DKGit/GitMenu.html","top",DKWindow_GetMouseY()-30+"px");
 		});
 	}
 	if(DK_Id(event, "OpenSource")){
