8649918bc7322f07d12e91995046113ef2136262
dkuser <dkuser@digitalknob.com>
2018-10-03 05:24:57 -0700

    commit from git

--- DKApps/DKGui/assets/app.js
+++ DKApps/DKGui/assets/app.js
@@ -3,6 +3,7 @@ var USE_WEBVIEW = 0; //TODO: Android, iOS
 var USE_SDL     = 0; //Use with caution
 var USE_ROCKET  = 0; //Use with caution
 var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"index.html";
+//var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"DKNotepad.html";
 //var DKApp_url = "http://digitalknob.com/DKGui/index.html";
 
 DKCreate("DK/init.js", function(){});
