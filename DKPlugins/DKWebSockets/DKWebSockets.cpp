e950bf2bfa3f88bd6025604bd200cb6559cb35e1
dkuser <dkuser@digitalknob.com>
2018-10-13 01:41:44 -0700

    commit from git

--- DKPlugins/DKWebSockets/DKWebSockets.cpp
+++ DKPlugins/DKWebSockets/DKWebSockets.cpp
@@ -3,11 +3,15 @@
 
 #include "libwebsockets.h"
 
+
 /////////////////////////
 bool DKWebSockets::Init()
 {
 	//DKClass::DKCreate("DKWebSocketsJS");
 	//DKClass::DKCreate("DKWebSocketsV8");
+
+	//TODO look into test-echo.c for details on how to build a simple echo server 
+	//C:\digitalknob\DK\3rdParty\libwebsockets-2.2-stable\test-server\test-echo.c
 	return true;
 }
 
