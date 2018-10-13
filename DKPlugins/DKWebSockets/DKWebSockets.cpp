954cd6a5e4707ed2e7d0fcaf755618ac8e409303
dkuser <dkuser@digitalknob.com>
2018-10-13 06:20:08 -0700

    commit from git

--- DKPlugins/DKWebSockets/DKWebSockets.cpp
+++ DKPlugins/DKWebSockets/DKWebSockets.cpp
@@ -3,7 +3,10 @@
 #include "DK/stdafx.h"
 #include "DKWebSockets/DKWebSockets.h"
 
-
+uWS::WebSocket<true>* DKWebSockets::_ws;
+char* DKWebSockets::_message;
+size_t DKWebSockets::_length;
+uWS::OpCode DKWebSockets::_opCode;
 
 /////////////////////////
 bool DKWebSockets::Init()
@@ -14,7 +17,6 @@ bool DKWebSockets::Init()
 	h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode){
 		ProcessMessage(ws, message, length, opCode);
 		//ws->send(message, length, opCode); //echo
-		
 	});
 
 	DKApp::AppendLoopFunc(&DKWebSockets::Loop, this);
@@ -34,14 +36,28 @@ void DKWebSockets::Loop()
 	if(h.listen(3000)){
 		h.poll();
 	}
+
+	// Test sending..   fails
+	/*
+	long ticks;
+	DKUtil::GetTicks(ticks);
+	DKLog("DKUtil::GetTicks() = "+toString(ticks)+"\n", DKINFO);
+	if(ticks > 26169041){
+		SendMessage("test");
+	}
+	*/
 }
 
 //////////////////////////////////////////////////////////////////////////
 bool DKWebSockets::ProcessMessage(uWS::WebSocket<true>* ws, char *message, size_t length, uWS::OpCode opCode)
 {
 	//TODO
-	DKLog("DKWebSockets::ProcessMessage()\n", DKINFO);
-	ws->send(message, length, opCode); //echo
+	DKLog("DKWebSockets::ProcessMessage("+DKString(message)+")\n", DKINFO);
+	_ws = ws;
+	_message = message;
+	_length = length;
+	_opCode = opCode;
+	//ws->send(message, length, opCode); //echo
 	return false;
 }
 
@@ -50,6 +66,6 @@ bool DKWebSockets::SendMessage(const DKString& message)
 {
 	//TODO
 	DKLog("DKWebSockets::SendMessage("+message+")\n", DKINFO);
-	
+	_ws->send(_message, _length, _opCode); //echo
 	return false;
 }
