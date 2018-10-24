8acf3da9dd815e495e40d47db5802fdc9a49b8d6
dkuser <dkuser@digitalknob.com>
2018-10-24 08:03:13 -0700

    commit from git

--- DKPlugins/DKDom/DKDom.js
+++ DKPlugins/DKDom/DKDom.js
@@ -20,41 +20,53 @@ function DKDom_OnEvent(event)
 //////////////////////////
 function DKDom_Test(event)
 {
-	// Underlying plain object.
-	var target = { foo: 'bar' };
+	
+	function Window(){
+		this.name = 'WindowName';
 
-	// Handler table, provides traps for interaction (can be modified on-the-fly).
-	var handler = {
-		has: function (targ, key) {
-			DKLog('has called for key=' + key+'\n');
-			return key in targ;  // return unmodified existence status
-		},
-
-		get: function (targ, key, recv) {
-			DKLog('get called for key=' + key+'\n');
-			return targ[key];  // return unmodified value
-		},
-
-		set: function (targ, key, val, recv) {
-			DKLog('set called for key=' + key + ', val=' + val+'\n');
-			targ[key] = val;  // must perform write to target manually if 'set' defined
-			return true;      // true: indicate that property write was allowed
-		},
-
-		deleteProperty: function (targ, key) {
-			DKLog('deleteProperty called for key=' + key+'\n');
-			delete targ[key];  // must perform delete to target manually if 'deleteProperty' defined
-			return true;       // true: indicate that property delete was allowed
+		Window.prototype.alert = function(str){
+			DKLog("alert: "+str+'\n');
 		}
-	};
+	
+		return new Proxy(this, { // Wrap it behind a proxy
+			// proxy traps
+			has: function (targ, key) {
+				//DKLog('has called for key=' + key+'\n');
+				return key in targ;  // return unmodified existence status
+			},
 
-	// Create proxy object.
-	var proxy = new Proxy(target, handler);
+			get: function (targ, key, recv) {
+				//DKLog('get called for key=' + key+'\n');
+				return targ[key];  // return unmodified value
+			},
 
-	// Proxy object is then accessed normally.
-	var str = 'foo' in proxy;
-	DKLog(String(str)+'\n');
-	proxy.foo = 321;
-	DKLog(String(proxy.foo)+'\n');
-	delete proxy.foo;
+			set: function (targ, key, val, recv) {
+				//DKLog('set called for key=' + key + ', val=' + val+'\n');
+				targ[key] = val;  // must perform write to target manually if 'set' defined
+				return true;      // true: indicate that property write was allowed
+			},
+		
+			deleteProperty: function (targ, key) {
+				//DKLog('deleteProperty called for key=' + key+'\n');
+				delete targ[key];  // must perform delete to target manually if 'deleteProperty' defined
+				return true;       // true: indicate that property delete was allowed
+			}
+		});
+	}
+
+
+	var window = new Window();
+	
+	//Test object funtion
+	window.alert("This is a test alert");
+	
+	//Test object property
+	if(!'name' in window){
+		DKLog("we could not find the property\n");
+	}
+	DKLog("window.name = "+window.name+'\n');
+	window.name = 321;
+	DKLog("window.name now = "+window.name+'\n');
+
+	delete window.name;
 }
