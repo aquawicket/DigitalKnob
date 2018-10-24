f13557d60a3608d8971de423879af67af7eefc88
dkuser <dkuser@digitalknob.com>
2018-10-24 02:55:35 -0700

    commit from git

--- DKPlugins/DK/DKBrowser.js
+++ DKPlugins/DK/DKBrowser.js
@@ -22,6 +22,43 @@ var objectY;
 var drag_id;
 var events = [];
 
+
+//This function is used to debug googlebot crawling
+/////////////////////////////////////////////
+window.addEventListener('error', function(e){
+    var errorText = [
+        e.message,
+        'URL: ' + e.filename,
+        'Line: ' + e.lineno + ', Column: ' + e.colno,
+        'Stack: ' + (e.error && e.error.stack || '(no stack trace)')
+    ].join('\n');
+
+    // Example: log errors as visual output into the host page.
+    // Note: you probably don’t want to show such errors to users, or
+    //       have the errors get indexed by Googlebot; however, it may
+    //       be a useful feature while actively debugging the page.
+    var DOM_ID = 'rendering-debug-pre';
+    if(!document.getElementById(DOM_ID)){
+        var log = document.createElement('pre');
+        log.id = DOM_ID;
+        log.style.whiteSpace = 'pre-wrap';
+        log.textContent = errorText;
+        if (!document.body) document.body = document.createElement('body');
+        document.body.insertBefore(log, document.body.firstChild);
+    } else {
+        document.getElementById(DOM_ID).textContent += '\n\n' + errorText;
+    }
+
+    // Example: log the error to remote service.
+    // Note: you can log errors to a remote service, to understand
+    //       and monitor the types of errors encountered by regular users,
+    //       Googlebot, and other crawlers.
+    var client = new XMLHttpRequest();
+    client.open('POST', 'https://example.com/logError');
+    client.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+    client.send(errorText);
+});
+
 ///////////////////////////////////
 document.onmousemove = function(e){
   if(DK_IE()){ // grab the x-y pos.s if browser is IE
