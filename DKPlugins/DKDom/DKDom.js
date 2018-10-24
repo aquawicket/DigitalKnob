8a03544b457662e21a9d9c4c620a6567598e42a1
dkuser <dkuser@digitalknob.com>
2018-10-24 10:06:29 -0700

    commit from git

--- DKPlugins/DKDom/DKDom.js
+++ DKPlugins/DKDom/DKDom.js
@@ -28,9 +28,6 @@ function DKDom_Create(event)
 	function Window(){
 		document = new Document();
 		this.document = document;
-		this.name;
-		this.innerWidth;
-		this.innerHeight;
 
 		Window.prototype.alert = function(str){
 			DKLog("alert: "+str+'\n');
@@ -61,13 +58,10 @@ function DKDom_Create(event)
 	
 	////////////////////
 	function Document(){
-		this.title;
 
 		Document.prototype.getElementById = function(id){
-			DKLog("document.getElementById("+id+")\n");
 			if(DKWidget_ElementExists(id)){
-				var element = new Element();
-				element.id = id;
+				var element = new Element(id);
 				return element;
 			}
 		}
@@ -90,33 +84,72 @@ function DKDom_Create(event)
 		});
 	}
 	
-	///////////////////
-	function Element(){
-		this.id;
-		this.position;
-		this.width;
-		this.height;
+	/////////////////////
+	function Element(id){
+		this.id = id;
+		this.style = new Style(id);
 
+		Element.prototype.getAttribute = function(attribute){
+			this[attribute] = DKWidget_GetAttribute(this.id, attribute);
+			return this[attribute];
+		}
+		Element.prototype.hasAttribute = function(attribute){
+			if(DKWidget_HasAttribute(this.id, attribute)){ return true; }
+			else{ return false; }
+		}
+		Element.prototype.setAttribute = function(attribute, value){
+			DKWidget_SetAttribute(this.id, attribute, value);
+			this[attribute] = value;
+		}
 		Element.prototype.toString = function(){
 			DKLog("element.toString()\n");
 		}
-	
+		
 		return new Proxy(this, {
 			has: function (targ, key){
 				return key in targ;
 			},
 			get: function (targ, key, recv){
-				if(key == "height"){ targ[key] = DKWidget_GetProperty(targ["id"], "height"); }
+				//if(key == "height"){ targ[key] = DKWidget_GetProperty(targ["id"], "height"); }
 				if(key == "id"){ targ[key] = DKWidget_GetAttribute(targ["id"], "id"); }
-				if(key == "position"){ targ[key] = DKWidget_GetProperty(targ["id"], "position"); }
-				if(key == "width"){ targ[key] = DKWidget_GetProperty(targ["id"], "width"); }
+				//if(key == "position"){ targ[key] = DKWidget_GetProperty(targ["id"], "position"); }
+				//if(key == "width"){ targ[key] = DKWidget_GetProperty(targ["id"], "width"); }
 				return targ[key];
 			},
 			set: function (targ, key, val, recv){
-				if(key == "height"){ targ[key] = DKWidget_SetProperty(targ["id"], "height", val); }
+				//if(key == "height"){ targ[key] = DKWidget_SetProperty(targ["id"], "height", val); }
 				//if(key == "id"){ targ[key] = DKWidget_SetAttribute(targ["id"], "id", val); }
-				if(key == "position"){ targ[key] = DKWidget_SetProperty(targ["id"], "position", val); }
-				if(key == "width"){ targ[key] = DKWidget_SetProperty(targ["id"], "width", val); }
+				//if(key == "position"){ targ[key] = DKWidget_SetProperty(targ["id"], "position", val); }
+				//if(key == "width"){ targ[key] = DKWidget_SetProperty(targ["id"], "width", val); }
+				targ[key] = val;
+				return true;
+			},
+			deleteProperty: function (targ, key){
+				delete targ[key];
+				return true;
+			}
+		});
+	}
+	
+	///////////////////
+	function Style(id){
+		this.id = id;
+		return new Proxy(this, {
+			has: function (targ, key){
+				return key in targ;
+			},
+			get: function (targ, key, recv){
+
+				DKLog("targ[id] = "+targ["id"]+"\n");
+				if(key == "id"){ return true; }
+				//if(key == "width"){ targ[key] = DKWidget_GetProperty(targ["id"], "width"); }
+				targ[key] = DKWidget_GetProperty(targ["id"], key);
+				return targ[key];
+			},
+			set: function (targ, key, val, recv){
+				if(key == "id"){ return true; }
+				//if(key == "width"){ targ[key] = DKWidget_SetProperty(targ["id"], "width", val); }
+				DKWidget_SetProperty(targ["id"], key, val);
 				targ[key] = val;
 				return true;
 			},
@@ -133,24 +166,26 @@ function DKDom_Create(event)
 /////////////////////
 function DKDom_Test()
 {
+	/*
+	//window tests
+	window.alert("test");
+	DKLog("window.innerHeight: "+window.innerHeight+"\n");
+	DKLog("window.innerWidth: "+window.innerWidth+"\n");
+	*/
+	
+	//document tests
+	//TODO
+	
+	//element tests 
 	var element = document.getElementById("BugReport_Image");
 	DKLog("element.id = "+element.id+"\n");
-	DKLog("element.position = "+element.position+"\n");
-	DKLog("element.width = "+element.width+"\n");
-	DKLog("element.height = "+element.height+"\n");
-	//window.innerWidth = 100;
-	//element.height = "20px";
-	//DKLog("element.width = "+element.width+"\n");
+	DKLog("element.style.width = "+element.style.width+"\n");
 	
-	/*
-	window.alert("This is a test alert");
-	if(!'name' in window){ DKLog("we could not find the property\n"); }
-	DKLog("window.name = "+window.name+'\n');
-	window.name = 321;
-	DKLog("window.name now = "+window.name+'\n');
-	DKLog("window.document.title = "+window.document.title+"\n");
-	window.document.getElementById("ElementId");
-	DKLog("window.innerWidth = "+window.innerWidth+"\n");
-	delete window;
-	*/
+	//DKLog("element.style.width = "+element.style.width+"\n");
+	//DKLog("element.style.height = "+element.style.height+"\n");
+	//DKLog("element.hasAttribute(id) = "+element.hasAttribute("id")+"\n");
+	
+	//element.setAttribute("test", "http://digitalknob.com/image.png");
+	//DKLog("element.getAttribute(test) = "+element.getAttribute("test")+"\n");
+
 }
