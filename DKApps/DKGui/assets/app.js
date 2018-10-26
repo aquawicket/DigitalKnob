3aed09746be801c6357fe246980589f1f5f336fc
dkuser <dkuser@digitalknob.com>
2018-10-26 12:00:17 -0700

    commit from git

--- DKApps/DKGui/assets/app.js
+++ DKApps/DKGui/assets/app.js
@@ -1,7 +1,7 @@
-var USE_CEF     = 1; //Desktop
+var USE_CEF     = 0; //Desktop
 var USE_WEBVIEW = 0; //TODO: Android, iOS
 var USE_SDL     = 0; //Use with caution
-var USE_ROCKET  = 0; //Use with caution
+var USE_ROCKET  = 1; //Use with caution
 var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"index.html";
 //var DKApp_url = "http://google.com";
 //var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"index.html?plugin=DKNotepad/DKNotepad.js";
