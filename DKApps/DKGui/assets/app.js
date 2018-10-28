6cb2b3c42ab198d047a8ca2b9b1b194fece32b84
dkuser <dkuser@digitalknob.com>
2018-10-28 02:39:43 -0700

    commit from git

--- DKApps/DKGui/assets/app.js
+++ DKApps/DKGui/assets/app.js
@@ -1,4 +1,4 @@
-var USE_CEF     = 1; //Desktop
+var USE_CEF     = 0; //Desktop
 var USE_WEBVIEW = 0; //TODO: Android, iOS
 var USE_SDL     = 0; //Use with caution
 var USE_ROCKET  = 1; //Use with caution
