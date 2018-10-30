8be3ebb6b9f02873277323c846207349ac307edd
dkuser <dkuser@digitalknob.com>
2018-10-30 02:05:23 -0700

    commit from git

--- DKPlugins/DKRocket/DKDom.js
+++ DKPlugins/DKRocket/DKDom.js
@@ -28,9 +28,11 @@ function DKDom_OnEvent(event)
 
 
 
-/////////////////////////////
-var EventTarget = function(){
-	DKDEBUGFUNC();
+////////////////////////////////////
+var EventTarget = function(pointer){
+	//DKDEBUGFUNC();
+	DKWARN("EventTarget("+pointer+")\n");
+	this.pointer = pointer;
 	this.listeners = {};
 };
 EventTarget.prototype.listeners = null;
@@ -63,20 +65,44 @@ EventTarget.prototype.dispatchEvent = function(event){
 	return !event.defaultPrevented;
 };
 	
+	
 /////////////////////////////
 var Node = function(pointer){
-	DKDEBUGFUNC();
-	this.pointer = pointer;
-	EventTarget.call(this);
+	//DKDEBUGFUNC();
+	DKWARN("Node("+pointer+")\n");
+	EventTarget.call(this, pointer);
 	
 	return new Proxy(this, { 
 		has: function(targ, key){
 			return key in targ;
 		},
 		get: function(targ, key, recv){
+			//DKINFO("Node:get("+targ+","+key+")\n");
+			if(typeof targ[key] === "function" || typeof key === "symbol" || key == "pointer" || key == "style"){ return targ[key]; }
+			if(key == "innerHTML"){
+				targ[key] = DKRocket_innerHTML(targ["pointer"], key); 
+			}
+			else if(key == "parentNode"){
+				var parentNode = DKRocket_parentNode(targ["pointer"], key);
+				targ[key] = new Node(parentNode);
+			}
+			else{
+				//DKWARN("targ['pointer']: "+targ["pointer"]+"\n");
+				if(targ["pointer"]){
+					targ[key] = DKRocket_getAttribute(targ["pointer"], key); 
+				}
+			}
 			return targ[key];
 		},
 		set: function(targ, key, val, recv){
+			//DKINFO("Node:set("+targ+","+key+","+val+")\n");
+			if(typeof targ[key] === "function" || typeof key === "symbol" || key == "pointer" || key == "style"){ return true; }
+			if(key == "innerHTML"){
+				DKRocket_setInnerHTML(targ["pointer"], val);
+			}
+			else{
+				DKRocket_setAttribute(targ["pointer"], key, val);
+			}
 			targ[key] = val;
 			return true;
 		},
@@ -93,18 +119,46 @@ Node.prototype.appendChild = function(aChild){
 	var element = new Node(pointer);
 	return element;
 }
-	
+Node.prototype.removeChild = function(aChild){
+	var pointer = DKRocket_removeChild(this.pointer, aChild.pointer);
+	if(!pointer){ return null; }
+	var node = new Node(pointer);
+	return node;
+}
+
 	
 ////////////////////////////////
 var Element = function(pointer){
-	DKDEBUGFUNC();
+	//DKDEBUGFUNC();
+	DKWARN("Element("+pointer+")\n");
 	return Node.call(this, pointer);
 }
 Element.prototype = Node.prototype;
-Element.prototype.hasAttribute = function(name){
-	return DKRocket_hasAttribute(name);
+Element.prototype.hasAttribute = function(attribute){
+	if(DKRocket_hasAttribute(this.pointer, attribute)){ return true; }
+	else{ return false; }
+}
+Element.prototype.getAttribute = function(attribute){
+	this[attribute] = DKRocket_getAttribute(this.pointer, attribute);
+	if(!this[attribute]){ return null; }
+	return this[attribute];
+}
+Element.prototype.setAttribute = function(attribute, value){
+	DKRocket_setAttribute(this.pointer, attribute, value);
+	this[attribute] = value;
 }
 	
+
+////////////////////////////////////
+var HTMLElement = function(pointer){
+	//DKDEBUGFUNC();
+	DKWARN("HTMLElement("+pointer+")\n");
+	this.style = new CSSStyleDeclaration(pointer);
+	return Element.call(this, pointer);
+}
+HTMLElement.prototype = Element.prototype;
+
+
 /////////////////////////////////
 var HTMLCollection = function(){}
 HTMLCollection.prototype = [];
@@ -112,17 +166,12 @@ HTMLCollection.prototype.item = function(index){
 	return this[index];
 }
 	
-////////////////////////////////////
-var HTMLElement = function(pointer){
-	DKDEBUGFUNC();
-	this.style = new CSSStyleDeclaration(pointer);
-	return Element.call(this, pointer);
-}
-HTMLElement.prototype = Element.prototype;
+
 
 ////////////////////////////////////////////
 var CSSStyleDeclaration = function(pointer){
-	DKDEBUGFUNC();
+	//DKDEBUGFUNC();
+	DKWARN("CSSStyleDeclaration("+pointer+")\n");
 	this.pointer = pointer;
 	
 	return new Proxy(this, {
@@ -160,15 +209,16 @@ CSSStyleDeclaration.prototype.getPropertyValue = function(propertyName){
 }
 	
 	
-////////////////////////////////
+/////////////////////////////////
 var Document = function(pointer){
-	DKDEBUGFUNC();
+	//DKDEBUGFUNC();
+	DKWARN("Document("+pointer+")\n");
 	this.body = this.getElementsByTagName("body")[0];
 	return Node.call(this, pointer);
 }
 Document.prototype = Node.prototype;
 Document.prototype.createElement = function(tagName){
-	var pointer = DKRocket_createElement(tag);
+	var pointer = DKRocket_createElement(tagName);
 	var htmlElement = new HTMLElement(pointer);
 	return htmlElement;
 }
@@ -202,7 +252,8 @@ Document.prototype.getElementsByTagName = function(name){
 	
 ////////////////////////
 var Window = function(){
-	DKDEBUGFUNC();
+	//DKDEBUGFUNC();
+	DKWARN("Window()\n");
 	document = new Document();
 	this.document = document;
 
@@ -210,13 +261,15 @@ var Window = function(){
 		has: function (targ, key) {
 			return key in targ;  // return unmodified existence status
 		},
-		get: function (targ, key, recv) {
+		get: function(targ, key, recv){
+			//DKINFO("Window:get("+targ+","+key+")\n");
 			if(key == "innerHeight"){ targ[key] = DKRocket_innerHeight(); }
 			if(key == "innerWidth"){ targ[key] = DKRocket_innerWidth(); }
 			if(key == "name"){ targ[key] = DKRocket_name(); }
 			return targ[key];  // return unmodified value
 		},
-		set: function (targ, key, val, recv) {
+		set: function(targ, key, val, recv){
+			//DKINFO("Window:get("+targ+","+key+","+val+")\n");
 			//if(key == "innerHeight"){ DKRocket_SetInnerHeight(val); }  //TODO
 			//if(key == "innerWidth"){ DKRocket_SetInnerWidth(val); }    //TODO
 			//if(key == "name"){ DKRocket_SetName(val); }    //TODO
@@ -236,6 +289,7 @@ Window.prototype.alert = function(msg){
 
 /////////////////////////
 var Console = function(){
+	DKWARN("Console()\n");
 	DKDEBUGFUNC();
 }
 Console.prototype.assert = function(assertion, msg){
@@ -253,7 +307,7 @@ Console.prototype.error = function(msg){
 }
 Console.prototype.exception = Console.prototype.error; //alias
 Console.prototype.info = function(msg){
-	DKINFO(str+"\n");
+	DKINFO(msg+"\n");
 }
 Console.prototype.log = function(msg){
 	DKINFO(msg+"\n");
@@ -264,126 +318,7 @@ Console.prototype.trace = function(){
 Console.prototype.warn = function(msg){
 		DKWARN(msg+"\n");
 }
-	
-	
-/*
-/////////////////////////
-function Element(pointer)
-{
-	DKDEBUGFUNC();
-	this.pointer = pointer;
-	this.style = new Style(pointer);
-		Element.prototype.TEST = function(){
-		DKERROR("TEST\n");
-	}
-	
-	Element.prototype.appendChild = function(element){
-		var pointer = DKRocket_appendChild(this.pointer, element.pointer);
-		if(!pointer){ return; }
-		var element = new Element(pointer);
-		return element;
-	}
-	Element.prototype.getAttribute = function(attribute){
-		this[attribute] = DKRocket_getAttribute(this.pointer, attribute);
-		if(!this[attribute]){ return null; }
-		return this[attribute];
-	}
-	Element.prototype.hasAttribute = function(attribute){
-		if(DKRocket_hasAttribute(this.pointer, attribute)){ return true; }
-		else{ return false; }
-	}
-	Element.prototype.removeChild = function(element){
-		var pointer = DKRocket_removeChild(this.pointer, element.pointer);
-		if(!pointer){ return null; }
-		var element = new Element(pointer);
-		return element;
-	}
-	Element.prototype.setAttribute = function(attribute, value){
-		DKRocket_setAttribute(this.pointer, attribute, value);
-		this[attribute] = value;
-	}
 
-	return new Proxy(this, {
-		has: function (targ, key){
-			return key in targ;
-		},
-		get: function(targ, key, recv){
-			if(typeof targ[key] === "function" || typeof key === "symbol" || key == "pointer" || key == "style"){ return targ[key]; }
-			if(key == "innerHTML"){
-				targ[key] = DKRocket_innerHTML(targ["pointer"], key); 
-			}
-			else if(key == "parentNode"){
-				var parentNode = DKRocket_parentNode(targ["pointer"], key);
-				targ[key] = new Element(parentNode);
-			}
-			else{
-				targ[key] = DKRocket_getAttribute(targ["pointer"], key); 
-			}
-			return targ[key];
-		},
-		set: function (targ, key, val, recv){
-			//DKINFO("Element:set("+targ+","+key+","+val+")\n");
-			if(typeof targ[key] === "function" || key == "pointer" || key == "style"){ return true; }
-			if(key == "innerHTML"){
-				DKRocket_setInnerHTML(targ["pointer"], val);
-			}
-			else{
-				DKRocket_setAttribute(targ["pointer"], key, val);
-			}
-			targ[key] = val;
-			return true;
-		},
-		deleteProperty: function (targ, key){
-			delete targ[key];
-			return true;
-		}
-	});
-}
-*/
-	
-/*
-///////////////////////
-function Style(pointer)
-{
-	DKDEBUGFUNC();
-	this.pointer = pointer;
-	
-	Style.prototype.setProperty = function(propertyName, propertyValue, priority){
-		DKRocket_setProperty(this.pointer, propertyName, propertyValue);
-		this[propertyName] = propertyValue;
-	}
-	Style.prototype.getPropertyValue = function(propertyName){
-		this[propertyName] = DKRocket_getPropertyValue(this.pointer, propertyName);
-		return this[propertyName];
-	}
-	
-	return new Proxy(this, {
-		has: function (targ, key){
-			return key in targ;
-		},
-		get: function (targ, key, recv){
-			DKINFO("Style:get("+targ+","+key+")\n");
-			if(typeof targ[key] === "function" || key == "pointer"){ return targ[key]; }
-			if(key == "backgroundColor"){ targ[key] = DKRocket_getPropertyValue(targ["pointer"], "background-color"); }
-			else{ targ[key] = DKRocket_getPropertyValue(targ["pointer"], key); }
-			return targ[key];
-		},
-		set: function (targ, key, val, recv){
-			DKINFO("Style:set("+targ+","+key+","+val+")\n");
-			if(typeof targ[key] === "function" || key == "pointer"){ return true; }
-			if(key == "backgroundColor"){ DKRocket_setProperty(targ["pointer"], "background-color", val); }
-			else{ DKRocket_setProperty(targ["pointer"], key, val); }
-			targ[key] = val;
-			return true;
-		},
-		deleteProperty: function (targ, key){
-			delete targ[key];
-			return true;
-		}
-	});
-}
-*/
-
-	
+//////////////////////	
 window = new Window();
 console = new Console();
