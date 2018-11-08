6b12863ed936d0532ec6b9f52be464438055d186
dkuser <dkuser@digitalknob.com>
2018-11-08 04:26:06 -0800

    commit from git

--- DKPlugins/DKRocket/DKDom.js
+++ DKPlugins/DKRocket/DKDom.js
@@ -843,25 +843,6 @@ var Window = function(pointer){
 }
 
 
-////////////////////////////////
-var XMLHttpRequest = function(){
-	
-	XMLHttpRequest.prototype.open = function(method, url, async, user, password){
-		DKWARN("XMLHttpRequest.open("+method+","+url+","+async+")");
-		this.method = method;
-		this.url = url;
-		this.async = async;
-	}
-	XMLHttpRequest.prototype.send = function(){
-		DKWARN("XMLHttpRequest.send()");
-		this.readyState = 4;
-		this.status = 200;
-		this.statusText = "TODO";
-		this.responseText = DKRocket_send(this.method, this.url, this.async);
-		this.onreadystatechange();
-	}
-}
-
 
 //Global prototypes. Must be in order by dependency
 Window.prototype = EventTarget.prototype;
