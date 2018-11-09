099096031e7415d44692b0a26058f9d606be4d3c
dkuser <dkuser@digitalknob.com>
2018-11-08 21:21:54 -0800

    commit from git

--- DKPlugins/DKRocket/DKLocation.cpp
+++ DKPlugins/DKRocket/DKLocation.cpp
@@ -1,6 +1,7 @@
 #ifdef USE_DKDuktape 
 #include "DK/DKApp.h"
 #include "DKRocket/DKLocation.h"
+#include "DKRocket/DKRocket.h"
 
 
 ///////////////////////
@@ -16,8 +17,8 @@ bool DKLocation::Init()
 //////////////////////////////////////
 int DKLocation::href(duk_context* ctx)
 {
-	//TODO
-	return false;
+	duk_push_string(ctx, DKRocket::Get()->_url.c_str());
+	return true;
 }
 
 #endif //USE_DKDuktape
