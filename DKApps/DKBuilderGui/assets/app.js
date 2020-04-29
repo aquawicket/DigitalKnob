03b53bcc26671ba848128263d8a8dd5321025aa8
dkuser <dkuser@digitalknob.com>
2020-04-28 19:46:13 -0500

    commit from git

---
+++ DKApps/DKBuilderGui/assets/app.js
@@ -0,0 +1,22 @@
+DKCreate("DKWindow");
+DKCreate("DKRml");
+//DKRml_ToggleDebugger();
+
+location.href = "C:/digitalknob/DKTestApps/DKApps/DKSDLRmlUi/assets/test.html";
+
+//console.log("***********  js file reports *****************");
+//console.log(document.getElementById("html").innerHTML);
+//console.log("**********************************************");
+//document.getElementById("html").innerHTML = "<body>html</body>";
+//console.log("***********  js file reports *****************");
+//console.log(document.getElementById("html").innerHTML);
+//console.log("**********************************************");
+//console.log("window = "+window);
+//console.log("location = "+location);
+//console.log("document = "+document);
+
+//console.log(document.getElementById("test.html").innerHTML);
+//document.getElementById("test.html").innerHTML = "<div id=\"text\">text</div>"
+//console.log(document.getElementById("test.html").innerHTML);
+//document.getElementById("test.html").innerHTML = "<div id=\"div1\" style=\"position:absolute;top:0px;left:0px;width:20px;height:20px;background-color:rgb(255,0,0);\"></div><div id=\"div2\" style=\"position:absolute;left:0px;bottom:0px;width:20px;height:20px;background-color:rgb(0,255,0);\"></div><div id=\"div3\" style=\"position:absolute;top:0px;right:0px;width:20px;height:20px;background-color:rgb(0,0,255);\"></div><div id=\"div4\" style=\"position:absolute;right:0px;bottom:0px;width:20px;height:20px;background-color:rgb(255,0,255);\"></div><img id=\"gif_image\" src=\"test.gif\" style=\"position:absolute;left:30%;top:35%;width:40%;\"></img><img id=\"png_image\" src=\"test.png\" style=\"position:absolute;left:45%;top:10%;width:10%;\"></img><img id=\"bmp_image\" src=\"test.bmp\" style=\"position:absolute;left:10%;top:45%;width:10%;\"></img><img id=\"jpg_image\" src=\"test.jpg\" style=\"position:absolute;right:10%;top:45%;width:10%;\"></img>";
+//location.href = "C:/digitalknob/DKTestApps/DKApps/DKSDLRmlUi/assets/test.html";
