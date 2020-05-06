7bdc0c446e107c7b6d693dfcb9f91528f1ba00b5
dkuser <dkuser@digitalknob.com>
2020-05-06 15:07:39 -0400

    commit from git

--- DKPlugins/DKBuild/DKBuild.js
+++ DKPlugins/DKBuild/DKBuild.js
@@ -52,7 +52,7 @@ function DKBuild_Init()
 		NDK = DKPATH+"/DK/3rdParty/android-ndk-r10e";
 	}
 	if(DK_GetOS() == "Linux"){
-		DKPATH = "/home/aqualinux/Desktop/digitalknob";
+		DKPATH = "/home/paul/digitalknob";
 		//SVN = "/usr/bin/svn";
 		CMAKE = "/usr/bin/cmake";
 		NDK = DKPATH+"/DK/3rdParty/android-ndk-r10e";
