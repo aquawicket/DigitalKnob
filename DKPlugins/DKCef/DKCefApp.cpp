b828d5c11dd12a639538d0fe275dc8f721a640aa
dkuser <dkuser@digitalknob.com>
2020-05-16 09:15:09 -0500

    commit from git

--- DKPlugins/DKCef/DKCefApp.cpp
+++ DKPlugins/DKCef/DKCefApp.cpp
@@ -12,22 +12,22 @@ std::map<DKString, boost::function<bool(CefArgs, CefReturn)>> DKV8::functions;
 #endif
 std::vector<std::string> DKV8::funcs;
 
-DKString DKV8::homepage;
-DKString DKV8::multi_process;
-DKString DKV8::sandbox;
-DKString DKV8::log_severity;
-DKString DKV8::multi_threaded_message_loop;
 DKString DKV8::disable_gpu;
 DKString DKV8::disable_gpu_compositing;
-DKString DKV8::ignore_gpu_blacklist;
-DKString DKV8::remote_debugging_port;
-DKString DKV8::enable_system_flash;
-DKString DKV8::enable_begin_frame_scheduling;
-DKString DKV8::enable_webgl;
-DKString DKV8::renderer_process_limit;
-DKString DKV8::enable_gpu;
 DKString DKV8::disable_web_security;
+DKString DKV8::enable_begin_frame_scheduling;
+DKString DKV8::enable_gpu;
+DKString DKV8::enable_system_flash;
+DKString DKV8::enable_webgl;
+DKString DKV8::homepage;
+DKString DKV8::ignore_gpu_blacklist;
+DKString DKV8::log_severity;
+DKString DKV8::multi_process;
+DKString DKV8::multi_threaded_message_loop;
 DKString DKV8::no_proxy_server;
+DKString DKV8::remote_debugging_port;
+DKString DKV8::renderer_process_limit;
+DKString DKV8::sandbox;
 //DKString DKV8::off_screen_rendering_enabled;
 
 
@@ -329,6 +329,7 @@ void DKCefApp::OnBeforeCommandLineProcessing(const CefString& process_type, CefR
 	}
 	command_line->AppendSwitchWithValue("disable-extensions", "1");
 #ifdef LINUX
+    //TODO - onlyh do this is the flash flag is turned on
 	command_line->AppendSwitchWithValue("ppapi-flash-version", "25.0.0.127");
 	command_line->AppendSwitchWithValue("ppapi-flash-path", "/usr/lib/pepperflashplugin-nonfree/libpepflashplayer.so");
 #endif
