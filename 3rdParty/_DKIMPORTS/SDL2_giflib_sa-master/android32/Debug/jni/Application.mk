06bf0ae2fa53390dea0d360c5a1d4c86e6c4be17
dkuser <dkuser@digitalknob.com>
2020-05-21 19:52:13 -0500

    commit from git

---
+++ 3rdParty/_DKIMPORTS/SDL2_giflib_sa-master/android32/Debug/jni/Application.mk
@@ -0,0 +1,7 @@
+APP_MODULES      := SDL2_gif
+APP_PLATFORM     := android-15
+APP_ABI          := armeabi-v7a
+APP_STL          := gnustl_static
+APP_OPTIM        := debug
+
+APP_CPPFLAGS 	:= -fexceptions -frtti
