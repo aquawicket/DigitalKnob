70d8bae2657bab3571bb332c8ea80e19ff0face0
dkuser <dkuser@digitalknob.com>
2020-06-07 16:04:35 -0500

    commit from git

--- DKApps/DKSDLRmlUi/assets/app.js
+++ DKApps/DKSDLRmlUi/assets/app.js
@@ -1,6 +1,6 @@
 DKCreate("DKWindow");
 DKCreate("DKRml");
 
-location.href = DKAssets_LocalAssets()+"DKWebTest/index.html";
+location.href = DKAssets_LocalAssets()+"DKWebTest/rem2.html";
 DKRml_DebuggerOn();
 //DKRml_DebuggerToggle(); //FIXME
