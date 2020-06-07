abec144be8ab8d0ee595dd3116b589997471cef0
dkuser <dkuser@digitalknob.com>
2020-06-07 16:12:21 -0500

    commit from git

--- DKApps/DKTestAll/assets/app.js
+++ DKApps/DKTestAll/assets/app.js
@@ -1,14 +1,11 @@
 //var url = DKAssets_LocalAssets()+"index.html";
-//var url = DKAssets_LocalAssets()+"DKWebTest/index.html";
-var url = "C:/rem.html";
+var url = DKAssets_LocalAssets()+"DKWebTest/index.html";
 
-/*
 //// Create Cef window ////
 var USE_CEF = 1;
 DKCreate("DKCef,Cef,0,0,800,600,"+url);
 DKCef_NewBrowser("Cef",5,5,screen.width/2-10,screen.height-45,url);
 //DKCef_ShowDevTools(0);
-*/
 
 //// Create SDL Rocket window ////
 DKCreate("DKWindow");
@@ -16,4 +13,4 @@ DKCreate("DKRml");
 location.href = url;
 window.moveTo(screen.width/2, 36);
 window.resizeTo(screen.width / 2 - 10, screen.height-84);
-DKRml_DebuggerOn();
+//DKRml_DebuggerOn();
