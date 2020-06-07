f942b907ab08ce8fee1569650162cf63de195a56
dkuser <dkuser@digitalknob.com>
2020-06-07 18:07:39 -0500

    commit from git

---
+++ DKApps/DKTestAll/assets/myapp.js
@@ -0,0 +1,5 @@
+// browser app script, called from index.html
+
+DKCreate("DK/DK.css", function(){});
+DKCreate("DKWidget/DKWidget.js", function(){});
+DKCreate("DKColorPicker/DKColorPicker.js", function(){});
