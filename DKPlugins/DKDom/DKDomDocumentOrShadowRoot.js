c857fde35998490df2039b07685d278ae3100f63
dkuser <dkuser@digitalknob.com>
2020-06-10 23:09:14 -0500

    commit from git

--- DKPlugins/DKDom/DKDomDocumentOrShadowRoot.js
+++ DKPlugins/DKDom/DKDomDocumentOrShadowRoot.js
@@ -1,46 +1,38 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/DocumentOrShadowRoot
 // https://javascript.info/mixins
 
-/*
-////////////////////////////////////////////
-var DocumentOrShadowRoot = function(pointer)
-{
-	// Properties
-	Object.defineProperty(this, "activeElement", { 
-		get: function(){ 
-			var elementAddress = DKDomDocumentOrShadowRoot_activeElement();
-			if(!elementAddress){ return; }
-			var element = new HTMLElement(elementAddress);
-			return element;
-		},
-	});
-		
-	// Methods
-	DocumentOrShadowRoot.prototype.caretPositionFromPoint = function(){
-		var caretPosition = DKDomDocumentOrShadowRoot_caretPositionFromPoint();
-		if(!CaretPosition ){ return; }
-		//TODO
-	}
-	DocumentOrShadowRoot.prototype.elementFromPoint = function(){
-		var elementAddress = DKDomDocumentOrShadowRoot_elementFromPoint();
-		if(!elementAddress ){ return; }
-		var element = new HTMLElement(elementAddress);
-		return element;
-	}
-}
-*/
-
 var DocumentOrShadowRoot = {
 	
 	// Properties
-	//activeElement = { console.log("activeElement"); } //FIXME
+	//activeElement =      { console.log("activeElement");      } //FIXME
+	//fullscreenElement =  { console.log("fullscreenElement");  } //FIXME
+	//pointerLockElement = { console.log("pointerLockElement"); } //FIXME
+	//styleSheets =        { console.log("styleSheets");        } //FIXME
 	
 	// Methods
+	caretPositionFromPoint(){ /* TODO */ }
+	
 	elementFromPoint(){
-		console.log("elementFromPoint");
 		var elementAddress = DKDomDocumentOrShadowRoot_elementFromPoint();
 		if(!elementAddress ){ return; }
 		var element = new HTMLElement(elementAddress);
 		return element;
+	}
+	
+	elementsFromPoint(){ 
+		//TODO
+		DKDomDocumentOrShadowRoot_elementsFromPoint();
+	}
+	getSelection(){
+		//TODO
+		DKDomDocumentOrShadowRoot_getSelection();
+	}
+	nodeFromPoint(){
+		//TODO
+		DKDomDocumentOrShadowRoot_nodeFromPoint();
+	}
+	nodesFromPoint(){
+		//TODO
+		DKDomDocumentOrShadowRoot_nodesFromPoint();
 	}
 };
