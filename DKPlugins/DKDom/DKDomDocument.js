809f01c59ecf8c2c4de36fb158868050a572d397
dkuser <dkuser@digitalknob.com>
2020-06-24 23:24:50 -0500

    commit from git

--- DKPlugins/DKDom/DKDomDocument.js
+++ DKPlugins/DKDom/DKDomDocument.js
@@ -32,7 +32,7 @@ var Document = function(pointer)
 	Document.prototype.createElement = function(tag){
 		var pointer = DKDomDocument_createElement(tag);
 		if(!pointer){ return; }
-		var element = new HTMLElement(pointer);
+		var element = HTMLElement(pointer);
 		return element;
 	}
 	Document.prototype.getElementById = function(id){
