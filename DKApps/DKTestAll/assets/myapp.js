862b06d0abb319ed437537cabf179dcc93c0a6ca
dkuser <dkuser@digitalknob.com>
2020-07-12 23:22:20 -0500

    commit from git

--- DKApps/DKTestAll/assets/myapp.js
+++ DKApps/DKTestAll/assets/myapp.js
@@ -1,11 +1,12 @@
-// browser app script, called from index.html
-
 DKCreate("DK/DK.css", function(){
 DKCreate("DKFile/DKFile.js", function(){
 DKCreate("DKWidget/DKWidget.js", function(){
 DKCreate("DKGui/DKFrame.js", function(){
 DKCreate("DKColorPicker/DKColorPicker.js", function(){
 	DKFrame_Widget("DKColorPicker/DKColorPicker.html");
+DKCreate("DKNotepad/DKNotepad.js", function(){
+	DKFrame_Widget("DKNotepad/DKNotepad.html");
+});
 });
 });
 });
