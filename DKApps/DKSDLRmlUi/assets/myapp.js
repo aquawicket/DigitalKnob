36f9004cf0038d13e3f8eccd80ac3ed584090511
dkuser <dkuser@digitalknob.com>
2020-07-15 20:08:16 -0500

    commit from git

--- DKApps/DKSDLRmlUi/assets/myapp.js
+++ DKApps/DKSDLRmlUi/assets/myapp.js
@@ -1,4 +1,3 @@
-/*
 DKCreate("DK/DK.css", function(){
 DKCreate("DKFile/DKFile.js", function(){
 DKCreate("DKWidget/DKWidget.js", function(){
@@ -10,18 +9,3 @@ DKCreate("DKColorPicker/DKColorPicker.js", function(){
 });
 });
 });
-*/
-
-console.log("\n");
-console.log("document = "+document);
-console.log("document.documentElement = "+document.documentElement);
-//console.log("document.documentElement.style = "+document.documentElement.style);
-
-document.documentElement.style.color = "blue";
-
-var div1 = document.createElement("div");
-div1.innerHTML = "Test";
-div1.style.color = "black";
-console.log("div1 = "+div1);
-
-console.log("\n");
