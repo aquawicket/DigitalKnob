7faf9b53bfede828b962cdf40bae23a6e70f69bd
dkuser <dkuser@digitalknob.com>
2020-07-22 11:51:14 -0500

    commit from git

--- DKPlugins/DKOS/DesktopMenu.js
+++ DKPlugins/DKOS/DesktopMenu.js
@@ -2,10 +2,13 @@
 function DesktopMenu_Init()
 {
 	DKDEBUGFUNC();
-	DKWidget_SetProperty("DKOS/DesktopMenu.html","top",DKWindow_GetMouseY()+"px");
-	DKWidget_SetProperty("DKOS/DesktopMenu.html","left",DKWindow_GetMouseX()+"px");
+	//DKWidget_SetProperty("DKOS/DesktopMenu.html","top",DKWindow_GetMouseY()+"px");
+	//DKWidget_SetProperty("DKOS/DesktopMenu.html","left",DKWindow_GetMouseX()+"px");
+	DKCreate("DKOS/DesktopMenu.html");
+	DKWidget_SetProperty("DKOS/DesktopMenu.html","top","100px");
+	DKWidget_SetProperty("DKOS/DesktopMenu.html","left","100px");
 	
-	DKAddEvent("window", "mousedown", DesktopMenu_OnEvent);
+	DKAddEvent("document", "mousedown", DesktopMenu_OnEvent);
 	DKAddEvent("OpenBackgtoundMenu", "click", DesktopMenu_OnEvent);
 	DKAddEvent("ToggleFullscreen", "click", DesktopMenu_OnEvent);
 }
@@ -31,12 +34,13 @@ function DesktopMenu_OnEvent(event)
 		Desktop_ToggleFullscreen();
 	}
 	
-	if(DK_Id(event, "window")){
+	if(DK_Id(event, "document")){
 		if(DKWidget_IsChildOf(DKWidget_GetHoverElement(), "DKOS/DesktopMenu.html")){
 			return;
 		}
 	}
 	DKClose("DKOS/DesktopMenu.js");
+	DKClose("DKOS/DesktopMenu.html");
 }
 
 ///////////////////////////////////
