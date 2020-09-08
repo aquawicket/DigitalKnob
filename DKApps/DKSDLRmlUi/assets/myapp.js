ffc2cbef025da04dd9aeeac30bdc0a956f0f3e68
dkuser <dkuser@digitalknob.com>
2020-09-08 14:17:51 -0500

    commit from git

--- DKApps/DKSDLRmlUi/assets/myapp.js
+++ DKApps/DKSDLRmlUi/assets/myapp.js
@@ -1,9 +1,11 @@
 DKCreate("DK/DK.css", function(){
 DKCreate("DKFile/DKFile.js", function(){
+DKCreate("DKAssets/DKAssets.js", function(){
 DKCreate("DKWidget/DKWidget.js", function(){
 DKCreate("DKGui/DKFrame.js", function(){
 DKCreate("DKGui/DKMenu.js", function(){
 DKCreate("DKOS/DKOS.js", function(){
+});
 });
 });
 });
