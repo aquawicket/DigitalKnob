1eef0b32393b3d5126633a722fed23c0d913302d
dkuser <dkuser@digitalknob.com>
2020-09-10 17:40:26 -0700

    commit from git

--- DKApps/DKSDLRmlUi/assets/myapp.js
+++ DKApps/DKSDLRmlUi/assets/myapp.js
@@ -5,6 +5,11 @@ DKCreate("DKWidget/DKWidget.js", function(){
 DKCreate("DKGui/DKFrame.js", function(){
 DKCreate("DKGui/DKMenu.js", function(){
 DKCreate("DKOS/DKOS.js", function(){
+	DKCreate("DKInputTest/DKInput.js", function(){
+		DKFrame_Widget("DKInputTest/DKInput.html");
+		var frame = document.getElementById("DKInputTest/DKInput.html").parentNode;
+		frame.style.left = "0px";
+	});
 });
 });
 });
