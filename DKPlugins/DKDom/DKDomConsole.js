64bd9a1b29f454553f98ed68ed53a1d1437a1c4e
dkuser <dkuser@digitalknob.com>
2020-09-12 00:47:31 -0700

    commit from git

--- DKPlugins/DKDom/DKDomConsole.js
+++ DKPlugins/DKDom/DKDomConsole.js
@@ -5,29 +5,29 @@ var Console = function(pointer)
 {
 	Console.prototype.assert = function(assertion, msg){
 		if(assertion){ return; }
-		DKDomConsole_error(msg+"\n");
+		DKDomConsole_error("[JS] "+msg+"\n");
 	}
 	Console.prototype.clear = function(){
 		DKDomConsole_clear();
 	}
 	Console.prototype.debug = function(msg){
-		DKDomConsole_debug(msg+"\n");
+		DKDomConsole_debug("[JS] "+msg+"\n");
 	}
 	Console.prototype.error = function(msg){
-		DKDomConsole_error(msg+"\n");
+		DKDomConsole_error("[JS] "+msg+"\n");
 	}
 	Console.prototype.exception = Console.prototype.error; //Deprecated
 	Console.prototype.info = function(msg){
-		DKDomConsole_info(msg+"\n");
+		DKDomConsole_info("[JS] "+msg+"\n");
 	}
 	Console.prototype.log = function(msg){
-		DKDomConsole_info(msg+"\n");
+		DKDomConsole_info("[JS] "+msg+"\n");
 	}
 	Console.prototype.trace = function(){
-		DKDomConsole_error("console.trace() not implemented\n");
+		DKDomConsole_error("[JS] console.trace() not implemented\n");
 	}
 	Console.prototype.warn = function(msg){
-		DKDomConsole_warn(msg+"\n");
+		DKDomConsole_warn("[JS] "+msg+"\n");
 	}
 }
 
