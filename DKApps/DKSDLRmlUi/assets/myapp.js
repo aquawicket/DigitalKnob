594421d3fa020ae61c14702836eb113aea14df50
dkuser <dkuser@digitalknob.com>
2020-09-12 03:24:22 -0700

    commit from git

--- DKApps/DKSDLRmlUi/assets/myapp.js
+++ DKApps/DKSDLRmlUi/assets/myapp.js
@@ -8,17 +8,7 @@ DKCreate("DKOS/DKOS.js", function(){
 	
 	window.resizeTo(window.screen.width/2, window.screen.height-60);
 	window.moveTo(window.screen.width/2, 30);
-	//Set window to right of screen
-	//window.innerWidth = window.screen.width / 2;
-	//window.innerHeight = window.screen.height - 20;
 	
-	/*
-	DKCreate("DKInputTest/DKInput.js", function(){
-		DKFrame_Widget("DKInputTest/DKInput.html");
-		var frame = byId("DKInputTest/DKInput.html").parentNode;
-		frame.style.left = "3px";
-	});
-	*/
 });
 });
 });
