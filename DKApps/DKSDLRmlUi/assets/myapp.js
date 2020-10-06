2fdf5bba4e452ecfebe4363b1f75a4124458b444
dkuser <dkuser@digitalknob.com>
2020-10-05 23:20:39 -0700

    commit from git

--- DKApps/DKSDLRmlUi/assets/myapp.js
+++ DKApps/DKSDLRmlUi/assets/myapp.js
@@ -1,24 +1,24 @@
-DKCreate("DK/DK.css", function(){
-DKCreate("DKFile/DKFile.js", function(){
-DKCreate("DKAssets/DKAssets.js", function(){
-DKCreate("DKWindow/DKWindow.js", function(){
-DKCreate("DKWidget/DKWidget.js", function(){
-DKCreate("DKGui/DKFrame.js", function(){
-DKCreate("DKGui/DKMenu.js", function(){
-DKCreate("DKOS/DKOS.js", function(){
-DKCreate("DKDebug/DKDebug.js", function(){
+DK_Create("DK/DK.css", function(){
+DK_Create("DKFile/DKFile.js", function(){
+DK_Create("DKAssets/DKAssets.js", function(){
+DK_Create("DKWindow/DKWindow.js", function(){
+DK_Create("DKWidget/DKWidget.js", function(){
+DK_Create("DKGui/DKFrame.js", function(){
+DK_Create("DKGui/DKMenu.js", function(){
+DK_Create("DKOS/DKOS.js", function(){
+DK_Create("DKDebug/DKDebug.js", function(){
 	
 	window.resizeTo(window.screen.width/2, window.screen.height-60);
 	window.moveTo(window.screen.width/2, 30);
 	//DKRml_DebuggerOn();
 	
-	DKCreate("DKNotepad/DKNotepad.js", function(){
+	DK_Create("DKNotepad/DKNotepad.js", function(){
 		DKFrame_Widget("DKNotepad/DKNotepad.html");
 		DKNotepad_Open("todo.txt");
 	});
 	
 	/*
-	DKCreate("DKInputTest/DKInput.js", function(){
+	DK_Create("DKInputTest/DKInput.js", function(){
 		DKFrame_Widget("DKInputTest/DKInput.html");
 	});
 	*/
