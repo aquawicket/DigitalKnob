0ad3d514443e59af3a895b07f011cd283f4e6de5
dkuser <dkuser@digitalknob.com>
2020-10-07 18:05:20 -0700

    commit from git

---
+++ DKPlugins/DKGui/DKMessageBox.js
@@ -0,0 +1,83 @@
+var DKMessageBox_callback;
+
+////////////////////////////
+function DKMessageBox_Init()
+{	
+	DK_Create("DKGui/DKMessageBox.html");
+	byId("DKMessageBoxOk").addEventListener("click", DKMessageBox_OnEvent);
+	byId("DKMessageBoxCancel").addEventListener("click", DKMessageBox_OnEvent);
+}
+
+///////////////////////////
+function DKMessageBox_End()
+{
+	byId("DKMessageBoxOk").removeEventListener("click", DKMessageBox_OnEvent);
+	byId("DKMessageBoxCancel").removeEventListener("click", DKMessageBox_OnEvent);
+	DK_Close("DKGui/DKMessageBox.html");
+}
+
+////////////////////////////////////
+function DKMessageBox_OnEvent(event)
+{	
+	if(event.currentTarget.id === "DKMessageBoxOk"){
+		if(DKMessageBox_callback){
+			if(byId("DKMessageBoxInput").style.visibilty){ //TODO: review this 
+				DKMessageBox_callback(DKWidget_GetValue("DKMessageBoxInput"));
+			}
+			else{
+				DKMessageBox_callback(true);
+			}
+		}
+		DKFrame_Close("DKMessageBox/DKMessageBox.html");
+		return;
+	}
+	
+	if(event.currentTarget.id === "DKMessageBoxCancel"){
+		if(DKMessageBox_callback){
+			DKMessageBox_callback(false);
+		}
+		DKFrame_Close("DKMessageBox/DKMessageBox.html");
+		return;
+	}
+}
+
+//////////////////////////////////////
+function DKMessageBox_Message(message)
+{
+	byId("DKMessageBoxText").innerHTML = message;
+	byId("DKMessageBoxInput").style.display = "none";
+	byId("DKMessageBoxCancel").style.display = "none";
+	byId("DKMessageBoxText").style.display = "block";
+	byId("DKMessageBoxText").style.visibility = "visible";
+	byId("DKMessageBox/DKMessageBox.html").style.display = "block";
+	byId("DKMessageBox/DKMessageBox.html").style.visibility = "visible";
+}
+
+////////////////////////////////////////////////
+function DKMessageBox_Confirm(message, callback)
+{
+	byId("DKMessageBoxText").innerHTML = message;
+	byId("DKMessageBoxInput").style.display = "none";
+	byId("DKMessageBoxText").style.display = "block";
+	byId("DKMessageBoxText").style.visibility = "visible";
+	byId("DKMessageBoxCancel").style.display = "block";
+	byId("DKMessageBoxCancel").style.visibility = "visible";
+	byId("DKMessageBox/DKMessageBox.html").style.display = "block";
+	byId("DKMessageBox/DKMessageBox.html").style.visibility = "visible";
+	DKMessageBox_callback = callback;
+}
+
+/////////////////////////////////////////////////
+function DKMessageBox_GetValue(message, callback)
+{
+	byId("DKMessageBoxText").innerHTML = message;
+	byId("DKMessageBoxText").style.display = "block";
+	byId("DKMessageBoxText").style.visibility = "visible";
+	byId("DKMessageBoxInput").style.display = "block";
+	byId("DKMessageBoxInput").style.visibility = "visible";
+	byId("DKMessageBoxCancel").style.display = "block";
+	byId("DKMessageBoxCancel").style.visibility = "visible";
+	byId("DKMessageBox/DKMessageBox.html").style.display = "block";
+	byId("DKMessageBox/DKMessageBox.html").style.visibility = "visible";
+	DKMessageBox_callback = callback;
+}
