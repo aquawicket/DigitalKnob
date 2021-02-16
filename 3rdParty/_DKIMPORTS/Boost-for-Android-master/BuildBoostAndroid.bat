2f0b7f4b3ef096d3eb8b0a9c2e1736223d9b98d7
dkuser <dkuser@digitalknob.com>
2021-02-16 03:26:07 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/Boost-for-Android-master/BuildBoostAndroid.bat
+++ 3rdParty/_DKIMPORTS/Boost-for-Android-master/BuildBoostAndroid.bat
@@ -8,15 +8,10 @@ set ANDROIDNDKROOT=C:\digitalknob\DK\3rdParty\android-ndk-r20b
 set NDKVER=r20b
 set CLANGPATH=%ANDROIDNDKROOT%\toolchains\llvm\prebuilt\windows-x86_64\bin
 
-echo Patching boost_1_74_0 for android . . 
-Rem : TODO 
-
 echo Copying user-config.jam to boost_1_74_0/tools/build/src . . .
 robocopy "C:\digitalknob\DK\3rdParty\Boost-for-Android-master" "C:\digitalknob\DK\3rdParty\Boost-for-Android-master\boost_1_74_0\tools\build\src" user-config.jam /IS /IT /IM /NDL /NJH /NJS /nc /ns
 echo.
 cd C:\digitalknob\DK\3rdParty\Boost-for-Android-master\boost_1_74_0
-echo Bootstraping Boost Build (b2) . . .
-bootstrap.bat
 echo **** Running b2 architecture=arm --ignore-site-config -j4 target-os=android toolset=clang-armeabiv7a link=static threading=multi --layout=tagged --build-dir=../build/arm/r20b --stagedir=../stage_arm_r20b stage
 echo.
 
--- 3rdParty/_DKIMPORTS/Boost-for-Android-master/build-android.sh
+++ 3rdParty/_DKIMPORTS/Boost-for-Android-master/build-android.sh
@@ -6,56 +6,15 @@ SCRIPTDIR="$(cd "$(dirname "$0")"; pwd)"
 BOOST_VER1=1
 BOOST_VER2=74
 BOOST_VER3=0
-register_option "--boost=<version>" boost_version "Boost version to be used, one of {1.74.0, 1.73.0, 1.71.0, 1.70.0, 1.69.0, 1.68.0, 1.67.0, 1.66.0, 1.65.1, 1.55.0, 1.54.0, 1.53.0, 1.49.0, 1.48.0, 1.45.0}, default is 1.74.0."
-boost_version()
-{
-  if [ "$1" = "1.74.0" ]; then
-    BOOST_VER1=1
-    BOOST_VER2=74
-    BOOST_VER3=0
-  elif [ "$1" = "1.73.0" ]; then
-    BOOST_VER1=1
-    BOOST_VER2=73
-    BOOST_VER3=0
-  else
-    echo "Unsupported boost version '$1'."
-    exit 1
-  fi
-}
-
 BOOST_TAR="boost_${BOOST_VER1}_${BOOST_VER2}_${BOOST_VER3}.tar.bz2"
 BOOST_DIR="boost_${BOOST_VER1}_${BOOST_VER2}_${BOOST_VER3}"
 
-# Check platform patch
-case "$HOST_OS" in
-    linux)
-        PlatformOS=linux
-        ;;
-    darwin|freebsd)
-        PlatformOS=darwin
-        ;;
-    windows|cygwin)
-        PlatformOS=windows
-        ;;
-    *)  # let's play safe here
-        PlatformOS=linux
-esac
-
-# -----------------------
-# Download required files
-# -----------------------
-
-# Downalod and unzip boost in a temporal folder and
-if [ ! -f $BOOST_TAR ]
-then
-	echo "Downloading boost ${BOOST_VER1}.${BOOST_VER2}.${BOOST_VER3} please wait..."
-	prepare_download
-	download_file $BOOST_DOWNLOAD_LINK $PROGDIR/$BOOST_TAR
-fi
-
+#------------------------
+# Unpack Boost\
+#------------------------
 if [ ! -f $PROGDIR/$BOOST_TAR ]
 then
-	echo "Failed to download boost! Please download boost ${BOOST_VER1}.${BOOST_VER2}.${BOOST_VER3} manually\nand save it in this directory as $BOOST_TAR"
+	echo "Please download boost ${BOOST_VER1}.${BOOST_VER2}.${BOOST_VER3} manually\nand save it in this directory as $BOOST_TAR"
 	exit 1
 fi
 
@@ -69,18 +28,31 @@ then
 	fi
 fi
 
-if [ $DOWNLOAD = yes ] ; then
-	echo "All required files has been downloaded and unpacked!"
-	exit 0
-fi
-
 # ---------
 # Bootstrap
 # ---------
 if [ ! -f ./$BOOST_DIR/b2 ]
 then
+  # Make the initial bootstrap
+  echo "Performing boost bootstrap"
+
+  cd $BOOST_DIR
+  case "$HOST_OS" in
+    windows)
+        cmd //c "bootstrap.bat" 2>&1 | tee -a $PROGDIR/build.log
+        ;;
+    *)  # Linux and others
+        ./bootstrap.sh 2>&1 | tee -a $PROGDIR/build.log
+    esac
+
+  if [ $? != 0 ] ; then
+  	dump "ERROR: Could not perform boostrap! See $TMPLOG for more info."
+  	exit 1
+  fi
+  cd $PROGDIR
+  
   # -------------------------------------------------------------
-  # Patching shpould be done only if we had a successfull bootstrap!
+  # Patching should be done only if we had a successfull bootstrap!
   # -------------------------------------------------------------
 
   # Apply patches to boost
