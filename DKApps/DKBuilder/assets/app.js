a7d45b3019d4360fd08e571ad539cd61574f8ad4
dkuser <dkuser@digitalknob.com>
2021-03-07 09:25:49 -0800

    commit from git

--- DKApps/DKBuilder/assets/app.js
+++ DKApps/DKBuilder/assets/app.js
@@ -1,22 +1,2 @@
-// executable app startup script
-
-var USE_CEF     = 0; //Use Cef browser
-var USE_ROCKET  = 1; //Use Rocket browser
-var USE_SDL     = 0; //Use SDL window
-var USE_WEBVIEW = 0; //TODO: Android, iOS
-
-var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"index.html";
-//var DKApp_url = "http://google.com";
-//var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"index.html?plugin=DKNotepad/DKNotepad";
-
-DK_Create("DK/init.js", function(){}); //load DKApp_url using flags above, then call app_LoadPlugins()
-
-//////////////////////////
-function app_LoadPlugins()
-{
-	if(USE_SDL){
-		DK_Create("DKSDLText"); //Class contains SDL fps counter
-	}
-	DK_Create("DKTray/DKTray.js", function(){});
-	DK_Create("DKDebug/DKDebug.js", function(){});
-}
+CPP_DKDuktape_Create("DKBuild/DKBuildConsole.js");
+//CPP_DKDuktape_Create("DKUpdate");
