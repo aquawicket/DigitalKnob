338acb5358d5612c26b73e612b401570662107ef
dkuser <dkuser@digitalknob.com>
2021-03-06 19:21:43 -0800

    commit from git

--- DKCMake/Build.bat
+++ DKCMake/Build.bat
@@ -3,6 +3,7 @@ cd ../DKApps/DKBuilder
 mkdir win32
 cd win32
 cmake -G "Visual Studio 16 2019" -A Win32 -DDEBUG=ON -DREBUILD=ON -DSTATIC=ON C:/digitalknob/DK
-"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe C:/digitalknob/DK/DKApps/DKBuilder/win32/DKBuilder.sln /p:Configuration=Debug"
+cd C:/digitalknob/DK/DKApps/DKBuilder/win32
+"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" DKBuilder.sln /p:Configuration=Debug
 
 set /p DUMMY=Hit ENTER to continue...
