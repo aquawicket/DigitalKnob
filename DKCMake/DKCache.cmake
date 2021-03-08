bb905451dd185555c953bbc67ad4170c5c95b139
dkuser <dkuser@digitalknob.com>
2021-03-08 07:06:31 -0800

    commit from git

--- DKCMake/DKCache.cmake
+++ DKCMake/DKCache.cmake
@@ -1,7 +1,36 @@
 MESSAGE("################ DKCache.cmake ##################")
-##We need to find a good way to pull in the variables for all the library paths
 
-SET(FREETYPE C:/digitalknob/DK/3rdParty/freetype-2.5.5)
+cmake_policy(SET CMP0074 NEW)
+
+
+SET(OS win32)
+SET(3RDPARTY C:/digitalknob/DK/3rdParty)
+SET(ZLIB ${3RDPARTY}/zlib-1.2.11)
+SET(BZIP2 ${3RDPARTY}/bzip2-1.0.6)
+SET(FREETYPE ${3RDPARTY}/freetype-2.5.5)
+project(HelloWorld C CXX)
+
+## ZLIB
+SET(ZLIB_ROOT ${ZLIB})
+#INCLUDE_DIRECTORIES(${ZLIB})
+#INCLUDE_DIRECTORIES(${ZLIB}/${OS})
+SET(ZLIB_INCLUDE_DIR ${ZLIB} CACHE PATH "")
+SET(ZLIB_LIBRARY ${ZLIB}/${OS}/Release/zlibstatic.lib CACHE FILEPATH "")
+list(APPEND CMAKE_PREFIX_PATH ${ZLIB})
+find_package(ZLIB REQUIRED)
+
+
+##EXECUTE_PROCESS(COMMAND cmd /c set /p DUMMY=Hit ENTER to continue... WORKING_DIRECTORY C:/)
+
+## BZIP2
+SET(BZip2_ROOT ${BZIP2})
+SET(BZIP2_INCLUDE_DIR ${BZIP2} CACHE PATH "")
+SET(BZIP2_LIBRARY_DEBUG ${BZIP2}/${OS}/libbz2.lib FILEPATH "")
+SET(BZIP2_LIBRARY_RELEASE ${BZIP2}/${OS}/libbz2.lib FILEPATH "")
+SET(LIBGCC_LIBRARY ${BZIP2}/${OS}/libgcc.lib FILEPATH "")
+list(APPEND CMAKE_PREFIX_PATH ${BZIP2})
+find_package(BZip2 REQUIRED)
+
 IF(EXISTS ${FREETYPE}/win32/Release/freetype.lib)
 SET(FREETYPE_INCLUDE_DIRS ${FREETYPE}/include CACHE PATH "")
 SET(FREETYPE_INCLUDE_DIR_freetype2 ${FREETYPE}/include CACHE PATH "")
