a0557e1e0c3e7a2f9a6294e86fe5284663cfef46
dkuser <dkuser@digitalknob.com>
2021-04-07 07:35:02 -0700

    commit from git

--- DKPlugins/DKAudio/DKAudio.js
+++ DKPlugins/DKAudio/DKAudio.js
@@ -1,15 +1,14 @@
 "use strict";
 //https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement
 
+
 let DKAudio_lastVolume = 0.0;
 
-///////////////////////
 function DKAudio_Init()
 {
 	DK_Create("DKAudio");
 }
 
-//////////////////////
 function DKAudio_End()
 {
 	
@@ -30,14 +29,12 @@ function DKAudio_CreateSound(src) {
     document.body.appendChild(audio);
 }
 
-/////////////////////////////
 function DKAudio_Resume(src)
 { 
 	const audio = byId(src);
     audio.play(); 
 }
 
-////////////////////////////
 function DKAudio_Pause(src)
 { 
 	const audio = byId(src);
@@ -59,7 +56,6 @@ function DKAudio_Stop(src) {
     audio.stop();
 }
 
-///////////////////////////////
 function DKAudio_OpenMusic(src)
 { 
 	const audio = DK_CreateElement(document.body, "audio", "audio");
@@ -80,44 +76,38 @@ function DKAudio_SetVolume(src, volume) {
     audio.volume = volume;
 }
 
-///////////////////////
 function DKAudio_Mute(src)
 {
 	DKAudio_lastVolume = DKAudio_GetVolume(src);
 	DKAudio_SetVolume(src, 0);
 }
 
-/////////////////////////
 function DKAudio_UnMute(src)
 {
 	DKAudio_SetVolume(src, DKAudio_lastVolume);
 }
 
-/////////////////////////////////
 function DKAudio_SetTime(src, seconds)
 {
 	byId(src).currentTime = seconds;
 }
 
-/////////////////////////////
 function DKAudio_TimeUpdate()
 {
 	DKSendEvent("window", "timeupdate");
 }
 
-////////////////////////
 function DKAudio_Ended()
 {
 	DKSendEvent("window", "ended");
 }
 
-//////////////////////////
 function DKAudio_GetTime(src)
 {
 	return byId(src).currentTime;
 }
 
-//////////////////////////////
+
 function DKAudio_GetDuration(src)
 {
 	return byId(src).duration;
