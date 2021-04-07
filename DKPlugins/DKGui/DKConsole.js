89b2e3cf200bf28d986523e5df1f8c9eca7b2b0f
dkuser <dkuser@digitalknob.com>
2021-04-07 04:28:11 -0700

    commit from git

---
+++ DKPlugins/DKGui/DKConsole.js
@@ -0,0 +1,242 @@
+"use strict";
+
+// https://developer.mozilla.org/en-US/docs/Web/API/console
+const consoleLimit = 100;
+let dkconsole = new Object;
+dkconsole.log = console.log;
+dkconsole.info = console.info;
+dkconsole.debug = console.debug;
+dkconsole.warn = console.warn;
+dkconsole.error = console.error;
+dkconsole.trace = console.trace;
+dkconsole.assert = console.assert;
+dkconsole.group = console.group;
+
+//intercept console with dkconsole
+//Example: If you use dkconsole.log, it will only log to the dk console.
+// If you use console.log, it will go to the dkconsole frame AND the browser console.
+// If you use console_log, it will only log the browser console.
+// Note: some messages cannot be withheld from the browser console.
+
+const console_log = console.log;
+const console_info = console.info;
+const console_debug = console.debug;
+const console_warn = console.warn;
+const console_error = console.error;
+const console_trace = console.trace;
+const console_assert = console.assert;
+const console_group = console.group;
+(function() {
+
+    console.log = function() {
+        dkconsole.log.apply(this, Array.prototype.slice.call(arguments));
+        console_log.apply(this, Array.prototype.slice.call(DKConsole_SpanFilter(arguments)));
+    }
+    console.info = function() {
+        dkconsole.info.apply(this, Array.prototype.slice.call(arguments));
+        console_info.apply(this, Array.prototype.slice.call(arguments));
+    }
+    console.debug = function() {
+        dkconsole.debug.apply(this, Array.prototype.slice.call(arguments));
+        console_debug.apply(this, Array.prototype.slice.call(arguments));
+    }
+    console.warn = function() {
+        dkconsole.warn.apply(this, Array.prototype.slice.call(arguments));
+        console_warn.apply(this, Array.prototype.slice.call(arguments));
+    }
+    console.error = function() {
+        dkconsole.error.apply(this, Array.prototype.slice.call(arguments));
+        console_error.apply(this, Array.prototype.slice.call(arguments));
+    }
+    console.trace = function() {
+        dkconsole.trace.apply(this, Array.prototype.slice.call(arguments));
+        console_trace.apply(this, Array.prototype.slice.call(arguments));
+    }
+    console.assert = function() {
+        dkconsole.assert.apply(this, Array.prototype.slice.call(arguments));
+        console_assert.apply(this, Array.prototype.slice.call(arguments));
+    }
+    console.group = function() {
+        dkconsole.group.apply(this, Array.prototype.slice.call(arguments));
+        console_group.apply(this, Array.prototype.slice.call(arguments));
+    }
+}());
+
+function DKConsole_SpanFilter(args) {
+    let argArray = [];
+    if (args.length) {
+        const startTagRe = /<span\s+style=(['"])([^'"]*)\1\s*>/gi;
+        const endTagRe = /<\/span>/gi;
+        let reResultArray;
+        argArray.push(args[0].replace(startTagRe, '%c').replace(endTagRe, '%c'));
+        while (reResultArray = startTagRe.exec(args[0])) {
+            argArray.push(reResultArray[2]);
+            argArray.push('');
+        }
+        // pass through subsequent args since chrome dev tools does not (yet) support console.log styling of the following form: console.log('%cBlue!', 'color: blue;', '%cRed!', 'color: red;');
+        for (let n = 1; n < args.length; n++) {
+            argArray.push(args[n]);
+        }
+    }
+    return argArray;
+}
+
+function DKConsole_Create(parent, id, top, bottom, left, right, width, height) {
+    DKLoadCSSFile("DKConsole.css");
+    const dkconsoleFrame = document.createElement("div");
+    dkconsoleFrame.style.padding = "0px";
+    dkconsoleFrame.style.margin = "0px";
+    dkconsoleFrame.id = id;
+    dkconsoleFrame.style.position = "absolute";
+    dkconsoleFrame.style.top = top;
+    dkconsoleFrame.style.bottom = bottom;
+    dkconsoleFrame.style.left = left;
+    dkconsoleFrame.style.right = right;
+    dkconsoleFrame.style.width = width;
+    dkconsoleFrame.style.height = height;
+    dkconsoleFrame.style.borderColor = "rgb(58,58,58)";
+    dkconsoleFrame.style.borderStyle = "solid";
+    dkconsoleFrame.style.borderWidth = "1px";
+    dkconsoleFrame.style.boxSizing = "border-box";
+    dkconsoleFrame.style.borderTopWidth = "5px";
+    parent.appendChild(dkconsoleFrame);
+
+    dkconsole = document.createElement("div");
+    dkconsole.className = "dkconsole";
+    dkconsole.style.position = "absolute";
+    dkconsole.style.padding = "0px";
+    dkconsole.style.backgroundColor = "rgb(36,36,36)";
+    dkconsole.style.color = "white";
+    dkconsole.style.fontColor = "white";
+    dkconsole.style.top = "0px";
+    dkconsole.style.bottom = "20px";
+    dkconsole.style.left = "0px";
+    dkconsole.style.right = "0px";
+    //dkconsole.style.width = "";
+    //dkconsole.style.height = "";
+    dkconsole.style.visibility = "visible";
+    dkconsole.style.overflow = "auto";
+    dkconsoleFrame.appendChild(dkconsole);
+
+    //command box
+    const cmdbox = document.createElement("input");
+    cmdbox.type = "text";
+    cmdbox.style.position = "absolute";
+    cmdbox.style.left = "0px";
+    cmdbox.style.bottom = "0px";
+    cmdbox.style.right = "0px";
+    cmdbox.style.height = "20px";
+    cmdbox.style.width = "100%";
+    cmdbox.style.backgroundColor = "rgb(150,150,150)";
+    dkconsoleFrame.style.borderColor = "rgb(40,40,40)";
+    cmdbox.onkeydown = function cmdboxOnKeydownCallback(event) {
+        const key = event.charCode || event.keyCode;
+        if (key === 13) {
+            //enter
+            if (cmdbox.value === "clear" || cmdbox.value === "cls") {
+                dkconsole.innerHTML = "";
+                cmdbox.value = "";
+                return;
+            }
+            dkconsole.debug("RUN Javascript -> " + cmdbox.value);
+
+            try {
+                eval(cmdbox.value);
+            } catch (x) {
+                dkconsole.error("eval failed", x.stack);
+            }
+
+            cmdbox.value = "";
+        }
+    }
+    dkconsoleFrame.appendChild(cmdbox);
+
+    dkconsole.message = function(msg, style) {
+        const msgDiv = document.createElement("div");
+        msgDiv.style.width = "100%";
+        msgDiv.style.fontSize = "12px";
+        msgDiv.style.fontFamily = "Consolas, Lucinda, Console, Courier New, monospace";
+        msgDiv.style.whiteSpace = "pre-wrap";
+        msgDiv.style.boxSizing = "border-box";
+        msgDiv.style.padding = "2px";
+        msgDiv.style.paddingLeft = "20px";
+        msgDiv.style.borderStyle = "solid";
+        msgDiv.style.borderWidth = "1px";
+        msgDiv.style.borderTopWidth = "0px";
+        msgDiv.style.borderLeftWidth = "0px";
+        msgDiv.style.borderRightWidth = "0px";
+
+        const msgText = document.createElement("span");
+        msgText.className = "dkconsole";
+        //TODO: If the message is the same as the last, just have a count next to the original. 
+        msgText.innerHTML = msg;
+
+        if (style === "red") {
+            msgText.style.color = "rgb(255,128,128)";
+            msgDiv.style.backgroundColor = "rgb(41,0,0)";
+            msgDiv.style.borderColor = "rgb(92,0,0)";
+        } else if (style === "yellow") {
+            msgText.style.color = "rgb(255,221,158)";
+            msgDiv.style.backgroundColor = "rgb(51,43,0)";
+            msgDiv.style.borderColor = "rgb(102,85,0)";
+        } else if (style === "blue") {
+            msgText.style.color = "rgb(77,136,255)";
+            msgDiv.style.backgroundColor = "rgb(36,36,36)";
+            msgDiv.style.borderColor = "rgb(58,58,58)";
+        } else if (style === "green") {
+            msgText.style.color = "rgb(128,255,128)";
+            msgDiv.style.backgroundColor = "rgb(0,41,0)";
+            msgDiv.style.borderColor = "rgb(0,92,0)";
+        } else {
+            msgText.style.color = "rgb(213,213,213)";
+            msgDiv.style.backgroundColor = "rgb(36,36,36)";
+            msgDiv.style.borderColor = "rgb(58,58,58)";
+        }
+
+        dkconsole.appendChild(msgDiv);
+        msgDiv.appendChild(msgText);
+        dkconsole.scrollTop = dkconsole.scrollHeight;
+
+        //Limit the number of stored lines
+        if (dkconsole.childElementCount > consoleLimit) {
+            dkconsole.removeChild(dkconsole.firstChild);
+        }
+    }
+
+    dkconsole.log = function(str) {
+        dkconsole.message(str);
+    }
+
+    dkconsole.info = function(str) {
+        dkconsole.message(str);
+    }
+
+    dkconsole.debug = function(str) {
+        dkconsole.message(str, "blue");
+    }
+
+    dkconsole.warn = function(str) {
+        dkconsole.message(str, "yellow");
+    }
+
+    //https://developer.mozilla.org/en-US/docs/Web/API/Console/error
+    dkconsole.error = function(arg) {
+        if (!arg) {
+            dkconsole.warn("WARNING: dkconsole.error requires a valid argument");
+            return false;
+        }
+        const errMsg = DKError_StackToConsoleString(arg, "dkconsole.error");
+        dkconsole.message(errMsg, "red");
+    }
+
+    dkconsole.trace = function(str) {
+        const stackStr = StackToConsoleString(str, "dkconsole.trace");
+        dkconsole.message(stackStr);
+    }
+
+    dkconsole.group = function(str) {
+        dkconsole.message(str);
+    }
+
+    return dkconsole;
+}
---
+++ DKPlugins/DKGui/DKTable.js
@@ -0,0 +1,269 @@
+"use strict";
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLTableElement
+
+//The idea is to create a name for every row and every column.
+//Currently, we cannot create a column name as eazy as we create row names
+//We can name a row, but we have to name each cell in a column.
+//This idea: pull a cell, row or column, just from names.
+//If by chance a row or column moves, the naming scheme will still work.
+// GetIndex(row, column) works until cells are moved and point to different cells.
+// GetCellByName(rowName, CellName) should always return the correct cell 
+// no matter where it is in the index chart. 
+
+// Example:  DKGetCell("Peter", "Address");
+// This should return the cell that is on the Peter Row and the Address Column
+// If we add and remove rows or colums, this method should stay functional
+// We can do this without altering id's and indexs using rowName and ColumnNama attributes
+
+// (fName) | (age) | (address)   |  (email)
+//------------------------------------------------------
+//  David  |  28   | 123 Fun St  |  Dave@silly.com
+//------------------------------------------------------
+//  Mary   |  31   |  P.O. 567   |  Mar02@grape.com
+//------------------------------------------------------
+//  Peter  |  25   |  464 Go ln. |  PeterDotson@abc.com
+//------------------------------------------------------
+//  John   |  47   | 09 till pk. |  Johnboy1@mail.com
+
+// The rowName is set to Peter on the <tr> element
+// The columnName is set to address on the <td> element
+
+function DKTable_Create(parent, id, top, bottom, left, right, width, height) {
+    const table = document.createElement('table');
+    table.id = id;
+    table.style.position = "absolute";
+    table.style.top = top;
+    table.style.bottom = bottom;
+    table.style.left = left;
+    table.style.right = right;
+    table.style.width = width;
+    table.style.height = height;
+    table.style.overflow = "auto";
+    //table.setAttribute('border', '1');
+    parent.appendChild(table);
+    return table;
+}
+
+function DKTable_InsertRow(table, name) {
+    if (!name) {
+        dkConsole.error("invalid name parameter");
+    }
+    const row = table.insertRow(-1);
+    row.id = "row" + table.rows.length;
+    row.setAttribute("name", name);
+    return row;
+}
+
+function DKTable_InsertCell(table, row, name) {
+    if (!name) {
+        dkconsole.error("invalid name parameter");
+    }
+    const cell = row.insertCell(-1);
+    cell.id = String.fromCharCode(65 + (cell.cellIndex)) + (row.rowIndex + 1);
+    cell.setAttribute("name", name);
+    return cell;
+}
+
+function DKTable_AddRow(table, rowName, cellName) {
+    const row = DKTable_InsertRow(table, rowName);
+    row.id = "row" + table.rows.length;
+    //dkconsole.debug("DKTableAddRow() -> row.id = "+row.id);
+    const row_count = table.rows.length;
+
+    let cell_count = table.rows[0].cells.length;
+    if (!cell_count) {
+        cell_count = 1;
+    }
+    for (let n = 0; n < cell_count; n++) {
+        //Grab the name of the cell from the root column cell if it exists
+        /*
+        if(!table.rows[0]){
+        	console.error("DKTableAddRow(): table.rows[0] is invalid");
+        	return;
+        }
+        */
+        if (!cellName) {
+            cellName = table.rows[0].cells[n].getAttribute("name");
+        }
+        const cell = DKTable_InsertCell(table, row, cellName);
+        //FIXME: The function above is NOT setting the cellName properly.
+        //This line is a temporary fix for now. 
+        cell.setAttribute("name", table.rows[0].cells[n].getAttribute("name"));
+    }
+    return row;
+}
+
+function DKTable_AddColumn(table, name) {
+    let row_count = table.rows.length;
+    if (!row_count) {
+        //FIXME: no name attribute for the row
+        const row = DKTable_InsertRow(table /*, name*/
+        );
+        row_count = 1;
+    }
+    const cell_count = table.rows[0].cells.length;
+    for (let n = 0; n < row_count; n++) {
+        const cell = DKTable_InsertCell(table, table.rows[n], name);
+    }
+    //return the created column number
+    return table.rows[0].cells.length;
+}
+
+function DKTable_AddRows(table, count) {
+    //The rows added will have no name
+    for (let n = 0; n < count; n++) {
+        DKTable_AddRow(table);
+    }
+    return table.rows.length;
+}
+
+function DKTable_AddColumns(table, count) {
+    //The columns added will have no name
+    for (let n = 0; n < count; n++) {
+        DKTable_AddColumn(table);
+    }
+    return table.rows[0].cells.length;
+}
+
+function DKTable_DeleteRow(table, number) {
+    table.deleteRow(number);
+    DKTable_UpdateIds(table);
+}
+
+function DKTable_DeleteColumn(table, number) {
+    for (let n = 0; n < table.rows.length; n++) {
+        const row = table.rows[n];
+        if (row.cells[number]) {
+            row.deleteCell(number - 1);
+        }
+    }
+    DKTable_UpdateIds(table);
+}
+
+function DKTable_UpdateIds(table) {
+    for (let n = 0; n < table.rows.length; n++) {
+        let row = table.rows[n];
+        row.id = "row" + (n + 1);
+        for (let nn = 0; nn < row.cells.length; nn++) {
+            const cell = row.cells[nn];
+            cell.id = String.fromCharCode(65 + (cell.cellIndex)) + (row.rowIndex + 1);
+            //cell.innerHTML = cell.id; //For debug
+        }
+    }
+}
+
+function DKTable_GetCellByIndex(table, rowNum, columnNum) {
+    const row = table.rows[rowNum];
+    const cell = row.cells[columnNum];
+    return cell;
+}
+
+function DKTable_DeleteCell(table, rowNum, columnNum) {
+    dkConsole.log("DKTableDeleteCell(table," + rowNum + "," + columnNum + ")")
+    //FIXME: This doesn't seem to be working properly.
+    //I'm using Brave browser which is a fork of chromium.
+    //Bug? or user error? 
+    const row = table.rows[rowNum];
+    //row.deleteCell(columnNum);
+    document.removeElement(row.cells[columnNum]);
+}
+
+function DKTable_GetIndex(cell) {
+    return cell.cellIndex;
+}
+
+function DKTable_GetRowByName(table, rowName) {
+    return DKTable_GetCellByName(table, rowName);
+    /*
+    for (let n = 0; n < table.rows.length; n++) {
+        if (!table.rows[n].getAttribute("name")) {
+            dkConsole.warn("row" + n + " has no name attribute");
+            return;
+        }
+        if (table.rows[n].getAttribute("name") == rowName) {
+            const row = table.rows[n];
+            return row;
+        }
+    }
+    */
+}
+
+function DKTable_GetCellByName(table, rowName, columnName) {
+    //TODO: We would like to retrieve a cell by names
+    // Example:  DKGetCell("Peter", "Address");
+    // This should return the cell that is on the Peter Row and the Address Column
+    // If we add and remove rows or colums, this method should stay functional
+    // We can do this without altering id's and indexs using rowName and ColumnNama attributes
+
+    // (fName) | (age) | (address)   |  (email)
+    //------------------------------------------------------
+    //  David  |  28   | 123 Fun St  |  Dave@silly.com
+    //------------------------------------------------------
+    //  Mary   |  31   |  P.O. 567   |  Mar02@grape.com
+    //------------------------------------------------------
+    //  Peter  |  25   |  464 Go ln. |  PeterDotson@abc.com
+    //------------------------------------------------------
+    //  John   |  47   | 09 till pk. |  Johnboy1@mail.com
+
+    // The rowName is set to Peter on the <tr> element
+    // The columnName is set to address on the <td> element
+
+    for (let n = 0; n < table.rows.length; n++) {
+        if (!table.rows[n].getAttribute("name")) {
+            dkConsole.warn("row" + n + " has no name attribute");
+            return;
+        }
+        if (table.rows[n].getAttribute("name") == rowName) {
+            let row = table.rows[n];
+            //if the column name wasn't requested, return the row 
+            if (!columnName) {
+                return row;
+            }
+
+            for (let nn = 0; nn < row.cells.length; nn++) {
+                if (!row.cells[nn].getAttribute("name")) {
+                    dkConsole.warn("row" + n + ", cell" + nn + " has no name attribute");
+                    return;
+                }
+                if (row.cells[nn].getAttribute("name") == columnName) {
+                    return row.cells[nn];
+                    //we found the cell, return it's element
+                }
+            }
+        }
+    }
+}
+
+function DKTable_Sort(table_id, col, reverse) {
+    const table = byId(table_id);
+    const tb = table.tBodies[0];
+    // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
+    let tr = Array.prototype.slice.call(tb.rows, 1);
+    // put rows into array, 1 to skip the header
+    reverse = -((+reverse) || -1);
+    tr = tr.sort(function(a, b) {
+        // sort rows
+        return reverse // `-1 *` if want opposite order
+        * (a.cells[col].textContent.trim()// using `.textContent.trim()` for test
+        .localeCompare(b.cells[col].textContent.trim()));
+    });
+    for (let i = 1; i < tr.length; ++i)
+        tb.appendChild(tr[i]);
+    // append each row in order
+}
+
+/*
+//TODO
+function DKTable_SortRow(table_id, row, col, reverse) {
+    const table = byId(table_id);
+    const tb = table.tBodies[0]; // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
+    let tr = Array.prototype.slice.call(tb.rows, 1); // put rows into array, 1 to skip the header
+    reverse = -((+reverse) || -1);
+    tr = tr.sort(function (a, b) { // sort rows
+        return reverse * (a.cells[col].textContent.trim().localeCompare(b.cells[col].textContent.trim()));
+    });
+    //just append the single row
+    //return tb.insertBefore(row, referenceNode)
+}
+*/
