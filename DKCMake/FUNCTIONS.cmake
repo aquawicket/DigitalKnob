1c5e8668eaf10522b3c1c550ac86439ad80b11c2
dkuser <dkuser@digitalknob.com>
2021-04-16 00:11:35 -0700

    commit from git

--- DKCMake/FUNCTIONS.cmake
+++ DKCMake/FUNCTIONS.cmake
@@ -240,6 +240,20 @@ FUNCTION(DKINSTALL url folder_name)
 		FILE(COPY ${DKIMPORTS}/${folder_name}/ DESTINATION ${3RDPARTY}/${folder_name})
 ENDFUNCTION()
 
+######################################################
+FUNCTION(DKINSTALL2 url import_folder 3rdparty_folder)
+	IF(EXISTS ${3RDPARTY}/${3rdparty_folder})
+		###
+	ELSE()
+		DKSET(CURRENT_DIR ${DIGITALKNOB}/Download)
+		DKDOWNLOAD(${url})
+		GET_FILENAME_COMPONENT(filename ${url} NAME)
+		DKEXTRACT(${DIGITALKNOB}/Download/${filename} ${3RDPARTY})
+	ENDIF()
+		FILE(MAKE_DIRECTORY ${3RDPARTY}/${3rdparty_folder}/${OS})
+		FILE(COPY ${DKIMPORTS}/${import_folder}/ DESTINATION ${3RDPARTY}/${3rdparty_folder})
+ENDFUNCTION()
+
 
 
 
