d3908477d191545d587c1b9462b205c7c15c9bae
dkuser <dkuser@digitalknob.com>
2021-04-18 21:01:05 -0700

    commit from git

--- DKApps/DKBuilder/assets/app.js
+++ DKApps/DKBuilder/assets/app.js
@@ -3,4 +3,11 @@ CPP_DKDuktape_Create("DKTray");
 CPP_DKDuktape_Create("DKBuild/DKBuildConsole.js");
 //CPP_DKDuktape_Create("DKUpdate");
 
-//console.log("<start>"+CPP_DKDuktape_System('C:/digitalknob/DK/DKApps/DKBuilder/assets/getShortPath.cmd "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe"')+"<end>");
+/*
+var fullPath = "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe";
+var local_assets = CPP_DKAssets_LocalAssets();
+var getShortPath = local_assets+"/DKFile/getShortPath.cmd";
+var shortPath = CPP_DKDuktape_Execute(getShortPath+' "'+fullPath+'"');
+shortPath = shortPath.slice(0, -1)
+console.log("\n\n\nshorPath = <start>"+shortPath+"<end>");
+*/
