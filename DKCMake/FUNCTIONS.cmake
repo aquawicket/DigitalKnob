3f95090c8f3be1bf63d0fe07b2611839102fb6f0
dkuser <dkuser@digitalknob.com>
2021-04-21 14:27:27 -0700

    commit from git

--- DKCMake/FUNCTIONS.cmake
+++ DKCMake/FUNCTIONS.cmake
@@ -293,8 +293,8 @@ FUNCTION(DKINSTALL url import_folder 3rdparty_folder)
 					DKREMOVE(${3RDPARTY}/UNZIPPED)
 				ENDIF() 
 			ENDIF()
-		ELSE() #NOT ARCHIVE
-			FILE(COPY ${DIGITALKNOB}/Download/${filename} ${3RDPARTY}/${3rdparty_folder})
+		ELSE() #NOT ARCHIVE, just copy the file into it's 3rdParty folder
+			FILE(COPY ${DIGITALKNOB}/Download/${filename} ${3RDPARTY}/${3rdparty_folder}/${filename})
 		ENDIF()
 	ENDIF()
 		#FILE(MAKE_DIRECTORY ${3RDPARTY}/${3rdparty_folder}/${OS})
