741b78e529cf15ab3a6a2ad58c67d6618ba6091a
dkuser <dkuser@digitalknob.com>
2021-04-24 06:16:29 -0700

    commit from git

---
+++ DKPlugins/DKDevTools/DKDevTools.js
@@ -0,0 +1,12 @@
+"use strict";
+
+dk.devtools = new Object;
+
+dk.devtools.init = function dk_devtools_init() {
+
+}
+
+dk.devtools.create = function dk_devtools_create(parent, id, top, bottom, left, right, width, height) {
+	//TODO
+}
+
