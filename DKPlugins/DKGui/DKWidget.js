e7904c2ba872d7bcdcb804de34444735a80d2c83
dkuser <dkuser@digitalknob.com>
2021-04-24 04:45:14 -0700

    commit from git

--- DKPlugins/DKGui/DKWidget.js
+++ DKPlugins/DKGui/DKWidget.js
@@ -1,32 +1,35 @@
 "use strict";
 
-
 var instances = []
 const DKWidget = function() {
-    //find an available index number for the instance
-    this.instance = this;
-    for(var i=0; i<instances.length; i++){
-        if(instances[i].instance === this.instance){
-            return instances[i]; //return already existing instance
-        }
-    }
-    instances.push(this);
 
-    DKWidget.prototype.create = function(){
-        console.log("DKWidget.create() called");
+    DKWidget.prototype.storeInstance = function() {
+        this.instance = this;
+        for (var i = 0; i < instances.length; i++) {
+            if (instances[i].instance === this.instance) {
+                return instances[i];
+                console.log("this instance allready exists");
+            }
+        }
+        instances.push(this);
     }
-    DKWidget.prototype.getInstanceInfo = function(callerName){
-        console.log("DKWidget.getInstanceIndex() called");
-        for(var n=0; n<instances.length; n++){
-            if(instances[n].instance === this.instance){
-                console.log("(INSTANCE) name:"+callerName+" index:"+n+" total:"+instances.length);
+
+    DKWidget.prototype.create = function() {
+        //console.log("DKWidget.create() called");
+    }
+    DKWidget.prototype.getInstanceInfo = function(callerName) {
+        //console.log("DKWidget.getInstanceIndex() called");
+        for (var n = 0; n < instances.length; n++) {
+            if (instances[n].instance === this.instance) {
+                console.log("(INSTANCE) name:" + callerName + " index:" + n + " total:" + instances.length);
                 return n;
             }
         }
     }
-    DKWidget.prototype.close = function(){
-		console.log("DKWidget.close() called");
-	}
+    DKWidget.prototype.close = function() {
+        console.log("DKWidget.close() called");
+    }
 
-    
+    //STArTS HERE
+    this.storeInstance();
 }
