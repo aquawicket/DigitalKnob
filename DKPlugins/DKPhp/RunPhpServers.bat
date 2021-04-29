43bab3eeb57bd6fd6520223ef05643640579272e
dkuser <dkuser@digitalknob.com>
2021-04-29 14:11:47 -0700

    commit from git

--- DKPlugins/DKPhp/RunPhpServers.bat
+++ DKPlugins/DKPhp/RunPhpServers.bat
@@ -4,5 +4,5 @@ mkdir DKPhp_running
 copy DKPhp\php.exe DKPhp_running\php_local.exe
 copy DKPhp\php.exe DKPhp_running\php_web.exe
 copy DKPhp\php8ts.dll DKPhp_running\php8ts.dll
-TASKLIST | FINDSTR php_local.exe || START /min "" "DKPhp_running\php_local.exe" -S 127.0.0.1:8000
-TASKLIST | FINDSTR php_web.exe || START /min "" "DKPhp_running\php_web.exe" -S 192.168.1.78:8000
+TASKLIST | FINDSTR php_local.exe || START /min "" "DKPhp_running\php_local.exe" -S 127.0.0.1:2393
+TASKLIST | FINDSTR php_web.exe || START /min "" "DKPhp_running\php_web.exe" -S 192.168.1.210:2393
