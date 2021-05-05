41fab616db4ae8f43b921804a6fe74111584356b
dkuser <dkuser@digitalknob.com>
2021-05-05 00:01:35 -0700

    commit from git

--- DKPlugins/DKDuktape/DKJavascript.cpp
+++ DKPlugins/DKDuktape/DKJavascript.cpp
@@ -3,11 +3,9 @@
 #include "DKDuktape/DKDuktape.h"
 #include "DKDuktape/DKJavascript.h"
 
-/////////////////////////
-bool DKJavascript::Init()
-{
+bool DKJavascript::Init(){
 	DKDEBUGFUNC();
-	//data = ("DKJavascript", file)
+	//data = ["DKJavascript", "path/file.ext")
 	if(data.size() < 2){
 		DKString _data = toString(data, ",");
 		DKERROR("DKJavascript::Init("+_data+"): missing parameters. \n");
@@ -16,27 +14,15 @@ bool DKJavascript::Init()
 		DKString _data = toString(data, ",");
 		DKERROR("DKJavascript::Init("+_data+"): too many parameters. \n");
 	}
-
-	DKString file = DKFile::local_assets;
-	if(data[1].front() != '/'){
-		file += '/';
-	}
-	file+=data[1];
-
+	DKString file = DKFile::local_assets+data[1];
 	if(!DKDuktape::LoadFile(file)){ return false; }
 	DKDuktape::CallInit(file);
 	return true;
 }
 
-////////////////////////
-bool DKJavascript::End()
-{
+bool DKJavascript::End(){
 	DKDEBUGFUNC();
-	DKString file = DKFile::local_assets;
-	if(data[1].front() != '/'){
-		file += '/';
-	}
-	file += data[1];
+	DKString file = DKFile::local_assets + data[1];
 	DKDuktape::CallEnd(file);
 	return true;
 }
