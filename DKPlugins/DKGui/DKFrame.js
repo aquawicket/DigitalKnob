bc33db58680487f6b0a76ba52c0123119f1e94c8
dkuser <dkuser@digitalknob.com>
2021-05-15 21:42:16 -0700

    commit from git

--- DKPlugins/DKGui/DKFrame.js
+++ DKPlugins/DKGui/DKFrame.js
@@ -168,7 +168,6 @@ dk.frame.createFrame = function dk_frame_createFrame(title, width, height) {
 
     frame.titlebaricon = dk.gui.createElement(frame.titlebar, "img", "dk_frame_titlebaricon");
     frame.titlebaricon.src = "DKGui/window.png";
-    frame.titlebaricon.setAttribute("src", "DKGui/window.png");
     frame.titlebaricon.setAttribute("dk_frame", "titlebaricon");
 
     //See DKFrame.css for styling
