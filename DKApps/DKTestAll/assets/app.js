327ab04c65474c15a23c07880240f1938016d68e
dkuser <dkuser@digitalknob.com>
2021-05-18 00:38:19 -0700

    commit from git

--- DKApps/DKTestAll/assets/app.js
+++ DKApps/DKTestAll/assets/app.js
@@ -1,5 +1,11 @@
-var url = CPP_DKAssets_LocalAssets()+"/index.html";
-//var url = CPP_DKAssets_LocalAssets()+"/DKWebTest/index.html";
+CPP_DK_Create("DKTray");
+CPP_DK_Create("DKWindow");
+CPP_DK_Create("DKRml");
+CPP_DK_Create("DKSDLText");
+
+location.href = CPP_DKAssets_LocalAssets()+"index.html";
+//location.href = CPP_DKAssets_LocalAssets()+"DKWebTest/index.html";
+
 
 /*
 //// Create Cef window ////
@@ -8,11 +14,3 @@ CPP_DK_Create("DKCef,Cef,0,0,800,600,"+url);
 CPP_DKCef_NewBrowser("Cef",5,5,screen.width/2-10,screen.height-45,url);
 //DKCef_ShowDevTools(0);
 */
-
-//// Create SDL Rocket window ////
-CPP_DK_Create("DKWindow");
-CPP_DK_Create("DKRml");
-location.href = url;
-window.moveTo(screen.width/2, 36);
-window.resizeTo(screen.width / 2 - 10, screen.height-84);
-//CPP_DKRml_DebuggerOn();
