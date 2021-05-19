6df0fd367eeb8439d6e8fd59ae3e178eb17462f8
dkuser <dkuser@digitalknob.com>
2021-05-18 21:55:50 -0700

    commit from git

--- DKPlugins/_DKIMPORT/android32/Release/jni/Application.mk
+++ DKPlugins/_DKIMPORT/android32/Release/jni/Application.mk
@@ -4,7 +4,7 @@ APP_BUILD_SCRIPT := $(call my-dir)/Android.mk
 APP_OPTIM := release
 
 APP_PLATFORM 	:= android-19
-APP_STL 		:= gnustl_static
+APP_STL 		:= c++_static
 APP_CPPFLAGS 	:= -fexceptions -frtti
 APP_ABI 		:= armeabi-v7a
 APP_MODULES     := DKAndroid
