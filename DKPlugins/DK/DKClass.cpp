e3e180e4f52076e9f31a9dd9e95c3e4d06fd1df4
dkuser <dkuser@digitalknob.com>
2021-05-31 03:13:06 -0700

    commit from git

--- DKPlugins/DK/DKClass.cpp
+++ DKPlugins/DK/DKClass.cpp
@@ -125,7 +125,7 @@ void DKClass::CloseAll(){
 	std::map<DKString, DKClass*>::reverse_iterator rit;
 	for(rit = (*classes).rbegin(); rit != (*classes).rend(); rit++){
 		if((*classes)[rit->first]){
-			DKLOG("DKClass::CloseAll(): Closing " + rit->first + "\n");
+			//DKLOG("DKClass::CloseAll(): Closing " + rit->first + "\n");
 			(*classes)[rit->first]->Close("");
 		}
 	}
