d3e53cba98f61e095cea35e9805cf23dbec4c45a
dkuser <dkuser@digitalknob.com>
2021-06-19 01:03:12 -0700

    commit from git

--- DKCMake/FUNCTIONS.cmake
+++ DKCMake/FUNCTIONS.cmake
@@ -45,7 +45,6 @@ function(DKUNSET arg)
 	#Unset a variable, cache variable, or environment variable.
 	#Removes the specified variable causing it to become undefined. If CACHE is present then the variable is removed from the cache instead of the current scope.
 	#If PARENT_SCOPE is present then the variable is removed from the scope above the current scope. See the same option in the set() command for further details.
-	#<variable> can be an environment variable such as:
 endfunction()
 
 ##########################
