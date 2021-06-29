94c032ea24e48856c35950ff7cda6d041344db50
dkuser <dkuser@digitalknob.com>
2021-06-29 06:03:51 -0700

    commit from git

--- DKApps/DKBuilder/Build.sh
+++ DKApps/DKBuilder/Build.sh
@@ -1,3 +1,8 @@
-#! /bin/sh
-sudo apt-get install git
-sudo apt-get install cmake
+# first remove the CMake cache file
+rm home/aquawicket/digitalknob/DK/DKApps/DKBuilder/win32/CMakeCache.txt
+
+cmake -G "Unix Makefiles" -m32 -DDEBUG=ON -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON -S home/aquawicket/digitalknob/DK -B home/aquawicket/digitalknob/DK/DKApps/DKBuilder/linux32
+
+cd home/aquawicket/digitalknob/DK/DKApps/DKBuilder/linux32
+make DKBuilder
+
