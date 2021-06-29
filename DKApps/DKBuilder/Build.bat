f396e1cf2dccc34ff022a0b1df4b3e86e1d697ad
dkuser <dkuser@digitalknob.com>
2021-06-29 05:01:03 -0700

    commit from git

--- DKApps/DKBuilder/Build.bat
+++ DKApps/DKBuilder/Build.bat
@@ -1,6 +1,8 @@
-@echo off
-C:/PROGRA~2/CMake/bin/cmake.exe -G "Visual Studio 16 2019" -A Win32 -DDEBUG=ON -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON -S C:/Users/%USERNAME%/digitalknob/DK -B C:/Users/%USERNAME%/digitalknob/DK/DKApps/DKBuilder/win32
-cd C:/Users/%USERNAME%/digitalknob/DK/DKApps/DKBuilder/win32
-"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" DKBuilder.sln /p:Configuration=Debug
+:: first remove any CMake case file
+DEL C:\Users\%USERNAME%\digitalknob\DK\DKApps\DKBuilder\win32\CMakeCache.txt
 
-set /p DUMMY=Hit ENTER to continue...
+C:\PROGRA~2\CMake\bin\cmake.exe -G "Visual Studio 16 2019" -A Win32 -DDEBUG=ON -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON -S C:\Users\%USERNAME%\digitalknob\DK -B C:\Users\%USERNAME%\digitalknob\DK\DKApps\DKBuilder\win32
+cd C:\Users\%USERNAME%\digitalknob\DK\DKApps\DKBuilder\win32
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" DKBuilder.sln \p:Configuration=Debug
+
+set \p DUMMY=Hit ENTER to continue...
