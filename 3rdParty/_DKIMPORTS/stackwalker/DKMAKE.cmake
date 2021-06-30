48f2eae9edac6968bd434a16761fdc38f52062c3
dkuser <dkuser@digitalknob.com>
2021-06-29 17:37:49 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/stackwalker/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/stackwalker/DKMAKE.cmake
@@ -4,14 +4,16 @@ IF(NOT CMAKE_HOST_WIN32)
 	RETURN()
 ENDIF()
 
+### VERSION ###
+DKSET(STACKWALKER_VERSION 2014-12-28)
 DKSET(STACKWALKER ${3RDPARTY}/stackwalker)
 
 
 ### INSTALL ###
 IF(NOT EXISTS ${3RDPARTY}/stackwalker)
 	DKSETPATH(${DIGITALKNOB}/Download)
-	DKDOWNLOAD(https://github.com/ConnectedVision/connectedvision/raw/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0/stackwalker-2014-12-28.zip)
-	DKEXTRACT(${DIGITALKNOB}/Download/stackwalker-2014-12-28.zip ${3RDPARTY})
+	DKDOWNLOAD(https://github.com/ConnectedVision/connectedvision/raw/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0/stackwalker-${STACKWALKER_VERSION}.zip)
+	DKEXTRACT(${DIGITALKNOB}/Download/stackwalker-${STACKWALKER_VERSION}.zip ${3RDPARTY})
 	DKRENAME(${3RDPARTY}/Main ${3RDPARTY}/stackwalker)
 	DKCOPY(${DKIMPORTS}/stackwalker ${3RDPARTY}/stackwalker TRUE)
 ENDIF()
