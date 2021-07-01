eb9606cf3b43af1eccfbf1738b5e8af7463ee311
aquawicket <aquawicket@digitalknob.com>
2021-07-01 14:17:09 -0700

    commit from git

---
+++ DKDocs/Debug.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+APP = "DKBuilder"
+OS = "linux64"
+
+git clone https://github.com/aquawicket/DigitalKnob.git /home/"$USER"/digitalknob/DK
+cd /home/"$USER"/digitalknob/DK
+git checkout -- .
+git pull origin master
+
+mkdir /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/Release
+cd /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/Release
+rm /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/Release/CMakeCache.txt
+cmake -G "Unix Makefiles" -m32 -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON /home/"$USER"/digitalknob/DK
+
+cd /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/Release
+make $APP
