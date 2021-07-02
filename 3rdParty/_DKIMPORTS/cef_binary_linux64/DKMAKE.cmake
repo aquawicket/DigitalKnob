ff9f865089a9f9e1637d5d8a8313f5b4bed607e4
aquawicket <aquawicket@digitalknob.com>
2021-07-01 18:10:59 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/cef_binary_linux64/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/cef_binary_linux64/DKMAKE.cmake
@@ -3,33 +3,40 @@ IF(NOT LINUX)
 ENDIF()
 
 
+### VERSION ###
+DKSET(CEF_VERSION 81.3.3+g072a5f5+chromium-81.0.4044.138_linux64)
+DKSET(CEF ${3RDPARTY}/cef_binary_${CEF_VERSION})
+
+
+
 ### INSTALL ###
-DKINSTALL(https://cef-builds.spotifycdn.com/cef_binary_81.3.3%2Bg072a5f5%2Bchromium-81.0.4044.138_linux64.tar.bz2 cef_binary_81.3.3+g072a5f5+chromium-81.0.4044.138_linux64)
-DKSET(CEF ${3RDPARTY}/cef_binary_81.3.3+g072a5f5+chromium-81.0.4044.138_linux64)
+## https://cef-builds.spotifycdn.com/cef_binary_81.3.3%2Bg072a5f5%2Bchromium-81.0.4044.138_linux64.tar.bz2 
+DKINSTALL(https://cef-builds.spotifycdn.com/cef_binary_81.3.3%2Bg072a5f5%2Bchromium-81.0.4044.138_linux64.tar.bz2 cef_binary_linux64 cef_binary_${CEF_VERSION})
 
 
-### LINK ###
+
+### DKPLUGINS LINK ###
 DKDEFINE(__STDC_CONSTANT_MACROS)
 DKDEFINE(__STDC_FORMAT_MACROS)
 DKDEFINE(PSAPI_VERSION=1)
 DKDEFINE(NOMINMAX)
 DKDEFINE(CEF_SANDBOX)
 DKDEFINE(CEF_ATL)
-
 LINUX_INCLUDE(${CEF})
-LINUX_DEBUG_LIB(${CEF}/Debug/libcef.so)
+LINUX_DEBUG_LIB(${CEF}/${DEBUG}/libcef.so)
 LINUX_RELEASE_LIB(${CEF}/${RELEASE}/libcef.so)
-LINUX_DEBUG_LIB(${CEF}/${OS}/Debug/libcef_dll_wrapper/libcef_dll_wrapper.a)
+LINUX_DEBUG_LIB(${CEF}/${OS}/${DEBUG}/libcef_dll_wrapper/libcef_dll_wrapper.a)
 LINUX_RELEASE_LIB(${CEF}/${OS}/${RELEASE}/libcef_dll_wrapper/libcef_dll_wrapper.a)
-## LINUX_DEBUG_LIB(${CEF}/Debug/cef_sandbox.a)
+## LINUX_DEBUG_LIB(${CEF}/${DEBUG}/cef_sandbox.a)
 ## LINUX_RELEASE_LIB(${CEF}/${RELEASE}/cef_sandbox.a)
 
 
+
 ### COMPILE ###
-DKSETPATH(${CEF}/${OS}/Debug)
+LINUX_DEBUG_PATH(${CEF}/${OS}/${DEBUG})
 LINUX_DEBUG_COMMAND(${CMAKE_COMMAND} -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-fPIC ${CEF})
 LINUX_DEBUG_COMMAND(make libcef_dll_wrapper)
 
-DKSETPATH(${CEF}/${OS}/${RELEASE})
+LINUX_RELEASE_PATH(${CEF}/${OS}/${RELEASE})
 LINUX_RELEASE_COMMAND(${CMAKE_COMMAND} -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-fPIC ${CEF})
 LINUX_RELEASE_COMMAND(make libcef_dll_wrapper)
