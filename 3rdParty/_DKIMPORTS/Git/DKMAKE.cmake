0bc00840c573c3ff5eb28d6b545f6c9ec8621d87
aquawicket <aquawicket@digitalknob.com>
2021-07-01 22:46:39 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/Git/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/Git/DKMAKE.cmake
@@ -1,16 +1,16 @@
-## VERSION ###
+### VERSION ###
 DKSET(GIT_VERSION 2.31.1)
 
-
-### INSTALL ###
-IF(NOT EXISTS "C:/Program Files/Git")
-	WIN32_PATH(${DIGITALKNOB}/Download)
-	DKDOWNLOAD(https://github.com/git-for-windows/git/releases/download/v${GIT_VERSION}.windows.1/Git-${GIT_VERSION}-32-bit.exe)
-	WIN32_PATH(C:/)
-	DKSET(QUEUE_BUILD ON)
-	WIN32_COMMAND(${DIGITALKNOB}/Download/Git-${GIT_VERSION}-32-bit.exe})
-ENDIF()
-
-## FIXME: These are incorrect
-DKSET(GIT ${3RDPARTY}/${GIT_VERSION})
-DKSET(GIT_EXE ${GIT}/bin/git.exe)
+if(CMAKE_HOST_WIN32)
+	DKSET(GIT "C:/Program Files/Git")
+	DKSET(GIT_EXE ${GIT}/bin/git.exe)
+	
+	### INSTALL ###
+	if(NOT EXISTS "C:/Program Files/Git")
+		WIN32_PATH(${DIGITALKNOB}/Download)
+		DKDOWNLOAD(https://github.com/git-for-windows/git/releases/download/v${GIT_VERSION}.windows.1/Git-${GIT_VERSION}-32-bit.exe)
+		WIN32_PATH(C:/)
+		DKSET(QUEUE_BUILD ON)
+		WIN32_COMMAND(${DIGITALKNOB}/Download/Git-${GIT_VERSION}-32-bit.exe})
+	endif()
+endif()
