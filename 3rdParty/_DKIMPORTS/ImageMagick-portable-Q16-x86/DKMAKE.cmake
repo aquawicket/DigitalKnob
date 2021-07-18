8f194a386916ad0b7a9ad3c3c2ddd4ef3122d8a8
aquawicket <aquawicket@digitalknob.com>
2021-07-18 08:05:06 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/ImageMagick-portable-Q16-x86/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ImageMagick-portable-Q16-x86/DKMAKE.cmake
@@ -7,11 +7,11 @@ DKDEPEND(gs922w64)
 
 
 ### VERSION ###
-DKSET(IMAGEMAGICK_VERSION "7.1.0-2")
+DKSET(IMAGEMAGICK_VERSION "7.1.0-3")
 DKSET(IMAGEMAGICK_NAME ImageMagick-${IMAGEMAGICK_VERSION}-portable-Q16-x86)
 DKSET(IMAGEMAGICK_ROOT ${3RDPARTY}/${IMAGEMAGICK_NAME})
 
 
 ### INSTALL ###
-## http://ftp.icm.edu.pl/packages/ImageMagick/binaries/ImageMagick-7.1.0-0-portable-Q16-x86.zip
+## http://ftp.icm.edu.pl/packages/ImageMagick/binaries/ImageMagick-7.1.0-3-portable-Q16-x86.zip
 DKINSTALL(http://ftp.icm.edu.pl/packages/ImageMagick/binaries/${IMAGEMAGICK_NAME}.zip ImageMagick-portable-Q16-x86 ${IMAGEMAGICK_NAME})
