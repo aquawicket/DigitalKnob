0430c01ab1614ae779f095e37500a36df04d72e0
aquawicket <aquawicket@digitalknob.com>
2021-07-27 08:34:50 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/android-sdk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-sdk/DKMAKE.cmake
@@ -14,10 +14,24 @@ if(CMAKE_HOST_WIN32)
 	DKINSTALL(https://dl.google.com/android/repository/commandlinetools-${ANDROIDSDK_VERSION}.zip android-sdk ${ANDROIDSDK_NAME})
 	DKSET(ANDROIDSDK ${3RDPARTY}/${ANDROIDSDK_NAME})
 	
-	#sdkmanager --install build-tools;30.0.3 --sdk_root=C:\Users\aquawicket\digitalknob\DK\3rdParty\android-sdk
-	#sdkmanager --install platforms;android-31 --sdk_root=C:\Users\aquawicket\digitalknob\DK\3rdParty\android-sdk
-	#sdkmanager --install ndk;22.1.7171670 --sdk_root=C:\Users\aquawicket\digitalknob\DK\3rdParty\android-sdk
-	#sdkmanager --install sources;android-30 --sdk_root=C:\Users\aquawicket\digitalknob\DK\3rdParty\android-sdk
+	ANDROID_PATH(${ANDROIDSDK}/bin)
+	DKSET(ANDROID_BUILDTOOLS "build-tools\\;30.0.3")
+	if(NOT EXISTS ${ANDROIDSDK}/build-tools/30.0.3)
+		DKSET(QUEUE_BUILD ON)
+		ANDROID_COMMAND("${ANDROIDSDK}/bin/sdkmanager ${ANDROID_BUILDTOOLS} --sdk_root=${ANDROIDSDK}")
+	endif()
+	if(NOT EXISTS ${ANDROIDSDK}/platforms/android-26)	
+		DKSET(QUEUE_BUILD ON)
+		ANDROID_COMMAND("${ANDROIDSDK}/bin/sdkmanager --install platforms;android-26 --sdk_root=${ANDROIDSDK}")
+	endif()
+	if(NOT EXISTS ${ANDROIDSDK}/ndk/22.1.7171670)
+		DKSET(QUEUE_BUILD ON)
+		ANDROID_COMMAND("${ANDROIDSDK}/bin/sdkmanager --install ndk;22.1.7171670 --sdk_root=${ANDROIDSDK}")
+	endif()
+	##if(NOT EXISTS ${ANDROIDSDK}/???)
+		#DKSET(QUEUE_BUILD ON)
+		#ANDROID_COMMAND("${ANDROIDSDK}/bin/sdkmanager --install sources;android-30 --sdk_root=${ANDROIDSDK}")
+	##endif()
 endif()
 
 if(CMAKE_HOST_APPLE)
