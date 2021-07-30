46c20c1ec21727f24ec8e1bf79e7310a53ea93ba
aquawicket <aquawicket@digitalknob.com>
2021-07-30 06:27:03 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/android-platforms/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-platforms/DKMAKE.cmake
@@ -1,14 +1,14 @@
-# https://dl.google.com/android/repository/platform-26_r02.zip
+# https://dl.google.com/android/repository/platform-24_r02.zip
 if(NOT ANDROID)
 	return()
 endif()
 
 
 ### VERSION ###
-DKSET(ANDROIDPLATFORMS_VERSION 26_r02)
+DKSET(ANDROIDPLATFORMS_VERSION 24_r02)
 DKSET(ANDROIDPLATFORMS_SHA1 ???)
 DKSET(ANDROIDPLATFORMS ${ANDROIDSDK}/platforms/${ANDROIDPLATFORMS_VERSION})
-DKSET(ANDROIDPLATFORMS_DL https://dl.google.com/android/repository/platform-26_r02.zip)
+DKSET(ANDROIDPLATFORMS_DL https://dl.google.com/android/repository/platform-${ANDROIDPLATFORMS_VERSION}.zip)
 
 
 ### INSTALL ###
--- 3rdParty/_DKIMPORTS/android-sources/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-sources/DKMAKE.cmake
@@ -1,14 +1,14 @@
-# https://dl.google.com/android/repository/sources-30_r01.zip
+https://dl.google.com/android/repository/sources-24_r01.zip
 if(NOT ANDROID)
 	return()
 endif()
 
 
 ### VERSION ###
-DKSET(ANDROIDSOURCES_VERSION 26_r02)
+DKSET(ANDROIDSOURCES_VERSION 24_r01)
 DKSET(ANDROIDSOURCES_SHA1 ???)
 DKSET(ANDROIDSOURCES ${ANDROIDSDK}/sources/${ANDROIDSOURCES_VERSION})
-DKSET(ANDROIDSOURCES_DL https://dl.google.com/android/repository/sources-30_r01.zip)
+DKSET(ANDROIDSOURCES_DL https://dl.google.com/android/repository/sources-${ANDROIDSOURCES_VERSION}.zip)
 
 
 ### INSTALL ###
