9222590cd536861a69d7f0b26b95202972da0310
aquawicket <aquawicket@digitalknob.com>
2021-07-30 06:07:45 -0700

    commit from git

---
+++ 3rdParty/_DKIMPORTS/android-system-images/DKMAKE.cmake
@@ -0,0 +1,20 @@
+# https://dl.google.com/android/repository/sys-img/android/armeabi-v7a-24_r07.zip
+# https://dl.google.com/android/repository/sys-img/android/arm64-v8a-24_r07.zip
+# https://dl.google.com/android/repository/sys-img/android/x86-24_r08.zip
+# https://dl.google.com/android/repository/sys-img/android/x86_64-24_r08.zip
+if(NOT ANDROID)
+	return()
+endif()
+
+
+### VERSION ###
+DKSET(ANDROIDIMAGES_VERSION 24)
+DKSET(ANDROIDIMAGES_SHA1 ???)
+DKSET(ANDROIDIMAGES ${ANDROIDSDK}/system-images/${ANDROIDIMAGES_VERSION})
+
+
+### INSTALL ###
+DKINSTALL(https://dl.google.com/android/repository/sys-img/android/armeabi-v7a-24_r07.zip android-system-images android-sdk/system-images/${ANDROIDIMAGES_VERSION})
+DKINSTALL(https://dl.google.com/android/repository/sys-img/android/arm64-v8a-24_r07.zip android-system-images android-sdk/system-images/${ANDROIDIMAGES_VERSION})
+DKINSTALL(https://dl.google.com/android/repository/sys-img/android/x86-24_r08.zip android-system-images android-sdk/system-images/${ANDROIDIMAGES_VERSION})
+DKINSTALL(https://dl.google.com/android/repository/sys-img/android/x86_64-24_r08.zip android-system-images android-sdk/system-images/${ANDROIDIMAGES_VERSION})
