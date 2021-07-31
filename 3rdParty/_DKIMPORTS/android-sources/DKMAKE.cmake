4e7cf72ea5a6ed8542b8efc3efe480ef8f3c8555
aquawicket <aquawicket@digitalknob.com>
2021-07-31 03:06:41 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/android-sources/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-sources/DKMAKE.cmake
@@ -1,12 +1,13 @@
 # https://dl.google.com/android/repository/sources-24_r01.zip
+# https://dl.google.com/android/repository/sources-26_r01.zip
 if(NOT ANDROID)
 	return()
 endif()
 
 
 ### VERSION ###
-DKSET(ANDROIDSOURCES_FOLDER android-24)
-DKSET(ANDROIDSOURCES_VERSION 24_r01)
+DKSET(ANDROIDSOURCES_FOLDER android-26)
+DKSET(ANDROIDSOURCES_VERSION 26_r01)
 DKSET(ANDROIDSOURCES_SHA1 ???)
 DKSET(ANDROIDSOURCES ${ANDROIDSDK}/sources/${ANDROIDSOURCES_FOLDER})
 DKSET(ANDROIDSOURCES_DL https://dl.google.com/android/repository/sources-${ANDROIDSOURCES_VERSION}.zip)
