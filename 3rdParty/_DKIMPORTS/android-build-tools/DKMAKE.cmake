ac5717b7dac462bd58fe74b342defd398fe24406
aquawicket <aquawicket@digitalknob.com>
2021-08-01 18:39:47 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/android-build-tools/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-build-tools/DKMAKE.cmake
@@ -1,11 +1,12 @@
+# Information
 # https://developer.android.com/studio/releases/build-tools
-#
+# https://androidsdkoffline.blogspot.com/p/android-sdk-build-tools.html
+
+# Downloads
 # https://dl.google.com/android/repository/build-tools_r30.0.1-windows.zip
 # https://dl.google.com/android/repository/build-tools_r30.0.1-darwin.zip
 # https://dl.google.com/android/repository/build-tools_r30.0.1-linux.zip
-if(NOT ANDROID)
-	return()
-endif()
+# https://dl.google.com/android/repository/91936d4ee3ccc839f0addd53c9ebf087b1e39251.build-tools_r30.0.3-windows.zip
 
 
 ### VERSION ###
@@ -13,16 +14,16 @@ DKSET(ANDROIDBUILDTOOLS_VERSION 30.0.3)
 DKSET(ANDROIDBUILDTOOLS_SHA1 ???)
 DKSET(ANDROIDBUILDTOOLS ${ANDROIDSDK}/build-tools/${ANDROIDBUILDTOOLS_VERSION})
 if(CMAKE_HOST_WIN32)
-	DKSET(ANDROIDBUILDTOOLS_DL https://dl.google.com/android/repository/build-tools_r${ANDROIDBUILDTOOLS_VERSION}-windows.zip)
+	DKSET(ANDROIDBUILDTOOLS_DL https://dl.google.com/android/repository/91936d4ee3ccc839f0addd53c9ebf087b1e39251.build-tools_r${ANDROIDBUILDTOOLS_VERSION}-windows.zip)
 endif()
 if(CMAKE_HOST_APPLE)
 	DKSET(ANDROIDBUILDTOOLS_DL https://dl.google.com/android/repository/build-tools_r${ANDROIDBUILDTOOLS_VERSION}-darwin.zip)
 endif()
 if(CMAKE_HOST_LINUX)
-	DKSET(ANDROIDBUILDTOOLS_DL https://dl.google.com/android/repository/build-tools_r${ANDROIDBUILDTOOLS_VERSION}-linux.zip)
+	DKSET(ANDROIDBUILDTOOLS_DL https://dl.google.com/android/repository/cc6bd534c6c37604205171784ac5621.build-tools_r${ANDROIDBUILDTOOLS_VERSION}-linux.zip)
 endif()
 
 
 ### INSTALL ###
-#file(MAKE_DIRECTORY ${ANDROIDSDK}/build-tools)
-#DKINSTALL(${ANDROIDBUILDTOOLS_DL} android-build-tools ${ANDROIDBUILDTOOLS})
+file(MAKE_DIRECTORY ${ANDROIDSDK}/build-tools)
+DKINSTALL(${ANDROIDBUILDTOOLS_DL} android-build-tools ${ANDROIDBUILDTOOLS})
