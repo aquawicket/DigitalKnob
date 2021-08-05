e07b752e20c4825b8c2bde7f4f02e37a17994ef4
aquawicket <aquawicket@digitalknob.com>
2021-08-05 15:22:35 -0700

    commit from git

--- DKApps/DKTestAll/DKMAKE.cmake
+++
@@ -1,9 +0,0 @@
-## DKTestAll is the "include everything" app.
-## Here we try to include all plugins and 3rd party libraries for all platforms.
-## DKTestAll is handy for debugging purposes and adding libraries and plugins to DK.
-
-DKSET(USE_DKCef ON) ##FIXME: Find a way to remove this requirement
-
-## ADD any and all plugins here
-DKDEPEND_ALL()
-return()
--- DKApps/README.txt
+++
@@ -1,7 +0,0 @@
-*** digitalknob/DKApps ***
-
-DKBuilder
-	This app attempts to build any DKApp from scratch on any desktop system. (WINDOWS, MAC, LINUX)
-	
-	*It will download automatically, install automatically, and build automatically.  Hopefully ;)
-	
