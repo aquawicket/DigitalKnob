ae10af2af3419ce9983b4909bf4fd0fb92c36094
aquawicket <aquawicket@digitalknob.com>
2021-08-06 04:30:46 -0700

    commit from git

---
+++ 3rdParty/_DKIMPORTS/nasm/DKMAKE.cmake
@@ -0,0 +1,11 @@
+# https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win32/nasm-2.15.05-win32.zip
+
+### VERSION ###
+DKSET(NASM_VERSION 2.15.05)
+DKSET(NASM_NAME nasm-${NASM_VERSION}-win32)
+DKSET(NASM_DL https://www.nasm.us/pub/nasm/releasebuilds/${NASM_VERSION}/win32/${NASM_NAME}.zip)
+DKSET(NASM ${3RDPARTY}/${NASM_NAME})
+DKSET(NASM_EXE ${NASM}/nasm.exe)
+
+### INSTALL ###
+DKINSTALL(${NASM_DL} nasm ${NASM})
