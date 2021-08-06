95a626489a075ae64221dab7adab70bdaa66dbc1
aquawicket <aquawicket@digitalknob.com>
2021-08-06 01:50:56 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/php/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/php/DKMAKE.cmake
@@ -3,7 +3,7 @@
 ### VERSION ###
 DKSET(PHP_VERSION 8.0.9)
 DKSET(PHP_NAME php-${PHP_VERSION})
-DKSET(PHP_DL https://www.php.net/distributions/${PHP_VERSION}.tar.gz)
+DKSET(PHP_DL https://www.php.net/distributions/${PHP_NAME}.tar.gz)
 DKSET(PHP ${3RDPARTY}/${PHP_NAME})
 
 ### INSTALL ###
