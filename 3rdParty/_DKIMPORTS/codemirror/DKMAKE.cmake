16f186b10770b5d8d629a1231e4400b291901a1d
aquawicket <aquawicket@digitalknob.com>
2021-08-05 19:31:20 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/codemirror/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/codemirror/DKMAKE.cmake
@@ -1,6 +1,10 @@
+# https://codemirror.net/codemirror.zip
+
 ### VERSION ###
-DKSET(CODEMIRROR_VERSION codemirror-5.60.0)
-DKSET(CODEMIRROR ${3RDPARTY}/${CODEMIRROR_VERSION})
+DKSET(CODEMIRROR_VERSION 5.60.0)
+DKSET(CODEMIRROR_NAME codemirror-${CODEMIRROR_VERSION})
+DKSET(CODEMIRROR_DL https://codemirror.net/codemirror.zip)
+DKSET(CODEMIRROR ${3RDPARTY}/${CODEMIRROR_NAME})
 
 ### INSTALL ###
-DKINSTALL(https://codemirror.net/codemirror.zip codemirror ${CODEMIRROR})
+DKINSTALL(${CODEMIRROR_DL} codemirror ${CODEMIRROR})
