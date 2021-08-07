9c92c489b33b7a4742e090aebf2f886b9a5e25e1
aquawicket <aquawicket@digitalknob.com>
2021-08-07 04:39:11 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/mingw32/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/mingw32/DKMAKE.cmake
@@ -2,9 +2,10 @@
 
 ### VERSION ###
 #DKSET(MINGW32_VERSION 4.9.2)
-DKSET(MINGW32_VERSION 5.1.0)
+DKSET(MINGW32_VERSION 8.1.0)
 DKSET(MINGW32_NAME mingw32-${MINGW32_VERSION})
-DKSET(MINGW32_DL https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/${MINGW32_VERSION}/threads-posix/dwarf/i686-${MINGW32_VERSION}-release-posix-dwarf-rt_v3-rev1.7z)
+#DKSET(MINGW32_DL https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/${MINGW32_VERSION}/threads-posix/dwarf/i686-${MINGW32_VERSION}-release-posix-dwarf-rt_v3-rev1.7z)
+DKSET(MINGW32_DL https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/8.1.0/threads-posix/dwarf/i686-8.1.0-release-posix-dwarf-rt_v6-rev0.7z)
 DKSET(MINGW32 ${3RDPARTY}/${MINGW32_NAME})
 
 ### INSTALL ###
