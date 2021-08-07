de0fd68db4a96c864d25c66cf129c2af2dac95c6
aquawicket <aquawicket@digitalknob.com>
2021-08-07 04:06:27 -0700

    commit from git

---
+++ 3rdParty/_DKIMPORTS/bzip2-win-build/DKMAKE.cmake
@@ -0,0 +1,21 @@
+# https://github.com/kiyolee/bzip2-win-build
+#
+# https://github.com/kiyolee/bzip2-win-build/archive/refs/tags/v1.0.8.zip
+
+### VERSION ###
+DKSET(BZIP2 1.0.8)
+DKSET(BZIP2_NAME bzip2-${BZIP2_VERSION})
+DKSET(BZIP2_DL https://github.com/kiyolee/bzip2-win-build/archive/refs/tags/v${BZIP2_VERSION}.zip)
+DKSET(BZIP2 ${3RDPARTY}/${BZIP2_NAME})
+
+
+### INSTALL ###
+DKINSTALL(${BZIP2_DL} bzip2-win-build ${BZIP2})
+
+
+### LINK ###
+#DKDEFINE(USING_BZIP2)
+#DKINCLUDE(${BZIP2}/include)
+#DKINCLUDE(${BZIP2}/${OS})
+#WIN_DEBUG_LIB(${BZIP2}/${OS}/${DEBUG_DIR}/libraryd.lib)
+#WIN_RELEASE_LIB(${BZIP2}/${OS}/${RELEASE_DIR}/library.lib)
--- 3rdParty/_DKIMPORTS/mingw64/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/mingw64/DKMAKE.cmake
@@ -1,7 +1,8 @@
 # https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.9.2/threads-posix/seh/x86_64-4.9.2-release-posix-seh-rt_v3-rev1.7z
 
 ### VERSION ###
-DKSET(MINGW64_VERSION 4.9.2)
+#DKSET(MINGW64_VERSION 4.9.2)
+DKSET(MINGW64_VERSION 5.1.0)
 DKSET(MINGW64_NAME mingw64-${MINGW32_VERSION})
 DKSET(MINGW64_DL https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/${MINGW64_VERSION}/threads-posix/seh/x86_64-${MINGW64_VERSION}-release-posix-seh-rt_v3-rev1.7z)
 DKSET(MINGW64 ${3RDPARTY}/${MINGW64_NAME})
