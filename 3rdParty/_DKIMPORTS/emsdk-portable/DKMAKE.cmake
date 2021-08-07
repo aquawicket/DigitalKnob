55bc9cbd9a65e19fd25d893899a4771a281f9ade
aquawicket <aquawicket@digitalknob.com>
2021-08-07 08:37:12 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/emsdk-portable/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/emsdk-portable/DKMAKE.cmake
@@ -4,7 +4,6 @@
 # https://github.com/emscripten-core/emsdk/archive/refs/tags/2.0.26.zip
 
 DKSET(EMSCRIPTEN_VERSION 2.0.26)
-#DKSET(EMSCRIPTEN_VERSION portable-64bit)
 DKSET(EMSCRIPTEN_NAME emsdk-${EMSCRIPTEN_VERSION})
 DKSET(EMSCRIPTEN_DL https://github.com/emscripten-core/emsdk/archive/refs/tags/${EMSCRIPTEN_VERSION}.zip)
 DKSET(EMSCRIPTEN ${3RDPARTY}/${EMSCRIPTEN_NAME})
