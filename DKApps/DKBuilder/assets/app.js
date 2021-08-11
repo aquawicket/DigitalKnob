712d754d5f23c9413a473d898d5caa1df42b9d4a
aquawicket <aquawicket@hotmail.com>
2021-08-11 04:14:50 -0700

    commit from git

--- DKApps/DKBuilder/assets/app.js
+++ DKApps/DKBuilder/assets/app.js
@@ -1,15 +1,6 @@
 CPP_DK_setConsolePosition(400, 0, (screen.width/2)-100, screen.height-40);
 
-CPP_DK_Create("DKTray");
+//CPP_DK_Create("DKTray");
 //CPP_DKTray_Init();
 CPP_DK_Create("DKBuild/DKBuildConsole.js");
 //CPP_DK_Create("DKUpdate");
-
-/*
-var fullPath = "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe";
-var local_assets = CPP_DKAssets_LocalAssets();
-var getShortPath = local_assets+"DKFile/getShortPath.cmd";
-var shortPath = CPP_DK_Execute(getShortPath+' "'+fullPath+'"');
-shortPath = shortPath.slice(0, -1)
-console.log("\n\n\nshorPath = <start>"+shortPath+"<end>");
-*/
