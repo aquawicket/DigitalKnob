93bee57eb157f922ed926e02b193ed861a657c51
aquawicket <aquawicket@hotmail.com>
2021-08-30 21:14:22 -0700

    commit from git

--- DKPlugins/DK/DKFile.h
+++ DKPlugins/DK/DKFile.h
@@ -3,18 +3,18 @@
 #define DKFile_H
 
 #ifndef __has_include
-static_assert(false, "__has_include not supported");
+	static_assert(false, "__has_include not supported");
 #else
-#  if __cplusplus >= 201703L && __has_include(<filesystem>)
-#    include <filesystem>
-namespace fs = std::filesystem;
-#  elif __has_include(<experimental/filesystem>)
-#    include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#  elif __has_include(<boost/filesystem.hpp>)
-#    include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
-#  endif
+	#if __cplusplus >= 201703L && __has_include(<filesystem>)
+		#include <filesystem>
+		namespace fs = std::filesystem;
+	#elif __has_include(<experimental/filesystem>)
+		#include <experimental/filesystem>
+		namespace fs = std::experimental::filesystem;
+	#elif __has_include(<boost/filesystem.hpp>)
+		#include <boost/filesystem.hpp>
+		namespace fs = boost::filesystem;
+	#endif
 #endif
 
 #include "DK/DK.h"
