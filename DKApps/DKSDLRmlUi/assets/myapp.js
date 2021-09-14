8f2af9847c08241d849cf7045be46f3d21b8b160
aquawicket <aquawicket@hotmail.com>
2021-09-13 18:56:16 -0700

    commit from git

--- DKApps/DKSDLRmlUi/assets/myapp.js
+++ DKApps/DKSDLRmlUi/assets/myapp.js
@@ -1,5 +1,4 @@
-DKPlugin("TestCode/CodeRunner.js")
-
+DKPlugin("DKCodeRunner/DKCodeRunner.js")
 
 window.resizeTo(window.screen.width/2, window.screen.height-60);
 window.moveTo(window.screen.width/2, 30);
--- DKApps/DKTestAll/assets/app.js
+++ DKApps/DKTestAll/assets/app.js
@@ -37,8 +37,3 @@ window.addEventListener("keydown", function mykeydown(event){
 	console.log("event.which: "+event.which)
 	console.log(" ");
 })
-
-//// Create Cef window ////
-//var USE_CEF = 1;
-//CPP_DK_Create("DKCef,Cef,0,0,800,600,"+location.href);
-//CPP_DKCef_NewBrowser("Cef",5,5,screen.width/2-10,screen.height-45,location.href);
