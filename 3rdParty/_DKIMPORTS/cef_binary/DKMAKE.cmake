98e470addbb7e861a15493daaca96f033647ef17
aquawicket <aquawicket@hotmail.com>
2021-09-13 22:18:06 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/cef_binary/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/cef_binary/DKMAKE.cmake
@@ -1,9 +1,12 @@
+# https://cef-builds.spotifycdn.com/index.html
+#
 # https://cef-builds.spotifycdn.com/cef_binary_81.3.3+g072a5f5+chromium-81.0.4044.138_windows32.tar.bz2
-# https://github.com/chromiumembedded/cef
 
 ### VERSION ###
 DKSET(CEF_VERSION 81.3.3+g072a5f5+chromium-81.0.4044.138)
-DKSET(CEF_NAME cef_binary_${CE_VERSION}_windows32)
+IF(WIN32)
+	DKSET(CEF_NAME cef_binary_${CEF_VERSION}_windows32)
+ENDIF()
 DKSET(CEF_DL https://cef-builds.spotifycdn.com/${CEF_NAME}.tar.bz2)
 DKSET(CEF ${3RDPARTY}/${CEF_NAME})
 
@@ -12,20 +15,20 @@ DKSET(CEF ${3RDPARTY}/${CEF_NAME})
 DKINSTALL(${CEF_DL} cef_binary ${CEF})
 
 
-### LINK ###
+### DKPLUGINS LINK ###
 DKDEFINE(__STDC_CONSTANT_MACROS)
 DKDEFINE(__STDC_FORMAT_MACROS)
 DKDEFINE(PSAPI_VERSION=1)
 DKDEFINE(NOMINMAX)
-## DKDEFINE(CEF_SANDBOX)
+#DKDEFINE(CEF_SANDBOX)
 DKDEFINE(CEF_ATL)
 WIN32_INCLUDE(${CEF})
 WIN32_DEBUG_LIB(${CEF}/${DEBUG_DIR}/libcef.lib)
 WIN32_RELEASE_LIB(${CEF}/${RELEASE_DIR}/libcef.lib)
 WIN32_DEBUG_LIB(${CEF}/${OS}/libcef_dll_wrapper/${DEBUG_DIR}/libcef_dll_wrapper.lib)
 WIN32_RELEASE_LIB(${CEF}/${OS}/libcef_dll_wrapper/${RELEASE_DIR}/libcef_dll_wrapper.lib)
-## WIN32_DEBUG_LIB(${CEF}/${DEBUG_DIR}/cef_sandbox.lib)
-## WIN32_RELEASE_LIB(${CEF}/${RELEASE_DIR}/cef_sandbox.lib)
+#WIN32_DEBUG_LIB(${CEF}/${DEBUG_DIR}/cef_sandbox.lib)
+#WIN32_RELEASE_LIB(${CEF}/${RELEASE_DIR}/cef_sandbox.lib)
 
 
 ### COMPILE ###
--- 3rdParty/_DKIMPORTS/cef_binary_windows32/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/cef_binary_windows32/DKMAKE.cmake
@@ -1,14 +1,18 @@
+# https://cef-builds.spotifycdn.com/index.html
+#
 # https://cef-builds.spotifycdn.com/cef_binary_81.3.3+g072a5f5+chromium-81.0.4044.138_windows32.tar.bz2
 
 ### VERSION ###
 DKSET(CEF_VERSION 81.3.3+g072a5f5+chromium-81.0.4044.138)
-DKSET(CEF_NAME cef_binary_${CEF_VERSION}_windows32)
+IF(WIN32)
+	DKSET(CEF_NAME cef_binary_${CEF_VERSION}_windows32)
+ENDIF()
 DKSET(CEF_DL https://cef-builds.spotifycdn.com/${CEF_NAME}.tar.bz2)
 DKSET(CEF ${3RDPARTY}/${CEF_NAME})
 
 
 ### INSTALL ###
-DKINSTALL(${CEF_DL} cef_binary_windows32 ${CEF})
+DKINSTALL(${CEF_DL} cef_binary ${CEF})
 
 
 ### DKPLUGINS LINK ###
@@ -16,19 +20,20 @@ DKDEFINE(__STDC_CONSTANT_MACROS)
 DKDEFINE(__STDC_FORMAT_MACROS)
 DKDEFINE(PSAPI_VERSION=1)
 DKDEFINE(NOMINMAX)
-## DKDEFINE(CEF_SANDBOX)
+#DKDEFINE(CEF_SANDBOX)
 DKDEFINE(CEF_ATL)
 WIN32_INCLUDE(${CEF})
 WIN32_DEBUG_LIB(${CEF}/${DEBUG_DIR}/libcef.lib)
 WIN32_RELEASE_LIB(${CEF}/${RELEASE_DIR}/libcef.lib)
 WIN32_DEBUG_LIB(${CEF}/${OS}/libcef_dll_wrapper/${DEBUG_DIR}/libcef_dll_wrapper.lib)
 WIN32_RELEASE_LIB(${CEF}/${OS}/libcef_dll_wrapper/${RELEASE_DIR}/libcef_dll_wrapper.lib)
-## WIN32_DEBUG_LIB(${CEF}/${DEBUG_DIR}/cef_sandbox.lib)
-## WIN32_RELEASE_LIB(${CEF}/${RELEASE_DIR}/cef_sandbox.lib)
+#WIN32_DEBUG_LIB(${CEF}/${DEBUG_DIR}/cef_sandbox.lib)
+#WIN32_RELEASE_LIB(${CEF}/${RELEASE_DIR}/cef_sandbox.lib)
 
 
 ### COMPILE ###
 WIN_PATH(${CEF}/${OS})
 WIN32_COMMAND(${DKCMAKE_WIN32} -DUSE_SANDBOX=Off ${CEF})
+WIN64_COMMAND(${DKCMAKE_WIN64} -DUSE_SANDBOX=Off ${CEF})
 WIN_VS(${CEF_NAME} cef.sln libcef_dll_wrapper)
 WIN_VS(${CEF_NAME} cef.sln cefclient)
