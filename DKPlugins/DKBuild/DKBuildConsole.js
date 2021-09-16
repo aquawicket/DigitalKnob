06acd4c4e7d600d8dc32cf6fb92a940ce037cfd6
aquawicket <aquawicket@hotmail.com>
2021-09-15 22:53:38 -0700

    commit from git

--- DKPlugins/DKBuild/DKBuildConsole.js
+++ DKPlugins/DKBuild/DKBuildConsole.js
@@ -252,7 +252,6 @@ function DKBuildConsole_Process() {
     console.log("###########################")
     console.log("Press any key to Build")
     CPP_DK_GetKey()
-	DKBuild_DoResults()
 	if(APP == "ALLAPPS"){
 		for (var i = 0; i < APP_LIST.length; ++i){
 		    APP = APP_LIST[i]
