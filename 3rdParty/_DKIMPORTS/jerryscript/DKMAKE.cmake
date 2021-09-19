017b54ca46847a83784ec44f121a278032782e07
aquawicket <aquawicket@hotmail.com>
2021-09-19 18:34:19 +0200

    commit from git

--- 3rdParty/_DKIMPORTS/jerryscript/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/jerryscript/DKMAKE.cmake
@@ -4,4 +4,25 @@
 # https://github.com/jerryscript-project/jerryscript/archive/refs/tags/v2.4.0.zip
 
 
-# TODO
+### VERSION ###
+DKSET(JERRYSCRIPT_VERSION 2.4.0)
+DKSET(JERRYSCRIPT_NAME jerryscript-${FREEALUT_VERSION})
+DKSET(JERRYSCRIPT_DL https://github.com/jerryscript-project/jerryscript/archive/refs/tags/v${JERRYSCRIPT_VERSION}.zip)
+DKSET(JERRYSCRIPT ${3RDPARTY}/${JERRYSCRIPT_NAME})
+
+
+### INSTALL ###
+DKINSTALL(${JERRYSCRIPT_DL} jerryscript ${JERRYSCRIPT})
+
+
+### LINK ###
+DKINCLUDE(${JERRYSCRIPT})
+WIN_DEBUG_LIB(${JERRYSCRIPT}/${OS}/${DEBUG_DIR}/jerryscript.lib)
+WIN_RELEASE_LIB(${JERRYSCRIPT}/${OS}/${RELEASE_DIR}/jerryscript.lib)
+
+
+### COMPILE ###
+WIN_PATH(${JERRYSCRIPT}/${OS})
+WIN32_COMMAND(${DKCMAKE_WIN32} ${JERRYSCRIPT})
+WIN64_COMMAND(${DKCMAKE_WIN64} ${JERRYSCRIPT})
+WIN_VS(${JERRYSCRIPT_NAME} JERRYSCRIPT.sln jerryscript)
