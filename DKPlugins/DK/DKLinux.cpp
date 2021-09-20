e9ec4fa064a71e89cc64bf344e09531c3a984f31
aquawicket <aquawicket@hotmail.com>
2021-09-19 22:32:23 -0700

    commit from git

--- DKPlugins/DK/DKLinux.cpp
+++ DKPlugins/DK/DKLinux.cpp
@@ -12,24 +12,29 @@
 #include <termios.h> //for getch()
 
 bool DKLinux::getch(int& key){
-    char buf = 0;
+    char buf[4];
     struct termios old = {0};
     fflush(stdout);
     if(tcgetattr(0, &old) < 0)
-        perror("tcsetattr()");
+        return DKERROR("tcsetattr() failed");
     old.c_lflag &= ~ICANON;
     old.c_lflag &= ~ECHO;
     old.c_cc[VMIN] = 1;
     old.c_cc[VTIME] = 0;
     if(tcsetattr(0, TCSANOW, &old) < 0)
-        perror("tcsetattr ICANON");
-    if(read(0, &buf, 1) < 0)
-        perror("read()");
+        return DKERROR("tcsetattr() ICANON failed");
+    if(read(0, &buf, sizeof(buf)) < 0)
+        return DKERROR("read() failed");
     old.c_lflag |= ICANON;
     old.c_lflag |= ECHO;
     if(tcsetattr(0, TCSADRAIN, &old) < 0)
-        perror("tcsetattr ~ICANON");
-    key = buf;
+        return DKERROR("tcsetattr() ~ICANON failed");
+	if(!buf[0])
+		return DKERROR("buf invalid");
+    int i=0;
+    while(i < 4 && buf[i])
+        i++;
+    key = buf[i-1];
 	return true;
 }
  
