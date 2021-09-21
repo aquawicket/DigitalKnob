c6aca03d85d0715e8a0b67b241a00bb576e4c0a6
aquawicket <aquawicket@hotmail.com>
2021-09-21 00:54:44 -0700

    commit from git

--- DKPlugins/DKDom/DKDomWorkerGlobalScope.js
+++ DKPlugins/DKDom/DKDomWorkerGlobalScope.js
@@ -1,66 +1,23 @@
-//https://udn.realityripple.com/docs/Web/API/WindowOrWorkerGlobalScope
-//https://html.spec.whatwg.org/multipage/webappapis.html#windoworworkerglobalscope-mixin
-//https://javascript.info/mixins
+//https://udn.realityripple.com/docs/Web/API/WorkerGlobalScope
 //
-// The WindowOrWorkerGlobalScope mixin is for use of APIs that are to be exposed on Window and WorkerGlobalScope objects.
+// The WorkerGlobalScope mixin is for use of APIs that are to be exposed on Window and WorkerGlobalScope objects.
 
 
-var WindowOrWorkerGlobalScope = function(pointer) {
+var WorkerGlobalScope = function(pointer) {
     this.pointer = pointer;
 
 	// Properties
-    Object.defineProperty(this, "caches", {
+    Object.defineProperty(this, "todo", {
         get: function() {
-            return CPP_DKWindowOrWorkerGlobalScope_caches(pointer)
+            return CPP_DKWorkerGlobalScope_todo(pointer)
         }
     })
-	Object.defineProperty(this, "crossOriginIsolated", {
-        get: function() {
-            return CPP_DKWindowOrWorkerGlobalScope_crossOriginIsolated(pointer)
-        }
-    })
-	Object.defineProperty(this, "indexedDB", {
-        get: function() {
-            return CPP_DKWindowOrWorkerGlobalScope_indexedDB(pointer)
-        }
-    })
-	Object.defineProperty(this, "isSecureContext", {
-        get: function() {
-            return CPP_DKWindowOrWorkerGlobalScope_isSecureContext(pointer)
-        }
-    })
-	Object.defineProperty(this, "origin", {
-        get: function() {
-            return CPP_DKWindowOrWorkerGlobalScope_origin(pointer)
-        }
-    })
-	
+
 	// Methods
-	WindowOrWorkerGlobalScope.prototype.atob = function() {
+	WorkerGlobalScope.prototype.todo = function() {
 		//TODO
     }
-	WindowOrWorkerGlobalScope.prototype.btoa = function() {
-		//TODO
-    }
-	WindowOrWorkerGlobalScope.prototype.clearInterval = function() {
-		//TODO
-    }
-	WindowOrWorkerGlobalScope.prototype.clearTimeout = function() {
-		//TODO
-    }
-	WindowOrWorkerGlobalScope.prototype.createImageBitmap = function() {
-		//TODO
-    }
-	WindowOrWorkerGlobalScope.prototype.queueMicrotask = function() {
-		//TODO
-    }
-	WindowOrWorkerGlobalScope.prototype.setInterval = function() {
-		//TODO
-    }
-	WindowOrWorkerGlobalScope.prototype.setTimeout = function() {
-		//TODO
-    }
-	
+
     return this;
 }
 
