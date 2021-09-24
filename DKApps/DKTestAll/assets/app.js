9451579511ef3accdcf463b72a5d6894943aa771
aquawicket <aquawicket@hotmail.com>
2021-09-23 18:58:37 -0700

    commit from git

--- DKApps/DKTestAll/assets/app.js
+++ DKApps/DKTestAll/assets/app.js
@@ -1,41 +1,49 @@
-const url = CPP_DKAssets_LocalAssets()+"index.html";
+const url = CPP_DKAssets_LocalAssets()+"index.html"
 
 /*
-const x = 0;
-const y = 0;
-const width = (window.screen.width/2);
-const height = window.screen.height;
+const x = 0
+const y = 0
+const width = (window.screen.width/2)
+const height = window.screen.height
 */
 
-const sdl_x = 10;
-const sdl_y = 10;
-const sdl_width = 500;
-const sdl_height = 500;
 
-const cef_x = 10;
-const cef_y = 10;
-const cef_width = 500;
-const cef_height = 500;
-
-const sdl_x_offset = 1;
-const sdl_y_offest = 31;
-const sdl_width_offset = 0;
-const sdl_height_offset = -30;
-
-const cef_x_offset = 0;
-const cef_y_offest = -7;
-const cef_width_offset = +16;
-const cef_height_offset = +9;
+//// Console window
+const console_x = 100
+const console_y = 100
+const console_width = 500
+const console_height = 500
+const console_x_offset = 0
+const console_y_offest = 0
+const console_width_offset = 0
+const console_height_offset = 0
+CPP_DK_setConsolePosition(console_x+console_x_offset, console_y+console_y_offest, console_width+console_width_offset, console_height+console_height_offset)
 
 
 //// Create Cef window
-CPP_DK_Create("DKCef");
-CPP_DKCef_NewBrowser("Cef", cef_x+cef_x_offset, cef_y+cef_y_offest, cef_width+cef_width_offset, cef_height+cef_height_offset, url);
+const cef_x = 100
+const cef_y = 100
+const cef_width = 500
+const cef_height = 500
+const cef_x_offset = -7
+const cef_y_offset = 0
+const cef_width_offset = +16
+const cef_height_offset = +9
+CPP_DK_Create("DKCef")
+CPP_DKCef_NewBrowser("Cef", cef_y+cef_y_offset, cef_x+cef_x_offset, cef_width+cef_width_offset, cef_height+cef_height_offset, url)
+
 
 //// Create SDLRml window
+const sdl_x = 100
+const sdl_y = 100
+const sdl_width = 500
+const sdl_height = 500
+const sdl_x_offset = 1
+const sdl_y_offest = 31
+const sdl_width_offset = 0
+const sdl_height_offset = -30
 CPP_DK_Create("DKWindow")
 CPP_DK_Create("DKRml")
-window.resizeTo(sdl_width+sdl_width_offset, sdl_height+sdl_height_offset);
-window.moveTo(sdl_x+sdl_x_offset, sdl_y+sdl_y_offest);
-location.href = url;
-
+window.resizeTo(sdl_width+sdl_width_offset, sdl_height+sdl_height_offset)
+window.moveTo(sdl_x+sdl_x_offset, sdl_y+sdl_y_offest)
+location.href = url
