e47dbe7aaab74f30b44a68daadaecc3bdfc15e44
aquawicket <aquawicket@hotmail.com>
2021-09-24 17:04:48 -0700

    commit from git

--- DKApps/DKTestAll/assets/myapp.js
+++ DKApps/DKTestAll/assets/myapp.js
@@ -1,19 +1,21 @@
 
-const MyApp = function() {};
-const myapp = new MyApp;
+function MyApp(){}
+const myapp = DKPlugin(MyApp);
+
 
 myapp.loadFiles = function myapp_loadFiles() {
+	DKPlugin("DKFile/DKFile.js")
 	DKPlugin("DKGui/DKGui.js")
+	DKPlugin("DKCodeRunner/DKCodeRunner.js")
+	//dk.notepad = DKPlugin("DKNotepad/DKNotepad.js")
 	DKPlugin("DKGui/DKConsole.js")
-	
-	//DKPlugin("DKCodeRunner/DKCodeRunner.js")
-	//DKPlugin("DKNotepad/DKNotepad.js")
 }
 
 myapp.loadApp = function myapp_loadApp() {
-	!DUKTAPE && DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")
-	console.debug("**** DKTestAll ****")
+	DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")
+	console.log("**** DKSDLRmlUi ****")
 }
+
 
 DUKTAPE && myapp.loadFiles();
 DUKTAPE && myapp.loadApp();
