18da2e78895626bbd39b1544952caa33fc582de2
aquawicket <aquawicket@hotmail.com>
2021-09-26 00:39:13 -0700

    commit from git

---
+++ DKPlugins/DKJerryscript/DKJerryScript.cpp
@@ -0,0 +1,18 @@
+#include "DK/stdafx.h"
+#include "DKJerryscript/DKJerryscript.h"
+
+
+bool DKJerryscript::Init(){
+	DKDEBUGFUNC();
+	return true;
+}
+
+bool DKJerryscript::End(){
+	DKDEBUGFUNC();
+	return true;
+}
+
+bool DKJerryscript::AttachFunction(const DKString& name, const DKString& vars){
+	DKDEBUGFUNC(name, vars);
+	return true;
+}
---
+++ DKPlugins/DKJerryscript/DKJerryScript.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef DKJerryscript_H
+#define DKJerryscript_H
+#include "DK/DK.h"
+#include "jerryscript.h"
+#include "DK/DKEvents.h"
+
+class DKJerryscript : public DKObjectT<DKJerryscript>{
+public:
+	bool Init();
+	bool End();
+	
+	//static bool AttachFunction(const DKString& name, const DKString& vars);
+
+};
+
+
+REGISTER_OBJECT(DKJerryscript, true);
+
+#endif //DKJerryscript_H
