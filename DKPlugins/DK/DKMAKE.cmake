c8036b3029dd5040ba48b9708d5d1dde9fb66b54
aquawicket <aquawicket@hotmail.com>
2021-09-27 09:48:00 -0700

    commit from git

--- DKPlugins/DK/DKMAKE.cmake
+++ DKPlugins/DK/DKMAKE.cmake
@@ -2,11 +2,12 @@ if(WIN)
 	DKDEFINE(_SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING)
 	LIST(APPEND WIN_LIBS Psapi.lib) # GetProcessMemoryInfo()
 	LIST(APPEND WIN_LIBS pdh.lib)   # PdhOpenQueryA()
-	LIST(APPEND WIN_LIBS Dxva2.dll) # GetNumberOfPhysicalMonitorsFromHMONITOR()	
+	LIST(APPEND WIN_LIBS Dxva2.dll) # GetNumberOfPhysicalMonitorsFromHMONITOR()
+	DKDEPEND(boxer)
 endif()
 
 if(MAC)
-
+	DKDEPEND(boxer)
 endif()
 
 if(IOS OR IOSSIM)
@@ -20,6 +21,7 @@ if(LINUX)
 	DKDEPEND(libasound2-dev) ##TODO: Move to DKAudio
 	DKDEPEND(libxtst-dev)
 	DKDEPEND(simple-getch)
+	DKDEPEND(boxer)
 endif()
 
 if(RASPBERRY)
@@ -29,12 +31,12 @@ if(RASPBERRY)
 	DKDEPEND(libasound2-dev) ##TODO: Move to DKAudio
 	DKDEPEND(libxtst-dev)
 	DKDEPEND(simple-getch)
+	DKDEPEND(boxer)
 endif()
 
 if(ANDROID)
 
 endif()
-
 
 
 DKPLUGIN(DK)
