9e9f3d65581dee09a2d9f359aa33ae7a333e3ef9
aquawicket <aquawicket@hotmail.com>
2021-10-02 13:34:35 -0700

    commit from git

--- DKPlugins/DKDom/DKDomHTMLImageElement.js
+++ DKPlugins/DKDom/DKDomHTMLImageElement.js
@@ -1,5 +1,11 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/span
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement
+
+var Image = function(width, height)
+	var image = document.createElement("img");
+	image.width = width;
+	image.height = height;
+	return new HTMLImageElement(image);
+}
 
 var HTMLImageElement = function(pointer) {
  
