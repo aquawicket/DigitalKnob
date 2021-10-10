28e3a7571094cc711c6fcf02c47ffdfa10c53e1e
aquawicket <aquawicket@hotmail.com>
2021-10-10 12:24:55 +0300

    commit from git

--- DKPlugins/DKCef/DKCefApp.cpp
+++ DKPlugins/DKCef/DKCefApp.cpp
@@ -5,13 +5,13 @@ bool DKV8::singleprocess = true;
 CefRefPtr<CefBrowser> DKV8::_browser = NULL;
 CefRefPtr<DKCefV8Handler> DKV8::v8handler = NULL;
 CefRefPtr<CefV8Value> DKV8::ctx = NULL;
-#ifdef MAC
-	//std::map<DKString, boost::function2<bool, CefArgs, CefReturn> > DKV8::functions;
-	std::map<DKString, std::function2<bool, CefArgs, CefReturn> > DKV8::functions;
-#else
+//#ifdef MAC
+		//std::map<DKString, boost::function2<bool, CefArgs, CefReturn> > DKV8::functions;
+	//std::map<DKString, std::function2<bool, CefArgs, CefReturn> > DKV8::functions;
+//#else
 	//std::map<DKString, boost::function<bool(CefArgs, CefReturn)>> DKV8::functions;
 	std::map<DKString, std::function<bool(CefArgs, CefReturn)>> DKV8::functions;
-#endif
+//#endif
 std::vector<std::string> DKV8::funcs;
 
 DKString DKV8::disable_gpu;
