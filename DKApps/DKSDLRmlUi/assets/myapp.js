cc2108c9c4cf867ba6f846afe500f6a02bf2fd6d
aquawicket <aquawicket@hotmail.com>
2021-10-20 17:53:06 -0700

    commit from git

--- DKApps/DKSDLRmlUi/assets/myapp.js
+++ DKApps/DKSDLRmlUi/assets/myapp.js
@@ -10,11 +10,22 @@ myapp.loadFiles = function myapp_loadFiles() {
 	//dk.notepad = DKPlugin("DKNotepad/DKNotepad.js")
 	//DKPlugin("DKGui/DKMessageBox.js")
 	//DKPlugin("DKGui/DKConsole.js")
+	
+	DKPlugin("DKGui/DKMessageBox.js")
 }
 
 myapp.loadApp = function myapp_loadApp() {
 	//DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")
 	console.log("**** DKSDLRmlUi ****")
+	
+	DKPlugin("DKGui/DKMessageBox.js", function() {
+		DKMessageBox.prototype.createMessage("this is a message", function(instance, okclicked) {
+            //if (instance) {
+                //console.log("instance: %c" + instance, "color:orange;")
+            //    instance.html && (instance.html.style.backgroundColor = "red")
+            //}
+        })
+    })
 }
 
 
---
+++ DKPlugins/DKGui/TestApp.js
@@ -0,0 +1,25 @@
+//"use strict";
+
+function TestApp(){}
+   
+TestApp.prototype.init = function init(init_callback){
+    //dk.create("DKGui/TestApp.css");
+    init_callback && init_callback(this);
+}
+
+TestApp.prototype.end = function end(){
+    //dk.close("DKGui/TestApp.css");
+}
+
+TestApp.prototype.create = function create(create_callback) {
+    const instance = DKPlugin(TestApp)
+    dk.create("DKGui/TestApp.html", function dkcreate_callback(html) {
+        if (!html)
+            return error("invalid html", create_callback);
+        instance.html = html;
+        //instance.message = html.querySelector("[TestApp='message']");
+        instance.dkframe = DKFrame.prototype.create(instance);
+        create_callback && create_callback(instance);
+        return instance;
+    });
+}
