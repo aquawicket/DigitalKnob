00cf393707135bcef4ee8fa326f7120310c0b828
aquawicket <aquawicket@hotmail.com>
2021-10-20 20:07:20 -0700

    commit from git

--- DKApps/DKTestAll/assets/myapp.js
+++ DKApps/DKTestAll/assets/myapp.js
@@ -4,19 +4,30 @@ const myapp = DKPlugin(MyApp);
 
 
 myapp.loadFiles = function myapp_loadFiles() {
-	DKPlugin("DK/DKTrace.js")
-	DKPlugin("DK/DKPhp.js")
-	DKPlugin("DKFile/DKFile.js")
-	DKPlugin("DKGui/DKGui.js")
+	//DKPlugin("DK/DKTrace.js")
+	//DKPlugin("DK/DKPhp.js")
+	//DKPlugin("DKFile/DKFile.js")
+	//DKPlugin("DKGui/DKGui.js")
 
-	DKPlugin("DKCodeRunner/DKCodeRunner.js")
+	//DKPlugin("DKCodeRunner/DKCodeRunner.js")
 	//dk.notepad = DKPlugin("DKNotepad/DKNotepad.js")
-	DKPlugin("DKGui/DKConsole.js")
+	//DKPlugin("DKGui/DKConsole.js")
+	
+	DKPlugin("DKGui/TestApp.js")
 }
 
 myapp.loadApp = function myapp_loadApp() {
-	DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")
-	console.log("**** DKSDLRmlUi ****")
+	//DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")
+	console.log("**** DKTestAll ****")
+	
+	DKPlugin("DKGui/TestApp.js", function() {
+		TestApp.prototype.create(function(instance) {
+            //if (instance) {
+                //console.log("instance: %c" + instance, "color:orange;")
+            //    instance.html && (instance.html.style.backgroundColor = "red")
+            //}
+        })
+    })
 }
 
 CPP_DK_SendEvent = function(){} //just consume the message from CEF for now
