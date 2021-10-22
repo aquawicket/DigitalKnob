79291c429beb2f5cd97fed91b6e9c0f1fed87ae3
aquawicket <aquawicket@hotmail.com>
2021-10-21 19:27:52 -0700

    commit from git

--- DKApps/DKSDLRmlUi/assets/myapp.js
+++ DKApps/DKSDLRmlUi/assets/myapp.js
@@ -4,34 +4,40 @@ const myapp = DKPlugin(MyApp);
 
 
 myapp.loadFiles = function myapp_loadFiles() {
-	DKPlugin("DK/DKPhp.js")
-	DKPlugin("DKFile/DKFile.js")
-	DKPlugin("DKGui/DKGui.js")
-	DKPlugin("DKGui/DKFrame.js")
-	DKPlugin("DKGui/DKDrag.js")
-	//DKPlugin("DKGui/DKResize.js")
-	//DKPlugin("DKCodeRunner/DKCodeRunner.js")
-	//dk.notepad = DKPlugin("DKNotepad/DKNotepad.js")
-	//DKPlugin("DKGui/DKMessageBox.js")
-	//DKPlugin("DKGui/DKConsole.js")
-	
-	DKPlugin("DKGui/TestApp.js")
+	//DKPlugin("DK/DKTrace.js")
+    //DKPlugin("DK/DKErrorHandler.js")
+    DKPlugin("DK/DKPhp.js")
+    DKPlugin("DK/DKJson.js")
+    DKPlugin("DKFile/DKFile.js")
+    //DKPlugin("DK/DKValidate.js")
+    //DKPlugin("DK/DKTime.js")
+    //DKPlugin("DK/DKMqtt.js")
+    //DKPlugin("DK/DKNotifications.js")
+    //DKPlugin("DKDebug/DKDebug.js")
+    //DKPlugin("DKAudio/DKAudio.js")
+    //DKPlugin("DKGui/DKConsole.js")
+    DKPlugin("DKGui/DKGui.js")
+    DKPlugin("DKGui/DKFrame.js")
+    //DKPlugin("DKGui/DKMenu.js")
+    DKPlugin("DKGui/DKMessageBox.js")
+    DKPlugin("DKGui/DKDrag.js")
+    //DKPlugin("DKGui/DKResize.js")
+    //DKPlugin("DKGui/DKClipboard.js")
+    //DKPlugin("DKGui/DKTable.js")
+    //DKPlugin("DKDevTools/DKDevToolsButton.js")
+    //DKPlugin("DKChart/DKChart.js")
+    //DKPlugin("DKCodeMirror/DKCodeMirror.js")
 }
 
 myapp.loadApp = function myapp_loadApp() {
-	//DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")
-	console.log("**** DKSDLRmlUi ****")
+	//DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")	
+	//TestApp.prototype.create(function(instance){})
 	
-	DKPlugin("DKGui/TestApp.js", function() {
-		TestApp.prototype.create(function(instance) {
-            //if (instance) {
-                //console.log("instance: %c" + instance, "color:orange;")
-            //    instance.html && (instance.html.style.backgroundColor = "red")
-            //}
-        })
-    })
+	DKPlugin("DKFile/filemanager.js", function() {
+        DKFileManager.prototype.init();
+        DKFileManager.prototype.create();
+    });
 }
-
 
 DUKTAPE && myapp.loadFiles();
 DUKTAPE && myapp.loadApp();
--- DKApps/DKTestAll/assets/myapp.js
+++ DKApps/DKTestAll/assets/myapp.js
@@ -4,33 +4,31 @@ const myapp = DKPlugin(MyApp);
 
 
 myapp.loadFiles = function myapp_loadFiles() {
-	//DKPlugin("DK/DKTrace.js")
 	DKPlugin("DK/DKPhp.js")
 	DKPlugin("DKFile/DKFile.js")
 	DKPlugin("DKGui/DKGui.js")
-	DKPlugin("DKGui/DKFrame.js")
-
-	//DKPlugin("DKCodeRunner/DKCodeRunner.js")
-	//dk.notepad = DKPlugin("DKNotepad/DKNotepad.js")
 	//DKPlugin("DKGui/DKConsole.js")
-	
+	DKPlugin("DKGui/DKFrame.js")
+	DKPlugin("DKGui/DKDrag.js")
+	//DKPlugin("DKGui/DKResize.js")
 	DKPlugin("DKGui/TestApp.js")
+	//DKPlugin("DKGui/DKMessageBox.js")
+	//dk.notepad = DKPlugin("DKNotepad/DKNotepad.js")
+	//DKPlugin("DKCodeRunner/DKCodeRunner.js")
+	//DKPlugin("DKFile/filemanager.js")
+	
 }
 
 myapp.loadApp = function myapp_loadApp() {
-	//DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")
-	console.log("**** DKTestAll ****")
+	//DKConsole.prototype.create(document.body, "", "0rem", "0rem", "0rem", "100%", "25%")	
+	//TestApp.prototype.create(function(instance){})
 	
-	DKPlugin("DKGui/TestApp.js", function() {
-		TestApp.prototype.create(function(instance) {
-            //if (instance) {
-                //console.log("instance: %c" + instance, "color:orange;")
-            //    instance.html && (instance.html.style.backgroundColor = "red")
-            //}
-        })
-    })
+	DKPlugin("DKFile/filemanager.js", function() {
+        DKFileManager.prototype.init();
+        DKFileManager.prototype.create();
+    });
 }
 
-CPP_DK_SendEvent = function(){} //just consume the message from CEF for now
+
 DUKTAPE && myapp.loadFiles();
 DUKTAPE && myapp.loadApp();
