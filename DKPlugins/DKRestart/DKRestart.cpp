f6b9cbed3f9639d1650d929c5d567e20a850fea2
aquawicket <aquawicket@hotmail.com>
2021-10-25 05:27:24 -0700

    commit from git

---
+++ DKPlugins/DKRestart/DKRestart.cpp
@@ -0,0 +1,12 @@
+#include "DKRestart.h"
+
+int main(int argc, char* argv[]){
+	printf("DKRestart\n");
+#ifdef WIN32
+	//DKMainArgs main_args;
+#else
+	//DKMainArgs main_args(argc, argv);
+#endif
+
+	//TODO
+}
---
+++ DKPlugins/DKRestart/DKRestart.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef DKRestart_H
+#define DKRestart_H
+
+class DKRestart
+{
+public:
+	DKRestart(){}
+};
+
+#endif //DKRestart_H
