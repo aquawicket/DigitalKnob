2e00c79de76f7d0b6687a53cb9ded2d492f00b40
aquawicket <aquawicket@hotmail.com>
2021-10-27 12:22:33 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/vs16-redistributable/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/vs16-redistributable/DKMAKE.cmake
@@ -1,13 +1,14 @@
 # https://docs.microsoft.com/en-US/cpp/windows/latest-supported-vc-redist?view=msvc-160#visual-studio-2015-2017-2019-and-2022
 #
 # https://aka.ms/vs/16/release/vc_redist.x86.exe
+# https://softmany.com/microsoft-visual-c-redistributable-package/download/
 
 ### VERSION ###
 DKSET(VS16REDIST_VERSION 14.29.30133)
 DKSET(VS16REDIST_PLATFORM x86)
 DKSET(VS16REDIST_NAME vc_redist.${VS16REDIST_PLATFORM}.exe)
 DKSET(VS16REDIST_DL https://aka.ms/vs/16/release/${VS16REDIST_NAME})
-DKSET(VS16REDIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\${VS16REDIST_VERSION}")
+DKSET(VS16REDIST "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Redist/MSVC/${VS16REDIST_VERSION}")
 
 ### INSTALL ###
 IF(NOT EXISTS "${VS16REDIST}")
