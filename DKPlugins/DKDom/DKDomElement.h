b6d7ba480cba8100f3217f80080245285f0e7d5c
aquawicket <aquawicket@hotmail.com>
2021-10-27 22:37:03 -0700

    commit from git

--- DKPlugins/DKDom/DKDomElement.h
+++ DKPlugins/DKDom/DKDomElement.h
@@ -13,25 +13,66 @@ class DKDomElement : public DKObjectT<DKDomElement>
 public:
 	bool Init();
 	
+	// Properties //
+
+	//assignedSlot
+	//attributes
+	//childElementCount
+	//children
+	//classList
+	//className
 	static int clientHeight(duk_context* ctx);
 	static int clientLeft(duk_context* ctx);
 	static int clientTop(duk_context* ctx);
 	static int clientWidth(duk_context* ctx);
+	//firstElementChild
+	//id
+	static int innerHTML(duk_context* ctx);
+	//lastElementChild
+	//lastElementChild
+	//localName
+	//namespaceURI
+	//nextElementSibling
+	static int outerHTML(duk_context* ctx);
+	//part
+	//prefix
+	//previousElementSibling
+	static int scrollHeight(duk_context* ctx);
+	static int scrollLeft(duk_context* ctx);
+	//scrollLeftMax
+	static int scrollTop(duk_context* ctx);
+	//scrollTopMax
+	static int scrollWidth(duk_context* ctx);
+	//shadowRoot
+	//openOrClosedShadowRoot
+	//setHTML
+	//slot
+	//tagName
+
+	// Properties included from ARIA //
+	//TODO
+
+	// Event handlers
+	//onfullscreenchange
+	//onfullscreenerror
+
+	// Methods //
+	
 	static int closest(duk_context* ctx);
 	static int getAttribute(duk_context* ctx);
 	static int getElementsByClassName(duk_context* ctx);
 	static int getElementsByTagName(duk_context* ctx);
 	static int hasAttribute(duk_context* ctx);
 	static int removeAttribute(duk_context* ctx);
-	static int innerHTML(duk_context* ctx);
-	static int outerHTML(duk_context* ctx);
+	
+	
 	static int querySelector(duk_context* ctx);
 	static int querySelectorAll(duk_context* ctx);
-	static int scrollHeight(duk_context* ctx);
+	
 	static int scrollIntoView(duk_context* ctx);
-	static int scrollLeft(duk_context* ctx);
-	static int scrollTop(duk_context* ctx);
-	static int scrollWidth(duk_context* ctx);
+	
+	
+	
 	static int setAttribute(duk_context* ctx);
 	static int tagName(duk_context* ctx);
 };
