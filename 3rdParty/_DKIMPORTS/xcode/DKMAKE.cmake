7da12065df59b76afdde3e8b938aaa4f8d175d16
aquawicket <aquawicket@hotmail.com>
2021-10-30 15:56:21 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/xcode/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/xcode/DKMAKE.cmake
@@ -1,10 +1,12 @@
+# https://developer.apple.com/xcode/
+# https://pewpewthespells.com/blog/buildsettings.html
+
 ### VERSION ###
 #DKSET(XCODE_VERSION ???)
 #DKSET(XCODE_NAME ???)
 #DKSET(XCODE_DL ???)
 #DKSET(XCODE ???)
 #DKSET(XCODE_EXE ???)
-
 
 ### INSTALL ###
 #DKINSTALL(${XCODE_DL} Xcode ${XCODE})
