88e6b596663a89be7434c52aeb09ff5bb1502ae4
aquawicket <aquawicket@hotmail.com>
2021-11-02 16:09:35 -0700

    commit from git

--- DKCMake/DISABLED.cmake
+++ DKCMake/DISABLED.cmake
@@ -150,6 +150,7 @@ if(CMAKE_HOST_APPLE)  # Disabled on Apple host machines
 	#DKDISABLE(sdl2_gif)
 	DKDISABLE(sdl2_mixer)
 	DKDISABLE(sfml)
+	DKDISABLE(smpeg2)
 	DKDISABLE(tidy-html5)
 	DKDISABLE(tiff)
 	DKDISABLE(upx)
