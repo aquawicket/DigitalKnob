7d0f18c50d4f233ce13d982d419ea3787f6c8c6c
aquawicket <aquawicket@hotmail.com>
2021-11-02 09:18:52 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/visualstudio/VSInstall.cmd
+++ 3rdParty/_DKIMPORTS/visualstudio/VSInstall.cmd
@@ -1,3 +1,16 @@
 set "DKDOWNLOAD=C:\Users\%USERNAME%\digitalknob\Download"
 
-%DKDOWNLOAD%\vs_Community.exe --layout %DKDOWNLOAD%\vslayout --add Microsoft.VisualStudio.Component.CoreEditor Microsoft.VisualStudio.Workload.CoreEditor Microsoft.VisualStudio.Component.Roslyn.Compiler Microsoft.Component.MSBuild Microsoft.VisualStudio.Component.TextTemplating Microsoft.VisualStudio.Component.VC.CoreIde Microsoft.VisualStudio.Component.VC.Tools.x86.x64 Microsoft.VisualStudio.Component.Windows10SDK.19041 Microsoft.VisualStudio.Component.VC.Redist.14.Latest Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core Microsoft.VisualStudio.Workload.NativeDesktop --lang en-US
+start /wait %DKDOWNLOAD%\vs_community__621291036.1634852628.exe --layout "%DKDOWNLOAD%\vslayout" ^
+--add Microsoft.VisualStudio.Component.CoreEditor ^
+--add Microsoft.VisualStudio.Workload.CoreEditor ^
+--add Microsoft.VisualStudio.Component.Roslyn.Compiler ^
+--add Microsoft.Component.MSBuild ^
+--add Microsoft.VisualStudio.Component.TextTemplating ^
+--add Microsoft.VisualStudio.Component.VC.CoreIde ^
+--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 ^
+--add Microsoft.VisualStudio.Component.Windows10SDK.19041 ^
+--add Microsoft.VisualStudio.Component.VC.Redist.14.Latest ^
+--add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core ^
+--add Microsoft.VisualStudio.Workload.NativeDesktop ^
+--lang en-US --passive --wait > nul
+echo %errorlevel%
