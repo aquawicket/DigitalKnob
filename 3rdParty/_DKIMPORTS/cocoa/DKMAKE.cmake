869f568e249c0418ec39c24388059bcdd6afa1c2
aquawicket <aquawicket@hotmail.com>
2021-11-02 21:01:45 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/cocoa/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/cocoa/DKMAKE.cmake
@@ -2,6 +2,6 @@
 
 FIND_LIBRARY(cocoa_lib Cocoa)
 if(NOT ${cocoa_lib})
-	message(FATAL_ERROR "Could not locate Cocoa framework")
+	#message(FATAL_ERROR "Could not locate Cocoa framework")
 endif()
 APPLE_LIB(${cocoa_lib})
