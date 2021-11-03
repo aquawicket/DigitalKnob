8f5e8e3f7f31ffdddab43112d8bcd9dc599f54ae
aquawicket <aquawicket@hotmail.com>
2021-11-03 08:29:03 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/uikit/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/uikit/DKMAKE.cmake
@@ -2,6 +2,6 @@
 
 FIND_LIBRARY(uikit_lib UIKit)
 if(NOT ${uikit_lib})
-	message(FATAL_ERROR "Could not locate UIKit framework")
+	#message(FATAL_ERROR "Could not locate UIKit framework")
 endif()
 APPLE_LIB(${uikit_lib})
