4f2e1dfbf619dc6684d6f8a77ec7221eda8c92ce
aquawicket <aquawicket@hotmail.com>
2021-11-02 19:36:37 -0700

    commit from git

---
+++ 3rdParty/_DKIMPORTS/core_services/DKMAKE.cmake
@@ -0,0 +1,7 @@
+# https://developer.apple.com/documentation/coregraphics?language=objc
+
+FIND_LIBRARY(core_services_lib CoreServices)
+if(NOT ${coreservices_lib})
+	#message(FATAL_ERROR "Could not locate CoreServices framework")
+endif()
+APPLE_LIB(${core_services_lib})
