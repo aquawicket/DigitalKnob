10a52b0bdb9c70be0b854a03de6fd6e1a585129c
aquawicket <aquawicket@hotmail.com>
2021-11-05 07:11:23 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/libgtk2.0-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libgtk2.0-dev/DKMAKE.cmake
@@ -4,7 +4,7 @@ DKCOMMAND(sudo apt -y install libglib2.0-dev)
 
 
 find_package(PkgConfig REQUIRED)
-pkg_check_modules(GLIB2 REQUIRED glib-2.0)
+pkg_check_modules(GLIB REQUIRED glib-2.0)
 
 DKINCLUDE(${GLIB_INCLUDE_DIRS})
 DK_LIB(${GLIB_LIBRARIES})
