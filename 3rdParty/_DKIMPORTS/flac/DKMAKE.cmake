c88ca5a5c5a915766af2b0915a896c04bc4750fb
aquawicket <aquawicket@hotmail.com>
2021-11-05 15:49:09 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/flac/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/flac/DKMAKE.cmake
@@ -24,16 +24,87 @@ APPLE_DEBUG_LIB(${FLAC}/${OS}/${DEBUG_DIR}/libflac.a)
 APPLE_RELEASE_LIB(${FLAC}/${OS}/${RELEASE_DIR}/libflac.a)
 LINUX_DEBUG_LIB(${FLAC}/${OS}/${DEBUG_DIR}/libflac.a)
 LINUX_RELEASE_LIB(${FLAC}/${OS}/${RELEASE_DIR}/libflac.a)
-ANDROID_DEBUG_LIB(${FLAC}/${OS}/$(LIBDIR)/libflac.a)
-ANDROID_RELEASE_LIB(${FLAC}/${OS}/$(LIBDIR)/libflac.a)
+ANDROID_DEBUG_LIB(${FLAC}/${OS}/$(DEBUG_DIR}/libflac.a)
+ANDROID_RELEASE_LIB(${FLAC}/${OS}/${RELEASE_DIR}/libflac.a)
+
+
+### 3rd Party Link ###
+DKSET(FLAC_WIN -DFLAC_INCLUDE_DIR=${FLAC}/include -DFLAC_LIBRARY_DEBUG=${FLAC}/${OS}/${DEBUG_DIR}/flac.lib -DFLAC_LIBRARY_RELEASE=${FLAC}/${OS}/${RELEASE_DIR}/flac.lib)
+DKSET(FLAC_APPLE -DFLAC_INCLUDE_DIR=${FLAC}/include -DFLAC_LIBRARY_DEBUG=${FLAC}/${OS}/${DEBUG_DIR}/flac.lib -DFLAC_LIBRARY_RELEASE=${FLAC}/${OS}/${RELEASE_DIR}/flac.lib)
+DKSET(FLAC_LINUX_DEBUG -DFLAC_INCLUDE_DIR=${FLAC}/include -DFLAC_LIBRARY=${FLAC}/${OS}/${DEBUG_DIR}/libflac.a)
+DKSET(FLAC_LINUX_RELEASE -DFLAC_INCLUDE_DIR=${FLAC}/include -DFLAC_LIBRARY=${FLAC}/${OS}/${RELEASE_DIR}/libflac.a)
+DKSET(FLAC_RASPBERRY_DEBUG -DFLAC_INCLUDE_DIR=${FLAC}/include -DFLAC_LIBRAR=${FLAC}/${OS}/${DEBUG_DIR}/libflac.a)
+DKSET(FLAC_RASPBERRY_RELEASE -DFLAC_INCLUDE_DIR=${FLAC}/include -DFLAC_LIBRARY=${FLAC}/${OS}/${RELEASE_DIR}/libflac.a)
+DKSET(FLAC_ANDROID -DFLAC_INCLUDE_DIR=${FLAC}/include -DFLAC_LIBRARY_DEBUG=${FLAC}/${OS}/${DEBUG_DIR}/libflac.a -DFLAC_LIBRARY_RELEASE=${FLAC}/${OS}/${RELEASE_DIR}/libflac.a)
+
+
 
 
 
 ### COMPILE ###
-LINUX_PATH(${FLAC}/${OS}/${DEBUG_DIR})
+WIN32_PATH(${FLAC}/${OS}/${DEBUG_DIR})
+WIN32_BASH("#!/bin/bash 
+cd /${FLAC}/${OS}/${DEBUG_DIR} 
+export PATH=/${MINGW32}/bin:$PATH\;
+export PATH=/${MSYS}/bin:$PATH\;
+../../configure --disable-shared
+make 
+exit \n")
+WIN32_PATH(${FLAC}/${OS}/${RELEASE_DIR})
+WIN32_BASH("#!/bin/bash 
+cd /${FLAC}/${OS}/${RELEASE_DIR} 
+export PATH=/${MINGW32}/bin:$PATH\;
+export PATH=/${MSYS}/bin:$PATH\;
+../../configure --disable-shared
+make 
+exit \n")
+
+
+WIN64_PATH(${FLAC}/${OS}/${DEBUG_DIR})
+WIN64_BASH("#!/bin/bash 
+cd /${FLAC}/${OS}/${DEBUG_DIR} 
+export PATH=/${MINGW64}/bin:$PATH\;
+export PATH=/${MSYS}/bin:$PATH\;
+../../configure --disable-shared
+make 
+exit \n")
+WIN64_PATH(${FLAC}/${OS}/${RELEASE_DIR})
+WIN64_BASH("#!/bin/bash 
+cd /${FLAC}/${OS}/${RELEASE_DIR} 
+export PATH=/${MINGW64}/bin:$PATH\;
+export PATH=/${MSYS}/bin:$PATH\;
+../../configure --disable-shared
+make 
+exit \n")
+
+
+MAC_DEBUG_PATH(${FLAC}/${OS}/${DEBUG_DIR})
+MAC_DEBUG_COMMAND(../../configure --disable-shared --enable-static)
+MAC_DEBUG_COMMAND(make)
+MAC_RELEASE_PATH(${FLAC}/${OS}/${RELEASE_DIR})
+MAC_RELEASE_COMMAND(../../configure --disable-shared --enable-static)
+MAC_RELEASE_COMMAND(make)
+
+
+LINUX_DEBUG_PATH(${FLAC}/${OS}/${DEBUG_DIR})
 LINUX_DEBUG_COMMAND(../../configure --disable-shared --enable-static)
 LINUX_DEBUG_COMMAND(make)
-
-DKSETPATH(${FLAC}/${OS}/${RELEASE_DIR})
+LINUX_RELEASE_PATH(${FLAC}/${OS}/${RELEASE_DIR})
 LINUX_RELEASE_COMMAND(../../configure --disable-shared --enable-static)
 LINUX_RELEASE_COMMAND(make)
+
+
+RASPBERRY_DEBUG_PATH(${FLAC}/${OS}/${DEBUG_DIR})
+RASPBERRY_DEBUG_COMMAND(../../configure --disable-shared --enable-static)
+RASPBERRY_DEBUG_COMMAND(make)
+RASPBERRY_RELEASE_PATH(${FLAC}/${OS}/${RELEASE_DIR})
+RASPBERRY_RELEASE_COMMAND(../../configure --disable-shared --enable-static)
+RASPBERRY_RELEASE_COMMAND(make)
+
+
+ANDROID_DEBUG_PATH(${FLAC}/${OS}/${DEBUG_DIR})
+ANDROID_DEBUG_COMMAND(../../configure --disable-shared --enable-static)
+ANDROID_DEBUG_COMMAND(make)
+ANDROID_RELEASE_PATH(${FLAC}/${OS}/${RELEASE_DIR})
+ANDROID_RELEASE_COMMAND(../../configure --disable-shared --enable-static)
+ANDROID_RELEASE_COMMAND(make)
