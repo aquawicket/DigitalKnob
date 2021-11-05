4806a6fa9ae28240f77aeba69e19fb59dd1a19ea
aquawicket <aquawicket@hotmail.com>
2021-11-05 07:00:43 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/libgtk2.0-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libgtk2.0-dev/DKMAKE.cmake
@@ -8,6 +8,3 @@ pkg_check_modules(GLIB2 REQUIRED glib-2.0)
 
 DKINCLUDE(${GLIB_INCLUDE_DIRS})
 DK_LIB(${GLIB_LIBRARIES})
-
-#INCLUDES -pthread -I/usr/include/gtk-2.0 -I/usr/lib/arm-linux-gnueabihf/gtk-2.0/include -I/usr/include/gio-unix-2.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libmount -I/usr/include/blkid -I/usr/include/pango-1.0 -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/glib-2.0 -I/usr/lib/arm-linux-gnueabihf/glib-2.0/include -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16
-#LIBRARIES -lgtk-x11-2.0 -lgdk-x11-2.0 -lpangocairo-1.0 -latk-1.0 -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lpangoft2-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lfontconfig -lfreetype
