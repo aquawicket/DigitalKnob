5a9542d8d473bfbd86bdba5aa36d68b74bcbc2e1
aquawicket <aquawicket@hotmail.com>
2021-11-04 21:34:26 -0700

    commit from git

--- DKCMake/PROCESS.cmake
+++ DKCMake/PROCESS.cmake
@@ -579,14 +579,14 @@ if(MAC)
 	
 	# Copy the DKCefChild into the app bundle
 	#if(EXISTS "${DKPLUGINS}/DKCefChild/${OS}/$<CONFIG>/DKCefChild.app")
-	#	message(STATUS "Adding Chromium Embedded Framework.framework to bundle . . .")
-	#	add_custom_command(
-	#		TARGET ${APP_NAME}
-	#		POST_BUILD
-	#		COMMAND ${CMAKE_COMMAND} -E copy
-	#				"${DKPLUGINS}/DKCefChild/${OS}/$<CONFIG>/DKCefChild.app"
-	#				"$<TARGET_FILE_DIR:${APP_NAME}>/../Frameworks/${APP_NAME} Helper.app"
-	#	)
+		message(STATUS "Adding Chromium Embedded Framework.framework to bundle . . .")
+		add_custom_command(
+			TARGET ${APP_NAME}
+			POST_BUILD
+			COMMAND ${CMAKE_COMMAND} -E copy
+				"${DKPLUGINS}/DKCefChild/${OS}/$<CONFIG>/DKCefChild.app"
+				"$<TARGET_FILE_DIR:${APP_NAME}>/../Frameworks/${APP_NAME} Helper.app"
+		)
 	#endif()
 	
 	# Make bundle run in Terminal
