e49a484b038ebb79112aa09ee42da10cef9123cb
aquawicket <aquawicket@hotmail.com>
2021-11-05 07:13:46 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/libglib2.0-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libglib2.0-dev/DKMAKE.cmake
@@ -1,13 +1,10 @@
 ### INSTALL ###
 DKSET(CURRENT_DIR /usr)
-DKCOMMAND(sudo apt -y install libgtk2.0-dev)
+DKCOMMAND(sudo apt -y install libglib2.0-dev)
 
 
 find_package(PkgConfig REQUIRED)
-pkg_check_modules(GTK2 REQUIRED gtk+-2.0)
+pkg_check_modules(GLIB REQUIRED glib-2.0)
 
-DKINCLUDE(${GTK2_INCLUDE_DIRS})
-DK_LIB(${GTK2_LIBRARIES})
-
-#INCLUDES -pthread -I/usr/include/gtk-2.0 -I/usr/lib/arm-linux-gnueabihf/gtk-2.0/include -I/usr/include/gio-unix-2.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libmount -I/usr/include/blkid -I/usr/include/pango-1.0 -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/glib-2.0 -I/usr/lib/arm-linux-gnueabihf/glib-2.0/include -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16
-#LIBRARIES -lgtk-x11-2.0 -lgdk-x11-2.0 -lpangocairo-1.0 -latk-1.0 -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lpangoft2-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lfontconfig -lfreetype
+DKINCLUDE(${GLIB_INCLUDE_DIRS})
+DK_LIB(${GLIB_LIBRARIES})
--- 3rdParty/_DKIMPORTS/libgtk2.0-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libgtk2.0-dev/DKMAKE.cmake
@@ -1,10 +1,10 @@
 ### INSTALL ###
 DKSET(CURRENT_DIR /usr)
-DKCOMMAND(sudo apt -y install libglib2.0-dev)
+DKCOMMAND(sudo apt -y install libgtk2.0-dev)
 
 
 find_package(PkgConfig REQUIRED)
-pkg_check_modules(GLIB REQUIRED glib-2.0)
+pkg_check_modules(GTK REQUIRED gtk+-2.0)
 
-DKINCLUDE(${GLIB_INCLUDE_DIRS})
-DK_LIB(${GLIB_LIBRARIES})
+DKINCLUDE(${GTK_INCLUDE_DIRS})
+DK_LIB(${GTK_LIBRARIES})
