28578ef431a795384fae5bfb6b6a0b00de3920a5
aquawicket <aquawicket@hotmail.com>
2021-11-04 22:57:50 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/libx11-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libx11-dev/DKMAKE.cmake
@@ -3,7 +3,6 @@ DKSET(CURRENT_DIR /usr)
 DKCOMMAND(sudo apt -y install libx11-dev)
 
 
-
 ### LINK ###
 DKINCLUDE(/usr/include/X11)
 SET(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE} -lX11")
