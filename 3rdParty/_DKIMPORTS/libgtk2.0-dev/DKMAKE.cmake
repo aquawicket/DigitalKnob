bdf8f798f6583caa9ce3ff0858e49ce6fb473c78
aquawicket <aquawicket@hotmail.com>
2021-11-06 18:40:00 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/libgtk2.0-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libgtk2.0-dev/DKMAKE.cmake
@@ -10,6 +10,7 @@ DKCOMMAND(sudo apt -y install libgtk2.0-dev)
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(GTK REQUIRED gtk+-2.0)
 
+DKDEFINE(USE_GDK)
 #message(STATUS "GTK_INCLUDE_DIRS = ${GTK_INCLUDE_DIRS}")
 DKINCLUDE(${GTK_INCLUDE_DIRS})
 #message(STATUS "GTK_LIBRARIES = ${GTK_LIBRARIES}")
