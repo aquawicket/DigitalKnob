0c125c5e2d2552db4614587001a8d572e8336603
aquawicket <aquawicket@hotmail.com>
2021-11-08 14:31:36 -0800

    commit from git

---
+++ 3rdParty/macports/DKMAKE.cmake
@@ -0,0 +1,4 @@
+# https://github.com/macports/macports-base
+# https://github.com/macports/macports-base/releases
+#
+# https://github.com/macports/macports-base/releases/download/v2.7.1/MacPorts-2.7.1-11-BigSur.pkg
