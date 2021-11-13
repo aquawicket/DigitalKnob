5a9bad11e3155c94c5c481b4a024af7de4939fa2
aquawicket <aquawicket@hotmail.com>
2021-11-13 13:25:16 -0800

    git commit

--- 3rdParty/_DKIMPORTS/astyle/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/astyle/DKMAKE.cmake
@@ -6,7 +6,7 @@
 ### VERSION ###
 DKSET(ASTYLE_VERSION 3.1)
 DKSET(ASTYLE_NAME AStyle_${ASTYLE_VERSION}_windows)
-DKSET(ASTYLE_DL https://managedway.dl.sourceforge.net/project/astyle/astyle/astyle%20${ASTYLE_VERSION}/${ASTYLE_NAME}.zip)
+DKSET(ASTYLE_DL https://sourceforge.net/projects/astyle/files/astyle/astyle%203.1/AStyle_3.1_windows.zip)
 DKSET(ASTYLE ${3RDPARTY}/${ASTYLE_NAME})
 
 
