ff1b09cb93ec905729dcb014f4638d35a6e8a8dd
aquawicket <aquawicket@hotmail.com>
2021-11-13 17:00:43 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/core_graphics/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/core_graphics/DKMAKE.cmake
@@ -5,7 +5,7 @@ endif()
 # https://developer.apple.com/documentation/coregraphics?language=objc
 
 FIND_LIBRARY(core_graphics_lib CoreGraphics)
-if(NOT coregraphics_lib)
+if(NOT core_graphics_lib_lib)
 	message(FATAL_ERROR "Could not locate CoreGraphics framework")
 endif()
 APPLE_LIB(${core_graphics_lib})
