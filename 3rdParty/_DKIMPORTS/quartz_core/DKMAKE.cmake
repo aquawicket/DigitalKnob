bedc447b7088c928459feac76236920931226380
aquawicket <aquawicket@hotmail.com>
2021-11-16 06:32:51 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/quartz_core/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/quartz_core/DKMAKE.cmake
@@ -4,8 +4,8 @@ endif()
 
 # https://developer.apple.com/documentation/quartzcore
 
-FIND_LIBRARY(quartz_core_lib QuartzCore)
-if(NOT quartz_core_lib)
+FIND_LIBRARY(QuartzCore_lib QuartzCore)
+if(NOT QuartzCore_lib)
 	message(FATAL_ERROR "Could not locate QuartzCore framework")
 endif()
-APPLE_LIB(${quartz_core_lib})
+APPLE_LIB(${QuartzCore_lib})
