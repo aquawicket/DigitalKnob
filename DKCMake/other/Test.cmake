5fa923ec8b64f25400a0a4bec95e067f62513632
aquawicket <aquawicket@hotmail.com>
2021-11-17 17:21:17 -0800

    commit from git

--- DKCMake/other/Test.cmake
+++ DKCMake/other/Test.cmake
@@ -1,7 +1,8 @@
 message(STATUS "running Test.cmake . . .")
 message(STATUS "CMAKE_SOURCE_DIR = ${CMAKE_SOURCE_DIR}")
-message(STATUS "include(${CMAKE_SOURCE_DIR}/../FUNCTIONS.cmake)")
-include(DKCMake/FUNCTIONS.cmake)
+get_filename_component(DKCMAKE "${CMAKE_SOURCE_DIR}/.." ABSOLUTE)
+include(${DKCMAKE}/FUNCTIONS.cmake)
+message(STATUS "Loaded Functions.cmake\n")
 
 
 message(STATUS "dk_getCurrentDirectory()")
