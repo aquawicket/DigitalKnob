fb3ec5f4bab746227c61338746b0f503873af3d8
aquawicket <aquawicket@hotmail.com>
2021-11-20 07:23:27 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/xquartz/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/xquartz/DKMAKE.cmake
@@ -1,3 +1,15 @@
 # https://www.xquartz.org/
 #
 # https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.8.1/XQuartz-2.8.1.dmg
+
+DKSET(XQUARTZ_VERSION 2.8.1)
+DKSET(XQUARTZ_NAME XQuartz-${XQUARTZ_VERSION})
+DKSET(XQUARTZ_DL https://github.com/XQuartz/XQuartz/releases/download/${XQUARTZ_NAME}/${XQUARTZ_NAME}.dmg)
+DKSET(XQUARTZ FIXME)
+
+### INSTALL ###
+IF(NOT EXISTS ${XQUARTZ})
+	DOWNLOAD(${XQUARTZ_DL} ${DKDOWNLOAD}/${XQUARTZ_NAME}.dmg)
+	DKSET(QUEUE_BUILD ON)
+	MAC_COMMAND(${DKDOWNLOAD}/${XQUARTZ_NAME}.dmg)
+ENDIF()
