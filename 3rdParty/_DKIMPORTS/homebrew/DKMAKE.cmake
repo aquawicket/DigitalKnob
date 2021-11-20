e7eb27fa2b2c0453716ec7969e14bcf5a11940a9
aquawicket <aquawicket@hotmail.com>
2021-11-20 08:25:23 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/homebrew/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/homebrew/DKMAKE.cmake
@@ -8,5 +8,5 @@ endif()
 
 MAC_COMMAND("ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"")
 # https://github.com/Homebrew/brew/issues/10368
-MAC_COMMAND("rm -fr $(brew --repo homebrew/core)")
-MAC_COMMAND("brew tap homebrew/core")
+MAC_COMMAND(rm -fr "$(brew --repo homebrew/core)")
+MAC_COMMAND(brew tap homebrew/core)
