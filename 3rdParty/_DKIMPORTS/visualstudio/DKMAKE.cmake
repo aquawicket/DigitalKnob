b4e1b0bcdb5f336da33e862936d93e72eb07aa9f
aquawicket <aquawicket@hotmail.com>
2021-11-20 01:36:06 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/visualstudio/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/visualstudio/DKMAKE.cmake
@@ -6,6 +6,7 @@ endif()
 # https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild?view=vs-2019
 # https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-command-line-reference?view=vs-2019
 # https://docs.microsoft.com/en-us/visualstudio/msbuild/common-msbuild-project-properties?view=vs-2019
+# https://cmake.org/cmake/help/v3.21/generator/Visual%20Studio%2017%202022.html
 #
 # Downloads
 # https://download.visualstudio.microsoft.com/download/pr/5e397ebe-38b2-4e18-a187-ac313d07332a/169156e6e9a005d49b357c42240184dc1e3ccc28ebc777e70d49257c074f77e8/vs_Community.exe
@@ -18,16 +19,18 @@ if(ANDROID)
 endif()
  
 ### VERSION ###
-DKSET(VISUALSTUDIO_BUILD 16)
+#DKSET(VISUALSTUDIO_BUILD 16)
+DKSET(VISUALSTUDIO_BUILD 17)
 #DKSET(VISUALSTUDIO_VERSION 2019)
 DKSET(VISUALSTUDIO_VERSION 2022)
-DKSET(VISUALSTUDIO_NAME "Visual Studio ${VISUALSTUDIO_BUILD} ${VISUALSTUDIO_VERSION}")
+#DKSET(VISUALSTUDIO_NAME "Visual Studio ${VISUALSTUDIO_BUILD} ${VISUALSTUDIO_VERSION}")
 DKSET(VISUALSTUDIO_DL https://download.visualstudio.microsoft.com/download/pr/5e397ebe-38b2-4e18-a187-ac313d07332a/169156e6e9a005d49b357c42240184dc1e3ccc28ebc777e70d49257c074f77e8/vs_Community.exe)
 DKSET(VISUALSTUDIO "C:/Program Files/Microsoft Visual Studio/${VISUALSTUDIO_VERSION}/Community")
 DKSET(MSBUILD "${VISUALSTUDIO}/MSBuild/Current/Bin/MSBuild.exe")
 
 #DKSET(GENERATOR "Visual Studio 16 ${VISUALSTUDIO_VERSION}")  #2019
-DKSET(GENERATOR "Visual Studio 17 ${VISUALSTUDIO_VERSION}")  #2022
+#DKSET(GENERATOR "Visual Studio 17 ${VISUALSTUDIO_VERSION}")  #2022
+DKSET(GENERATOR "Visual Studio ${VISUALSTUDIO_BUILD} ${VISUALSTUDIO_VERSION}")
 
 
 
