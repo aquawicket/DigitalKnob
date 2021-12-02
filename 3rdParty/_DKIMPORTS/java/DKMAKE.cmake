cd43622b9b047c627a46d903924f03fa3e9be0ff
aquawicket <aquawicket@hotmail.com>
2021-12-01 19:54:08 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/java/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/java/DKMAKE.cmake
@@ -2,12 +2,11 @@
 # https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245029_d3c52aa6bfa54d3ca74e617f18309292
 # https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245058_d3c52aa6bfa54d3ca74e617f18309292
 # https://sdlc-esd.oracle.com/ESD6/JSCDL/jdk/8u301-b09/d3c52aa6bfa54d3ca74e617f18309292/jre-8u301-windows-i586.exe?GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/8u301-b09/d3c52aa6bfa54d3ca74e617f18309292/jre-8u301-windows-i586.exe&BHost=javadl.sun.com&File=jre-8u301-windows-i586.exe&AuthParam=1631595621_2291054da58bdda192ed0500e141024c&ext=.exe
-# https://www.oracle.com/java/technologies/javase-jre8-downloads.html#license-lightbox
 
 ### VERSION ###
 #DKSET(JAVA_VERSION ???)
 #DKSET(JAVA_NAME java-${JDK_VERSION})
-#DKSET(JAVA_DL ???)
+#DKSET(JAVA_DL https://us.softpedia-secure-download.com/dl/b98e1543ee54316ef787e417cb87ac67/61a84020/400123332/mac/System-Utilities/jre-8u311-macosx-x64.dmg)
 #DKSET(JAVA ${3RDPARTY}/${JAVA_NAME})
 
 
