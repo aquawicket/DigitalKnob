54ac53cc2237f07145af37fd9a4f1fb9d77fe873
aquawicket <aquawicket@hotmail.com>
2021-12-02 00:42:31 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/openssl/getCert.cmd
+++ 3rdParty/_DKIMPORTS/openssl/getCert.cmd
@@ -13,13 +13,15 @@ if exist "C:\Windows\System32\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:
 if exist "C:\Program Files (x86)\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:\Program Files (x86)\OpenSSL-win32\bin\openssl.exe"
 if exist "C:\Program Files\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:\Program Files\OpenSSL-win32\bin\openssl.exe"
 if exist "C:\Users\Administrator\digitalknob\DK\3rdParty\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:\Users\Administrator\digitalknob\DK\3rdParty\OpenSSL-win32\bin\openssl.exe"
-
-if(EXISTS C:\Users\Administrator\digitalknob\DK\3rdParty\OpenSSL-win32\bin\openssl.exe)
-set OPENSSL_EXE="C:\Program Files (x86)\OpenSSL-Win32\bin\openssl.exe"
-set GOOGLE_CER="C:\Program Files (x86)\OpenSSL-Win32\bin\google.cer"
+set GOOGLE_CER="C:\Users\Administrator\digitalknob\DK\3rdParty\openjdk-9.0.4_windows-x64_bin\google.cer"
+set MAVEN_CER="C:\Users\Administrator\digitalknob\DK\3rdParty\openjdk-9.0.4_windows-x64_bin\maven.cer"
 set KEYTOOL_EXE="C:\Users\Administrator\digitalknob\DK\3rdParty\openjdk-9.0.4_windows-x64_bin\bin\keytool.exe"
 set CACERTS="C:\Users\Administrator\digitalknob\DK\3rdParty\openjdk-9.0.4_windows-x64_bin\lib\security\cacerts"
 
 echo -n | %OPENSSL_EXE% s_client -connect google.com:443 | %OPENSSL_EXE% x509 > %GOOGLE_CER%
 %KEYTOOL_EXE% -import -alias google -keystore %CACERTS% -file %GOOGLE_CER%
+
+echo -n | %OPENSSL_EXE% s_client -connect maven.apache.org:443 | %OPENSSL_EXE% x509 > %MAVEN_CER%
+%KEYTOOL_EXE% -import -alias maven -keystore %CACERTS% -file %MAVEN_CER%
+
 echo press and key to continue && timeout /t 60 > nul
