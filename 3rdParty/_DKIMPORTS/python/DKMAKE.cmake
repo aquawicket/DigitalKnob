9adbdf3264a30e01f86912fec7f714cab140a9c7
aquawicket <aquawicket@hotmail.com>
2021-12-03 03:07:54 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/python/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/python/DKMAKE.cmake
@@ -1,18 +1,34 @@
-# https://www.python.org/ftp/python/2.7.18/python-2.7.18.msi
 # https://docs.python.org/3/using/windows.html
+
+# Windows ttps://www.python.org/ftp/python/2.7.18/python-2.7.18.msi
+# Mac      https://www.python.org/ftp/python/2.7.18/python-2.7.18-macosx10.9.pkg
+# Linux    https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
 
 ### VERSION ###
 DKSET(PYTHON_VERSION 2.7.18)
 DKSET(PYTHON_NAME python-${PYTHON_VERSION})
 WIN_DKSET(PYTHON_DL https://www.python.org/ftp/python/${PYTHON_VERSION}/${PYTHON_NAME}.msi)
+MAC_DKSET(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18-macosx10.9.pkg)
+LINUX_DKSET(PYTHON_DL https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
 DKSET(PYTHON ${3RDPARTY}/${PYTHON_NAME})
 WIN_DKSET(PYTHON_EXE ${PYTHON}/python.exe)
-
+MAC_DKSET(PYTHON_APP ${PYTHON}/python.app)
+LINUX_DKSET(PYTHON ${PYTHON}/python)
 
 ### INSTALL ###
-IF(NOT EXISTS ${PYTHON_EXE})
+IF(WIN AND NOT EXISTS ${PYTHON_EXE})
 	DOWNLOAD(${PYTHON_DL} ${DKDOWNLOAD}/${PYTHON_NAME}.msi)
 	string(REPLACE "/" "\\" PYTHON_PATH ${PYTHON})
 	DKCOMMAND(${DKDOWNLOAD}/${PYTHON_NAME}.msi /passive PrependPath=1 TargetDir=${PYTHON_PATH})
 	DKSETENV("PATH" "${PYTHON}")
+ENDIF()
+
+IF(MAC AND NOT EXISTS ${PYTHON_MAC})
+	DOWNLOAD(${PYTHON_DL} ${DKDOWNLOAD}/python-2.7.18-macosx10.9.pkg)
+	#TODO
+ENDIF()
+
+IF(LINUX AND NOT EXISTS ${PYTHON})
+	DOWNLOAD(${PYTHON_DL} ${DKDOWNLOAD}/Python-2.7.18.tgz)
+	#TODO
 ENDIF()
