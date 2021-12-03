f0807191f094c2e1816c9a4b38ddbcbc9aeab59f
aquawicket <aquawicket@hotmail.com>
2021-12-02 23:52:26 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/audiounit/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/audiounit/DKMAKE.cmake
@@ -6,6 +6,8 @@ endif()
 
 FIND_LIBRARY(AudioUnit_lib AudioUnit)
 if(NOT AudioUnit_lib)
+	APPLE_LIB(${AudioUnit_lib})
+else()
 	message(STATUS "Could not locate AudioUnit framework")
 endif()
-APPLE_LIB(${AudioUnit_lib})
+
