dcc1204b66640b84434abd29744043b61a6abefd
aquawicket <aquawicket@hotmail.com>
2021-12-06 22:09:57 -0800

    commit from git

---
+++ 3rdParty/_DKIMPORTS/metal-cpp/DKMAKE.cmake
@@ -0,0 +1,15 @@
+if(NOT CMAKE_HOST_APPLE)
+	return()
+endif()
+
+# https://developer.apple.com/metal/cpp/
+# https://developer.apple.com/metal/cpp/files/metal-cpp_macOS12_iOS15.zip
+
+### VERSION ###
+DKSET(METALCPP_NAME metal-cpp_macOS12_iOS15)
+DKSET(METALCPP_DL https://developer.apple.com/metal/cpp/files/metal-cpp_macOS12_iOS15.zip)
+DKSET(METALCPP ${3RDPARTY}/${METALCPP_NAME})
+
+
+### INSTALL ###
+DKINSTALL(${METALCPP_DL} metal-cpp ${METALCPP})
