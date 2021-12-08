9c936576cfd8d406aa349caa209aaf353926bc5a
aquawicket <aquawicket@hotmail.com>
2021-12-07 19:38:11 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/android-ndk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-ndk/DKMAKE.cmake
@@ -2,16 +2,23 @@ if(NOT CMAKE_HOST_WIN32)
 	return()
 endif()
 
-# https://dl.google.com/android/repository/android-ndk-r21e-windows-x86_64.zip
-# https://dl.google.com/android/repository/android-ndk-r21e-darwin-x86_64.dmg
-# https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip
+# https://androidsdkoffline.blogspot.com/p/android-ndk-side-by-side-direct-download.html
+# https://dl.google.com/android/repository/android-ndk-r21e-windows-x86_64.zip LTS
+# https://dl.google.com/android/repository/android-ndk-r21e-darwin-x86_64.dmg LTS
+# https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip LTS
 
 ### VERSION ###
-DKSET(ANDROIDNDK_VERSION r22b)
-DKSET(ANDROIDNDK_BUILD 22.1.7171670)
-WIN_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r22b-windows-x86_64.zip)
-MAC_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r22b-darwin-x86_64.zip)
-LINUX_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r22b-linux-x86_64.zip)
+DKSET(ANDROIDNDK_VERSION r21e)
+DKSET(ANDROIDNDK_BUILD 21.4.7075529)
+WIN_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r21e-windows-x86_64.zip)
+MAC_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r21e-darwin-x86_64.zip)
+LINUX_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip)
+
+#DKSET(ANDROIDNDK_VERSION r22b)
+#DKSET(ANDROIDNDK_BUILD 22.1.7171670)
+#WIN_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r22b-windows-x86_64.zip)
+#MAC_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r22b-darwin-x86_64.zip)
+#LINUX_DKSET(ANDROIDNDK_DL https://dl.google.com/android/repository/android-ndk-r22b-linux-x86_64.zip)
 
 #DKSET(ANDROIDNDK_VERSION r23b)
 #DKSET(ANDROIDNDK_BUILD 23.1.7779620)
