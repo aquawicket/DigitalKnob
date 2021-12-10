8d31ff4e627b6f97e64620d92499e10037d7f8ee
aquawicket <aquawicket@hotmail.com>
2021-12-10 00:53:11 -0800

    commit from git

--- DKCMake/PROCESS.cmake
+++ DKCMake/PROCESS.cmake
@@ -105,7 +105,7 @@ foreach(plugin ${dkdepend_list})
 		endif()
 		
 		## Prebuild Plugins switch
-		if(FALSE)
+		if(asdf)
 			DKSET(CURRENT_DIR ${plugin_path}/${OS})
 			dk_makeDirectory(${CURRENT_DIR})
 			if(WIN_32)
