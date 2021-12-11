ea31068f562bf809edd9c4af125ecabc65f0f88b
aquawicket <aquawicket@hotmail.com>
2021-12-11 05:14:15 -0800

    commit from git

--- DKCMake/other/Test.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS "running Test.cmake . . .")
-message(STATUS "CMAKE_SOURCE_DIR = ${CMAKE_SOURCE_DIR}")
-get_filename_component(DKCMAKE "${CMAKE_SOURCE_DIR}/.." ABSOLUTE)
-include(${DKCMAKE}/FUNCTIONS.cmake)
-message(STATUS "Loaded Functions.cmake\n")
-
-
-
-include(C:/Users/Administrator/digitalknob/DK/3rdParty/_DKIMPORTS/far/DKMAKE.cmake)
