eb3faf66ff1abee77e7d2f3d85c1e062c156d9e6
aquawicket <aquawicket@hotmail.com>
2021-12-12 10:06:33 -0800

    commit from git

---
+++ 3rdParty/_DKIMPORTS/libxcursor-dev/DKMAKE.cmake
@@ -0,0 +1,10 @@
+if(NOT CMAKE_HOST_UNIX)
+	return()
+endif()
+
+### INSTALL ###
+DKSET(CURRENT_DIR /usr)
+DKCOMMAND(sudo apt -y install libxcursor-dev)
+
+### LINK ###
+SET(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE} -lXcursor")
--- 3rdParty/_DKIMPORTS/libxrandr-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libxrandr-dev/DKMAKE.cmake
@@ -6,7 +6,5 @@ endif()
 DKSET(CURRENT_DIR /usr)
 DKCOMMAND(sudo apt -y install libxrandr-dev)
 
-
 ### LINK ###
-## DKINCLUDE(/usr/include/X11)
 SET(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE} -lXrandr")
