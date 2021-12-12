0ea70362cb03d92a6947ad69fe4e50030bf15cba
aquawicket <aquawicket@hotmail.com>
2021-12-12 03:18:24 -0800

    git commit

--- 3rdParty/_DKIMPORTS/giflib/lib/stdbool.h
+++ 3rdParty/_DKIMPORTS/giflib/lib/stdbool.h
@@ -8,8 +8,8 @@
 #ifndef __cplusplus
 #ifndef _BOOL
 typedef unsigned char bool;
-static const bool False = 0;
-static const bool True = 1;
+static const bool false = 0;
+static const bool true = 1;
 #endif
 #endif 
 
