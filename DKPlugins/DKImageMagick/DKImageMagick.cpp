b26774cb0b7e282e0662e5f1bc44be3602b1b864
aquawicket <aquawicket@hotmail.com>
2021-12-14 02:14:01 -0800

    commit from git

--- DKPlugins/DKImageMagick/DKImageMagick.cpp
+++ DKPlugins/DKImageMagick/DKImageMagick.cpp
@@ -3,32 +3,22 @@
 #include "DK/DKFile.h"
 
 
-//////////////////////////
-bool DKImageMagick::Init()
-{
+bool DKImageMagick::Init(){
 	//DKCreate("DKImageMagickJS");
 	//DKCreate("DKImageMagickV8");
-	
 	return true;
 }
 
-/////////////////////////
-bool DKImageMagick::End()
-{
+bool DKImageMagick::End(){
 	return true;
 }
 
-
-/////////////////////////////////////////////////////////////////
-bool DKImageMagick::PdfToBmp(DKString& filein, DKString& fileout)
-{
-	DKString ret;
+bool DKImageMagick::PdfToBmp(DKString& filein, DKString& fileout){
+	int ret;
 	return DKUtil::System(DKFile::local_assets+"DKImageMagick/magick.exe convert -density 300 "+filein+" BMP3:"+fileout, ret);
 }
 
-/////////////////////////////////////////////////////////////////
-bool DKImageMagick::PdfToPng(DKString& filein, DKString& fileout)
-{
-	DKString ret;
+bool DKImageMagick::PdfToPng(DKString& filein, DKString& fileout){
+	int ret;
 	return DKUtil::System(DKFile::local_assets+"DKImageMagick/magick.exe convert -density 300 "+filein+" "+fileout, ret);
 }
