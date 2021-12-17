4b17b8f009a0a324a27f0969ed6cad3578c4f33b
aquawicket <aquawicket@hotmail.com>
2021-12-17 09:42:28 -0800

    commit from git

---
+++ 3rdParty/_DKIMPORTS/gl_kit/DKMAKE.cmake
@@ -0,0 +1,11 @@
+if(NOT CMAKE_HOST_APPLE)
+	return()
+endif()
+
+# https://developer.apple.com/documentation/GLKit?language=objc
+
+FIND_LIBRARY(GLKit_Lib GLKit)
+if(NOT GLKit_Lib)
+	message(FATAL_ERROR "Could not locate GLKit framework")
+endif()
+APPLE_LIB(${GLKit_Lib})
