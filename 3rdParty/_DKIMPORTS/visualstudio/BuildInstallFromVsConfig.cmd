413473bc3354f92658e147f1bd4ce35f372b8fb6
aquawicket <aquawicket@hotmail.com>
2021-12-19 01:20:18 -0800

    git commit

--- 3rdParty/_DKIMPORTS/visualstudio/BuildInstallFromVsConfig.cmd
+++ 3rdParty/_DKIMPORTS/visualstudio/BuildInstallFromVsConfig.cmd
@@ -1,20 +1,25 @@
 :: Build a Visual Studio 17 2022 installer from 2022.vsconfig file
-@echo off &setlocal enabledelayedexpansion
-set "vsconfig=2022.vsconfig"
-set "vs_community=vs_community__621291036.1634852628.exe"
+@echo off 
+setlocal enabledelayedexpansion
+
 set "dkdownload=C:\Users\%USERNAME%\digitalknob\Download"
-if not exist "!vsconfig!" (
-	set "errorlevel=The file !vsconfig! does no exist" 
+if not exist "%dkdownload%" (
+	set "errorlevel=The folder %dkdownload% does no exist" 
 	Goto :Error
 )
-if not exist "!vs_community!" (
-	set "errorlevel=The file !vs_community! does no exist" 
+
+set "vsconfig=2022.vsconfig"
+if not exist "%vsconfig%" (
+	set "errorlevel=The file %vsconfig% does no exist" 
 	Goto :Error
 )
-if not exist "!dkdownload!" (
-	set "errorlevel=The folder !dkdownload! does no exist" 
+
+set "vs_community=%dkdownload%\vs_Community.exe"
+if not exist "%vs_community%" (
+	set "errorlevel=The file %vs_community% does no exist" 
 	Goto :Error
 )
+
 set /A i=0
 
 :: Loop through file and store lines into an array
@@ -43,16 +48,16 @@ for /L %%i in (1,1,%length%) do (
 
 :: We have the variables we need, so preform the task
 ::****************************************
-start /wait %dkdownload%\%vs_community% --layout "%dkdownload%\VisualStudio" ^
+start /wait %vs_community% --layout "%dkdownload%\VisualStudio" ^
 for /L %%i in (1,1,%length%) do (
 	--add !array[%%i]! ^
 )
 --lang en-US --passive --wait > nul
-echo %errorlevel%
 ::****************************************
 
 ::****************************************
 :End
+echo errorlevel = %errorlevel%
 echo end
 pause
 exit /b
