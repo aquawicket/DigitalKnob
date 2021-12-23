329974544a9d544e9e264167491f158a1b712fa7
aquawicket <aquawicket@hotmail.com>
2021-12-23 06:23:16 -0800

    commit from git

---
+++ DKCMake/dkbuild.cmd
@@ -0,0 +1,11 @@
+:: Let's ask the use to type in what they want to build
+:: could be an app, library or anything with a DKMAKE.cmake file
+:: We whould also give the option to list targets as well
+
+@echo off
+echo Please type the name of the library, tool or app to build. Then press enter.
+
+
+
+
+pause
