c94b9976294d05906245c212c96d79eb42dd80eb
aquawicket <aquawicket@hotmail.com>
2021-12-24 00:22:23 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/moment.js/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/moment.js/DKMAKE.cmake
@@ -1,12 +1,16 @@
+# https://github.com/moment/moment
+# https://momentjs.com/
+# 
+# https://github.com/moment/moment/raw/2.29.1/moment.js
 # https://momentjs.com/downloads/moment.min.js
 
 ### VERSION ###
-##DKSET(MOMENTJS_MAJOR 0)
-##DKSET(MOMENTJS_MINOR 0)
-##DKSET(MOMENTJS_BUILD 0)
-#DKSET(MOMENTJS_VERSION ???)
-DKSET(MOMENTJS_NAME moment.min)
-DKSET(MOMENTJS_DL https://momentjs.com/downloads/moment.min.js)
+DKSET(MOMENTJS_MAJOR 2)
+DKSET(MOMENTJS_MINOR 29)
+DKSET(MOMENTJS_BUILD 1)
+KSET(MOMENTJS_VERSION ${MOMENTJS_MAJOR}.${MOMENTJS_MINOR}.${MOMENTJS_BUILD})
+DKSET(MOMENTJS_NAME moment.js-${MOMENTJS_VERSION})
+DKSET(MOMENTJS_DL https://github.com/moment/moment/raw/${MOMENTJS_VERSION}/moment.js)
 DKSET(MOMENTJS ${3RDPARTY}/${MOMENTJS_NAME})
 
 ### INSTALL ###
