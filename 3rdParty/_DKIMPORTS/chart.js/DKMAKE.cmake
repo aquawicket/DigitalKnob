22042a1287ccb3c0f145dd314c13c8ca3eb67cd5
aquawicket <aquawicket@hotmail.com>
2021-12-24 00:16:43 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/chart.js/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/chart.js/DKMAKE.cmake
@@ -1,12 +1,16 @@
+# https://github.com/chartjs/Chart.js
+# https://www.chartjs.org/
+#
+# https://github.com/chartjs/Chart.js/releases/download/v2.9.3/Chart.min.js
 # https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js
 
 ### VERSION ###
 DKSET(CHARTJS_MAJOR 2)
 DKSET(CHARTJS_MINOR 9)
-DKSET(CHARTJS_BUILD 4)
+DKSET(CHARTJS_BUILD 3)
 DKSET(CHARTJS_VERSION ${CHARTJS_MAJOR}.${CHARTJS_MINOR}.${CHARTJS_BUILD})
-DKSET(CHARTJS_NAME chart.min)
-DKSET(CHARTJS_DL https://cdn.jsdelivr.net/npm/chart.js@${CHARTJS_VERSION}/dist/Chart.min.js)
+DKSET(CHARTJS_NAME chart.min-${CHARTJS_VERSION})
+DKSET(CHARTJS_DL https://github.com/chartjs/Chart.js/releases/download/v${CHARTJS_VERSION}/Chart.min.js)
 DKSET(CHARTJS ${3RDPARTY}/${CHARTJS_NAME})
 
 ## https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js
