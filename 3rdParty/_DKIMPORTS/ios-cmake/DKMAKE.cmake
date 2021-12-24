1102f2b9424bb72db05794193b14ebe328d7e1e0
aquawicket <aquawicket@hotmail.com>
2021-12-23 22:48:49 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/ios-cmake/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ios-cmake/DKMAKE.cmake
@@ -6,6 +6,6 @@
 DKSET(IOSCMAKE_VERSION master)
 DKSET(IOSCMAKE_NAME ios-${VERSION})
 DKSET(IOSCMAKE_DL https://github.com/leetal/ios-cmake/archive/refs/heads/master.zip)
-DKSET(IOSCMAKE ${3RDPARTY}/${IOSCMAKE})
+DKSET(IOSCMAKE ${3RDPARTY}/${IOSCMAKE_NAME})
 
 DKINSTALL(${IOSCMAKE_DL} ios-cmake ${IOSCMAKE}) 
