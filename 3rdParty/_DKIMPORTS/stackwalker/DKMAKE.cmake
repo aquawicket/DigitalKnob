1c5bce6bb9f83231ee59805eab266ef9ef90d681
aquawicket <aquawicket@hotmail.com>
2021-12-24 00:32:13 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/stackwalker/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/stackwalker/DKMAKE.cmake
@@ -1,3 +1,9 @@
+# https://www.codeproject.com/Articles/11132/Walking-the-callstack-2
+# https://github.com/ConnectedVision/connectedvision/tree/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0
+#
+# https://github.com/ConnectedVision/connectedvision/raw/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0/stackwalker-2014-12-28.zip
+# https://www.codeproject.com/KB/threads/StackWalker/stackwalker.zip
+
 if(NOT CMAKE_HOST_WIN32)
 	return()
 endif()
@@ -6,13 +12,15 @@ endif()
 # https://www.boost.org/doc/libs/1_75_0/doc/html/stacktrace.html
 
 ### VERSION ###
-DKSET(STACKWALKER_VERSION 2014-12-28)
-DKSET(STACKWALKER ${3RDPARTY}/stackwalker)
+DKSET(STACKWALKER_VERSION 2005-07-27-v1)
+DKSET(STACKWALKER_NAME stackwalker-${STACKWALKER_VERSION})
+DKSET(STACKWALKER_DL https://github.com/ConnectedVision/connectedvision/raw/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0/stackwalker-2014.12.28.zip)
+DKSET(STACKWALKER ${3RDPARTY}/${STACKWALKER_NAME})
 
 
 ### INSTALL ###
-## https://github.com/ConnectedVision/connectedvision/raw/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0/stackwalker-2014-12-28.zip
-DKINSTALL(https://github.com/ConnectedVision/connectedvision/raw/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0/stackwalker-${STACKWALKER_VERSION}.zip stackwalker ${STACKWALKER})
+
+DKINSTALL(${STACKWALKER_DL} stackwalker ${STACKWALKER})
 
 
 ### LINK ###
