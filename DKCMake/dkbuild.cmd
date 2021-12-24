a7ec72062d676ff0afe496365d167b356a437cc9
aquawicket <aquawicket@hotmail.com>
2021-12-24 05:33:40 -0800

    commit from git

--- DKCMake/dkbuild.cmd
+++ DKCMake/dkbuild.cmd
@@ -14,9 +14,30 @@ if NOT exist "%CMAKE%" (
 	exit
 )
 
+set "OS="
+set "CPU="
+:pickos
+echo " "
+echo 1. win32
+echo 2. win64
+echo 3. android32
+echo 4. android64
+echo 5. Exit
+set choice=
+set /p choice=Please select an OS to build for: 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' set "OS=win32" && set "CPU=Win32" && goto picktarget
+if '%choice%'=='2' set "OS=win64" && set "CPU=x64" && goto picktarget
+if '%choice%'=='3' set "OS=android32" && set "CPU=ARM" && goto picktarget
+if '%choice%'=='4' set "OS=android64" && set "CPU=ARM64" && goto picktarget
+if '%choice%'=='5' goto end
+ECHO "%choice%" is not valid, please try again
+goto pickos
+
+:picktarget
 echo Please type the name of the library, tool or app to build. Then press enter.
 set /p input=
-set "cmnd="%CMAKE%" -G "Visual Studio 17 2022" -A Win32 -DDEBUG=ON -DRELEASE=ON -DSTATIC=ON -DREBUILDALL=ON -DTARGET=%input% -S "%DKCMAKE%" -B "%DKCMAKE%\build""
+set "cmnd="%CMAKE%" -G "Visual Studio 17 2022" -A %CPU% -DDEBUG=ON -DRELEASE=ON -DSTATIC=ON -DREBUILDALL=ON -DTARGET=%input% -DOS=%OS% -S "%DKCMAKE%" -B "%DKCMAKE%\temp""
 echo %cmnd%
 %cmnd%
 
---
+++ DKCMake/dkbuild.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Let's ask the use to type in what they want to build
+# could be an app, library or anything with a DKMAKE.cmake file
+# We whould also give the option to list targets as well
+
+
+if [ -e /proc/device-tree/model ]; then
+	MODEL=$(tr -d '\0' </proc/device-tree/model)
+fi
+echo "hostname = $HOSTNAME"
+echo "hosttype = $HOSTTYPE"
+echo "ostype =   $OSTYPE"
+echo "machtype = $MACHTYPE"
+
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	DIGITALKNOB="/home/$USER/digitalknob"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	DIGITALKNOB="/Users/$USER/digitalknob"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+	DIGITALKNOB="C:/Users/$USERNAME/digitalknob"
+elif [[ "$OSTYPE" == "msys" ]]; then
+	DIGITALKNOB="C:/Users/$USERNAME/digitalknob"
+elif [[ "$OSTYPE" == "win32" ]]; then #I'm not sure this can happen
+	DIGITALKNOB="C:/Users/$USERNAME/digitalknob" 
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+	echo "TODO: DIGITALKNOB NOT SET"
+else
+    echo "UNKNOWN OS TYPE ($OSTYPE)"
+fi
+DKPATH="$DIGITALKNOB/DK"
+DKCMAKE="$DIGITALKNOB/DK/DKCMake"
+
+sudo echo
+
+# https://unix.stackexchange.com/a/293605
+COLUMNS=1
+
+echo " "
+PS3='Please select an OS to build for: '
+if [[ "$MODEL" == "Raspberry"* ]]; then
+	options=("raspberry32" "Exit")
+elif [[ "$OSTYPE" == "linux-gnu"* ]] && [[ "$HOSTTYPE" == "x86_64"* ]]; then
+	options=("linux64" "Exit")
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	options=("linux32" "Exit")
+elif [[ "$OSTYPE" == "darwin"* ]] && [[ "$HOSTTYPE" == "x86_64"* ]]; then
+	options=("mac64" "Exit")
+else
+	echo "UNKNOWN OS TYPE ($OSTYPE)"
+	options=("Exit")
+fi
+select opt in "${options[@]}"
+do
+	case $opt in
+		"linux32")
+			echo "$opt"
+			OS="linux32"
+			break
+			;;
+		"linux64")
+			echo "$opt"
+			OS="linux64"
+			break
+			;;
+		"mac64")
+			echo "$opt"
+			OS="mac64"
+			break
+			;;
+		"raspberry32")
+			echo "$opt"
+			OS="raspberry32"
+			break
+			;;
+		"Exit")
+			echo "$opt"
+			exit 0
+			;;
+		*) echo "invalid option $REPLY";;
+	esac 
+	REPLY=
+done
+	
+echo Please type the name of the library, tool or app to build. Then press enter.
+
+# set /p input=
+input="DKCore"
+
+cmnd="cmake -G \"Visual Studio 17 2022\" -A Win32 -DDEBUG=ON -DRELEASE=ON -DSTATIC=ON -DREBUILDALL=ON -DTARGET=$input -S $DKCMAKE -B $DKCMAKE%\build"
+
+echo "$cmnd"
+"$cmnd"
+
+exec $SHELL #keep terminal open
