4a6130419c959a6210c4291687ebcb11d0f42662
aquawicket <aquawicket@hotmail.com>
2021-12-26 04:04:27 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/cmake/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/cmake/DKMAKE.cmake
@@ -1,46 +1,38 @@
 # https://github.com/Kitware/CMake/releases/download/v3.21.1/cmake-3.21.1-windows-i386.msi
 # https://developer.android.com/studio/projects/configure-cmake
 
-if(MAC)
-	DKDEPEND(homebrew)
-endif()
+MAC_DEPEND(homebrew)
 
 
 ### VERSION ###
 WIN_HOST_SET(CMAKE_VERSION 3.21.1)
-WIN_HOST_SET(CMAKE_NAME cmake-${CMAKE_VERSION}-windows-i386)
-WIN_HOST_SET(CMAKE_DL https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_NAME}.msi)
+WIN_HOST_SET(CMAKE_NAME cmake-${CMAKE_VERSION})
+WIN_HOST_SET(CMAKE_DL https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_NAME}-windows-i386.msi)
 #dk_getShortPath("C:/Program Files (x86)/CMake/bin" CMAKE)
+
 WIN_HOST_SET(CMAKE C:/PROGRA~2/CMake/bin)
 MAC_HOST_SET(CMAKE /usr/local/bin)
 LINUX_HOST_SET(CMAKE /usr/bin)
-WIN_HOST_SET(CMAKE_EXE ${CMAKE}/cmake.exe)
-MAC_HOST_SET(CMAKE_EXE ${CMAKE}/cmake)
-LINUX_HOST_SET(CMAKE_EXE ${CMAKE}/cmake)
 
+WIN_HOST_SET(CMAKE_EXE ${CMAKE}/cmake.exe)
+UNIX_HOST_SET(CMAKE_EXE ${CMAKE}/cmake)
 
 
 ### INSTALL ###
-message(STATUS "looking for cmake at ${CMAKE_EXE}")
+DKINFO("looking for cmake at ${CMAKE_EXE}")
 if(NOT EXISTS "${CMAKE_EXE}")
-	message(STATUS "cmake NOT FOUND")
-	message(STATUS "Installing cmake ${CMAKE_VERSION}")
-	if(WIN_HOST)
-		DOWNLOAD(${CMAKE_DL} ${DKDOWNLOAD})
-		DKCOMMAND(${DKDOWNLOAD}/${CMAKE_NAME}.msi)
-	endif()
-	if(MAC_HOST)
-		DKCOMMAND(brew install cmake)
-	endif()
-	if(LINUX_HOST)
-		DKCOMMAND(sudo apt-get -y install cmake)
-	endif()
+	DKINFO("cmake NOT FOUND")
+	DKINFO("Installing cmake ${CMAKE_VERSION}")
+	WIN_HOST_DOWNLOAD(${CMAKE_DL} ${DKDOWNLOAD})
+	WIN_HOST_COMMAND(${DKDOWNLOAD}/${CMAKE_NAME}-windows-i386.msi)
+	MAC_HOST_COMMAND(brew install cmake)
+	LINUX_HOST_COMMAND(sudo apt-get -y install cmake)
 else()
-	message(STATUS "Found cmake at ${CMAKE_EXE}")
+	DKINFO("Found cmake at ${CMAKE_EXE}")
 endif()
 
 if(NOT EXISTS "${CMAKE_EXE}")
-	message(FATAL_ERROR "CMAKE IS NOT FOUND OR INVALID")
+	DKERROR("CMAKE IS NOT FOUND OR INVALID")
 endif()
 
 
