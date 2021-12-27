19af4c80a43a7fe7f18c3138dba730eb2db26b39
aquawicket <aquawicket@hotmail.com>
2021-12-27 12:02:10 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/gl_kit/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/gl_kit/DKMAKE.cmake
@@ -6,6 +6,6 @@ endif()
 
 FIND_LIBRARY(GLKit_Lib GLKit)
 if(NOT GLKit_Lib)
-	message(FATAL_ERROR "Could not locate GLKit framework")
+	message(WARNING "Could not locate GLKit framework")
 endif()
 APPLE_LIB(${GLKit_Lib})
