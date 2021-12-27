309c93cef1100f8daecfd2a95a34c2e21e58e006
aquawicket <aquawicket@hotmail.com>
2021-12-27 11:45:05 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/audio_toolbox/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/audio_toolbox/DKMAKE.cmake
@@ -6,6 +6,6 @@ endif()
 
 FIND_LIBRARY(AudioToolbox_lib AudioToolbox)
 if(NOT AudioToolbox_lib)
-	message(FATAL_ERROR "Could not locate AudioToolbox framework")
+	message(STATUS "Could not locate AudioToolbox framework")
 endif()
 APPLE_LIB(${AudioToolbox_lib})
