df481a968320c6e5bd6d071a3beea359c38775b8
aquawicket <aquawicket@hotmail.com>
2021-12-26 20:10:17 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/ios-cmake/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ios-cmake/DKMAKE.cmake
@@ -4,8 +4,8 @@
 
 ## Version ##
 DKSET(IOSCMAKE_VERSION master)
-DKSET(IOSCMAKE_NAME ios-${VERSION})
-DKSET(IOSCMAKE_DL https://github.com/leetal/ios-cmake/archive/refs/heads/master.zip)
+DKSET(IOSCMAKE_NAME ios-${IOSCMAKE_VERSION})
+DKSET(IOSCMAKE_DL https://github.com/leetal/ios-cmake/archive/refs/heads/${IOSCMAKE_VERSION}.zip)
 DKSET(IOSCMAKE ${3RDPARTY}/${IOSCMAKE_NAME})
 
 DKINSTALL(${IOSCMAKE_DL} ios-cmake ${IOSCMAKE}) 
--- 3rdParty/_DKIMPORTS/sdl-gpu/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/sdl-gpu/DKMAKE.cmake
@@ -7,7 +7,7 @@
 
 ### VERSION ###
 DKSET(SDLGPU_VERSION master)
-DKSET(SDLGPU_NAME sdl-gpu-${SDLGUP_VERSION})
+DKSET(SDLGPU_NAME sdl-gpu-${SDLGPU_VERSION})
 DKSET(SDLGPU_DL https://github.com/aquawicket/sdl-gpu/archive/refs/heads/${SDLGPU_VERSION}.zip)
 DKSET(SDLGPU ${3RDPARTY}/${SDLGPU_NAME})
 
