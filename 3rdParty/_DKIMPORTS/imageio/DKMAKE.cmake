e3bf79aa8c1ff9e79a4e75f8625200f131d50c4e
aquawicket <aquawicket@hotmail.com>
2021-12-27 12:04:41 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/imageio/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/imageio/DKMAKE.cmake
@@ -6,6 +6,6 @@ endif()
 
 FIND_LIBRARY(ImageIO_lib ImageIO)
 if(NOT ImageIO_lib)
-	message(FATAL_ERROR "Could not locate ImageIO framework")
+	message(WARNING "Could not locate ImageIO framework")
 endif()
 APPLE_LIB(${ImageIO_lib})
--- 3rdParty/_DKIMPORTS/iokit/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/iokit/DKMAKE.cmake
@@ -6,6 +6,6 @@ endif()
 
 FIND_LIBRARY(IOKit_lib IOKit)
 if(NOT IOKit_lib)
-	message(FATAL_ERROR "Could not locate IOKit framework")
+	message(WARNING "Could not locate IOKit framework")
 endif()
 APPLE_LIB(${IOKit_lib})
