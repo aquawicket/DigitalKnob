79e5a595152ea0aba442c2549d0b68c2bd085380
aquawicket <aquawicket@hotmail.com>
2021-12-29 11:46:46 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/java/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/java/DKMAKE.cmake
@@ -5,24 +5,24 @@
 # http://deb.sambaedu.org/wpkg/files/jre/jre-8u311-windows-x64.exe
 
 ### VERSION ###
+# jre1.8.0_311
 WIN_HOST_DKSET(JAVA_VERSION 8u311)
 WIN_HOST_DKSET(JAVA_NAME jre-${JAVA_VERSION}-windows-x64)
 WIN_HOST_DKSET(JAVA_DL http://deb.sambaedu.org/wpkg/files/jre/jre-8u311-windows-x64.exe)
-WIN_HOST_DKSET(JAVA "C:/Program Files/Java/bin/jre1.8.0_311/bin")
-WIN_GetShortPath(${JAVA} JAVA)
-DKSET(JAVA "${JAVA}")
-WIN_HOST_DKSET(JAVA_EXE ${JAVA}/java.exe)
+#WIN_HOST_DKSET(JAVA "C:/Program Files/Java/bin/jre1.8.0_311")
+WIN_HOST_DKSET(JAVA ${3RDPARTY}/${JAVA_NAME})
+#WIN_GetShortPath(${JAVA} JAVA)
+#DKSET(JAVA "${JAVA}")
+WIN_HOST_DKSET(JAVA_EXE ${JAVA}/bin/java.exe)
 
 
 ### INSTALL ###
 DKINFO("looking for java at ${JAVA_EXE}")
 if(NOT EXISTS "${JAVA_EXE}")
 	DKINFO("java NOT FOUND")
-	DKINFO("Installing ${JAVA_NAME}")
 	WIN_HOST_DOWNLOAD(${JAVA_DL} ${DKDOWNLOAD})
-	Wait()
-	dk_exit()
-	WIN_HOST_DKCOMMAND(${DKDOWNLOAD}/${JAVA_NAME}.exe)
+	DKINFO("Installing ${JAVA_NAME} . . . please wait")
+	WIN_HOST_DKCOMMAND(${DKDOWNLOAD}/${JAVA_NAME}.exe /s INSTALLDIR=${JAVA})
 else()
 	DKINFO("Found cmake at ${JAVA_EXE}")
 endif()
@@ -30,3 +30,5 @@ endif()
 if(NOT EXISTS "${JAVA_EXE}")
 	DKERROR("JAVA IS NOT FOUND OR INVALID")
 endif()
+
+DKSETENV("JAVA_HOME" ${JAVA})
