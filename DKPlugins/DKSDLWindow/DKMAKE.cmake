0a7e72b0cd26288a0abadb6919c0ca453d41ae55
aquawicket <aquawicket@hotmail.com>
2021-12-31 00:37:59 -0800

    commit from git

--- DKPlugins/DKSDLWindow/DKMAKE.cmake
+++ DKPlugins/DKSDLWindow/DKMAKE.cmake
@@ -4,8 +4,8 @@ DKDEPEND(DKWindow)
 
 # add SDL_main to the top of the DKPlugins.h file for iOS
 if(IOS)
-	set(ios_sdl2main "#ifdef IOS\n #include "SDL_main.h"\n #endif")
-	DKSET(PLUGINS_FILE ${ios_sdl2main} ${PLUGINS_FILE})
+	set(ios_sdl2main "#ifdef IOS\n #include "SDL_main.h"\n #endif \n")
+	DKSET(PLUGINS_FILE "${ios_sdl2main} ${PLUGINS_FILE}")
 endif()
 
 generateCmake(DKSDLWindow)
