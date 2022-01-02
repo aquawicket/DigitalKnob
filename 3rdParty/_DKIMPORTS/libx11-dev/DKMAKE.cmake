cfab700d7b5819a18149fb12f82728950ae4a9b5
aquawicket <aquawicket@hotmail.com>
2022-01-01 21:14:31 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/libx11-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libx11-dev/DKMAKE.cmake
@@ -14,5 +14,12 @@ if(LINUX OR RASPBERRY)
 
 	### LINK ###
 	DKINCLUDE(/usr/include/X11)
+	
+	#dynamic linking
 	SET(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE} -lX11")
+	
+	#static linking
+	#RASPBERRY_DKLIB(/usr/lib/arm-linux-gnueabihf/libX11.a)
+	#RASPBERRY_DKLIB(/usr/lib/arm-linux-gnueabihf/libxcb.a)
+	#RASPBERRY_DKLIB(/usr/lib/arm-linux-gnueabihf/libXau.a)
 endif()
