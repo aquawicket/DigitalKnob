814df4091a1bbfb9e0c27c19b541a5d375846d78
aquawicket <aquawicket@hotmail.com>
2022-01-03 06:17:21 -0800

    commit from git

--- DKCMake/Disabled.cmake
+++ DKCMake/Disabled.cmake
@@ -334,7 +334,7 @@ if(ANDROID) # Disabled for Android targets
 	DKDISABLE(rmlui)
 	DKDISABLE(rtmidi)
 	DKDISABLE(sdl-gpu)
-	#DKDISABLE(sdl2_mixer)
+	DKDISABLE(sdl2_mixer)
 	DKDISABLE(sdl2_ttf)
 	DKDISABLE(sfml)
 	DKDISABLE(smpeg2)
