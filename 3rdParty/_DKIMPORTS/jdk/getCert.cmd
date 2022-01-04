a7d334d75180a73a3e2a483f8cbf89a57f098118
aquawicket <aquawicket@hotmail.com>
2022-01-03 19:49:06 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/jdk/getCert.cmd
+++ 3rdParty/_DKIMPORTS/jdk/getCert.cmd
@@ -14,13 +14,12 @@ echo press and key to continue && timeout /t 60 > nul
 set "DIGITALKNOB=C:\Users\%USERNAME%\digitalknob"
 ::set "JDK=%DIGITALKNOB%\DK\3rdParty\openssl-1.0.2h-vs2015
 set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020"
-#set "OPENSSL=C:\Program Files\OpenSSL-Win64"
 set "GOOGLE_CERT=%JDK%\google.cer"
 set "MAVEN_CERT=%JDK%\maven.cer"
 set "KEYTOOL_EXE=%JDK%\bin\keytool.exe"
 set "CACERTS=%JDK%\lib\security\cacerts"
 
-
+::set "OPENSSL=C:\Program Files\OpenSSL-Win64"
 if exist "C:\Windows\System32\openssl.exe" set "OPENSSL_EXE=C:\Windows\System32\openssl.exe"
 if exist "C:\Windows\System32\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:\Windows\System32\OpenSSL-win32\bin\openssl.exe"
 if exist "C:\Program Files (x86)\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:\Program Files (x86)\OpenSSL-win32\bin\openssl.exe"
@@ -33,13 +32,10 @@ if exist "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" set "OPENSSL_EXE=C:\Pr
 
 
 ::echo -n | "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" s_client -connect google.com:443 | "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" x509 > "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020\google.cer" "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020\bin\keytool.exe" -import -alias google -file "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020"\google.cer"
-
-::echo -n | "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" s_client -connect repo.maven.apache.org:443 | "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" x509 > "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020\maven.cer" "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020\bin\keytool.exe" -import -alias maven -file "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020\maven.cer"
-
-
 echo -n | "%OPENSSL_EXE%" s_client -connect google.com:443 | "%OPENSSL_EXE%" x509 > "%GOOGLE_CERT%"
 "%KEYTOOL_EXE%" -import -alias google -file "%GOOGLE_CERT%"
 
+::echo -n | "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" s_client -connect repo.maven.apache.org:443 | "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" x509 > "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020\maven.cer" "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020\bin\keytool.exe" -import -alias maven -file "C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020\maven.cer"
 echo -n | "%OPENSSL_EXE%" s_client -connect repo.maven.apache.org:443 | "%OPENSSL_EXE%" x509 > "%MAVEN_CERT%"
 "%KEYTOOL_EXE%" -import -alias maven -file "%MAVEN_CERT%"
 
