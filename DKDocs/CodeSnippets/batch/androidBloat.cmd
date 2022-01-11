f9d9b31118d9ba46a3f156e1581c405f734b33bd
aquawicket <aquawicket@hotmail.com>
2022-01-11 15:58:30 -0800

    commit from git

---
+++ DKDocs/CodeSnippets/batch/androidBloat.cmd
@@ -0,0 +1,80 @@
+@echo off
+set /p Y=Enter adb.exe folder path:    
+cd %Y%
+adb devices
+pause
+for %%X in (
+"com.android.browser"
+"com.android.chrome"
+"com.android.email"
+"com.android.thememanager"
+"com.android.wallpaper.livepicker"
+"com.facebook.services"
+"com.facebook.system"
+"com.facebook.appmanager"
+
+"com.google.android.apps.docs"
+"com.google.android.apps.maps"
+"com.google.android.apps.photos"
+"com.google.android.apps.tachyon"
+"com.google.android.feedback"
+"com.google.android.gm"
+"com.google.android.googlequicksearchbox"
+"com.google.android.marvin.talkback"
+"com.google.android.music"
+"com.google.android.syncadapters.calendar"
+"com.google.android.syncadapters.contacts"
+"com.google.android.talk"
+"com.google.android.tts"
+"com.google.android.videos"
+"com.google.android.youtube"
+
+"com.mfashiongallery.emag"
+"com.mi.android.globalpersonalassistant"
+"com.mi.global.shop"
+"com.mi.global.bbs"
+"com.mi.webkit.core"
+"com.mipay.wallet.in"
+"com.miui.analytics"
+"com.miui.android.fashiongallery"
+"com.miui.bugreport"
+"com.miui.cloudbackup"
+"com.miui.cloudservice"
+"com.miui.cloudservice.sysbase"
+"com.miui.hybrid.accessory"
+"com.miui.klo.bugreport"
+"com.miui.miwallpaper"
+"com.miui.player"
+"com.miui.providers.weather"
+"com.miui.screenrecorder"
+"com.miui.translationservice"
+"com.miui.translation.kingsoft"
+"com.miui.translation.youdao"
+"com.miui.touchassistant"
+"com.miui.videoplayer"
+"com.miui.virtualsim"
+"com.miui.weather2"
+"com.miui.yellowpage"
+"com.swiftkey.languageprovider"
+"com.swiftkey.swiftkeyconfigurator"
+"com.xiaomi.account"
+"com.xiaomi.discover"
+"com.xiaomi.micloud.sdk"
+"com.xiaomi.midrop"
+"com.xiaomi.mipicks"
+"com.xiaomi.oversea.ecom"
+"com.xiaomi.payment"
+"com.xiaomi.xmsf" ) do (
+    adb shell pm uninstall %%X
+    adb shell pm uninstall --user 0 %%X
+)
+pause
+::Optional
+::Theme Module ==> "com.android.thememanager.module"
+::Play Store ==> "com.android.vending"
+::Play Store Service ==> "com.google.android.gms"
+::Gboard ==> "com.google.android.inputmethod.latin"
+::Google Login Service ==> "com.google.android.gsf.login"
+::Explorer ==> "com.mi.android.globalFileexplorer"
+::Find device ==> "com.xiaomi.finddevice"
+::END-80#
