805b5697da89074a93b44c53130816263b87d4f7
aquawicket <aquawicket@hotmail.com>
2022-01-11 22:08:52 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/jdk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/jdk/DKMAKE.cmake
@@ -9,8 +9,12 @@
 
 
 ### VERSION ###
-#WIN_HOST_DKSET(JDK8_VERSION openjdk-8u41-b04-windows-i586-14_jan_2020)
-#WIN_HOST_DKSET(JDK8_DL https://download.java.net/openjdk/jdk8u41/ri/openjdk-8u41-b04-windows-i586-14_jan_2020.zip)
+WIN_HOST_DKSET(JDK8_VERSION openjdk-8u41-b04-windows-i586-14_jan_2020)
+WIN_HOST_DKSET(JDK8_DL https://download.java.net/openjdk/jdk8u41/ri/openjdk-8u41-b04-windows-i586-14_jan_2020.zip)
+DKSET(JDK8_NAME openjdk-${JDK_VERSION})
+DKSET(JDK8 ${3RDPARTY}/${JDK_NAME})
+DKINSTALL(${JDK_DL} jdk ${JDK})
+
 
 #WIN_HOST_DKSET(JDK_VERSION 9.0.4_windows-x64_bin)
 #WIN_HOST_DKSET(JDK_DL https://download.java.net/java/GA/jdk9/9.0.4/binaries/openjdk-9.0.4_windows-x64_bin.tar.gz)
