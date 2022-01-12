2cbe5c3578d4232d0b91e6c9b6a1adc23baa60c8
aquawicket <aquawicket@hotmail.com>
2022-01-12 03:38:12 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/jdk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/jdk/DKMAKE.cmake
@@ -42,6 +42,6 @@ DKSETENV("VS_JavaHome" ${JDK})
 
 #Add registry entries
 WIN_HOST_DKCOMMAND(reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment" /v CurrentVersion /t REG_SZ /d %JAVA_VERSION% /f /reg:64)
-WIN_HOST_DKCOMMAND(reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%JAVA_VERSION%" /v JavaHome /t REG_SZ /d "%JAVA_HOME%" /f /reg:64)
-WIN_HOST_DKCOMMAND(reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%JAVA_VERSION%" /v RuntimeLib /t REG_SZ /d "%JAVA_HOME%\bin\server\jvm.dll" /f /reg:64)
+WIN_HOST_DKCOMMAND(reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\${JDK_VERSION}" /v JavaHome /t REG_SZ /d "${JDK_HOME}" /f /reg:64)
+WIN_HOST_DKCOMMAND(reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\${JDK_VERSION}" /v RuntimeLib /t REG_SZ /d "${JDK_HOME}\bin\server\jvm.dll" /f /reg:64)
 
