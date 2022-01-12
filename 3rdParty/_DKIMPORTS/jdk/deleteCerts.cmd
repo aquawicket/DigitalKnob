024d02477d20b40085c7c248f75f50701f119a94
aquawicket <aquawicket@hotmail.com>
2022-01-11 19:38:29 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/jdk/deleteCerts.cmd
+++ 3rdParty/_DKIMPORTS/jdk/deleteCerts.cmd
@@ -3,16 +3,16 @@
 
 
 set "DIGITALKNOB=C:\Users\%USERNAME%\digitalknob"
-::set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020"
-set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-9.0.4_windows-x64_bin"
+set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020"
+::set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-9.0.4_windows-x64_bin"
 set "GOOGLE_CERT=%JDK%\google.cer"
 set "MAVEN_CERT=%JDK%\maven.cer"
 set "KEYTOOL_EXE=%JDK%\bin\keytool.exe"
 set "CACERTS=%JDK%\lib\security\cacerts"
 
 ::set "OPENSSL=C:\Program Files\OpenSSL-Win64"
-if exist "C:\Windows\System32\openssl.exe" set "OPENSSL_EXE=C:\Windows\System32\openssl.exe"
-if exist "C:\Windows\System32\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:\Windows\System32\OpenSSL-win32\bin\openssl.exe"
+::if exist "C:\Windows\System32\openssl.exe" set "OPENSSL_EXE=C:\Windows\System32\openssl.exe"
+::if exist "C:\Windows\System32\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:\Windows\System32\OpenSSL-win32\bin\openssl.exe"
 if exist "C:\Program Files (x86)\OpenSSL-win32\bin\openssl.exe" set "OPENSSL_EXE=C:\Program Files (x86)\OpenSSL-win32\bin\openssl.exe"
 if exist "C:\Program Files\OpenSSL-Win64\bin\openssl.exe" set "OPENSSL_EXE=C:\Program Files\OpenSSL-Win64\bin\openssl.exe"
 
@@ -21,8 +21,10 @@ taskkill /IM "java.exe" /F
 
 :: delete the keys first if they already exist
 echo deleting "%GOOGLE_CERT%"
-"%KEYTOOL_EXE%" -delete -noprompt -alias google -cacerts -file "%GOOGLE_CERT%" -storepass changeit
+::"%KEYTOOL_EXE%" -delete -noprompt -alias google -cacerts -file "%GOOGLE_CERT%" -storepass changeit
+"%KEYTOOL_EXE%" -delete -noprompt -alias google -file "%GOOGLE_CERT%" -storepass changeit
 echo deleting "%MAVEN_CERT%"
-"%KEYTOOL_EXE%" -delete -noprompt -alias maven -cacerts -file "%MAVEN_CERT%" -storepass changeit
+::"%KEYTOOL_EXE%" -delete -noprompt -alias maven -cacerts -file "%MAVEN_CERT%" -storepass changeit
+"%KEYTOOL_EXE%" -delete -noprompt -alias maven -file "%MAVEN_CERT%" -storepass changeit
 
 echo press and key to continue or wait 5 seconds... && timeout /t 5 > nul
--- 3rdParty/_DKIMPORTS/jdk/importCerts.cmd
+++ 3rdParty/_DKIMPORTS/jdk/importCerts.cmd
@@ -14,9 +14,9 @@ if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
 
 set "DIGITALKNOB=C:\Users\%USERNAME%\digitalknob"
-::set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020"
+set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-8u41-b04-windows-i586-14_jan_2020"
 ::set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-9.0.4_windows-x64_bin"
-set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-11_windows-x64_bin"
+::set "JDK=%DIGITALKNOB%\DK\3rdParty\openjdk-11_windows-x64_bin"
 set "GOOGLE_CERT=%JDK%\google.cer"
 set "MAVEN_CERT=%JDK%\maven.cer"
 set "KEYTOOL_EXE=%JDK%\bin\keytool.exe"
@@ -42,10 +42,12 @@ taskkill /IM "java.exe" /F
 
 :: create and import the google.cer key
 echo -n | "%OPENSSL_EXE%" s_client -connect google.com:443 | "%OPENSSL_EXE%" x509 > "%GOOGLE_CERT%"
-"%KEYTOOL_EXE%" -import -noprompt -alias google -cacerts -file "%GOOGLE_CERT%" -storepass changeit
+::"%KEYTOOL_EXE%" -import -noprompt -alias google -cacerts -file "%GOOGLE_CERT%" -storepass changeit
+"%KEYTOOL_EXE%" -import -noprompt -alias google -file "%GOOGLE_CERT%" -storepass changeit
 
 :: create and import the maven.cer key
 echo -n | "%OPENSSL_EXE%" s_client -connect repo.maven.apache.org:443 | "%OPENSSL_EXE%" x509 > "%MAVEN_CERT%"
-"%KEYTOOL_EXE%" -import -noprompt -alias maven -cacerts -file "%MAVEN_CERT%" -storepass changeit
+::"%KEYTOOL_EXE%" -import -noprompt -alias maven -cacerts -file "%MAVEN_CERT%" -storepass changeit
+"%KEYTOOL_EXE%" -import -noprompt -alias maven -file "%MAVEN_CERT%" -storepass changeit
 
 echo press and key to continue or wait 5 seconds... && timeout /t 5 > nul
