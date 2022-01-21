6d864008811cc87039af442a97bf816f4ae8cd04
aquawicket <aquawicket@hotmail.com>
2022-01-21 13:10:45 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/sdl2/android-project2/build.cmd
+++ 3rdParty/_DKIMPORTS/sdl2/android-project2/build.cmd
@@ -1,5 +1,13 @@
-set JAVA_HOME=C:\Users\Administrator\digitalknob\DK\3rdParty\openjdk-11
-setx JAVA_HOME C:\Users\Administrator\digitalknob\DK\3rdParty\openjdk-11
+@echo off
+
+set "DIGITALKNOB=C:\Users\Administrator\digitalknob"
+set "JAVA_HOME=%DIGITALKNOB%\DK\3rdParty\openjdk-11"
+setx JAVA_HOME %DIGITALKNOB%\DK\3rdParty\openjdk-11
+
+set APPPATH=%~dp0
+if "%APPPATH%" == "" set APPPATH=.
+set "GRADLE_USER_HOME=%APPPATH%\gradle"
+setx GRADLE_USER_HOME %APPPATH%\gradle
 
 gradlew clean build --info
 gradlew build --info
