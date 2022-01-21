9a651ead06debdf4531a6888d8e21e92970b3205
aquawicket <aquawicket@hotmail.com>
2022-01-21 10:48:37 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/sdl2/android-project2/build.cmd
+++ 3rdParty/_DKIMPORTS/sdl2/android-project2/build.cmd
@@ -4,8 +4,19 @@ setx JAVA_HOME C:\Users\Administrator\digitalknob\DK\3rdParty\openjdk-11
 gradlew clean build --info
 gradlew build --info
 
-:: Files Created
+:: Files Created with build.cmd
 :: C:/Uses/%USERNAME%/.gradle/*
 :: android-project2/.gradle/*
 :: android-project2/app/.cxx/*
 :: android-project2/app/build/*
+
+
+:: Files Created with SDL2_project.sln
+:: C:/Uses/%USERNAME%/.gradle/*
+:: android-project2/.gradle/*
+:: android-project2/.vs/*
+:: android-project2/ARM/*
+:: android-project2/x64/*
+:: android-project2/app/.cxx/*
+:: android-project2/app/build/*
+:: android-project2/app/jni/src/ARM/*
