f1b122f9ade5e7f11ae9a4a674602b02958aa02d
aquawicket <aquawicket@hotmail.com>
2022-02-01 03:58:14 -0800

    commit from git

---
+++ 3rdParty/_DKIMPORTS/jdk11/DKMAKE.cmake
@@ -0,0 +1,28 @@
+# https://docs.microsoft.com/en-us/java/openjdk/download
+# https://jdk.java.net/archive/
+#
+# https://download.java.net/java/ga/jdk11/openjdk-11_windows-x64_bin.zip
+# https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.13%2B8/OpenJDK11U-jdk_x64_windows_11.0.13_8.zip
+
+### VERSION ###
+
+WIN_HOST_DKSET(JDK_VERSION 11)
+WIN_HOST_DKSET(JDK_DL https://download.java.net/java/ga/jdk11/openjdk-11_windows-x64_bin.zip)
+
+
+### INSTALL ###
+DKSET(JDK11_NAME openjdk-${JDK11_VERSION})
+DKSET(JDK11 ${3RDPARTY}/${JDK11_NAME})
+DKINSTALL(${JDK11_DL} jdk11 ${JDK11})
+
+
+### LINK ###
+#DKINCLUDE(${JDK11}/)	
+#DKSETENV("JAVA_HOME" ${JDK11})
+#DKSETENV("JAVA_VERSION" ${JDK11_VERSION})
+#DKSETENV("VS_JavaHome" ${JDK11})
+
+#Add registry entries
+#dk_addRegistryKey("HKLM/SOFTWARE/JavaSoft/Java Runtime Environment" "CurrentVersion" "${JDK11_VERSION}")
+#dk_addRegistryKey("HKLM/SOFTWARE/JavaSoft/Java Runtime Environment/${JDK11_VERSION}" "JavaHome" "${JDK11}")
+#dk_addRegistryKey("HKLM/SOFTWARE/JavaSoft/Java Runtime Environment/${JDK11_VERSION}" "RuntimeLib" "${JDK11}/bin/server/jvm.dll")
