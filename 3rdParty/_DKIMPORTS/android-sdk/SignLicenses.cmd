eb12b35e549cc7771704378c0eda1b245b6448f4
aquawicket <aquawicket@hotmail.com>
2022-02-01 06:50:58 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/android-sdk/SignLicenses.cmd
+++ 3rdParty/_DKIMPORTS/android-sdk/SignLicenses.cmd
@@ -2,7 +2,7 @@
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
 set "JAVA_HOME=C:\Users\%USERNAME%\digitalknob\DK\3rdParty\openjdk-9.0.4"
-"%JAVA_HOME%\registerJDK.cmd"
+call "%JAVA_HOME%\registerJDK.cmd"
 set "SDKMANAGER=C:\Users\%USERNAME%\digitalknob\DK\3rdParty\android-sdk\tools\bin\sdkmanager.bat
 
 %SDKMANAGER% --licenses
