39a5ab3253109f36bb8e497f1354615b66bbe3ee
aquawicket <aquawicket@hotmail.com>
2022-02-01 06:04:38 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/jdk8/registerJDK8.cmd
+++ 3rdParty/_DKIMPORTS/jdk8/registerJDK8.cmd
@@ -1,10 +1,14 @@
 @echo off
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
+set "Folder=openjdk-8u41"
 set "CurrentVersion=1.8.0_41"
-set "JavaHome=%JAVA_HOME%"
+
+set "JAVA_HOME=C:\Users\Administrator\digitalknob\DK\3rdParty\%Folder%"
+setx JAVA_HOME %JAVA_HOME%
+setx VS_JavaHome %JAVA_HOME% 
 
 reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment" /v CurrentVersion /t REG_SZ /d %CurrentVersion% /f
-reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%CurrentVersion%" /v JavaHome /t REG_SZ /d "%JavaHome%" /f
-reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%CurrentVersion%" /v RuntimeLib /t REG_SZ /d "%JavaHome%\bin\server\jvm.dll" /f
+reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%CurrentVersion%" /v JavaHome /t REG_SZ /d "%JAVA_HOME%" /f
+reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%CurrentVersion%" /v RuntimeLib /t REG_SZ /d "%JAVA_HOME%\bin\server\jvm.dll" /f
 
