5b810773fbcd3bc8f4bbdcc996e8a34e5e000d44
aquawicket <aquawicket@hotmail.com>
2022-02-07 05:44:33 -0800

    commit from git

--- DKCMake/DKBatch/FileFunctions/AppendEnvPath.cmd
+++ DKCMake/DKBatch/FileFunctions/AppendEnvPath.cmd
@@ -14,10 +14,18 @@ set "path=%~1"
 if %DEBUG%==1 echo AppendEnvPath(path: %path%)
 
 call Contains "%PATH%\" "%path%" result
-if not "%result%" == "1" (setx PATH "%PATH%";"%path%" >nul)
-if not "%ERRORLEVEL%" == "0" (echo ERROR:%ERRORLEVEL% & goto :EOF)
 
-if %DEBUG%==1 echo AppendEnvPath(result: %result%)
+if not "%result%" == "1" (
+	setx PATH "%PATH%";"%path%" >nul
+)
+
+if not "%ERRORLEVEL%" == "0" (
+	echo ERROR:%ERRORLEVEL% & goto :EOF
+)
+
+if %DEBUG%==1 (
+	echo AppendEnvPath result: %result%
+)
 endlocal & set "%3=%result%"
 
 
