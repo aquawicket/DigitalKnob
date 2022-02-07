f7a8ca5f4be5fab6a58774082a3ed66c5e461a2a
aquawicket <aquawicket@hotmail.com>
2022-02-07 05:37:11 -0800

    commit from git

---
+++ DKCMake/DKBatch/AndroidFunctions/UninstallPackage.cmd
@@ -0,0 +1,36 @@
+%DKBATCH%
+%DKIN% %~n0()
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:UninstallPackage name error
+::
+:: Func:  Uninstall a packgage from an Android device
+::
+:: name:  The name of the package to uninstall
+:: error:   Returned error code if any
+::
+:: Example:  call UninstallPackage com.company.appname error
+::           echo UninstallPackage returned: %error%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set "name=%~1"
+
+if not defined %name% (
+	echo ERROR: The "name" argument is invalid
+	goto :end
+)
+
+if not defined %ANDROID_HOME% (
+	echo ERROR: The ANDROID_HOME environment variable is undefined
+	goto :end
+)
+
+if not exist "%ANDROID_HOME%/platform-tools/adb" (
+	echo ERROR: Could not locate adb
+	goto :end
+) 
+
+"%ANDROID_HOME%/platform-tools/adb" shell pm uninstall %name%
+
+:end 
+endlocal & set "%2=%ERRORLEVEL%"
+%DKOUT% %~n0()
+goto :EOF
