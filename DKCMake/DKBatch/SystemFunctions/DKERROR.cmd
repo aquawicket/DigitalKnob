d107b4360cf4386987e1ca1194c372fc2f2fcc8a
aquawicket <aquawicket@hotmail.com>
2022-02-08 01:16:46 -0800

    commit from git

--- DKCMake/DKBatch/SystemFunctions/DKERROR.cmd
+++ DKCMake/DKBatch/SystemFunctions/DKERROR.cmd
@@ -1,23 +1,16 @@
 ::%DKBATCH%
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::%ERROR% message
+::    %ERROR%  message
+:: %IF_ERROR%  message
+::    %FATAL%  message
+:: %IF_FATAL%  message
 ::
-:: Func:  Throw and error with a message and halt further batch execution
 ::
-:: message:  The message to display with the error
+::    %ERROR%: Throw an error message, then continue script
+:: %IF_ERROR%: Throw an error message if %ERRORLEVEL% is not 0, then continue script
+::    %FATAL%: Throw an error message, then exit script
+:: %IF_FATAL%: Throw an error messate if %ERRORLEVEL% is not 0, then exit script
 ::
-:: Example:   if not exist "somefile.txt" (
-::				  %ERROR% "File not found"
-::		      )
-::            prints: ERROR:0 "File not found"
-::                     File: C:\path\to\file\that\threw\the\error.bat
-::
-::           
-:: Example 2: if not "%ERRORLEVEL%"=="0" (
-::				  %ERROR% "The Last Command Failed"
-::            )
-::            prints: ERROR:18 "The Last Command Failed"
-::                     File: C:\path\to\file\that\threw\the\error.bat
 ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 if "%1"=="IF_ERROR" (goto :IF_ERROR)
@@ -28,9 +21,8 @@ if "%1"=="FATAL" (goto :FATAL)
 :IF_ERROR
 if not "%ERRORLEVEL%"=="0" (
 	for /f "delims=:" %%a in ('findstr /n /c:"%~3" "%~f2"') do set "line=%%a"
-	echo [31m IF_ERROR: %ERRORLEVEL% - %3 [0m
-	echo [31m  File: %~f2: !line! [0m
-	::echo:
+	echo [91m IF_ERROR: %ERRORLEVEL% - %3 [0m
+	echo [91m  File: %~f2: !line! [0m
 	set /A n = 1
 	set /A min = !line!-6
 	set /A max = !line!+6
@@ -54,8 +46,24 @@ goto :EOF
 :IF_FATAL
 if not "%ERRORLEVEL%"=="0" (
 	for /f "delims=:" %%a in ('findstr /n /c:"%~3" "%~f2"') do set "line=%%a"
-	echo [91m IF_FATAL: %ERRORLEVEL% - %3 [0m
-	echo [91m  File: %~f2:!line! [0m
+	echo [31m IF_FATAL: %ERRORLEVEL% - %3 [0m
+	echo [31m  File: %~f2:!line! [0m
+	set /A n = 1
+	set /A min = !line!-6
+	set /A max = !line!+6
+	for /f "delims=" %%a in ('findstr /n "^" "%~f2"') do (
+		if !n! lss !max! (
+			if !n! gtr !min! (
+				set "string=%%a"
+				if !n! equ !line! (
+					echo [30;41m^>	!n!:	!string:~2,-1%![0m
+				) else (
+					echo		!n!:	!string:~2,-1%!
+				)
+			)
+		)		
+		set /a n+=1
+	)
 	echo Press any key to exit . . .
 	pause >nul
 	exit %ERRORLEVEL%
@@ -66,10 +74,42 @@ goto :EOF
 for /f "delims=:" %%a in ('findstr /n /c:"%~3" "%~f2"') do set "line=%%a"
 echo [91m ERROR: %ERRORLEVEL% - %3 [0m
 echo [91m  File: %~f2:!line! [0m
+set /A n = 1
+set /A min = !line!-6
+set /A max = !line!+6
+for /f "delims=" %%a in ('findstr /n "^" "%~f2"') do (
+	if !n! lss !max! (
+		if !n! gtr !min! (
+			set "string=%%a"
+			if !n! equ !line! (
+				echo [30;41m^>	!n!:	!string:~2,-1%![0m
+			) else (
+				echo		!n!:	!string:~2,-1%!
+			)
+		)
+	)		
+	set /a n+=1
+)
 exit /b %ERRORLEVEL%
 
 :FATAL
 for /f "delims=:" %%a in ('findstr /n /c:"%~3" "%~f2"') do set "line=%%a"
-echo [91m FATAL: %ERRORLEVEL% - %3 [0m
-echo [91m  File: %~f2:!line! [0m
+echo [31m FATAL: %ERRORLEVEL% - %3 [0m
+echo [31m  File: %~f2:!line! [0m
+set /A n = 1
+set /A min = !line!-6
+set /A max = !line!+6
+for /f "delims=" %%a in ('findstr /n "^" "%~f2"') do (
+	if !n! lss !max! (
+		if !n! gtr !min! (
+			set "string=%%a"
+			if !n! equ !line! (
+				echo [30;41m^>	!n!:	!string:~2,-1%![0m
+			) else (
+				echo		!n!:	!string:~2,-1%!
+			)
+		)
+	)		
+	set /a n+=1
+)
 exit /b %ERRORLEVEL%
