2c1762aebf83fa2613faa27ba3416f3c5018b71f
aquawicket <aquawicket@hotmail.com>
2022-02-14 13:08:52 -0800

    commit from git

--- DKPlugins/_DKIMPORT/main.cpp
+++ DKPlugins/_DKIMPORT/main.cpp
@@ -1,16 +1,18 @@
-//#if HAVE_sdl2
+#if HAVE_DKSDLWindow
 #if !IOS && !ANDROID
 	#define SDL_MAIN_HANDLED
 #else
 	#include "SDL_main.h"
 #endif
-//#endif
+#endif
 #include "DK/DKApp.h"
 
 int main(int argc, char** argv)
 {
+#if HAVE_DKSDLWindow
 #if !IOS
 	SDL_SetMainReady();
+#endif
 #endif
 	DKApp dkapp(argc, argv);
 	DKApp::Init();
