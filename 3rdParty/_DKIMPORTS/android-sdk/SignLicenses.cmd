af9c5a5ed7b366f7ba91e2d3993c996dd42f3e89
aquawicket <aquawicket@hotmail.com>
2022-02-23 06:41:31 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/android-sdk/SignLicenses.cmd
+++ 3rdParty/_DKIMPORTS/android-sdk/SignLicenses.cmd
@@ -3,7 +3,8 @@
 call "C:/Users/%USERNAME%/digitalknob/DK/3rdParty/_DKIMPORTS/jdk8/registerJDK.cmd"
 
 set "SDKMANAGER=C:\Users\%USERNAME%\digitalknob\DK\3rdParty\android-sdk\tools\bin\sdkmanager.bat
-%SDKMANAGER% --licenses
+::%SDKMANAGER% --licenses
+echo y | %SDKMANAGER% --licenses
 
 call "C:/Users/%USERNAME%/digitalknob/DK/3rdParty/_DKIMPORTS/jdk/registerJDK.cmd" 
  
