593c5494d89e138ae809aaeba58d2d1de03ae26c
aquawicket <aquawicket@hotmail.com>
2022-02-23 02:53:54 -0800

    commit from git

--- DKCMake/other/mergeDevelopment.cmd
+++ DKCMake/other/mergeDevelopment.cmd
@@ -7,20 +7,27 @@ if exist "C:\Program Files (x86)\Git\bin\git.exe" set "GIT=C:\Program Files (x86
 if NOT exist "%GIT%" (
 	ERROR "Could not find git"
 )
-
+pause
 echo Merging %branch% into master and pushing to remote
 "%GIT%" checkout %branch%
+pause
 "%GIT%" pull
+pause
 "%GIT%" checkout master
+pause
 "%GIT%" pull origin master
+pause
 "%GIT%" merge --no-ff --no-commit %branch%
+pause
 
 if NOT "%ERRORLEVEL%" == "0" (
 	echo THERE WAN AN ERROR MERGING
+	pause
 	:: If there are conflicts
 	CPP_DK_Execute(GIT + " git status")
 ) else (
 	echo THE MERGE WAS SUCCESSFUL
+	pause
 	:: After conflicts resolved
 	"%GIT%" commit -a -m "Merge %branch% Branch in to Master"
 	"%GIT%" push origin master
