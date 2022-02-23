0ec119003775c7b3fda344222e1482518f84278f
aquawicket <aquawicket@hotmail.com>
2022-02-23 11:41:24 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/zstd/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/zstd/DKMAKE.cmake
@@ -1,5 +1,6 @@
 # https://github.com/facebook/zstd
 
-DKIMPORT(https://github.com/facebook/zstd)
+#DKIMPORT(https://github.com/facebook/zstd)
+DKIMPORT(https://github.com/facebook/zstd/archive/621d79898811d42529dbabebe7a2d7d5cb0e8dcd.zip)
 
 # TODO
