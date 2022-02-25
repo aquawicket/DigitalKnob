a904efd9131b713e11ffd11eef091e9a74517de0
aquawicket <aquawicket@hotmail.com>
2022-02-24 22:56:21 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/osgrmlui/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/osgrmlui/DKMAKE.cmake
@@ -1,5 +1,6 @@
 # https://github.com/triblatron/osgRmlUi
 
-DKIMPORT(https://github.com/triblatron/osgRmlUi)
+#DKIMPORT(https://github.com/triblatron/osgRmlUi)
+DKIMPORT(https://github.com/triblatron/osgRmlUi/archive/621fad4fcbdd01306aba3f5c1112c8b03dc8a382.zip)
 
 #TODO
