7427bc23661f03c444e4662cc955759b70c86575
aquawicket <aquawicket@hotmail.com>
2022-02-24 21:27:10 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/ws2_32.lib/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ws2_32.lib/DKMAKE.cmake
@@ -4,4 +4,10 @@ if(NOT WIN_HOST)
 	return()
 endif()
 
+
+#FIND_LIBRARY(Ws2_32.lib_path Ws2_32.lib)
+#if(NOT Ws2_32.lib_path)
+#	DKWARN("Could not locate Ws2_32.lib")
+#endif()
+#DKINFO("Ws2_32.lib_path = ${Ws2_32.lib_path}")
 WIN_DKLIB(Ws2_32.lib)
