f15bf41fcbcce5ff75a510fca83ede6ccd8782ec
aquawicket <aquawicket@hotmail.com>
2022-05-25 05:09:45 -0700

    commit from git

--- DKPlugins/DKRmlIframe/ElementIframe.cpp
+++ DKPlugins/DKRmlIframe/ElementIframe.cpp
@@ -27,14 +27,14 @@
  */
 
 #include "ElementIframe.h"
-#include "../../../Include/RmlUi/Core/ComputedValues.h"
-#include "../../../Include/RmlUi/Core/ElementDocument.h"
-#include "../../../Include/RmlUi/Core/ElementUtilities.h"
-#include "../../../Include/RmlUi/Core/GeometryUtilities.h"
-#include "../../../Include/RmlUi/Core/PropertyIdSet.h"
-#include "../../../Include/RmlUi/Core/StyleSheet.h"
-#include "../../../Include/RmlUi/Core/URL.h"
-#include "../TextureDatabase.h"
+#include "../Include/RmlUi/Core/ComputedValues.h"
+#include "../Include/RmlUi/Core/ElementDocument.h"
+#include "../Include/RmlUi/Core/ElementUtilities.h"
+#include "../Include/RmlUi/Core/GeometryUtilities.h"
+#include "../Include/RmlUi/Core/PropertyIdSet.h"
+#include "../Include/RmlUi/Core/StyleSheet.h"
+#include "../Include/RmlUi/Core/URL.h"
+#include "../Source/Core/TextureDatabase.h"
 
 namespace Rml {
 
