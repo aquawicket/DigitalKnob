4bef812c2368e314f5618b5a057573e36edaa3f6
aquawicket <aquawicket@hotmail.com>
2022-06-07 02:16:39 -0700

    commit from git

---
+++ DKPlugins/DK/DKTextColor.cpp
@@ -0,0 +1,7 @@
+#include "DK/stdafx.h"
+#include "DKTextColor.h"
+
+bool DKTextColor::Test(int& test){
+	DKDEBUGFUNC(test);
+	return true;
+}
---
+++ DKPlugins/DK/DKTextColor.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef DKTextColor_H
+#define DKTextColor_H
+
+class DKTextColor{
+public:
+	static bool Test(int& test);
+};
+
+#endif //DKTextColor_H
