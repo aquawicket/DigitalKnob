5049168542d442513046611d16edc33ae62e7c45
aquawicket <aquawicket@hotmail.com>
2022-06-07 04:36:13 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/_TEMPLATE-LIB_/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/_TEMPLATE-LIB_/DKMAKE.cmake
@@ -1,44 +1,35 @@
-# http://www.library.com
+# https://github.com/name/library
 #
 # http://www.library.com/downloads/library-1.0.0.zip
+
 
 ### DEPENDS ###
 #DKDEPEND(depend_name)
 
 
-### VERSION ###
-DKSET(LIBRARY_VERSION 1.0.0)
-DKSET(LIBRARY_NAME library-${LIBRARY_VERSION})
-DKSET(LIBRARY_DL http://www.library.com/downloads/${LIBRARY_NAME}.zip)
-DKSET(LIBRARY ${3RDPARTY}/${LIBRARY_NAME})
-
-
-### INSTALL ###
-DKINSTALL(${LIBRARY_DL} library ${LIBRARY})
+### DOWNLOAD ###
+DKIMPORT(https://github.com/name/library.git)
+#DKIMPORT(https://github.com/name/library/releases/download/1.0.0/library-1.0.0.zip)
 
 
 ### LINK ###
 DKINCLUDE(${LIBRARY}/include)
-WIN_DEBUG_DKLIB(${LIBRARY}/${OS}/${DEBUG_DIR}/libraryd.lib)
+WIN_DEBUG_DKLIB(${LIBRARY}/${OS}/${DEBUG_DIR}/library.lib)
 WIN_RELEASE_DKLIB(${LIBRARY}/${OS}/${RELEASE_DIR}/library.lib)
-APPLE_DEBUG_DKLIB(${LIBRARY}/${OS}/${DEBUG_DIR}/liblibraryd.a)
-APPLE_RELEASE_DKLIB(${LIBRARY}/${OS}/${RELEASE_DIR}/liblibrary.a)
-LINUX_DEBUG_DKLIB(${LIBRARY}/${OS}/${DEBUG_DIR}/liblibraryd.a)
-LINUX_RELEASE_DKLIB(${LIBRARY}/${OS}/${RELEASE_DIR}/liblibrary.a)
-RASPBERRY_DEBUG_DKLIB(${LIBRARY}/${OS}/${DEBUG_DIR}/liblibraryd.a)
-RASPBERRY_RELEASE_DKLIB(${LIBRARY}/${OS}/${RELEASE_DIR}/liblibrary.a)
-ANDROID_DEBUG_DKLIB(${LIBRARY}/${OS}/${DEBUG_DIR}/liblibraryd.a)
-ANDROID_RELEASE_DKLIB(${LIBRARY}/${OS}/${RELEASE_DIR}/liblibrary.a)
+UNIX_DEBUG_DKLIB(${LIBRARY}/${OS}/${DEBUG_DIR}/library.a)
+UNIX_RELEASE_DKLIB(${LIBRARY}/${OS}/${RELEASE_DIR}/library.a)
+
+
+### GENERATE ###
+DKSETPATH(${LIBRARY}/${BUILD_DIR})
+DKQCOMMAND(${DKCMAKE_BUILD} ${LIBRARY})
 
 
 ### COMPILE ###
-DKSETPATH(${LIBRARY}/${BUILD_DIR})
-DKCOMMAND(${DKCMAKE_BUILD} ${LIBRARY})
-
 WIN_VS(${LIBRARY_NAME} library.sln library)
 MAC_XCODE(${LIBRARY_NAME} library)
 IOS_XCODE(${LIBRARY_NAME} library)
 IOSSIM_XCODE(${LIBRARY_NAME} library)
-LINUX_DKCOMMAND(make library)
-RASPBERRY_DKCOMMAND(make library)
+LINUX_DKQCOMMAND(make library)
+RASPBERRY_DKQCOMMAND(make library)
 ANDROID_VS(${LIBRARY_NAME} library.sln library)
--- 3rdParty/_DKIMPORTS/fmt/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/fmt/DKMAKE.cmake
@@ -2,4 +2,28 @@
 
 DKGITCLONE(https://github.com/fmtlib/fmt.git)
 
-DKINCLUDE(${FMT})
+# Head only includes
+DKINCLUDE(${FMT}/include)
+
+#TODO: this library can be compiled
+
+### LINK ###
+WIN_DEBUG_DKLIB(${FMT}/${OS}/${DEBUG_DIR}/fmt.lib)
+WIN_RELEASE_DKLIB(${FMT}/${OS}/${RELEASE_DIR}/fmt.lib)
+UNIX_DEBUG_DKLIB(${FMT}/${OS}/${DEBUG_DIR}/fmt.a)
+UNIX_RELEASE_DKLIB(${FMT}/${OS}/${RELEASE_DIR}/fmt.a)
+
+
+### GENERATE ###
+DKSETPATH(${FMT}/${BUILD_DIR})
+DKQCOMMAND(${DKCMAKE_BUILD} ${FMT})
+
+
+### COMPILE ###
+WIN_VS(${FMT_NAME} fmt.sln fmt)
+MAC_XCODE(${FMT_NAME} fmt)
+IOS_XCODE(${FMT_NAME} fmt)
+IOSSIM_XCODE(${FMT_NAME} fmt)
+LINUX_DKQCOMMAND(make fmt)
+RASPBERRY_DKQCOMMAND(make fmt)
+ANDROID_VS(${FMT_NAME} fmt.sln fmt)
