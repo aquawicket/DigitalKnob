b3d075de9e8074ee5692bcee2a0c23f3933b2206
aquawicket <aquawicket@hotmail.com>
2022-06-07 09:13:54 -0700

    git commit

--- DKPlugins/DKArchive/DKArchiveJS.cpp
+++ DKPlugins/DKArchive/DKArchiveJS.cpp
@@ -29,12 +29,14 @@
 #include "DKArchive/DKArchiveJS.h"
 
 bool DKArchiveJS::Init(){
+	DKDEBUGFUNC();
 	DKDuktape::AttachFunction("CPP_DKArchive_Compress", DKArchiveJS::Compress);
 	DKDuktape::AttachFunction("CPP_DKArchive_Extract", DKArchiveJS::Extract);
 	return true;
 }
 
 int DKArchiveJS::Compress(duk_context* ctx){
+	DKDEBUGFUNC(ctx);
 	DKString path = duk_require_string(ctx, 0);
 	DKString file = duk_require_string(ctx, 1);
 	if(!DKArchive::Compress(path, file))
@@ -43,6 +45,7 @@ int DKArchiveJS::Compress(duk_context* ctx){
 }
 
 int DKArchiveJS::Extract(duk_context* ctx){
+	DKDEBUGFUNC(ctx);
 	DKString file = duk_require_string(ctx, 0);
 	DKString path = duk_require_string(ctx, 1);
 	if(!DKArchive::Extract(file, path)) 
