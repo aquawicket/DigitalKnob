35cde93aa5dd556d8b3377a5c4dc1368ba3f3769
aquawicket <aquawicket@hotmail.com>
2022-06-08 12:31:44 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/fmt/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/fmt/DKMAKE.cmake
@@ -1,30 +1,25 @@
 # https://github.com/fmtlib/fmt
 # https://fmt.dev/latest/index.html
 
+### DEPENDS ###
+DKDEPEND(python)
+
+### IMPORT ###
 DKIMPORT(https://github.com/fmtlib/fmt.git)
 
-# Head only includes
+### INCLUDE/LINK ###
 DKINCLUDE(${FMT}/include)
-
-#TODO: this library can be compiled
-
-### LINK ###
 WIN_DEBUG_DKLIB(${FMT}/${OS}/${DEBUG_DIR}/fmtd.lib)
 WIN_RELEASE_DKLIB(${FMT}/${OS}/${RELEASE_DIR}/fmt.lib)
 UNIX_DEBUG_DKLIB(${FMT}/${OS}/${DEBUG_DIR}/libfmt.a)
 UNIX_RELEASE_DKLIB(${FMT}/${OS}/${RELEASE_DIR}/libfmt.a)
 
-
 ### GENERATE ###
 DKSETPATH(${FMT}/${BUILD_DIR})
 DKQCOMMAND(${DKCMAKE_BUILD} -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE ${FMT})
 
-
 ### COMPILE ###
-WIN_VS(${FMT_NAME} fmt.sln fmt)
-MAC_XCODE(${FMT_NAME} fmt)
-IOS_XCODE(${FMT_NAME} fmt)
-IOSSIM_XCODE(${FMT_NAME} fmt)
-LINUX_DKQCOMMAND(make fmt)
+VS(${FMT_NAME} fmt.sln fmt)  # windows, android
+XCODE(${FMT_NAME} fmt)       # mac, ios, iossim
+LINUX_DKQCOMMAND(make fmt)   
 RASPBERRY_DKQCOMMAND(make fmt)
-ANDROID_VS(${FMT_NAME} fmt.sln fmt)
