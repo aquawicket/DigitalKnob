96fb041cf62d04e80b04ae724da662d1e0301282
aquawicket <aquawicket@hotmail.com>
2022-06-18 19:24:57 -0700

    git commit

--- DKCMake/dk_info.cmake
+++ DKCMake/dk_info.cmake
@@ -2,9 +2,9 @@ dk_load(Color)
 
 # dk_info(msg)
 #
-macro(dk_info msg)
+macro(dk_info)
 	dk_call(dk_updateLogInfo)
-	message(STATUS ${H_black}${STACK_HEADER}${CLR}${white}${msg}${CLR})
+	message(STATUS ${H_black}${STACK_HEADER}${CLR}${white}${ARGV}${CLR})
 endmacro()
 macro(DKINFO)
 	dk_info(${ARGV})
