21de828711d226a689af1a130a0d50bd89ba9766
aquawicket <aquawicket@hotmail.com>
2022-06-18 17:08:16 -0700

    git commit

--- 3rdParty/_DKIMPORTS/python/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/python/DKMAKE.cmake
@@ -9,24 +9,24 @@
 #LINUX_HOST_DKIMPORT		(PYTHON_DL https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
 #RASPBERRY_HOST_DKIMPORT	(PYTHON_DL https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
 
-WIN_dk_set		(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18.msi)
-MAC_dk_set		(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18-macosx10.9.pkg)
+WIN_dk_set			(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18.msi)
+MAC_dk_set			(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18-macosx10.9.pkg)
 LINUX_dk_set		(PYTHON_DL https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
 RASPBERRY_dk_set	(PYTHON_DL https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
 
 dk_set(PYTHON_NAME python-2.7.18)
 dk_set(PYTHON ${3RDPARTY}/${PYTHON_NAME})
 
-WIN_dk_set		(PYTHON_APP ${PYTHON}/python.exe)
-MAC_dk_set		(PYTHON_APP ${PYTHON}/python.app)
+WIN_dk_set			(PYTHON_APP ${PYTHON}/python.exe)
+MAC_dk_set			(PYTHON_APP ${PYTHON}/python.app)
 LINUX_dk_set		(PYTHON_APP ${PYTHON}/python)
 RASPBERRY_dk_set	(PYTHON_APP ${PYTHON}/python)
 
 ### INSTALL ###
 IF(WIN AND NOT EXISTS ${PYTHON_APP})
-	dk_download(${PYTHON_DL} ${DKDOWNLOAD}/${PYTHON_NAME}.msi)
-	dk_command(cmd /c ${DKDOWNLOAD}/${PYTHON_NAME}.msi /passive PrependPath=1 TargetDir=${PYTHON})
-	string(REPLACE "/" "\\" PYTHON_PATH ${PYTHON})
+	dk_download(${PYTHON_DL} ${DKDOWNLOAD}/python-2.7.18.msi)
+	dk_command(cmd /c ${DKDOWNLOAD}/python-2.7.18.msi /passive PrependPath=1 TargetDir=${PYTHON})
+	#string(REPLACE "/" "\\" PYTHON_PATH ${PYTHON})
 	dk_setEnv("PATH" "${PYTHON}")
 ENDIF()
 IF(MAC AND NOT EXISTS ${PYTHON_APP})
