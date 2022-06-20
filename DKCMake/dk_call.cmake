0f3003b3d4266c197b1d2bf9d5ea8c85cfdee253
aquawicket <aquawicket@hotmail.com>
2022-06-20 00:13:04 -0700

    commit from git

--- DKCMake/dk_call.cmake
+++ DKCMake/dk_call.cmake
@@ -1,12 +1,11 @@
 include_guard()
+include(${DKCMAKE}/dk_load.cmake)
 
 ##################################################################################
 # dk_Call(func)
 #
 #	load a ${func}.cmake file located in the DKCMake path amd call the function 
 #
-include(${DIGITALKNOB}/DK/DKCMake/dk_load.cmake)
-
 macro(dk_call func)
 	dk_load(${func})
 	cmake_language(CALL ${func} ${ARGV})
