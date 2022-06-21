c5658fe373ba02e668b30f429e1ac565be9e9998
aquawicket <aquawicket@hotmail.com>
2022-06-20 23:22:43 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/android-cmake/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-cmake/DKMAKE.cmake
@@ -3,8 +3,8 @@
 
 #dk_import(https://dl.google.com/android/repository/7c386a739f915f5bd60051f2572c24782388e807.cmake-3.18.1-windows.zip)
 
-WIN_dk_set(ANDROID_CMAKE_DL https://dl.google.com/android/repository/7c386a739f915f5bd60051f2572c24782388e807.cmake-3.18.1-windows.zip)
-WIN_dk_set(ANDROID_CMAKE_NAME android-sdk/cmake)
-WIN_dk_set(ANDROID_CMAKE ${3RDPARTY}/${ANDROID_CMAKE_NAME})
-dk_makeDirectory(${ANDROID_CMAKE})
-WIN_dk_install(${ANDROID_CMAKE_DL} android-cmake ${ANDROID_CMAKE})
+dk_set(ANDROID_CMAKE_DL https://dl.google.com/android/repository/7c386a739f915f5bd60051f2572c24782388e807.cmake-3.18.1-windows.zip)
+dk_set(ANDROID_CMAKE_NAME android-sdk/cmake)
+dk_set(ANDROID_CMAKE ${3RDPARTY}/${ANDROID_CMAKE_NAME})
+#dk_makeDirectory(${ANDROID_CMAKE})
+dk_install(${ANDROID_CMAKE_DL} android-cmake ${ANDROID_CMAKE})
