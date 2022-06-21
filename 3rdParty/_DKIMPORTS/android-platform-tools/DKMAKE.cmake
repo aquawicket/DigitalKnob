8d8434d9f22acf2024cb8074f055eed205a87f17
aquawicket <aquawicket@hotmail.com>
2022-06-21 00:54:01 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
@@ -1,15 +1,12 @@
-if(NOT WIN_HOST)
-	return()
-endif()
-
 # https://dl.google.com/android/repository/platform-tools_r31.0.3-windows.zip
 # https://dl.google.com/android/repository/platform-tools_r31.0.3-darwin.zip
 # https://dl.google.com/android/repository/platform-tools_r31.0.3-linux.zip
 
 
-#WIN_HOST_DKIMPORT	(ANDROIDPLATFORMTOOLS_DL https://dl.google.com/android/repository/platform-tools_r${ANDROIDPLATFORMTOOLS_VERSION}-windows.zip)
-#MAC_HOST_DKIMPORT	(ANDROIDPLATFORMTOOLS_DL https://dl.google.com/android/repository/platform-tools_r${ANDROIDPLATFORMTOOLS_VERSION}-darwin.zip)
-#LINUX_HOST_DKIMPORT	(ANDROIDPLATFORMTOOLS_DL https://dl.google.com/android/repository/platform-tools_r${ANDROIDPLATFORMTOOLS_VERSION}-linux.zip)
+#WIN_HOST_DKIMPORT	(ANDROIDPLATFORMTOOLS_DL https://dl.google.com/android/repository/platform-tools_r31.0.3-windows.zip)
+#MAC_HOST_DKIMPORT	(ANDROIDPLATFORMTOOLS_DL https://dl.google.com/android/repository/platform-tools_r31.0.3-darwin.zip)
+#LINUX_HOST_DKIMPORT(ANDROIDPLATFORMTOOLS_DL https://dl.google.com/android/repository/platform-tools_r31.0.3-linux.zip)
+
 
 dk_set(ANDROIDPLATFORMTOOLS_VERSION 31.0.3)
 WIN_HOST_dk_set(ANDROIDPLATFORMTOOLS_DL https://dl.google.com/android/repository/platform-tools_r${ANDROIDPLATFORMTOOLS_VERSION}-windows.zip)
