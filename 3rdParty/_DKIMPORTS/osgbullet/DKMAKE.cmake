2b7ea0afd7acf9374472b09afa6be4af2a4b40ec
aquawicket <aquawicket@hotmail.com>
2022-06-21 22:53:02 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/osgbullet/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/osgbullet/DKMAKE.cmake
@@ -1,27 +1,24 @@
-### DKDEPENDS ###
+# osgbullet
+
 #dk_depend(bullet)
 dk_depend(bullet3)
 dk_depend(osgworks)
 
 
+#dk_install(http://TODO.com/osgbullet-master.zip osgbullet-master)
+#dk_set(OSGBULLET ${3RDPARTY}/osgbullet-master)
+dk_import(http://TODO.com/osgbullet-master.zip)
 
-#dk_import(http://TODO.com/osgbullet-master.zip)
-
-
-dk_install(http://TODO.com/osgbullet-master.zip osgbullet-master)
-dk_set(OSGBULLET ${3RDPARTY}/osgbullet-master)
+dk_set(OSGBULLET_GL2 ${3RDPARTY}/osgbullet-master-GL2)
+if(OpenGL2)
+	dk_set(OSGBULLET OSGBULLET_GL2)
+endif(OpenGL2)
 
 
 ### LINK ###
-dk_set(OSGBULLET_GL2 ${3RDPARTY}/osgbullet-master-GL2)
-IF(OpenGL2)
-	dk_set(OSGBULLET OSGBULLET_GL2)
-ENDIF(OpenGL2)
-
 dk_include(${OSGBULLET}/include)
 dk_define(OSGBULLET_STATIC)
-
-IF(osgbCollision)
+if(osgbCollision)
 	dk_define(OSGBCOLLISION_LIBRARY)	
 	WIN_dk_libDebug(${OSGBULLET}/${OS}/lib/Debug/osgbCollisiond.lib)
 	WIN_dk_libRelease(${OSGBULLET}/${OS}/lib/Release/osgbCollision.lib)
@@ -31,9 +28,9 @@ IF(osgbCollision)
 	LINUX_dk_libRelease(${OSGBULLET}/${OS}/Release/lib/libosgbCollision.a)
 	ANDROID_dk_libDebug(${OSGBULLET}/${OS}/Debug/obj/local/armeabi-v7a/libosgbCollision.a)
 	ANDROID_dk_libRelease(${OSGBULLET}/${OS}/Release/obj/local/armeabi-v7a/libosgbCollision.a)
-ENDIF(osgbCollision)
+endif()
 	
-IF(osgbDynamics)
+if(osgbDynamics)
 	dk_define(OSGBDYNAMICS_LIBRARY)
 	WIN_dk_libDebug(${OSGBULLET}/${OS}/lib/Debug/osgbDynamicsd.lib)
 	WIN_dk_libRelease(${OSGBULLET}/${OS}/lib/Release/osgbDynamics.lib)
@@ -43,9 +40,9 @@ IF(osgbDynamics)
 	LINUX_dk_libRelease(${OSGBULLET}/${OS}/Release/lib/libosgbDynamics.a)
 	ANDROID_dk_libDebug(${OSGBULLET}/${OS}/Debug/obj/local/armeabi-v7a/libosgbDynamics.a)
 	ANDROID_dk_libRelease(${OSGBULLET}/${OS}/Release/obj/local/armeabi-v7a/libosgbDynamics.a)
-ENDIF(osgbDynamics)
+endif()
 	
-IF(osgbInteraction)
+if(osgbInteraction)
 	dk_define(OSGBINTERACTION_LIBRARY)
 	WIN_dk_libDebug(${OSGBULLET}/${OS}/lib/Debug/osgbInteractiond.lib)
 	WIN_dk_libRelease(${OSGBULLET}/${OS}/lib/Release/osgbInteraction.lib)
@@ -55,9 +52,9 @@ IF(osgbInteraction)
 	LINUX_dk_libRelease(${OSGBULLET}/${OS}/Release/lib/libosgbInteraction.a)
 	ANDROID_dk_libDebug(${OSGBULLET}/${OS}/Debug/obj/local/armeabi-v7a/libosgbInteraction.a)
 	ANDROID_dk_libRelease(${OSGBULLET}/${OS}/Release/obj/local/armeabi-v7a/libosgbInteraction.a)
-ENDIF(osgbInteraction)
+endif()
 	
-IF(osgdb_osgbdynamics)
+if(osgdb_osgbdynamics)
 	WIN_dk_libDebug(${OSGBULLET}/${OS}/lib/Debug/osgdb_osgbdynamicsd.lib)
 	WIN_dk_libRelease(${OSGBULLET}/${OS}/lib/Release/osgdb_osgbdynamics.lib)
 	APPLE_dk_libDebug(${OSGBULLET}/${OS}/lib/${DEBUG}/osgdb_osgbdynamics.a)
@@ -66,9 +63,9 @@ IF(osgdb_osgbdynamics)
 	LINUX_dk_libRelease(${OSGBULLET}/${OS}/Release/lib/osgdb_osgbdynamics.a)
 	ANDROID_dk_libDebug(${OSGBULLET}/${OS}/Debug/obj/local/armeabi-v7a/libosgdb_osgbdynamics.a)
 	ANDROID_dk_libRelease(${OSGBULLET}/${OS}/Release/obj/local/armeabi-v7a/libosgdb_osgbdynamics.a)
-ENDIF(osgdb_osgbdynamics)
+endif()
 	
-IF(osgdb_sgb)
+if(osgdb_sgb)
 	WIN_dk_libDebug(${OSGBULLET}/${OS}/lib/Debug/osgdb_sgbd.lib)
 	WIN_dk_libRelease(${OSGBULLET}/${OS}/lib/Release/osgdb_sgb.lib)
 	APPLE_dk_libDebug(${OSGBULLET}/${OS}/lib/${DEBUG}/osgdb_sgb.a)
@@ -77,44 +74,37 @@ IF(osgdb_sgb)
 	LINUX_dk_libRelease(${OSGBULLET}/${OS}/Release/lib/osgdb_sgb.a)
 	ANDROID_dk_libDebug(${OSGBULLET}/${OS}/Debug/obj/local/armeabi-v7a/libosgdb_sgb.a)
 	ANDROID_dk_libRelease(${OSGBULLET}/${OS}/Release/obj/local/armeabi-v7a/libosgdb_sgb.a)
-ENDIF(osgdb_sgb)
+endif()
 
 
-### COMPILE ###
+### GENERATE ###
 dk_setPath(${OSGBULLET}/${BUILD_DIR})
 
 WIN_dk_queueCommand(${DKCMAKE_BUILD} "-DOSGInstallType=Source And Build Tree" -DOSGBuildRoot=${OSG}/${OS} -DOSGSourceRoot=${OSG} "-DBulletInstallType=Source And Build Tree" -DBulletBuildRoot=${BULLET}/${OS} -DBulletSourceRoot=${BULLET} -DosgWorks_DIR=${OSGWORKS}/${OS}/lib -DOSGBULLET_BUILD_APPLICATIONS=OFF -DOSGBULLET_BUILD_EXAMPLES=OFF -DOSGBULLET_INSTALL_DATA=OFF ${OSGBULLET})
-WIN_dk_visualStudioDebug(osgbullet-master osgBullet.sln)
-WIN_dk_visualStudioRelease(osgbullet-master osgBullet.sln)
-
-
-WIN_dk_queueCommand(${DKCMAKE_BUILD} "-DOSGInstallType=Source And Build Tree" -DOSGBuildRoot=${OSG}/${OS} -DOSGSourceRoot=${OSG} "-DBulletInstallType=Source And Build Tree" -DBulletBuildRoot=${BULLET}/${OS} -DBulletSourceRoot=${BULLET} -DosgWorks_DIR=${OSGWORKS}/${OS}/lib -DOSGBULLET_BUILD_APPLICATIONS=OFF -DOSGBULLET_BUILD_EXAMPLES=OFF -DOSGBULLET_INSTALL_DATA=OFF ${OSGBULLET})
-WIN_dk_visualStudioDebug(osgbullet-master osgBullet.sln)
-WIN_dk_visualStudioRelease(osgbullet-master osgBullet.sln)
-
 
 MAC_dk_queueCommand(${DKCMAKE_BUILD} "-DOSGInstallType=Source And Build Tree" -DOSGBuildRoot=${OSG}/${OS} -DOSGSourceRoot=${OSG} "-DBulletInstallType=Source And Build Tree" -DBulletBuildRoot=${BULLET}/${OS} -DBulletSourceRoot=${BULLET} -DosgWorks_DIR=${OSGWORKS}/${OS}/lib -DOSGBULLET_BUILD_APPLICATIONS=OFF -DOSGBULLET_BUILD_EXAMPLES=OFF -DOSGBULLET_INSTALL_DATA=OF ${OSGBULLET})
-MAC_dk_xcodeDebug(osgbullet-master)
-MAC_dk_xcodeRelease(osgbullet-master)
-
 
 IOS_dk_queueCommand(${DKCMAKE_BUILD} "-DOSGInstallType=Source And Build Tree" -DOSGBuildRoot=${OSG}/ios -DOSGSourceRoot=${OSG} "-DBulletInstallType=Source And Build Tree" -DBulletBuildRoot=${BULLET}/ios -DBulletSourceRoot=${BULLET} -DosgWorks_DIR=${OSGWORKS}/ios/lib -DOSGBULLET_BUILD_APPLICATIONS=OFF -DOSGBULLET_BUILD_EXAMPLES=OFF -DOSGBULLET_INSTALL_DATA=OF ${OSGBULLET})
-IOS_dk_xcodeDebug(osgbullet-master)
-IOS_dk_xcodeRelease(osgbullet-master)
-
 
 IOSSIM_dk_queueCommand(${DKCMAKE_BUILD} "-DOSGInstallType=Source And Build Tree" -DOSGBuildRoot=${OSG}/ios-simulator -DOSGSourceRoot=${OSG} "-DBulletInstallType=Source And Build Tree" -DBulletBuildRoot=${BULLET}/ios-simulator -DBulletSourceRoot=${BULLET} -DosgWorks_DIR=${OSGWORKS}/ios-simulator/lib -DOSGBULLET_BUILD_APPLICATIONS=OFF -DOSGBULLET_BUILD_EXAMPLES=OFF -DOSGBULLET_INSTALL_DATA=OF ${OSGBULLET})
-IOSSIM_dk_xcodeDebug(osgbullet-master)
-IOSSIM_dk_xcodeRelease(osgbullet-master)
-
 
 LINUX_DEBUG_dk_queueCommand(${DKCMAKE_BUILD} "-DOSGInstallType=Source And Build Tree" -DOSGBuildRoot=${OSG}/${OS}/Debug -DOSGSourceRoot=${OSG} "-DBulletInstallType=Source And Build Tree" -DBulletBuildRoot=${BULLET}/${OS}/Debug -DBulletSourceRoot=${BULLET} -DosgWorks_DIR=${OSGWORKS}/${OS}/Debug/lib/x86_64-linux-gnu -DOSGBULLET_BUILD_APPLICATIONS=OFF -DOSGBULLET_BUILD_EXAMPLES=OFF -DOSGBULLET_INSTALL_DATA=OFF ${OSGBULLET})
-LINUX_DEBUG_dk_queueCommand(make)
+
 LINUX_RELEASE_dk_queueCommand(${DKCMAKE_BUILD} "-DOSGInstallType=Source And Build Tree" -DOSGBuildRoot=${OSG}/${OS}/Release -DOSGSourceRoot=${OSG} "-DBulletInstallType=Source And Build Tree" -DBulletBuildRoot=${BULLET}/${OS}/Release -DBulletSourceRoot=${BULLET} -DosgWorks_DIR=${OSGWORKS}/${OS}/Release/lib/x86_64-linux-gnu -DOSGBULLET_BUILD_APPLICATIONS=OFF -DOSGBULLET_BUILD_EXAMPLES=OFF -DOSGBULLET_INSTALL_DATA=OFF ${OSGBULLET})
-LINUX_RELEASE_dk_queueCommand(make)
-
-
 
 ANDROID_dk_queueCommand(${DKCMAKE_BUILD} "-DOSGInstallType=Source And Build Tree" -DOSGBuildRoot=${OSG}/${OS} -DOSGSourceRoot=${OSG} "-DBulletInstallType=Source And Build Tree" -DBulletBuildRoot=${BULLET}/${OS} -DBulletSourceRoot=${BULLET} -DosgWorks_DIR=${OSGWORKS}/${OS}/lib -DOSGBULLET_BUILD_APPLICATIONS=OFF -DOSGBULLET_BUILD_EXAMPLES=OFF -DOSGBULLET_INSTALL_DATA=OFF ${OSGBULLET})
+
+
+### COMPILE ###
+WIN_dk_visualStudioDebug(osgbullet-master osgBullet.sln)
+WIN_dk_visualStudioRelease(osgbullet-master osgBullet.sln)
+MAC_dk_xcodeDebug(osgbullet-master)
+MAC_dk_xcodeRelease(osgbullet-master)
+IOS_dk_xcodeDebug(osgbullet-master)
+IOS_dk_xcodeRelease(osgbullet-master)
+IOSSIM_dk_xcodeDebug(osgbullet-master)
+IOSSIM_dk_xcodeRelease(osgbullet-master)
+LINUX_DEBUG_dk_queueCommand(make)
+LINUX_RELEASE_dk_queueCommand(make)
 ANDROID_dk_visualStudioDebug(osgbullet-master osgBullet.sln)
 ANDROID_dk_visualStudioRelease(osgbullet-master osgBullet.sln)
--- 3rdParty/_DKIMPORTS/rlottie/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/rlottie/DKMAKE.cmake
@@ -1,18 +1,14 @@
 # https://github.com/Samsung/rlottie
-#
 # https://github.com/Samsung/rlottie/archive/refs/tags/v0.2.zip
 # https://github.com/Samsung/rlottie/archive/refs/heads/master.zip
-
-
-
-dk_import(https://github.com/Samsung/rlottie.git)
 
 #dk_import(https://github.com/Samsung/rlottie/archive/875626965959d8e269ca22175c8e1ad190696c43.zip)
 #dk_set(RLOTTIE_VERSION 0.2)
 #dk_set(RLOTTIE_DL https://github.com/Samsung/rlottie/archive/refs/tags/v0.2.zip)
 #dk_set(RLOTTIE_NAME rlottie-${RLOTTIE_VERSION})
 #dk_set(RLOTTIE ${3RDPARTY}/${RLOTTIE_NAME})
-#dk_install(${RLOTTIE_DL} rlottie ${RLOTTIE})#
+#dk_install(${RLOTTIE_DL} rlottie ${RLOTTIE})
+dk_import(https://github.com/Samsung/rlottie.git)
 
 
 ### LINK ###
@@ -36,35 +32,27 @@ ANDROID_dk_libRelease(${RLOTTIE}/${OS}/${RELEASE_DIR}/librlottie.a)
 
 ### COMPILE ###
 dk_setPath(${RLOTTIE}/${BUILD_DIR})
-
-
 WIN_dk_queueCommand(${DKCMAKE_BUILD} /EHsc ${RLOTTIE})
-WIN_dk_visualStudio(${RLOTTIE_NAME} rlottie.sln rlottie)
+#MAC_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
+#IOS_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
+#IOSSIM_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
+#LINUX_DEBUG_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
+#LINUX_RELEASE_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
+#RASPBERRY_DEBUG_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
+#RASPBERRY_RELEASE_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
+#ANDROID_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
+UNIX_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
 
 
-MAC_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
-MAC_dk_xcode(${RLOTTIE_NAME} rlottie)
-
-
-IOS_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
-IOS_dk_xcode(${RLOTTIE_NAME} rlottie)
-
-
-IOSSIM_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
-IOSSIM_dk_xcode(${RLOTTIE_NAME} rlottie)
-
-
-LINUX_DEBUG_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
-LINUX_DEBUG_dk_queueCommand(make rlottie)
-LINUX_RELEASE_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
-LINUX_RELEASE_dk_queueCommand(make rlottie)
-
-
-RASPBERRY_DEBUG_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
-RASPBERRY_DEBUG_dk_queueCommand(make rlottie)
-RASPBERRY_RELEASE_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
-RASPBERRY_RELEASE_dk_queueCommand(make rlottie)
-
-
-ANDROID_dk_queueCommand(${DKCMAKE_BUILD} ${RLOTTIE})
-ANDROID_dk_visualStudio(${RLOTTIE_NAME} rlottie.sln rlottie)
+### GENERATE ###
+#WIN_dk_visualStudio(${RLOTTIE_NAME} rlottie.sln rlottie)
+dk_visualStudio(${RLOTTIE_NAME} rlottie.sln rlottie)
+#MAC_dk_xcode(${RLOTTIE_NAME} rlottie)
+#IOS_dk_xcode(${RLOTTIE_NAME} rlottie)
+#IOSSIM_dk_xcode(${RLOTTIE_NAME} rlottie)
+dk_xcode(${RLOTTIE_NAME} rlottie)
+#LINUX_DEBUG_dk_queueCommand(make rlottie)
+LINUX_dk_queueCommand(make rlottie)
+#RASPBERRY_DEBUG_dk_queueCommand(make rlottie)
+RASPBERRY_dk_queueCommand(make rlottie)
+#ANDROID_dk_visualStudio(${RLOTTIE_NAME} rlottie.sln rlottie)
