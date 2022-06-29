8ef07baf2b5989114b8b0e1f4417344ed096130e
aquawicket <aquawicket@hotmail.com>
2022-06-29 09:42:11 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/dl/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/dl/DKMAKE.cmake
@@ -1,11 +1,14 @@
+# C:/Users/Administrator/digitalknob/DK/3rdParty/android-sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/arm-linux-androideabi/libdl.a
 ANDROID_dk_depend(android-ndk)
 
+
 if(dl_processed)
-	FIND_LIBRARY(dl_Lib dl)
-	message(STATUS "\${dl_Lib} = ${dl_Lib}")
-	if(NOT dl_Lib)
-		dk_error("Could not locate dl libraries") 
+	find_library(dl_LIBRARY NAMES dl) #HINTS "C:/Users/Administrator/digitalknob/DK/3rdParty/android-sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/arm-linux-androideabi")
+	if(dl_LIBRARY)
+		dk_info(dl_LIBRARY)
+		dk_lib(${dl_LIBRARY})
+	else()
+		dk_assert("Could not locate dl libraries") 
 	endif()
-	dk_lib(${dl_Lib})
 endif()
 dk_set(dl_processed true)
