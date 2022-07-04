997406a4e57767c21c643854f08fd46adb8c7b57
aquawicket <aquawicket@hotmail.com>
2022-07-04 15:26:38 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/android-studio/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-studio/DKMAKE.cmake
@@ -5,15 +5,15 @@ if(NOT WIN_HOST)
 endif()
 
 
-#dk_depend(openjdk)
+dk_depend(openjdk-8u41)
 #dk_depend(android-sdk)
 dk_depend(android-ndk)
 
 
-dk_set(ANDROID-STUDIO_VERSION "2020.3.1.26-windows")
-dk_set(ANDROID-STUDIO_NAME "android-studio-2020.3.1.26-windows.exe")
-WIN_HOST_dk_set(ANDROID-STUDIO_DL "https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe")
-#WIN_dk_import(https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe)
+#dk_set(ANDROID-STUDIO_VERSION "2020.3.1.26-windows")
+#dk_set(ANDROID-STUDIO_NAME "android-studio-2020.3.1.26-windows.exe")
+#WIN_HOST_dk_set(ANDROID-STUDIO_DL "https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe")
+WIN_dk_import(https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe)
 
 WIN_HOST_dk_set(ANDROID-STUDIO "C:/Program Files/Android/Android Studio/bin")
 WIN_HOST_dk_set(ANDROID-STUDIO_EXE "${ANDROID-STUDIO}/studio.exe")
@@ -24,14 +24,14 @@ MAC_HOST_dk_set(ANDROID-STUDIO_EXE "${ANDROID-STUDIO}/Android Studio.app")
 LINUX_HOST_dk_set(ANDROID-STUDIO "${3RDPARTY}/android-studio/bin")
 LINUX_HOST_dk_set(ANDROID-STUDIO_EXE "${ANDROID-STUDIO}/Android Studio.app")
 
-dk_setEnv("STUDIO_JDK" ${JDK8})
-dk_setEnv("STUDIO_GRADLE_JDK" ${JDK8})
+dk_setEnv("STUDIO_JDK" ${OPENJDK-8U41})
+dk_setEnv("STUDIO_GRADLE_JDK" ${OPENJDK-8U41})
 
 
 ### INSTALL ###
 #dk_import(${ANDROID-STUDIO_DL} ${ANDROID-STUDIO_EXE})
 
 if(NOT EXISTS ${ANDROID-STUDIO_EXE})
-	dk_download(${ANDROID-STUDIO_DL} ${DKDOWNLOAD}/${ANDROID-STUDIO_NAME})
-	dk_command(${DKDOWNLOAD}/${ANDROID-STUDIO_NAME})
+	#dk_download(${ANDROID-STUDIO_DL} ${DKDOWNLOAD}/${ANDROID-STUDIO_NAME})
+	#dk_command(${DKDOWNLOAD}/${ANDROID-STUDIO_NAME})
 endif()
