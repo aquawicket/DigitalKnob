44cac658d20bc80b9b0bfe731bc21a104188de9b
aquawicket <aquawicket@hotmail.com>
2022-07-04 15:59:55 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/android-studio/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-studio/DKMAKE.cmake
@@ -11,9 +11,9 @@ dk_depend(android-ndk)
 
 
 #dk_set(ANDROID-STUDIO_VERSION "2020.3.1.26-windows")
-#dk_set(ANDROID-STUDIO_NAME "android-studio-2020.3.1.26-windows.exe")
-#WIN_HOST_dk_set(ANDROID-STUDIO_DL "https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe")
-WIN_dk_import(https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe)
+dk_set(ANDROID-STUDIO_NAME "android-studio-2020.3.1.26-windows.exe")
+WIN_HOST_dk_set(ANDROID-STUDIO_DL "https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe")
+#WIN_dk_import(https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe)
 
 WIN_HOST_dk_set(ANDROID-STUDIO "C:/Program Files/Android/Android Studio/bin")
 WIN_HOST_dk_set(ANDROID-STUDIO_EXE "${ANDROID-STUDIO}/studio.exe")
@@ -32,6 +32,6 @@ dk_setEnv("STUDIO_GRADLE_JDK" ${OPENJDK-8U41})
 #dk_import(${ANDROID-STUDIO_DL} ${ANDROID-STUDIO_EXE})
 
 if(NOT EXISTS ${ANDROID-STUDIO_EXE})
-	#dk_download(${ANDROID-STUDIO_DL} ${DKDOWNLOAD}/${ANDROID-STUDIO_NAME})
-	#dk_command(${DKDOWNLOAD}/${ANDROID-STUDIO_NAME})
+	dk_download(${ANDROID-STUDIO_DL} ${DKDOWNLOAD}/${ANDROID-STUDIO_NAME})
+	dk_command(${DKDOWNLOAD}/${ANDROID-STUDIO_NAME})
 endif()
