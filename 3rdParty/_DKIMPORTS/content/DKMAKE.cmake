06c16f615702bdb0ed9c0980d27a8af410fe4114
aquawicket <aquawicket@hotmail.com>
2022-07-06 00:39:01 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/content/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/content/DKMAKE.cmake
@@ -2,9 +2,8 @@
 #https://github.com/mdn/content
 
 
-#dk_import(https://github.com/mdn/content.git main)
 dk_import(https://github.com/mdn/content/archive/4a616cb539e1ccece651e171c00e8f8db64fb570.zip)
-
+#dk_import(https://github.com/mdn/content.git main)
 
 #https://www.peterbe.com/plog/how-to-get-all-of-mdn-web-docs-running-locally
 # TODO
