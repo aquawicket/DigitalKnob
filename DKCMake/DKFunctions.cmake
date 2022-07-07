344716b70c3bbd7b923f35761440574029340227
aquawicket <aquawicket@hotmail.com>
2022-07-07 05:10:07 -0700

    commit from git

--- DKCMake/DKFunctions.cmake
+++ DKCMake/DKFunctions.cmake
@@ -666,7 +666,7 @@ function(dk_download src_path dest_path) # ARGV1 = dest_path #NOERROR
 		if(NOT EXISTS ${dest_path})
 			dk_assert("dest_path:(${dest_path}) Could not locate downloaded file")
 		endif()
-		dk_message("${green} Finnished downloading ${dest_filename} ${CLR}")
+		dk_info("${green} Finnished downloading ${dest_filename} ${CLR}")
 	endif() 
 endfunction()
 dk_createOsMacros("dk_download")
@@ -1425,7 +1425,7 @@ function(dk_executeProcess commands)
 		set(command ${commands} WORKING_DIRECTORY ${CURRENT_DIR}) # add WORKING_DIRECTORY if missing
 	endif()	
 	
-	dk_message("\n${magenta}-> ${commands}${CLR}\n")
+	dk_info("\n${magenta} -> ${commands} ${CLR}\n")
 	
 	if(WIN_HOST)
 		execute_process(COMMAND cmd /c ${commands} RESULT_VARIABLE result ERROR_VARIABLE error) # FIXME: Do we always need  cmd /c  here?
