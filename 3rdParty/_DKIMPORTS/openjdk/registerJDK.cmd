53d173208deb295c85a728b79f938840dc7abc53
aquawicket <aquawicket@hotmail.com>
2022-07-11 17:33:18 -0700

    commit from git

--- 3rdParty/_DKIMPORTS/openjdk/registerJDK.cmd
+++ 3rdParty/_DKIMPORTS/openjdk/registerJDK.cmd
@@ -13,6 +13,5 @@ setx STUDIO_GRADLE_JDK %JAVA_HOME%
 reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment" /v CurrentVersion /t REG_SZ /d %CurrentVersion% /f
 reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%CurrentVersion%" /v JavaHome /t REG_SZ /d "%JAVA_HOME%" /f
 reg add "HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%CurrentVersion%" /v RuntimeLib /t REG_SZ /d "%JAVA_HOME%\bin\server\jvm.dll" /f
- 
- 
+
 %DKEND% 
