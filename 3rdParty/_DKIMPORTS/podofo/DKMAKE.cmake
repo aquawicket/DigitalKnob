7ff13d802c3770b56b4dafcd87b47594bb01b27c
aquawicket <aquawicket@hotmail.com>
2022-08-24 14:52:38 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/podofo/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/podofo/DKMAKE.cmake
@@ -1,17 +1,11 @@
 # http://podofo.sourceforge.net
 
-
 dk_depend(zlib)
 dk_depend(freetype)
 dk_depend(libpng)
 
-dk_import(http://sourceforge.net/projects/podofo/files/podofo/0.9.7/podofo-0.9.7.tar.gz PATCH)
 
-#dk_set(PODOFO_VERSION 0.9.5)
-#dk_set(POFODO_DL http://sourceforge.net/projects/podofo/files/podofo/0.9.7/podofo-0.9.7.tar.gz/download)
-#dk_set(PODOFO ${3RDPARTY}/podofo-${PODOFO_VERSION})
-#dk_import(${POFODO_DL} ${PODOFO})
-
+dk_import(http://sourceforge.net/projects/podofo/files/podofo/0.9.7/podofo-0.9.7.tar.gz)
 
 
 ### LINK ###
