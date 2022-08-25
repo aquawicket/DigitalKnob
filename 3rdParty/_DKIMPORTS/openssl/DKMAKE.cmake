5b579b09a8b32e984e0e831d8fa1cdc7c892f8b8
aquawicket <aquawicket@hotmail.com>
2022-08-25 17:55:14 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/openssl/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/openssl/DKMAKE.cmake
@@ -3,8 +3,10 @@
 # https://www.npcglib.org/~stathis/downloads/openssl-1.0.2h-vs2015.7z
 
 
-dk_import(https://www.npcglib.org/~stathis/downloads/openssl-1.0.2h-vs2015.7z)
-#dk_import(https://github.com/openssl/openssl.git)
+WIN_dk_import(https://www.npcglib.org/~stathis/downloads/openssl-1.0.2h-vs2015.7z)
+if(NOT WIN)
+	dk_import(https://github.com/openssl/openssl.git)
+endif()
 
 
 ### LINK ###
@@ -13,27 +15,27 @@ WIN32_dk_libDebug		(${OPENSSL}/lib/libeay32MTd.lib)
 WIN32_dk_libRelease		(${OPENSSL}/lib/libeay32MT.lib)
 WIN64_dk_libDebug		(${OPENSSL}/lib64/libeay32MTd.lib)
 WIN64_dk_libRelease		(${OPENSSL}/lib64/libeay32MT.lib)
-#APPLE_dk_libDebug		(${3RDPARTY}/openssl-mac/lib/libeay32MT.a)
-#APPLE_dk_libRelease	(${3RDPARTY}/openssl-mac/lib/libeay32MT.a)
-#LINUX_dk_libDebug		(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
-#LINUX_dk_libRelease	(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
-#RASPBERRY_dk_libDebug	(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
-#RASPBERRY_dk_libRelease(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
-#ANDROID_dk_libDebug	(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
-#ANDROID_dk_libRelease	(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
+APPLE_dk_libDebug		(${3RDPARTY}/openssl-mac/lib/libeay32MT.a)
+APPLE_dk_libRelease		(${3RDPARTY}/openssl-mac/lib/libeay32MT.a)
+LINUX_dk_libDebug		(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
+LINUX_dk_libRelease		(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
+RASPBERRY_dk_libDebug	(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
+RASPBERRY_dk_libRelease	(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
+ANDROID_dk_libDebug		(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
+ANDROID_dk_libRelease	(${3RDPARTY}/openssl-linux/lib/libeay32MT.a)
 
 WIN32_dk_libDebug		(${OPENSSL}/lib/ssleay32MTd.lib)
 WIN32_dk_libRelease		(${OPENSSL}/lib/ssleay32MT.lib)
 WIN64_dk_libDebug		(${OPENSSL}/lib64/ssleay32MTd.lib)
 WIN64_dk_libRelease		(${OPENSSL}/lib64/ssleay32MT.lib)
-#APPLE_dk_libDebug		(${3RDPARTY}/openssl-mac/lib/ssleay32MTd.a)
-#APPLE_dk_libRelease	(${3RDPARTY}/openssl-mac/lib/ssleay32MT.a)
-#LINUX_dk_libDebug		(${3RDPARTY}/openssl-linux/lib/ssleay32MTd.a)
-#LINUX_dk_libRelease	(${3RDPARTY}/openssl-linux/lib/ssleay32MT.a)
-#RASPBERRY_dk_libDebug	(${3RDPARTY}/openssl-linux/lib/ssleay32MTd.a)
-#RASPBERRY_dk_libRelease(${3RDPARTY}/openssl-linux/lib/ssleay32MT.a)
-#ANDROID_dk_libDebug	(${3RDPARTY}/openssl-linux/lib/ssleay32MTd.a)
-#ANDROID_dk_libRelease	(${3RDPARTY}/openssl-linux/lib/ssleay32MT.a)
+APPLE_dk_libDebug		(${3RDPARTY}/openssl-mac/lib/ssleay32MTd.a)
+APPLE_dk_libRelease		(${3RDPARTY}/openssl-mac/lib/ssleay32MT.a)
+LINUX_dk_libDebug		(${3RDPARTY}/openssl-linux/lib/ssleay32MTd.a)
+LINUX_dk_libRelease		(${3RDPARTY}/openssl-linux/lib/ssleay32MT.a)
+RASPBERRY_dk_libDebug	(${3RDPARTY}/openssl-linux/lib/ssleay32MTd.a)
+RASPBERRY_dk_libRelease	(${3RDPARTY}/openssl-linux/lib/ssleay32MT.a)
+ANDROID_dk_libDebug		(${3RDPARTY}/openssl-linux/lib/ssleay32MTd.a)
+ANDROID_dk_libRelease	(${3RDPARTY}/openssl-linux/lib/ssleay32MT.a)
 
 
 ### 3RDPARTY LINK ###
