6104b2386d582ddf8511b38be6f87373ff612763
aquawicket <aquawicket@hotmail.com>
2022-08-25 22:40:18 -0400

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -370,7 +370,7 @@ if(ANDROID) # Disabled for Android targets
 	dk_disable(lua)
 	dk_disable(openal)
 	dk_disable(openal-android)
-	#dk_disable(openssl)
+	dk_disable(openssl)
 	dk_disable(podofo)
 	dk_disable(rlottie)
 	dk_disable(rtmidi)
