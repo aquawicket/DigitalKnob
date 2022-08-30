c4c35f9e8f89a8ee9e70614a8403bf1a58216129
aquawicket <aquawicket@hotmail.com>
2022-08-30 15:08:14 -0400

    commit from git

--- DKApps/05_DKCef/assets/app.js
+++ DKApps/05_DKCef/assets/app.js
@@ -1,10 +1,5 @@
 CPP_DK_Create("DKDuktapeDom")
 
 var USE_CEF = 1;
-var url = "chrome://gpu";
-//var url = "https://www.google.com";
-
-var width = 1240;
-var height = 900;
 CPP_DK_Create("DKCef");
-CPP_DKCef_NewBrowser("Cef",0,0,width,height,url);
+CPP_DKCef_NewBrowser("Cef", 0, 0, 1240, 900, height, "chrome://gpu");
