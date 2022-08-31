a422a4815dbedee127ef9bab7ba71af597666ab3
aquawicket <aquawicket@hotmail.com>
2022-08-30 21:09:13 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/openssl/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/openssl/DKMAKE.cmake
@@ -88,3 +88,32 @@ ANDROID_DEBUG_dk_queueMsys(make)
 ANDROID_RELEASE_dk_setPath(${OPENSSL}/${OS}/${RELEASE_DIR})
 ANDROID_RELEASE_dk_queueMsys(../../Configure --no-shared --release)
 ANDROID_RELEASE_dk_queueMsys(make)
+
+
+####### openssl_binary ###############################################
+# https://slproweb.com/products/Win32OpenSSL.html
+# https://slproweb.com/download/Win64OpenSSL-3_0_4.exe
+### INSTALL ###
+# WIN_HOST_dk_set(OPENSSL_BINARY "C:/Program Files/OpenSSL-Win64")
+# WIN_HOST_dk_set(OPENSSL_BINARY_EXE ${OPENSSL_BINARY}/bin/openssl.exe)
+# MAC_HOST_dk_set(OPENSSL_BINARY /usr/bin)
+# MAC_HOST_dk_set(OPENSSL_BINARY_EXE ${OPENSSL_BINARY}/openssl)
+# LINUX_HOST_dk_set(OPENSSL_BINARY /usr/bin)
+# LINUX_HOST_dk_set(OPENSSL_BINARY_EXE ${OPENSSL_BINARY}/openssl)
+# if(NOT EXISTS ${OPENSSL_BINARY_EXE})
+# 	dk_info("Installing openssl_binary")
+# 	if(WIN_HOST)	
+# 		dk_download(https://slproweb.com/download/Win64OpenSSL-3_0_4.exe ${DKDOWNLOAD}/Win64OpenSSL-3_0_4.exe)
+# 		dk_command(${DKDOWNLOAD}/Win64OpenSSL-3_0_4.exe)
+# 	endif()
+# 	if(MAC_HOST)
+# 		dk_command(brew install openssl)
+# 	endif()
+# 	if(LINUX_HOST)
+# 		dk_command(sudo apt-get -y install openssl)
+# 	endif()
+# endif()
+# if(NOT EXISTS "${OPENSSL_BINARY_EXE}")
+# 	dk_assert("openssl_binary IS NOT FOUND OR INVALID")
+# endif()
+###################################################################
