4b706a4b00531ca0db2f9e5d670f5a768c533932
aquawicket <aquawicket@hotmail.com>
2022-08-31 18:09:56 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
@@ -1,9 +1,11 @@
 # https://dl.google.com/android/repository/platform-tools_r31.0.3-windows.zip
 # https://dl.google.com/android/repository/platform-tools_r31.0.3-darwin.zip
 # https://dl.google.com/android/repository/platform-tools_r31.0.3-linux.zip
+# https://mirrors.cloud.tencent.com/AndroidSDK/
 
 dk_depend(android-sdk)
 
 WIN_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r31.0.3-windows.zip PATH ${ANDROID-SDK}/platform-tools)
-MAC_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r31.0.3-darwin.zip PATH ${ANDROID-SDK}/platform-tools)
+#MAC_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r31.0.3-darwin.zip PATH ${ANDROID-SDK}/platform-tools)
+MAC_HOST_dk_import(https://mirrors.cloud.tencent.com/AndroidSDK/platform-tools_r33.0.3-darwin.zip PATH ${ANDROID-SDK}/platform-tools)
 LINUX_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r31.0.3-linux.zip PATH ${ANDROID-SDK}/platform-tools)
