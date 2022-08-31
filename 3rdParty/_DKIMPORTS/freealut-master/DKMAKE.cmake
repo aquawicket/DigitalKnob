0bdbc264e964f0efc856e141947184968b86b8d6
aquawicket <aquawicket@hotmail.com>
2022-08-31 18:02:16 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/freealut-master/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/freealut-master/DKMAKE.cmake
@@ -1,19 +1,20 @@
+return() # migrated to /_DKIMPORTS/freealut/DKMAKE.cmake
 ## https://github.com/vancegroup/freealut
 
 
 ### INSTALL ###
-dk_set(FREEALUT_VERSION master)
-dk_set(FREEALUT_NAME freealut-${FREEALUT_VERSION})
-#dk_set(FREEALUT_DL ???)
-dk_set(FREEALUT ${3RDPARTY}/${FREEALUT_NAME})
-dk_import(${FREEALUT_DL} ${FREEALUT})
+dk_set(FREEALUT-MASTER_VERSION master)
+dk_set(FREEALUT-MASTER_NAME freealut-${FREEALUT-MASTER_VERSION})
+#dk_set(FREEALUT-MASTER_DL ???)
+dk_set(FREEALUT-MASTER ${3RDPARTY}/${FREEALUT-MASTER_NAME})
+dk_import(${FREEALUT-MASTER_DL} ${FREEALUT-MASTER})
 
 
 ### LINK ###
-dk_include(${FREEALUT}/include)
-ANDROID_dk_libDebug(${FREEALUT}/${OS}/${DEBUG_DIR}/obj/local/armeabi-v7a/libalut.a)
-ANDROID_dk_libRelease(${FREEALUT}/${OS}/${RELEASE_DIR}/obj/local/armeabi-v7a/libalut.a)
+dk_include(${FREEALUT-MASTER}/include)
+ANDROID_dk_libDebug(${FREEALUT-MASTER}/${OS}/${DEBUG_DIR}/obj/local/armeabi-v7a/libalut.a)
+ANDROID_dk_libRelease(${FREEALUT-MASTER}/${OS}/${RELEASE_DIR}/obj/local/armeabi-v7a/libalut.a)
 
 
 ### COMPILE ###
-ANDROID_dk_ndk(${FREEALUT_NAME})
+ANDROID_dk_ndk(${FREEALUT-MASTER_NAME})
