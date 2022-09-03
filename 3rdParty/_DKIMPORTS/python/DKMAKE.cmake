f522057c538c5d84e9851682d3bccd971ee770c8
aquawicket <aquawicket@hotmail.com>
2022-09-03 14:41:42 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/python/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/python/DKMAKE.cmake
@@ -5,16 +5,14 @@
 # https://silentinstallhq.com/python-2-7-silent-install-how-to-guide/
 
 
-dk_set				(PYTHON_NAME python-2.7.18)
-dk_set				(PYTHON ${3RDPARTY}/${PYTHON_NAME})
-WIN_dk_set			(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18.msi)
-APPLE_dk_set		(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18-macosx10.9.pkg)
-LINUX_dk_set		(PYTHON_DL https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
-RASPBERRY_dk_set	(PYTHON_DL https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
-WIN_dk_set			(PYTHON_APP ${PYTHON}/python.exe)
-APPLE_dk_set		(PYTHON_APP python)
-LINUX_dk_set		(PYTHON_APP python)
-RASPBERRY_dk_set	(PYTHON_APP python)
+dk_set(PYTHON_NAME python-2.7.18)
+dk_set(PYTHON ${3RDPARTY}/${PYTHON_NAME})
+WIN_dk_set(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18.msi)
+APPLE_dk_set(PYTHON_DL https://www.python.org/ftp/python/2.7.18/python-2.7.18-macosx10.9.pkg)
+WIN_dk_set(PYTHON_APP ${PYTHON}/python.exe)
+APPLE_dk_set(PYTHON_APP python)
+LINUX_dk_set(PYTHON_APP python)
+RASPBERRY_dk_seT(PYTHON_APP python)
 
 
 ### INSTALL ###
@@ -38,11 +36,8 @@ if(MAC AND NOT EXISTS "/Applications/Python\ 2.7")
 	dk_executeProcess(sudo installer -verbose -pkg ${DKDOWNLOAD}/python-2.7.18-macosx10.9.pkg -target /)
 endif()
 if(LINUX AND NOT EXISTS ${PYTHON_APP}) #FIXME
-	#dk_download(${PYTHON_DL} ${DKDOWNLOAD}/Python-2.7.18.tgz)
-	#dk_executeProcess(${DKDOWNLOAD}/Python-2.7.18.tgz)
 	dk_import(https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
 endif()
 if(RASPBERRY AND NOT EXISTS ${PYTHON_APP}) #FIXME
-	dk_download(${PYTHON_DL} ${DKDOWNLOAD}/Python-2.7.18.tgz)
-	dk_executeProcess(${DKDOWNLOAD}/Python-2.7.18.tgz)
+	dk_import(https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz)
 endif()
