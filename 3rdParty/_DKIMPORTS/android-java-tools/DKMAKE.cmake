3705307cd50d01ea97c252a49777e8442cfbb815
aquawicket <aquawicket@hotmail.com>
2022-09-04 17:48:23 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/android-java-tools/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-java-tools/DKMAKE.cmake
@@ -1,10 +1,11 @@
 # https://marketplace.visualstudio.com/items?itemName=VisualCPPTeam.JavaToolsForAndroidProjVS2019
-#
 # https://marketplace.visualstudio.com/_apis/public/gallery/publishers/VisualCPPTeam/vsextensions/JavaToolsForAndroidProjVS2019/2.8/vspackage
 if(NOT WIN_HOST)
 	return()
 endif()
 
+
 dk_depend(visualstudio)
+
 
 dk_import(https://marketplace.visualstudio.com/_apis/public/gallery/publishers/VisualCPPTeam/vsextensions/JavaToolsForAndroidProjVS2019/2.8/vspackage PATH ${3RDPARTY}/androoidjavatools-2.8)
--- 3rdParty/_DKIMPORTS/mobile_core_services/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/mobile_core_services/DKMAKE.cmake
@@ -1,5 +1,5 @@
 # https://developer.apple.com/documentation/mobilecoreservices
-#if(NOT MAC_HOST)
+
 if(NOT IOS AND NOT IOSSIM)
 	dk_undepend(mobile_core_services)
 	return()
