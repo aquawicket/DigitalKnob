06621b7f4515d48c55f1b205cfa7ea8afcd8685f
aquawicket <aquawicket@hotmail.com>
2022-09-07 14:39:42 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/sw-client/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/sw-client/DKMAKE.cmake
@@ -1,6 +1,8 @@
-# https://software-network.org/client/sw-master-windows-client.zip
+# https://software-network.org/client/
 
-dk_import(https://software-network.org/client/sw-master-windows-client.zip)
-dk_set(SW_EXE ${SW-CLIENT}/sw.exe)
+WIN_dk_import(https://software-network.org/client/sw-master-windows-client.zip)
+MAC_dk_import(https://software-network.org/client/sw-master-macos-client.tar.gz)
+LINUX_dk_import(https://software-network.org/client/sw-master-linux-client.tar.gz)
 
-#dk_setEnv("PATH" "${SW-CLIENT}")
+#WIN_dk_setEnv("PATH" "${SW-CLIENT}")
+WIN_dk_set(SW_EXE ${SW-CLIENT}/sw.exe)
