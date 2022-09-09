c9849bae05354774a57ee3e710d8f8c217d4e53e
aquawicket <aquawicket@hotmail.com>
2022-09-09 16:36:26 -0400

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -173,17 +173,16 @@ if(LINUX) # Disabled for Linux targets
 	dk_disable(freealut)				# AL/al.h: No such file or directory
 	dk_disable(giflib)					# aclocal-1.15: command not found
 	dk_disable(imagemagick)				#libimagemagik.a not found
-	#dk_disable(openjdk)
 	dk_disable(jerryscript)
 	dk_disable(kdevelop)
 	dk_disable(leptonica)
 	dk_disable(libtorrent)				# Could not find BOOST
 	dk_disable(libwebp)
 	dk_disable(libwebsockets)
-	dk_disable(libxml2)
-	dk_disable(lighttpd)
-	dk_disable(lua)
-	dk_disable(podofo)
+	#dk_disable(libxml2)
+	#dk_disable(lighttpd)
+	#dk_disable(lua)
+	#dk_disable(podofo)
 	dk_disable(rtmidi)
 	dk_disable(sdl-gpu)
 	dk_disable(sfml)           			# sfml_sfml-network)  #TODO: disabling individual sublibraries
