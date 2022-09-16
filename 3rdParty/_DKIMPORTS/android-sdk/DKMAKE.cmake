8c21a5d31fff9efc0873fbf3ea5c770803637ba6
aquawicket <aquawicket@hotmail.com>
2022-09-16 15:51:14 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/android-sdk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-sdk/DKMAKE.cmake
@@ -8,11 +8,12 @@ endif()
 dk_set(ANDROID-SDK ${3RDPARTY}/android-sdk)
 dk_depend(openjdk)
 dk_depend(openjdk-8u41)
-#dk_depend(openjdk-11)
 dk_depend(android-cmdline-tools)
 
 
-if(NOT EXISTS ${3RDPARTY}/android-sdk)
+if(NOT EXISTS ${ANDROID-SDK})
+	dk_info("Installing android-sdk")
+	dk_wait()
 	dk_set(ANDROID-SDK ${3RDPARTY}/android-sdk)
 	dk_makeDirectory(${ANDROID-SDK})
 	dk_patch(android-sdk ${ANDROID-SDK})
@@ -20,7 +21,6 @@ if(NOT EXISTS ${3RDPARTY}/android-sdk)
 	dk_setEnv("VS_AndroidHome" ${ANDROID-SDK})
 
 	### SignLicenses ###
-	dk_copy(${DKIMPORTS}/android-sdk/SignLicenses.cmd ${ANDROID-SDK}/SignLicenses.cmd OVERWRITE)
 	dk_killProcess(java.exe NOASSERT)
 	dk_killProcess(adb.exe NOASSERT)
 	dk_executeProcess(call "${OPENJDK-8U41}/registerJDK.cmd")
@@ -28,4 +28,10 @@ if(NOT EXISTS ${3RDPARTY}/android-sdk)
 	#dk_executeProcess(call "${OPENJDK}/registerJDK.cmd")
 	dk_executeProcess(call ${ANDROID-SDK}/SignLicenses.cmd)
 	dk_executeProcess(call "${OPENJDK-11}/registerJDK.cmd")
+endif()
+
+### SignLicenses ###
+if(NOT EXISTS ${ANDROID-SDK}/SignLicenses.cmd)
+	dk_copy(${DKIMPORTS}/android-sdk/SignLicenses.cmd ${ANDROID-SDK}/SignLicenses.cmd OVERWRITE)
+	dk_executeProcess(call ${ANDROID-SDK}/SignLicenses.cmd)
 endif()
