b38d68cb3d20551768edf25ec508ddcaf952240b
aquawicket <aquawicket@hotmail.com>
2022-09-26 19:39:30 -0400

    commit from git

--- DKApps/05_DKSDLRml/assets/app.js
+++ DKApps/05_DKSDLRml/assets/app.js
@@ -1,8 +1,8 @@
 CPP_DK_Create("DKDuktapeDom")
 
 //const url = CPP_DKAssets_LocalAssets()+"index.html"
-const url = CPP_DKAssets_LocalAssets()+"DKRmlElement/index.html"
-//const url = CPP_DKAssets_LocalAssets()+"DKWebTest/index.html"
+//const url = CPP_DKAssets_LocalAssets()+"DKRmlElement/index.html"
+const url = CPP_DKAssets_LocalAssets()+"DKWebTest/index.html"
 CPP_DK_Create("DKWindow")
 CPP_DK_Create("DKRml")
 location.href = url
