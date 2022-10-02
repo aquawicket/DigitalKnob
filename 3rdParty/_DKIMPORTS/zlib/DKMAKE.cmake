5be05268d413b453d1c93be9ed745ef0ea3cc734
aquawicket <aquawicket@hotmail.com>
2022-10-02 19:10:06 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/zlib/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/zlib/DKMAKE.cmake
@@ -29,6 +29,6 @@ dk_queueCommand	(${DKCMAKE_BUILD} ${ZLIB})
 
 
 ### COMPILE ###
-dk_visualStudiov(${ZLIB_NAME} zlibstatic) # windows, android
+dk_visualStudio	(${ZLIB_NAME} zlibstatic) # windows, android
 dk_xcode		(${ZLIB_NAME} zlibstatic) # mac, ios, iossim
 dk_make			(${ZLIB_NAME} zlibstatic) # linux, raspberry
