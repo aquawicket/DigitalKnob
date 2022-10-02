e79c11b683533b331a3e11c3453e02bd8acd68bd
aquawicket <aquawicket@hotmail.com>
2022-10-02 17:54:03 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/libcaca/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libcaca/DKMAKE.cmake
@@ -2,32 +2,32 @@
 # http://caca.zoy.org/wiki/libcaca
 
 
+### IMPORT ###
 #dk_import(http://caca.zoy.org/files/libcaca/libcaca-0.99.beta19.tar.gz)
 dk_import(https://github.com/cacalabs/libcaca.git BRANCH main)
 
 
 ### LINK ###
-dk_include(${LIBCACA}/include)
-dk_include(${LIBCACA}/${OS})
-WIN_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.lib)
-WIN_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.lib)
-APPLE_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
-APPLE_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
-LINUX_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
-LINUX_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
-RASPBERRY_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
-RASPBERRY_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
-ANDROID_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
-ANDROID_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+dk_include				(${LIBCACA}/include)
+dk_include				(${LIBCACA}/${OS})
+WIN_dk_libDebug			(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.lib)
+WIN_dk_libRelease		(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.lib)
+APPLE_dk_libDebug		(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
+APPLE_dk_libRelease		(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+LINUX_dk_libDebug		(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
+LINUX_dk_libRelease		(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+RASPBERRY_dk_libDebug	(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
+RASPBERRY_dk_libRelease	(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+ANDROID_dk_libDebug		(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
+ANDROID_dk_libRelease	(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
 
 
 ### GENERATE ####
-dk_setPath(${LIBCACA}/${BUILD_DIR})
-dk_queueCommand(${DKCMAKE_BUILD} ${LIBCACA})
+dk_setPath		(${LIBCACA}/${BUILD_DIR})
+dk_queueCommand	(${DKCMAKE_BUILD} ${LIBCACA})
 
 
 ### COMPILE ###
-dk_visualStudio(${LIBCACA_NAME} libcaca)
-dk_xcode(${LIBCACA_NAME} libcaca)
-LINUX_dk_queueCommand(make libcaca)
-RASPBERRY_dk_queueCommand(make libcaca)
+dk_visualStudio	(${LIBCACA_NAME} libcaca)
+dk_xcode		(${LIBCACA_NAME} libcaca)
+dk_make			(${LIBCACA_NAME} libcaca)
--- 3rdParty/_DKIMPORTS/libjpeg-turbo/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libjpeg-turbo/DKMAKE.cmake
@@ -3,51 +3,46 @@
 # For ANDROID - https://github.com/DeviceFarmer/android-libjpeg-turbo
 # https://sourceforge.net/projects/libjpeg-turbo/files/2.1.1/libjpeg-turbo-2.1.1.tar.gz/download
 
+
+### IMPORT ###
 #dk_import(https://sourceforge.net/projects/libjpeg-turbo/files/2.1.1/libjpeg-turbo-2.1.1.tar.gz ${3RDPARTY}/libjpeg-turbo-2.1.1)
 dk_import(https://github.com/libjpeg-turbo/libjpeg-turbo.git BRANCH main)
 
 
 ### LINK ###
-dk_include(${LIBJPEG-TURBO})
-dk_include(${LIBJPEG-TURBO}/${OS})
-dk_include(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR})
-dk_include(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR})
-ANDROID_dk_include(${LIBJPEG-TURBO}/${OS}/$(BUILD_TYPE)/jni)
-WIN_dk_libDebug(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/turbojpeg-static.lib)
-WIN_dk_libRelease(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/turbojpeg-static.lib)
-APPLE_dk_libDebug(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
-APPLE_dk_libRelease(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
-LINUX_dk_libDebug(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
-LINUX_dk_libRelease(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
-RASPBERRY_dk_libDebug(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
-RASPBERRY_dk_libRelease(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
-##ANDROID_dk_libDebug(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/obj/local/armeabi-v7a/libturbojpeg.a)
-##ANDROID_dk_libRelease(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/obj/local/armeabi-v7a/libturbojpeg.a)
-ANDROID_dk_libDebug(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
-ANDROID_dk_libRelease(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
+dk_include				(${LIBJPEG-TURBO})
+dk_include				(${LIBJPEG-TURBO}/${OS})
+dk_include				(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR})
+dk_include				(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR})
+ANDROID_dk_include		(${LIBJPEG-TURBO}/${OS}/$(BUILD_TYPE)/jni)
+WIN_dk_libDebug			(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/turbojpeg-static.lib)
+WIN_dk_libRelease		(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/turbojpeg-static.lib)
+APPLE_dk_libDebug		(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
+APPLE_dk_libRelease		(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
+LINUX_dk_libDebug		(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
+LINUX_dk_libRelease		(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
+RASPBERRY_dk_libDebug	(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
+RASPBERRY_dk_libRelease	(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
+ANDROID_dk_libDebug		(${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
+ANDROID_dk_libRelease	(${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
 
 
 ### 3RDPARTY LINK ###
-WIN_dk_set(LIBJPEG-TURBO_CMAKE "-DCMAKE_C_FLAGS=/I${LIBJPEG-TURBO}/${OS}" -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_LIBRARY_RELEASE=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/turbojpeg-static.lib)
-APPLE_dk_set(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS} -DJPEG_LIBRARY_RELEASE=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
-LINUX_DEBUG_dk_set(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
-LINUX_RELEASE_dk_set(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
-RASPBERRY_DEBUG_dk_set(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
+WIN_dk_set				(LIBJPEG-TURBO_CMAKE "-DCMAKE_C_FLAGS=/I${LIBJPEG-TURBO}/${OS}" -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_LIBRARY_RELEASE=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/turbojpeg-static.lib)
+APPLE_dk_set			(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS} -DJPEG_LIBRARY_RELEASE=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
+LINUX_DEBUG_dk_set		(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
+LINUX_RELEASE_dk_set	(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
+RASPBERRY_DEBUG_dk_set	(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${DEBUG_DIR}/libturbojpeg.a)
 RASPBERRY_RELEASE_dk_set(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
-ANDROID_dk_set(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
+ANDROID_dk_set			(LIBJPEG-TURBO_CMAKE -DJPEG_INCLUDE_DIR=${LIBJPEG-TURBO} -DJPEG_INCLUDE_DIR2=${LIBJPEG-TURBO}/${OS} -DJPEG_LIBRARY=${LIBJPEG-TURBO}/${OS}/${RELEASE_DIR}/libturbojpeg.a)
 
 
-
+### GENERATE ##
 dk_setPath(${LIBJPEG-TURBO}/${BUILD_DIR})
 dk_queueCommand(${DKCMAKE_BUILD} -DWITH_SIMD=OFF ${LIBJPEG-TURBO})
 
 
-#WIN_dk_visualStudio(${LIBJPEG-TURBO_NAME} turbojpeg-static)
-dk_visualStudio(${LIBJPEG-TURBO_NAME} turbojpeg-static)
-#MAC_dk_xcode(${LIBJPEG-TURBO_NAME} turbojpeg-static)
-#IOS_dk_xcode(${LIBJPEG-TURBO_NAME} turbojpeg-static)
-#IOSSIM_dk_xcode(${LIBJPEG-TURBO_NAME} turbojpeg-static)
-dk_xcode(${LIBJPEG-TURBO_NAME} turbojpeg-static)
-LINUX_dk_queueCommand(make turbojpeg-static)
-RASPBERRY_dk_queueCommand(make turbojpeg-static)
-#ANDROID_dk_visualStudio(${LIBJPEG-TURBO_NAME} turbojpeg-static)
+### COMPILE ###
+dk_visualStudio	(${LIBJPEG-TURBO_NAME} turbojpeg-static)	# windows, android
+dk_xcode		(${LIBJPEG-TURBO_NAME} turbojpeg-static)	# mac, ios, iossim
+dk_make			(${LIBJPEG-TURBO_NAME} turbojpeg-static)	# linux, raspberry
