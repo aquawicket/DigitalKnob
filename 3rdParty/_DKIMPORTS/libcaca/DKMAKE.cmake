5a75a22ce1fceb0999964c194b6aa173c473566a
aquawicket <aquawicket@hotmail.com>
2022-10-01 21:52:43 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/libcaca/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libcaca/DKMAKE.cmake
@@ -2,37 +2,32 @@
 # http://caca.zoy.org/wiki/libcaca
 
 
-dk_import(https://github.com/cacalabs/libcaca)
-#dk_set(CACA_VERSION 0.99.beta19)
-#dk_set(CACA_NAME libcaca-${CACA_VERSION})
-#dk_set(CACA_DL http://caca.zoy.org/files/libcaca/libcaca-0.99.beta19.tar.gz)
-#dk_set(CACA ${3RDPARTY}/${CACA_NAME})
-#dk_import(${CACA_DL} ${CACA})
+#dk_import(http://caca.zoy.org/files/libcaca/libcaca-0.99.beta19.tar.gz)
+dk_import(https://github.com/cacalabs/libcaca.git)
 
 
 ### LINK ###
-dk_include(${CACA}/include)
-dk_include(${CACA}/${OS})
-WIN_dk_libDebug(${CACA}/${OS}/${DEBUG_DIR}/libcacad.lib)
-WIN_dk_libRelease(${CACA}/${OS}/${RELEASE_DIR}/libcaca.lib)
-APPLE_dk_libDebug(${CACA}/${OS}/${DEBUG_DIR}/libcacad.a)
-APPLE_dk_libRelease(${CACA}/${OS}/${RELEASE_DIR}/libcaca.a)
-LINUX_dk_libDebug(${CACA}/${OS}/${DEBUG_DIR}/libcacad.a)
-LINUX_dk_libRelease(${CACA}/${OS}/${RELEASE_DIR}/libcaca.a)
-RASPBERRY_dk_libDebug(${CACA}/${OS}/${DEBUG_DIR}/libcacad.a)
-RASPBERRY_dk_libRelease(${CACA}/${OS}/${RELEASE_DIR}/libcaca.a)
-ANDROID_dk_libDebug(${CACA}/${OS}/${DEBUG_DIR}/libcacad.a)
-ANDROID_dk_libRelease(${CACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+dk_include(${LIBCACA}/include)
+dk_include(${LIBCACA}/${OS})
+WIN_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.lib)
+WIN_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.lib)
+APPLE_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
+APPLE_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+LINUX_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
+LINUX_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+RASPBERRY_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
+RASPBERRY_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+ANDROID_dk_libDebug(${LIBCACA}/${OS}/${DEBUG_DIR}/libcacad.a)
+ANDROID_dk_libRelease(${LIBCACA}/${OS}/${RELEASE_DIR}/libcaca.a)
+
+
+### GENERATE ####
+dk_setPath(${LIBCACA}/${BUILD_DIR})
+dk_queueCommand(${DKCMAKE_BUILD} ${LIBCACA})
 
 
 ### COMPILE ###
-dk_setPath(${CACA}/${OS})
-dk_queueCommand(${DKCMAKE_BUILD} ${CACA})
-
-WIN_dk_visualStudio(${CACA_NAME} libcaca)
-MAC_dk_xcode(${CACA_NAME} libcaca)
-IOS_dk_xcode(${CACA_NAME} libcaca)
-IOSSIM_dk_xcode(${CACA_NAME} libcaca)
+dk_visualStudio(${LIBCACA_NAME} libcaca)
+dk_xcode(${LIBCACA_NAME} libcaca)
 LINUX_dk_queueCommand(make libcaca)
 RASPBERRY_dk_queueCommand(make libcaca)
-ANDROID_dk_visualStudio(${CACA_NAME} libcaca)
