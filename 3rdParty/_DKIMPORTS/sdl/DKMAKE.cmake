7174bd7b2992ca0e314ce9991a74db612686a661
aquawicket <aquawicket@hotmail.com>
2022-10-02 20:24:15 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/sdl/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/sdl/DKMAKE.cmake
@@ -5,10 +5,13 @@
 # https://github.com/libsdl-org/SDL/blob/53141a56b4520489ab64ad0ba2c49fbd1a456901/docs/README-cmake.md
 
 
+### DEPEND ###
 WIN_dk_depend(opengl)
 WIN_dk_depend(winmm.lib)
-WIN_dk_lib(imm32.lib)
-WIN_dk_lib(version.lib)
+#WIN_dk_lib(imm32.lib)
+WIN_dk_depend(imm32.lib)
+#WIN_dk_lib(version.lib)
+WIN_dk_depend(version.lib)
 WIN_dk_lib(Setupapi.lib)
 MAC_dk_depend(appkit)
 MAC_dk_depend(audio_toolbox)
@@ -42,13 +45,12 @@ if(IOS OR IOSSIM)
 	dk_depend(metal)
 	#dk_define(SDL_VIDEO_RENDER_OGL)
 endif()
-    LINUX_dk_depend(opengl)
+LINUX_dk_depend(opengl)
 RASPBERRY_dk_depend(opengl)
-  ANDROID_dk_depend(opengles)
-  ANDROID_dk_depend(opensles)
+ANDROID_dk_depend(opengles)
 
 
-## TODO: we can set up everything and from this we can extract all of the information needed to import the library
+## FIXME: move this information into template DKMAKE.cmake files
 # dk_import(https://github.com/libsdl-org/SDL)
 # SDL         = /3rdParty/SDL                                                                             :the library name and the plugin folder it resides in 
 # SDL_main    = 18032979d892639ad4e03584a40a4d205cd2f5fd                                                  :the id of the origin head
@@ -56,57 +58,58 @@ RASPBERRY_dk_depend(opengl)
 # SDL_DL      = https://github.com/libsdl-org/SDL/archive/18032979d892639ad4e03584a40a4d205cd2f5fd.zip    :the library  download link 
 
 
+### IMPORT ###
 dk_import(https://github.com/libsdl-org/SDL.git BRANCH main)
 
 
-### DKPLUGINS LINK ###
-#ANDROID_dk_define(GL_GLEXT_PROTOTYPES)
-dk_include(${SDL}/include)
-ANDROID_dk_include(${ANDROID-NDK}/sources/android/cpufeatures)
-ANDROID_dk_include(${SDL}/src)
+### LINK ###
+#ANDROID_dk_define	(GL_GLEXT_PROTOTYPES)
+dk_include			(${SDL}/include)
+ANDROID_dk_include	(${ANDROID-NDK}/sources/android/cpufeatures)
+ANDROID_dk_include	(${SDL}/src)
 RASPBERRY_dk_include(/opt/vc/lib)
-LINUX_dk_include(${SDL}/${OS}/${RELEASE_DIR}/include)
+LINUX_dk_include	(${SDL}/${OS}/${RELEASE_DIR}/include)
 
 #dk_addTarget(sdl2 SDL2static)
 #dk_addTarget(sdl2 SDL2main)
 
 #if(sdl2_SDL2static)
-	#WIN_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/SDL2d.lib)
-	WIN_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/SDL2-staticd.lib)
-	#WIN_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/SDL2.lib)
-	WIN_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/SDL2-static.lib)
-	MAC_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
-	MAC_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
-	IOS_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
-	IOS_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
-	IOSSIM_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
-	IOSSIM_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
-	LINUX_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
-	LINUX_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
-	RASPBERRY_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
-	RASPBERRY_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
-	ANDROID_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2.a)
-	ANDROID_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
+	#WIN_dk_libDebug		(${SDL}/${OS}/${DEBUG_DIR}/SDL2d.lib)
+	WIN_dk_libDebug			(${SDL}/${OS}/${DEBUG_DIR}/SDL2-staticd.lib)
+	#WIN_dk_libRelease		(${SDL}/${OS}/${RELEASE_DIR}/SDL2.lib)
+	WIN_dk_libRelease		(${SDL}/${OS}/${RELEASE_DIR}/SDL2-static.lib)
+	MAC_dk_libDebug			(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
+	MAC_dk_libRelease		(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
+	IOS_dk_libDebug			(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
+	IOS_dk_libRelease		(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
+	IOSSIM_dk_libDebug		(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
+	IOSSIM_dk_libRelease	(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
+	LINUX_dk_libDebug		(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
+	LINUX_dk_libRelease		(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
+	RASPBERRY_dk_libDebug	(${SDL}/${OS}/${DEBUG_DIR}/libSDL2d.a)
+	RASPBERRY_dk_libRelease	(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
+	ANDROID_dk_libDebug		(${SDL}/${OS}/${DEBUG_DIR}/libSDL2.a)
+	ANDROID_dk_libRelease	(${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
 #endif()
 
 #ANDROID_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libhidapi.a)
 #ANDROID_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libhidapi.a)
 
 #if(sdl2_SDL2main)
-	#WIN_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/SDL2maind.lib)
-	#WIN_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/SDL2main.lib)
-	#MAC_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
-	#MAC_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
-	IOS_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
-	IOS_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
-	IOSSIM_dk_libDebug(${SDL}/${OS}/Debug-iphonesimulator/libSDL2maind.a)
-	IOSSIM_dk_libRelease(${SDL}/${OS}/Release-iphonesimulator/libSDL2main.a)
-	#LINUX_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
-	#LINUX_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
-	#RASPBERRY_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
+	#WIN_dk_libDebug		(${SDL}/${OS}/${DEBUG_DIR}/SDL2maind.lib)
+	#WIN_dk_libRelease		(${SDL}/${OS}/${RELEASE_DIR}/SDL2main.lib)
+	#MAC_dk_libDebug		(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
+	#MAC_dk_libRelease		(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
+	IOS_dk_libDebug			(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
+	IOS_dk_libRelease		(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
+	IOSSIM_dk_libDebug		(${SDL}/${OS}/Debug-iphonesimulator/libSDL2maind.a)
+	IOSSIM_dk_libRelease	(${SDL}/${OS}/Release-iphonesimulator/libSDL2main.a)
+	#LINUX_dk_libDebug		(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
+	#LINUX_dk_libRelease	(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
+	#RASPBERRY_dk_libDebug	(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
 	#RASPBERRY_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
-	#ANDROID_dk_libDebug(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
-	#ANDROID_dk_libRelease(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
+	#ANDROID_dk_libDebug	(${SDL}/${OS}/${DEBUG_DIR}/libSDL2maind.a)
+	#ANDROID_dk_libRelease	(${SDL}/${OS}/${RELEASE_DIR}/libSDL2main.a)
 #endif()
 
 
@@ -186,72 +189,38 @@ ANDROID_dk_set(SDL_CMAKE
 	-DSDL2_LIBRARY_RELEASE=${SDL}/${OS}/${RELEASE_DIR}/libSDL2.a)
 	
 
-### COMPILE ###
-dk_setPath(${SDL}/${BUILD_DIR})
-
-WIN_dk_queueCommand(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DSDL_LIBC=ON ${SDL})
-#if(sdl2_SDL2static)
-	WIN_dk_visualStudio(${SDL_NAME} SDL2-static)
-#endif()
-#if(sdl2_SDL2main)
-	WIN_dk_visualStudio(${SDL_NAME} SDL2main)
-#endif()
-
-
+### GENERATE ###
+dk_setPath					(${SDL}/${BUILD_DIR})
+WIN_dk_queueCommand			(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DSDL_LIBC=ON ${SDL})
 if(MAC)
 	dk_removeSubstring("-std=c17" "${DKCMAKE_BUILD}" DKCMAKE_BUILD)
 endif()
-MAC_dk_queueCommand(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DSDL_OPENGL=ON -DSDL_METAL=ON -DHAVE_BUILTIN_ICONV=0 -DHAVE_LIBICONV=0 ${ICONV_CMAKE} ${SDL})
-#if(sdl2_SDL2static)
-	MAC_dk_xcode(${SDL_NAME} SDL2-static)
-#endif()
-#if(sdl2_SDL2main)
-	MAC_dk_xcode(${SDL_NAME} SDL2main)
-#endif()
-
-IOS_dk_queueCommand(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DSDL_OPENGLES=ON -DSDL_METAL=ON -DSDL_JOYSTICK=OFF -DSDL_HAPTIC=OFF ${SDL})
-#if(sdl2_SDL2static)
-	IOS_dk_xcode(${SDL_NAME} SDL2-static)
-#endif()
-#if(sdl2_SDL2main)
-	IOS_dk_xcode(${SDL_NAME} SDL2main)
-#endif()
-
-IOSSIM_dk_queueCommand(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DSDL_OPENGLES=ON -DSDL_METAL=ON -DSDL_JOYSTICK=OFF -DSDL_HAPTIC=OFF ${SDL})
-#if(sdl2_SDL2static)
-	IOSSIM_dk_xcode(${SDL_NAME} SDL2-static)
-#endif()
-#if(sdl2_SDL2main)
-	IOSSIM_dk_xcode(${SDL_NAME} SDL2main)
-#endif()
-
-LINUX_dk_queueCommand(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DVIDEO_OPENGLES=OFF -DVIDEO_OPENGL=ON -DDIRECTX=OFF -DVIDEO_WAYLAND=OFF ${SDL})
-#if(sdl2_SDL2static)
-	LINUX_dk_queueCommand(make)
-#endif()
-#if(sdl2_SDL2main)
-	#LINUX_dk_queueCommand(make SDL2main)
-#endif()
-
-
-RASPBERRY_dk_queueCommand(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DVIDEO_OPENGLES=ON ${SDL})
-#if(sdl2_SDL2static)
-	RASPBERRY_dk_queueCommand(make)
-#endif()
-#if(sdl2_SDL2main)
-	#RASPBERRY_RELEASE_dk_queueCommand(make SDL2main)
-#endif()
-
-
-
+MAC_dk_queueCommand			(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DSDL_OPENGL=ON -DSDL_METAL=ON -DHAVE_BUILTIN_ICONV=0 -DHAVE_LIBICONV=0 ${ICONV_CMAKE} ${SDL})
+IOS_dk_queueCommand			(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DSDL_OPENGLES=ON -DSDL_METAL=ON -DSDL_JOYSTICK=OFF -DSDL_HAPTIC=OFF ${SDL})
+IOSSIM_dk_queueCommand		(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DSDL_OPENGLES=ON -DSDL_METAL=ON -DSDL_JOYSTICK=OFF -DSDL_HAPTIC=OFF ${SDL})
+LINUX_dk_queueCommand		(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DVIDEO_OPENGLES=OFF -DVIDEO_OPENGL=ON -DDIRECTX=OFF -DVIDEO_WAYLAND=OFF ${SDL})
+RASPBERRY_dk_queueCommand	(${DKCMAKE_BUILD} -DSDL_SHARED=OFF -DVIDEO_OPENGLES=ON ${SDL})
 if(ANDROID)
-set(DKCMAKE_BUILD_SDL2 ${DKCMAKE_BUILD})
-#dk_removeSubstring("-std=gnu17" "${DKCMAKE_BUILD}" DKCMAKE_BUILD_SDL2)
-#dk_removeSubstring("-std=gnu++1z" "${DKCMAKE_BUILD}" DKCMAKE_BUILD_SDL2)
-dk_removeSubstring("-std=c17" "${DKCMAKE_BUILD_SDL2}" DKCMAKE_BUILD_SDL2)
-dk_removeSubstring("-std=c++1z" "${DKCMAKE_BUILD_SDL2}" DKCMAKE_BUILD_SDL2)
+	set(DKCMAKE_BUILD_SDL2 ${DKCMAKE_BUILD})
+	#dk_removeSubstring("-std=gnu17" "${DKCMAKE_BUILD}" DKCMAKE_BUILD_SDL2)
+	#dk_removeSubstring("-std=gnu++1z" "${DKCMAKE_BUILD}" DKCMAKE_BUILD_SDL2)
+	dk_removeSubstring("-std=c17" "${DKCMAKE_BUILD_SDL2}" DKCMAKE_BUILD_SDL2)
+	dk_removeSubstring("-std=c++1z" "${DKCMAKE_BUILD_SDL2}" DKCMAKE_BUILD_SDL2)
 endif()
-ANDROID32_dk_queueCommand(${DKCMAKE_BUILD_SDL2} -DLIBTYPE=STATIC -DSDL_SHARED=OFF ${SDL}) 
-ANDROID64_dk_queueCommand(${DKCMAKE_BUILD_SDL2} "-DCMAKE_CXX_FLAGS=-DHAVE_GCC_ATOMICS=1" -DLIBTYPE=STATIC -DSDL_SHARED=OFF ${SDL})
-ANDROID_dk_visualStudio(${SDL_NAME} SDL2-static)
-ANDROID_dk_visualStudio(${SDL_NAME} SDL2main)
+ANDROID32_dk_queueCommand	(${DKCMAKE_BUILD_SDL2} -DLIBTYPE=STATIC -DSDL_SHARED=OFF ${SDL}) 
+ANDROID64_dk_queueCommand	(${DKCMAKE_BUILD_SDL2} "-DCMAKE_CXX_FLAGS=-DHAVE_GCC_ATOMICS=1" -DLIBTYPE=STATIC -DSDL_SHARED=OFF ${SDL})
+
+
+### COMPILE ###
+#if(sdl2_SDL2static)
+	dk_visualStudio	(${SDL_NAME} SDL2-static) # windows, android
+	dk_xcode		(${SDL_NAME} SDL2-static) # mac, ios, iossim
+	#LINUX_dk_queueCommand(make)
+	#RASPBERRY_dk_queueCommand(make)
+	dk_make			(${SDL_NAME} SDL2-static) # linux, raspberry
+#endif()
+#if(sdl2_SDL2main)
+	dk_visualStudio	(${SDL_NAME} SDL2main) # windows, android
+	dk_xcode		(${SDL_NAME} SDL2main) # mac, ios, iossim
+	#dk_make		(${SDL_NAME} SDL2main) #linux, raspberry
+#endif()
