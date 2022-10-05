39760b34b4130b12542dec38bbcf3558b836c848
aquawicket <aquawicket@hotmail.com>
2022-10-04 20:12:08 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/sdl_mixer/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/sdl_mixer/DKMAKE.cmake
@@ -2,13 +2,15 @@
 # https://www.libsdl.org/projects/SDL_mixer
 
 
+### DEPEND ###
 dk_depend(smpeg2)
 dk_depend(ogg)
 dk_depend(vorbis)
 
 
+### IMPORT ###
 dk_import(https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.zip PATCH)
-#dk_import(https://github.com/libsdl-org/SDL_mixer.git main PATCH)
+#dk_import(https://github.com/libsdl-org/SDL_mixer.git BRANCH main PATCH)
 
 
 ### DKPLUGINS LINK ###
@@ -21,8 +23,6 @@ LINUX_dk_libDebug		(${SDL_MIXER}/${OS}/${DEBUG_DIR}/lib/SDL_mixer.a)
 LINUX_dk_libRelease		(${SDL_MIXER}/${OS}/${RELEASE_DIR}/lib/SDL_mixer.a)
 RASPBERRY_dk_libDebug	(${SDL_MIXER}/${OS}/${DEBUG_DIR}/lib/SDL_mixer.a)
 RASPBERRY_dk_libRelease	(${SDL_MIXER}/${OS}/${RELEASE_DIR}/lib/SDL_mixer.a)
-#ANDROID_dk_libDebug	(${SDL_MIXER}/${OS}/${DEBUG_DIR}/obj/local/armeabi-v7a/libSDL2_mixer.a)
-#ANDROID_dk_libRelease	(${SDL_MIXER}/${OS}/${RELEASE_DIR}/obj/local/armeabi-v7a/libSDL2_mixer.a)
 ANDROID_dk_libDebug		(${SDL_MIXER}/${OS}/lib/${DEBUG_DIR}/SDL_mixer.a)
 ANDROID_dk_libRelease	(${SDL_MIXER}/${OS}/lib/${RELEASE_DIR}/SDL_mixer.a)
 
@@ -33,7 +33,6 @@ dk_queueCommand	(${DKCMAKE_BUILD} ${SDL_CMAKE} ${VORBIS_CMAKE} ${OGG_CMAKE} ${SM
 
 
 ### COMPILE ###
-dk_visualStudio				(${SDL_MIXER_NAME} SDLMIXER)
-dk_xcode					(${SDL_MIXER_NAME} SDLMIXER)
-LINUX_dk_queueCommand		(make SDLMIXER)
-RASPBERRY_dk_queueCommand	(make SDLMIXER)
+dk_visualStudio	(${SDL_MIXER_NAME} SDLMIXER) # windows, android
+dk_xcode		(${SDL_MIXER_NAME} SDLMIXER) # mac, ios, iossim
+dk_make			(${SDL_MIXER_NAME} SDLMIXER) # linux, raspberry
