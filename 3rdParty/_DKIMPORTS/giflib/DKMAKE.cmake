0bac74b4188d6ad7b8a944e4f176ebed03c2aa72
aquawicket <aquawicket@hotmail.com>
2022-10-04 22:06:53 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/giflib/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/giflib/DKMAKE.cmake
@@ -58,8 +58,9 @@ APPLE_RELEASE_dk_setPath			(${GIFLIB}/${OS}/${RELEASE_DIR})
 APPLE_RELEASE_dk_queueCommand		(${DKCONFIGURE_BUILD})
 APPLE_RELEASE_dk_queueCommand		(make -C lib)
 
-LINUX_DEBUG_dk_setPath				(${GIFLIB}/${OS}/${DEBUG_DIR})
+LINUX_DEBUG_dk_setPath				(${GIFLIB})
 LINUX_DEBUG_dk_queueCommand			(autoreconf -f -i)
+LINUX_DEBUG_dk_setPath				(${GIFLIB}/${OS}/${DEBUG_DIR})
 LINUX_DEBUG_dk_queueCommand			(${DKCONFIGURE_BUILD})
 LINUX_DEBUG_dk_queueCommand			(make -C lib)
 
