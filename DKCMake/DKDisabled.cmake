29e2105f965b62e56154208a0bb9795b46c8ca3e
aquawicket <aquawicket@hotmail.com>
2022-10-09 14:34:26 -0400

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -193,11 +193,11 @@ if(RASPBERRY) # Disabled for Raspberry Pi targets
 	dk_disable(emsdk)				# 64bit source only
 	dk_disable(freealut)			# no ruleto make 'alut_static'
 	dk_disable(java)
-	#dk_disable(jerryscript)
+	dk_disable(jerryscript)			# build errors
 	dk_disable(kdevelop)
 	dk_disable(opencv)				# error C3861: 'random_shuffle': identifier not found
 	dk_disable(openjdk)
-	dk_disable(leptonica)
+	#dk_disable(leptonica)
 	dk_disable(libcaca)				# no CMakeLists.txt
 	dk_disable(libtorrent)			# Could not find BOOST
 	dk_disable(libvncserver)		# error C2065: 'nonBlocking': undeclared identifier
