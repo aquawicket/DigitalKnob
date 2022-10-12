8c8833cbd9d11c5dbb77ac59c5ac824d0ac76800
aquawicket <aquawicket@hotmail.com>
2022-10-12 16:42:02 -0400

    commit from git

--- DKPlugins/DKOSGRml/DKOSGRmlSystem.h
+++ DKPlugins/DKOSGRml/DKOSGRmlSystem.h
@@ -1,28 +1,28 @@
 #pragma once
-#ifndef DKOSGRocketSystem_H
-#define DKOSGRocketSystem_H
+#ifndef DKOSGRmlSystem_H
+#define DKOSGRmlSystem_H
 
-#include <Rocket/Core/Core.h>
+#include <Rml/Core/Core.h>
 #include <osg/Notify>
 #include <osg/Timer>
 
-class DKOSGRocketSystem : public Rocket::Core::SystemInterface
+class DKOSGRmlSystem : public Rml::Core::SystemInterface
 {
 public:
-	DKOSGRocketSystem();
+	DKOSGRmlSystem();
     virtual float GetElapsedTime();
 
 	/// Translate the input string into the translated string.
 	/// @param[out] translated Translated string ready for display.
 	/// @param[in] input String as received from XML.
 	/// @return Number of translations that occured.
-	virtual int TranslateString(Rocket::Core::String& translated, const Rocket::Core::String& input);
+	virtual int TranslateString(Rml::Core::String& translated, const Rml::Core::String& input);
 
 	/// Log the specified message.
 	/// @param[in] type Type of log message, ERROR, WARNING, etc.
 	/// @param[in] message Message to log.
 	/// @return True to continue execution, false to break into the debugger.
-	virtual bool LogMessage(Rocket::Core::Log::Type type, const Rocket::Core::String& message);
+	virtual bool LogMessage(Rml::Core::Log::Type type, const Rml::Core::String& message);
 
 
 	/// Called when this system interface is no longer required.
@@ -31,4 +31,4 @@ public:
 private:
       osg::Timer_t _timer;
 };
-#endif //DKOSGRocketSystem_H
+#endif //DKOSGRmlSystem_H
