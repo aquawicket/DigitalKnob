0cb7b670f7acbfc7232ea24ea13fe5cbbafa4fc3
aquawicket <aquawicket@hotmail.com>
2022-10-12 18:22:34 -0400

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -97,8 +97,7 @@ if(MAC)  # Disabled for MAC targets
 	dk_disable(DKOSGAudio)			# requires openscenegraph, freealut
 	dk_disable(DKOSGCef)			# build errors
 	dk_disable(DKOSGLights)
-	dk_disable(DKOSGManipulator)
-	#dk_disable(DKOSGModel)
+	#dk_disable(DKOSGManipulator)
 	dk_disable(DKOSGNotify)
 	dk_disable(DKOSGPhysics)		# 'btBulletDynamicsCommon.h' file not found
 	dk_disable(DKOSGPicker)
