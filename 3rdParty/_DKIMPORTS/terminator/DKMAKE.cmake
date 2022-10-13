6d123d9b0fdb102fd251686485b377a4565e54ca
aquawicket <aquawicket@hotmail.com>
2022-10-13 16:31:21 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/terminator/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/terminator/DKMAKE.cmake
@@ -2,5 +2,5 @@
 
 
 #LINUX_HOST_dk_set(CURRENT_DIR /usr)
-LINUX_HOST_dk_command(apt -y install terminator) # sudo
+LINUX_HOST_dk_command(sudo apt -y install terminator) #FIXME sudo needed
 MAC_HOST_dk_command(brew install terminator)
