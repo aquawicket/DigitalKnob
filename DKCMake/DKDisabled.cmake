3927f56d8c778028f1c8c1d63d1076f61947fca8
aquawicket <aquawicket@hotmail.com>
2022-10-14 15:48:11 -0400

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -116,7 +116,7 @@ if(MAC)  # Disabled for MAC targets
 	dk_disable(imagemagick)			# no such file or directory: libimagemagik.a
 	dk_disable(leptonica)			# Build Failed: fhmtauto.o
 	dk_disable(libcaca)				# no CMakeLists.txt
-	dk_disable(libtorrent)			# Could not find BOOST
+	#dk_disable(libtorrent)			# Could not find BOOST
 	dk_disable(libvncserver)		# build errors in main.c
 	dk_disable(libwebsockets)		# error: unknown type name 'lws_mutex_t'
 	dk_disable(libxml2)				# fatal error: 'extra/stricmp.h' file not found
@@ -175,7 +175,7 @@ if(IOS OR IOSSIM)  # Disabled for iOS and iOS-Simulator targets
 	dk_disable(DKVncServer)			# requires libvncserver
 	dk_disable(freealut)			# freealut-master/iossim64 does not contain an Xcode project
 	dk_disable(imagemagick)			# No such file or directory
-	dk_disable(jerryscript)			# build failed
+	#dk_disable(jerryscript)		# build failed
 	dk_disable(leptonica)			# Build Failed: fhmtauto.o
 	dk_disable(libcaca)				# no CMakeLists.txt
 	dk_disable(libiconv)			# CMake Error at cmake/dist.cmake:144 (install): install TARGETS given no BUNDLE DESTINATION for MACOSX_BUNDLE executable target "iconvcli"
@@ -286,7 +286,7 @@ if(RASPBERRY) # Disabled for Raspberry Pi targets
 	dk_disable(emsdk)				# 64bit source only
 	dk_disable(freealut)			# no ruleto make 'alut_static'
 	dk_disable(java)
-	#dk_disable(jerryscript)			# build errors
+	#dk_disable(jerryscript)		# build errors
 	dk_disable(kdevelop)
 	dk_disable(openjdk)
 	dk_disable(libcaca)				# no CMakeLists.txt
