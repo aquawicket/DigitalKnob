ccca119db9c92ad4b169366ac7cbabfe716c0c26
aquawicket <aquawicket@hotmail.com>
2022-10-16 17:05:22 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/ghostscript/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ghostscript/DKMAKE.cmake
@@ -8,8 +8,9 @@ endif()
 
 ### IMPORT ###
 #dk_set(GHOSTSCRIPT "C:/Program Files/gs/gs9.22/bin")
-dk_import(https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe)
+#dk_import(https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe)
+dk_set(GHOSTSCRIPT ${3RDPARTY}/ghostscript-9.22)
 IF(NOT EXISTS ${GHOSTSCRIPT}/bin)
-	#dk_download(https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe gs922w64.exe)
+	dk_download(https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe ${DKDOWNLOAD}/gs922w64.exe)
 	dk_command(${DKDOWNLOAD}/gs922w64.exe /S /D=${GHOSTSCRIPT})
 ENDIF()
