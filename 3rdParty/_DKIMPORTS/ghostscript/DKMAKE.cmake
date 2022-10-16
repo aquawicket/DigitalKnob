74c6dab77ca47a3fe46d0dccb451434c50659201
aquawicket <aquawicket@hotmail.com>
2022-10-16 16:48:04 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/ghostscript/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ghostscript/DKMAKE.cmake
@@ -1,14 +1,15 @@
 # https://github.com/ArtifexSoftware/ghostpdl-downloads
 # https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe
+# https://silentinstallhq.com/ghostscript-silent-install-how-to-guide/
 if(NOT WIN_HOST)
 	return()
 endif()
 
 
 ### IMPORT ###
-dk_set(GHOSTSCRIPT "C:/Program Files/gs/gs9.22/bin")
-IF(NOT EXISTS ${GHOSTSCRIPT})
+#dk_set(GHOSTSCRIPT "C:/Program Files/gs/gs9.22/bin")
+dk_import(https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe)
+IF(NOT EXISTS ${GHOSTSCRIPT}/bin)
 	#dk_download(https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe gs922w64.exe)
-	dk_import(https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe)
-	dk_command(${DKDOWNLOAD}/gs922w64.exe)
+	dk_command(${DKDOWNLOAD}/gs922w64.exe /S /D=${GHOSTSCRIPT})
 ENDIF()
