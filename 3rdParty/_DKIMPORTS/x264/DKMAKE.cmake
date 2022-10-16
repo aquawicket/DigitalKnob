d4bd1b33aa97fc1e3d2f77a492b6d6b126e9a77a
aquawicket <aquawicket@hotmail.com>
2022-10-16 14:47:39 -0400

    git commit

--- 3rdParty/_DKIMPORTS/x264/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/x264/DKMAKE.cmake
@@ -5,4 +5,22 @@
 dk_import(https://code.videolan.org/videolan/x264.git)
 
 
+### LINK ###
 # TODO
+dk_include(${X264})
+
+
+### GENERATE / COMPILE ###
+WIN_DEBUG_dk_setPath	(${X264}/${OS}/${DEBUG_DIR})
+WIN_DEBUG_dk_msys		(${DKCONFIGURE_BUILD})
+WIN_DEBUG_dk_msys		(make)
+WIN_RELEASE_dk_setPath	(${X264}/${OS}/${RELEASE_DIR})
+WIN_RELEASE_dk_msys		(${DKCONFIGURE_BUILD})
+WIN_RELEASE_dk_msys		(make)
+
+UNIX_DEBUG_dk_setPath		(${X264}/${OS}/${DEBUG_DIR})
+UNIX_DEBUG_dk_queueCommand	(${DKCONFIGURE_BUILD})
+UNIX_DEBUG_dk_queueCommand	(make)
+UNIX_RELEASE_dk_setPath		(${X264}/${OS}/${RELEASE_DIR})
+UNIX_RELEASE_dk_queueCommand(${DKCONFIGURE_BUILD})
+UNIX_RELEASE_dk_queueCommand(make)
--- 3rdParty/_DKIMPORTS/x265/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/x265/DKMAKE.cmake
@@ -5,4 +5,22 @@
 dk_import(https://bitbucket.org/multicoreware/x265_git.git)
 
 
+### LINK ###
+dk_include(${X265})
 # TODO
+
+
+### GENERATE / COMPILE ###
+WIN_DEBUG_dk_setPath	(${X265}/${OS}/${DEBUG_DIR})
+WIN_DEBUG_dk_msys		(${DKCONFIGURE_BUILD})
+WIN_DEBUG_dk_msys		(make)
+WIN_RELEASE_dk_setPath	(${X265}/${OS}/${RELEASE_DIR})
+WIN_RELEASE_dk_msys		(${DKCONFIGURE_BUILD})
+WIN_RELEASE_dk_msys		(make)
+
+UNIX_DEBUG_dk_setPath		(${X265}/${OS}/${DEBUG_DIR})
+UNIX_DEBUG_dk_queueCommand	(${DKCONFIGURE_BUILD})
+UNIX_DEBUG_dk_queueCommand	(make)
+UNIX_RELEASE_dk_setPath		(${X265}/${OS}/${RELEASE_DIR})
+UNIX_RELEASE_dk_queueCommand(${DKCONFIGURE_BUILD})
+UNIX_RELEASE_dk_queueCommand(make)
