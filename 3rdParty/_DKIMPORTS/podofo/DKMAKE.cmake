bd9c01498edf988f6924e205158b0ee6298b7891
aquawicket <aquawicket@hotmail.com>
2022-10-19 20:07:01 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/podofo/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/podofo/DKMAKE.cmake
@@ -10,6 +10,7 @@ dk_depend(libpng)
 dk_depend(tiff)
 dk_depend(zlib)
 
+
 ### IMPORT ###
 dk_import(http://sourceforge.net/projects/podofo/files/podofo/0.9.7/podofo-0.9.7.tar.gz)
 #dk_import(https://github.com/mekentosj/podofo.git)
@@ -19,29 +20,27 @@ dk_import(http://sourceforge.net/projects/podofo/files/podofo/0.9.7/podofo-0.9.7
 dk_include				(${PODOFO})
 dk_include				(${PODOFO}/podofo)
 dk_include				(${PODOFO}/${OS})
-dk_include				(${PODOFO}/${OS}/${RELEASE_DIR})
+DEBUG_dk_include		(${PODOFO}/${OS}/${DEBUG_DIR})
+RELEASE_dk_include		(${PODOFO}/${OS}/${RELEASE_DIR})
 WIN_dk_libDebug			(${PODOFO}/${OS}/src/podofo/${DEBUG_DIR}/podofo.lib)
 WIN_dk_libRelease		(${PODOFO}/${OS}/src/podofo/${RELEASE_DIR}/podofo.lib)
-MAC_dk_libDebug			(${PODOFO}/${OS}/src/podofo/${DEBUG_DIR}/libpodofo.a)
-MAC_dk_libRelease		(${PODOFO}/${OS}/src/podofo/${RELEASE_DIR}/libpodofo.a)
-IOS_dk_libDebug			(${PODOFO}/${OS}/src/podofo/${DEBUG_DIR}/libpodofo.a)
-IOS_dk_libRelease		(${PODOFO}/${OS}/src/podofo/${RELEASE_DIR}/libpodofo.a)
-IOSSIM_dk_libDebug		(${PODOFO}/${OS}/src/podofo/${DEBUG_DIR}/libpodofo.a)
-IOSSIM_dk_libRelease	(${PODOFO}/${OS}/src/podofo/${RELEASE_DIR}/libpodofo.a)
+APPLE_dk_libDebug		(${PODOFO}/${OS}/src/podofo/${DEBUG_DIR}/libpodofo.a)
+APPLE_dk_libRelease		(${PODOFO}/${OS}/src/podofo/${RELEASE_DIR}/libpodofo.a)
 LINUX_dk_libDebug		(${PODOFO}/${OS}/${DEBUG_DIR}/src/podofo/libpodofo.a)
 LINUX_dk_libRelease		(${PODOFO}/${OS}/${RELEASE_DIR}/src/podofo/libpodofo.a)
+RASPBERRY_dk_libDebug	(${PODOFO}/${OS}/${DEBUG_DIR}/src/podofo/libpodofo.a)
+RASPBERRY_dk_libRelease	(${PODOFO}/${OS}/${RELEASE_DIR}/src/podofo/libpodofo.a)
 ANDROID_dk_libDebug		(${PODOFO}/${OS}/${DEBUG_DIR}/obj/local/armeabi-v7a/libpodofo.a)
 ANDROID_dk_libRelease	(${PODOFO}/${OS}/${RELEASE_DIR}/obj/local/armeabi-v7a/libpodofo.a)
 
 
 ### GENERATE ###
-dk_setPath				(${PODOFO}/${BUILD_DIR})
-WIN_dk_queueCommand		(${DKCMAKE_BUILD} "-DCMAKE_CXX_FLAGS=/I${ZLIB}/${OS} /I${LIBPNG}" -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${ZLIB_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${PODOFO})
-MAC_dk_queueCommand		(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${ZLIB_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${PODOFO}) #-DPODOFO_NO_FONTMANAGER=ON
-IOS_dk_queueCommand		(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${ZLIB_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${PODOFO}) #-DPODOFO_NO_FONTMANAGER=ON
-IOSSIM_dk_queueCommand	(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${ZLIB_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${PODOFO}) #-DPODOFO_NO_FONTMANAGER=ON
-LINUX_dk_queueCommand	(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${ZLIB_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${PODOFO})
-ANDROID_dk_queueCommand	(${DKCMAKE_BUILD} "-DCMAKE_CXX_FLAGS=/I${ZLIB}/${OS}" -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${ZLIB_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${PODOFO})
+dk_setPath					(${PODOFO}/${BUILD_DIR})
+WIN_dk_queueCommand			(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${ZLIB_CMAKE} "-DCMAKE_CXX_FLAGS=/I${ZLIB}/${OS} /I${LIBPNG}" ${PODOFO})
+APPLE_dk_queueCommand		(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${ZLIB_CMAKE} ${PODOFO}) #-DPODOFO_NO_FONTMANAGER=ON
+LINUX_dk_queueCommand		(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${ZLIB_CMAKE} ${PODOFO})
+RASPBERRY_dk_queueCommand	(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${ZLIB_CMAKE} ${PODOFO})
+ANDROID_dk_queueCommand		(${DKCMAKE_BUILD} -DPODOFO_BUILD_STATIC=ON ${CRYPTOPP_CMAKE} ${FREETYPE_CMAKE} ${LIBJPEG-TURBO_CMAKE} ${LIBPNG_CMAKE} ${TIFF_CMAKE} ${ZLIB_CMAKE} "-DCMAKE_CXX_FLAGS=/I${ZLIB}/${OS}" ${PODOFO})
 
 
 ### COMPILE ###
