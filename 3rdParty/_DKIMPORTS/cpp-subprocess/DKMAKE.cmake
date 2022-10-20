523b28fce49b994bf73314892a02841c1600b3d3
aquawicket <aquawicket@hotmail.com>
2022-10-20 11:00:21 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/cpp-subprocess/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/cpp-subprocess/DKMAKE.cmake
@@ -1,26 +1,11 @@
 # https://github.com/tsaarni/cpp-subprocess
 
+# NOTE:		Header only library
+#			Use #include "subprocess.hpp"
 
 ### IMPORT ###
 dk_import(https://github.com/tsaarni/cpp-subprocess.git)
 
 
 ### LINK ###
-dk_include(${CPP-SUBPROCESS})
-# TODO
-
-
-### GENERATE / COMPILE ###
-WIN_DEBUG_dk_setPath	(${CPP-SUBPROCESS}/${OS}/${DEBUG_DIR})
-WIN_DEBUG_dk_msys		(${DKCONFIGURE_BUILD})
-WIN_DEBUG_dk_msys		(make)
-WIN_RELEASE_dk_setPath	(${CPP-SUBPROCESS}/${OS}/${RELEASE_DIR})
-WIN_RELEASE_dk_msys		(${DKCONFIGURE_BUILD})
-WIN_RELEASE_dk_msys		(make)
-
-UNIX_DEBUG_dk_setPath		(${CPP-SUBPROCESS}/${OS}/${DEBUG_DIR})
-UNIX_DEBUG_dk_queueCommand	(${DKCONFIGURE_BUILD})
-UNIX_DEBUG_dk_queueCommand	(make)
-UNIX_RELEASE_dk_setPath		(${CPP-SUBPROCESS}/${OS}/${RELEASE_DIR})
-UNIX_RELEASE_dk_queueCommand(${DKCONFIGURE_BUILD})
-UNIX_RELEASE_dk_queueCommand(make)
+dk_include(${CPP-SUBPROCESS}/include)
--- 3rdParty/_DKIMPORTS/openframeworks/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/openframeworks/DKMAKE.cmake
@@ -1,5 +1,6 @@
 # https://github.com/openframeworks/openFrameworks
 # https://github.com/openframeworks/openFrameworks/archive/refs/tags/0.10.0.zip
+# https://github.com/openframeworks/openFrameworks/releases/download/0.11.2/of_v0.11.2_vs2017_release.zip  # WINDOWS
 
 
 ### DEPEND ###
@@ -10,7 +11,14 @@ dk_depend(opengles)
 
 ### IMPORT ###
 #dk_import(https://github.com/openframeworks/openFrameworks/archive/refs/tags/0.10.0.zip PATCH)
-dk_import(https://github.com/openframeworks/openFrameworks.git PATCH)
+#dk_import(https://github.com/openframeworks/openFrameworks.git PATCH)
+WIN_dk_import(https://github.com/openframeworks/openFrameworks/releases/download/0.11.2/of_v0.11.2_vs2017_release.zip PATCH)
+MAC_dk_import(https://github.com/openframeworks/openFrameworks/releases/download/0.11.2/of_v0.11.2_osx_release.zip)
+IOS_dk_import(https://github.com/openframeworks/openFrameworks/releases/download/0.11.2/of_v0.11.2_ios_release.zip)
+IOSSIM_dk_import(https://github.com/openframeworks/openFrameworks/releases/download/0.11.2/of_v0.11.2_ios_release.zip)
+LINUX_dk_import(https://github.com/openframeworks/openFrameworks/releases/download/0.11.2/of_v0.11.2_linux64gcc6_release.tar.gz)
+RASPBERRY_dk_import(https://github.com/openframeworks/openFrameworks/releases/download/0.11.2/of_v0.11.2_linuxarmv6l_release.tar.gz)
+ANDROID_dk_import(https://github.com/openframeworks/openFrameworks/releases/download/0.11.2/of_v0.11.2_android_release.tar.gz)
 
 
 ### LINK ###
@@ -19,6 +27,7 @@ dk_include(${OPENFRAMEWORKS}/libs/openFrameworks)
 dk_include(${OPENFRAMEWORKS}/libs/openFrameworks/utils)
 dk_include(${OPENFRAMEWORKS}/libs/glew/include)
 dk_include(${OPENFRAMEWORKS}/libs/glu/include)
+dk_include(${OPENFRAMEWORKS}/libs/glm/include)
 dk_include(${OPENFRAMEWORKS}/libs/tess2/include)
 dk_include(${OPENFRAMEWORKS}/libs/json/include)
 dk_include(${OPENFRAMEWORKS}/libs/openFrameworks/types)
