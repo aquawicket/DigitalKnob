7d08c3b7fbaf2ed6d9a8fb9e675218178a7d2221
aquawicket <aquawicket@hotmail.com>
2022-10-28 00:16:38 -0400

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -250,6 +250,7 @@ if(LINUX) # Disabled for Linux targets
 	dk_disable(kdevelop)			# permission denied
 	dk_disable(leptonica)			# build errors
 	dk_disable(libcaca)				# no CMakeLists.txt
+	dk_disable(libexpat)
 	dk_disable(libtorrent)			# CMake Error: cannot find source file: deps/try_signal/try_singal.cpp
 	dk_disable(libxml2)				# Not such file or directory
 	dk_disable(lighttpd)			# DKMAKE.cmake incomplete
