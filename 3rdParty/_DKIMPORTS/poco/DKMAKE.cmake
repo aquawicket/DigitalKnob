2e3f1f52111f5b30ca89b9b67df16c2416cd7494
aquawicket <aquawicket@hotmail.com>
2022-10-28 01:29:15 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/poco/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/poco/DKMAKE.cmake
@@ -6,12 +6,18 @@ dk_import(https://github.com/pocoproject/poco.git)
 
 
 ### LINK ###
-dk_include			(${POCO}/include)
-dk_include			(${POCO}/${OS})
-WIN_dk_libDebug		(${POCO}/${OS}/lib/${DEBUG_DIR}/PocoFoundationmdd.lib)
-WIN_dk_libRelease	(${POCO}/${OS}/lib/${RELEASE_DIR}/PocoFoundationm.lib)
-UNIX_dk_libDebug	(${POCO}/${OS}/lib/${DEBUG_DIR}/libPocoFoundationd.a)
-UNIX_dk_libRelease	(${POCO}/${OS}/lib/${RELEASE_DIR}/libPocoFoundation.a)
+dk_include				(${POCO}/include)
+dk_include				(${POCO}/${OS})
+WIN_dk_libDebug			(${POCO}/${OS}/lib/${DEBUG_DIR}/PocoFoundationmdd.lib)
+WIN_dk_libRelease		(${POCO}/${OS}/lib/${RELEASE_DIR}/PocoFoundationm.lib)
+APPLE_dk_libDebug		(${POCO}/${OS}/lib/${DEBUG_DIR}/libPocoFoundationd.a)
+APPLE_dk_libRelease		(${POCO}/${OS}/lib/${RELEASE_DIR}/libPocoFoundation.a)
+LINUX_dk_libDebug		(${POCO}/${OS}/${DEBUG_DIR}/lib/libPocoFoundationd.a)
+LINUX_dk_libRelease		(${POCO}/${OS}/${RELEASE_DIR}/lib/libPocoFoundation.a)
+RASPBERRY_dk_libDebug	(${POCO}/${OS}/${DEBUG_DIR}/lib/libPocoFoundationd.a)
+RASPBERRY_dk_libRelease	(${POCO}/${OS}/${RELEASE_DIR}/lib/libPocoFoundation.a)
+ANDROID_dk_libDebug		(${POCO}/${OS}/lib/${DEBUG_DIR}/PocoFoundationmdd.a)
+ANDROID_dk_libRelease	(${POCO}/${OS}/lib/${RELEASE_DIR}/PocoFoundationm.a)
 
 
 ### GENERATE ###
--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -81,6 +81,7 @@ if(WIN) # Disabled for Windows targets
 	dk_disable(rmlui-d3d11)
 	dk_disable(waave)				# error C2065: 'PIX_FMT_YUV420P': undeclared identifier.  https://sourceforge.net/p/guvcview/tickets/34/
 	dk_disable(x264)
+	dk_disable(x265)
 endif()
 
 if(WIN_64) # Disabled for Windows 64bit targets
@@ -185,6 +186,7 @@ if(IOS OR IOSSIM)  # Disabled for iOS and iOS-Simulator targets
 	dk_disable(jerryscript)			# Error: unknown linker option '-z'
 	dk_disable(leptonica)			# Build Failed: fhmtauto.o
 	dk_disable(libcaca)				# no CMakeLists.txt
+	dk_disable(libexpat)
 	dk_disable(libiconv)			# CMake Error at cmake/dist.cmake:144 (install): install TARGETS given no BUNDLE DESTINATION for MACOSX_BUNDLE executable target "iconvcli"
 	dk_disable(libjpeg-turbo)		# CMake Error at CMakeLists.txt:60 (string): string no output variable specified
 	dk_disable(libmd)				# dk_getExtension Function invoked with incorrect arguments
