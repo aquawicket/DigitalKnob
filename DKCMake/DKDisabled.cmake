f13cee9c82f0edce49c5a8422c7ae82f62723c0e
aquawicket <aquawicket@hotmail.com>
2022-10-27 23:28:49 -0400

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -134,6 +134,7 @@ if(MAC)  # Disabled for MAC targets
 	dk_disable(tesseract)			# requires leptonica
 	dk_disable(waave)				# error: streaming_objet.h:59 field has incomplete type 'enum PixelFormat'
 	dk_disable(x264)
+	dk_disable(x265)
 endif()
 
 if(IOS OR IOSSIM)  # Disabled for iOS and iOS-Simulator targets
