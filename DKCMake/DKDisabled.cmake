266055d5267d41d8fc38beae9a4d4ad89f81594f
aquawicket <aquawicket@hotmail.com>
2022-10-28 00:26:18 -0400

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -260,6 +260,7 @@ if(LINUX) # Disabled for Linux targets
 	dk_disable(openssl)				# compiling errors
 	dk_disable(osgaudio)			# requires freealut
 	dk_disable(osgbullet)			# CMake Error: Could NOT find Bullet
+	dk_disable(osgrmlui)
 	dk_disable(osgworks)			# error: osgUtil::Optimizer::MergeGeometryVisitor has no member named 'mergeGeode'
 	dk_disable(podofo)				# build errors
 	dk_disable(tesseract)			# requires leptonica
