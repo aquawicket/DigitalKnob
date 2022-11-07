9acf913687c2595334af6deb0bee52c57a0d8064
aquawicket <aquawicket@hotmail.com>
2022-11-06 19:14:24 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/backward-cpp/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/backward-cpp/DKMAKE.cmake
@@ -5,4 +5,18 @@
 dk_import(https://github.com/bombela/backward-cpp.git)
 
 
-# TODO
+### LINK ###
+dk_include			(${BACKWARD-CPP})
+WIN_dk_libDebug		(${BACKWARD-CPP}/${DEBUG_DIR}/backward.lib)
+WIN_dk_libRelease	(${BACKWARD-CPP}/${RELEASE_DIR}/backward.lib)
+UNIX_dk_libDebug	(${BACKWARD-CPP}/${DEBUG_DIR}/backward.a)
+UNIX_dk_libRelease	(${BACKWARD-CPP}/${RELEASE_DIR}/backward.a)
+
+
+### GENERATE ###
+dk_setPath		(${BACKWARD-CPP}/${BUILD_DIR})
+dk_queueCommand	(${DKCMAKE_BUILD} ${BACKWARD-CPP})
+
+
+### COMPILE ###
+dk_build(${BACKWARD-CPP_FOLDER})
