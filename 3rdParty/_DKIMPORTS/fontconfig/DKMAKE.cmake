fa7b63e7e3fe99d17c8329c606da5c1f41dc81d3
aquawicket <aquawicket@hotmail.com>
2022-11-08 23:14:45 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/fontconfig/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/fontconfig/DKMAKE.cmake
@@ -17,14 +17,14 @@ dk_set		(FONTCONFIG_CMAKE -DFONTCONFIG_INCLUDE_DIR=${FONTCONFIG}/lib -DFONTCONFI
 
 
 ### GENERATE / COMPILE ###
-#WIN_DEBUG_dk_setPath				(${FONTCONFIG})
-#WIN_DEBUG_dk_queueCommand			(autoreconf -f -i)
-WIN_DEBUG_dk_setPath				(${FONTCONFIG}/${OS}/${DEBUG_DIR})
-WIN_DEBUG_dk_msys					(${DKCONFIGURE_BUILD})
-WIN_DEBUG_dk_msys					(make -C lib)
+WIN_DEBUG_dk_setPath		(${FONTCONFIG})
+WIN_DEBUG_dk_queueCommand	(autoreconf -f -i)
+WIN_DEBUG_dk_setPath		(${FONTCONFIG}/${OS}/${DEBUG_DIR})
+WIN_DEBUG_dk_msys			(${DKCONFIGURE_BUILD})
+WIN_DEBUG_dk_msys			(make -C lib)
 
-#WIN_RELEASE_dk_setPath				(${FONTCONFIG})
-#WIN_RELEASE_dk_queueCommand		(autoreconf -f -i)
-WIN_RELEASE_dk_setPath				(${FONTCONFIG}/${OS}/${RELEASE_DIR})
-WIN_RELEASE_dk_msys					(${DKCONFIGURE_BUILD})
-WIN_RELEASE_dk_msys					(make -C lib)
+WIN_RELEASE_dk_setPath		(${FONTCONFIG})
+WIN_RELEASE_dk_queueCommand	(autoreconf -f -i)
+WIN_RELEASE_dk_setPath		(${FONTCONFIG}/${OS}/${RELEASE_DIR})
+WIN_RELEASE_dk_msys			(${DKCONFIGURE_BUILD})
+WIN_RELEASE_dk_msys			(make -C lib)
