7368382b13d540a429b4218429ec99828ef16c8c
aquawicket <aquawicket@hotmail.com>
2022-11-29 18:16:25 -0500

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -236,7 +236,8 @@ if(IOS OR IOSSIM)  # Disabled for iOS and iOS-Simulator targets
 	dk_disable(zstd)
 endif(IOS OR IOSSIM)
 if(IOS)  # Disabled for iOS targets
-	dk_disable(giflib)
+	dk_disable(giflib)				# configure: error: C compiler cannot create executables
+	dk_disable(ogg)					# configure: error: C compiler cannot create executables
 endif()
 
 
