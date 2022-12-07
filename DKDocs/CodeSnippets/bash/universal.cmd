196e353f026adf1bbe824b7456384195accbfd73
aquawicket <aquawicket@hotmail.com>
2022-12-07 18:57:50 -0500

    commit from git

--- DKDocs/CodeSnippets/bash/universal.cmd
+++ DKDocs/CodeSnippets/bash/universal.cmd
@@ -1,14 +1,12 @@
-:<<"::CMDLITERAL"
+:<<"::UNIX"
 @ECHO OFF
 GOTO :WINDOWS
-::CMDLITERAL
 
+
+::UNIX
 echo "This universal script is running on Unix"
-if :; then
-  echo "This makes conditional constructs so much easier because"
-  echo "they can now span multiple lines."
-fi
 exit $?
+
 
 :WINDOWS
 ECHO This universal script is running on Windows
