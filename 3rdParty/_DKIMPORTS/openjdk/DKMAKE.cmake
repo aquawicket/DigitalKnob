6536820c4d5951c2c786d233cf4bcad3ac15501b
aquawicket <aquawicket@hotmail.com>
2022-12-08 19:23:50 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/openjdk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/openjdk/DKMAKE.cmake
@@ -7,14 +7,19 @@
 # https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java110151/jdk-11.0.15.1_osx-x64_bin.dmg
 # https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java110151/jdk-11.0.15.1_solaris-sparcv9_bin.tar.gz
 # https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java110151/jdk-11.0.15.1_windows-x64_bin.zip
+# https://gist.github.com/douglarek/bbda8cc23a562cb5d5798717d57bc9e9
 
 #dk_set(OPENJDK-11_VERSION 11)
 
 WIN_HOST_dk_import	(https://download.java.net/java/ga/jdk11/openjdk-11_windows-x64_bin.zip PATCH)
-#MAC_HOST_dk_import	(https://cfdownload.adobe.com/pub/adobe/coldfusion/java/java11/java110151/jdk-11.0.15.1_osx-x64_bin.dmg PATCH)
 MAC_HOST_dk_import	(https://download.java.net/java/ga/jdk11/openjdk-11_osx-x64_bin.tar.gz PATCH)
-LINUX_HOST_dk_import	(https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz PATCH)
+LINUX_HOST_dk_import(https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz PATCH)
 
+# MAC Install
+#$ curl -C - https://download.java.net/java/ga/jdk11/openjdk-11_osx-x64_bin.tar.gz -O openjdk-11_osx-x64_bin.tar.gz
+#$ tar xf openjdk-11_osx-x64_bin.tar.gz
+#$ sudo mv jdk-11.jdk /Library/Java/JavaVirtualMachines/
+#$ java -version
 
 ### LINK ###
 #dk_include(${OPENJDK-11}/)	
