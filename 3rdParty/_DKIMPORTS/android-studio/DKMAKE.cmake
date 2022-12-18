386ab1b4007feb98a5460f40beea0ac87312c523
aquawicket <aquawicket@hotmail.com>
2022-12-18 14:01:45 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/android-studio/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-studio/DKMAKE.cmake
@@ -1,27 +1,30 @@
 # https://developer.android.com/studio/archive
 # https://developer.android.com/studio#downloads
 
-if(NOT ANDROID AND NOT WIN_HOST)
+if(NOT ANDROID)
 	dk_undepend(android-studio)
 	return()
 endif()
 
 
+### DEPEND ###
 dk_depend(openjdk-8u41)
 dk_depend(android-ndk)
 
 
-#dk_set(ANDROID-STUDIO_VERSION "2020.3.1.26-windows")
-dk_set(ANDROID-STUDIO_NAME "android-studio-2020.3.1.26-windows.exe")
+### IMPORT ###
+WIN_HOST_dk_set(ANDROID-STUDIO_NAME "android-studio-2020.3.1.26-windows.exe")
 WIN_HOST_dk_set(ANDROID-STUDIO_DL "https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe")
-#WIN_dk_import(https://redirector.gvt1.com/edgedl/android/studio/install/2020.3.1.26/android-studio-2020.3.1.26-windows.exe)
-
 WIN_HOST_dk_set(ANDROID-STUDIO "C:/Program Files/Android/Android Studio/bin")
 WIN_HOST_dk_set(ANDROID-STUDIO_EXE "${ANDROID-STUDIO}/studio.exe")
 
+MAC_HOST_dk_set(ANDROID-STUDIO_NAME "android-studio-2021.3.1.17-mac.dmg")
+MAC_HOST_dk_set(ANDROID-STUDIO_DL "https://redirector.gvt1.com/edgedl/android/studio/install/2021.3.1.17/android-studio-2021.3.1.17-mac.dmg")
 MAC_HOST_dk_set(ANDROID-STUDIO "/Applications")
 MAC_HOST_dk_set(ANDROID-STUDIO_EXE "${ANDROID-STUDIO}/Android Studio.app")
 
+LINUX_HOST_dk_set(ANDROID-STUDIO_NAME "android-studio-2021.3.1.17-linux.tar.gz")
+LINUX_HOST_dk_set(ANDROID-STUDIO_DL "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.3.1.17/android-studio-2021.3.1.17-linux.tar.gz")
 LINUX_HOST_dk_set(ANDROID-STUDIO "${3RDPARTY}/android-studio/bin")
 LINUX_HOST_dk_set(ANDROID-STUDIO_EXE "${ANDROID-STUDIO}/Android Studio.app")
 
