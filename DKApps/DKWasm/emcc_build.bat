22b0c5842b052c6a2a07a0ce90f97a673537ba66
aquawicket <aquawicket@hotmail.com>
2022-12-27 17:46:04 -0500

    commit from git

--- DKApps/DKWasm/emcc_build.bat
+++ DKApps/DKWasm/emcc_build.bat
@@ -1,6 +1,6 @@
 %dkbatch%
 
 call C:\Users\Administrator\digitalknob\DK\3rdParty\emsdk-main\emsdk_env.bat
-emcc main.cpp -o C:\Users\Administrator\digitalknob\DK\DKApps\DKWasm\assets\DKWasm.html 
+call emcc -v main.cpp -o C:\Users\Administrator\digitalknob\DK\DKApps\DKWasm\assets\DKWasm.html 
  
 %DKEND% 
