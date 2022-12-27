03d6e3ab38626b796e3f7e8d7133ea66d0339785
aquawicket <aquawicket@hotmail.com>
2022-12-27 17:16:08 -0500

    commit from git

---
+++ DKApps/DKWasm/emcc_build.bat
@@ -0,0 +1,7 @@
+%dkbatch%
+
+mkdir emscripten
+call C:\Users\Administrator\digitalknob\DK\3rdParty\emsdk-main\emsdk_env.bat
+emcc main.cpp -o C:\Users\Administrator\digitalknob\DK\DKApps\DKWasm\emscripten\DKWasm.html 
+ 
+%DKEND% 
