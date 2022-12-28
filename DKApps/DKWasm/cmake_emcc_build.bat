3e24148cc697bc55c1e7d64e15f5267fa1a5ebba
aquawicket <aquawicket@hotmail.com>
2022-12-27 22:52:05 -0500

    commit from git

--- DKApps/DKWasm/cmake_emcc_build.bat
+++ DKApps/DKWasm/cmake_emcc_build.bat
@@ -3,7 +3,8 @@
 mkdir emscripten
 cd emscripten
 call C:\Users\Administrator\digitalknob\DK\3rdParty\emsdk-main\emsdk_env.bat
-call emcmake cmake -G "MinGW Makefiles" ..
+::call emcmake cmake -G "MinGW Makefiles" ..
+call cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="C:/Users/Administrator/digitalknob/DK/3rdParty/emsdk-main/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" ..
 call cmake --build .
 copy dkwasm.* C:\Users\Administrator\digitalknob\DK\DKApps\DKWasm\assets\
 cd C:\Users\Administrator\digitalknob\DK\DKApps\DKWasm\assets\DKPhp
