9e28f94bfc2a2c855d183838dfc15bb5f3d02da3
aquawicket <aquawicket@hotmail.com>
2022-12-28 16:05:06 -0500

    commit from git

--- DKPlugins/DK/DKPreprocessor.h
+++ DKPlugins/DK/DKPreprocessor.h
@@ -60,6 +60,7 @@
 #	define DKBITS 32
 #else
 #	define DKARCH "UNKNOWN"
+#	define DKBITS "UNKNOWN"
 #endif
 
 // https://web.archive.org/web/20191012035921/http://nadeausoftware.com/articles/2012/01/c_c_tip_how_use_compiler_predefined_macros_detect_operating_system
