9bfe9eec67ef3156e73a4c15276fc851a3503f6f
aquawicket <aquawicket@hotmail.com>
2022-12-29 14:26:13 -0500

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -484,6 +484,7 @@ if(EMSCRIPTEN) # Disabled for Emscripten targets
 	dk_disable(libarchive)			# /archive_write_set_format_7zip.c:1541:13:  error: implicit truncation from 'int' to a one-bit wide bit-filed
 	dk_disable(libpng)				# CMake Error: The following variables are used in this project, but they are set to NOTFOUND. (M_LIBRARY)
 	dk_disable(libxml2)				# fatal error: 'config.h' file not found
+	dk_disable(m)
 	#dk_disable(sdl)
 	dk_disable(sdl_image)			# error: call to undeclared function 'SDL_CreateRGBSurfaceWithFormat'
 	dk_disable(sdl_ttf)				# requires sdl
