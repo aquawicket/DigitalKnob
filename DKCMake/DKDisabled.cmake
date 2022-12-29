9e4d5f4db28d180399c39a802d96b26877e039fd
aquawicket <aquawicket@hotmail.com>
2022-12-29 14:46:58 -0500

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -499,6 +499,7 @@ if(EMSCRIPTEN) # Disabled for Emscripten targets
 	dk_disable(game_controller)
 	dk_disable(giflib)				# dk_msys(): ERROR: not WIN_32, WIN_64, ANDROID_32 or ANDROID_64
 	dk_disable(gl_kit)
+	dk_disable(iokit)
 	dk_disable(libarchive)			# /archive_write_set_format_7zip.c:1541:13:  error: implicit truncation from 'int' to a one-bit wide bit-filed
 	dk_disable(libpng)				# CMake Error: The following variables are used in this project, but they are set to NOTFOUND. (M_LIBRARY)
 	dk_disable(libxml2)				# fatal error: 'config.h' file not found
