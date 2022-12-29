2350e1fae49edb7cce1db3907a2cac5392f2cd6b
aquawicket <aquawicket@hotmail.com>
2022-12-29 01:51:39 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/wasm3/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/wasm3/DKMAKE.cmake
@@ -10,9 +10,8 @@ dk_include			(${WASM3})
 dk_include			(${WASM3}/${OS})
 WIN_dk_libDebug		(${WASM3}/${OS}/source/${DEBUG_DIR}/m3.lib)
 WIN_dk_libRelease	(${WASM3}/${OS}/source/${RELEASE_DIR}/m3.lib)
-UNIX_dk_libDebug	(${WASM3}/${OS}/${DEBUG_DIR}/libm3.a)
-UNIX_dk_libRelease	(${WASM3}/${OS}/${RELEASE_DIR}/libm3.a)
-
+UNIX_dk_libDebug	(${WASM3}/${OS}/${DEBUG_DIR}/source/libm3.a)
+UNIX_dk_libRelease	(${WASM3}/${OS}/${RELEASE_DIR}/source/libm3.a)
 
 
 ### GENERATE ###
