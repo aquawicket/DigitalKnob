33b972f8337590c9aff7236a863a9777d4014b3f
aquawicket <aquawicket@hotmail.com>
2022-12-30 15:51:58 -0500

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -505,6 +505,8 @@ if(EMSCRIPTEN) # Disabled for Emscripten targets
 	dk_disable(DKOSGLights)
 	dk_disable(DKOSGManipulator)
 	dk_disable(DKOSGModel)
+	dk_disable(DKOSGNotify)
+	dk_disable(DKOSGPhysics)
 	dk_disable(DKOSGViewer)
 	dk_disable(DKOSGWindow)
 	dk_disable(DKRml)				# fatal error: 'sys/utime.h' file not found
