b1e3845909aa3b4169d4303eeea8444ac4c01bbb
aquawicket <aquawicket@hotmail.com>
2022-12-30 01:31:12 -0500

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -501,6 +501,7 @@ if(EMSCRIPTEN) # Disabled for Emscripten targets
 	dk_disable(DKMySql)
 	dk_disable(DKOFWindow)
 	dk_disable(DKOSGViewer)
+	dk_disable(DKOSGWindow)
 	dk_disable(DKRml)				# fatal error: 'sys/utime.h' file not found
 	dk_disable(DKRmlElement)		# requires DKRml
 	dk_disable(DKSDLRml)
