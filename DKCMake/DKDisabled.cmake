58f2f01ba7efc2498000e608d4121f6fcd36db68
aquawicket <aquawicket@hotmail.com>
2022-12-29 19:31:25 -0500

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -535,6 +535,7 @@ if(EMSCRIPTEN) # Disabled for Emscripten targets
 	dk_disable(sdl_image)			# error: call to undeclared function 'SDL_CreateRGBSurfaceWithFormat'
 	dk_disable(sdl_ttf)				# error: unknown type name 'SDL_INLINE'
 	dk_disable(system_configuration)
+	dk_disable(waave)
 	dk_disable(xz)					# CMake Error: TEST_BIG_ENDIAN found no result!
 	dk_disable(zstd)				# error: call to undeclared function 'fileno'; ISO C99 and later do not support implicit function declarations
 endif()
