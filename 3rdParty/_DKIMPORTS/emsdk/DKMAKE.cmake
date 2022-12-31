164b0012ae810490dd149803c43e20128c026c0f
aquawicket <aquawicket@hotmail.com>
2022-12-30 20:10:30 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/emsdk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/emsdk/DKMAKE.cmake
@@ -4,7 +4,7 @@
 # https://github.com/emscripten-core/emsdk/archive/refs/tags/2.0.26.zip
 
 
-dk_import(https://github.com/emscripten-core/emsdk.git BRANCH main PATCH)
+dk_import(https://github.com/emscripten-core/emsdk.git BRANCH main)
 
 
 #WIN_dk_command(${EMSDK}/emsdk update)
@@ -20,6 +20,7 @@ WIN_HOST_dk_command(${EMSDK}/emsdk install mingw-4.6.2-32bit)
 WIN_HOST_dk_command(${EMSDK}/emsdk activate mingw-4.6.2-32bit)
 
 dk_set(EMCMAKE ${EMSDK}/upstream/emscripten/emcmake)
+#dk_copy(${DKIMPORTS}/emsdk/upstream/emscripten/src/)
 
 
 ######################################################################################################################
