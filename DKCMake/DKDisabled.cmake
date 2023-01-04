f316fbe9dbe486b5bd5c9119e89936feb8b273f4
aquawicket <aquawicket@hotmail.com>
2023-01-04 03:12:07 -0500

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -543,6 +543,7 @@ if(EMSCRIPTEN) # Disabled for Emscripten targets
 	dk_disable(gzip)
 	dk_disable(imagemagick)
 	dk_disable(iokit)
+	dk_disable(jasper)
 	dk_disable(libarchive)			# /archive_write_set_format_7zip.c:1541:13:  error: implicit truncation from 'int' to a one-bit wide bit-filed
 	dk_disable(libexpat)
 	dk_disable(libsndfile)
