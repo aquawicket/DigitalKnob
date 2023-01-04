1bf860cb7d50d6b1be7e48894626e8cdcf3c6d83
aquawicket <aquawicket@hotmail.com>
2023-01-03 21:29:21 -0500

    commit from git

--- DKApps/DKWasm/assets/app.js
+++
@@ -1,7 +0,0 @@
-//CPP_DK_Create("DKSDLWindow")
-//CPP_DK_Create("DKSDLText")
-
-//console.log("CPP_DKWindow_TestInt(1234) = "+CPP_DKWindow_TestInt(1234)+"\n")
-//console.log("CPP_DKWindow_TestString(\"test string\") = "+CPP_DKWindow_TestString("test string")+"\n")
-//console.log("CPP_DKWindow_TestReturnInt() = "+CPP_DKWindow_TestReturnInt()+"\n")
-//console.log("CPP_DKWindow_TestReturnString() = "+CPP_DKWindow_TestReturnString()+"\n")
--- DKApps/DKWasm/emsdk_build.bat
+++
@@ -1,9 +0,0 @@
-%dkbatch%
-
-call C:\Users\Administrator\digitalknob\DK\3rdParty\emsdk-main\emsdk_env.bat
-call emcc -v main.cpp -o C:\Users\Administrator\digitalknob\DK\DKApps\DKWasm\assets\DKWasm.html
-cd C:\Users\Administrator\digitalknob\DK\DKApps\DKWasm\assets\DKPhp
-call RunPhpServers.bat
-call rundll32 url.dll,FileProtocolHandler http://127.0.0.1:2393/DKWasm.html
- 
-%DKEND% 
