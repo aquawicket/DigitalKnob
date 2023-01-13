6f32f7ee880c5298ef512ffdb68d6fe51f0147d5
aquawicket <aquawicket@hotmail.com>
2023-01-12 22:21:43 -0500

    commit from git

--- DKPlugins/DKDuktapeDebugger/DKMAKE.cmake
+++ DKPlugins/DKDuktapeDebugger/DKMAKE.cmake
@@ -1,3 +1,9 @@
 dk_depend(DKDuktape)
+
+if(NOT EXISTS ${DKPLUGINS}/DKDuktapeDebugger/duk_trans_dvalue.c)
+	dk_copy(${DUKTAPE}/examples/debug-trans-dvalue/duk_trans_dvalue.h ${DKPLUGINS}/DKDuktapeDebugger/duk_trans_dvalue.h OVERWRITE)
+	dk_copy(${DUKTAPE}/examples/debug-trans-dvalue/duk_trans_dvalue.c ${DKPLUGINS}/DKDuktapeDebugger/duk_trans_dvalue.c OVERWRITE)
+endif()
+
 dk_generateCmake(DKDuktapeDebugger)
 dk_assets(DKDuktapeDebugger)
