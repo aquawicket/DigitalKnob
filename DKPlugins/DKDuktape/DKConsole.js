4d9510893aa25c2ca12c08f209dde72c1fe6fc7a
aquawicket <aquawicket@hotmail.com>
2023-01-14 21:40:55 -0500

    commit from git

--- DKPlugins/DKDuktape/DKConsole.js
+++ DKPlugins/DKDuktape/DKConsole.js
@@ -1,7 +1,7 @@
 // https://console.spec.whatwg.org/
 // https://developer.mozilla.org/en-US/docs/Web/API/Console
 
-const Console_HandleColor = function(msg){
+const Console_HandleColor = function Console_HandleColor(msg){
 	//TODO: handle coloring, (currently disabled)
 	if(msg){
 		msg = msg.replace("%c ","")
@@ -12,20 +12,20 @@ const Console_HandleColor = function(msg){
 
 // https://console.spec.whatwg.org/#console-namespace
 var Console = function(pointer) {
-    Console.prototype.assert = function(msg) {
+    Console.prototype.assert = function assert(msg) {
 		msg = Console_HandleColor(msg)
         //if (assertion)
         //    return;
         CPP_DKConsole_assert(msg + "\n");
     }
-    Console.prototype.clear = function() {
+    Console.prototype.clear = function clear() {
         CPP_DKConsole_clear();
     }
-    Console.prototype.debug = function(msg) {
+    Console.prototype.debug = function debug(msg) {
 		msg = Console_HandleColor(msg)
         CPP_DKConsole_debug(msg + "\n");
     }
-    Console.prototype.error = function(msg) {
+    Console.prototype.error = function error(msg) {
 		msg = Console_HandleColor(msg)
 		if(!msg){
 			CPP_DKConsole_error("msg invalid \n");
@@ -43,35 +43,35 @@ var Console = function(pointer) {
 		}
 		CPP_DKConsole_error(err.stack+"\n");
     }
-    Console.prototype.exception = function(msg) {
+    Console.prototype.exception = function exception(msg) {
 		msg = Console_HandleColor(msg)
         CPP_DKConsole_exception(msg + "\n");
     }
-    Console.prototype.group = function(msg) {
+    Console.prototype.group = function group(msg) {
 		msg = Console_HandleColor(msg)
         CPP_DKConsole_group(msg + "\n");
     }
-    Console.prototype.groupCollapsed = function(msg) {
+    Console.prototype.groupCollapsed = function groupCollapsed(msg) {
 		msg = Console_HandleColor(msg)
         CPP_DKConsole_groupCollapsed(msg + "\n");
     }
-    Console.prototype.groupEnd = function(msg) {
+    Console.prototype.groupEnd = function groupEnd(msg) {
 		msg = Console_HandleColor(msg)
         CPP_DKConsole_groupEnd(msg + "\n");
     }
-    Console.prototype.info = function(msg) {
+    Console.prototype.info = function info(msg) {
 		msg = Console_HandleColor(msg)
         CPP_DKConsole_info(msg + "\n");
     }
-    Console.prototype.log = function(msg) {
+    Console.prototype.log = function log(msg) {
 		msg = Console_HandleColor(msg)
         CPP_DKConsole_log(msg + "\n");
     }
-    Console.prototype.trace = function(msg) {
+    Console.prototype.trace = function trace(msg) {
 		msg = Console_HandleColor(msg)
 		CPP_DKConsole_trace(msg + "\n");
     }
-    Console.prototype.warn = function(msg) {
+    Console.prototype.warn = function warn(msg) {
 		msg = Console_HandleColor(msg)
         CPP_DKConsole_warn(msg + "\n");
     }
