b50c5af43d272b45e1e4f108a6d23a8e8f86ad5e
aquawicket <aquawicket@hotmail.com>
2023-01-14 23:58:08 -0500

    commit from git

--- DKPlugins/DKDom/DKDomHTMLScriptElement.js
+++ DKPlugins/DKDom/DKDomHTMLScriptElement.js
@@ -2,75 +2,75 @@
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script
 
 // https://html.spec.whatwg.org/multipage/scripting.html#the-script-element
-var HTMLScriptElement = function(pointer) {
+var HTMLScriptElement = function HTMLScriptElement(pointer) {
     // Properties
 	Object.defineProperty(this, "async", {
-        get: function() {
+        get: function async() {
             return CPP_DKDomHTMLScriptElement_async(pointer);
         }
     });
 	Object.defineProperty(this, "charset", {
-        get: function() {
+        get: function charset() {
             return CPP_DKDomHTMLScriptElement_charset(pointer);
         }
     });
 	Object.defineProperty(this, "crossOrigin", {
-        get: function() {
+        get: function crossOrigin() {
             return CPP_DKDomHTMLScriptElement_crossOrigin(pointer);
         }
     });
 	Object.defineProperty(this, "defer", {
-        get: function() {
+        get: function defer() {
             return CPP_DKDomHTMLScriptElement_defer(pointer);
         }
     });
 	Object.defineProperty(this, "event", {
-        get: function() {
+        get: function event() {
             return CPP_DKDomHTMLScriptElement_event(pointer);
         }
     });
 	Object.defineProperty(this, "integrity", {
-        get: function() {
+        get: function integrity() {
             return CPP_DKDomHTMLScriptElement_integrity(pointer);
         }
     });
 	Object.defineProperty(this, "noModule", {
 		configurable: true,
-        get: function() {
+        get: function noModule() {
             return CPP_DKDomHTMLScriptElement_noModule(pointer);
         }
     });
 	Object.defineProperty(this, "nonce", {
 		configurable: true,
-        get: function() {
+        get: function nonce() {
             return CPP_DKDomHTMLScriptElement_nonce(pointer);
         }
     });
 	Object.defineProperty(this, "language", {
-        get: function() {
+        get: function language() {
             return CPP_DKDomHTMLScriptElement_language(pointer);
         }
     });
 	Object.defineProperty(this, "referrerPolicy", {
-        get: function() {
+        get: function referrerPolicy() {
             return CPP_DKDomHTMLScriptElement_referrerPolicy(pointer);
         }
     });
 	Object.defineProperty(this, "src", {
-        get: function() {
+        get: function src() {
 			return CPP_DKDomElement_getAttribute(pointer, "src")
         },
-		set: function(val) {
+		set: function src(val) {
 			return CPP_DKDomElement_setAttribute(pointer, "src", val)
 		}
     });
 	Object.defineProperty(this, "text", {
-        get: function() {
+        get: function text() {
             return CPP_DKDomHTMLScriptElement_text(pointer);
         }
     });
 	Object.defineProperty(this, "type", {
-        get: function() {
+        get: function type() {
             return CPP_DKDomHTMLScriptElement_type(pointer);
         }
     });
