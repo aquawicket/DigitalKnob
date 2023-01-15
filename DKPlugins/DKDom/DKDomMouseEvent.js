22229e1858b729adbd3f1fdc70d74a1b94fd4709
aquawicket <aquawicket@hotmail.com>
2023-01-15 00:08:13 -0500

    commit from git

--- DKPlugins/DKDom/DKDomMouseEvent.js
+++ DKPlugins/DKDom/DKDomMouseEvent.js
@@ -1,158 +1,158 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
 
-var MouseEvent = function(pointer) {
+var MouseEvent = function MouseEvent(pointer) {
     // Properties
     Object.defineProperty(this, "altKey", {
         //Read only
-        get: function() {
+        get: function altKey() {
             return CPP_DKDomMouseEvent_altKey(pointer)
         }
     })
     Object.defineProperty(this, "button", {
         //Read only
-        get: function() {
+        get: function button() {
             return CPP_DKDomMouseEvent_button(pointer)
         }
     })
     Object.defineProperty(this, "buttons", {
         //Read only
-        get: function() {
+        get: function buttons() {
             return CPP_DKDomMouseEvent_buttons(pointer)
         }
     })
     Object.defineProperty(this, "clientX", {
         //Read only
-        get: function() {
+        get: function clientX() {
             return CPP_DKDomMouseEvent_clientX(pointer)
         }
     })
     Object.defineProperty(this, "clientY", {
         //Read only
-        get: function() {
+        get: function clientY() {
             return CPP_DKDomMouseEvent_clientY(pointer)
         }
     })
     Object.defineProperty(this, "ctrlKey", {
         //Read only
-        get: function() {
+        get: function ctrlKey() {
             return CPP_DKDomMouseEvent_ctrlKey(pointer)
         }
     })
     Object.defineProperty(this, "metaKey", {
         //Read only
-        get: function() {
+        get: function metaKey() {
             return CPP_DKDomMouseEvent_metaKey(pointer)
         }
     })
     Object.defineProperty(this, "movementX", {
         //Read only
-        get: function() {
+        get: function movementX() {
             return CPP_DKDomMouseEvent_movementX(pointer)
         }
     })
     Object.defineProperty(this, "movementY", {
         //Read only
-        get: function() {
+        get: function movementY() {
             return CPP_DKDomMouseEvent_movementY(pointer)
         }
     })
     Object.defineProperty(this, "offsetX", {
         //Read only
-        get: function() {
+        get: function offsetX() {
             return CPP_DKDomMouseEvent_offsetX(pointer)
         }
     })
     Object.defineProperty(this, "offsetY", {
         //Read only
-        get: function() {
+        get: function offsetY() {
             return CPP_DKDomMouseEvent_offsetY(pointer)
         }
     })
     Object.defineProperty(this, "pageX", {
         //Read only
-        get: function() {
+        get: function pageX() {
             return CPP_DKDomMouseEvent_pageX(pointer)
         }
     })
     Object.defineProperty(this, "pageY", {
         //Read only
-        get: function() {
+        get: function pageY() {
             return CPP_DKDomMouseEvent_pageY(pointer)
         }
     })
     Object.defineProperty(this, "region", {
         //Read only
-        get: function() {
+        get: function region() {
             return CPP_DKDomMouseEvent_pageX(pointer)
         }
     })
     Object.defineProperty(this, "relatedTarget", {
         //Read only
-        get: function() {
+        get: function relatedTarget() {
             return CPP_DKDomMouseEvent_relatedTarget(pointer)
         }
     })
     Object.defineProperty(this, "screenX", {
         //Read only
-        get: function() {
+        get: function screenX() {
             return CPP_DKDomMouseEvent_screenX(pointer)
         }
     })
     Object.defineProperty(this, "screenY", {
         //Read only
-        get: function() {
+        get: function screenY() {
             return CPP_DKDomMouseEvent_screenY(pointer)
         }
     })
     Object.defineProperty(this, "shiftKey", {
         //Read only
-        get: function() {
+        get: function shiftKey() {
             return CPP_DKDomMouseEvent_shiftKey(pointer)
         }
     })
     Object.defineProperty(this, "which", {
         //Read only
-        get: function() {
+        get: function which() {
             return CPP_DKDomMouseEvent_which(pointer)
         }
     })
     Object.defineProperty(this, "moxPressure", {
         //Read only,
-        get: function() {
+        get: function moxPressure() {
             return CPP_DKDomMouseEvent_pageX(pointer)
         }
     })
     Object.defineProperty(this, "mozInputSource", {
         //Read only
-        get: function() {
+        get: function mozInputSource() {
             return CPP_DKDomMouseEvent_mozInputSource(pointer)
         }
     })
     Object.defineProperty(this, "webkitForce", {
         //Read only
-        get: function() {
+        get: function webkitForce() {
             return CPP_DKDomMouseEvent_webkitForce(pointer)
         }
     })
     Object.defineProperty(this, "x", {
         //Read only
-        get: function() {
+        get: function x() {
             return CPP_DKDomMouseEvent_x(pointer)
         }
     })
     Object.defineProperty(this, "y", {
         //Read only
-        get: function() {
+        get: function y() {
             return CPP_DKDomMouseEvent_y(pointer)
         }
     })
 
     // Methods
-    MouseEvent.prototype.getModifierState = function() {
+    MouseEvent.prototype.getModifierState = function getModifierState() {
         CPP_DKDomMouseEvent_getModifierState(pointer)
     }
     
-    MouseEvent.prototype.initMouseEvent = function() {
+    MouseEvent.prototype.initMouseEvent = function initMouseEvent() {
         CPP_DKDomMouseEvent_initMouseEvent(pointer)
     }
     
