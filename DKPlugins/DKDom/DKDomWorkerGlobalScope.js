efa7677a4e2ec95f9a93847451de93600491f6c2
aquawicket <aquawicket@hotmail.com>
2023-01-15 00:51:58 -0500

    commit from git

--- DKPlugins/DKDom/DKDomWorkerGlobalScope.js
+++ DKPlugins/DKDom/DKDomWorkerGlobalScope.js
@@ -3,18 +3,18 @@
 // The WorkerGlobalScope mixin is for use of APIs that are to be exposed on Window and WorkerGlobalScope objects.
 
 
-var WorkerGlobalScope = function(pointer) {
+var WorkerGlobalScope = function WorkerGlobalScope(pointer) {
     this.pointer = pointer;
 
 	// Properties
     Object.defineProperty(this, "todo", {
-        get: function() {
+        get: function todo() {
             return CPP_DKWorkerGlobalScope_todo(pointer)
         }
     })
 
 	// Methods
-	WorkerGlobalScope.prototype.todo = function() {
+	WorkerGlobalScope.prototype.todo = function todo() {
 		//TODO
     }
 
