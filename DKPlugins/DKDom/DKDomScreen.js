054608de4664442ce12327a6e8cebe8046a697b5
aquawicket <aquawicket@hotmail.com>
2023-01-15 00:26:22 -0500

    commit from git

--- DKPlugins/DKDom/DKDomScreen.js
+++ DKPlugins/DKDom/DKDomScreen.js
@@ -1,81 +1,81 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/Screen
 
-var Screen = function(pointer) {
+var Screen = function Screen(pointer) {
     Object.defineProperty(this, "availTop", {
-        get: function() {
+        get: function availTop() {
             return CPP_DKDomScreen_availTop(this.pointer);
         }
     });
     Object.defineProperty(this, "availLeft", {
-        get: function() {
+        get: function availLeft() {
             return CPP_DKDomScreen_availLeft(this.pointer);
         }
     });
     Object.defineProperty(this, "availHeight", {
-        get: function() {
+        get: function availHeight() {
             return CPP_DKDomScreen_availHeight(this.pointer);
         }
     });
     Object.defineProperty(this, "availWidth", {
-        get: function() {
+        get: function availWidth() {
             return CPP_DKDomScreen_availWidth(this.pointer);
         }
     });
     Object.defineProperty(this, "colorDepth", {
-        get: function() {
+        get: function colorDepth() {
             return CPP_DKDomScreen_colorDepth(this.pointer);
         }
     });
     Object.defineProperty(this, "height", {
-        get: function() {
+        get: function height() {
             return CPP_DKDomScreen_height(this.pointer);
         }
     });
     Object.defineProperty(this, "left", {
-        get: function() {
+        get: function left() {
             return CPP_DKDomScreen_left(this.pointer);
         }
     });
     Object.defineProperty(this, "orientation", {
-        get: function() {
+        get: function orientation() {
             return CPP_DKDomScreen_orientation(this.pointer);
         }
     });
     Object.defineProperty(this, "pixelDepth", {
-        get: function() {
+        get: function pixelDepth() {
             return CPP_DKDomScreen_pixelDepth(this.pointer);
         }
     });
     Object.defineProperty(this, "top", {
-        get: function() {
+        get: function top() {
             return CPP_DKDomScreen_top(this.pointer);
         }
     });
     Object.defineProperty(this, "width", {
-        get: function() {
+        get: function width() {
             return CPP_DKDomScreen_width(this.pointer);
         }
     });
     Object.defineProperty(this, "mozEnabled", {
-        get: function() {
+        get: function mozEnabled() {
             return CPP_DKDomScreen_mozEnabled(this.pointer);
         }
     });
     Object.defineProperty(this, "mozBrightness", {
-        get: function() {
+        get: function mozBrightness() {
             return CPP_DKDomScreen_mozBrightness(this.pointer);
         }
     });
 
-    Screen.prototype.lockOrientation = function(orientation) {
+    Screen.prototype.lockOrientation = function lockOrientation(orientation) {
         return CPP_DKDomScreen_lockOrientation(orientation);
     }
 	/*
-	Screen.prototype.toString = function(){
+	Screen.prototype.toString = function toString(){
 		return "[object Screen]"
 	}
 	*/
-    Screen.prototype.unlockOrientation = function() {
+    Screen.prototype.unlockOrientation = function unlockOrientation() {
         return CPP_DKDomScreen_unlockOrientation();
     }
 
