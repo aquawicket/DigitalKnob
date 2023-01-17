5405264198c75f302acb91b13ad62d7747dae4d2
aquawicket <aquawicket@hotmail.com>
2023-01-17 16:22:12 -0500

    commit from git

--- DKPlugins/DKDuktapeDebugger/DKDuktapeDebugger.cpp
+++ DKPlugins/DKDuktapeDebugger/DKDuktapeDebugger.cpp
@@ -12,12 +12,12 @@
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions :
 *
-* The above copyright noticeand this permission notice shall be included in all
+* The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
 *
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -107,7 +107,7 @@ bool DKDuktapeDebugger::attach() {
 		duk_trans_dvalue_peek_cb,
 		duk_trans_dvalue_read_flush_cb,
 		duk_trans_dvalue_write_flush_cb,
-		NULL,  /* app request cb */
+		NULL,  // app request cb
 		duk_trans_dvalue_detached_cb,
 		(void*)trans_ctx);
 
