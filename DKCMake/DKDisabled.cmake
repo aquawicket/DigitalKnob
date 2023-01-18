97e58c8673d6fade0a7059a06cc234ebc5f7bd39
aquawicket <aquawicket@hotmail.com>
2023-01-18 15:15:30 -0500

    commit from git

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -569,6 +569,7 @@ if(EMSCRIPTEN) # Disabled for Emscripten targets
 	dk_disable(openframeworks)
 	dk_disable(openssl)
 	dk_disable(opus)
+	dk_disable(osgaudio)			# missing osg cmake includes
 	dk_disable(osgrmlui)
 	dk_disable(quartz_core)
 	dk_disable(rmlui-d3d11)
