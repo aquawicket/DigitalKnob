1b33b8952d00c0189488c1715a51fae30bada8bd
aquawicket <aquawicket@hotmail.com>
2023-01-17 19:31:37 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/simple-getch/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/simple-getch/DKMAKE.cmake
@@ -9,21 +9,11 @@ dk_import(https://github.com/mouuff/simple-getch.git)
 
 
 ### LINK ###
-dk_include				(${GETCH}/getch/include)
-WIN_dk_libDebug			(${GETCH}/getch/libgetch.a)
-WIN_dk_libRelease		(${GETCH}/getch/libgetch.a)
-APPLE_dk_libDebug		(${GETCH}/getch/libgetch.a)
-APPLE_dk_libRelease		(${GETCH}/getch/libgetch.a)
-LINUX_dk_libDebug		(${GETCH}/getch/libgetch.a)
-LINUX_dk_libRelease		(${GETCH}/getch/libgetch.a)
-RASPBERRY_dk_libDebug	(${GETCH}/getch/libgetch.a)
-RASPBERRY_dk_libRelease	(${GETCH}/getch/libgetch.a)
-ANDROID_dk_libDebug		(${GETCH}/getch/libgetch.a)
-ANDROID_dk_libRelease	(${GETCH}/getch/libgetch.a)
+dk_include		(${SIMPLE-GETCH}/getch/include)
+dk_libDebug		(${SIMPLE-GETCH}/getch/libgetch.a)
+dk_libRelease	(${SIMPLE-GETCH}/getch/libgetch.a)
 
 
 ### COMPILE ###
-LINUX_DEBUG_dk_queueCommand			(../../make)
-LINUX_RELEASE_dk_queueCommand		(../../make)
-RASPBERRY_DEBUG_dk_queueCommand		(../../make)
-RASPBERRY_RELEASE_dk_queueCommand	(../../make)
+DEBUG_dk_queueCommand	(../../make)
+RELEASE_dk_queueCommand	(../../make)
