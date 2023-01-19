08ada2c9a445219248432adcf01b5f94a2f37431
aquawicket <aquawicket@hotmail.com>
2023-01-18 22:01:15 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/kdevelop/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/kdevelop/DKMAKE.cmake
@@ -6,17 +6,11 @@ if(NOT LINUX_HOST)
 endif()
 
 
-
-#dk_set(KDEVELOP_VERSION 5.6.1)
-#dk_set(KDEVELOP_FOLDER kdevelop-${KDEVELOP_VERSION})
-#dk_set(KDEVELOP_DL https://download.kde.org/stable/kdevelop/5.6.1/bin/linux/KDevelop-5.6.1-x86_64.AppImage)
-#dk_set(KDEVELOP ${3RDPARTY}/${KDEVELOP_FOLDER})
-#dk_import(${KDEVELOP_DL} ${KDEVELOP})
+### IMPORT ###
 dk_import(https://download.kde.org/stable/kdevelop/5.6.1/bin/linux/KDevelop-5.6.1-x86_64.AppImage)
 
 
-
-LINUX_dk_setPath(${KDEVELOP})
-LINUX_dk_queueCommand(chmod +x KDevelop-5.6.1-x86_64.AppImage)
-LINUX_dk_queueCommand(./KDevelop-5.6.1-x86_64.AppImage)
-
+### INSTALL ###
+#LINUX_dk_setPath(${KDEVELOP})
+LINUX_dk_queueCommand(chmod 777 ${KDEVELOP}/KDevelop-5.6.1-x86_64.AppImage)
+LINUX_dk_queueCommand(${KDEVELOP}/KDevelop-5.6.1-x86_64.AppImage)
