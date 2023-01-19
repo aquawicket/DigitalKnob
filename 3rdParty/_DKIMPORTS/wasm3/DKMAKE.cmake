008b69289f3fa289b577f20bbca82df5e958f86a
aquawicket <aquawicket@hotmail.com>
2023-01-18 19:53:34 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/wasm3/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/wasm3/DKMAKE.cmake
@@ -6,12 +6,16 @@ dk_import(https://github.com/wasm3/wasm3.git BRANCH main)
 
 
 ### LINK ###
-dk_include			(${WASM3})
-dk_include			(${WASM3}/${OS})
-UNIX_dk_libDebug	(${WASM3}/${OS}/${DEBUG_DIR}/source/libm3.a)
-UNIX_dk_libRelease	(${WASM3}/${OS}/${RELEASE_DIR}/source/libm3.a)
-WIN_dk_libDebug		(${WASM3}/${OS}/source/${DEBUG_DIR}/m3.lib)
-WIN_dk_libRelease	(${WASM3}/${OS}/source/${RELEASE_DIR}/m3.lib)
+dk_include				(${WASM3})
+dk_include				(${WASM3}/${OS})
+APPLE_dk_libDebug		(${WASM3}/${OS}/source/${DEBUG_DIR}/libm3.a)
+APPLE_dk_libRelease		(${WASM3}/${OS}/srouce/${RELEASE_DIR}/libm3.a)
+if(NOT APPLE)
+	UNIX_dk_libDebug	(${WASM3}/${OS}/${DEBUG_DIR}/source/libm3.a)
+	UNIX_dk_libRelease	(${WASM3}/${OS}/${RELEASE_DIR}/source/libm3.a)
+endif()
+WIN_dk_libDebug			(${WASM3}/${OS}/source/${DEBUG_DIR}/m3.lib)
+WIN_dk_libRelease		(${WASM3}/${OS}/source/${RELEASE_DIR}/m3.lib)
 
 
 ### GENERATE ###
