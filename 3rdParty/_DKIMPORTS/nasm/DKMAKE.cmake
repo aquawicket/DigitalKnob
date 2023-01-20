24defabad6e89bd04f0051b628e9f99dd77686b7
aquawicket <aquawicket@hotmail.com>
2023-01-19 20:38:02 -0500

    git commit

--- 3rdParty/_DKIMPORTS/nasm/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/nasm/DKMAKE.cmake
@@ -2,10 +2,11 @@
 
 
 LINUX32_HOST_dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.i686.rpm)
-LINUX64_HOST_dk_set(NASM_EXE ${NASM}/nasm)
+LINUX32_HOST_dk_set(NASM_EXE ${NASM}/nasm)
 
-LINUX64_HOST_dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.x86_64.rpm)
-LINUX64_HOST_dk_set(NASM_EXE ${NASM}/nasm)
+#LINUX64_HOST_dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.x86_64.rpm)
+LINUX64_HOST_dk_download(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.x86_64.rpm ${DOWNLOADS})
+#LINUX64_HOST_dk_set(NASM_EXE ${NASM}/nasm)
 
 MAC_HOST_dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/macosx/nasm-2.16.01-macosx.zip)
 MAC_HOST_dk_set(NASM_EXE ${NASM}/nasm)
