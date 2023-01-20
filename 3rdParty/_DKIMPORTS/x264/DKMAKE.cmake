513034e4d7fc596877d43cb7f4d01929eab47d79
aquawicket <aquawicket@hotmail.com>
2023-01-19 20:53:38 -0500

    git commit

--- 3rdParty/_DKIMPORTS/x264/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/x264/DKMAKE.cmake
@@ -20,8 +20,8 @@ WIN_dk_libRelease	(${X264}/${OS}/${RELEASE_DIR}/x264.lib)
 
 ### GENERATE / COMPILE ###
 DEBUG_dk_setPath		(${X264}/${OS}/${DEBUG_DIR})
-DEBUG_dk_queueShell		(${DKCONFIGURE_BUILD})
+DEBUG_dk_queueShell		(${DKCONFIGURE_BUILD} --disable-asm)
 DEBUG_dk_queueShell		(make)
 RELEASE_dk_setPath		(${X264}/${OS}/${RELEASE_DIR})
-RELEASE_dk_queueShell	(${DKCONFIGURE_BUILD})
+RELEASE_dk_queueShell	(${DKCONFIGURE_BUILD} --disable-asm)
 RELEASE_dk_queueShell	(make)
