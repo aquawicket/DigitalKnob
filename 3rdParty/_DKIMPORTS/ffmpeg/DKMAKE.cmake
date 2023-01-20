084212b41c2a77cef5bde8f71002f63a804e1b82
aquawicket <aquawicket@hotmail.com>
2023-01-19 21:27:22 -0500

    git commit

--- 3rdParty/_DKIMPORTS/ffmpeg/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ffmpeg/DKMAKE.cmake
@@ -13,10 +13,48 @@ dk_import(https://git.ffmpeg.org/ffmpeg.git)
 
 ### LINK ###
 dk_include			(${FFMPEG}/src)
-UNIX_dk_libDebug	(${FFMPEG}/${OS}/${DEBUG_DIR}/libffmpeg.a)
-UNIX_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libffmpeg.a)
-WIN_dk_libDebug		(${FFMPEG}/${OS}/${DEBUG_DIR}/ffmpeg.lib)
-WIN_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/ffmpeg.lib)
+
+# avcodec
+UNIX_dk_libDebug	(${FFMPEG}/${OS}/${DEBUG_DIR}/libavcodec.a)
+UNIX_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libavcodec.a)
+WIN_dk_libDebug		(${FFMPEG}/${OS}/${DEBUG_DIR}/avcodec.lib)
+WIN_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/avcodec.lib)
+
+# avdevice
+UNIX_dk_libDebug	(${FFMPEG}/${OS}/${DEBUG_DIR}/libavdevice.a)
+UNIX_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libavdevice.a)
+WIN_dk_libDebug		(${FFMPEG}/${OS}/${DEBUG_DIR}/avdevice.lib)
+WIN_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/avdevice.lib)
+
+# avfilter
+UNIX_dk_libDebug	(${FFMPEG}/${OS}/${DEBUG_DIR}/libavfilter.a)
+UNIX_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libavfilter.a)
+WIN_dk_libDebug		(${FFMPEG}/${OS}/${DEBUG_DIR}/avfilter.lib)
+WIN_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/avfilter.lib)
+
+# avformat
+UNIX_dk_libDebug	(${FFMPEG}/${OS}/${DEBUG_DIR}/libavformat.a)
+UNIX_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libavformat.a)
+WIN_dk_libDebug		(${FFMPEG}/${OS}/${DEBUG_DIR}/avformat.lib)
+WIN_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/avformat.lib)
+
+# avutil
+UNIX_dk_libDebug	(${FFMPEG}/${OS}/${DEBUG_DIR}/libavutil.a)
+UNIX_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libavutil.a)
+WIN_dk_libDebug		(${FFMPEG}/${OS}/${DEBUG_DIR}/avutil.lib)
+WIN_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/avutil.lib)
+
+# swresample
+UNIX_dk_libDebug	(${FFMPEG}/${OS}/${DEBUG_DIR}/libswresample.a)
+UNIX_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libswresample.a)
+WIN_dk_libDebug		(${FFMPEG}/${OS}/${DEBUG_DIR}/swresample.lib)
+WIN_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/swresample.lib)
+
+# swscale
+UNIX_dk_libDebug	(${FFMPEG}/${OS}/${DEBUG_DIR}/libswscale.a)
+UNIX_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libswscale.a)
+WIN_dk_libDebug		(${FFMPEG}/${OS}/${DEBUG_DIR}/swscale.lib)
+WIN_dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/swscale.lib)
 
 
 ### GENERATE / COMPILE ###
