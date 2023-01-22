c0acbd6531372a987a64afa103d1fb3e99aa80f6
aquawicket <aquawicket@hotmail.com>
2023-01-22 16:08:28 -0500

    commit from git

---
+++ 3rdParty/_DKIMPORTS/dependencywalker/DKMAKE.cmake
@@ -0,0 +1,10 @@
+# https://www.dependencywalker.com
+
+if(NOT WIN_HOST)
+	dk_undepend(dependencywalker)
+	dk_return()
+endif()
+
+
+WIN32_HOST_dkimport(https://www.dependencywalker.com/depends22_x86.zip)
+WIN64_HOST_dkimport(https://www.dependencywalker.com/depends22_x64.zip)
