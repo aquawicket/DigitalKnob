113833495f76ab3596710919cc07875ed7c6593e
aquawicket <aquawicket@hotmail.com>
2023-01-21 22:52:38 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/ffmpeg/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ffmpeg/DKMAKE.cmake
@@ -1,7 +1,7 @@
 # https://git.ffmpeg.org/ffmpeg.git
 # https://trac.ffmpeg.org/wiki/CompilationGuide
 # https://www.roxlu.com/2019/062/compiling-ffmpeg-with-x264-on-windows-10-using-msvc
-
+# https://itnext.io/build-ffmpeg-webassembly-version-ffmpeg-js-part-2-compile-with-emscripten-4c581e8c9a16
 
 ### DEPENDS ###
 dk_depend(core_media)
@@ -53,21 +53,75 @@ dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libavformat/libavformat.a)
 
 ### GENERATE / COMPILE ###
 set(USE_MSYS2 1)
-DEBUG_dk_setPath			(${FFMPEG}/${OS}/${DEBUG_DIR})
-#DEBUG_dk_queueshell		(${DKCONFIGURE_BUILD})
-#DEBUG_dk_queueshell		(../../configure --disable-x86asm)
-#DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --disable-shared --enable-static --arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32-)
-UNIX_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --disable-x86asm --disable-iconv)
-WIN32_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw32)
-WIN64_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw64)
-DEBUG_dk_queueshell			(make)
+DEBUG_dk_setPath				(${FFMPEG}/${OS}/${DEBUG_DIR})
+if(EMSCRIPTEN)
+	DEBUG_dk_queueshell(${EMCONFIGURE} ../../configure
+		--pkg-config-flags=--static 
+		--disable-shared 
+		--enable-static 
+		--disable-x86asm 
+		--disable-iconv 
+		--target-os=none 
+		--arch=x86_32 
+		--enable-cross-compile
+		--disable-inline-asm
+		--disable-stripping
+		--disable-programs
+		--disable-doc
+		--extra-cflags="-s USE_PTHREADS"
+		--extra-cxxflags="-s USE_PTHREADS"
+		--extra-ldflags="-s USE_PTHREADS -s INITIAL_MEMORY=33554432"
+		--nm="llvm-nm"
+		--ar=emar
+		--ranlib=emranlib
+		--cc=emcc
+		--cxx=em++
+		--objcc=emcc
+		--dep-cc=emcc)
+	DEBUG_dk_queueShell(${EMMAKE} make)
+else()
+	#DEBUG_dk_queueshell		(${DKCONFIGURE_BUILD})
+	#DEBUG_dk_queueshell		(../../configure --disable-x86asm)
+	#DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --disable-shared --enable-static --arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32-)
+	UNIX_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --disable-x86asm --disable-iconv)
+	WIN32_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw32)
+	WIN64_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw64)
+	DEBUG_dk_queueshell			(make)
+endif()
 
-RELEASE_dk_setPath			(${FFMPEG}/${OS}/${RELEASE_DIR})
-#RELEASE_dk_queueshell		(${DKCONFIGURE_BUILD})
-#RELEASE_dk_queueshell		(../../configure --disable-x86asm)
-#RELEASE_dk_queueshell		(../../configure --pkg-config-flags=--static --disable-shared --enable-static --arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32-)
-UNIX_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --disable-x86asm --disable-iconv)
-WIN32_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw32)
-WIN64_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw64)
-RELEASE_dk_queueshell		(make)
+RELEASE_dk_setPath				(${FFMPEG}/${OS}/${RELEASE_DIR})
+if(EMSCRIPTEN)
+	RELEASE_dk_queueshell(${EMCONFIGURE} ../../configure 
+		--pkg-config-flags=--static 
+		--disable-shared 
+		--enable-static 
+		--disable-x86asm 
+		--disable-iconv
+		--target-os=none 
+		--arch=x86_32 
+		--enable-cross-compile
+		--disable-inline-asm
+		--disable-stripping
+		--disable-programs
+		--disable-doc
+		--extra-cflags="-s USE_PTHREADS"
+		--extra-cxxflags="-s USE_PTHREADS"
+		--extra-ldflags="-s USE_PTHREADS -s INITIAL_MEMORY=33554432"
+		--nm="llvm-nm"
+		--ar=emar
+		--ranlib=emranlib
+		--cc=emcc
+		--cxx=em++
+		--objcc=emcc
+		--dep-cc=emcc)
+	RELEASE_dk_queueShell(${EMMAKE} make)
+else()
+	#RELEASE_dk_queueshell		(${DKCONFIGURE_BUILD})
+	#RELEASE_dk_queueshell		(../../configure --disable-x86asm)
+	#RELEASE_dk_queueshell		(../../configure --pkg-config-flags=--static --disable-shared --enable-static --arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32-)
+	UNIX_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --disable-x86asm --disable-iconv)
+	WIN32_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw32)
+	WIN64_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw64)
+	RELEASE_dk_queueshell		(make)
+endif()
 set(USE_MSYS2 0)
