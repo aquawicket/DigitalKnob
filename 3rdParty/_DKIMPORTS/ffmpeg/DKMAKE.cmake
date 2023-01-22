4f1b74127d7d93eb16f86c6a289619fdb6feb1a1
aquawicket <aquawicket@hotmail.com>
2023-01-22 01:20:33 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/ffmpeg/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/ffmpeg/DKMAKE.cmake
@@ -54,82 +54,89 @@ dk_libRelease	(${FFMPEG}/${OS}/${RELEASE_DIR}/libavformat/libavformat.a)
 ### GENERATE / COMPILE ###
 set(USE_MSYS2 1)
 
-set(FFMPEG_CONFIGURE "${DKCONFIGURE_BUILD}")
-string(REPLACE "CFLAGS=-DMAC -DMAC64 -std=c17" "" FFMPEG_CONFIGURE "${FFMPEG_CONFIGURE}")
-string(REPLACE "CXXFLAGS=-DMAC -DMAC64 -std=c++17" "" FFMPEG_CONFIGURE "${FFMPEG_CONFIGURE}")
-#string(REPLACE "-DMAC64" "" FFMPEG_CONFIGURE "${FFMPEG_CONFIGURE}")
-#string(REPLACE "-std=c17" "" FFMPEG_CONFIGURE "${FFMPEG_CONFIGURE}")
-#string(REPLACE "  " " " FFMPEG_CONFIGURE "${FFMPEG_CONFIGURE}")
+DEBUG_dk_setPath(${FFMPEG}/${OS}/${DEBUG_DIR})
+EMSCRIPTEN_DEBUG_dk_queueshell(${EMCONFIGURE} ../../configure
+	--pkg-config-flags=--static 
+	--disable-shared 
+	--enable-static 
+	--disable-x86asm 
+	--disable-iconv 
+	--target-os=none 
+	--arch=x86_32 
+	--enable-cross-compile
+	--disable-inline-asm
+	--disable-stripping
+	--disable-programs
+	--disable-doc
+	#--extra-cflags="-s USE_PTHREADS"
+	#--extra-cxxflags="-s USE_PTHREADS"
+	#--extra-ldflags="-s USE_PTHREADS -s INITIAL_MEMORY=33554432"
+	--nm="llvm-nm"
+	--ar=emar
+	--ranlib=emranlib
+	--cc=emcc
+	--cxx=em++
+	--objcc=emcc
+	--dep-cc=emcc)
+IOS32_DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=arm --disable-x86asm --disable-iconv)
+IOS64_DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=arm64 --disable-x86asm --disable-iconv)
+IOSSIM32_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --arch=i686 --disable-x86asm --disable-iconv)
+IOSSIM64_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --arch=x86_64 --disable-x86asm --disable-iconv)
+LINUX32_DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=i686 --disable-x86asm --disable-iconv)
+LINUX64_DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=x86_64 --disable-x86asm --disable-iconv)
+MAC32_DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=i686 --disable-x86asm --disable-iconv)
+MAC64_DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=x86_64 --disable-x86asm --disable-iconv)
+RASPBERRY32_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --arch=i686 --disable-x86asm --disable-iconv)
+RASPBERRY64_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --arch=x86_64 --disable-x86asm --disable-iconv)
+WIN32_DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw32)
+WIN64_DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw64)
 
-DEBUG_dk_setPath				(${FFMPEG}/${OS}/${DEBUG_DIR})
-if(EMSCRIPTEN)
-	DEBUG_dk_queueshell(${EMCONFIGURE} ../../configure
-		--pkg-config-flags=--static 
-		--disable-shared 
-		--enable-static 
-		--disable-x86asm 
-		--disable-iconv 
-		--target-os=none 
-		--arch=x86_32 
-		--enable-cross-compile
-		--disable-inline-asm
-		--disable-stripping
-		--disable-programs
-		--disable-doc
-		#--extra-cflags="-s USE_PTHREADS"
-		#--extra-cxxflags="-s USE_PTHREADS"
-		#--extra-ldflags="-s USE_PTHREADS -s INITIAL_MEMORY=33554432"
-		--nm="llvm-nm"
-		--ar=emar
-		--ranlib=emranlib
-		--cc=emcc
-		--cxx=em++
-		--objcc=emcc
-		--dep-cc=emcc)
-	DEBUG_dk_queueShell(${EMMAKE} make)
-else()
-	UNIX_DEBUG_dk_queueshell	(${FFMPEG_CONFIGURE} --pkg-config-flags=--static --disable-x86asm --disable-iconv)
-	#DEBUG_dk_queueshell		(../../configure --disable-x86asm)
-	#DEBUG_dk_queueshell		(../../configure --pkg-config-flags=--static --disable-shared --enable-static --arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32-)
-	#UNIX_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --disable-x86asm --disable-iconv)
-	WIN32_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw32)
-	WIN64_DEBUG_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw64)
-	DEBUG_dk_queueshell			(make)
+EMSCRIPTEN_DEBUG_dk_queueShell(${EMMAKE} make)
+if(NOT EMSCRIPTEN)
+	DEBUG_dk_queueshell(make)
 endif()
 
-RELEASE_dk_setPath				(${FFMPEG}/${OS}/${RELEASE_DIR})
-if(EMSCRIPTEN)
-	RELEASE_dk_queueshell(${EMCONFIGURE} ../../configure 
-		--pkg-config-flags=--static 
-		--disable-shared 
-		--enable-static 
-		--disable-x86asm 
-		--disable-iconv
-		--target-os=none 
-		--arch=x86_32 
-		--enable-cross-compile
-		--disable-inline-asm
-		--disable-stripping
-		--disable-programs
-		--disable-doc
-		#--extra-cflags="-s USE_PTHREADS"
-		#--extra-cxxflags="-s USE_PTHREADS"
-		#--extra-ldflags="-s USE_PTHREADS -s INITIAL_MEMORY=33554432"
-		--nm="llvm-nm"
-		--ar=emar
-		--ranlib=emranlib
-		--cc=emcc
-		--cxx=em++
-		--objcc=emcc
-		--dep-cc=emcc)
-	RELEASE_dk_queueShell(${EMMAKE} make)
-else()
-	#RELEASE_dk_queueshell		(${DKCONFIGURE_BUILD})
-	#RELEASE_dk_queueshell		(../../configure --disable-x86asm)
-	#RELEASE_dk_queueshell		(../../configure --pkg-config-flags=--static --disable-shared --enable-static --arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32-)
-	UNIX_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --disable-x86asm --disable-iconv)
-	WIN32_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw32)
-	WIN64_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw64)
-	RELEASE_dk_queueshell		(make)
+
+RELEASE_dk_setPath(${FFMPEG}/${OS}/${RELEASE_DIR})
+EMSCRIPTEN_RELEASE_dk_queueshell(${EMCONFIGURE} ../../configure 
+	--pkg-config-flags=--static 
+	--disable-shared 
+	--enable-static 
+	--disable-x86asm 
+	--disable-iconv
+	--target-os=none 
+	--arch=x86_32 
+	--enable-cross-compile
+	--disable-inline-asm
+	--disable-stripping
+	--disable-programs
+	--disable-doc
+	#--extra-cflags="-s USE_PTHREADS"
+	#--extra-cxxflags="-s USE_PTHREADS"
+	#--extra-ldflags="-s USE_PTHREADS -s INITIAL_MEMORY=33554432"
+	--nm="llvm-nm"
+	--ar=emar
+	--ranlib=emranlib
+	--cc=emcc
+	--cxx=em++
+	--objcc=emcc
+	--dep-cc=emcc)
+IOS32_RELEASE_dk_queueshell			(../../configure --pkg-config-flags=--static --arch=arm --disable-x86asm --disable-iconv)
+IOS64_RELEASE_dk_queueshell			(../../configure --pkg-config-flags=--static --arch=arm64 --disable-x86asm --disable-iconv)
+IOSSIM32_RELEASE_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=i686 --disable-x86asm --disable-iconv)
+IOSSIM64_RELEASE_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=x86_64 --disable-x86asm --disable-iconv)
+LINUX32_RELEASE_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=i686 --disable-x86asm --disable-iconv)
+LINUX64_RELEASE_dk_queueshell		(../../configure --pkg-config-flags=--static --arch=x86_64 --disable-x86asm --disable-iconv)
+MAC32_RELEASE_dk_queueshell			(../../configure --pkg-config-flags=--static --arch=i686 --disable-x86asm --disable-iconv)
+MAC64_RELEASE_dk_queueshell			(../../configure --pkg-config-flags=--static --arch=x86_64 --disable-x86asm --disable-iconv)
+RASPBERRY32_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --arch=i686 --disable-x86asm --disable-iconv)
+RASPBERRY64_RELEASE_dk_queueshell	(../../configure --pkg-config-flags=--static --arch=x86_64 --disable-x86asm --disable-iconv)
+WIN32_RELEASE_dk_queueshell			(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw32)
+WIN64_RELEASE_dk_queueshell			(../../configure --pkg-config-flags=--static --disable-shared --enable-static --target-os=mingw64)
+
+EMSCRIPTEN_RELEASE_dk_queueShell(${EMMAKE} make)
+if(NOT EMSCRIPTEN)
+	RELEASE_dk_queueshell(make)
 endif()
+
 set(USE_MSYS2 0)
