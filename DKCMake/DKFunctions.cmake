702845ec3f4aef3762f73ef1471cbc3856918cb2
aquawicket <aquawicket@hotmail.com>
2023-01-23 16:11:52 -0500

    commit from git

--- DKCMake/DKFunctions.cmake
+++ DKCMake/DKFunctions.cmake
@@ -890,18 +890,20 @@ endfunction()
 ###############################################################################
 # dk_deleteEmptyDirectories(path)
 #
-#	Delete all empty directories with a path
+#	Delete all empty directories within a path
 #
-#	@path	- The path to search for empty folders to delete
+#	@path	- The path to remove all empty directories from
 #
 function(dk_deleteEmptyDirectories path)
 	DKDEBUGFUNC(${ARGV})
 	if(NOT EXISTS ${path})
 		dk_error("path:${path} does not exist")
-		return()
+		dk_return()
 	endif()
 	if(WIN_HOST)
-		execute_process(COMMAND for /f "delims=" %d in ('dir /s /b /ad ^| sort /r') do rd "%d" WORKING_DIRECTORY ${path})
+		#execute_process(COMMAND for /f "delims=" %d in ('dir /s /b /ad ^| sort /r') do rd "%d" WORKING_DIRECTORY ${path})
+		# https://stackoverflow.com/a/30138960/688352
+		execute_process(COMMAND ROBOCOPY ${path} ${path} /S /MOVE WORKING_DIRECTORY ${path})
 	else()
 		dk_error("Not implemented for this platform")
 	endif()
