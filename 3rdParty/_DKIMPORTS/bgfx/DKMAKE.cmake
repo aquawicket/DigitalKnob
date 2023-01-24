a5cfaf78af42190d1190c34adcdd13b216afd403
aquawicket <aquawicket@hotmail.com>
2023-01-23 22:35:34 -0500

    commit from git

---
+++ 3rdParty/_DKIMPORTS/bgfx/DKMAKE.cmake
@@ -0,0 +1,24 @@
+# https://github.com/bkaradzic/bgfx.git
+
+### DEPENDS ###
+#dk_depend(libname)
+
+
+### INSTALL ###
+dk_import(https://github.com/bkaradzic/bgfx.git)
+
+
+### LINK ###
+dk_include			(${BGFX}/include)
+UNIX_dk_libDebug	(${BGFX}/${OS}/${DEBUG_DIR}/libbgfx.a)
+UNIX_dk_libRelease	(${BGFX}/${OS}/${RELEASE_DIR}/libbgfx.a)
+WIN_dk_libDebug		(${BGFX}/${OS}/${DEBUG_DIR}/bgfx.lib)
+WIN_dk_libRelease	(${BGFX}/${OS}/${RELEASE_DIR}/bgfx.lib)
+
+
+### GENERATE ###
+dk_queueCommand(${DKCMAKE_BUILD} ${BGFX})
+
+
+### COMPILE ###
+dk_build(${BGFX})
