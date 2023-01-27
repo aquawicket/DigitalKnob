4aa865baeca7d8e9b3179c0ac0958f7a0771f101
aquawicket <aquawicket@hotmail.com>
2023-01-26 19:40:50 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/libxcursor-dev/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libxcursor-dev/DKMAKE.cmake
@@ -4,8 +4,12 @@ if(NOT LINUX AND NOT RASPBERRY)
 endif()
 
 ### INSTALL ###
-dk_set(CURRENT_DIR /usr)
-dk_command(sudo apt -y install libxcursor-dev)
+#dk_set(CURRENT_DIR /usr)
+if(TINYCORE)
+	dk_command(tce-load -wi libXcursor-dev.tcz)
+else()
+	dk_command(sudo apt -y install libxcursor-dev)
+endif()
 
 ### LINK ###
 SET(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE} -lXcursor")
