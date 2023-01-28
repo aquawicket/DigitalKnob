a4dfb46650d1ff7d949bb0eb82e61ee04e04680e
aquawicket <aquawicket@hotmail.com>
2023-01-27 18:50:48 -0800

    commit from git

---
+++ DKDocs/BuildScripts/build.cmd
@@ -0,0 +1,250 @@
+:: Windows Batch file DK builder
+@echo off
+if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
+
+set "DIGITALKNOB=C:\Users\%USERNAME%\digitalknob"
+set "DKPATH=%DIGITALKNOB%\DK"
+set "DKCMAKE=%DIGITALKNOB%\DK\DKCMake"
+set "DKDOWNLOAD=%DIGITALKNOB%\download"
+set "GIT_DL=https://github.com/git-for-windows/git/releases/download/v2.30.1.windows.1/Git-2.30.1-32-bit.exe"
+set "CMAKE_DL=https://github.com/Kitware/CMake/releases/download/v3.21.1/cmake-3.21.1-windows-i386.msi"
+::set "MSBUILD_DL=https://download.visualstudio.microsoft.com/download/pr/5e397ebe-38b2-4e18-a187-ac313d07332a/169156e6e9a005d49b357c42240184dc1e3ccc28ebc777e70d49257c074f77e8/vs_Community.exe"
+set "MSBUILD_DL=https://aka.ms/vs/17/release/vs_community.exe"
+set "download=certutil.exe -urlcache -split -f"
+::set "download=bitsadmin /transfer myDownloadJob /download /priority normal"
+set "APP="
+set "OS="
+set "TYPE="
+if NOT exist "%DIGITALKNOB%" mkdir "%DIGITALKNOB%"
+if NOT exist "%DKDOWNLOAD%" mkdir "%DKDOWNLOAD%"
+
+:pickapp
+ECHO.
+ECHO 1. Git Update
+ECHO 2. Git Commit
+ECHO 3. DKBuilder
+ECHO 4. DKBuilderGui
+ECHO 5. Clear Screen
+ECHO 6. Exit
+set choice=
+set /p choice=Please select an app to build: 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto gitupdate
+if '%choice%'=='2' goto gitcommit
+if '%choice%'=='3' goto dkbuilder
+if '%choice%'=='4' goto dkbuildergui
+:: if '%choice%'=='5' goto 01_DKCore
+:: if '%choice%'=='6' goto 01_DKJavascript
+:: if '%choice%'=='7' goto 01_DKSDL
+:: if '%choice%'=='8' goto 01_DKSDLRml
+:: if '%choice%'=='9' goto 01_DKCef
+:: if '%choice%'=='10' goto 01_DKSDLCef
+:: if '%choice%'=='11' goto 01_DKSDLText
+:: if '%choice%'=='12' goto 01_DKSDLRmlCef
+:: if '%choice%'=='13' goto 01_DKTestAll
+if '%choice%'=='5' goto clearscreen
+if '%choice%'=='6' goto end
+ECHO "%choice%" is not valid, try again
+goto pickapp
+
+:gitupdate
+if exist "C:\Program Files\Git\bin\git.exe" set "GIT=C:\Program Files\Git\bin\git.exe"
+if exist "C:\Program Files (x86)\Git\bin\git.exe" set "GIT=C:\Program Files (x86)\Git\bin\git.exe"
+if NOT exist "%GIT%" (
+	ECHO "installing git"
+	%download% %GIT_DL% "%DKDOWNLOAD%\Git-2.30.1-32-bit.exe"
+	::if NOT "%ERRORLEVEL%" == "0" goto error
+	"%DKDOWNLOAD%\Git-2.30.1-32-bit.exe" /VERYSILENT /NORESTART
+	::if NOT "%ERRORLEVEL%" == "0" goto error
+	if exist "C:\Program Files\Git\bin\git.exe" set "GIT=C:\Program Files\Git\bin\git.exe"
+	if exist "C:\Program Files (x86)\Git\bin\git.exe" set "GIT=C:\Program Files (x86)\Git\bin\git.exe"
+)
+if NOT exist "%DKPATH%\.git" (
+	"%GIT%" clone https://github.com/aquawicket/DigitalKnob.git "%DKPATH%"
+)
+if NOT "%ERRORLEVEL%" == "0" goto error
+
+cd "%DKPATH%"
+"%GIT%" pull --all
+"%GIT%" checkout -- .
+if NOT "%ERRORLEVEL%" == "0" goto error
+"%GIT%" checkout Development
+if NOT "%ERRORLEVEL%" == "0" (
+	echo Remote has no Development branch. Creating...
+	"%GIT%" checkout -b Development main
+	"%GIT%" push --set-upstream origin Development
+)
+
+
+::if NOT "%ERRORLEVEL%" == "0" goto error
+goto pickapp
+
+:gitcommit
+if exist "C:\Program Files\Git\bin\git.exe" set "GIT=C:\Program Files\Git\bin\git.exe"
+if exist "C:\Program Files (x86)\Git\bin\git.exe" set "GIT=C:\Program Files (x86)\Git\bin\git.exe"
+if NOT exist "%GIT%" (
+	ECHO "installing git"
+	%download% %GIT_DL% "%DKDOWNLOAD%\Git-2.30.1-32-bit.exe"
+	::if NOT "%ERRORLEVEL%" == "0" goto error
+	"%DKDOWNLOAD%\Git-2.30.1-32-bit.exe /VERYSILENT /NORESTART"
+	::if NOT "%ERRORLEVEL%" == "0" goto error
+	if exist "C:\Program Files\Git\bin\git.exe" set "GIT=C:\Program Files\Git\bin\git.exe"
+	if exist "C:\Program Files (x86)\Git\bin\git.exe" set "GIT=C:\Program Files (x86)\Git\bin\git.exe"
+)
+cd %DKPATH%
+"%GIT%" config user.email "aquawicket@hotmail.com"
+"%GIT%" config user.name "aquawicket"
+"%GIT%" commit -a -m "git commit"
+::if NOT "%ERRORLEVEL%" == "0" goto error
+"%GIT%" push
+::if NOT "%ERRORLEVEL%" == "0" goto error
+goto pickapp
+
+:clearscreen
+cls
+goto pickapp
+
+:dkbuilder
+set APP=DKBuilder
+goto checkApp
+
+:dkbuildergui
+set APP=DKBuilderGui
+goto checkApp
+
+:checkApp
+if NOT exist "%DKPATH%\DKApps\%APP%\DKMAKE.cmake" (
+	ECHO ERROR: %APP%/DKMAKE.cmake file not found
+	goto pickapp
+) 
+goto pickos
+
+
+:pickos
+ECHO %APP%
+ECHO.
+ECHO 1. Windows 32
+ECHO 2. Windows 64
+ECHO 3. Go Back
+ECHO 4. Exit
+set choice=
+set /p choice=Please select an OS to build for: 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto win32
+if '%choice%'=='2' goto win64
+if '%choice%'=='3' goto pickapp
+if '%choice%'=='4' goto end
+ECHO "%choice%" is not valid, try again
+goto pickos
+
+:win32
+set OS="win32"
+goto build
+
+:win64
+set OS="win64"
+goto build
+
+
+:type
+ECHO %APP% - %OS%
+ECHO.
+ECHO 1. Debug
+ECHO 2. Release
+ECHO 3. All
+ECHO 4. Go Back
+ECHO 5. Exit
+set choice=
+set /p choice=Please select a build type: 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto debug
+if '%choice%'=='2' goto release
+if '%choice%'=='3' goto all
+if '%choice%'=='4' goto pickos
+if '%choice%'=='5' goto end
+ECHO "%choice%" is not valid, try again
+goto type
+
+:debug
+set TYPE="Debug"
+goto build
+
+:release
+set TYPE="Release"
+goto build
+
+:all
+set TYPE="All"
+goto build
+
+
+
+:build
+echo Deleteing CMake cache . . .
+cd "%DIGITALKNOB%"
+for /r %%i in (CMakeCache.*) do del "%%i"
+::if NOT "%ERRORLEVEL%" == "0" goto error
+for /d /r %%i in (*CMakeFiles*) do rd /s /q "%%i"
+::if NOT "%ERRORLEVEL%" == "0" goto error
+
+echo Deleteing .tmp files . . .
+cd "%DIGITALKNOB%"
+for /r %%i in (*.tmp) do del "%%i"
+::if NOT "%ERRORLEVEL%" == "0" goto error
+for /r %%i in (*.TMP) do del "%%i"
+::if NOT "%ERRORLEVEL%" == "0" goto error
+
+
+echo ****** BUILDING %APP% - %OS% ******
+if exist "C:\Program Files\CMake\bin\cmake.exe" set "CMAKE=C:\Program Files\CMake\bin\cmake.exe"
+if exist "C:\Program Files (x86)\CMake\bin\cmake.exe" set "CMAKE=C:\Program Files (x86)\CMake\bin\cmake.exe"
+if NOT exist "%CMAKE%" (
+    echo "installing cmake"
+	echo "%CMAKE_DL%"
+	%download% %CMAKE_DL% "%DKDOWNLOAD%\cmake-3.21.1-windows-i386.msi"
+	if NOT "%ERRORLEVEL%" == "0" goto error
+	"%DKDOWNLOAD%\cmake-3.21.1-windows-i386.msi"
+	if NOT "%ERRORLEVEL%" == "0" goto error
+	if exist "C:\Program Files\CMake\bin\cmake.exe" set "CMAKE=C:\Program Files\CMake\bin\cmake.exe"
+	if exist "C:\Program Files (x86)\CMake\bin\cmake.exe" set "CMAKE=C:\Program Files (x86)\CMake\bin\cmake.exe"
+)
+if exist "C:\Program Files\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" set "MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" set "MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+if NOT exist "%MSBUILD%" (
+    echo "installing Visual Studio"
+	echo "%MSBUILD_DL%"
+	
+	%download% %MSBUILD_DL% "%DKDOWNLOAD%\vs_Community.exe"
+	::if NOT "%ERRORLEVEL%" == "0" goto error
+	"%DKDOWNLOAD%\vs_Community.exe"
+	::if NOT "%ERRORLEVEL%" == "0" goto error
+	if exist "C:\Program Files\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" set "MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+	if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" set "MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" set "MSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+)
+set "APP_PATH=%DKPATH%\DKApps\%APP%"
+ECHO %APP_PATH%
+if NOT exist "%APP_PATH%\%OS%" mkdir "%APP_PATH%\%OS%"
+::if NOT "%ERRORLEVEL%" == "0" goto error
+cd "%APP_PATH%\%OS%"
+::"%CMAKE%" -G "Visual Studio 16 2019" -A Win32 -DDEBUG=ON -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON %DKCMAKE%
+"%CMAKE%" -G "Visual Studio 17 2022" -A Win32 -DDEBUG=ON -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON %DKCMAKE%
+::if NOT "%ERRORLEVEL%" == "0" goto error
+"%MSBUILD%" %APP%_APP.sln /p:Configuration=Debug
+::if NOT "%ERRORLEVEL%" == "0" goto error
+"%MSBUILD%" %APP%_APP.sln /p:Configuration=Release
+::if NOT "%ERRORLEVEL%" == "0" goto error
+
+goto pickapp
+
+
+:error
+echo Failed with error code: %ERRORLEVEL%
+pause
+
+:end
+ECHO Done
+exit
---
+++ DKDocs/BuildScripts/tinycorelinux.sh
@@ -0,0 +1,228 @@
+#!/bin/sh
+
+# to run this script requires privledges
+# > chmod 777 build.sh
+
+if [ -e /proc/device-tree/model ]; then
+	MODEL=$(tr -d '\0' </proc/device-tree/model)
+fi
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	DIGITALKNOB="/home/$USER/digitalknob"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	DIGITALKNOB="/Users/$USER/digitalknob"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+	DIGITALKNOB="C:/Users/$USERNAME/digitalknob"
+elif [[ "$OSTYPE" == "msys" ]]; then
+	DIGITALKNOB="C:/Users/$USERNAME/digitalknob"
+elif [[ "$OSTYPE" == "win32" ]]; then #I'm not sure this can happen
+	DIGITALKNOB="C:/Users/$USERNAME/digitalknob" 
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+	echo "TODO: DIGITALKNOB NOT SET"
+else
+    echo "UNKNOWN OS TYPE ($OSTYPE)"
+fi
+
+DKPATH="$DIGITALKNOB/DK"
+DKCMAKE="$DIGITALKNOB/DK/DKCMake"
+
+echo "hostname		= $HOSTNAME"
+echo "hosttype		= $HOSTTYPE"
+echo "ostype		= $OSTYPE"
+echo "machtype		= $MACHTYPE"
+echo "model			= $MODEL"
+echo "user			= $USER"
+echo "username		= $USERNAME"
+echo "digitalknob	= $DIGITALKNOB"
+echo "dkpath		= $DKPATH"
+echo "dkcmake		= $DKCMAKE"
+
+sudo echo
+
+# https://unix.stackexchange.com/a/293605
+COLUMNS=1
+
+while :
+	do
+	echo " "
+	PS3='Please update and select an app to build: '
+	options=("Git Update" "Git Commit" "DKBuilder" "DKSDLRmlUi" "DKTestAll" "Clear Screen" "Exit")
+	select opt in "${options[@]}"
+	do
+		case $opt in
+			"Git Update")
+				echo "$opt"
+				if [[ "$OSTYPE" == "darwin"* ]]; then
+					echo "install Homebrew and git"
+					#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+					# https://github.com/Homebrew/brew/issues/10368
+					# rm -fr $(brew --repo homebrew/core)
+					# brew tap homebrew/core
+					# brew install git
+				else
+					sudo apt-get -y install git
+				fi
+				
+				
+				if [[ ! -d "$DKPATH/.git" ]]; then
+					git clone https://github.com/aquawicket/DigitalKnob.git $DKPATH
+				fi
+				cd $DKPATH
+				git pull --all
+				git checkout -- .
+				git checkout Development
+				if [[ "$?" == "0" ]]; then
+					echo "Development branch selected"
+				else
+					echo "Remote has no Development branch. Creating..."
+					git checkout -b Development main
+					git push --set-upstream origin Development
+				fi
+								
+				
+				chmod +x $DKPATH/build.sh
+				chmod +x $DKCMAKE/dkbuild.sh
+				;;
+			"Git Commit")
+				echo "$opt"
+				if [[ "$OSTYPE" == "darwin"* ]]; then
+					echo "install Homebrew and git"
+					#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+					# https://github.com/Homebrew/brew/issues/10368
+					# rm -fr $(brew --repo homebrew/core)
+					# brew tap homebrew/core
+					# brew install git
+				else
+					sudo apt-get -y install git
+				fi
+				cd $DKPATH
+				git commit -a -m "git commit"
+				git push
+				;;
+			"DKBuilder")
+				echo "$opt"
+				APP="DKBuilder"
+				break
+				;;
+			"DKSDLRmlUi")
+				echo "$opt"
+				APP="DKSDLRmlUi"
+				break
+				;;
+			"DKTestAll")
+				echo "$opt"
+				APP="DKTestAll"
+				break
+				;;
+			"Clear Screen")
+				echo "$opt"
+				clear
+				;;
+			"Exit")
+				echo "$opt"
+				exit 0
+				;;
+			*) echo "invalid option $REPLY";;
+		esac 
+		REPLY=
+	done
+
+	echo " "
+	PS3='Please select an OS to build for: '
+	if [[ "$MODEL" == "Raspberry"* ]]; then
+		options=("raspberry32" "Exit")
+	elif [[ "$OSTYPE" == "linux-gnu"* ]] && [[ "$HOSTTYPE" == "x86_64"* ]]; then
+		options=("linux64" "Exit")
+	elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		options=("linux32" "Exit")
+	elif [[ "$OSTYPE" == "darwin"* ]] && [[ "$HOSTTYPE" == "x86_64"* ]]; then
+		options=("mac64" "Exit")
+	else
+		echo "UNKNOWN OS TYPE ($OSTYPE)"
+		options=("Exit")
+	fi
+	select opt in "${options[@]}"
+	do
+		case $opt in
+			"linux32")
+				echo "$opt"
+				OS="linux32"
+				break
+				;;
+			"linux64")
+				echo "$opt"
+				OS="linux64"
+				break
+				;;
+			"mac64")
+				echo "$opt"
+				OS="mac64"
+				break
+				;;
+			"raspberry32")
+				echo "$opt"
+				OS="raspberry32"
+				break
+				;;
+			"Exit")
+				echo "$opt"
+				exit 0
+				;;
+			*) echo "invalid option $REPLY";;
+		esac 
+		REPLY=
+	done
+
+	cd $DIGITALKNOB
+	echo Deleteing CMake cache . . .
+	find . -name "CMakeCache.*" -delete
+	rm -rf `find . -type d -name CMakeFiles`
+	
+	echo Deleteing .tmp files . . .
+	find . -name "*.tmp" -delete
+	find . -name "*.TMP" -delete
+		
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		#CLANG_PATH=$(which clang)
+		#CLANGPP_PATH=$(which clang++)
+		#export CC="$CLANG_PATH"
+		#export CXX="$CLANGPP_PATH"
+		#echo "CLANG_PATH = $CLANG_PATH"
+		#echo "CLANGPP_PATH = $CLANGPP_PATH"
+		#brew install cmake		
+		mkdir $DKPATH/DKApps/$APP/$OS
+		cd $DKPATH/DKApps/$APP/$OS
+		cmake -G "Xcode" -DMAC_64=ON -DCMAKE_OSX_ARCHITECTURES=x86_64 -DDEBUG=ON -DRELEASE=ON -DREBUILD=ON -DSTATIC=ON $DKCMAKE
+		xcodebuild -configuration Debug build
+		xcodebuild -configuration Release build
+	else #Linux, Raspberry Pi
+		GCC_PATH=$(which gcc)
+		GPP_PATH=$(which g++)
+		export CC="$GCC_PATH"
+		export CXX="$GPP_PATH"
+		echo "GCC_PATH = $GCC_PATH"
+		echo "GPP_PATH = $GPP_PATH"
+		sudo apt-get -y install cmake
+		sudo apt-get -y install gcc
+		sudo apt-get -y install g++
+		
+		mkdir $DKPATH/DKApps/$APP/$OS
+		mkdir $DKPATH/DKApps/$APP/$OS/Debug
+		cd $DKPATH/DKApps/$APP/$OS/Debug
+		cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER="$GCC_PATH" -DCMAKE_CXX_COMPILER="$GPP_PATH" -DRELEASE=OFF -DDEBUG=ON -DREBUILDALL=ON -DSTATIC=ON $DKCMAKE
+		make #$APP
+		chmod +x $DKPATH/DKApps/$APP/$OS/Debug/$APP
+		
+		mkdir $DKPATH/DKApps/$APP/$OS/Release
+		cd $DKPATH/DKApps/$APP/$OS/Release
+		cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER="$GCC_PATH" -DCMAKE_CXX_COMPILER="$GPP_PATH" -DDEBUG=OFF -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON $DKCMAKE
+		make #$APP
+		chmod +x $DKPATH/DKApps/$APP/$OS/Release/$APP
+	fi
+
+    unset APP
+	unset OS
+done
+
+
+exec $SHELL #keep terminal open
