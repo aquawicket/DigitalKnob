91618ba137848c292bb47fa2fb9836ce3d166856
aquawicket <aquawicket@hotmail.com>
2023-01-28 18:17:37 -0800

    commit from git

--- DKCMake/DKFunctions.cmake
+++ DKCMake/DKFunctions.cmake
@@ -1253,16 +1253,18 @@ function(dk_enable plugin)
 		if(${ARGC} GREATER 1)
 			dk_set(${${ARGV1}} ON)
 			dk_set(HAVE_${${ARGV1}} ON)
-			# In c++ we can't use certian symbals in the preprocess or for macros. - must be turned to _
+			# In c++ we can't use certian symbals in the preprocess or for macros. I.E. - must be turned to _
 			string(REPLACE "-" "_" argv1_macro "${ARGV1}")
 			string(REPLACE "." "_" argv1_macro "${argv1_macro}")
+			string(REPLACE "+" "x" argv1_macro "${argv1_macro}")
 			dk_define(HAVE_${argv1_macro})
 		else()
 			dk_set(${plugin} ON)
 			dk_set(HAVE_${plugin} ON)
-			# In c++ we can't use certian symbals in the preprocess or for macros. - must be turned to _
+			# In c++ we can't use certian symbals in the preprocess or for macros. I.E. - must be turned to _
 			string(REPLACE "-" "_" plugin_macro "${plugin}")
 			string(REPLACE "." "_" plugin_macro "${plugin_macro}")
+			string(REPLACE "+" "x" plugin_macro "${plugin_macro}")
 			dk_define(HAVE_${plugin_macro})
 		endif()
 	endif()
