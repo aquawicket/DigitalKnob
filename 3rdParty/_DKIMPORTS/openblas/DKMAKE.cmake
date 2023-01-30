d7d13ce9a87764abb3533d5a1594d6ad6e65051f
aquawicket <aquawicket@hotmail.com>
2023-01-29 17:30:58 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/openblas/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/openblas/DKMAKE.cmake
@@ -7,8 +7,8 @@ dk_import(https://github.com/xianyi/OpenBLAS.git BRANCH develop)
 
 ### LINK ###
 dk_include				(${OPENBLAS})
-UNIX_dk_libDebug		(${OPENBLAS}/${OS}/${DEBUG_DIR}/libopenblas.a)
-UNIX_dk_libRelease		(${OPENBLAS}/${OS}/${RELEASE_DIR}/libopenblas.a)
+UNIX_dk_libDebug		(${OPENBLAS}/${OS}/${DEBUG_DIR}/lib/libopenblas.a)
+UNIX_dk_libRelease		(${OPENBLAS}/${OS}/${RELEASE_DIR}/lib/libopenblas.a)
 WIN_dk_libDebug			(${OPENBLAS}/${OS}/lib/DEBUG/openblas.lib)
 WIN_dk_libRelease		(${OPENBLAS}/${OS}/lib/RELEASE/openblas.lib)
 
