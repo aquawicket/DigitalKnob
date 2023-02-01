332949f69d0bafb094cdcc6d134bfc2c19447df9
aquawicket <aquawicket@hotmail.com>
2023-02-01 04:29:15 -0800

    commit from git

--- DKPlugins/DK/DKPreprocessor.h
+++ DKPlugins/DK/DKPreprocessor.h
@@ -355,7 +355,7 @@ DKMSG(test DKMSG)
     #define DISABLE_WARNING_UNREFERENCED_FUNCTION			DISABLE_WARNING(4505)
     // other warnings you want to deactivate... 
 #elif defined(__GNUC__) || defined(__clang__)
-    #define DO_PRAGMA(X) _Pragma(#X)
+   // #define DO_PRAGMA(X) _Pragma(#X)
     #define DISABLE_WARNING_PUSH							DO_PRAGMA(GCC diagnostic push)
     #define DISABLE_WARNING_POP								DO_PRAGMA(GCC diagnostic pop) 
     #define DISABLE_WARNING(warningName)					DO_PRAGMA(GCC diagnostic ignored #warningName)
