fa183048a0ba4355fd7e4bd569c9b03622f15719
aquawicket <aquawicket@hotmail.com>
2023-02-07 09:34:56 -0800

    commit from git

--- DKPlugins/DKDom/DKDomWebSocketServer.js
+++ DKPlugins/DKDom/DKDomWebSocketServer.js
@@ -2,7 +2,61 @@
 var WebSocketServer = function WebSocketServer(pointer) {
     this.pointer = pointer;
 
-	//TODO
+	//// Instance properties ////
+	Object.defineProperty(this, "isConnected", {
+        get: function isConnected() {
+            return CPP_DKDomWebSocketServer_isConnected(pointer)
+        }
+    })
+	
+	//// Instance methods ////
+	WebSocket.prototype.disconnect = function disconnect() {
+		//TODO
+    }
+	WebSocket.prototype.send = function send() {
+		//TODO
+    }	
+	WebSocket.prototype.start = function start() {
+		//TODO
+    }	
+	
+	//// Events ////
+	this.close_func = null;
+    Object.defineProperty(this, "onclose", {
+        get: function onclose() {
+            return this.close_func;
+        },
+        set: function onclose(func) {
+            func && this.addEventListener("close", func)
+			!func && this.removeEventListener("close", this.close_func)
+            this.close_func = func
+        }
+    });
+	
+	this.message_func = null;
+    Object.defineProperty(this, "onmessage", {
+        get: function onmessage() {
+            return this.message_func;
+        },
+        set: function onmessage(func) {
+            func && this.addEventListener("message", func)
+			!func && this.removeEventListener("message", this.message_func)
+            this.message_func = func
+        }
+    });
+	
+	this.open_func = null;
+    Object.defineProperty(this, "onopen", {
+        get: function onopen() {
+            return this.open_func;
+        },
+        set: function onopen(func) {
+            func && this.addEventListener("open", func)
+			!func && this.removeEventListener("open", this.open_func)
+            this.open_func = func
+        }
+    });
+	
 	
 	//// toString ////
 	if(this.toString() === "[object Object]")
