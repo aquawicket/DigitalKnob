7651a389267d68329b9cbf73900c947f62c94962
aquawicket <aquawicket@hotmail.com>
2023-02-08 16:01:30 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/msys2/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/msys2/DKMAKE.cmake
@@ -17,16 +17,31 @@ if(NOT EXISTS ${MSYS2}/msys2.exe)
 	dk_info("Installing ${MSYS2_FOLDER}")
 	dk_download(${MSYS2_DL} ${DKDOWNLOAD})
 	dk_command(${DKDOWNLOAD}/msys2-x86_64-20221216.exe install --root ${MSYS2} --confirm-command)
-	
-	### ADD msys2 bin directory to path environment variable
-	dk_setEnv("PATH" "${MSYS2}/usr/bin")
-	
-	# install needed packages
-	dk_command(bash -c "pacman -S make --noconfirm")						# make
-	dk_command(bash -c "pacman -S diffutils --noconfirm")					# diffutils
-	dk_command(bash -c "pacman -S yasm --noconfirm")						# yasm
-	dk_command(bash -c "pacman -S mingw-w64-x86_64-gcc --noconfirm")		# WIN64 builds
-	dk_command(bash -c "pacman -S mingw-w64-i686-gcc --noconfirm")			# WIN32 builds
-	dk_command(bash -c "pacman -S mingw-w64-x86_64-autotools --noconfirm")	# used by giflib
+endif()	
+
+
+### ADD msys2 bin directory to path environment variable
+dk_setEnv("PATH" "${MSYS2}/usr/bin")
+
+### Install other utilities ###
+if(NOT EXISTS ${MSYS2}/usr/bin/make.exe)
+	dk_command(bash -c "pacman -S make --noconfirm")					# make
 endif()
 
+if(NOT EXISTS ${MSYS2}/usr/bin/diff.exe)
+	dk_command(bash -c "pacman -S diffutils --noconfirm")				# diffutils
+endif()
+
+if(NOT EXISTS ${MSYS2}/usr/bin/yasm.exe)
+	dk_command(bash -c "pacman -S yasm --noconfirm")					# yasm
+endif()
+
+if(NOT EXISTS ${MSYS2}/mingw64/bin/gcc.exe)
+	dk_command(bash -c "pacman -S mingw-w64-x86_64-gcc --noconfirm")	# WIN64 builds
+endif()
+
+if(NOT EXISTS ${MSYS2}/mingw32/bin/gcc.exe)
+	dk_command(bash -c "pacman -S mingw-w64-i686-gcc --noconfirm")		# WIN32 builds
+endif()
+
+dk_command(bash -c "pacman -S mingw-w64-x86_64-autotools --noconfirm")	# used by giflib
