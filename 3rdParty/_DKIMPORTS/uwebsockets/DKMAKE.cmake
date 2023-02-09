7deca5f092a0eeb38390fb7a99f92e020e40685a
aquawicket <aquawicket@hotmail.com>
2023-02-08 20:20:19 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/uwebsockets/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/uwebsockets/DKMAKE.cmake
@@ -9,28 +9,30 @@ dk_depend(zlib)
 
 ### IMPORT ###
 #dk_import(https://github.com/uNetworking/uWebSockets.git BRANCH v0.14 PATCH)
-#dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.14.8.zip PATCH)
-#dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.15a1.zip)
-#dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.15rc1.zip)
-dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v20.36.0.zip)
+dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.14.8.zip PATCH)
+#dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v20.36.0.zip)
 
 
 ### LINK ###
-dk_include		(${UWEBSOCKETS})
-dk_include		(${UWEBSOCKETS}/${OS})
-dk_libDebug		(${UWEBSOCKETS}/${OS}/${DEBUG_DIR}/libuWS.a)
-dk_libRelease	(${UWEBSOCKETS}/${OS}/${RELEASE_DIR}/libuWS.a)
-#WIN_dk_libDebug		(${UWEBSOCKETS}/${OS}/${DEBUG_DIR}/uWS.lib)
-#WIN_dk_libRelease	(${UWEBSOCKETS}/${OS}/${RELEASE_DIR}/uWS.lib)
+dk_include			(${UWEBSOCKETS})
+dk_include			(${UWEBSOCKETS}/${OS})
+UNIX_dk_libDebug	(${UWEBSOCKETS}/${OS}/${DEBUG_DIR}/libuWS.a)
+UNIX_dk_libRelease	(${UWEBSOCKETS}/${OS}/${RELEASE_DIR}/libuWS.a)
+WIN_dk_libDebug		(${UWEBSOCKETS}/${OS}/${DEBUG_DIR}/uWS.lib)
+WIN_dk_libRelease	(${UWEBSOCKETS}/${OS}/${RELEASE_DIR}/uWS.lib)
 
 
 ### GENERATE ###
-#dk_queueCommand(${DKCMAKE_BUILD} 
-#	${LIBUV_CMAKE}
-#	${OPENSSL_CMAKE}
-#	${ZLIB_CMAKE}
-#	${UWEBSOCKETS})
+dk_queueCommand(${DKCMAKE_BUILD} 
+	${LIBUV_CMAKE}
+	${OPENSSL_CMAKE}
+	${ZLIB_CMAKE}
+	${UWEBSOCKETS})
 
+### COMPILE ###
+dk_build(${UWEBSOCKETS} uWS)
+
+###### NON CMAKE #########
 ### GENERATE / COMPILE ###
 #DEBUG_dk_setPath		(${UWEBSOCKETS}/${OS}/${DEBUG_DIR})
 #DEBUG_dk_queueShell	(${DKCONFIGURE_BUILD})
@@ -39,8 +41,7 @@ dk_libRelease	(${UWEBSOCKETS}/${OS}/${RELEASE_DIR}/libuWS.a)
 #RELEASE_dk_queueShell	(${DKCONFIGURE_BUILD})
 #RELEASE_dk_queueShell	(make)
 
-dk_setPath		(${UWEBSOCKETS})
-dk_queueShell	(make)
+#dk_setPath		(${UWEBSOCKETS})
+#dk_queueShell	(make)
+##########################
 
-### COMPILE ###
-dk_build(${UWEBSOCKETS})
