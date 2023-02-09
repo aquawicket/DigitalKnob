9cc439ec50204ec7efdae7ad668b043b591fc95f
aquawicket <aquawicket@hotmail.com>
2023-02-08 20:14:40 -0800

    commit from git

--- DKPlugins/DKDom/DKMAKE.cmake
+++ DKPlugins/DKDom/DKMAKE.cmake
@@ -1,7 +1,7 @@
 dk_depend(DKDuktape)
 dk_depend(DKRml)
-dk_depend(DKWebSocketsClient)
-dk_depend(DKWebSocketsServer)
+dk_depend(DKUWebSocketsClient)
+dk_depend(DKUWebSocketsServer)
 
 dk_generateCmake(DKDom)
 dk_assets(DKDom)
