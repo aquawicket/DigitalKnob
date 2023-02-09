e367ccca491537967ffd10b4a7cc62ef1c0cd3bf
aquawicket <aquawicket@hotmail.com>
2023-02-08 18:35:25 -0800

    commit from git

--- 3rdParty/_DKIMPORTS/uwebsockets/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/uwebsockets/DKMAKE.cmake
@@ -11,7 +11,8 @@ dk_depend(zlib)
 #dk_import(https://github.com/uNetworking/uWebSockets.git BRANCH v0.14 PATCH)
 #dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.14.0.zip PATCH)
 #dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.14.1.zip PATCH)
-dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.14.2.zip PATCH)
+#dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.14.2.zip PATCH)
+dk_import(https://github.com/uNetworking/uWebSockets/archive/refs/tags/v0.14.8.zip PATCH)
 
 
 ### LINK ###
