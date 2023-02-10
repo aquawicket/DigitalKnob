efdc0b7716bf2679d0b3f60a38c9460441132d91
aquawicket <aquawicket@hotmail.com>
2023-02-09 16:37:12 -0800

    commit from git

--- DKApps/DKWebSocketServer/assets/app.js
+++ DKApps/DKWebSocketServer/assets/app.js
@@ -1,4 +1,4 @@
-
+console.log("CPP_DK_GetLocalIP() = "+CPP_DK_GetLocalIP())
 CPP_DK_Create("DKSDLWindow")
 CPP_DK_Create("DKRml")
 location.href = CPP_DKAssets_LocalAssets()+"index.html"
--- DKPlugins/DK/DKWindows.cpp
+++ DKPlugins/DK/DKWindows.cpp
@@ -308,26 +308,165 @@ bool DKWindows::GetLastError(DKString& error){
 	return true;
 }
 
-// https://www.geekpage.jp/en/programming/winsock/getaddrinfo-0.php
+// https://learn.microsoft.com/en-us/windows/win32/api/ws2tcpip/nf-ws2tcpip-getaddrinfo
 bool DKWindows::GetLocalIP(DKString& ip){
 	DKDEBUGFUNC(ip);
 	
-	char *hostname = "localhost";
-	struct addrinfo hints, *res;
-	struct in_addr addr;
-	WSAData data;
-	WSAStartup(MAKEWORD(2,0), &data);
-	memset(&hints, 0, sizeof(hints));
+	int argc = DKApp::argc;
+	char** argv = DKApp::argv;
+
+	//-----------------------------------------
+	// Declare and initialize variables
+	WSADATA wsaData;
+	int iResult;
+	INT iRetval;
+
+	DWORD dwRetval;
+
+	int i = 1;
+
+	struct addrinfo* result = NULL;
+	struct addrinfo* ptr = NULL;
+	struct addrinfo hints;
+
+	struct sockaddr_in* sockaddr_ipv4;
+	//    struct sockaddr_in6 *sockaddr_ipv6;
+	LPSOCKADDR sockaddr_ip;
+
+	char ipstringbuffer[46];
+	DWORD ipbufferlength = 46;
+
+	// Validate the parameters
+	if (argc != 3) {
+		printf("usage: %s <hostname> <servicename>\n", argv[0]);
+		printf("getaddrinfo provides protocol-independent translation\n");
+		printf("   from an ANSI host name to an IP address\n");
+		printf("%s example usage\n", argv[0]);
+		printf("   %s www.contoso.com 0\n", argv[0]);
+		return 1;
+	}
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	//--------------------------------
+	// Setup the hints address info structure
+	// which is passed to the getaddrinfo() function
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_family = AF_INET;
-	int err;
-	if ((err = getaddrinfo(hostname, NULL, &hints, &res)) != 0)
-	  return DKERROR("getaddrinfo() failed!: error "+toString(err)+"\n");
-	addr.S_un = ((struct sockaddr_in *)(res->ai_addr))->sin_addr.S_un;
-	ip = toString(inet_ntoa(addr));
-	DKINFO("ip address = " + ip + "\n");
-	freeaddrinfo(res);
+	hints.ai_protocol = IPPROTO_TCP;
+
+	printf("Calling getaddrinfo with following parameters:\n");
+	printf("\tnodename = %s\n", argv[1]);
+	printf("\tservname (or port) = %s\n\n", argv[2]);
+
+	//--------------------------------
+	// Call getaddrinfo(). If the call succeeds,
+	// the result variable will hold a linked list
+	// of addrinfo structures containing response
+	// information
+	dwRetval = getaddrinfo(argv[1], argv[2], &hints, &result);
+	if (dwRetval != 0) {
+		printf("getaddrinfo failed with error: %d\n", dwRetval);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("getaddrinfo returned success\n");
+
+	// Retrieve each address and print out the hex bytes
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+		printf("getaddrinfo response %d\n", i++);
+		printf("\tFlags: 0x%x\n", ptr->ai_flags);
+		printf("\tFamily: ");
+		switch (ptr->ai_family) {
+		case AF_UNSPEC:
+			printf("Unspecified\n");
+			break;
+		case AF_INET:
+			printf("AF_INET (IPv4)\n");
+			sockaddr_ipv4 = (struct sockaddr_in*)ptr->ai_addr;
+			printf("\tIPv4 address %s\n",
+				inet_ntoa(sockaddr_ipv4->sin_addr));
+			break;
+		case AF_INET6:
+			printf("AF_INET6 (IPv6)\n");
+			// the InetNtop function is available on Windows Vista and later
+			// sockaddr_ipv6 = (struct sockaddr_in6 *) ptr->ai_addr;
+			// printf("\tIPv6 address %s\n",
+			//    InetNtop(AF_INET6, &sockaddr_ipv6->sin6_addr, ipstringbuffer, 46) );
+
+			// We use WSAAddressToString since it is supported on Windows XP and later
+			sockaddr_ip = (LPSOCKADDR)ptr->ai_addr;
+			// The buffer length is changed by each call to WSAAddresstoString
+			// So we need to set it for each iteration through the loop for safety
+			ipbufferlength = 46;
+			iRetval = WSAAddressToString(sockaddr_ip, (DWORD)ptr->ai_addrlen, NULL,
+				ipstringbuffer, &ipbufferlength);
+			if (iRetval)
+				printf("WSAAddressToString failed with %u\n", WSAGetLastError());
+			else
+				printf("\tIPv6 address %s\n", ipstringbuffer);
+			break;
+		case AF_NETBIOS:
+			printf("AF_NETBIOS (NetBIOS)\n");
+			break;
+		default:
+			printf("Other %ld\n", ptr->ai_family);
+			break;
+		}
+		printf("\tSocket type: ");
+		switch (ptr->ai_socktype) {
+		case 0:
+			printf("Unspecified\n");
+			break;
+		case SOCK_STREAM:
+			printf("SOCK_STREAM (stream)\n");
+			break;
+		case SOCK_DGRAM:
+			printf("SOCK_DGRAM (datagram) \n");
+			break;
+		case SOCK_RAW:
+			printf("SOCK_RAW (raw) \n");
+			break;
+		case SOCK_RDM:
+			printf("SOCK_RDM (reliable message datagram)\n");
+			break;
+		case SOCK_SEQPACKET:
+			printf("SOCK_SEQPACKET (pseudo-stream packet)\n");
+			break;
+		default:
+			printf("Other %ld\n", ptr->ai_socktype);
+			break;
+		}
+		printf("\tProtocol: ");
+		switch (ptr->ai_protocol) {
+		case 0:
+			printf("Unspecified\n");
+			break;
+		case IPPROTO_TCP:
+			printf("IPPROTO_TCP (TCP)\n");
+			break;
+		case IPPROTO_UDP:
+			printf("IPPROTO_UDP (UDP) \n");
+			break;
+		default:
+			printf("Other %ld\n", ptr->ai_protocol);
+			break;
+		}
+		printf("\tLength of this sockaddr: %d\n", ptr->ai_addrlen);
+		printf("\tCanonical name: %s\n", ptr->ai_canonname);
+	}
+
+	freeaddrinfo(result);
 	WSACleanup();
+
 	return true;
 }
 
