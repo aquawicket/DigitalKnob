d0d427e76b3a5e49a1a623c8ebe1bd48fa8afdbb
aquawicket <aquawicket@hotmail.com>
2023-02-15 18:33:45 -0500

    commit from git

--- DKPlugins/DKSDLRml/DKMAKE.cmake
+++ DKPlugins/DKSDLRml/DKMAKE.cmake
@@ -2,7 +2,5 @@ dk_depend(DKAssets)
 dk_depend(DKSDLWindow)
 dk_depend(DKRml)
 dk_depend(sdl_image)
-## dk_depend(sdl2_gif) ##Deprecated
-## dk_depend(sdl2_giflib_sa-master) ##FIXME
 
 dk_generateCmake(DKSDLRml)
