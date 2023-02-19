3abeb62210b5fe1c9969a17b69b6efca628d1356
aquawicket <aquawicket@hotmail.com>
2023-02-19 17:27:53 -0500

    commit from git

--- DKPlugins/DKWindow/DKWindow.js
+++ DKPlugins/DKWindow/DKWindow.js
@@ -35,11 +35,11 @@ dk.window.getMouseY = function dk_window_GetMouseY() {
     return mouseY;
 }
 
-dk.window.getPixelRatio function dk_window_getPixelRatio() {
+dk.window.getPixelRatio = function dk_window_getPixelRatio() {
     return window.devicePixelRatio || 1;
 }
 
-dk.window.getWidth function = dk.window_getWidth() {
+dk.window.getWidth = function dk_window_getWidth() {
     var w = window
       , d = document
       , e = d.documentElement
@@ -63,7 +63,7 @@ dk.window.isFullscreen = function dk_window_isFullscreen() {
     return true;
 }
 
-if (DK_GetBrowser() !== "CEF") {
+if (CPP_DK_GetBrowser() !== "CEF") {
 	dk.window.setHeight = function dk_window_setHeight(h) {
         //FIXME - does not work
         window.resizeTo(dk.window.getWidth(), h);
@@ -71,7 +71,7 @@ if (DK_GetBrowser() !== "CEF") {
     }
 }
 
-if (DK_GetBrowser() !== "CEF") {
+if (CPP_DK_GetBrowser() !== "CEF") {
 	dk.window.setWidth = function dk_window_setWidth(w) {
         //FIXME - does not work
         window.resizeTo(w, dk.window.getHeight());
@@ -79,7 +79,7 @@ if (DK_GetBrowser() !== "CEF") {
     }
 }
 
-if (DK_GetBrowser() !== "CEF") {
+if (CPP_DK_GetBrowser() !== "CEF") {
 	dk.window.setX = function dk_window_setX(x) {
         //FIXME - does not work
         window.moveTo(x, dk.window.getY());
@@ -87,7 +87,7 @@ if (DK_GetBrowser() !== "CEF") {
     }
 }
 
-if (DK_GetBrowser() !== "CEF") {
+if (CPP_DK_GetBrowser() !== "CEF") {
 	dk.window.setY = function dk_window_setY(y) {
         //FIXME - does not work
         window.moveTo(dk.window.getX(), y);
