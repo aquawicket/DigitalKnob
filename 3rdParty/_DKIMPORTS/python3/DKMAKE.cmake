6d3d35eea67a734d43e5b9dbfd8d60ac649b461e
aquawicket <aquawicket@hotmail.com>
2023-02-20 19:19:57 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/python3/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/python3/DKMAKE.cmake
@@ -1,4 +1,6 @@
 # https://docs.python.org/3/using/windows.html
+# https://silentinstallhq.com/python-3-10-silent-install-how-to-guide/
 
 
 dk_import(https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe)
+dk_executeProcess(${DKIMPORTS}/python3/install.cmd)
