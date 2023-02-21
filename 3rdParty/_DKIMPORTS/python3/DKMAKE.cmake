62fbe747a65425c058763f864b6f37f6b3496dfd
aquawicket <aquawicket@hotmail.com>
2023-02-20 19:29:37 -0500

    commit from git

--- 3rdParty/_DKIMPORTS/python3/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/python3/DKMAKE.cmake
@@ -2,5 +2,18 @@
 # https://silentinstallhq.com/python-3-10-silent-install-how-to-guide/
 
 
-dk_import(https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe)
-dk_executeProcess(${DKIMPORTS}/python3/install.cmd)
+dk_set			(PYTHON3_FOLDER python-3.11.2)
+dk_set			(PYTHON3 ${3RDPARTY}/${PYTHON3_FOLDER})
+WIN_HOST_dk_set	(PYTHON3_DL https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe)
+#APPLE_dk_set	(PYTHON3_DL TODO)
+WIN_HOST_dk_set	(PYTHON3_APP ${PYTHON3}/python.exe)
+
+
+### INSTALL ###
+if(WIN_HOST)
+	dk_makeDirectory(${PYTHON3})
+	if(NOT EXISTS ${PYTHON3_APP})
+		dk_download(${PYTHON3_DL} ${DKDOWNLOAD}/python-3.11.2-amd64.exe)
+		dk_executeProcess(${DKIMPORTS}/python3/install.cmd)
+	endif()
+endif()
