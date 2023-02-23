9e0ca38d2ea3b02dec5055d24a7b32436f713975
aquawicket <aquawicket@hotmail.com>
2023-02-23 13:49:57 -0500

    commit from git

--- DKApps/DKEvents/assets/Test.js
+++ DKApps/DKEvents/assets/Test.js
@@ -3,14 +3,16 @@ console.log("\n\n ___Event Test___");
 
 /////////// MyObjectA /////////////////////
 console.log("/////////// MyObjectA /////////////////////")
-function myCallbackA(){
-	console.log("myCallbackA()")
-}
+
 const myObjectA = new MyObject('myObjectA')
 console.log("myObjectA = "+myObjectA)
 console.log("myObjectA.value = "+myObjectA.value)
 console.log("myObjectA.test() = "+myObjectA.test())
+console.log("\n")
 
+function myCallbackA(){
+	console.log("myCallbackA()\n")
+}
 myObjectA.addEventListener('myEventA', myCallbackA)
 const myEventA = new Event('myEventA')
 myObjectA.dispatchEvent(myEventA);
@@ -22,24 +24,37 @@ console.log("\n\n")
 
 /////////// MyObjectB /////////////////////
 console.log("/////////// MyObjectB /////////////////////")
-function myCallbackB(){
-	console.log("myCallbackB()")
-}
-function myObjectB_onmessage(){
-	console.log("myObjectB_onmessage()")
-}
+
 const myObjectB = new MyObject('myObjectB')
 console.log("myObjectB = "+myObjectB)
 console.log("myObjectB.value = "+myObjectB.value)
 console.log("myObjectB.test() = "+myObjectB.test())
+console.log("\n")
 
+
+function myCallbackB(){
+	console.log("myCallbackB()\n")
+}
 myObjectB.addEventListener('myEventB', myCallbackB)
 const myEventB = new Event('myEventB')
 myObjectB.dispatchEvent(myEventB);
 myObjectB.removeEventListener('myEventB', myCallbackB)
 myObjectB.dispatchEvent(myEventB);
 
+
+function myObjectB_onmessage(){
+	console.log("myObjectB_onmessage()\n")
+}
 myObjectB.onmessage = myObjectB_onmessage;
 const myEventB_onmessage = new Event('message')
 myObjectB.dispatchEvent(myEventB_onmessage);
+
+
+function myObjectB_ontest(){
+	console.log("myObjectB_ontest()\n")
+}
+myObjectB.ontest = myObjectB_ontest;
+const myEventB_ontest = new Event('test')
+myObjectB.dispatchEvent(myEventB_ontest);
+
 console.log("\n\n")
