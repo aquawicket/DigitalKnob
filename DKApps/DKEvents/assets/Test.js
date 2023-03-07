edc380c719965b05ed0c4bef53ef448e8f634e6d
aquawicket <aquawicket@hotmail.com>
2023-03-07 14:31:57 -0500

    commit from git

--- DKApps/DKEvents/assets/Test.js
+++ DKApps/DKEvents/assets/Test.js
@@ -24,7 +24,7 @@ console.log("/////////// CPPObject /////////////////////")
 CPP_DK_Create("EventTest/CPPObject.js");
 const myCPPObject = new CPPObject('myCPPObject')
 
-function myCPPCallback(){
+function myCPPCallback(event){
 	console.log("myCPPCallback()\n")
 }
 myCPPObject.addEventListener('myCPPEvent', myCPPCallback)
@@ -62,59 +62,59 @@ function onKeyDown(event){
 }
 myConsoleInput.addEventListener('keydown', onKeyDown)
 function onKeyUp(event){
-	console.log("onKeyUp()\n")
+	console.log("onKeyUp() event.key="+event.key)
 }
 myConsoleInput.addEventListener('keyup', onKeyUp)
 function onKeyPress(event){
-	console.log("onKeyPress()\n")
+	console.log("onKeyPress() event.key="+event.key)
 }
 myConsoleInput.addEventListener('keypress', onKeyPress)
 
 // Mouse events
 function onClick(event){
-	console.log("onClick()\n")
+	console.log("onClick() event.button="+event.button)
 }
 myConsoleInput.addEventListener('click', onClick)
 function onContextMenu(event){
-	console.log("onContextMenu()\n")
+	console.log("onContextMenu() event.button="+event.button)
 }
 myConsoleInput.addEventListener('contextmenu', onContextMenu)
 function onDblclick(event){
-	console.log("onDblclick()\n")
+	console.log("onDblclick() event.button="+event.button)
 }
 myConsoleInput.addEventListener('dblclick', onDblclick)
 function onMouseDown(event){
-	console.log("onMouseDown()\n")
+	console.log("onMouseDown() event.button="+event.button)
 }
 myConsoleInput.addEventListener('mousedown', onMouseDown)
 function onMouseEnter(event){
-	console.log("onMouseEnter()\n")
+	console.log("onMouseEnter() event.button="+event.button)
 }
 myConsoleInput.addEventListener('mouseenter', onMouseEnter)
 function onMouseLeave(event){
-	console.log("onMouseLeave()\n")
+	console.log("onMouseLeave() event.button="+event.button)
 }
 myConsoleInput.addEventListener('mouseleave', onMouseLeave)
 function onMouseMove(event){
-	console.log("onMouseMove()\n")
+	console.log("onMouseMove() event.button="+event.button)
 }
 myConsoleInput.addEventListener('mousemove', onMouseMove)
 function onMouseOut(event){
-	console.log("onMouseOut()\n")
+	console.log("onMouseOut() event.button="+event.button)
 }
 myConsoleInput.addEventListener('mouseout', onMouseOut)
 function onMouseOver(event){
-	console.log("onMouseOver()\n")
+	console.log("onMouseOver() event.button="+event.button)
 }
 myConsoleInput.addEventListener('mouseover', onMouseOver)
 function onMouseUp(event){
-	console.log("onMouseUp()\n")
+	console.log("onMouseUp() event.button="+event.button)
 }
 myConsoleInput.addEventListener('mouseup', onMouseUp)
 
 // Resize events
 function onResize(event){
-	console.log("onResize()\n")
+	console.log("onResize() event.x="+event.x)
 }
 myConsoleInput.addEventListener('resize', onResize)
 
@@ -130,58 +130,58 @@ function onKeyDown(event){
 }
 myNcursesInput.addEventListener('keydown', onKeyDown)
 function onKeyUp(event){
-	console.log("onKeyUp()\n")
+	console.log("onKeyUp() event.key="+event.key)
 }
 myNcursesInput.addEventListener('keyup', onKeyUp)
 function onKeyPress(event){
-	console.log("onKeyPress()\n")
+	console.log("onKeyPress() event.key="+event.key)
 }
 myNcursesInput.addEventListener('keypress', onKeyPress)
 
 // Mouse events
 function onClick(event){
-	console.log("onClick()\n")
+	console.log("onClick() event.button="+event.button)
 }
 myNcursesInput.addEventListener('click', onClick)
 function onContextMenu(event){
-	console.log("onContextMenu()\n")
+	console.log("onContextMenu() event.button="+event.button)
 }
 myNcursesInput.addEventListener('contextmenu', onContextMenu)
 function onDblclick(event){
-	console.log("onDblclick()\n")
+	console.log("onDblclick() event.button="+event.button)
 }
 myNcursesInput.addEventListener('dblclick', onDblclick)
 function onMouseDown(event){
-	console.log("onMouseDown()\n")
+	console.log("onMouseDown() event.button="+event.button)
 }
 myNcursesInput.addEventListener('mousedown', onMouseDown)
 function onMouseEnter(event){
-	console.log("onMouseEnter()\n")
+	console.log("onMouseEnter() event.button="+event.button)
 }
 myNcursesInput.addEventListener('mouseenter', onMouseEnter)
 function onMouseLeave(event){
-	console.log("onMouseLeave()\n")
+	console.log("onMouseLeave() event.button="+event.button)
 }
 myNcursesInput.addEventListener('mouseleave', onMouseLeave)
 function onMouseMove(event){
-	console.log("onMouseMove()\n")
+	console.log("onMouseMove() event.button="+event.button)
 }
 myNcursesInput.addEventListener('mousemove', onMouseMove)
 function onMouseOut(event){
-	console.log("onMouseOut()\n")
+	console.log("onMouseOut() event.button="+event.button)
 }
 myNcursesInput.addEventListener('mouseout', onMouseOut)
 function onMouseOver(event){
-	console.log("onMouseOver()\n")
+	console.log("onMouseOver() event.button="+event.button)
 }
 myNcursesInput.addEventListener('mouseover', onMouseOver)
 function onMouseUp(event){
-	console.log("onMouseUp()\n")
+	console.log("onMouseUp() event.button="+event.button)
 }
 myNcursesInput.addEventListener('mouseup', onMouseUp)
 
 // Resize events
 function onResize(event){
-	console.log("onResize()\n")
+	console.log("onResize() event.x="+event.x)
 }
 myNcursesInput.addEventListener('resize', onResize)
