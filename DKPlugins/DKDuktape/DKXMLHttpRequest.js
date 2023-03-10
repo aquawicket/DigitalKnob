61fa54f2462477c26495fd763725a14387c78290
aquawicket <aquawicket@hotmail.com>
2023-03-10 18:42:16 -0500

    commit from git

--- DKPlugins/DKDuktape/DKXMLHttpRequest.js
+++ DKPlugins/DKDuktape/DKXMLHttpRequest.js
@@ -1,6 +1,28 @@
-//https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+// [INTERFACE] https://xhr.spec.whatwg.org/#interface-xmlhttprequest
 
+
+// [XMLHttpRequest()] https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/XMLHttpRequest
 var XMLHttpRequest = function XMLHttpRequest() {
+
+	////// Instance properties //////
+	// [XMLHttpRequest.readyState](Read only)
+	Object.defineProperty(this, "readyState", {
+        get: function readyState() {
+            //TODO return CPP_DKXMLHttpRequest_readyState(_pointer)
+        }
+    })
+	// [XMLHttpRequest.response](Read only)
+	// [XMLHttpRequest.responseText](Read only)
+	// [XMLHttpRequest.responseType]
+	// [XMLHttpRequest.responseURL](Read only)
+	// [XMLHttpRequest.responseXML](Read only)
+	// [XMLHttpRequest.status](Read only)
+	// [XMLHttpRequest.statusText](Read only)
+	// [XMLHttpRequest.timeout]
+	// [XMLHttpRequest.upload](Read only)
+	// [XMLHttpRequest.withCredentials]
+
 
     XMLHttpRequest.prototype.open = function open(method, url, async, user, password) {
         console.warn("XMLHttpRequest.open(" + method + "," + url + "," + async + ")");
