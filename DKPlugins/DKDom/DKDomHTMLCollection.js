788a6ca91d1c99cb00f0f206493474d742c25d0b
aquawicket <aquawicket@hotmail.com>
2023-03-16 01:28:13 -0400

    commit from git

--- DKPlugins/DKDom/DKDomHTMLCollection.js
+++ DKPlugins/DKDom/DKDomHTMLCollection.js
@@ -1,27 +1,39 @@
-//https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
+// [INTERFACE] https://dom.spec.whatwg.org/#interface-htmlcollection
 
-// https://dom.spec.whatwg.org/#interface-htmlcollection
 var HTMLCollection = function HTMLCollection(pointers) {
     var arry = pointers.split(",");
     for (var i = 0; i < arry.length; i++) {
         this.push(new HTMLElement(arry[i]));
     }
 
+	////// Instance properties //////
+	// [HTMLCollection.length](Read only) https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection/length
+    Object.defineProperty(this, "length", {
+        get: function length() {
+            return arry.length
+        }
+    })
+	
+	////// Methods //////
+	// [HTMLCollection.item()] https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection/item
     HTMLCollection.prototype.item = function item(index) {
         return this[index];
     }
-    HTMLCollection.prototype.namedItem = function namedItem(name) {
+	// [HTMLCollection.namedItem()] https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection/namedItem
+    HTMLCollection.prototype.namedItem = function namedItem(key) {
         for (var i = 0; i < this.length; i++) {
-            if (this.id && this.id === name) {
+            if (this.id && this.id === key) {
                 return this[i];
             }
-            if (this.name && this.name === name) {
+            if (this.name && this.name === key) {
                 return this[i];
             }
         }
         return null;
     }
 
+	////// toString //////
 	if(this.toString() === "[object Object]"){
 		this.toString = function(){
 			return "[object HTMLCollection]"
