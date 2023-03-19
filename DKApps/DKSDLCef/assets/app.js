6d26ca22ba25d091a998b40f642cba498af2850a
aquawicket <aquawicket@hotmail.com>
2023-03-19 12:59:08 -0400

    commit from git

--- DKApps/DKSDLCef/assets/app.js
+++ DKApps/DKSDLCef/assets/app.js
@@ -1,6 +1,6 @@
 var USE_CEF = 1
 //var url = "chrome://gpu";
-var url = "https://pluto.tv";
+var url = "https://google.com";
 
 CPP_DK_Create("DKSDLWindow")
 CPP_DK_Create("DKSDLText")
@@ -9,21 +9,12 @@ var width = CPP_DKWindow_GetWidth()
 var height = CPP_DKWindow_GetHeight()
 CPP_DKCef_NewBrowser("CefSDL", 0, 0, width, height, url)
 
-//var currentBrowser = CPP_DKCef_GetCurrentBrowser("CefSDL")
-//CPP_DKCef_SetUrl(0, url, currentBrowser)
-//CPP_DKCef_SetFocus("CefSDL")
 
-//CPP_DK_Create("DKDebug/DKDebug.js")
-
-
-window.addEventListener("resize", User_OnEvent)
-
-function User_OnEvent(event){
-	if(event.type == "resize"){
-		console.log("User_OnEvent(): resize\n")
-		//CPP_DKCef_SetSize("CefSDL", 100, 100)
-		var width = CPP_DKWindow_GetWidth()
-		var height = CPP_DKWindow_GetHeight()
-		DK_CallFunc("CefSDL::OnResize", "0,0,"+String(width)+","+String(height))
-	}
+function OnResize(event){
+	console.log("onresize\n")
+	//CPP_DKCef_SetSize("CefSDL", 100, 100)
+	var width = CPP_DKWindow_GetWidth()
+	var height = CPP_DKWindow_GetHeight()
+	DK_CallFunc("CefSDL::OnResize", "0,0,"+String(width)+","+String(height))
 }
+window.addEventListener("resize", OnResize)
