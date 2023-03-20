558ae5d1b0f781ba309e9be9abf72beb1bc2eccb
aquawicket <aquawicket@hotmail.com>
2023-03-20 16:45:26 -0400

    commit from git

--- DKApps/CPPEventsTest/App.h
+++ DKApps/CPPEventsTest/App.h
@@ -10,6 +10,11 @@ class App : public DKObjectT<App>
 public:
 	bool Init();
 	bool End();
+
+	////// KeyboardEvents //////
+	void printKeyboardEventProperties(/*event*/);
+	void onKeyDown(/*event*/);
+	void onKeyUp(/*event*/);
 };
 REGISTER_OBJECT(App, false);
 
