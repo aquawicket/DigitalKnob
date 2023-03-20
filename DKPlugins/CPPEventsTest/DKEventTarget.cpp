2e8f9ddf4e87fbfca7470cf785a5711219ed1ecc
aquawicket <aquawicket@hotmail.com>
2023-03-20 18:15:26 -0400

    commit from git

--- DKPlugins/CPPEventsTest/DKEventTarget.cpp
+++ DKPlugins/CPPEventsTest/DKEventTarget.cpp
@@ -1,1 +1,15 @@
-// DKEventTarget.cpp
+#include "DK/stdafx.h"
+#include "CPPEventsTest/DKEventTarget.h"
+
+
+bool DKEventTarget::Init(){
+	DKDEBUGFUNC();
+	DKINFO("DKEventTarget::Init() \n");
+    return true;
+}
+
+bool DKEventTarget::End(){
+	DKDEBUGFUNC();
+	DKINFO("DKEventTarget::End() \n");
+	return true;
+}
--- DKPlugins/CPPEventsTest/DKEventTarget.h
+++ DKPlugins/CPPEventsTest/DKEventTarget.h
@@ -1,1 +1,17 @@
-// DKEventTarget.h
+#pragma once
+#ifndef DKEventTarget_H
+#define DKEventTarget_H
+
+#include "DK/DK.h"
+
+
+class DKEventTarget : public DKObjectT<DKEventTarget>
+{
+public:
+	bool Init();
+	bool End();
+};
+REGISTER_OBJECT(DKEventTarget, false);
+
+
+#endif //DKEventTarget_H
