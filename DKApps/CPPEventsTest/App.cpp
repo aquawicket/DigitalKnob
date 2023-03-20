524a714e91646baf29e384969adc6ebeb2f2968b
aquawicket <aquawicket@hotmail.com>
2023-03-20 15:50:59 -0400

    commit from git

---
+++ DKApps/CPPEventsTest/App.cpp
@@ -0,0 +1,14 @@
+#include "DK/stdafx.h"
+
+#include "App/App.h"
+
+
+bool App::Init(){
+	DKDEBUGFUNC();	
+	return true;
+}
+
+bool App::End(){
+	DKDEBUGFUNC();
+	return true;
+}
---
+++ DKApps/CPPEventsTest/App.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef App_H
+#define App_H
+
+#include "DK/DK.h"
+
+
+class App : public DKObjectT<App>
+{
+public:
+	bool Init();
+	bool End();
+};
+REGISTER_OBJECT(App, false);
+
+
+#endif //App_H
