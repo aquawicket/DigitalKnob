5c82f7add8cab2effa06976d8de75a2ebebd9fb3
aquawicket <aquawicket@hotmail.com>
2023-03-28 21:55:44 -0400

    commit from git

--- DKApps/JSEventsTest/assets/app.js
+++ DKApps/JSEventsTest/assets/app.js
@@ -1,6 +1,6 @@
 CPP_DK_Create("DKConsole");
 CPP_DK_Create("JSEventsTest/DKEvent.js");
-CPP_DK_Create("DKEventTarget");
+CPP_DK_Create("JSEventsTest/DKEventTarget.js");
 CPP_DK_Create("JSEventsTest/DKGlobalEventHandlers.js");
 CPP_DK_Create("DKUIEvent");
 CPP_DK_Create("DKKeyboardEvent");
