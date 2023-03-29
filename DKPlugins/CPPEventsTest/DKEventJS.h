e03320118ff6d2e7bde61d8c67cf89a6812a2cb5
aquawicket <aquawicket@hotmail.com>
2023-03-29 15:40:20 -0400

    commit from git

--- DKPlugins/CPPEventsTest/DKEventJS.h
+++ DKPlugins/CPPEventsTest/DKEventJS.h
@@ -3,26 +3,38 @@
 #define DKEventJS_H
 
 #include "DKDuktape/DKDuktape.h"
+#include "CPPEventsTest/DKEventTarget.h"
 
 
 class DKEventJS : public DKObjectT<DKEventJS>
 {
 public:
 	bool Init(){
-		DKDuktape::AttachFunction("CPP_DKEvent_bubbles",			DKEventJS::bubbles);
-		DKDuktape::AttachFunction("CPP_DKEvent_cancelable",			DKEventJS::cancelable);
-		DKDuktape::AttachFunction("CPP_DKEvent_composed",			DKEventJS::composed);
-		DKDuktape::AttachFunction("CPP_DKEvent_currentTarget",		DKEventJS::currentTarget);
-		DKDuktape::AttachFunction("CPP_DKEvent_defaultPrevented",	DKEventJS::defaultPrevented);
-		DKDuktape::AttachFunction("CPP_DKEvent_eventPhase",			DKEventJS::eventPhase);
-		DKDuktape::AttachFunction("CPP_DKEvent_isTrusted",			DKEventJS::isTrusted);
-		DKDuktape::AttachFunction("CPP_DKEvent_target",				DKEventJS::target);
-		DKDuktape::AttachFunction("CPP_DKEvent_timeStamp",			DKEventJS::timeStamp);
-		DKDuktape::AttachFunction("CPP_DKEvent_type",				DKEventJS::type);
+		DKDuktape::AttachFunction("CPP_DKEvent_bubbles", DKEventJS::bubbles);
+		DKDuktape::AttachFunction("CPP_DKEvent_cancelable", DKEventJS::cancelable);
+		DKDuktape::AttachFunction("CPP_DKEvent_composed", DKEventJS::composed);
+		DKDuktape::AttachFunction("CPP_DKEvent_currentTarget", DKEventJS::currentTarget);
+		DKDuktape::AttachFunction("CPP_DKEvent_defaultPrevented", DKEventJS::defaultPrevented);
+		DKDuktape::AttachFunction("CPP_DKEvent_eventPhase", DKEventJS::eventPhase);
+		DKDuktape::AttachFunction("CPP_DKEvent_isTrusted", DKEventJS::isTrusted);
+		DKDuktape::AttachFunction("CPP_DKEvent_target", DKEventJS::target);
+		DKDuktape::AttachFunction("CPP_DKEvent_timeStamp", DKEventJS::timeStamp);
+		DKDuktape::AttachFunction("CPP_DKEvent_type", DKEventJS::type);
 		return true;
 	}
 	
+	//template <typename EventType>
 	static int bubbles(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKString pointer = duk_require_string(ctx, 0);
+		
+		/*
+		for (auto& eventObj : DKEventTarget::events<EventType>) {
+			if (eventObj.pointer == pointer)
+				DKINFO("match");
+		}
+		*/
+
 		return DKTODO();
 	}
 	static int cancelable(duk_context* ctx){
