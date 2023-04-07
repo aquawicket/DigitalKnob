8a73b589cc89bdbab2e515f64e39b602ad6d7982
aquawicket <aquawicket@hotmail.com>
2023-04-06 20:49:35 -0400

    commit from git

--- DKApps/DKEventTest/assets/app.js
+++ DKApps/DKEventTest/assets/app.js
@@ -1,10 +1,18 @@
-
+// https://console.spec.whatwg.org/
 CPP_DK_Create("DKConsole");
+
+// https://dom.spec.whatwg.org/
 CPP_DK_Create("DKEventJS");
+CPP_DK_Create("DKCustomEventJS");
+CPP_DK_Create("DKEventTargetJS");
+
+// https://w3c.github.io/uievents/
 CPP_DK_Create("DKUIEventJS");
-CPP_DK_Create("DKKeyboardEventJS");
+CPP_DK_Create("DKFocusEventJS");
 CPP_DK_Create("DKMouseEventJS");
 CPP_DK_Create("DKWheelEventJS");
-CPP_DK_Create("DKFocusEventJS");
-CPP_DK_Create("DKEventTargetJS");
+CPP_DK_Create("DKInputEventJS");
+CPP_DK_Create("DKKeyboardEventJS");
+//CPP_DK_Create("CompositionEventJS");
+
 CPP_DK_Create("Test.js");
