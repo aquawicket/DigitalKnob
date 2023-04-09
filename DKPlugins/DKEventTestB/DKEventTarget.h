93abf4f748e7657333019a3249408fab8fbfe691
aquawicket <aquawicket@hotmail.com>
2023-04-09 10:16:35 -0400

    commit from git

--- DKPlugins/DKEventTestB/DKEventTarget.h
+++ DKPlugins/DKEventTestB/DKEventTarget.h
@@ -6,16 +6,6 @@
 #include "DKEventTestB/DKEvent.h"
 #include "DKDuktape/DKDuktape.h"
 
-/*
-typedef std::function<bool(const DKString&, const DKString&)> AddEventListenerFunc;
-typedef std::map<DKString, AddEventListenerFunc> AddEventListenerMap;
-
-typedef std::function<bool(const DKString&, const DKString&)> RemoveEventListenerFunc;
-typedef std::map<DKString, RemoveEventListenerFunc> RemoveEventListenerMap;
-
-typedef std::function<bool(const DKString&, const DKString&)> DispatchEventFunc;
-typedef std::map<DKString, DispatchEventFunc> DispatchEventMap;
-*/
 
 struct EventObject {
     DKString type;
@@ -25,7 +15,7 @@ struct EventObject {
 
 
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
-class DKEventTarget : public DKObjectT<DKEventTarget>
+class DKEventTarget// : public DKObjectT<DKEventTarget>
 {
 public:
 
@@ -69,7 +59,7 @@ public:
 	void removeEventListener(const DKString& type, std::function<void(DKEvent*)> listener){
 		DKString eventTargetAddress = DKDuktape::pointerToAddress(this);
 		DKDEBUGFUNC(type, listener, eventTargetAddress);
-		//DKINFO("DKEventTarget::removeEventListener("+type+", listener, "+eventTargetAddress+") \n");
+		DKINFO("DKEventTarget::removeEventListener("+type+", listener, "+eventTargetAddress+") \n");
 		for(auto it = events.begin(); it != events.end();){
 			if(it->type == type && it->eventTargetAddress == eventTargetAddress) // && it->listener == listener)
 				it = events.erase(it);
@@ -97,83 +87,8 @@ public:
 	
 	////// DK properties //////	
 	static std::vector<EventObject> events;
-	
-	/*
-	static AddEventListenerMap* addEventListenerMap;
-	static RemoveEventListenerMap* removeEventListenerMap;
-	static DispatchEventMap* dispatchEventMap;
-	
-	template<class T>
-	static bool LinkAddEventListenerFunc(const DKString& name, bool (T::*func) (const DKString&, const DKString&), T* _this){
-		DKDEBUGFUNC(name);//, func, _this);
-		//DKINFO("DKEventTarget::LinkAddEventListenerFunc("+name+", func(DKString,DKString)) \n");
-		if(!addEventListenerMap)
-			addEventListenerMap = new AddEventListenerMap();
-		(*addEventListenerMap)[name] = std::bind(func, _this, std::placeholders::_1, std::placeholders::_2);
-		return true;
-	}
-	
-	static bool CallAddEventListenerFunc(const DKString& type, const DKString& eventTargetAddress){
-		DKDEBUGFUNC(type, eventTargetAddress);
-		//DKINFO("DKEventTarget::CallAddEventListenerFunc("+type+", "+eventTargetAddress+") \n");
-		if(!addEventListenerMap)
-			return DKERROR("addEventListenerMap invalid! \n");
-		if(addEventListenerMap->empty())
-			return DKERROR("addEventListenerMap empty! \n");
-		if(addEventListenerMap->find(type) == addEventListenerMap->end())
-			return DKERROR(type+" not registered to a function! \n");
-		return (*addEventListenerMap)[type](type, eventTargetAddress);
-	}
-	
-	template<class T>
-	static bool LinkRemoveEventListenerFunc(const DKString& name, bool (T::*func) (const DKString&, const DKString&), T* _this){
-		DKDEBUGFUNC(name);//, func, _this);
-		//DKINFO("DKEventTarget::LinkRemoveEventListenerFunc("+name+", func(DKString,DKString)) \n");
-		if(!removeEventListenerMap)
-			removeEventListenerMap = new RemoveEventListenerMap();
-		(*removeEventListenerMap)[name] = std::bind(func, _this, std::placeholders::_1, std::placeholders::_2);
-		return true;
-	}
-	
-	static bool CallRemoveEventListenerFunc(const DKString& type, const DKString& eventTargetAddress){
-		DKDEBUGFUNC(type, eventTargetAddress);
-		//DKINFO("DKEventTarget::CallRemoveEventListenerFunc("+type+", "+eventTargetAddress+") \n");
-		if(!removeEventListenerMap)
-			return DKERROR("removeEventListenerMap invalid! \n");
-		if(removeEventListenerMap->empty())
-			return DKERROR("removeEventListenerMap empty! \n");
-		if(removeEventListenerMap->find(type) == removeEventListenerMap->end())
-			return DKERROR(type+" not registered to a function! \n");
-		return (*removeEventListenerMap)[type](type, eventTargetAddress);
-	}
-	
-	template<class T>
-	static bool LinkDispatchEventFunc(const DKString& name, bool (T::*func) (const DKString&, const DKString&), T* _this){
-		DKDEBUGFUNC(name);//, func, _this);
-		//DKINFO("DKEventTarget::LinkDispatchEventFunc("+name+", func(DKString,DKString)) \n");
-		if(!dispatchEventMap)
-			dispatchEventMap = new DispatchEventMap();
-		(*dispatchEventMap)[name] = std::bind(func, _this, std::placeholders::_1, std::placeholders::_2);
-		return true;
-	}
-	static bool CallDispatchEventFunc(const DKString& eventAddress, const DKString& eventTargetAddress){
-		DKDEBUGFUNC(eventAddress, eventTargetAddress);
-		//DKINFO("DKEventTarget::CallDispatchEventFunc("+eventAddress+", "+eventTargetAddress+") \n");
-		if(!dispatchEventMap)
-			return DKERROR("dispatchEventMap invalid! \n");
-		if(dispatchEventMap->empty())
-			return DKERROR("dispatchEventMap empty! \n");
-		if(dispatchEventMap->find(eventAddress) == dispatchEventMap->end())
-			return DKERROR(eventAddress+" not registered to a function! \n");
-		return (*dispatchEventMap)[eventAddress](eventAddress, eventTargetAddress);
-	}
-	*/
+
 };
-
-
-
-
-REGISTER_OBJECT(DKEventTarget, true);
 
 
 #endif //DKEventTarget_H
