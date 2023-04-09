038ef228ad2711169137de273c138ccfa491b821
aquawicket <aquawicket@hotmail.com>
2023-04-09 10:21:11 -0400

    commit from git

--- DKApps/DKEventTestB/assets/Test.js
+++ DKApps/DKEventTestB/assets/Test.js
@@ -1,35 +1,55 @@
 // create EventTarget
 const obj = new EventTarget();
 
-////// Event //////
+////// Event ///////
+function printEventProperties(event){
+	////// Instance properties //////
+	console.log("event.bubbles = "					+event.bubbles)
+	console.log("event.cancelable = "				+event.cancelable)
+	console.log("event.composed = "					+event.composed)
+	console.log("event.currentTarget = "			+event.currentTarget)
+	console.log("event.defaultPrevented = "			+event.defaultPrevented)
+	console.log("event.eventPhase = "				+event.eventPhase)			
+	console.log("event.isTrusted = "				+event.isTrusted)		
+	console.log("event.target = "					+event.target)			
+	console.log("event.timeStamp = "				+event.timeStamp)
+	console.log("event.type = "						+event.type)
+	////// Legacy and non-standard properties //////
+	console.log("event.cancelBubble = "				+event.cancelBubble)
+	console.log("event.explicitOriginalTarget = "	+event.explicitOriginalTarget)
+	console.log("event.originalTarget = "			+event.originalTarget)
+	console.log("event.returnValue = "				+event.returnValue)
+	console.log("event.scoped = "					+event.scoped)
+}
+
 console.log('\n');
 const myEventA = new Event('myeventA');
-obj.addEventListener('myeventA', function(e){
+obj.addEventListener('myeventA', function(event){
 	console.log('onmyeventA')
-	console.log('e = '+e)
-	console.log('e.type = '+e.type)
-	console.log('e.target = '+e.target)
+	printEventProperties(event)
 })
 obj.dispatchEvent(myEventA);
 
 console.log('\n');
-obj.addEventListener('myeventB', function(e){
+obj.addEventListener('myeventB', function(event){
 	console.log('onmyeventB')
-	console.log('e = '+e)
-	console.log('e.type = '+e.type)
-	console.log('e.target = '+e.target)
+	printEventProperties(event)
 })
 obj.dispatchEvent(new Event('myeventB'));
 
 
+
 ////// CustomEvent //////
+function printCustomEventProperties(customevent){
+	////// Instance properties //////
+	console.log("customevent.detail = "	+customevent.detail)
+}
+
 console.log('\n');
 const myCustomEvent = new CustomEvent('customevent');
-obj.addEventListener('customevent', function(e){
+obj.addEventListener('customevent', function(event){
 	console.log('oncustomevent')
-	console.log('e = '+e)
-	console.log('e.type = '+e.type)
-	console.log('e.target = '+e.target)
-	console.log('e.detail = '+e.detail)
+	printEventProperties(event)
+	printCustomEventProperties(event)
 })
 obj.dispatchEvent(myCustomEvent);
