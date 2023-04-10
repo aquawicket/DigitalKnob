d7c7e324aa259d3572a933a37524c6b0efdba663
aquawicket <aquawicket@hotmail.com>
2023-04-10 17:05:30 -0400

    commit from git

--- DKPlugins/DKEventTargetTest/DKWindow.h
+++ DKPlugins/DKEventTargetTest/DKWindow.h
@@ -15,6 +15,58 @@ public:
 		//eventTargetClass = "Window";
 		
 		////// Instance properties //////
+		caches = "";
+		clientInformation = "";
+		closed = false;
+		credentialless = false;
+		crypto = false;
+		customElements = "";
+		document = "";
+		frameElement = "";
+		frames = "";
+		fullScreen = false;
+		history = "";
+		indexedDB = "";
+		innerHeight = 0;
+		innerWidth = 0;
+		isSecureContext = false;
+		launchQueue = "";
+		length = 0;
+		location = "";
+		locationbar = "";
+		localStorage = "";
+		mozInnerScreenX = 0.0;
+		mozInnerScreenY = 0.0;
+		name = "";
+		navigation = "";
+		navigator = "";
+		opener = "";
+		origin = "";
+		outerHeight = 0;
+		outerWidth = 0;
+		pageXOffset = 0;
+		pageYOffset = 0;
+		parent = "";
+		performance = "";
+		personalbar = "";
+		scheduler = "";
+		screen = "";
+		screenX = 0;
+		screenY = 0;
+		scrollbars = "";
+		scrollMaxX = 0;
+		scrollMaxY = 0;
+		scrollX = 0;
+		scrollY = 0;
+		self = "";
+		sessionStorage = "";
+		speechSynthesis = "";
+		statusbar = "";
+		toolbar = "";
+		top = "";
+		visualViewport = "";
+		window = "";
+		//windows = "";
 		
 		
 		////// Deprecated properties //////
@@ -26,60 +78,115 @@ public:
 	
 	////// Instance properties //////
 	// [Window.caches](Read only) https://developer.mozilla.org/en-US/docs/Web/API/caches
+	DKString caches; 
 	// [Window.clientInformation](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator
+	DKString clientInformation;
 	// [Window.closed](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/closed
+	bool closed;
 	// [Window.console](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/console
+	DKString console;
 	// [Window.credentialless](Read only)(Experimental)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/credentialless
+	bool credentialless;
 	// [Window.crypto](Read only) https://developer.mozilla.org/en-US/docs/Web/API/crypto_property
+	DKString crypto;
 	// [Window.customElements](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/customElements
+	DKString customElements;
 	// [Window.devicePixelRatio](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio
+	float devicePixelRatio;
 	// [Window.document](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/document
+	DKString document;
 	// [Window.frameElement](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/frameElement
+	DKString frameElement;
 	// [Window.frames](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/frames
+	DKString frames;
 	// [Window.fullScreen](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/fullScreen
+	bool fullScreen;
 	// [Window.history](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/History
+	DKString history;
 	// [Window.indexedDB](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/indexedDB
+	DKString indexedDB;
 	// [Window.innerHeight](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/innerHeight
+	unsigned int innerHeight;
 	// [Window.innerWidth](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth
-	// [Window.isSecureContext](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/isSecureContext
+	unsigned int innerWidth;
+	// [Window.isSecureContext](Read only) https://developer.mozilla.org/en-US/docs/Web/API/isSecureContext
+	bool isSecureContext;
 	// [Window.launchQueue](Read only)(Experimental) https://developer.mozilla.org/en-US/docs/Web/API/Window/launchQueue
+	DKString launchQueue;
 	// [Window.length](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/length
+	unsigned int length;
 	// [Window.location] https://developer.mozilla.org/en-US/docs/Web/API/Window/location
+	DKString location;
 	// [Window.locationbar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/locationbar
+	DKString locationbar;
 	// [Window.localStorage](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+	DKString localStorage;
 	// [Window.menubar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/menubar
+	DKString menubar;
 	// [Window.mozInnerScreenX](Read only)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/mozInnerScreenX
+	float mozInnerScreenX;
 	// [Window.mozInnerScreenY](Read only)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/mozInnerScreenY
+	float mozInnerScreenY;
 	// [Window.name] https://developer.mozilla.org/en-US/docs/Web/API/Window/name
+	DKString name;
 	// [Window.navigation](Read only)(Experimental) https://developer.mozilla.org/en-US/docs/Web/API/Window/navigation
+	DKString navigation;
 	// [Window.navigator](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator
+	DKString navigator;
 	// [Window.opener] https://developer.mozilla.org/en-US/docs/Web/API/Window/opener
-	// [Window.origin](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/origin
+	DKString opener;
+	// [Window.origin](Read only) https://developer.mozilla.org/en-US/docs/Web/API/origin
+	DKString origin;
 	// [Window.outerHeight](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/outerHeight
+	unsigned int outerHeight;
 	// [Window.outerWidth](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/outerWidth
+	unsigned int outerWidth;
 	// [Window.pageXOffset](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/pageXOffset
+	unsigned int pageXOffset;
 	// [Window.pageYOffset](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
+	unsigned int pageYOffset;
 	// [Window.parent](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/parent
+	DKString parent;
 	// [Window.performance](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/performance
+	DKString performance;
 	// [Window.personalbar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/personalbar
+	DKString personalbar;
 	// [Window.scheduler](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scheduler
+	DKString scheduler;
 	// [Window.screen](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/screen
+	DKString screen;
 	// [Window.screenX and Window.screenLeft](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/screenX
+	int screenX;
 	// [Window.screenY and Window.screenTop](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/screenY
+	int screenY;
 	// [Window.scrollbars](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollbars
+	DKString scrollbars;
 	// [Window.scrollMaxX](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollMaxX
+	unsigned int scrollMaxX;
 	// [Window.scrollMaxY](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollMaxY
+	unsigned int scrollMaxY;
 	// [Window.scrollX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollX
-	// [Window.scrollY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/screenY
+	unsigned int scrollX;
+	// [Window.scrollY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
+	unsigned int scrollY;
 	// [Window.self](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/self
+	DKString self;
 	// [Window.sessionStorage] https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
+	DKString sessionStorage;
 	// [Window.speechSynthesis](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/speechSynthesis
+	DKString speechSynthesis;
 	// [Window.statusbar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/statusbar
+	DKString statusbar;
 	// [Window.toolbar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/toolbar
+	DKString toolbar;
 	// [Window.top](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/top
+	DKString top;
 	// [Window.visualViewport](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/visualViewport
+	DKString visualViewport;
 	// [Window.window](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/window
+	DKString window;
 	// [window[0], window[1], etc.] https://developer.mozilla.org/en-US/docs/Web/API/Window
+	DKStringArray windows;
 
 	
 	////// Deprecated properties //////
@@ -236,7 +343,6 @@ public:
 	// [vrdisplaydisconnect](Deprecated)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/vrdisplaydisconnect_event
 	// [vrdisplaydeactivate](Deprecated)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/vrdisplaydeactivate_event
 	// [vrdisplaypresentchange](Deprecated)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/vrdisplaypresentchange_event
-
 };
 
 
