d9bf469eb179534a66b346b92be2a4dab1e3264c
aquawicket <aquawicket@hotmail.com>
2023-04-09 22:59:35 -0400

    commit from git

--- DKPlugins/DKEventTest/DKPointerEventJS.h
+++ DKPlugins/DKEventTest/DKPointerEventJS.h
@@ -21,14 +21,17 @@ public:
 	
 	
 		////// Instance properties //////
-		DKDuktape::AttachFunction("CPP_DKPointerEvent_pointerId", 	DKPointerEventJS::pointerId);
-		DKDuktape::AttachFunction("CPP_DKPointerEvent_width", 		DKPointerEventJS::width);
-		DKDuktape::AttachFunction("CPP_DKPointerEvent_height", 		DKPointerEventJS::height);
-		DKDuktape::AttachFunction("CPP_DKPointerEvent_pressure", 	DKPointerEventJS::pressure);
-	
-	
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_pointerId", 			DKPointerEventJS::pointerId);
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_width", 				DKPointerEventJS::width);
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_height", 				DKPointerEventJS::height);
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_pressure", 			DKPointerEventJS::pressure);
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_tangentialPressure", 	DKPointerEventJS::tangentialPressure);
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_tiltX", 				DKPointerEventJS::tiltX);
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_tiltY", 				DKPointerEventJS::tiltY);
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_twist", 				DKPointerEventJS::twist);
+		DKDuktape::AttachFunction("CPP_DKPointerEvent_pointerType", 		DKPointerEventJS::pointerType);
+		
 		////// Events //////
-
 
 
 		////// Load .js files //////
@@ -94,20 +97,77 @@ public:
 		return true;
 	}
 	// [PointerEvent.tangentialPressure) https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/tangentialPressure
+	static int tangentialPressure(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKString eventAddress = duk_require_string(ctx, 0);
+		DKPointerEvent* event = (DKPointerEvent*)DKDuktape::addressToPointer(eventAddress);
+		if (duk_is_number(ctx, 1))
+			event->tangentialPressure = duk_to_number(ctx, 1);
+		duk_push_number(ctx, event->tangentialPressure);	
+		return true;
+	}
 	// [PointerEvent.tiltX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/tiltX
+	static int tiltX(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKString eventAddress = duk_require_string(ctx, 0);
+		DKPointerEvent* event = (DKPointerEvent*)DKDuktape::addressToPointer(eventAddress);
+		if (duk_is_number(ctx, 1))
+			event->tiltX = duk_to_int(ctx, 1);
+		duk_push_int(ctx, event->tiltX);	
+		return true;
+	}
 	// [PointerEvent.tiltY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/tiltY
+	static int tiltY(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKString eventAddress = duk_require_string(ctx, 0);
+		DKPointerEvent* event = (DKPointerEvent*)DKDuktape::addressToPointer(eventAddress);
+		if (duk_is_number(ctx, 1))
+			event->tiltY = duk_to_int(ctx, 1);
+		duk_push_int(ctx, event->tiltY);	
+		return true;
+	}
 	// [PointerEvent.twist](Read only) https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/twist
+	static int twist(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKString eventAddress = duk_require_string(ctx, 0);
+		DKPointerEvent* event = (DKPointerEvent*)DKDuktape::addressToPointer(eventAddress);
+		if (duk_is_number(ctx, 1))
+			event->twist = duk_to_uint(ctx, 1);
+		duk_push_uint(ctx, event->twist);	
+		return true;
+	}
 	// [PointerEvent.pointerType](Read only) https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/pointerType
+	static int pointerType(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKString eventAddress = duk_require_string(ctx, 0);
+		DKPointerEvent* event = (DKPointerEvent*)DKDuktape::addressToPointer(eventAddress);
+		if (duk_is_string(ctx, 1))
+			event->pointerType = duk_to_string(ctx, 1);
+		duk_push_uint(ctx, event->pointerType.c_str());	
+		return true;
+	}
 	// [PointerEvent.isPrimary](Read only) https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/isPrimary
+	static int isPrimary(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKString eventAddress = duk_require_string(ctx, 0);
+		DKPointerEvent* event = (DKPointerEvent*)DKDuktape::addressToPointer(eventAddress);
+		if (duk_is_boolean(ctx, 1))
+			event->isPrimary = duk_to_boolean(ctx, 1);
+		duk_push_boolean(ctx, event->isPrimary);	
+		return true;
+	}
 	
 	////// Instance methods //////
 	// [PointerEvent.getCoalescedEvents()] https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent/getCoalescedEvents
-	/*
-	void getCoalescedEvents() {
-		DKTODO();
+	static int getCoalescedEvents(duk_context* ctx){
+		DKDEBUGFUNC(ctx);	
+		return DKTODO();
 	}
-	*/
 	// [PointerEvent.getPredictedEvents()] https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent
+	static int getPredictedEvents(duk_context* ctx){
+		DKDEBUGFUNC(ctx);	
+		return DKTODO();
+	}
 	
 };
 REGISTER_OBJECT(DKPointerEventJS, true)
