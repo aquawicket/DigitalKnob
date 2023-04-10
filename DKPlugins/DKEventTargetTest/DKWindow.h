b49f7834d0ae9d1b82e28ae7fa2ad128be2a4b25
aquawicket <aquawicket@hotmail.com>
2023-04-10 15:36:58 -0400

    commit from git

--- DKPlugins/DKEventTargetTest/DKWindow.h
+++ DKPlugins/DKEventTargetTest/DKWindow.h
@@ -15,9 +15,79 @@ public:
 		//eventTargetClass = "Window";
 		
 		////// Instance properties //////
+		
+		
+		////// Deprecated properties //////
 
 	}
+	
+	////// Instance properties //////
+	// [Window.caches](Read only) https://developer.mozilla.org/en-US/docs/Web/API/caches
+	// [Window.clientInformation](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator
+	// [Window.closed](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/closed
+	// [Window.console](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/console
+	// [Window.credentialless](Read only)(Experimental)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/credentialless
+	// [Window.crypto](Read only) https://developer.mozilla.org/en-US/docs/Web/API/crypto_property
+	// [Window.customElements](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/customElements
+	// [Window.devicePixelRatio](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio
+	// [Window.document](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/document
+	// [Window.frameElement](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/frameElement
+	// [Window.frames](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/frames
+	// [Window.fullScreen](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/fullScreen
+	// [Window.history](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/History
+	// [Window.indexedDB](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/indexedDB
+	// [Window.innerHeight](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/innerHeight
+	// [Window.innerWidth](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth
+	// [Window.isSecureContext](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/isSecureContext
+	// [Window.launchQueue](Read only)(Experimental) https://developer.mozilla.org/en-US/docs/Web/API/Window/launchQueue
+	// [Window.length](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/length
+	// [Window.location] https://developer.mozilla.org/en-US/docs/Web/API/Window/location
+	// [Window.locationbar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/locationbar
+	// [Window.localStorage](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+	// [Window.menubar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/menubar
+	// [Window.mozInnerScreenX](Read only)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/mozInnerScreenX
+	// [Window.mozInnerScreenY](Read only)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Window/mozInnerScreenY
+	// [Window.name] https://developer.mozilla.org/en-US/docs/Web/API/Window/name
+	// [Window.navigation](Read only)(Experimental) https://developer.mozilla.org/en-US/docs/Web/API/Window/navigation
+	// [Window.navigator](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator
+	// [Window.opener] https://developer.mozilla.org/en-US/docs/Web/API/Window/opener
+	// [Window.origin](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/origin
+	// [Window.outerHeight](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/outerHeight
+	// [Window.outerWidth](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/outerWidth
+	// [Window.pageXOffset](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/pageXOffset
+	// [Window.pageYOffset](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
+	// [Window.parent](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/parent
+	// [Window.performance](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/performance
+	// [Window.personalbar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/personalbar
+	// [Window.scheduler](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scheduler
+	// [Window.screen](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/screen
+	// [Window.screenX and Window.screenLeft](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/screenX
+	// [Window.screenY and Window.screenTop](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/screenY
+	// [Window.scrollbars](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollbars
+	// [Window.scrollMaxX](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollMaxX
+	// [Window.scrollMaxY](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollMaxY
+	// [Window.scrollX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollX
+	// [Window.scrollY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/screenY
+	// [Window.self](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/self
+	// [Window.sessionStorage] https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
+	// [Window.speechSynthesis](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/speechSynthesis
+	// [Window.statusbar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/statusbar
+	// [Window.toolbar](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/toolbar
+	// [Window.top](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/top
+	// [Window.visualViewport](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/visualViewport
+	// [Window.window](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/window
+	// [window[0], window[1], etc.] https://developer.mozilla.org/en-US/docs/Web/API/Window
 
+	
+	////// Deprecated properties //////
+	// [Window.content and Window._content](Deprecated)(Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window
+	// [Window.defaultStatus](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Window/defaultStatus
+	// [Window.event](Deprecated)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/event
+	// [Window.external](Deprecated)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/external
+	// [Window.orientation](Deprecated)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/orientation
+	// [Window.returnValue](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Window
+	// [Window.sidebar](Deprecated)(Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Window/sidebar
+	// [Window.status](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Window/status
 };
 
 
