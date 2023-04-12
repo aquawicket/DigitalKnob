a2c9b248f4727ebc6fa4e403c5b87287855900cc
aquawicket <aquawicket@hotmail.com>
2023-04-12 12:44:48 -0400

    commit from git

---
+++ DKPlugins/DKEventTargetTest/DKSDLWindowJS.h
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef DKSDLWindowJS_H
+#define DKSDLWindowJS_H
+
+#include "DKDuktape/DKDuktape.h"
+
+WARNING_DISABLE
+#include "dukglue/dukglue.h"
+WARNING_ENABLE
+
+
+class DKSDLWindowJS : public DKObjectT<DKSDLWindowJS>
+{
+public:
+	bool Init(){
+		
+		////// Constructor //////
+		DKDuktape::AttachFunction("CPP_DKSDLWindow", DKSDLWindowJS::constructor);
+			
+			
+		////// Load .js files
+		DKClass::DKCreate("DKEventTargetTest/DKSDLWindow.js");
+		
+		return true;
+	}
+	
+	
+	////// Constructor //////
+	static int constructor(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKINFO("CPP_DKSDLWindow()\n");
+		DKSDLWindow* sdlwindow = new DKSDLWindow();
+		DKString eventTargetAddress = pointerToAddress(sdlwindow);
+		duk_push_string(ctx, eventTargetAddress.c_str());	
+		return true;
+	}
+	
+};
+REGISTER_OBJECT(DKSDLWindowJS, true)
+
+
+#endif //DKSDLWindowJS_H
