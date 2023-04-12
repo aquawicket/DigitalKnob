a1312939a7ebce5ff560923e976dd56ae27b5aff
aquawicket <aquawicket@hotmail.com>
2023-04-12 14:46:21 -0400

    commit from git

---
+++ DKApps/DKDomTest/assets/EventTest.js
@@ -0,0 +1,39 @@
+// create EventTarget
+const obj = new EventTarget();
+
+
+////// Event ///////
+function printEventProperties(event){
+	////// Instance properties //////
+	console.log("event.bubbles = "					+event.bubbles)
+	console.log("event.cancelable = "				+event.cancelable)
+	console.log("event.composed = "					+event.composed)
+	console.log("event.currentTarget = "			+event.currentTarget)
+	console.log("event.defaultPrevented = "			+event.defaultPrevented)
+	console.log("event.eventPhase = "				+event.eventPhase)			
+	console.log("event.isTrusted = "				+event.isTrusted)		
+	console.log("event.target = "					+event.target)			
+	console.log("event.timeStamp = "				+event.timeStamp)
+	console.log("event.type = "						+event.type)
+	////// Legacy and non-standard properties //////
+	console.log("event.cancelBubble = "				+event.cancelBubble)
+	console.log("event.explicitOriginalTarget = "	+event.explicitOriginalTarget)
+	console.log("event.originalTarget = "			+event.originalTarget)
+	console.log("event.returnValue = "				+event.returnValue)
+	console.log("event.scoped = "					+event.scoped)
+}
+
+console.log('\n');
+const myEventA = new Event('myeventA');
+obj.addEventListener('myeventA', function(event){
+	console.log('onmyeventA')
+	printEventProperties(event)
+})
+obj.dispatchEvent(myEventA);
+
+console.log('\n');
+obj.addEventListener('myeventB', function(event){
+	console.log('onmyeventB')
+	printEventProperties(event)
+})
+obj.dispatchEvent(new Event('myeventB'));
---
+++ DKApps/DKDomTest/assets/app.js
@@ -0,0 +1,33 @@
+// https://console.spec.whatwg.org
+CPP_DK_Create("DKConsole");
+
+// https://dom.spec.whatwg.org
+CPP_DK_Create("DKEventJS");
+CPP_DK_Create("DKCustomEventJS");
+CPP_DK_Create("DKEventTargetJS");
+CPP_DK_Create("EventTest.js");
+
+/*
+// https://w3c.github.io/uievents
+CPP_DK_Create("DKUIEventJS");
+CPP_DK_Create("DKFocusEventJS");
+CPP_DK_Create("DKMouseEventJS");
+CPP_DK_Create("DKWheelEventJS");
+CPP_DK_Create("DKInputEventJS");
+CPP_DK_Create("DKKeyboardEventJS");
+CPP_DK_Create("DKCompositionEventJS");
+*/
+
+/*
+// https://html.spec.whatwg.org/
+CPP_DK_Create("DKWindowJS");
+*/
+
+/*
+// DigitalKnob
+CPP_DK_Create("DKConsoleWindowJS");
+//CPP_DK_Create("DKSDLWindowJS");
+*/
+
+//CPP_DK_Create("SDLWindowTest.js");
+//CPP_DK_Create("TestDictParams.js");
