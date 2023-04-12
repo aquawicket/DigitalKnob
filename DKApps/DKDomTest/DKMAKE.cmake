1f42c4ac763aecc36f86e86a276b558826a660ad
aquawicket <aquawicket@hotmail.com>
2023-04-12 14:25:21 -0400

    commit from git

--- DKApps/DKDomTest/DKMAKE.cmake
+++ DKApps/DKDomTest/DKMAKE.cmake
@@ -1,4 +1,5 @@
 dk_depend(DK)
 dk_depend(DKConsole)
 dk_depend(DKEvent)
+dk_depend(DKCustomEvent)
 dk_depend(DKEventTarget)
---
+++ DKPlugins/DKCustomEvent/DKCustomEvent.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef DKCustomEvent_H
+#define DKCustomEvent_H
+
+#include "DKEvent/DKEvent.h"
+
+
+// [INTERFACE] https://dom.spec.whatwg.org/#interface-customevent
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
+class DKCustomEvent : public DKEvent
+{
+public:
+	////// Constructor //////
+	// [CustomEvent()] https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+	DKCustomEvent(DKString _type, DKString _options) : DKEvent(_type, _options) {
+		DKINFO("DKCustomEvent("+_type+", "+_options+") \n");
+		
+		eventClass = "CustomEvent";
+		eventAddress = pointerToAddress(this);
+		
+		////// Instance properties //////
+		detail = "{}";
+	}
+	
+	
+	////// Instance properties //////
+	// [CustomEvent.detail](Read only) https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/detail
+	DKString detail;
+
+	
+	////// Instance methods //////
+	// [CustomEvent.initCustomEvent()](Deprecated)
+	void initCustomEvent(){
+		DKDEPRECATED();
+	}
+};
+
+
+#endif //DKCustomEvent_H
