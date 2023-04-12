eb9b0aa85e4b8d0b5c14f8a275b5654dfad0d63c
aquawicket <aquawicket@hotmail.com>
2023-04-12 16:59:20 -0400

    commit from git

--- DKApps/DKDomTest/assets/app.js
+++ DKApps/DKDomTest/assets/app.js
@@ -5,7 +5,7 @@ CPP_DK_Create("DKConsole");
 CPP_DK_Create("DKEventJS");
 CPP_DK_Create("DKCustomEventJS");
 CPP_DK_Create("DKEventTargetJS");
-CPP_DK_Create("EventTest.js");
+//CPP_DK_Create("EventTest.js");
 
 // https://w3c.github.io/uievents
 CPP_DK_Create("DKUIEventJS");
@@ -14,17 +14,15 @@ CPP_DK_Create("DKMouseEventJS");
 CPP_DK_Create("DKWheelEventJS");
 CPP_DK_Create("DKInputEventJS");
 CPP_DK_Create("DKKeyboardEventJS");
-CPP_DK_Create("DKCompositionEventJS");
+//CPP_DK_Create("DKCompositionEventJS");
 
 // https://html.spec.whatwg.org/
 CPP_DK_Create("DKWindowJS");
 
-// DigitalKnob
+// ConsoleWindow
 CPP_DK_Create("DKConsoleWindowJS");
-CPP_DK_Create("ConsoleWindowTest.js");
+//CPP_DK_Create("ConsoleWindowTest.js");
 
-//CPP_DK_Create("DKSDLWindowJS");
-
-
-//CPP_DK_Create("SDLWindowTest.js");
-//CPP_DK_Create("TestDictParams.js");
+// SDLWindow
+CPP_DK_Create("DKSDLWindowJS");
+CPP_DK_Create("SDLWindowTest.js");
