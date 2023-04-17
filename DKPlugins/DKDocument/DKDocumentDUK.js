744ad93c2fac7476520200f39a0f070bc8271131
aquawicket <aquawicket@hotmail.com>
2023-04-17 15:25:45 -0400

    commit from git

--- DKPlugins/DKDocument/DKDocumentDUK.js
+++ DKPlugins/DKDocument/DKDocumentDUK.js
@@ -2,7 +2,8 @@
 // [MDN] https://developer.mozilla.org/en-US/docs/Web/API/Document
 
 
-// [Document()]
+////// Constructor //////
+// [Document()] https://developer.mozilla.org/en-US/docs/Web/API/Document/Document
 var Document = function Document(address) {
 	//console.log("Document("+address+")")
 	
@@ -49,6 +50,96 @@ var Document = function Document(address) {
 	// [Document.visibilityState](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/visibilityState
 	
 	
+	////// Extensions for HTMLDocument //////
+	// [Document.cookie] https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
+	// [Document.defaultView](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/defaultView
+	// [Document.designMode] https://developer.mozilla.org/en-US/docs/Web/API/Document/designMode
+	// [Document.dir] https://developer.mozilla.org/en-US/docs/Web/API/Document/dir
+	// [Document.domain](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/domain
+	// [Document.fullscreenEnabled](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/fullscreenEnabled
+	// [Document.lastModified](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/lastModified
+	// [Document.location](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/location
+	// [Document.readyState](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/readyState
+	// [Document.referrer](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/referrer
+	// [Document.title] https://developer.mozilla.org/en-US/docs/Web/API/Document/title
+	// [Document.URL](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/URL
+		
+	
+	////// Deprecated properties //////
+	// [Document.alinkColor](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/alinkColor
+	// [Document.all](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/all
+	// [Document.anchors](Deprecated)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/anchors
+	// [Document.applets](Deprecated)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/applets
+	// [Document.bgColor](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/bgColor
+	// [Document.charset](Deprecated)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/characterSet
+	// [Document.fgColor](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/fgColor
+	// [Document.fullscreen](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/fullscreen
+	// [Document.height](Non-standard)(Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Element/clientHeight
+	// [Document.inputEncoding](Deprecated)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/Document/characterSet
+	// [Document.lastStyleSheetSet](Deprecated)(Read only)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/lastStyleSheetSet
+	// [Document.linkColor](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/linkColor
+	// [Document.preferredStyleSheetSet](Deprecated)(Read only)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/preferredStyleSheetSet
+	// [Document.rootElement](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/rootElement
+	// [Document.selectedStyleSheetSet](Deprecated)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/selectedStyleSheetSet
+	// [Document.styleSheetSets](Deprecated)(Read only)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/styleSheetSets
+	// [Document.vlinkColor](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/vlinkColor
+	// [Document.width](Non-standard)(Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Element/clientWidth
+	// [Document.xmlEncoding](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/xmlEncoding
+	// [Document.xmlStandalone](Deprecated)
+	// [Document.xmlVersion](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/xmlVersion
+		
+	
+	////// Instance methods //////
+	// [Document.adoptNode()] https://developer.mozilla.org/en-US/docs/Web/API/Document/adoptNode
+	// [Document.append()] https://developer.mozilla.org/en-US/docs/Web/API/Document/append
+	// [Document.captureEvents()](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document/caretPositionFromPoint
+	// [Document.caretPositionFromPoint()] https://developer.mozilla.org/en-US/docs/Web/API/Document/caretPositionFromPoint
+	// [Document.caretRangeFromPoint()](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/caretRangeFromPoint
+	// [Document.createAttribute()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createAttribute
+	// [Document.createAttributeNS()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createAttributeNS
+	// [Document.createCDATASection()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createCDATASection
+	// [Document.createComment()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createComment
+	// [Document.createDocumentFragment()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment
+	// [Document.createElement()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+	// [Document.createElementNS()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createElementNS
+	// [Document.createEntityReference()](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Document
+	// [Document.createEvent()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createEvent
+	// [Document.createNodeIterator()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createNodeIterator
+	// [Document.createProcessingInstruction()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createProcessingInstruction
+	// [Document.createRange()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createRange
+	// [Document.createTextNode()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createTextNode
+	// [Document.createTouch()](Deprecated)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/createTouch
+	// [Document.createTouchList()](Deprecated)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/createTouchList
+	// [Document.createTreeWalker()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createTreeWalker
+	// [Document.elementFromPoint()] https://developer.mozilla.org/en-US/docs/Web/API/Document/elementFromPoint
+	// [Document.elementsFromPoint()] https://developer.mozilla.org/en-US/docs/Web/API/Document/elementsFromPoint
+	// [Document.enableStyleSheetsForSet()](Deprecated)(Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/enableStyleSheetsForSet
+	// [Document.exitFullscreen()] https://developer.mozilla.org/en-US/docs/Web/API/Document/exitFullscreen
+	// [Document.exitPictureInPicture()] https://developer.mozilla.org/en-US/docs/Web/API/Document/exitPictureInPicture
+	// [Document.exitPointerLock()] https://developer.mozilla.org/en-US/docs/Web/API/Document/exitPointerLock
+	// [Document.getAnimations()] https://developer.mozilla.org/en-US/docs/Web/API/Document/getAnimations
+	// [Document.getBoxQuads()](Experimental)
+	// [Document.getElementById()] https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+	// [Document.getElementsByClassName()] https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName
+	// [Document.getElementsByTagName()] https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByTagName
+	// [Document.getElementsByTagNameNS()] https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByTagNameNS
+	// [Document.getSelection()] https://developer.mozilla.org/en-US/docs/Web/API/Document/getSelection
+	// [Document.hasStorageAccess()] https://developer.mozilla.org/en-US/docs/Web/API/Document/hasStorageAccess
+	// [Document.importNode()] https://developer.mozilla.org/en-US/docs/Web/API/Document/importNode
+	// [Document.prepend()] https://developer.mozilla.org/en-US/docs/Web/API/Document/prepend
+	// [Document.querySelector()] https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+	// [Document.querySelectorAll()] https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+	// [Document.releaseCapture()](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/releaseCapture
+	// [Document.releaseEvents()](Deprecated)
+	// [Document.replaceChildren()] https://developer.mozilla.org/en-US/docs/Web/API/Document/replaceChildren
+	// [Document.requestStorageAccess()] https://developer.mozilla.org/en-US/docs/Web/API/Document/requestStorageAccess
+	// [Document.startViewTransition()](Experimental) https://developer.mozilla.org/en-US/docs/Web/API/Document/startViewTransition
+	// [Document.mozSetImageElement()](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Document/mozSetImageElement
+	// [Document.createExpression()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createExpression
+	// [Document.createNSResolver()] https://developer.mozilla.org/en-US/docs/Web/API/Document/createNSResolver
+	// [Document.evaluate()] https://developer.mozilla.org/en-US/docs/Web/API/Document/evaluate
+		
+		
 	////// toString //////
 	if(this.toString() === "[object Object]")
 		this.toString = function(){	return "[object Document]" }
