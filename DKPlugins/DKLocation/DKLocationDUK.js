fc3c268879d63b3127a2397ed5f1cdcb9ffeb302
aquawicket <aquawicket@hotmail.com>
2023-04-17 13:08:35 -0400

    commit from git

--- DKPlugins/DKLocation/DKLocationDUK.js
+++ DKPlugins/DKLocation/DKLocationDUK.js
@@ -10,6 +10,26 @@ var Location = function Location(address) {
 	if(!this.address)
 		this.address = CPP_DKLocationDUK();
 
+
+	////// Instance properties //////
+	// [Location.ancestorOrigins] https://developer.mozilla.org/en-US/docs/Web/API/Location/ancestorOrigins
+	// [Location.href] https://developer.mozilla.org/en-US/docs/Web/API/Location/href
+	// [Location.protocol] https://developer.mozilla.org/en-US/docs/Web/API/Location/protocol
+	// [Location.host] https://developer.mozilla.org/en-US/docs/Web/API/Location/host
+	// [Location.hostname] https://developer.mozilla.org/en-US/docs/Web/API/Location/hostname
+	// [Location.port] https://developer.mozilla.org/en-US/docs/Web/API/Location/port
+	// [Location.pathname] https://developer.mozilla.org/en-US/docs/Web/API/Location/pathname
+	// [Location.search] https://developer.mozilla.org/en-US/docs/Web/API/Location/search
+	// [Location.hash] https://developer.mozilla.org/en-US/docs/Web/API/Location/hash
+	// [Location.origin](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Location/origin
+		
+		
+	////// Instance methods //////
+	// [Location.assign()] https://developer.mozilla.org/en-US/docs/Web/API/Location/assign
+	// [Location.reload()] https://developer.mozilla.org/en-US/docs/Web/API/Location/reload
+	// [Location.replace()] https://developer.mozilla.org/en-US/docs/Web/API/Location/replace
+	// [Location.toString()] https://developer.mozilla.org/en-US/docs/Web/API/Location/toString
+		
 	
 	////// toString //////
 	if(this.toString() === "[object Object]")
