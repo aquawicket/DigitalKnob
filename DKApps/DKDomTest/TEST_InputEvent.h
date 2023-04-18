60cadcf9050ad9ff555c5cbb2f78bfcd893daac0
aquawicket <aquawicket@hotmail.com>
2023-04-17 22:08:53 -0400

    commit from git

--- DKApps/DKDomTest/TEST_InputEvent.h
+++ DKApps/DKDomTest/TEST_InputEvent.h
@@ -6,11 +6,34 @@
 #include "DKInputEvent/DKInputEvent.h"
 
 
+// [W3C] https://w3c.github.io/uievents/#events-inputevents
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/InputEvent
 class TEST_InputEvent : public DKObjectT<TEST_InputEvent>
 {
 public:
 	bool Init(){
 		DKINFO("////// TEST_InputEvent.h ////// \n");
+		
+		////// Constructor //////
+		// [InputEvent()] https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/InputEvent
+	
+		
+		////// Instance properties //////
+		// [InputEvent.data](Read only) https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/data
+		// [InputEvent.dataTransfer](Read only) https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/dataTransfer
+		// [InputEvent.inputType](Read only) https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/inputType
+		// [InputEvent.isComposing](Read only) https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/isComposing
+	
+	
+		////// Instance methods //////
+		// [InputEvent.getTargetRanges()] https://developer.mozilla.org/en-US/docs/Web/API/InputEvent/getTargetRanges
+	
+	
+		////// Events //////
+		// [beforeinput] https://w3c.github.io/uievents/#event-type-beforeinput
+		// [input] https://w3c.github.io/uievents/#event-type-input
+	
+	
 		return DKTODO();
 	}
 	
