da14658e6a457113dceb304a3accd2e3237f074b
aquawicket <aquawicket@hotmail.com>
2023-04-17 22:02:16 -0400

    commit from git

--- DKApps/DKDomTest/assets/TEST_DragEvent.js
+++ DKApps/DKDomTest/assets/TEST_DragEvent.js
@@ -1,3 +1,21 @@
 console.log("////// TEST_DragEvent.js //////")
+// [IDL] https://html.spec.whatwg.org/multipage/dnd.html#the-dragevent-interface
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/DragEvent
 
-// TODO
+
+////// Constructor //////
+// [DragEvent()] https://developer.mozilla.org/en-US/docs/Web/API/DragEvent/DragEvent
+
+
+////// Instance properties //////
+// [DragEvent.dataTransfer](Read only) https://developer.mozilla.org/en-US/docs/Web/API/DragEvent/dataTransfer
+	
+	
+////// Event types //////
+// [drag] https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/drag_event
+// [dragend] https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragend_event
+// [dragenter] https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragenter_event
+// [dragleave] https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragleave_event
+// [dragover] https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragover_event
+// [dragstart] https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dragstart_event
+// [drop] https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/drop_event
