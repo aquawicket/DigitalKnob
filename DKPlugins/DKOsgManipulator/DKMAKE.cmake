279483a49393d6e3a6a869fc30dc346bccd6effc
aquawicket <aquawicket@hotmail.com>
2023-04-20 17:16:14 -0400

    commit from git

---
+++ DKPlugins/DKOsgManipulator/DKMAKE.cmake
@@ -0,0 +1,4 @@
+dk_depend(DKOSGWindow)
+#dk_depend(openscenegraph osgManipulator)
+
+dk_generateCmake(DKOSGManipulator)
---
+++ DKPlugins/DKOsgManipulator/DKOsgManipulator.cpp
@@ -0,0 +1,44 @@
+/*
+* This source file is part of digitalknob, the cross-platform C/C++/Javascript/Html/Css Solution
+*
+* For the latest information, see https://github.com/aquawicket/DigitalKnob
+*
+* Copyright(c) 2010 - 2023 Digitalknob Team, and contributors
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files(the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions :
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#include "DK/stdafx.h"
+#include "DKOSGManipulator/DKOSGManipulator.h"
+#include "DKOSGWindow/DKOSGWindow.h"
+
+
+bool DKOSGManipulator::Init(){
+	tb = new osgGA::TrackballManipulator;
+	DKString test = data[1];
+	//tb->setHomePosition(osg::Vec3(10., -100., 10.), osg::Vec3(10., 0., 10.), osg::Vec3(0., 0., 0.));
+	tb->setHomePosition(osg::Vec3(0., -100., 0.), osg::Vec3(0., 0., 0.), osg::Vec3(0., 0., 0.));
+	DKOSGWindow::Instance(data[1])->view->setCameraManipulator(tb);
+	return true;
+}
+
+bool DKOSGManipulator::End(){
+	//dkWindow->view->removeEventHandler(tb);
+	//tb = NULL;
+	return true; 
+}
