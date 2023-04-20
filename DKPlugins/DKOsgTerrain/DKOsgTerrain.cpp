cc29c3c80cac469cfb0c578952b3bdfa6b48eccf
aquawicket <aquawicket@hotmail.com>
2023-04-20 17:15:41 -0400

    commit from git

---
+++ DKPlugins/DKOsgTerrain/DKOsgTerrain.cpp
@@ -0,0 +1,103 @@
+/*
+* This source file is part of digitalknob, the cross-platform C/C++/Javascript/Html/Css Solution
+*
+* For the latest information, see https://github.com/aquawicket/DigitalKnob
+*
+* Copyright(c) 2010 - 2023 Digitalknob Team, and contributors
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files(the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions :
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#include "DK/stdafx.h"
+#include "DKOSGTerrain/DKOSGTerrain.h"
+#include "DKOSGWindow/DKOSGWindow.h"
+
+WARNING_DISABLE
+#include <osg/ShapeDrawable>
+#include "terrain_coords.h"
+#include <osgDB/ReadFile>
+WARNING_ENABLE
+
+
+bool DKOSGTerrain::Init(){
+	osg::Vec3 center(0.0f,0.0f,0.0f);
+	float radius = 100.0f;
+    terrain = createBase(center-osg::Vec3(0.0f,0.0f,radius*0.1),radius);
+	DKOSGWindow::Instance("DKOSGWindow")->world->addChild(terrain);
+	return true;
+}
+
+bool DKOSGTerrain::End(){
+	DKOSGWindow::Instance("DKOSGWindow")->world->removeChild(terrain);
+	return true;
+}
+
+osg::Node* DKOSGTerrain::createBase(const osg::Vec3& center,float radius){
+    osg::Geode* geode = new osg::Geode;
+    
+    // set up the texture of the base.
+    osg::StateSet* stateset = new osg::StateSet();
+    osg::Image* image = osgDB::readImageFile("lz.rgb");
+    if (image)
+    {
+        osg::Texture2D* texture = new osg::Texture2D;
+        texture->setImage(image);
+        stateset->setTextureAttributeAndModes(0,texture,osg::StateAttribute::ON);
+    }
+    
+    geode->setStateSet( stateset );
+
+    osg::HeightField* grid = new osg::HeightField;
+    grid->allocate(38,39);
+    grid->setOrigin(center+osg::Vec3(-radius,-radius,0.0f));
+    grid->setXInterval(radius*2.0f/(float)(38-1));
+    grid->setYInterval(radius*2.0f/(float)(39-1));
+    
+    float minHeight = FLT_MAX;
+    float maxHeight = -FLT_MAX;
+
+    unsigned int r;
+    for(r=0;r<39;++r)
+    {
+        for(unsigned int c=0;c<38;++c)
+        {
+            float h = vertex[r+c*39][2];
+            if (h>maxHeight) maxHeight=h;
+            if (h<minHeight) minHeight=h;
+        }
+    }
+    
+    float hieghtScale = radius*0.5f/(maxHeight-minHeight);
+    float hieghtOffset = -(minHeight+maxHeight)*0.5f;
+
+    for(r=0;r<39;++r)
+    {
+        for(unsigned int c=0;c<38;++c)
+        {
+            float h = vertex[r+c*39][2];
+            grid->setHeight(c,r,(h+hieghtOffset)*hieghtScale);
+        }
+    }
+    
+    geode->addDrawable(new osg::ShapeDrawable(grid));
+     
+    osg::Group* group = new osg::Group;
+    group->addChild(geode);
+     
+    return group;
+}
