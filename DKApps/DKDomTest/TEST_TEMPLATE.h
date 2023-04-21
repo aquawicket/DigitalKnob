6e71537c48c76f8872450de4a77cb61a17ca99a4
aquawicket <aquawicket@hotmail.com>
2023-04-20 20:46:10 -0400

    commit from git

---
+++ DKApps/DKDomTest/TEST_TEMPLATE.h
@@ -0,0 +1,29 @@
+// TEST_TEMPLATE
+// [IDL]
+// [MDN]
+
+#pragma once
+#ifndef TEST_TEMPLATE_H
+#define TEST_TEMPLATE_H
+
+#include "DK/DK.h"
+//#include "DKTemplate/DKTemplate.h"
+
+
+class TEST_TEMPLATE : public DKObjectT<TEST_TEMPLATE>
+{
+public:
+	bool Init(){
+		DKINFO("////// TEST_TEMPLATE.h ////// \n");
+		
+		////// Instance properties //////
+		// etc . . .
+		
+		return DKTODO();
+	}
+	
+};
+REGISTER_OBJECT(TEST_TEMPLATE, true);
+
+
+#endif //TEST_TEMPLATE_H
