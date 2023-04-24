a1a6e3ce05140a7d289a26070be3be588a1c21a3
aquawicket <aquawicket@hotmail.com>
2023-04-24 15:04:50 -0400

    commit from git

---
+++ DKPlugins/DKRmlDocument/DKRmlDocumentDUK.h
@@ -0,0 +1,42 @@
+#if HAVE_DKDuktape
+
+#pragma once
+#ifndef DKRmlDocumentDUK_H
+#define DKRmlDocumentDUK_H
+
+#include "DKRmlDocument/DKRmlDocument.h"
+#include "DKDuktape/DKDuktape.h"
+
+
+class DKRmlDocumentDUK : public DKObjectT<DKRmlDocumentDUK>
+{
+public:
+	bool Init(){
+		
+		////// Constructor //////
+		DKDuktape::AttachFunction("CPP_DKRmlDocumentDUK", DKRmlDocumentDUK::constructor);
+			
+			
+		////// Load .js files
+		DKClass::DKCreate("DKRmlDocument/DKRmlDocumentDUK.js");
+		
+		return true;
+	}
+	
+	
+	////// Constructor //////
+	static int constructor(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKINFO("CPP_DKRmlDocumentDUK()\n");
+		DKRmlDocument* rmlDocument = (DKRmlDocument*)DKClass::DKCreate("DKRmlDocument");
+		DKString eventTargetAddress = pointerToAddress(rmlDocument);
+		duk_push_string(ctx, eventTargetAddress.c_str());	
+		return true;
+	}
+	
+};
+REGISTER_OBJECT(DKRmlDocumentDUK, true)
+
+
+#endif //DKRmlDocumentDUK_H
+#endif //HAVE_DKDuktape
