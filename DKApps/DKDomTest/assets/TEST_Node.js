0143298130d9a2752c9b9f1e34ae3979c4a0d817
aquawicket <aquawicket@hotmail.com>
2023-04-27 14:02:40 -0400

    commit from git

--- DKApps/DKDomTest/assets/TEST_Node.js
+++ DKApps/DKDomTest/assets/TEST_Node.js
@@ -1,6 +1,22 @@
+// [IDL] https://dom.spec.whatwg.org/#interface-node
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/Node
 console.log("////// TEST_Node.js //////")
+
 
 function printNodeProperties(node){
 	////// Instance properties //////
-	// TODO
+	console.log("node.baseURI = "			+node.baseURI)			// [Node.baseURI](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/baseURI
+	console.log("node.childNodes = "		+node.childNodes)		// [Node.childNodes](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/childNodes
+	console.log("node.firstChild = "		+node.firstChild)		// [Node.firstChild](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/firstChild
+	console.log("node.isConnected = "		+node.isConnected)		// [Node.isConnected](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/isConnected
+	console.log("node.lastChild = "			+node.lastChild)		// [Node.lastChild](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/lastChild
+	console.log("node.nextSibling = "		+node.nextSibling)		// [Node.nextSibling](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/nextSibling
+	console.log("node.nodeName = "			+node.nodeName)			// [Node.nodeName](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeName
+	console.log("node.nodeType = "			+node.nodeType)			// [Node.nodeType](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+	console.log("node.nodeValue = "			+node.nodeValue)		// [Node.nodeValue] https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeValue
+	console.log("node.ownerDocument = "		+node.ownerDocument)	// [Node.ownerDocument](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/ownerDocument
+	console.log("node.parentNode = "		+node.parentNode)		// [Node.parentNode](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/parentNode
+	console.log("node.parentElement = "		+node.parentElement)	// [Node.parentElement](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/parentElement
+	console.log("node.previousSibling = "	+node.previousSibling)	// [Node.previousSibling](Read only) https://developer.mozilla.org/en-US/docs/Web/API/Node/previousSibling
+	console.log("node.textContent = "		+node.textContent)		// [Node.textContent] https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
 }
--- DKApps/DKDomTest/assets/TEST_Window.js
+++ DKApps/DKDomTest/assets/TEST_Window.js
@@ -1,4 +1,7 @@
+// [IDL] https://html.spec.whatwg.org/multipage/nav-history-apis.html#the-window-object
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/Window
 console.log("////// TEST_Window.js //////")
+
 
 function printWindowProperties(window){
 	////// Instance properties //////
