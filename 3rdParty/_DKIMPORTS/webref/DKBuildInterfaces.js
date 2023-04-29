6a8cfcd933e20cac474c80af6a55878021f53449
aquawicket <aquawicket@hotmail.com>
2023-04-29 17:23:19 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/webref/DKBuildInterfaces.js
+++ 3rdParty/_DKIMPORTS/webref/DKBuildInterfaces.js
@@ -1,1 +1,10 @@
 console.log("////// DKBuildInterfaces.js //////\n");
+
+const glob = require(“glob”);
+
+glob("*.js", (error, filesWithJs)=>{
+  if(error){
+    console.log(error)
+  }
+  console.log(filesWithJs)
+}
