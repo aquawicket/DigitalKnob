6605785de5b85877c0d1c6e679327df54385c851
aquawicket <aquawicket@hotmail.com>
2023-04-29 17:03:25 -0400

    commit from git

---
+++ 3rdParty/_DKIMPORTS/webref/DKBuildInterfaces.js
@@ -0,0 +1,1 @@
+console.log("////// DKBuildInterfaces.js //////\n");
--- 3rdParty/_DKIMPORTS/webref/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/webref/DKMAKE.cmake
@@ -1,15 +1,18 @@
 # https://github.com/w3c/webref.git
 
+dk_depend(nodejs)
 
 dk_import(https://github.com/w3c/webref.git BRANCH curated)
 
 
 ### Build Interface Tree ###
-dk_makeDirectory(${WEBREF}/DKWebAPIs)
-dk_remove(${WEBREF}/DKWebAPIs/interfaces.txt NOERROR)
-if(NOT EXISTS ${WEBREF}/DKWebAPIs/interfaces.txt)
-	file(GLOB items RELATIVE "${WEBREF}/ed/idlnames/" "${WEBREF}/ed/idlnames/*")
-	foreach(item ${items})
-		file(APPEND ${WEBREF}/DKWebAPIs/interfaces.txt "${item}\n")
-	endforeach()
-endif()
+dk_command(${NODE_EXE} ${WEBREF}/DKBuildInterfaces.js)
+
+#dk_makeDirectory(${WEBREF}/DKWebAPIs)
+#dk_remove(${WEBREF}/DKWebAPIs/interfaces.txt NOERROR)
+#if(NOT EXISTS ${WEBREF}/DKWebAPIs/interfaces.txt)
+#	file(GLOB items RELATIVE "${WEBREF}/ed/idlnames/" "${WEBREF}/ed/idlnames/*")
+#	foreach(item ${items})
+#		file(APPEND ${WEBREF}/DKWebAPIs/interfaces.txt "${item}\n")
+#	endforeach()
+#endif()
