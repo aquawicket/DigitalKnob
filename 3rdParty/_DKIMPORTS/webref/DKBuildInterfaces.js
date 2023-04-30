3d58a3f13095af4bde2b66072a7c68fe293e4bd1
aquawicket <aquawicket@hotmail.com>
2023-04-29 21:12:45 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/webref/DKBuildInterfaces.js
+++ 3rdParty/_DKIMPORTS/webref/DKBuildInterfaces.js
@@ -3,6 +3,28 @@ console.log("////// DKBuildInterfaces.js //////\n");
 const path = require('path');
 const fs = require('fs');
 
+/*
+const readline = require('readline');
+async function processLineByLine(file) {
+	console.log('\n//////'+file)
+  const fileStream = fs.createReadStream(file);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    console.log(`Line from file: ${line}`);
+  }
+}
+*/
+
+
+
 if (fs.existsSync('temp.txt')) {
     // remove temp.txt
 	fs.unlink('temp.txt', (err) => {
@@ -23,10 +45,21 @@ fs.readdir(directoryPath, function (err, files) {
         return console.log('Unable to scan directory: ' + err);
     } 
     files.forEach(function (file) {
-        console.log(file); 
+		var filepath = directoryPath+'/'+file
+        
+		/*
 		fs.appendFile("temp.txt", file+"\n", (err) => {
 			if (err) console.log(err);
 			//console.log("Successfully Written to File.");
+		});
+		*/
+		
+		// print file lines
+		fs.readFile(filepath, 'utf-8', (err, data) => {
+			console.log('\n////// '+file+' //////'); 
+			const lines = data.split('\n')
+			for (let line of lines)
+				console.log(line)
 		});
     });
 });
