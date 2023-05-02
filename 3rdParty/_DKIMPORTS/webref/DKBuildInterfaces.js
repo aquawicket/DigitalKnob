658cd986ee3fc61925a7e6f8963687ef2c66873d
aquawicket <aquawicket@hotmail.com>
2023-05-02 14:22:38 -0400

    commit from git

--- 3rdParty/_DKIMPORTS/webref/DKBuildInterfaces.js
+++ 3rdParty/_DKIMPORTS/webref/DKBuildInterfaces.js
@@ -3,27 +3,6 @@ console.log("////// DKBuildInterfaces.js //////\n");
 const path = require('path');
 const fs = require('fs');
 
-/*
-const readline = require('readline');
-async function processLineByLine(file) {
-	console.log('\n//////'+file)
-  const fileStream = fs.createReadStream(file);
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
-
-  for await (const line of rl) {
-    // Each line in input.txt will be successively available here as `line`.
-    console.log(`Line from file: ${line}`);
-  }
-}
-*/
-
-
 
 if (fs.existsSync('temp.txt')) {
 	fs.unlinkSync('temp.txt')
@@ -38,34 +17,84 @@ var interfaces = {}
 
 var files = fs.readdirSync(directoryPath);
 
+// add base interfaces
 files.forEach(file => {
-	let fileStat = fs.statSync(directoryPath + '/' + file).isDirectory();
-	if(!fileStat) {
-		var filepath = directoryPath+'/'+file
-		var interface = file.slice(0, -4);
+	var filepath = directoryPath+'/'+file
+	var interface = file.slice(0, -4);
 
-		var data = fs.readFileSync(filepath, 'utf-8')
-		const lines = data.split('\n')
-		for (let line of lines){
-				if(!line.includes('interface'))
-					continue;
-				if(!line.includes(interface))
-					continue;
-				if(line.includes(':'))
-					continue;
-				if(line.includes('partial'))
-					continue;
-				if(line.includes('mixin'))
-					continue;
-				if(line.includes('//'))
-					continue;
-				if(line.includes('*'))
-					continue;
-				
-				interfaces[interface] = {};	
-			}
+	var data = fs.readFileSync(filepath, 'utf-8')
+	const lines = data.split('\n')
+	for (let line of lines){
+		if(!line.includes('interface'))
+			continue;
+		if(!line.includes(interface))
+			continue;
+		if(line.includes(':'))
+			continue;
+		if(line.includes('partial'))
+			continue;
+		if(line.includes('mixin'))
+			continue;
+		if(line.includes('//'))
+			continue;
+		if(line.includes('*'))
+			continue;
+		
+		interfaces[interface] = {};	
+		
+		// remove file from array
+		const index = files.indexOf(file);
+		if (index > -1) { // only splice array when item is found
+			files.splice(index, 1); // 2nd parameter means remove one item only
+		}
 	}
 });
+
+// add inherited interfaces
+files.forEach(file => {
+	var filepath = directoryPath+'/'+file
+	var interface = file.slice(0, -4);
+
+	var data = fs.readFileSync(filepath, 'utf-8')
+	const lines = data.split('\n')
+	for (let line of lines){
+		if(!line.includes('interface'))
+			continue;
+		if(!line.includes(interface))
+			continue;
+		if(!line.includes(':'))
+			continue;	
+
+		// get the parent name
+		let start = line.indexOf(":");
+		let end = line.indexOf("{");
+		let parent = line.substring(start+2, end-1);
+		
+		//console.log('['+parent+']')
+		if(!interfaces[parent])
+			continue;
+		
+		
+
+		interfaces[parent][interface] = {};	
+		
+		
+		
+
+		
+		// remove file from array
+		const index = files.indexOf(file);
+		if (index > -1) { // only splice array when item is found
+			files.splice(index, 1); // 2nd parameter means remove one item only
+		}
+	}
+});
+
+/*	
+for (const [key, value] of Object.entries(interfaces)) {
+	console.log(`${key}: ${value}`);
+}
+*/
 
 
 var interfaces_string = JSON.stringify(interfaces);
