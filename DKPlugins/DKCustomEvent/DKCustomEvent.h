5bdde2d13cb595dfae06ea89629649ec00813bfb
aquawicket <aquawicket@hotmail.com>
2023-05-04 16:47:17 -0400

    commit from git

--- DKPlugins/DKCustomEvent/DKCustomEvent.h
+++ DKPlugins/DKCustomEvent/DKCustomEvent.h
@@ -1,4 +1,5 @@
-// [IDL] https://dom.spec.whatwg.org/#interface-customevent
+// [IDL] file:///C:/Users/Administrator/digitalknob/DK/3rdParty/webref-curated/ed/idlnames/CustomEvent.idl
+// [SOURCE] https://dom.spec.whatwg.org/#interface-customevent
 // [MDN] https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
 #pragma once
 #ifndef DKCustomEvent_H
@@ -6,28 +7,25 @@
 
 #include "DKEvent/DKEvent.h"
 
-
+// Source: DOM Standard (https://dom.spec.whatwg.org/)
+// [Exposed=*]
+// interface CustomEvent : Event {
 class DKCustomEvent : public DKEvent
 {
 public:
-	////// Constructor //////
-	// [CustomEvent()] https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
-	DKCustomEvent(DKString _type, DKString _options) : DKEvent(_type, _options) {
+	// constructor(DOMString type, optional CustomEventInit eventInitDict = {});
+	DKCustomEvent(DKString _type, DKString _options) : DKEvent(_type, _options) { // [CustomEvent()] https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
 		DKINFO("DKCustomEvent("+_type+", "+_options+") \n");
 		
 		eventClass = "CustomEvent";
 		eventAddress = pointerToAddress(this);
 	}
 	
-	
-	////// Instance properties //////
-	// [CustomEvent.detail](Read only) https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/detail
-	DKString detail = "{}";
+	// readonly attribute any detail;
+	DKString detail = "{}";	// [CustomEvent.detail](Read only) https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/detail
 
-	
-	////// Instance methods //////
-	// [CustomEvent.initCustomEvent()](Deprecated)
-	void initCustomEvent(){
+	// undefined initCustomEvent(DOMString type, optional boolean bubbles = false, optional boolean cancelable = false, optional any detail = null); // legacy
+	void initCustomEvent(){	// [CustomEvent.initCustomEvent()](Deprecated)
 		DKDEPRECATED();
 	}
 };
