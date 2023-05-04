b0b70ac4cbcda5160227af2ca8b7bbf612ddf62d
aquawicket <aquawicket@hotmail.com>
2023-05-04 17:21:10 -0400

    commit from git

--- DKPlugins/DKMouseEvent/DKMouseEvent.h
+++ DKPlugins/DKMouseEvent/DKMouseEvent.h
@@ -1,4 +1,5 @@
-// [IDL] https://w3c.github.io/uievents/#events-mouseevents
+// [IDL] file:///C:/Users/Administrator/digitalknob/DK/3rdParty/webref-curated/ed/idlnames/MouseEvent.idl
+// [SOURCE] https://w3c.github.io/uievents/#events-mouseevents
 // [MDN] https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
 #pragma once
 #ifndef DKMouseEvent_H
@@ -6,103 +7,104 @@
 
 #include "DKUIEvent/DKUIEvent.h"
 
-
+// Source: UI Events (https://www.w3.org/TR/uievents/)
+// [Exposed=Window]
+// interface MouseEvent : UIEvent {
 class DKMouseEvent : public DKUIEvent
 {
 public:
-	////// Constructor //////
-	// [MouseEvent()] https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent
-	DKMouseEvent(DKString _type, DKString _options) : DKUIEvent(_type, _options) {
-		DKINFO("DKMouseEvent("+_type+", "+_options+") \n");
+	// constructor(DOMString type, optional MouseEventInit eventInitDict = {});
+	DKMouseEvent(DKString _type, DKString _eventInitDict) : DKUIEvent(_type, _eventInitDict) { // [MouseEvent()] https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/MouseEvent
+		DKINFO("DKMouseEvent("+_type+", "+_eventInitDict+") \n");
 		
 		eventClass = "MouseEvent";
 		eventAddress = pointerToAddress(this);
 	}
 	
-	////// Static properties //////
-	// [MouseEvent.WEBKIT_FORCE_AT_MOUSE_DOWN](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/WEBKIT_FORCE_AT_MOUSE_DOWN
-	// [MouseEvent.WEBKIT_FORCE_AT_FORCE_MOUSE_DOWN](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/WEBKIT_FORCE_AT_FORCE_MOUSE_DOWN
+	// readonly attribute long screenX;
+	unsigned int screenX = 0; // [MouseEvent.screenX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/screenX
 	
+	// readonly attribute long screenY;
+	unsigned int screenY = 0; // [MouseEvent.screenY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/screenY
 	
-	////// Instance properties //////
-	// [MouseEvent.altKey](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/altKey
-	bool altKey = false;
-	// [MouseEvent.button](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
-	unsigned int button = 0;
-	// [MouseEvent.buttons](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/buttons
-	unsigned int buttons = 0;
-	// [MouseEvent.clientX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientX
-	unsigned int clientX = 0;
-	// [MouseEvent.clientY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientY
-	unsigned int clientY = 0;
-	// [MouseEvent.ctrlKey](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/ctrlKey
-	bool ctrlKey = false;
-	// [MouseEvent.layerX](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/layerX
-	int layerX = 0;
-	// [MouseEvent.layerY](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/layerY
-	int layerY = 0;
-	// [MouseEvent.metaKey](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/metaKey
-	bool metaKey = false;
-	// [MouseEvent.movementX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/movementX
-	int movementX = 0;
-	// [MouseEvent.movementY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/movementY
-	int movementY = 0;
-	// [MouseEvent.offsetX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/offsetX
-	int offsetX = 0;
-	// [MouseEvent.offsetY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/offsetY
-	int offsetY = 0;
-	// [MouseEvent.pageX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageX
-	int pageX = 0;
-	// [MouseEvent.pageY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageY
-	int pageY = 0;
-	// [MouseEvent.relatedTarget](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/relatedTarget
-	DKString relatedTarget = "";
-	// [MouseEvent.screenX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/screenX
-	unsigned int screenX = 0;
-	// [MouseEvent.screenY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/screenY
-	unsigned int screenY = 0;
-	// [MouseEvent.shiftKey](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/shiftKey
-	bool shiftKey = false;
-	// [MouseEvent.mozPressure](Non-standard)(Deprecated)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/mozPressure
-	float mozPressure = 0.0;
-	// [MouseEvent.mozInputSource](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/mozInputSource
-	DKString mozInputSource = "";
-	// [MouseEvent.webkitForce](Non-standard)(Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/webkitForce
-	unsigned int webkitForce = 0;
-	// [MouseEvent.x](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/x
-	unsigned int x = 0;
-	// [MouseEvent.y](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/y
-	unsigned int y = 0;		
+	// readonly attribute long clientX;
+	unsigned int clientX = 0; // [MouseEvent.clientX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientX
 	
+	// readonly attribute long clientY;
+	unsigned int clientY = 0; // [MouseEvent.clientY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientY
+	 
+	// readonly attribute boolean ctrlKey;
+	bool ctrlKey = false; // [MouseEvent.ctrlKey](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/ctrlKey
 	
-	////// Instance methods //////
-	// [MouseEvent.getModifierState()] https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/getModifierState
-	void getModifierState() {
+	// readonly attribute boolean shiftKey;
+	bool shiftKey = false; // [MouseEvent.shiftKey](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/shiftKey
+	
+	// readonly attribute boolean altKey;
+	bool altKey = false; // [MouseEvent.altKey](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/altKey
+	
+	// readonly attribute boolean metaKey;
+	bool metaKey = false; // [MouseEvent.metaKey](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/metaKey
+	
+	// readonly attribute short button;
+	unsigned int button = 0; // [MouseEvent.button](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+	
+	// readonly attribute unsigned short buttons;
+	unsigned int buttons = 0; // [MouseEvent.buttons](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/buttons
+	
+	// readonly attribute EventTarget? relatedTarget;
+	DKString relatedTarget = ""; // [MouseEvent.relatedTarget](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/relatedTarget
+	
+	// boolean getModifierState(DOMString keyArg);
+	void getModifierState() { // [MouseEvent.getModifierState()] https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/getModifierState
 		DKTODO();
 	}
-	// [MouseEvent.initMouseEvent()](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/initMouseEvent
-	void initMouseEvent() {
-		DKDEPRECATED();
-	}
 	
+	// Source: CSSOM View Module (https://www.w3.org/TR/cssom-view-1/)
+	// partial interface MouseEvent {
+	//  	readonly attribute double pageX;
+			int pageX = 0; // [MouseEvent.pageX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageX
+	//  	readonly attribute double pageY;
+			int pageY = 0;  // [MouseEvent.pageY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageY
+	//  	readonly attribute double x;
+			unsigned int x = 0; // [MouseEvent.x](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/x
+	//  	readonly attribute double y;
+			unsigned int y = 0;	// [MouseEvent.y](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/y
+	//  	readonly attribute double offsetX; 
+			int offsetX = 0; // [MouseEvent.offsetX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/offsetX
+	//  	readonly attribute double offsetY;
+			int offsetY = 0; // [MouseEvent.offsetY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/offsetY
+	// };
 	
-	////// Events //////
-	// [auxclick] https://developer.mozilla.org/en-US/docs/Web/API/Element/auxclick_event
-	// [click] https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
-	// [contextmenu] https://developer.mozilla.org/en-US/docs/Web/API/Element/contextmenu_event
-	// [dblclick] https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
-	// [DOMActivate](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/Element/DOMActivate_event
-	// [mousedown] https://developer.mozilla.org/en-US/docs/Web/API/Element/mousedown_event
-	// [mouseenter] https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseenter_event
-	// [mouseleave] https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseleave_event
-	// [mousemove] https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
-	// [mouseout] https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseout_event
-	// [mouseover] https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseover_event
-	// [mouseup] https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseup_event
-	// [webkitmouseforcechanged](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Element/webkitmouseforcechanged_event
-	// [webkitmouseforcedown](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Element/webkitmouseforcedown_event
-	// [webkitmouseforceup](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Element/webkitmouseforceup_event
-	// [webkitmouseforcewillbegin](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/Element/webkitmouseforcewillbegin_event
+	// Source: Pointer Lock 2.0 (https://www.w3.org/TR/pointerlock-2/)
+	// partial interface MouseEvent {
+	//   	readonly attribute double movementX;
+			int movementX = 0; // [MouseEvent.movementX](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/movementX
+	//   	readonly attribute double movementY;
+			int movementY = 0; // [MouseEvent.movementY](Read only) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/movementY
+	// };
+	
+	// Source: UI Events (https://www.w3.org/TR/uievents/)
+	// partial interface MouseEvent {
+	//		Deprecated in this specification
+	//	  	undefined initMouseEvent(DOMString typeArg,
+	//		optional boolean bubblesArg = false,
+	//		optional boolean cancelableArg = false,
+	//		optional Window? viewArg = null,
+	//		optional long detailArg = 0,
+	//		optional long screenXArg = 0,
+	//		optional long screenYArg = 0,
+	//		optional long clientXArg = 0,
+	//		optional long clientYArg = 0,
+	//		optional boolean ctrlKeyArg = false,
+	//		optional boolean altKeyArg = false,
+	//		optional boolean shiftKeyArg = false,
+	//		optional boolean metaKeyArg = false,
+	//		optional short buttonArg = 0,
+	//		optional EventTarget? relatedTargetArg = null);
+			void initMouseEvent() { // [MouseEvent.initMouseEvent()](Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/initMouseEvent
+				DKDEPRECATED();
+			}
+	// };
 };
 
 
