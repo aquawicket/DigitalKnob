2bc75e2f4f558526df6e1e5fd1cece37053f93a6
aquawicket <aquawicket@hotmail.com>
2023-05-07 19:06:59 -0400

    commit from git

--- DKApps/DKDomTest/assets/TEST_DKRmlDocument.js
+++ DKApps/DKDomTest/assets/TEST_DKRmlDocument.js
@@ -11,9 +11,3 @@ function printRmlDocumentProperties(rmlDocument){
 const myRmlDocument = new DKRmlDocument()
 printRmlDocumentProperties(myRmlDocument)
 printDocumentProperties(myRmlDocument)		//requires TEST_Document.js
-
-const myRmlLocation = new DKRmlLocation()
-//console.log("myRmlDocument.href = " +myRmlDocument.href)
-//const url = CPP_DKAssets_LocalAssets()+"index.html"
-//console.log("url = "+url)
-//myRmlDocument.href = url;
