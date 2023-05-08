2afd3e13a273184db27f3639066d4cf99bf0a0a3
aquawicket <aquawicket@hotmail.com>
2023-05-08 18:13:24 -0400

    commit from git

--- DKApps/DKDomTest/TEST_InputEvent.h
+++ DKApps/DKDomTest/TEST_InputEvent.h
@@ -1,5 +1,4 @@
-// [IDL] file:///C:/Users/Administrator/digitalknob/DK/3rdParty/webref-curated/ed/idlnames/InputEvent.idl
-// [SOURCE] https://w3c.github.io/uievents/#events-inputevents
+// [IDL] https://w3c.github.io/uievents/#idl-inputevent
 // [MDN] https://developer.mozilla.org/en-US/docs/Web/API/InputEvent
 #pragma once
 #ifndef TEST_InputEvent_H
--- DKApps/DKDomTest/TEST_Location.h
+++ DKApps/DKDomTest/TEST_Location.h
@@ -1,5 +1,4 @@
-// [IDL] file:///C:/Users/Administrator/digitalknob/DK/3rdParty/webref-curated/ed/idlnames/Location.idl
-// [SOURCE] https://html.spec.whatwg.org/multipage/nav-history-apis.html#the-location-interface
+// [IDL] https://html.spec.whatwg.org/multipage/nav-history-apis.html#the-location-interface
 // [MDN] https://developer.mozilla.org/en-US/docs/Web/API/Location
 #pragma once
 #ifndef TEST_Location_H
