10d55db3da1cb82bbb0ccad74cd9278f9a41faa3
aquawicket <aquawicket@hotmail.com>
2023-05-08 22:00:39 -0400

    commit from git

--- DKPlugins/DKDocument/DKDocumentDUK.h
+++ DKPlugins/DKDocument/DKDocumentDUK.h
@@ -51,60 +51,60 @@ public:
 		DKDuktape::AttachFunction("CPP_DKDocumentDUK_documentElement", DKDocumentDUK::documentElement);
 		
 		// HTMLCollection getElementsByTagName(DOMString qualifiedName);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_getElementsByTagName", DKDocumentDUK::getElementsByTagName);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_getElementsByTagName", DKDocumentDUK::getElementsByTagName);
 		
 		// HTMLCollection getElementsByTagNameNS(DOMString? namespace, DOMString localName);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_getElementsByTagNameNS", DKDocumentDUK::getElementsByTagNameNS);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_getElementsByTagNameNS", DKDocumentDUK::getElementsByTagNameNS);
 		
 		// HTMLCollection getElementsByClassName(DOMString classNames);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_getElementsByClassName", DKDocumentDUK::getElementsByClassName);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_getElementsByClassName", DKDocumentDUK::getElementsByClassName);
 		
 		// [CEReactions, NewObject] Element createElement(DOMString localName, optional (DOMString or ElementCreationOptions) options = {});
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createElement", DKDocumentDUK::createElement);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createElement", DKDocumentDUK::createElement);
 		
 		// [CEReactions, NewObject] Element createElementNS(DOMString? namespace, DOMString qualifiedName, optional (DOMString or ElementCreationOptions) options = {});
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createElementNS", DKDocumentDUK::createElementNS);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createElementNS", DKDocumentDUK::createElementNS);
 		
 		// [NewObject] DocumentFragment createDocumentFragment();
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createDocumentFragment", DKDocumentDUK::createDocumentFragment);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createDocumentFragment", DKDocumentDUK::createDocumentFragment);
 		
 		// [NewObject] Text createTextNode(DOMString data);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createTextNode", DKDocumentDUK::createTextNode);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createTextNode", DKDocumentDUK::createTextNode);
 		
 		// [NewObject] CDATASection createCDATASection(DOMString data);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createCDATASection", DKDocumentDUK::createCDATASection);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createCDATASection", DKDocumentDUK::createCDATASection);
 		
 		// [NewObject] Comment createComment(DOMString data);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createComment", DKDocumentDUK::createComment);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createComment", DKDocumentDUK::createComment);
 		
 		// [NewObject] ProcessingInstruction createProcessingInstruction(DOMString target, DOMString data);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createProcessingInstruction", DKDocumentDUK::createProcessingInstruction);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createProcessingInstruction", DKDocumentDUK::createProcessingInstruction);
 		
 		// [CEReactions, NewObject] Node importNode(Node node, optional boolean deep = false);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_importNode", DKDocumentDUK::importNode);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_importNode", DKDocumentDUK::importNode);
 		
 		// [CEReactions] Node adoptNode(Node node);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_adoptNode", DKDocumentDUK::adoptNode);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_adoptNode", DKDocumentDUK::adoptNode);
 		
 		// [NewObject] Attr createAttribute(DOMString localName);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createAttribute", DKDocumentDUK::createAttribute);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createAttribute", DKDocumentDUK::createAttribute);
 		
 		// [NewObject] Attr createAttributeNS(DOMString? namespace, DOMString qualifiedName);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createAttributeNS", DKDocumentDUK::createAttributeNS);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createAttributeNS", DKDocumentDUK::createAttributeNS);
 		
 		// [NewObject] Event createEvent(DOMString interface); // legacy
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createEvent", DKDocumentDUK::createEvent);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createEvent", DKDocumentDUK::createEvent);
 		
 		// [NewObject] Range createRange();
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createRange", DKDocumentDUK::createRange);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createRange", DKDocumentDUK::createRange);
 		
 		// NodeFilter.SHOW_ALL = 0xFFFFFFFF
 		
 		// [NewObject] NodeIterator createNodeIterator(Node root, optional unsigned long whatToShow = 0xFFFFFFFF, optional NodeFilter? filter = null);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createNodeIterator", DKDocumentDUK::createNodeIterator);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createNodeIterator", DKDocumentDUK::createNodeIterator);
 	
 		// [NewObject] TreeWalker createTreeWalker(Node root, optional unsigned long whatToShow = 0xFFFFFFFF, optional NodeFilter? filter = null);
-		DKDuktape::AttachFunction("CPP_DKDocumentDUK_createTreeWalker", DKDocumentDUK::createTreeWalker);
+		//DKDuktape::AttachFunction("CPP_DKDocumentDUK_createTreeWalker", DKDocumentDUK::createTreeWalker);
 		
 	// };
 	  
@@ -123,28 +123,28 @@ public:
 		// Source: Font Metrics API Level 1 (https://drafts.css-houdini.org/font-metrics-api-1/)
 		// partial interface Document {
 		//		FontMetrics measureElement(Element element);
-				DKDuktape::AttachFunction("CPP_DKDocumentDUK_measureElement", DKDocumentDUK::measureElement);
+				//DKDuktape::AttachFunction("CPP_DKDocumentDUK_measureElement", DKDocumentDUK::measureElement);
 		//
 		//		FontMetrics measureText(DOMString text, StylePropertyMapReadOnly styleMap);
-				DKDuktape::AttachFunction("CPP_DKDocumentDUK_measureText", DKDocumentDUK::measureText);
+				//DKDuktape::AttachFunction("CPP_DKDocumentDUK_measureText", DKDocumentDUK::measureText);
 		//};
 	
 		// Source: Fullscreen API Standard (https://fullscreen.spec.whatwg.org/)
 		// partial interface Document {
 		//		[LegacyLenientSetter] readonly attribute boolean fullscreenEnabled;
-				DKDuktape::AttachFunction("CPP_DKDocumentDUK_fullscreenEnabled", DKDocumentDUK::fullscreenEnabled);
+				//DKDuktape::AttachFunction("CPP_DKDocumentDUK_fullscreenEnabled", DKDocumentDUK::fullscreenEnabled);
 		//		
 		//		[LegacyLenientSetter, Unscopable] readonly attribute boolean fullscreen; // historical
-				DKDuktape::AttachFunction("CPP_DKDocumentDUK_fullscreen", DKDocumentDUK::fullscreen);
+				//DKDuktape::AttachFunction("CPP_DKDocumentDUK_fullscreen", DKDocumentDUK::fullscreen);
 		//
 		//		Promise<undefined> exitFullscreen();
-				DKDuktape::AttachFunction("CPP_DKDocumentDUK_exitFullscreen", DKDocumentDUK::exitFullscreen);
+				//DKDuktape::AttachFunction("CPP_DKDocumentDUK_exitFullscreen", DKDocumentDUK::exitFullscreen);
 		//
 		//		attribute EventHandler onfullscreenchange;
-				DKDuktape::AttachFunction("CPP_DKDocumentDUK_onfullscreenchange", DKDocumentDUK::onfullscreenchange);
+				//DKDuktape::AttachFunction("CPP_DKDocumentDUK_onfullscreenchange", DKDocumentDUK::onfullscreenchange);
 		//
 		//		attribute EventHandler onfullscreenerror;
-				DKDuktape::AttachFunction("CPP_DKDocumentDUK_onfullscreenerror", DKDocumentDUK::onfullscreenerror);
+				//DKDuktape::AttachFunction("CPP_DKDocumentDUK_onfullscreenerror", DKDocumentDUK::onfullscreenerror);
 		//};
 	
 	
