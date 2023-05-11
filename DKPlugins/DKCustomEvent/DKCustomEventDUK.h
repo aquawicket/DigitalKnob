a97bfd0db2e10ae4f7631773a09d515b9f999271
aquawicket <aquawicket@hotmail.com>
2023-05-11 12:45:24 -0400

    commit from git

--- DKPlugins/DKCustomEvent/DKCustomEventDUK.h
+++ DKPlugins/DKCustomEvent/DKCustomEventDUK.h
@@ -38,6 +38,31 @@ public:
 		DKString customEventAddress = duk_require_string(ctx, 0);
 		return (DKCustomEvent*)addressToPointer(customEventAddress);
 	}
+	static bool GetBool(duk_context* ctx, int index = 1){
+		if (duk_is_boolean(ctx, index))
+			return duk_to_boolean(ctx, index);
+		return false;
+	}
+	static double GetDouble(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_number(ctx, index);
+		return 0.0;
+	}
+	static int GetInt(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_int(ctx, index);
+		return 0;
+	}
+	static DKString GetString(duk_context* ctx, int index = 1){
+		if (duk_is_string(ctx, index))
+			return duk_to_string(ctx, index);
+		return "";
+	}
+	static unsigned int GetUint(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_uint(ctx, index);
+		return 0;
+	}
 	
 	// constructor(DOMString type, optional CustomEventInit eventInitDict = {});
 	static int constructor(duk_context* ctx){
