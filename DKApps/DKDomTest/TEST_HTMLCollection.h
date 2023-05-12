c95ce2a87838e210c125cca90bab10f62edf622f
aquawicket <aquawicket@hotmail.com>
2023-05-11 23:18:04 -0400

    commit from git

---
+++ DKApps/DKDomTest/TEST_HTMLCollection.h
@@ -0,0 +1,24 @@
+// [IDL] https://dom.spec.whatwg.org/#interface-htmlcollection
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
+#pragma once
+#ifndef TEST_HTMLCollection_H
+#define TEST_HTMLCollection_H
+
+#include "DK/DK.h"
+#include "DKHTMLCollection/DKHTMLCollection.h"
+
+
+class TEST_HTMLCollection : public DKObjectT<TEST_HTMLCollection>
+{
+public:
+	bool Init(){
+		DKINFO("////// TEST_HTMLCollection.h ////// \n");
+		
+		return DKTODO();
+	}
+
+};
+REGISTER_OBJECT(TEST_HTMLCollection, true);
+
+
+#endif //TEST_HTMLCollection_H
