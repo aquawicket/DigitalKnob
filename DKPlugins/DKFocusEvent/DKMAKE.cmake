025740706334cb623920ade5df74fa200d9a72bb
aquawicket <aquawicket@hotmail.com>
2023-05-11 21:26:22 -0400

    commit from git

--- DKPlugins/DKFocusEvent/DKMAKE.cmake
+++ DKPlugins/DKFocusEvent/DKMAKE.cmake
@@ -1,4 +1,5 @@
-dk_depend(DK)
+# [IDL] https://w3c.github.io/uievents/#idl-focusevent
+# [MDN] https://developer.mozilla.org/en-US/docs/Web/API/FocusEvent
 dk_depend(DKUIEvent)
 
 dk_generateCmake(DKFocusEvent)
--- DKPlugins/DKUIEvent/DKMAKE.cmake
+++ DKPlugins/DKUIEvent/DKMAKE.cmake
@@ -1,4 +1,6 @@
-dk_depend(DK)
+# [IDL] https://w3c.github.io/uievents/#idl-uievent
+# [MDN] https://developer.mozilla.org/en-US/docs/Web/API/UIEvent
 dk_depend(DKEvent)
+
 dk_generateCmake(DKUIEvent)
 dk_assets(DKUIEvent)
