0683861d148acbff492ea9da8e76fc0d4acefa6b
aquawicket <aquawicket@hotmail.com>
2023-05-11 23:14:46 -0400

    commit from git

---
+++ DKApps/DKDomTest/TEST_HTMLBodyElement.h
@@ -0,0 +1,24 @@
+// [IDL] https://html.spec.whatwg.org/multipage/sections.html#htmlbodyelement
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/HTMLBodyElement
+#pragma once
+#ifndef TEST_HTMLBodyElement_H
+#define TEST_HTMLBodyElement_H
+
+#include "DK/DK.h"
+#include "DKHTMLBodyElement/DKHTMLBodyElement.h"
+
+
+class TEST_HTMLBodyElement : public DKObjectT<TEST_HTMLBodyElement>
+{
+public:
+	bool Init(){
+		DKINFO("////// TEST_HTMLBodyElement.h ////// \n");
+		
+		return DKTODO();
+	}
+
+};
+REGISTER_OBJECT(TEST_HTMLBodyElement, true);
+
+
+#endif //TEST_HTMLBodyElement_H
