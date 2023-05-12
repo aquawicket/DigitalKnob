352102da926b40f2c176db4beef3b416e0eb3293
aquawicket <aquawicket@hotmail.com>
2023-05-11 21:45:08 -0400

    commit from git

--- DKPlugins/DKHTMLBodyElement/DKMAKE.cmake
+++ DKPlugins/DKHTMLBodyElement/DKMAKE.cmake
@@ -1,7 +1,7 @@
 # [IDL] https://html.spec.whatwg.org/multipage/sections.html#htmlbodyelement
 # [MDN] https://developer.mozilla.org/en-US/docs/Web/API/HTMLBodyElement
 
-dk_depend(HTMLElement)
+dk_depend(DKHTMLElement)
 
-dk_generateCmake(HTMLBodyElement)
-dk_assets(HTMLBodyElement)
+dk_generateCmake(DKHTMLBodyElement)
+dk_assets(DKHTMLBodyElement)
