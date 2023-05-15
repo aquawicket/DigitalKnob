571c3edfd71c4da5814c945e8e9d434e001d4151
aquawicket <aquawicket@hotmail.com>
2023-05-15 12:19:26 -0400

    commit from git

--- DKApps/DKDomTest/assets/TEST_Console.js
+++ DKApps/DKDomTest/assets/TEST_Console.js
@@ -1,30 +1,63 @@
-// [IDL] https://console.spec.whatwg.org/
+// [IDL] https://console.spec.whatwg.org/#console-namespace
 // [MDN] https://developer.mozilla.org/en-US/docs/Web/API/Console
 console.log("\n////// TEST_Console.js //////")
 
+//undefined assert(optional boolean condition = false, any... data);
+console.assert("console.assert() test")
 
-////// Instance methods //////
-// [console.assert()] https://developer.mozilla.org/en-US/docs/Web/API/console/assert
-console.assert("assert test")
-// [console.clear()] https://developer.mozilla.org/en-US/docs/Web/API/console/clear
-// [console.count()] https://developer.mozilla.org/en-US/docs/Web/API/console/count
-// [console.countReset()] https://developer.mozilla.org/en-US/docs/Web/API/console/countReset
-// [console.debug()] https://developer.mozilla.org/en-US/docs/Web/API/console/debug
-// [console.dir()] https://developer.mozilla.org/en-US/docs/Web/API/console/dir
-// [console.dirxml()] https://developer.mozilla.org/en-US/docs/Web/API/console/dirxml
-// [console.error()] https://developer.mozilla.org/en-US/docs/Web/API/console/error
-// [console.exception()](Non-standard)(Deprecated) https://developer.mozilla.org/en-US/docs/Web/API/console/exception
-// [console.group()] https://developer.mozilla.org/en-US/docs/Web/API/console/group
-// [console.groupCollapsed()] https://developer.mozilla.org/en-US/docs/Web/API/console/groupCollapsed
-// [console.groupEnd()] https://developer.mozilla.org/en-US/docs/Web/API/console/groupEnd
-// [console.info()] https://developer.mozilla.org/en-US/docs/Web/API/console/info
-// [console.log()] https://developer.mozilla.org/en-US/docs/Web/API/console/log
-// [console.profile()](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/console/profile
-// [console.profileEnd()](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/console/profileEnd
-// [console.table()] https://developer.mozilla.org/en-US/docs/Web/API/console/table
-// [console.time()] https://developer.mozilla.org/en-US/docs/Web/API/console/time
-// [console.timeEnd()] https://developer.mozilla.org/en-US/docs/Web/API/console/timeEnd
-// [console.timeLog()] https://developer.mozilla.org/en-US/docs/Web/API/console/timeLog
-// [console.timeStamp()](Non-standard) https://developer.mozilla.org/en-US/docs/Web/API/console/timeStamp
-// [console.trace() https://developer.mozilla.org/en-US/docs/Web/API/console/trace
-// [console.warn() https://developer.mozilla.org/en-US/docs/Web/API/console/warn
+//undefined clear();
+//console.clear()
+
+//undefined debug(any... data);
+console.debug("console.debug() test")
+
+//undefined error(any... data);
+console.error("console.error() test")
+
+//undefined info(any... data);
+console.info("console.info() test")
+
+//undefined log(any... data);
+console.log("console.log() test")
+
+//undefined table(optional any tabularData, optional sequence<DOMString> properties);
+//console.table() //TODO
+
+//undefined trace(any... data);
+console.trace("console.trace() test")
+
+//undefined warn(any... data);
+console.warn("console.warn() test")
+
+//undefined dir(optional any item, optional object? options);
+//console.dir() //TODO
+
+//undefined dirxml(any... data);
+console.dirxml("console.dirxml() test")
+
+// Counting
+//undefined count(optional DOMString label = "default");
+console.count()
+
+//undefined countReset(optional DOMString label = "default");
+console.countReset()
+
+// Grouping
+//undefined group(any... data);
+console.group("console.group() test")
+
+//undefined groupCollapsed(any... data);
+console.groupCollapsed("console.groupCollapsed() test")
+
+//undefined groupEnd();
+console.groupEnd()
+
+// Timing
+//undefined time(optional DOMString label = "default");
+console.time()
+
+//undefined timeLog(optional DOMString label = "default", any... data);
+console.timeLog("default", "console.timeLog() test")
+
+//undefined timeEnd(optional DOMString label = "default");
+console.timeEnd()
