3e96730b763f5633cca4c6ffd565bb92565b3665
aquawicket <aquawicket@hotmail.com>
2023-05-15 14:19:45 -0400

    commit from git

--- DKPlugins/DKConsole/DKConsoleDUK.js
+++ DKPlugins/DKConsole/DKConsoleDUK.js
@@ -1,7 +1,7 @@
 // [IDL] https://console.spec.whatwg.org/#console-namespace
 // [MDN] https://developer.mozilla.org/en-US/docs/Web/API/Console
 
-
+/*
 const Console_HandleColor = function Console_HandleColor(data){
 	//TODO: handle coloring, (currently disabled)
 	if(data){
@@ -10,6 +10,7 @@ const Console_HandleColor = function Console_HandleColor(data){
 	}
 	return data;
 }
+*/
 
 //[Exposed=*]
 //namespace console { // but see namespace object requirements below
@@ -17,9 +18,8 @@ var Console = function Console(address) {
 	
 	// Logging
 	//undefined assert(optional boolean condition = false, any... data);
-    Console.prototype.assert = function assert(data) {
-		data = Console_HandleColor(data)
-        CPP_DKConsoleDUK_assert(data);
+    Console.prototype.assert = function assert(condition, data) {
+        CPP_DKConsoleDUK_assert(condition, data);
     }
 	
 	//undefined clear();	
@@ -29,13 +29,11 @@ var Console = function Console(address) {
 	
 	//undefined debug(any... data);
 	Console.prototype.debug = function debug(data) {
-		data = Console_HandleColor(data)
         CPP_DKConsoleDUK_debug(data);
     }
 	
 	//undefined error(any... data);
 	Console.prototype.error = function error(data) {
-		data = Console_HandleColor(data)
 		if(!data){
 			CPP_DKConsoleDUK_error("data invalid \n");
 			return;
@@ -55,64 +53,58 @@ var Console = function Console(address) {
 	
 	//undefined info(any... data);
 	Console.prototype.info = function info(data) {
-		data = Console_HandleColor(data)
         CPP_DKConsoleDUK_info(data);
     }
 	
 	//undefined log(any... data);
 	Console.prototype.log = function log(data) {
-		data = Console_HandleColor(data)
         CPP_DKConsoleDUK_log(data);
     }
 	
 	//undefined table(optional any tabularData, optional sequence<DOMString> properties);
-	Console.prototype.table = function table() {
-        //TODO
+	Console.prototype.table = function table(tabularData, properties) {
+        CPP_DKConsoleDUK_table(tabularData, properties);
     }
 	
 	//undefined trace(any... data);
 	Console.prototype.trace = function trace(data) {
-		data = Console_HandleColor(data)
 		CPP_DKConsoleDUK_trace(data);
     }
 	
 	//undefined warn(any... data);
 	Console.prototype.warn = function warn(data) {
-		data = Console_HandleColor(data)
         CPP_DKConsoleDUK_warn(data);
     }
 	
 	//undefined dir(optional any item, optional object? options);
-	Console.prototype.dir = function dir() {
-        //TODO
+	Console.prototype.dir = function dir(item, options) {
+        CPP_DKConsoleDUK_dir(item, options);
     }
 	
 	//undefined dirxml(any... data);
-	Console.prototype.dirxml = function dirxml() {
-        //TODO
+	Console.prototype.dirxml = function dirxml(data) {
+        CPP_DKConsoleDUK_dirxml(data);
     }
 	
 	// Counting
 	//undefined count(optional DOMString label = "default");
-	Console.prototype.count = function count() {
-        //TODO
+	Console.prototype.count = function count(label) {
+        CPP_DKConsoleDUK_count(label);
     }
 	
 	//undefined countReset(optional DOMString label = "default");
-	Console.prototype.countReset = function countReset() {
-        //TODO
+	Console.prototype.countReset = function countReset(label) {
+        CPP_DKConsoleDUK_dirxml(label);
     }
    
 	// Grouping
 	//undefined group(any... data);
 	Console.prototype.group = function group(data) {
-		data = Console_HandleColor(data)
         CPP_DKConsoleDUK_group(data);
     }
 	
 	//undefined groupCollapsed(any... data);
 	Console.prototype.groupCollapsed = function groupCollapsed(data) {
-		data = Console_HandleColor(data)
         CPP_DKConsoleDUK_groupCollapsed(data);
     }
 	
@@ -123,18 +115,18 @@ var Console = function Console(address) {
 	
 	// Timing
 	//undefined time(optional DOMString label = "default");
-	Console.prototype.time = function time() {
-        //TODO
+	Console.prototype.time = function time(label) {
+        CPP_DKConsoleDUK_time(label);
     }
 	
 	//undefined timeLog(optional DOMString label = "default", any... data);
-	Console.prototype.timeLog = function timeLog() {
-        //TODO
+	Console.prototype.timeLog = function timeLog(label, data) {
+        CPP_DKConsoleDUK_timeLog(label, data);
     }
 	
 	//undefined timeEnd(optional DOMString label = "default");
-	Console.prototype.timeEnd = function timeEnd() {
-        //TODO
+	Console.prototype.timeEnd = function timeEnd(label) {
+        CPP_DKConsoleDUK_timeEnd(label);
     }
 	   
 	
