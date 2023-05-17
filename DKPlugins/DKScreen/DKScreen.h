790378b87108e680f90c6569134f21895e7710b0
aquawicket <aquawicket@hotmail.com>
2023-05-17 14:33:20 -0400

    commit from git

--- DKPlugins/DKScreen/DKScreen.h
+++ DKPlugins/DKScreen/DKScreen.h
@@ -26,22 +26,99 @@ public:
 	//virtual bool availHeight(int&, bool) { return false; }
 	
 	// readonly attribute long width;
-	//virtual bool width(int&, bool) { return false; }
-	virtual int width() { 
+	virtual int width() { // getter
 		DKDEBUGFUNC();
-		#if WIN
+		#if ANDROID
+			// https://stackoverflow.com/a/12134363/688352
+			//#include <sys/ioctl.h>
+			//#include <linux/fb.h>
+			//struct fb_var_screeninfo fb_var;
+			//int fd = open("/dev/graphics/fb0", O_RDONLY);
+			//ioctl(fd, FBIOGET_VSCREENINFO, &fb_var);
+			//close(fd);
+			#if HAVE_sdl
+				SDL_DisplayMode sdl_displayMode;
+				SDL_GetCurrentDisplayMode(0, &sdl_displayMode);
+				return sdl_displayMode.w;
+			#else 
+				DKERROR("sdl unavailable! \n");
+				return 0;
+			#endif
+		#elif EMSCRIPTEN
+			DKWARN("Screen.width not implemented!");
+			return 0;
+		#elif LINUX
+			#if HAVE_libx11_dev
+				Display* d = XOpenDisplay(NULL);
+				Screen* s = DefaultScreenOfDisplay(d);
+				return s->width;
+			#else
+				return DKERROR("!HAVE_libx11_dev");
+			#endif
+		#elif MAC
+			CGRect mainMonitor = CGDisplayBounds(CGMainDisplayID());
+			CGFloat monitorWidth = CGRectGetWidth(mainMonitor);
+			return monitorWidth;
+		#elif WIN
 			RECT desktop;
 			const HWND hDesktop = GetDesktopWindow();
 			if(!GetWindowRect(hDesktop, &desktop))
 				DKERROR("GetWindowRect() failed\n");
 			return desktop.right;
+		#else
+			DKWARN("Screen.width not implemented!");
+			return 0;
 		#endif
-		return 0;
 	}
-	virtual int width(const int&) { return 0; }
+	virtual int width(const int&) { return 0; } // setter
 	
 	// readonly attribute long height;
-	virtual bool height(int&, bool) { return false; }
+	virtual bool height(int&, bool) { // getter
+		DKDEBUGFUNC(h);
+		#if ANDROID
+			// https://stackoverflow.com/a/12134363/688352
+			//#include <sys/ioctl.h>
+			//#include <linux/fb.h>
+			//struct fb_var_screeninfo fb_var;
+			//int fd = open("/dev/graphics/fb0", O_RDONLY);
+			//ioctl(fd, FBIOGET_VSCREENINFO, &fb_var);
+			//close(fd);
+			#if HAVE_sdl
+				SDL_DisplayMode sdl_displayMode;
+				SDL_GetCurrentDisplayMode(0, &sdl_displayMode);
+				return sdl_displayMode.h;
+			#else
+				DKERROR("sdl unavailable! \n");
+				return 0;
+			#endif
+		#elif EMSCRIPTEN
+			DKWARN("Screen.height not implemented!");
+			return 0;
+		#elif LINUX
+			#if HAVE_libx11_dev
+				Display* d = XOpenDisplay(NULL);
+				Screen* s = DefaultScreenOfDisplay(d);
+				return s->height;
+			#else
+				DKERROR("!HAVE_libx11_dev");
+				return 0;
+			#endif
+		#elif MAC
+			CGRect mainMonitor = CGDisplayBounds(CGMainDisplayID());
+			CGFloat monitorHeight = CGRectGetHeight(mainMonitor);
+			return monitorHeight;
+		#elif WIN
+			RECT desktop;
+			const HWND hDesktop = GetDesktopWindow();
+			if(!GetWindowRect(hDesktop, &desktop))
+				DKERROR("GetWindowRect() failed\n");
+			return desktop.bottom;
+		#else
+			DKWARN("Screen.height not implemented!");
+			return 0;
+		#endif
+	}
+	virtual int height(const int&) { return 0; } // setter
 	
 	// readonly attribute unsigned long colorDepth;
 	//virtual bool colorDepth(int&, bool) { return false; }
