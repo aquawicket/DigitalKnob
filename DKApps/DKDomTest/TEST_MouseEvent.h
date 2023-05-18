81c608c6a9b93d0ca2fd1970b2222acfc4fce4c1
aquawicket <aquawicket@hotmail.com>
2023-05-18 15:03:33 -0400

    commit from git

--- DKApps/DKDomTest/TEST_MouseEvent.h
+++ DKApps/DKDomTest/TEST_MouseEvent.h
@@ -23,25 +23,90 @@ public:
 
 	static void printMouseEventProperties(DKMouseEvent& mouseEvent){
 		DKDEBUGFUNC(mouseEvent);
-		DKINFO("mouseEvent.altKey = "			+toString(mouseEvent.altKey)		+"\n");
-		DKINFO("mouseEvent.button = "			+toString(mouseEvent.button)		+"\n");
-		DKINFO("mouseEvent.buttons = "			+toString(mouseEvent.buttons)		+"\n");
-		DKINFO("mouseEvent.clientX = "			+toString(mouseEvent.clientX)		+"\n");
-		DKINFO("mouseEvent.clientY = "			+toString(mouseEvent.clientY)		+"\n");
-		DKINFO("mouseEvent.ctrlKey = "			+toString(mouseEvent.ctrlKey)		+"\n");
-		DKINFO("mouseEvent.metaKey = "			+toString(mouseEvent.metaKey)		+"\n");
-		DKINFO("mouseEvent.movementX = "		+toString(mouseEvent.movementX)		+"\n");
-		DKINFO("mouseEvent.movementY = "		+toString(mouseEvent.movementY)		+"\n");
-		DKINFO("mouseEvent.offsetX = "			+toString(mouseEvent.offsetX)		+"\n");
-		DKINFO("mouseEvent.offsetY = "			+toString(mouseEvent.offsetY)		+"\n");
-		DKINFO("mouseEvent.pageX = "			+toString(mouseEvent.pageX)			+"\n");
-		DKINFO("mouseEvent.pageY = "			+toString(mouseEvent.pageY)			+"\n");
-		DKINFO("mouseEvent.relatedTarget = "	+toString(mouseEvent.relatedTarget)	+"\n");
+		
+		// constructor(DOMString type, optional MouseEventInit eventInitDict = {});
+		// function
+		
+		// readonly attribute long screenX;
 		DKINFO("mouseEvent.screenX = "			+toString(mouseEvent.screenX)		+"\n");
+		
+		// readonly attribute long screenY;
 		DKINFO("mouseEvent.screenY = "			+toString(mouseEvent.screenY)		+"\n");
+		
+		// readonly attribute long clientX;
+		DKINFO("mouseEvent.clientX = "			+toString(mouseEvent.clientX)		+"\n");
+		
+		// readonly attribute long clientY;
+		DKINFO("mouseEvent.clientY = "			+toString(mouseEvent.clientY)		+"\n");
+		
+		// readonly attribute boolean ctrlKey;
+		DKINFO("mouseEvent.ctrlKey = "			+toString(mouseEvent.ctrlKey)		+"\n");
+		
+		// readonly attribute boolean shiftKey;
 		DKINFO("mouseEvent.shiftKey = "			+toString(mouseEvent.shiftKey)		+"\n");
-		DKINFO("mouseEvent.x = "				+toString(mouseEvent.x)				+"\n");
-		DKINFO("mouseEvent.y = "				+toString(mouseEvent.y)				+"\n");
+		
+		// readonly attribute boolean altKey;
+		DKINFO("mouseEvent.altKey = "			+toString(mouseEvent.altKey)		+"\n");
+		
+		// readonly attribute boolean metaKey;
+		DKINFO("mouseEvent.metaKey = "			+toString(mouseEvent.metaKey)		+"\n");
+		
+		// readonly attribute short button;
+		DKINFO("mouseEvent.button = "			+toString(mouseEvent.button)		+"\n");
+		
+		// readonly attribute unsigned short buttons;		
+		DKINFO("mouseEvent.buttons = "			+toString(mouseEvent.buttons)		+"\n");
+		
+		// readonly attribute EventTarget? relatedTarget;
+		DKINFO("mouseEvent.relatedTarget = "	+toString(mouseEvent.relatedTarget)	+"\n");
+		
+		// boolean getModifierState(DOMString keyArg);
+		// function
+		
+		// Source: CSSOM View Module (https://www.w3.org/TR/cssom-view-1/)
+		// partial interface MouseEvent {
+		//  	readonly attribute double pageX;
+				DKINFO("mouseEvent.pageX = "			+toString(mouseEvent.pageX)			+"\n");
+		//  	readonly attribute double pageY;
+				DKINFO("mouseEvent.pageY = "			+toString(mouseEvent.pageY)			+"\n");
+		//  	readonly attribute double x;
+				DKINFO("mouseEvent.x = "				+toString(mouseEvent.x)				+"\n");
+		//  	readonly attribute double y;
+				DKINFO("mouseEvent.y = "				+toString(mouseEvent.y)				+"\n");
+		//  	readonly attribute double offsetX; 
+				DKINFO("mouseEvent.offsetX = "			+toString(mouseEvent.offsetX)		+"\n");
+		//  	readonly attribute double offsetY;
+				DKINFO("mouseEvent.offsetY = "			+toString(mouseEvent.offsetY)		+"\n");
+		// };
+	
+		// Source: Pointer Lock 2.0 (https://www.w3.org/TR/pointerlock-2/)
+		// partial interface MouseEvent {
+		//   	readonly attribute double movementX;
+				DKINFO("mouseEvent.movementX = "		+toString(mouseEvent.movementX)		+"\n");
+		//   	readonly attribute double movementY;
+				DKINFO("mouseEvent.movementY = "		+toString(mouseEvent.movementY)		+"\n");
+		// };
+	
+		// Source: UI Events (https://www.w3.org/TR/uievents/)
+		// partial interface MouseEvent {
+		//		Deprecated in this specification
+		//	  	undefined initMouseEvent(DOMString typeArg,
+		//		optional boolean bubblesArg = false,
+		//		optional boolean cancelableArg = false,
+		//		optional Window? viewArg = null,
+		//		optional long detailArg = 0,
+		//		optional long screenXArg = 0,
+		//		optional long screenYArg = 0,
+		//		optional long clientXArg = 0,
+		//		optional long clientYArg = 0,
+		//		optional boolean ctrlKeyArg = false,
+		//		optional boolean altKeyArg = false,
+		//		optional boolean shiftKeyArg = false,
+		//		optional boolean metaKeyArg = false,
+		//		optional short buttonArg = 0,
+		//		optional EventTarget? relatedTargetArg = null);
+				// function
+		// };
 	}
 
 	static bool onMouseEvent(DKEvent& event){
--- DKApps/DKDomTest/assets/TEST_MouseEvent.js
+++ DKApps/DKDomTest/assets/TEST_MouseEvent.js
@@ -4,23 +4,88 @@ console.log("\n////// TEST_MouseEvent.js //////")
 
 
 function printMouseEventProperties(mouseevent){
-	console.log("mouseevent.altKey = "			+mouseevent.altKey)
-	console.log("mouseevent.button = "			+mouseevent.button)
-	console.log("mouseevent.buttons = "			+mouseevent.buttons)
-	console.log("mouseevent.clientX = "			+mouseevent.clientX)
-	console.log("mouseevent.clientY = "			+mouseevent.clientY)
-	console.log("mouseevent.ctrlKey = "			+mouseevent.ctrlKey)
-	console.log("mouseevent.metaKey = "			+mouseevent.metaKey)
-	console.log("mouseevent.movementX = "		+mouseevent.movementX)
-	console.log("mouseevent.movementY = "		+mouseevent.movementY)
-	console.log("mouseevent.offsetX = "			+mouseevent.offsetX)
-	console.log("mouseevent.offsetY = "			+mouseevent.offsetY)
-	console.log("mouseevent.pageX = "			+mouseevent.pageX)
-	console.log("mouseevent.pageY = "			+mouseevent.pageY)
-	console.log("mouseevent.relatedTarget = "	+mouseevent.relatedTarget)
+	
+	// constructor(DOMString type, optional MouseEventInit eventInitDict = {});
+	// function
+		
+	// readonly attribute long screenX;
 	console.log("mouseevent.screenX = "			+mouseevent.screenX)
+	
+	// readonly attribute long screenY;
 	console.log("mouseevent.screenY = "			+mouseevent.screenY)
+	
+	// readonly attribute long clientX;
+	console.log("mouseevent.clientX = "			+mouseevent.clientX)
+	
+	// readonly attribute long clientY;
+	console.log("mouseevent.clientY = "			+mouseevent.clientY)
+	
+	// readonly attribute boolean ctrlKey;
+	console.log("mouseevent.ctrlKey = "			+mouseevent.ctrlKey)
+	
+	// readonly attribute boolean shiftKey;
 	console.log("mouseevent.shiftKey = "		+mouseevent.shiftKey)
-	console.log("mouseevent.x = "				+mouseevent.x)
-	console.log("mouseevent.y = "				+mouseevent.y)
+	
+	// readonly attribute boolean altKey;	
+	console.log("mouseevent.altKey = "			+mouseevent.altKey)
+	
+	// readonly attribute boolean metaKey;
+	console.log("mouseevent.metaKey = "			+mouseevent.metaKey)
+	
+	// readonly attribute short button;
+	console.log("mouseevent.button = "			+mouseevent.button)
+	
+	// readonly attribute unsigned short buttons;	
+	console.log("mouseevent.buttons = "			+mouseevent.buttons)
+	
+	// readonly attribute EventTarget? relatedTarget;
+	console.log("mouseevent.relatedTarget = "	+mouseevent.relatedTarget)
+	
+	// boolean getModifierState(DOMString keyArg);
+	// function
+	
+	// Source: CSSOM View Module (https://www.w3.org/TR/cssom-view-1/)
+	// partial interface MouseEvent {
+	//  	readonly attribute double pageX;
+			console.log("mouseevent.pageX = "			+mouseevent.pageX)
+	//  	readonly attribute double pageY;
+			console.log("mouseevent.pageY = "			+mouseevent.pageY)
+	//  	readonly attribute double x;
+			console.log("mouseevent.x = "				+mouseevent.x)
+	//  	readonly attribute double y;
+			console.log("mouseevent.y = "				+mouseevent.y)
+	//  	readonly attribute double offsetX; 
+			console.log("mouseevent.offsetX = "			+mouseevent.offsetX)
+	//  	readonly attribute double offsetY;
+			console.log("mouseevent.offsetY = "			+mouseevent.offsetY)
+	// };
+	
+	// Source: Pointer Lock 2.0 (https://www.w3.org/TR/pointerlock-2/)
+	// partial interface MouseEvent {
+	//   	readonly attribute double movementX;
+			console.log("mouseevent.movementX = "		+mouseevent.movementX)
+	//   	readonly attribute double movementY;
+			console.log("mouseevent.movementY = "		+mouseevent.movementY)
+	// };
+	
+	// Source: UI Events (https://www.w3.org/TR/uievents/)
+	// partial interface MouseEvent {
+	//		Deprecated in this specification
+	//	  	undefined initMouseEvent(DOMString typeArg,
+	//		optional boolean bubblesArg = false,
+	//		optional boolean cancelableArg = false,
+	//		optional Window? viewArg = null,
+	//		optional long detailArg = 0,
+	//		optional long screenXArg = 0,
+	//		optional long screenYArg = 0,
+	//		optional long clientXArg = 0,
+	//		optional long clientYArg = 0,
+	//		optional boolean ctrlKeyArg = false,
+	//		optional boolean altKeyArg = false,
+	//		optional boolean shiftKeyArg = false,
+	//		optional boolean metaKeyArg = false,
+	//		optional short buttonArg = 0,
+	//		optional EventTarget? relatedTargetArg = null);
+			// function
+	// };
 }
