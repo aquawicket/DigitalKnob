dfebb1ad149fa9153b3758c21d3abacafd7c7cad
aquawicket <aquawicket@hotmail.com>
2023-05-20 22:04:46 -0400

    commit from git

--- DKPlugins/DKHTMLCollection/DKHTMLCollectionDUK.h
+++ DKPlugins/DKHTMLCollection/DKHTMLCollectionDUK.h
@@ -37,6 +37,31 @@ public:
 		DKString eventTargetAddress = duk_require_string(ctx, 0);
 		return (DKHTMLCollection*)addressToPointer(eventTargetAddress);
 	}
+	static bool GetBool(duk_context* ctx){
+		if (duk_is_boolean(ctx, 1))
+			return duk_to_boolean(ctx, 1);
+		return false;
+	}
+	static double GetDouble(duk_context* ctx){
+		if (duk_is_number(ctx, 1))
+			return duk_to_number(ctx, 1);
+		return 0.0;
+	}
+	static int GetInt(duk_context* ctx){
+		if (duk_is_number(ctx, 1))
+			return duk_to_int(ctx, 1);
+		return 0;
+	}
+	static DKString GetString(duk_context* ctx){
+		if (duk_is_string(ctx, 1))
+			return duk_to_string(ctx, 1);
+		return "";
+	}
+	static unsigned int GetUint(duk_context* ctx){
+		if (duk_is_number(ctx, 1))
+			return duk_to_uint(ctx, 1);
+		return 0;
+	}
 
 	static int constructor(duk_context* ctx){
 		DKDEBUGFUNC(ctx);
