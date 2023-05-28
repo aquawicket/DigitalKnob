890b6cd94352c44e87084797b34f610df586dff8
aquawicket <aquawicket@hotmail.com>
2023-05-28 19:58:54 -0400

    commit from git

--- DKApps/DKDomTest/assets/TEST_HTMLBodyElement.js
+++ DKApps/DKDomTest/assets/TEST_HTMLBodyElement.js
@@ -5,7 +5,30 @@ console.log("\n////// TEST_HTMLBodyElement.js //////")
 
 function printHTMLBodyElementProperties(htmlBodyElement){
 		
-	// TODO
+	// [HTMLConstructor] constructor();
+	console.log("htmlBodyElement = "						+htmlBodyElement);
+
+	// attribute EventHandler onorientationchange;
+	console.log("htmlBodyElement.onorientationchange = "	+htmlBodyElement.onorientationchange);
+	
+	// [CEReactions] attribute [LegacyNullToEmptyString] DOMString text;
+	console.log("htmlBodyElement.text = "					+htmlBodyElement.text);
+	
+	// [CEReactions] attribute [LegacyNullToEmptyString] DOMString link;
+	console.log("htmlBodyElement.link = "					+htmlBodyElement.link);
+	
+	// [CEReactions] attribute [LegacyNullToEmptyString] DOMString vLink;
+	console.log("htmlBodyElement.vLink = "					+htmlBodyElement.vLink);
+	
+	// [CEReactions] attribute [LegacyNullToEmptyString] DOMString aLink;
+	console.log("htmlBodyElement.aLink = "					+htmlBodyElement.aLink);
+	
+	// [CEReactions] attribute [LegacyNullToEmptyString] DOMString bgColor;
+	console.log("htmlBodyElement.bgColor = "				+htmlBodyElement.bgColor);
+	
+	// [CEReactions] attribute DOMString background;
+	console.log("htmlBodyElement.background = "				+htmlBodyElement.background);
+  
 	
 	printHTMLElementProperties(htmlBodyElement);
 }
