8c3a31003714319c737a8db29fc7f36018a2f669
aquawicket <aquawicket@hotmail.com>
2023-05-28 19:02:19 -0400

    commit from git

--- DKPlugins/DKHTMLElement/DKHTMLElementDUK.h
+++ DKPlugins/DKHTMLElement/DKHTMLElementDUK.h
@@ -16,67 +16,67 @@ public:
 	bool Init(){
 		
 		// [HTMLConstructor] constructor();
-		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK", DKHTMLElementDUK::constructor);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK", 					DKHTMLElementDUK::constructor);
 		
 		// metadata attributes
 		// [CEReactions] attribute DOMString title;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_title", DKHTMLElementDUK::title);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_title", 			DKHTMLElementDUK::title);
 
 		// [CEReactions] attribute DOMString lang;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_title", DKHTMLElementDUK::title);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_title", 			DKHTMLElementDUK::title);
 		
 		// [CEReactions] attribute boolean translate;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_translate", DKHTMLElementDUK::translate);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_translate", 		DKHTMLElementDUK::translate);
 		
 		// [CEReactions] attribute DOMString dir;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_dir", DKHTMLElementDUK::dir);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_dir",				DKHTMLElementDUK::dir);
 		
 		// user interaction
 		// [CEReactions] attribute (boolean or unrestricted double or DOMString)? hidden;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_hidden", DKHTMLElementDUK::hidden);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_hidden", 			DKHTMLElementDUK::hidden);
 	
 		// [CEReactions] attribute boolean inert;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_inert", DKHTMLElementDUK::inert);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_inert",				DKHTMLElementDUK::inert);
 		
 		// undefined click();
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_click", DKHTMLElementDUK::click);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_click", 			DKHTMLElementDUK::click);
 		
 		// [CEReactions] attribute DOMString accessKey;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_accessKey", DKHTMLElementDUK::accessKey);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_accessKey", 		DKHTMLElementDUK::accessKey);
 		
 		// readonly attribute DOMString accessKeyLabel;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_accessKeyLabel", DKHTMLElementDUK::accessKeyLabel);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_accessKeyLabel", 	DKHTMLElementDUK::accessKeyLabel);
 		
 		// [CEReactions] attribute boolean draggable;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_draggable", DKHTMLElementDUK::draggable);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_draggable", 		DKHTMLElementDUK::draggable);
 		
 		// [CEReactions] attribute boolean spellcheck;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_spellcheck", DKHTMLElementDUK::spellcheck);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_spellcheck", 		DKHTMLElementDUK::spellcheck);
 		
 		// [CEReactions] attribute DOMString autocapitalize;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_autocapitalize", DKHTMLElementDUK::autocapitalize);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_autocapitalize", 	DKHTMLElementDUK::autocapitalize);
 		
 		// [CEReactions] attribute [LegacyNullToEmptyString] DOMString innerText;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_innerText", DKHTMLElementDUK::innerText);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_innerText", 		DKHTMLElementDUK::innerText);
 		
 		// [CEReactions] attribute [LegacyNullToEmptyString] DOMString outerText;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_outerText", DKHTMLElementDUK::outerText);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_outerText", 		DKHTMLElementDUK::outerText);
 		
 		// ElementInternals attachInternals();
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_attachInternals", DKHTMLElementDUK::attachInternals);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_attachInternals", 	DKHTMLElementDUK::attachInternals);
 		
 		// The popover API
 		// undefined showPopover();
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_showPopover", DKHTMLElementDUK::showPopover);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_showPopover", 		DKHTMLElementDUK::showPopover);
 		
 		// undefined hidePopover();
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_hidePopover", DKHTMLElementDUK::hidePopover);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_hidePopover", 		DKHTMLElementDUK::hidePopover);
 		
 		// undefined togglePopover(optional boolean force);
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_togglePopover", DKHTMLElementDUK::togglePopover);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_togglePopover", 	DKHTMLElementDUK::togglePopover);
 		
 		// [CEReactions] attribute DOMString? popover;
-		//DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_popover", DKHTMLElementDUK::popover);
+		DKDuktape::AttachFunction("CPP_DKHTMLElementDUK_popover", 			DKHTMLElementDUK::popover);
 	//};
 	
 		// HTMLElement includes GlobalEventHandlers;
@@ -99,7 +99,32 @@ public:
 		DKString eventTargetAddress = duk_require_string(ctx, 0);
 		return (DKHTMLElement*)addressToPointer(eventTargetAddress);
 	}
-
+	static bool GetBool(duk_context* ctx, int index = 1){
+		if (duk_is_boolean(ctx, index))
+			return duk_to_boolean(ctx, index);
+		return false;
+	}
+	static double GetDouble(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_number(ctx, index);
+		return 0.0;
+	}
+	static int GetInt(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_int(ctx, index);
+		return 0;
+	}
+	static DKString GetString(duk_context* ctx, int index = 1){
+		if (duk_is_string(ctx, index))
+			return duk_to_string(ctx, index);
+		return "";
+	}
+	static unsigned int GetUint(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_uint(ctx, index);
+		return 0;
+	}
+	
 	// [HTMLConstructor] constructor();
 	static int constructor(duk_context* ctx){
 		DKDEBUGFUNC(ctx);
@@ -112,63 +137,162 @@ public:
 	
 	// metadata attributes
 	// [CEReactions] attribute DOMString title;
-	// TODO
+	static int title(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->title(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->title());
+		return true;
+	}
 	
 	// [CEReactions] attribute DOMString lang;
-	// TODO
+	static int lang(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->lang(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->lang());
+		return true;
+	}
 	
 	// [CEReactions] attribute boolean translate;
-	// TODO
+	static int translate(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->translate(GetBool(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->translate());
+		return true;
+	}
 	
 	// [CEReactions] attribute DOMString dir;
-	// TODO
+	static int dir(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->dir(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->dir());
+		return true;
+	}
 	
 	// user interaction
 	// [CEReactions] attribute (boolean or unrestricted double or DOMString)? hidden;
-	// TODO
+	static int hidden(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->hidden(GetBool(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->hidden());
+		return true;
+	}
 	
 	// [CEReactions] attribute boolean inert;
-	// TODO
+	static int hidden(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->inert(GetBool(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->inert());
+		return true;
+	}
 	
 	// undefined click();
-	// TODO
+	static int click(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
 	
 	// [CEReactions] attribute DOMString accessKey;
-	// TODO
+	static int accessKey(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->accessKey(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->accessKey());
+		return true;
+	}
 	
 	// readonly attribute DOMString accessKeyLabel;
-	// TODO
+	static int accessKey(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->accessKeyLabel(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->accessKeyLabel());
+		return true;
+	}
 	
 	// [CEReactions] attribute boolean draggable;
-	// TODO
+	static int draggable(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->draggable(GetBool(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->draggable());
+		return true;
+	}
 	
 	// [CEReactions] attribute boolean spellcheck;
-	// TODO
+	static int spellcheck(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->spellcheck(GetBool(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->spellcheck());
+		return true;
+	}
 	
 	// [CEReactions] attribute DOMString autocapitalize;
-	// TODO
+	static int autocapitalize(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->autocapitalize(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->autocapitalize());
+		return true;
+	}
 	
 	// [CEReactions] attribute [LegacyNullToEmptyString] DOMString innerText;
-	// TODO
+	static int innerText(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->innerText(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->innerText());
+		return true;
+	}
 	
 	// [CEReactions] attribute [LegacyNullToEmptyString] DOMString outerText;
-	// TODO
+	static int outerText(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->outerText(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->outerText());
+		return true;
+	}
 	
 	// ElementInternals attachInternals();
-	// TODO
+	static int attachInternals(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
 	
 	// The popover API
 	// undefined showPopover();
-	// TODO
+	static int showPopover(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
 	
 	// undefined hidePopover();
-	// TODO
+	static int hidePopover(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
 	
 	// undefined togglePopover(optional boolean force);
-	// TODO
+	static int togglePopover(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
 	
 	// [CEReactions] attribute DOMString? popover;
-	// TODO
+	static int popover(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			eventTarget(ctx)->popover(GetString(ctx));
+		dukglue_push(ctx, eventTarget(ctx)->popover());
+		return true;
+	}
 	
 //};
 
