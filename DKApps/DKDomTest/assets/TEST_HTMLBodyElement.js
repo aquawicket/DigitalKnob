ffdf6c494506029c3c20c3f2994adeac0f493e6b
aquawicket <aquawicket@hotmail.com>
2023-05-27 22:15:29 -0400

    commit from git

---
+++ DKApps/DKDomTest/assets/TEST_HTMLBodyElement.js
@@ -0,0 +1,15 @@
+// [IDL] https://html.spec.whatwg.org/multipage/sections.html#htmlbodyelement
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/HTMLBodyElement
+console.log("\n////// TEST_HTMLBodyElement.js //////")
+
+
+function printHTMLBodyElementProperties(htmlBodyElement){
+		
+	// TODO
+	
+	printHTMLElementProperties(htmlBodyElement);
+}
+
+
+var htmlBodyElement = new HTMLBodyElement();
+printHTMLBodyElementProperties(htmlBodyElement);
