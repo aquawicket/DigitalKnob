5a2d398b469dc57b06dd651930a8bbbcfe1868c3
aquawicket <aquawicket@hotmail.com>
2023-06-02 11:49:25 -0400

    commit from git

--- DKApps/DKBuilder/assets/app.js
+++ DKApps/DKBuilder/assets/app.js
@@ -1,3 +1,3 @@
 //CPP_DK_setConsolePosition(0, 0, (screen.width/2)-100, screen.height-40);
-CPP_DK_setConsolePosition(0, 0, screen.width, screen.height);
+//CPP_DK_setConsolePosition(0, 0, screen.width, screen.height);
 CPP_DK_Create("DKBuild/DKBuildConsole.js");
