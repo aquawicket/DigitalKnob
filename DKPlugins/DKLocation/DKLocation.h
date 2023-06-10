f4567bd2aed936b9af9f98f081658cef6d2ed576
aquawicket <aquawicket@hotmail.com>
2023-06-10 18:44:27 -0400

    commit from git

--- DKPlugins/DKLocation/DKLocation.h
+++ DKPlugins/DKLocation/DKLocation.h
@@ -28,7 +28,7 @@ public:
 	// [LegacyUnforgeable] stringifier attribute USVString href;
 	USVString _href = "";
 	virtual const USVString& href()					{ return _href; }			// getter
-	virtual void href(const USVString& href); 		{ 							// setter
+	virtual void href(const USVString& href) 		{ 							// setter
 		
 		UriUriA uri;
 		const char* const uriString = href.c_str();
