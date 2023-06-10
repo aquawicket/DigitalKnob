05d3cc277093ab7df8ae4ae64d66819bc90f668f
aquawicket <aquawicket@hotmail.com>
2023-06-10 13:32:13 -0400

    commit from git

--- DKPlugins/DKURL/DKURL.h
+++ DKPlugins/DKURL/DKURL.h
@@ -35,63 +35,63 @@ public:
 	
 	// stringifier attribute USVString href;
 	USVString _href = "";
-	virtual const USVString& 	href()									{ return _href; }					// getter
-	virtual void 				href(const USVString& href) 			{ _href = href; } 					// setter
+	virtual const USVString& 	href()										{ return _href; }					// getter
+	virtual void 				href(const USVString& href) 				{ _href = href; } 					// setter
 	
 	// readonly attribute USVString origin;
 	USVString _origin = "";
-	virtual const USVString& 	origin()								{ return _origin; }					// getter
-	virtual void 				origin(const USVString& origin) 		{ _origin = origin; } 				// setter
+	virtual const USVString& 	origin()									{ return _origin; }					// getter
+	virtual void 				origin(const USVString& origin) 			{ _origin = origin; } 				// setter
 	
 	// attribute USVString protocol;
 	USVString _protocol = "";
-	virtual const USVString& 	protocol()								{ return _protocol; }				// getter
-	virtual void 				protocol(const USVString& protocol) 	{ _protocol = protocol; } 			// setter
+	virtual const USVString& 	protocol()									{ return _protocol; }				// getter
+	virtual void 				protocol(const USVString& protocol) 		{ _protocol = protocol; } 			// setter
 	
 	// attribute USVString username;
 	USVString _username = "";
-	virtual const USVString& 	username()								{ return _username; }				// getter
-	virtual void 				username(const USVString& protocol) 	{ _username = username; } 			// setter
+	virtual const USVString& 	username()									{ return _username; }				// getter
+	virtual void 				username(const USVString& username) 		{ _username = username; } 			// setter
 	
 	// attribute USVString password;
 	USVString _password = "";
-	virtual const USVString& 	password()								{ return _password; }				// getter
-	virtual void 				password(const USVString& protocol) 	{ _password = password; } 			// setter
+	virtual const USVString& 	password()									{ return _password; }				// getter
+	virtual void 				password(const USVString& password) 		{ _password = password; } 			// setter
 	
 	// attribute USVString host;
 	USVString _host = "";
-	virtual const USVString& 	host()									{ return _host; }					// getter
-	virtual void 				host(const USVString& protocol) 		{ _host = host; } 					// setter
+	virtual const USVString& 	host()										{ return _host; }					// getter
+	virtual void 				host(const USVString& host) 				{ _host = host; } 					// setter
 	
 	// attribute USVString hostname;
 	USVString _hostname = "";
-	virtual const USVString& 	hostname()								{ return _hostname; }				// getter
-	virtual void 				hostname(const USVString& protocol) 	{ _hostname = hostname; } 			// setter
+	virtual const USVString& 	hostname()									{ return _hostname; }				// getter
+	virtual void 				hostname(const USVString& hostname) 		{ _hostname = hostname; } 			// setter
 	
 	// attribute USVString port;
 	USVString _port = "";
-	virtual const USVString& 	port()									{ return _port; }					// getter
-	virtual void 				port(const USVString& protocol) 		{ _port = port; } 					// setter
+	virtual const USVString& 	port()										{ return _port; }					// getter
+	virtual void 				port(const USVString& port) 				{ _port = port; } 					// setter
 	
 	// attribute USVString pathname;
 	USVString _pathname = "";
-	virtual const USVString& 	pathname()								{ return _pathname; }				// getter
-	virtual void 				pathname(const USVString& protocol) 	{ _pathname = pathname; } 			// setter
+	virtual const USVString& 	pathname()									{ return _pathname; }				// getter
+	virtual void 				pathname(const USVString& pathname) 		{ _pathname = pathname; } 			// setter
 	
 	// attribute USVString search;
 	USVString _search = "";
-	virtual const USVString& 	search()								{ return _search; }					// getter
-	virtual void 				search(const USVString& protocol) 		{ _search = search; } 				// setter
+	virtual const USVString& 	search()									{ return _search; }					// getter
+	virtual void 				search(const USVString& search) 			{ _search = search; } 				// setter
 	
 	// [SameObject] readonly attribute URLSearchParams searchParams;
 	DKString _searchParams = "";
-	virtual const DKString& 	searchParams()							{ return _searchParams; }			// getter
-	virtual void 				searchParams(const DKString& protocol)	{ _searchParams = searchParams; } 	// setter
+	virtual const DKString& 	searchParams()								{ return _searchParams; }			// getter
+	virtual void 				searchParams(const DKString& searchParams)	{ _searchParams = searchParams; } 	// setter
 	
 	// attribute USVString hash;
 	USVString _hash = "";
-	virtual const USVString& 	hash()									{ return _hash; }					// getter
-	virtual void 				hash(const USVString& protocol) 		{ _hash = hash; } 					// setter
+	virtual const USVString& 	hash()										{ return _hash; }					// getter
+	virtual void 				hash(const USVString& hash) 				{ _hash = hash; } 					// setter
 
 	// USVString toJSON();
 	USVString _toJSON = "";
@@ -107,8 +107,8 @@ public:
 	// partial interface URL {
 	// static DOMString createObjectURL((Blob or MediaSource) obj);
 	DOMString _createObjectURL = "";
-	virtual const DOMString& createObjectURL() {
-		DKDEBUGFUNC();
+	virtual const DOMString& createObjectURL(const DKString& obj) {
+		DKDEBUGFUNC(obj);
 		DKTODO();
 		return _createObjectURL;
 	}
