922eb2a543a6dfdd0f194931bbe15ff9834e50c2
aquawicket <aquawicket@hotmail.com>
2023-06-13 18:12:30 -0400

    commit from git

---
+++ DKPlugins/DKElementCSSInlineStyle/DKElementCSSInlineStyle.h
@@ -0,0 +1,16 @@
+// [IDL] https://drafts.csswg.org/cssom/#the-elementcssinlinestyle-mixin
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
+#pragma once
+#ifndef DKElementCSSInlineStyle_H
+#define DKElementCSSInlineStyle_H
+
+#include "DKInterface/DKInterface.h"
+
+class DKElementCSSInlineStyle : public DKInterface
+{
+public:
+
+
+};
+
+#endif //DKElementCSSInlineStyle_H
