c9ee6d1da1a77bbf7e036f946cbcc41e0cce1579
aquawicket <aquawicket@hotmail.com>
2023-06-14 15:31:57 -0400

    commit from git

--- DKApps/DKDomTest/App.h
+++ DKApps/DKDomTest/App.h
@@ -40,13 +40,13 @@
 #include "TEST_DKConsoleWindow.h"
 #include "TEST_DKSdlWindow.h"
 //#include "TEST_DKSdlRmlDocument.h"		// Mixin	// TODO
-#include "TEST_DKRmlInterface.h"
-#include "TEST_DKRmlEventTarget.h"
-#include "TEST_DKRmlLocation.h"
+//#include "TEST_DKRmlInterface.h"
+//#include "TEST_DKRmlEventTarget.h"
+//#include "TEST_DKRmlLocation.h"
 //#include "TEST_RmlCSSStyleDeclaration.h"	// TODO
-#include "TEST_DKRmlNode.h"
-#include "TEST_DKRmlDocument.h"
-#include "TEST_DKRmlElement.h"
+//#include "TEST_DKRmlNode.h"
+//#include "TEST_DKRmlDocument.h"
+//#include "TEST_DKRmlElement.h"
 //#include "TEST_DKRmlHTMLElement.h"		// TODO
 
 
@@ -59,12 +59,12 @@ public:
 	std::unique_ptr<TEST_DKConsoleWindow> 		test_DKConsoleWindow;
 	//std::unique_ptr<TEST_DKSdlRmlDocument> 	test_DKSdlRmlDocument;	// Mixin
 	std::unique_ptr<TEST_DKSdlWindow> 			test_DKSdlWindow;
-	std::unique_ptr<TEST_DKRmlInterface>		test_DKRmlInterface;
-	std::unique_ptr<TEST_DKRmlEventTarget> 		test_DKRmlEventTarget;
-	std::unique_ptr<TEST_DKRmlLocation> 		test_DKRmlLocation;
-	std::unique_ptr<TEST_DKRmlNode> 			test_DKRmlNode;
-	std::unique_ptr<TEST_DKRmlDocument>			test_DKRmlDocument;
-	std::unique_ptr<TEST_DKRmlElement> 			test_DKRmlElement;
+	//std::unique_ptr<TEST_DKRmlInterface>		test_DKRmlInterface;
+	//std::unique_ptr<TEST_DKRmlEventTarget> 	test_DKRmlEventTarget;
+	//std::unique_ptr<TEST_DKRmlLocation> 		test_DKRmlLocation;
+	//std::unique_ptr<TEST_DKRmlNode> 			test_DKRmlNode;
+	//std::unique_ptr<TEST_DKRmlDocument>		test_DKRmlDocument;
+	//std::unique_ptr<TEST_DKRmlElement> 		test_DKRmlElement;
 
 };
 REGISTER_OBJECT(App, true);
