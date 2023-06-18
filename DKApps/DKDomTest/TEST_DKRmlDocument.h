9812e39f9007ab6e5632edfb55abf1117f32d280
aquawicket <aquawicket@hotmail.com>
2023-06-18 19:09:59 -0400

    commit from git

--- DKApps/DKDomTest/TEST_DKRmlDocument.h
+++ DKApps/DKDomTest/TEST_DKRmlDocument.h
@@ -38,27 +38,6 @@ public:
 		console.log("body->tagName() = "+body->tagName());
 		///////////////////////////////
 		
-		/*
-		//////////// Post processing <a hfer></a> hyperlinks ////////////
-		DKHTMLCollection& aElements = *dkRmlDocument->getElementsByTagName("a");
-		if(!&aElements){
-			console.error("aElements invalid!");
-		}
-		else{
-			//console.log("aElement.length() = "+toString(aElements.length()));
-			for(unsigned int i=0; i<aElements.length(); ++i){
-				DKElement& item = *aElements.item(i);
-				if (!&item)
-					console.error("aElements->item(" + toString(i) + ") invalid!");
-				if (item.hasAttribute("href")) {
-					item.style().setProperty("color", "rgb(0,0,255)");
-					item.style().setProperty("text-decoration", "underline");
-					item.addEventListener("click", &TEST_DKRmlDocument::onHyperlink);
-				}
-			}
-		}
-		/////////////////////////////////////////////////////////////////
-		*/
 		dkRmlDocument->addEventListener("load", &TEST_DKRmlDocument::onLoad);
 
 		printRmlDocumentProperties(*dkRmlDocument);
@@ -72,7 +51,7 @@ public:
 		DKDEBUGFUNC(event);
 		console.log("onLoad()");
 		
-		//////////// Post processing <a hfer></a> hyperlinks ////////////
+		//////////// Post processing <a href></a> hyperlinks ////////////
 		DKHTMLCollection& aElements = *dkRmlDocument->getElementsByTagName("a");
 		if(!&aElements){
 			console.error("aElements invalid!");
@@ -90,7 +69,6 @@ public:
 				}
 			}
 		}
-		/////////////////////////////////////////////////////////////////
 	}
 	
 	static void onHyperlink(DKEvent& event){
