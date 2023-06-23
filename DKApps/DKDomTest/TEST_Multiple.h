3d47ff9933c2b416f7198c8a35e050d902887146
aquawicket <aquawicket@hotmail.com>
2023-06-23 16:09:26 -0400

    commit from git

--- DKApps/DKDomTest/TEST_Multiple.h
+++ DKApps/DKDomTest/TEST_Multiple.h
@@ -9,19 +9,27 @@ class TEST_Multiple //: public DKObjectT<TEST_Multiple>
 {
 public:
 	DKSdlWindow* dkSdlWindowA;
+	DKRmlInterface* dkRmlInterfaceA;
+	
 	DKSdlWindow* dkSdlWindowB;
+	DKRmlInterface* dkRmlInterfaceB;
 
 	TEST_Multiple(){
 		DKDEBUGFUNC();
 		console.log("\n////// TEST_Multiple.h //////");
 		
 		dkSdlWindowA = new DKSdlWindow();
+		dkRmlInterfaceA = new DKRmlInterface(dkSdlWindowA);
+		
 		dkSdlWindowB = new DKSdlWindow();
+		dkRmlInterfaceB = new DKRmlInterface(dkSdlWindowB);
 	}
 	
 	~TEST_Multiple(){
 		delete dkSdlWindowA;
 		delete dkSdlWindowB;
+		delete dkRmlInterfaceA;
+		delete dkRmlInterfaceB;
 	}
 };
 //REGISTER_OBJECT(TEST_Multiple, true);
