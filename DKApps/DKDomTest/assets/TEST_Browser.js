4670a16685cd34d98efeaf155a3467103fad8821
aquawicket <aquawicket@hotmail.com>
2023-06-28 17:24:17 -0400

    commit from git

--- DKApps/DKDomTest/assets/TEST_Browser.js
+++ DKApps/DKDomTest/assets/TEST_Browser.js
@@ -1,5 +1,6 @@
 console.log("\n////// TEST_Browser.js //////")
 
+////// Setup //////
 const window = new DKSdlWindow()
 console.log("window = "+window);
 
@@ -15,9 +16,52 @@ console.log("dkRmlLocation = "+dkRmlLocation);
 const dkRmlDocument = new DKRmlDocument(dkRmlInterface, dkRmlEventListener);
 console.log("dkRmlDocument = "+dkRmlDocument);
 
-const document = dkRmlDocument.documentElement;
+const document = dkRmlDocument;//.documentElement;
 const location = dkRmlLocation;
+///////////////////
 
-///////////////////////////////////////////////////////////////////////////
+
 const url = "DKWebTest/index.html"
 location.href = url;
+
+
+function onHyperlink(event) {
+	console.log("onHyperlink()");
+	//const target = event.target;
+	//const value = target.getAttribute("href");
+	//location.href = value;
+}
+
+function onLoad(event) {
+	console.log("onLoad()");
+	
+	//////////// Post processing <a href></a> hyperlinks ////////////
+	var aElements = document.getElementsByTagName("a");
+	if(!aElements){
+		console.error("aElements invalid!");
+		return;
+	}
+
+	//console.log("aElement->length() = "+toString(aElements->length()));
+	for(var i=0; i<aElements.length; i++){
+		const item = aElements.item(i);
+		if(!item){
+			console.error("aElements.item("+i+") invalid!");
+			return;
+		}
+		if (item.hasAttribute("href")) {
+			item.addEventListener("click", onHyperlink);
+			const style = item.style;
+			if(!style){
+				console.error("style invalid!");
+				return;
+			}
+			style.setProperty("color", "rgb(0,0,255)");
+			style.setProperty("text-decoration", "underline");
+		}
+	}
+}
+//onLoad();
+document.addEventListener("load", onLoad);
+//const load_event = new Event("load", "");
+//document.dispatchEvent(load_event);
