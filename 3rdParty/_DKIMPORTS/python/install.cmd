b1d8658547ce892305b0735af7bf38097bed99d8
aquawicket <aquawicket@hotmail.com>
2023-11-22 18:43:35 -0500

    git commit

--- 3rdParty/_DKIMPORTS/python/install.cmd
+++ 3rdParty/_DKIMPORTS/python/install.cmd
@@ -1,4 +1,5 @@
 set "DIGITALKNOB=C:\Users\%USERNAME%\digitalknob"
+set "BRANCH=Development"
 
-%DIGITALKNOB%\download\python-2.7.18.msi /passive PrependPath=1 TargetDir=%DIGITALKNOB%\DK\3rdParty\python-2.7.18
+%DIGITALKNOB%\download\python-2.7.18.msi /passive PrependPath=1 TargetDir=%DIGITALKNOB%\%BRANCH%\3rdParty\python-2.7.18
 
