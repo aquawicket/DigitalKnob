c1d527eee1ca1d8c7753ec8ef2d584ee6c80292c
aquawicket <aquawicket@hotmail.com>
2023-11-22 18:46:12 -0500

    git commit

--- 3rdParty/_DKIMPORTS/python3/install.cmd
+++ 3rdParty/_DKIMPORTS/python3/install.cmd
@@ -1,4 +1,5 @@
 set "DIGITALKNOB=C:\Users\%USERNAME%\digitalknob"
+set "BRANCH=Development"
 
-%DIGITALKNOB%\download\python3-python-3.11.2-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 TargetDir=%DIGITALKNOB%\DK\3rdParty\python-3.11.2
+%DIGITALKNOB%\download\python3-python-3.11.2-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 TargetDir=%DIGITALKNOB%\%BRANCH%\3rdParty\python-3.11.2
 
