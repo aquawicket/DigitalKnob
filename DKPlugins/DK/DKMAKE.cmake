91b12cf52b8758fa545afbca2f54581709bb1385
aquawicket <aquawicket@hotmail.com>
2023-11-23 03:58:37 -0500

    git commit

--- DKPlugins/DK/DKMAKE.cmake
+++ DKPlugins/DK/DKMAKE.cmake
@@ -27,7 +27,7 @@ if(LINUX)
 	#dk_depend(libgl1-mesa-dev)
 	dk_depend(libx11-dev)
 	#dk_depend(libxrandr-dev)
-	#dk_depend(libasound2-dev) ##TODO: Move to DKAudio
+	dk_depend(libasound2-dev) ##TODO: Move to DKAudio
 	dk_depend(libxtst-dev)
 	#dk_depend(simple-getch)
 endif()
