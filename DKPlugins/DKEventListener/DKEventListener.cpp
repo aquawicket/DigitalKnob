9a7f5cc2a50fe4c0eb90b356fee088eb668d4f49
aquawicket <aquawicket@hotmail.com>
2023-11-24 03:47:40 -0500

    git commit

---
+++ DKPlugins/DKEventListener/DKEventListener.cpp
@@ -0,0 +1,1 @@
+#include "DKEventListener/DKEventListener.h"
---
+++ DKPlugins/DKEventListener/DKEventListener.h
@@ -0,0 +1,41 @@
+// [IDL] https://dom.spec.whatwg.org/#callbackdef-eventlistener
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/EventListener
+#pragma once
+#ifndef DKEventListener_H
+#define DKEventListener_H
+
+#include "DKEvent/DKEvent.h"
+
+
+typedef std::function<void(DKEvent*)> DKCallback;
+
+
+// Source: DOM Standard (https://dom.spec.whatwg.org/)
+// callback interface EventListener {
+class DKEventListener : virtual public DKInterface
+{
+public:
+	DKEventListener() : DKInterface() {
+		DKDEBUGFUNC();
+		interfaceName = "EventListener";
+		address[interfaceName] = pointerToAddress(this);
+		DKINFO("DK"+interfaceName+"("+interfaceAddress+","+address[interfaceName]+") \n");
+	}
+	
+	// undefined handleEvent(Event event);
+	virtual const void handleEvent(DKEvent& event) {
+		DKDEBUGFUNC(event);
+		DKTODO();
+	}
+	
+// };
+
+	////// DK properties //////
+	DKEventTarget* dkEventTarget;
+	DOMString type;
+	DKCallback callback;
+};
+
+
+
+#endif //DKEventListener_H
