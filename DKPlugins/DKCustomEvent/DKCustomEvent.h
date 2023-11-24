378ea9296b0628e5a1e2b10dbc76db3f93da13ea
aquawicket <aquawicket@hotmail.com>
2023-11-24 03:51:48 -0500

    git commit

---
+++ DKPlugins/DKCustomEvent/DKCustomEvent.h
@@ -0,0 +1,40 @@
+// [IDL] https://dom.spec.whatwg.org/#interface-customevent
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
+#pragma once
+#ifndef DKCustomEvent_H
+#define DKCustomEvent_H
+
+#include "DKEvent/DKEvent.h"
+
+////// CustomEventInit //////
+typedef std::string CustomEventInit;
+
+
+// Source: DOM Standard (https://dom.spec.whatwg.org/)
+// [Exposed=*]
+// interface CustomEvent : Event {
+class DKCustomEvent : public DKEvent
+{
+public:
+	// constructor(DOMString type, optional CustomEventInit eventInitDict = {});
+	DKCustomEvent(const DOMString& type, const CustomEventInit& eventInitDict = "{}") : DKEvent(type, eventInitDict) {
+		DKDEBUGFUNC(type, eventInitDict);
+		interfaceName = "CustomEvent";
+		address[interfaceName] = pointerToAddress(this);
+		DKINFO("DK"+interfaceName+"("+interfaceAddress+","+address[interfaceName]+") \n");
+	}
+	
+	// readonly attribute any detail;
+	DKString _detail = "{}";
+	virtual const DKString&		detail()						{ return _detail; }		// getter
+	virtual void 				detail(const DKString& detail) 	{ _detail = detail; }	// setter
+
+	// undefined initCustomEvent(DOMString type, optional boolean bubbles = false, optional boolean cancelable = false, optional any detail = null); // legacy
+	virtual void initCustomEvent(const DOMString& type, const bool& bubbles, const bool& cancelable, const DKString& detail){
+		DKDEBUGFUNC(type, bubbles, cancelable, detail);
+		DKTODO();
+	}
+};
+
+
+#endif //DKCustomEvent_H
---
+++ DKPlugins/DKCustomEvent/DKCustomEventDUK.js
@@ -0,0 +1,43 @@
+// [IDL] file:///C:/Users/Administrator/digitalknob/DK/3rdParty/webref-curated/ed/idlnames/CustomEvent.idl
+// [SOURCE] https://dom.spec.whatwg.org/#interface-customevent
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
+
+
+// Source: DOM Standard (https://dom.spec.whatwg.org/)
+// [Exposed=*]
+// interface CustomEvent : Event {
+// constructor(DOMString type, optional CustomEventInit eventInitDict = {});
+var CustomEvent = function CustomEvent(type, eventInitDict, address) {
+	//console.log("CustomEvent("+type+","+eventInitDict+","+address+")")
+	
+	if(address)
+		this.address = address;
+	if(!this.address)
+		this.address = CPP_DKCustomEventDUK_constructor(type, eventInitDict);
+
+	// readonly attribute any detail;
+	Object.defineProperty(this, "detail", {
+        get: function detail()		{ return JSON.parse(CPP_DKCustomEventDUK_detail(this.address)) },
+		set: function detail(str)	{ return JSON.parse(CPP_DKCustomEventDUK_detail(this.address, JSON.stringify(str))) },
+		configurable: true,
+    })
+	
+	// undefined initCustomEvent(DOMString type, optional boolean bubbles = false, optional boolean cancelable = false, optional any detail = null); // legacy
+	CustomEvent.prototype.initCustomEvent = function initCustomEvent() {
+		CPP_DKCustomEventDUK_initCustomEvent(this.address);
+    }
+	
+	
+	////// toString //////
+	if(this.toString() === "[object Object]")
+		this.toString = function(){ return "[object CustomEvent]" }
+	
+	// assign eventInitDict
+	var event = Event.call(this, type, eventInitDict)
+	
+	// Make properties (Read Only) after assignment
+	//Object.defineProperty(this, "detail", 		{ set: undefined })
+	
+	return event
+};
+CustomEvent.prototype = Event.prototype;
