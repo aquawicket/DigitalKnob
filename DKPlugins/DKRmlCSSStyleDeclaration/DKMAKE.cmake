ba177dc2c01a856cb884edaefb2961e59ba58c50
aquawicket <aquawicket@hotmail.com>
2023-11-24 04:41:24 -0500

    git commit

---
+++ DKPlugins/DKRmlCSSStyleDeclaration/DKMAKE.cmake
@@ -0,0 +1,5 @@
+# [IDL] https://drafts.csswg.org/cssom/#the-cssstyledeclaration-interface
+dk_depend(DKCSSStyleDeclaration)
+
+dk_generateCmake(DKRmlCSSStyleDeclaration)
+dk_assets(DKRmlCSSStyleDeclaration)
