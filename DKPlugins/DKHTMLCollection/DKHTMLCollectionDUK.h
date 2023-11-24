f4d108ee40e6434e9c31aabf6b033e1815089095
aquawicket <aquawicket@hotmail.com>
2023-11-24 03:57:33 -0500

    git commit

---
+++ DKPlugins/DKHTMLCollection/DKHTMLCollectionDUK.h
@@ -0,0 +1,111 @@
+// [IDL] https://dom.spec.whatwg.org/#interface-htmlcollection
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
+#if HAVE_DKDuktape
+
+#pragma once
+#ifndef DKHTMLCollectionDUK_H
+#define DKHTMLCollectionDUK_H
+
+#include "DKInterface/DKInterface.h"
+
+// [Exposed=Window, LegacyUnenumerableNamedProperties]
+// interface HTMLCollection {
+class DKHTMLCollectionDUK : public DKObjectT<DKHTMLCollectionDUK>
+{
+public:
+	bool Init(){
+		
+		DKDuktape::AttachFunction("CPP_DKHTMLCollectionDUK_constructor", 	DKHTMLCollectionDUK::constructor);
+		
+		// readonly attribute unsigned long length;
+		DKDuktape::AttachFunction("CPP_DKHTMLCollectionDUK_length", 		DKHTMLCollectionDUK::length);
+
+		// getter Element? item(unsigned long index);
+		DKDuktape::AttachFunction("CPP_DKHTMLCollectionDUK_item", 			DKHTMLCollectionDUK::item);
+	
+		// getter Element? namedItem(DOMString name);
+		DKDuktape::AttachFunction("CPP_DKHTMLCollectionDUK_namedItem", 		DKHTMLCollectionDUK::namedItem);
+		
+	
+		////// Load .js files
+		DKClass::DKCreate("DKHTMLCollection/DKHTMLCollectionDUK.js");
+		
+		return true;
+	}
+	
+	static DKHTMLCollection* htmlCollection(duk_context* ctx){
+		DKString interfaceAddress = duk_require_string(ctx, 0);
+		DKInterface* interface = (DKInterface*)addressToPointer(interfaceAddress);
+		DKASSERT(interface);
+		
+		DKString htmlCollectionAddress = interface->address["HTMLCollection"];
+		DKHTMLCollection* _htmlCollection = (DKHTMLCollection*)addressToPointer(htmlCollectionAddress);
+		DKASSERT(_htmlCollection);
+		
+		return _htmlCollection;
+	}
+	static bool GetBool(duk_context* ctx){
+		if (duk_is_boolean(ctx, 1))
+			return duk_to_boolean(ctx, 1);
+		return false;
+	}
+	static double GetDouble(duk_context* ctx){
+		if (duk_is_number(ctx, 1))
+			return duk_to_number(ctx, 1);
+		return 0.0;
+	}
+	static int GetInt(duk_context* ctx){
+		if (duk_is_number(ctx, 1))
+			return duk_to_int(ctx, 1);
+		return 0;
+	}
+	static DKString GetString(duk_context* ctx){
+		if (duk_is_string(ctx, 1))
+			return duk_to_string(ctx, 1);
+		return "";
+	}
+	static unsigned int GetUint(duk_context* ctx){
+		if (duk_is_number(ctx, 1))
+			return duk_to_uint(ctx, 1);
+		return 0;
+	}
+
+	static int constructor(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		std::vector<DKElement*> list;
+		
+		DKINFO("CPP_DKHTMLCollectionDUK_constructor(list)\n");
+		DKHTMLCollection* _htmlCollection = new DKHTMLCollection(list);
+		dukglue_push(ctx, _htmlCollection->interfaceAddress);
+		return true;
+	}
+	
+	// readonly attribute unsigned long length;
+	static int length(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		//if(duk_is_valid_index(ctx, 1))
+		//	htmlCollection(ctx)->length(GetUint(ctx));
+		dukglue_push(ctx, htmlCollection(ctx)->length());
+		return true;
+	}
+	
+	// getter Element? item(unsigned long index);
+	static int item(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		dukglue_push(ctx, htmlCollection(ctx)->item(GetUint(ctx))->interfaceAddress);
+		return true;
+	}
+	
+	// getter Element? namedItem(DOMString name);
+	static int namedItem(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		dukglue_push(ctx, htmlCollection(ctx)->namedItem(GetString(ctx))->interfaceAddress);
+		return true;
+	}
+		
+};
+REGISTER_OBJECT(DKHTMLCollectionDUK, true)
+
+
+#endif //DKHTMLCollectionDUK_H
+#endif //HAVE_DKDuktape
---
+++ DKPlugins/DKHTMLCollection/DKHTMLCollectionDUK.js
@@ -0,0 +1,40 @@
+// [IDL] https://dom.spec.whatwg.org/#interface-htmlcollection
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
+
+
+// [Exposed=Window, LegacyUnenumerableNamedProperties]
+// interface HTMLCollection {
+var HTMLCollection = function HTMLCollection(address) {
+	console.log("HTMLCollection("+address+")")
+	
+	if(address)
+		this.address = address;
+	if(!this.address)
+		this.address = CPP_DKHTMLCollectionDUK_constructor();
+	
+	
+	// readonly attribute unsigned long length;
+	Object.defineProperty(this, "length", {
+        get: function length()			{ return CPP_DKHTMLCollectionDUK_length(this.address) },
+		//set: function length(data)	{ return CPP_DKHTMLCollectionDUK_length(this.address, data) },
+    })
+	
+	// getter Element? item(unsigned long index);
+	HTMLCollection.prototype.item = function item(index) {
+		return new Element(CPP_DKHTMLCollectionDUK_item(this.address, index));
+    }
+	
+	// getter Element? namedItem(DOMString name);
+	HTMLCollection.prototype.namedItem = function namedItem(name) {
+		return new Element(CPP_DKHTMLCollectionDUK_namedItem(this.address, name));
+    }
+	
+//};
+		
+		
+	////// toString //////
+	if(this.toString() === "[object Object]")
+		this.toString = function(){	return "[object HTMLCollection]" }
+	
+	return this;
+}
