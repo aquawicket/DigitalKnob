98764a4bd9599260ad3421dc344319a3bc2819ab
aquawicket <aquawicket@hotmail.com>
2023-11-24 04:05:55 -0500

    git commit

---
+++ DKPlugins/DKMouseEvent/DKMouseEvent.h
@@ -0,0 +1,177 @@
+// [IDL] https://w3c.github.io/uievents/#idl-mouseevent
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+#pragma once
+#ifndef DKMouseEvent_H
+#define DKMouseEvent_H
+
+#include "DKUIEvent/DKUIEvent.h"
+
+////// MouseEventInit //////
+typedef std::string MouseEventInit;
+
+
+// Source: UI Events (https://www.w3.org/TR/uievents/)
+// [Exposed=Window]
+// interface MouseEvent : UIEvent {
+class DKMouseEvent : public DKUIEvent
+{
+public:
+	// constructor(DOMString type, optional MouseEventInit eventInitDict = {});
+	DKMouseEvent(const DOMString& type, const MouseEventInit& eventInitDict = "{}") : DKUIEvent(type, eventInitDict) {
+		DKDEBUGFUNC(type, eventInitDict);
+		interfaceName = "MouseEvent";
+		address[interfaceName] = pointerToAddress(this);
+		DKINFO("DK"+interfaceName+"("+interfaceAddress+","+address[interfaceName]+") \n");
+	}
+	
+	// readonly attribute long screenX;
+	int _screenX = 0;
+	virtual const int& 				screenX()									{ return _screenX; }				// getter
+	virtual void 					screenX(const int& screenX) 				{ _screenX = screenX; } 			// setter
+
+	// readonly attribute long screenY;
+	int _screenY = 0;
+	virtual const int& 				screenY()									{ return _screenY; }				// getter
+	virtual void 					screenY(const int& screenY) 				{ _screenY = screenY; } 			// setter
+	
+	// readonly attribute long clientX;
+	int _clientX = 0;
+	virtual const int& 				clientX()									{ return _clientX; }				// getter
+	virtual void 					clientX(const int& clientX) 				{ _clientX = clientX; } 			// setter
+	
+	// readonly attribute long clientY;
+	int _clientY = 0;
+	virtual const int& 				clientY()									{ return _clientY; }				// getter
+	virtual void 					clientY(const int& clientY) 				{ _clientY = clientY; } 			// setter
+
+	// readonly attribute boolean ctrlKey;
+	bool _ctrlKey = false;
+	virtual const bool& 			ctrlKey()									{ return _ctrlKey; }				// getter
+	virtual void 					ctrlKey(const bool& ctrlKey) 				{ _ctrlKey = ctrlKey; } 			// setter
+	
+	// readonly attribute boolean shiftKey;
+	bool _shiftKey = false;
+	virtual const bool& 			shiftKey()									{ return _shiftKey; }				// getter
+	virtual void 					shiftKey(const bool& shiftKey) 				{ _shiftKey = shiftKey; } 			// setter
+	
+	// readonly attribute boolean altKey;
+	bool _altKey = false;
+	virtual const bool& 			altKey()									{ return _altKey; }					// getter
+	virtual void 					altKey(const bool& altKey) 					{ _altKey = altKey; } 				// setter
+	
+	// readonly attribute boolean metaKey;
+	bool _metaKey = false;
+	virtual const bool& 			metaKey()									{ return _metaKey; }				// getter
+	virtual void 					metaKey(const bool& metaKey) 				{ _metaKey = metaKey; } 			// setter
+	
+	// readonly attribute short button;
+	short _button = 0;
+	virtual const short& 			button()									{ return _button; }					// getter
+	virtual void 					button(const short& button) 				{ _button = button; } 				// setter
+	
+	// readonly attribute unsigned short buttons;
+	unsigned short _buttons = 0;
+	virtual const unsigned short& 	buttons()									{ return _buttons; }				// getter
+	virtual void 					buttons(const unsigned short& buttons) 		{ _buttons = buttons; }				// setter
+	
+	// readonly attribute EventTarget? relatedTarget;
+	DKEventTarget* _relatedTarget = NULL;
+	virtual DKEventTarget* 	relatedTarget()										{ return _relatedTarget; }			// getter
+	virtual void 			relatedTarget(DKEventTarget* relatedTarget) 		{ _relatedTarget = relatedTarget; } // setter
+	
+	// boolean getModifierState(DOMString keyArg);
+	bool _getModifierState = false;
+	virtual const bool& getModifierState(const DOMString& keyArg) {
+		DKDEBUGFUNC(keyArg);
+		DKTODO();
+		return _getModifierState;
+	}
+	
+	// Source: CSSOM View Module (https://www.w3.org/TR/cssom-view-1/)
+	// partial interface MouseEvent {
+	// readonly attribute double pageX;
+	double _pageX = 0;
+	virtual const double& 			pageX()									{ return _pageX; }					// getter
+	virtual void 					pageX(const double& pageX)				{ _pageX = pageX; } 				// setter
+	
+	// readonly attribute double pageY;
+	double _pageY = 0;
+	virtual const double& 			pageY()									{ return _pageY; }					// getter
+	virtual void 					pageY(const double& pageY)				{ _pageY = pageY; } 				// setter
+
+	// readonly attribute double x;
+	double _x = 0;
+	virtual const double& 			x()										{ return _x; }						// getter
+	virtual void 					x(const double& x)						{ _x = x; } 						// setter
+	//
+	// readonly attribute double y;
+	double _y = 0;
+	virtual const double&			y()										{ return _y; }						// getter
+	virtual void 					y(const double& y)						{ _y = y; } 						// setter
+	
+	// readonly attribute double offsetX; 
+	double _offsetX = 0;
+	virtual const double&			offsetX()								{ return _offsetX; }				// getter
+	virtual void 					offsetX(const double& offsetX)			{ _offsetX = offsetX; } 			// setter
+	
+	// readonly attribute double offsetY;
+	double _offsetY = 0;
+	virtual const double&			offsetY()								{ return _offsetY; }				// getter
+	virtual void 					offsetY(const double& offsetY)			{ _offsetY = offsetY; } 			// setter
+	// };
+	
+	// Source: Pointer Lock 2.0 (https://www.w3.org/TR/pointerlock-2/)
+	// partial interface MouseEvent {
+	// readonly attribute double movementX;
+	double _movementX = 0;
+	virtual const double&			movementX()								{ return _movementX; }				// getter
+	virtual void 					movementX(const double& movementX)		{ _movementX = movementX; } 		// setter
+
+	// readonly attribute double movementY;
+	double _movementY = 0;
+	virtual const double&			movementY()								{ return _movementY; }				// getter
+	virtual void 					movementY(const double& movementY)		{ _movementY = movementY; } 		// setter
+	// };
+	
+	// Source: UI Events (https://www.w3.org/TR/uievents/)
+	// partial interface MouseEvent {
+	// Deprecated in this speciofication
+	// undefined initMouseEvent(DOMString typeArg,
+	// optional boolean bubblesArg = false,
+	// optional boolean cancelableArg = false,
+	// optional Window? viewArg = null,
+	// optional long detailArg = 0,
+	// optional long screenXArg = 0,
+	// optional long screenYArg = 0,
+	// optional long clientXArg = 0,
+	// optional long clientYArg = 0,
+	// optional boolean ctrlKeyArg = false,
+	// optional boolean altKeyArg = false,
+	// optional boolean shiftKeyArg = false,
+	// optional boolean metaKeyArg = false,
+	// optional short buttonArg = 0,
+	// optional EventTarget? relatedTargetArg = null);
+	virtual void initMouseEvent(
+		const DOMString& typeArg, 
+		const bool& bubblesArg = false,
+		const bool& cancelableArg = false,
+		DKWindow* viewArg = NULL,
+		const int& detailArg = 0,
+		const int& screenXArg = 0,
+		const int& screenYArg = 0,
+		const int& clientXArg = 0,
+		const int& clientYArg = 0,
+		const bool& ctrlKeyArg = false,
+		const bool& altKeyArg = false,
+		const bool& shiftKeyArg = false,
+		const bool& metaKeyArg = false,
+		const short& buttonArg = 0,
+		DKEventTarget* relatedTargetArg = NULL) {
+			DKDEBUGFUNC(typeArg, bubblesArg, cancelableArg, viewArg, detailArg, screenXArg, screenYArg, clientXArg, clientYArg, ctrlKeyArg, altKeyArg, shiftKeyArg, metaKeyArg, buttonArg, relatedTargetArg);
+			DKTODO();
+		}
+	// };
+};
+
+
+#endif //DKMouseEvent_H
---
+++ DKPlugins/DKMouseEvent/DKMouseEventDUK.js
@@ -0,0 +1,179 @@
+// [IDL] file:///C:/Users/Administrator/digitalknob/DK/3rdParty/webref-curated/ed/idlnames/MouseEvent.idl
+// [SOURCE] https://w3c.github.io/uievents/#events-mouseevents
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+
+
+// Source: UI Events (https://www.w3.org/TR/uievents/)
+// [Exposed=Window]
+// interface MouseEvent : UIEvent {
+// constructor(DOMString type, optional MouseEventInit eventInitDict = {});
+var MouseEvent = function MouseEvent(type, eventInitDict, address) {
+	console.log("MouseEvent("+type+","+eventInitDict+","+address+")")
+	
+	if(address)
+		this.address = address;
+	if(!this.address)
+		this.address = CPP_DKMouseEventDUK_constructor(type, eventInitDict);
+	
+		
+	// readonly attribute long screenX;
+	Object.defineProperty(this, "screenX", {
+        get: function screenX()		{ return CPP_DKMouseEventDUK_screenX(this.address) },
+		set: function screenX(num)	{ return CPP_DKMouseEventDUK_screenX(this.address, num) },
+		configurable: true,
+    })
+	
+	// readonly attribute long screenY;
+	Object.defineProperty(this, "screenY", {
+        get: function screenY() 	{ return CPP_DKMouseEventDUK_screenY(this.address) },
+		set: function screenY(num)	{ return CPP_DKMouseEventDUK_screenY(this.address, num) },
+		configurable: true,
+    })
+	
+	// readonly attribute long clientX;
+	Object.defineProperty(this, "clientX", {
+        get: function clientX()		{ return CPP_DKMouseEventDUK_clientX(this.address) },
+		set: function clientX(num) 	{ return CPP_DKMouseEventDUK_clientX(this.address, num) },
+		configurable: true,
+    })
+	
+	// readonly attribute long clientY;
+	Object.defineProperty(this, "clientY", {
+        get: function clientY() 	{ return CPP_DKMouseEventDUK_clientY(this.address) },
+		set: function clientY(num) 	{ return CPP_DKMouseEventDUK_clientY(this.address, num) },
+		configurable: true,
+    })
+	
+	// readonly attribute boolean ctrlKey;
+	Object.defineProperty(this, "ctrlKey", {
+        get: function ctrlKey()		{ return CPP_DKMouseEventDUK_ctrlKey(this.address) },
+		set: function ctrlKey(flag)	{ return CPP_DKMouseEventDUK_ctrlKey(this.address, flag) },
+		configurable: true,
+    })
+	
+	// readonly attribute boolean shiftKey;
+	Object.defineProperty(this, "shiftKey", {
+        get: function shiftKey() 		{ return CPP_DKMouseEventDUK_shiftKey(this.address) },
+		set: function shiftKey(flag)	{ return CPP_DKMouseEventDUK_shiftKey(this.address, flag) },
+		configurable: true,
+    })
+	
+	// readonly attribute boolean altKey;
+	Object.defineProperty(this, "altKey", {
+        get: function altKey() 		{ return CPP_DKMouseEventDUK_altKey(this.address) },
+		set: function altKey(flag)	{ return CPP_DKMouseEventDUK_altKey(this.address, flag) },
+		configurable: true,
+    })
+	
+	// readonly attribute boolean metaKey;
+	Object.defineProperty(this, "metaKey", {
+        get: function metaKey() 	{ return CPP_DKMouseEventDUK_metaKey(this.address) },
+		set: function metaKey(flag) { return CPP_DKMouseEventDUK_metaKey(this.address, flag) },
+		configurable: true,
+    })
+	
+	// readonly attribute short button;
+	Object.defineProperty(this, "button", {
+        get: function button() { return CPP_DKMouseEventDUK_button(this.address) },
+    })
+	
+	// readonly attribute unsigned short buttons;
+	Object.defineProperty(this, "buttons", {
+        get: function buttons() { return CPP_DKMouseEventDUK_buttons(this.address) },
+    })
+	
+	// readonly attribute EventTarget? relatedTarget;
+	Object.defineProperty(this, "relatedTarget", {
+        get: function relatedTarget() { return new EventTarget(CPP_DKMouseEventDUK_relatedTarget(this.address)) },
+    })
+	
+	// boolean getModifierState(DOMString keyArg);
+	MouseEvent.prototype.getModifierState = function getModifierState() {
+		CPP_DKMouseEventDUK_getModifierState(this.address);
+    }
+	
+	
+	// Source: CSSOM View Module (https://www.w3.org/TR/cssom-view-1/)
+	// partial interface MouseEvent {
+	//  	readonly attribute double pageX;
+			Object.defineProperty(this, "pageX", {
+				get: function pageX() { return CPP_DKMouseEventDUK_pageX(this.address) },
+			})
+	//  	readonly attribute double pageY;
+			Object.defineProperty(this, "pageY", {
+				get: function pageY() { return CPP_DKMouseEventDUK_pageY(this.address) },
+			})
+	//  	readonly attribute double x;
+			Object.defineProperty(this, "x", {
+				get: function x() { return CPP_DKMouseEventDUK_x(this.address) },
+			})
+	//  	readonly attribute double y;
+			Object.defineProperty(this, "y", {
+				get: function y() { return CPP_DKMouseEventDUK_y(this.address) },
+			})
+	//  	readonly attribute double offsetX; 
+			Object.defineProperty(this, "offsetX", {
+				get: function offsetX() { return CPP_DKMouseEventDUK_offsetX(this.address) },
+			})
+	//  	readonly attribute double offsetY;
+			Object.defineProperty(this, "offsetY", {
+				get: function offsetY() { return CPP_DKMouseEventDUK_offsetY(this.address) },
+			})	
+	// };
+		
+	// Source: Pointer Lock 2.0 (https://www.w3.org/TR/pointerlock-2/)
+	// partial interface MouseEvent {
+	//   	readonly attribute double movementX;
+			Object.defineProperty(this, "movementX", {
+				get: function movementX() { return CPP_DKMouseEventDUK_movementX(this.address) },
+			})
+	//   	readonly attribute double movementY;
+			Object.defineProperty(this, "movementY", {
+				get: function movementY() { return CPP_DKMouseEventDUK_movementY(this.address) },
+			})
+	// };
+		
+	// Source: UI Events (https://www.w3.org/TR/uievents/)
+	// partial interface MouseEvent {
+	//		Deprecated in this specification
+	//	  	undefined initMouseEvent(DOMString typeArg,
+	//		optional boolean bubblesArg = false,
+	//		optional boolean cancelableArg = false,
+	//		optional Window? viewArg = null,
+	//		optional long detailArg = 0,
+	//		optional long screenXArg = 0,
+	//		optional long screenYArg = 0,
+	//		optional long clientXArg = 0,
+	//		optional long clientYArg = 0,
+	//		optional boolean ctrlKeyArg = false,
+	//		optional boolean altKeyArg = false,
+	//		optional boolean shiftKeyArg = false,
+	//		optional boolean metaKeyArg = false,
+	//		optional short buttonArg = 0,
+	//		optional EventTarget? relatedTargetArg = null);
+			MouseEvent.prototype.initMouseEvent = function initMouseEvent() {
+				CPP_DKMouseEventDUK_initMouseEvent(this.address);
+			}
+	// };
+	
+
+	////// toString //////
+	if(this.toString() === "[object Object]")
+		this.toString = function(){ return "[object MouseEvent]" }
+	
+
+	var event = UIEvent.call(this, type, eventInitDict);
+	
+	// Make properties (Read Only) after assignment
+	Object.defineProperty(this, "altKey ", 		{ set: undefined });
+	Object.defineProperty(this, "clientX",  	{ set: undefined });
+	Object.defineProperty(this, "clientY",  	{ set: undefined });
+	Object.defineProperty(this, "ctrlKey ", 	{ set: undefined });
+	Object.defineProperty(this, "metaKey ", 	{ set: undefined });
+	Object.defineProperty(this, "screenX",  	{ set: undefined });
+	Object.defineProperty(this, "screenY",  	{ set: undefined });
+	Object.defineProperty(this, "shiftKey ", 	{ set: undefined });
+	
+	return event;
+}
+MouseEvent.prototype = UIEvent.prototype
