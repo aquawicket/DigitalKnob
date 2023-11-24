8d406091b0be6ca24d327fea8f41ba6b9a802b6a
aquawicket <aquawicket@hotmail.com>
2023-11-24 03:34:19 -0500

    git commit

--- build.cmd
+++ build.cmd
@@ -37,7 +37,8 @@ ECHO 5. DKBuilder
 ECHO 6. DKBuilderGui
 ECHO 7. DKSDL
 ECHO 8. DKSDLRml
-ECHO 9. DKTestAll
+ECHO 9. DKDomTest
+ECHO a. DKTestAll
 ECHO c. Clear Screen
 ECHO x. Exit
 set choice=
@@ -51,7 +52,8 @@ if '%choice%'=='5' goto dkbuilder
 if '%choice%'=='6' goto dkbuildergui
 if '%choice%'=='7' goto dksdl
 if '%choice%'=='8' goto dksdlrml
-if '%choice%'=='9' goto dktestall
+if '%choice%'=='9' goto dkdomtest
+if '%choice%'=='a' goto dktestall
 if '%choice%'=='c' goto clearscreen
 if '%choice%'=='x' goto end
 ECHO "%choice%" is not valid, try again
@@ -136,6 +138,10 @@ goto checkApp
 
 :dksdlrml
 set APP=DKSDLRml
+goto checkApp
+
+:dkdomtest
+set APP=DKDomTest
 goto checkApp
 
 :dktestall
