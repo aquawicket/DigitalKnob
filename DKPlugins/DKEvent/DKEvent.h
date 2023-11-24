b24fc1199957fd37980ec83571483de77a58924b
aquawicket <aquawicket@hotmail.com>
2023-11-24 03:46:05 -0500

    git commit

---
+++ DKPlugins/DKEvent/DKEvent.h
@@ -0,0 +1,164 @@
+// [IDL] https://dom.spec.whatwg.org/#interface-event
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/Event
+#pragma once
+#ifndef DKEvent_H
+#define DKEvent_H
+
+#include "DKInterface/DKInterface.h"
+
+
+////// EventInit //////
+typedef std::string EventInit;
+
+////// DOMHighResTimeStamp //////
+typedef double DOMHighResTimeStamp;
+
+////// EventTarget //////
+class DKEventTarget;
+
+
+// Source: DOM Standard (https://dom.spec.whatwg.org/)
+// [Exposed=*]
+// interface Event {
+class DKEvent : public DKInterface
+{
+public:
+	// constructor(DOMString type, optional EventInit eventInitDict = {});
+	DKEvent(const DOMString& type, const EventInit& eventInitDict = "{}") : DKInterface(){
+		DKDEBUGFUNC(type, eventInitDict);
+		interfaceName = "Event";
+		address[interfaceName] = pointerToAddress(this);
+		DKINFO("DK"+interfaceName+"("+interfaceAddress+","+address[interfaceName]+") \n");
+		
+		long ticks;
+		DKUtil::GetTicks(ticks);
+		_timeStamp = ticks / 1000000.0;
+		
+		_type = type;
+		
+		_eventInitDict = eventInitDict;
+	}
+	
+	// readonly attribute DOMString type;
+	DOMString _type = "";
+	virtual const DOMString&	type()															{ return _type; }							// getter
+	virtual void 				type(const DOMString& type) 									{ _type = type; } 							// setter
+	
+	// readonly attribute EventTarget? target;
+	DKEventTarget* _target = NULL;
+	virtual DKEventTarget* 	target()															{ return _target; }							// getter
+	virtual void 			target(DKEventTarget* target)										{ _target = target; } 						// setter
+	
+	// readonly attribute EventTarget? srcElement; // legacy
+	DKEventTarget* _srcElement = NULL;
+	virtual DKEventTarget* 	srcElement()														{ return _srcElement; }						// getter
+	virtual void 			srcElement(DKEventTarget* srcElement) 								{ _srcElement = srcElement; } 				// setter
+	
+	// readonly attribute EventTarget? currentTarget;
+	DKEventTarget* _currentTarget = NULL;
+	virtual DKEventTarget* 		currentTarget()													{ return _currentTarget; }					// getter
+	virtual void 				currentTarget(DKEventTarget* currentTarget) 					{ _currentTarget = currentTarget; }			// setter
+	
+	// sequence<EventTarget> composedPath();
+	DKString _composedPath = "";
+	virtual const DKString& composedPath() {
+		DKDEBUGFUNC();
+		DKTODO();
+		return _composedPath;
+	}
+	
+	// const unsigned short NONE = 0;
+	const unsigned short _NONE = 0;
+	virtual const unsigned short& 		NONE()													{ return _NONE; }							// getter
+	//virtual void 						NONE(const unsigned short& NONE) 						{ _NONE = NONE; }			 				// setter
+	
+	// const unsigned short CAPTURING_PHASE = 1;
+	const unsigned short _CAPTURING_PHASE = 1;
+	virtual const unsigned short& 		CAPTURING_PHASE()										{ return _CAPTURING_PHASE; }				// getter
+	//virtual void 						CAPTURING_PHASE(const unsigned short& CAPTURING_PHASE) 	{ _CAPTURING_PHASE = CAPTURING_PHASE; }		// setter
+	
+	// const unsigned short AT_TARGET = 2;
+	const unsigned short _AT_TARGET = 2;
+	virtual const unsigned short& 		AT_TARGET()												{ return _AT_TARGET; }						// getter
+	//virtual void 						AT_TARGET(const unsigned short& AT_TARGET) 				{ _AT_TARGET = AT_TARGET; }					// setter
+	
+	// const unsigned short BUBBLING_PHASE = 3;
+	const unsigned short _BUBBLING_PHASE = 3;
+	virtual const unsigned short& 		BUBBLING_PHASE()										{ return _BUBBLING_PHASE; }					// getter
+	//virtual void 						BUBBLING_PHASE(const unsigned short& BUBBLING_PHASE) 	{ _BUBBLING_PHASE = BUBBLING_PHASE; }		// setter
+	
+	// readonly attribute unsigned short eventPhase;
+	unsigned short _eventPhase = 0;
+	virtual const unsigned short &		eventPhase()											{ return _eventPhase; }						// getter
+	virtual void 						eventPhase(const unsigned short& eventPhase) 			{ _eventPhase = eventPhase; }				// setter
+	
+	// undefined stopPropagation();
+	virtual void stopPropagation() {
+		DKDEBUGFUNC();
+		DKTODO();
+	}
+	
+	// attribute boolean cancelBubble; // legacy alias of .stopPropagation()
+	bool _cancelBubble = false;
+	virtual const bool& 				cancelBubble()											{ return _cancelBubble; }					// getter
+	virtual void 						cancelBubble(const bool& cancelBubble) 					{ _cancelBubble = cancelBubble; }			// setter
+	
+	// undefined stopImmediatePropagation();
+	virtual void stopImmediatePropagation() {
+		DKDEBUGFUNC();
+		DKTODO();
+	}
+	
+	// readonly attribute boolean bubbles;
+	bool _bubbles = false;
+	virtual const bool& 				bubbles()												{ return _bubbles; }						// getter
+	virtual void 						bubbles(const bool& bubbles) 							{ _bubbles = bubbles; }						// setter
+	
+	// readonly attribute boolean cancelable;
+	bool _cancelable = false;
+	virtual const bool& 				cancelable()											{ return _cancelable; }						// getter
+	virtual void 						cancelable(const bool& cancelable) 						{ _cancelable = cancelable; }				// setter
+	
+	// attribute boolean returnValue;  // legacy
+	bool _returnValue = false;
+	virtual const bool& 				returnValue()											{ return _returnValue; }					// getter
+	virtual void 						returnValue(const bool& returnValue) 					{ _returnValue = returnValue; }				// setter
+	
+	// undefined preventDefault();
+	virtual void preventDefault() {
+		DKDEBUGFUNC();
+		DKTODO();
+	}
+	
+	// readonly attribute boolean defaultPrevented;
+	bool _defaultPrevented = false;
+	virtual const bool& 				defaultPrevented()										{ return _defaultPrevented; }				// getter
+	virtual void 						defaultPrevented(const bool& defaultPrevented) 			{ _defaultPrevented = defaultPrevented; }	// setter
+	
+	// readonly attribute boolean composed;
+	bool _composed = false;
+	virtual const bool& 				composed()												{ return _composed; }						// getter
+	virtual void 						composed(const bool& composed) 							{ _composed = composed; }					// setter
+	
+	// [LegacyUnforgeable] readonly attribute boolean isTrusted;
+	bool _isTrusted = false;
+	virtual const bool& 				isTrusted()												{ return _isTrusted; }						// getter
+	virtual void 						isTrusted(const bool& isTrusted) 						{ _isTrusted = isTrusted; }					// setter
+	
+	// readonly attribute DOMHighResTimeStamp timeStamp;
+	DOMHighResTimeStamp _timeStamp = 0;
+	virtual const DOMHighResTimeStamp& 	timeStamp()												{ return _timeStamp; }						// getter
+	virtual void 				 		timeStamp(const DOMHighResTimeStamp& timeStamp) 		{ _timeStamp = timeStamp; }					// setter
+	
+	// undefined initEvent(DOMString type, optional boolean bubbles = false, optional boolean cancelable = false); // legacy
+	virtual void initEvent(const DOMString& type, const bool& bubbles = false, const bool& cancelable = false) {
+		DKDEBUGFUNC(type, bubbles, cancelable);
+		DKTODO();
+	}
+	
+	
+	////// DK properties //////
+	DKString _eventInitDict;
+};
+
+#endif //DKEvent_H
---
+++ DKPlugins/DKEvent/DKEventDUK.h
@@ -0,0 +1,350 @@
+// [IDL] https://dom.spec.whatwg.org/#interface-event
+// [MDN] https://developer.mozilla.org/en-US/docs/Web/API/Event
+#if HAVE_DKDuktape
+
+#pragma once
+#ifndef DKEventDUK_H
+#define DKEventDUK_H
+
+#include "DKDuktape/DKDuktape.h"
+#include "DKEvent/DKEvent.h"
+#include "DKEventTarget/DKEventTarget.h"
+
+
+// Source: DOM Standard (https://dom.spec.whatwg.org/)
+// [Exposed=*]
+// interface Event {
+class DKEventDUK : public DKObjectT<DKEventDUK>
+{
+public:
+	bool Init(){
+		
+		// constructor(DOMString type, optional EventInit eventInitDict = {});
+		DKDuktape::AttachFunction("CPP_DKEventDUK_constructor", 				DKEventDUK::constructor); 
+		
+		// readonly attribute DOMString type;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_type", 						DKEventDUK::type);
+		
+		// readonly attribute EventTarget? target;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_target", 						DKEventDUK::target);
+		
+		// readonly attribute EventTarget? srcElement; // legacy
+		DKDuktape::AttachFunction("CPP_DKEventDUK_srcElement", 					DKEventDUK::srcElement);
+		
+		// readonly attribute EventTarget? currentTarget;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_currentTarget", 				DKEventDUK::currentTarget);
+		
+		// sequence<EventTarget> composedPath();
+		DKDuktape::AttachFunction("CPP_DKEventDUK_composedPath", 				DKEventDUK::composedPath);
+		
+		// const unsigned short NONE = 0;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_NONE", 						DKEventDUK::NONE);
+		
+		// const unsigned short CAPTURING_PHASE = 1;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_CAPTURING_PHASE", 			DKEventDUK::CAPTURING_PHASE);
+	
+		// const unsigned short AT_TARGET = 2;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_AT_TARGET", 					DKEventDUK::AT_TARGET);
+	
+		// const unsigned short BUBBLING_PHASE = 3;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_BUBBLING_PHASE", 				DKEventDUK::BUBBLING_PHASE);
+		
+		// readonly attribute unsigned short eventPhase;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_eventPhase", 					DKEventDUK::eventPhase);
+		
+		// undefined stopPropagation();
+		DKDuktape::AttachFunction("CPP_DKEventDUK_stopPropagation", 			DKEventDUK::stopPropagation);
+		
+		// attribute boolean cancelBubble; // legacy alias of .stopPropagation()
+		DKDuktape::AttachFunction("CPP_DKEventDUK_cancelBubble", 				DKEventDUK::cancelBubble);
+		
+		// undefined stopImmediatePropagation();
+		DKDuktape::AttachFunction("CPP_DKEventDUK_stopImmediatePropagation",	DKEventDUK::stopImmediatePropagation);
+		
+		// readonly attribute boolean bubbles;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_bubbles", 					DKEventDUK::bubbles);
+		
+		// readonly attribute boolean cancelable;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_cancelable", 					DKEventDUK::cancelable);
+		
+		// attribute boolean returnValue;  // legacy
+		DKDuktape::AttachFunction("CPP_DKEventDUK_returnValue", 				DKEventDUK::returnValue);
+		
+		// undefined preventDefault();
+		DKDuktape::AttachFunction("CPP_DKEventDUK_preventDefault", 				DKEventDUK::preventDefault);
+		
+		// readonly attribute boolean defaultPrevented;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_defaultPrevented", 			DKEventDUK::defaultPrevented);
+		
+		// readonly attribute boolean composed;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_composed", 					DKEventDUK::composed);
+		
+		// [LegacyUnforgeable] readonly attribute boolean isTrusted;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_isTrusted", 					DKEventDUK::isTrusted);
+		
+		// readonly attribute DOMHighResTimeStamp timeStamp;
+		DKDuktape::AttachFunction("CPP_DKEventDUK_timeStamp", 					DKEventDUK::timeStamp);
+		
+		// undefined initEvent(DOMString type, optional boolean bubbles = false, optional boolean cancelable = false); // legacy
+		DKDuktape::AttachFunction("CPP_DKEventDUK_initEvent", 					DKEventDUK::initEvent);
+		
+		
+		////// Load .js files
+		DKClass::DKCreate("DKEvent/DKEventDUK.js");
+		
+		return true;
+	}
+	
+	// NOTE:  Example of converting interfaceAddress to DKEvent*
+	static DKEvent* event(duk_context* ctx){
+		DKString interfaceAddress = duk_require_string(ctx, 0);
+		DKInterface* interface = (DKInterface*)addressToPointer(interfaceAddress);
+		DKString eventAddress = interface->address["Event"];
+		DKEvent* _event = (DKEvent*)addressToPointer(eventAddress);
+		return _event;
+	}
+	static bool GetBool(duk_context* ctx, int index = 1){
+		if (duk_is_boolean(ctx, index))
+			return duk_to_boolean(ctx, index);
+		return false;
+	}
+	static double GetDouble(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_number(ctx, index);
+		return 0.0;
+	}
+	static int GetInt(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_int(ctx, index);
+		return 0;
+	}
+	static DKString GetString(duk_context* ctx, int index = 1){
+		if (duk_is_string(ctx, index))
+			return duk_to_string(ctx, index);
+		return "";
+	}
+	static unsigned int GetUint(duk_context* ctx, int index = 1){
+		if (duk_is_number(ctx, index))
+			return duk_to_uint(ctx, index);
+		return 0;
+	}
+	
+	// constructor(DOMString type, optional EventInit eventInitDict = {});
+	static int constructor(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DOMString type = duk_require_string(ctx, 0);
+		EventInit eventInitDict = "{}";		//duk_require_string(ctx, 1);
+		
+		DKINFO("CPP_DKEventDUK_constructor("+type+","+eventInitDict+")\n");
+		DKEvent* _event = new DKEvent(type, eventInitDict);
+		dukglue_push(ctx, _event->interfaceAddress);
+		return true;
+	}
+	
+	// readonly attribute DOMString type;
+	static int type(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->type(GetString(ctx));
+		dukglue_push(ctx, event(ctx)->type());
+		return true;
+	}
+	
+	// readonly attribute EventTarget? target;
+	static int target(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1)){
+			DKString interfaceAddress = GetString(ctx);
+			DKInterface* interface = (DKInterface*)addressToPointer(interfaceAddress);
+			DKString eventTargetAddress = interface->address["EventTarget"];
+			DKEventTarget* eventTarget = (DKEventTarget*)addressToPointer(eventTargetAddress);
+			event(ctx)->target(eventTarget);
+		}
+		dukglue_push(ctx, event(ctx)->target()->interfaceAddress);
+		return true;
+	}
+	
+	// readonly attribute EventTarget? srcElement; // legacy
+	static int srcElement(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1)){
+			DKString interfaceAddress = GetString(ctx);
+			DKInterface* interface = (DKInterface*)addressToPointer(interfaceAddress);
+			DKString eventTargetAddress = interface->address["EventTarget"];
+			DKEventTarget* eventTarget = (DKEventTarget*)addressToPointer(eventTargetAddress);
+			event(ctx)->srcElement(eventTarget);
+		}
+		dukglue_push(ctx, event(ctx)->srcElement()->interfaceAddress);
+		return true;
+	}
+	
+	// readonly attribute EventTarget? currentTarget;
+	static int currentTarget(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1)){
+			DKString interfaceAddress = GetString(ctx);
+			DKInterface* interface = (DKInterface*)addressToPointer(interfaceAddress);
+			DKString eventTargetAddress = interface->address["EventTarget"];
+			DKEventTarget* eventTarget = (DKEventTarget*)addressToPointer(eventTargetAddress);
+			event(ctx)->currentTarget(eventTarget);
+		}
+		dukglue_push(ctx, event(ctx)->currentTarget()->interfaceAddress);
+		return true;
+	}
+	
+	// sequence<EventTarget> composedPath();
+	static int composedPath(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
+	
+	// const unsigned short NONE = 0;
+	static int NONE(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		//if(duk_is_valid_index(ctx, 1))
+		//	event(ctx)->NONE((unsigned short)GetUint(ctx));
+		dukglue_push(ctx, event(ctx)->NONE());
+		return true;
+	}
+	
+	// const unsigned short CAPTURING_PHASE = 1;
+	static int CAPTURING_PHASE(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		//if(duk_is_valid_index(ctx, 1))
+		//	event(ctx)->CAPTURING_PHASE((unsigned short)GetUint(ctx));
+		dukglue_push(ctx, event(ctx)->CAPTURING_PHASE());
+		return true;
+	}
+	
+	// const unsigned short AT_TARGET = 2;
+	static int AT_TARGET(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		//if(duk_is_valid_index(ctx, 1))
+		//	event(ctx)->AT_TARGET((unsigned short)GetUint(ctx));
+		dukglue_push(ctx, event(ctx)->AT_TARGET());
+		return true;
+	}
+	
+	// const unsigned short BUBBLING_PHASE = 3;
+	static int BUBBLING_PHASE(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		//if(duk_is_valid_index(ctx, 1))
+		//	event(ctx)->BUBBLING_PHASE((unsigned short)GetUint(ctx));
+		dukglue_push(ctx, event(ctx)->BUBBLING_PHASE());
+		return true;
+	}
+	
+	// readonly attribute unsigned short eventPhase;
+	static int eventPhase(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->eventPhase((unsigned short)GetUint(ctx));
+		dukglue_push(ctx, event(ctx)->eventPhase());
+		return true;
+	}
+	
+	// undefined stopPropagation();
+	static int stopPropagation(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
+	
+	// attribute boolean cancelBubble; // legacy alias of .stopPropagation()
+	static int cancelBubble(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->cancelBubble(GetBool(ctx));
+		dukglue_push(ctx, event(ctx)->cancelBubble());
+		return true;
+	}
+	
+	// undefined stopImmediatePropagation();
+	static int stopImmediatePropagation(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
+	
+	// readonly attribute boolean bubbles;
+	static int bubbles(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->bubbles(GetBool(ctx));
+		dukglue_push(ctx, event(ctx)->bubbles());
+		return true;
+	}
+	
+	// readonly attribute boolean cancelable;
+	static int cancelable(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->cancelable(GetBool(ctx));
+		dukglue_push(ctx, event(ctx)->cancelable());
+		return true;
+	}
+	
+	// attribute boolean returnValue;  // legacy
+	static int returnValue(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->returnValue(GetBool(ctx));
+		dukglue_push(ctx, event(ctx)->returnValue());
+		return true;
+	}
+	
+	// undefined preventDefault();
+	static int preventDefault(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		return DKTODO();
+	}
+	
+	// readonly attribute boolean defaultPrevented;
+	static int defaultPrevented(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->defaultPrevented(GetBool(ctx));
+		dukglue_push(ctx, event(ctx)->defaultPrevented());
+		return true;
+	}
+	
+	// readonly attribute boolean composed;
+	static int composed(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->composed(GetBool(ctx));
+		dukglue_push(ctx, event(ctx)->composed());
+		return true;
+	}
+	
+	// [LegacyUnforgeable] readonly attribute boolean isTrusted;
+	static int isTrusted(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->isTrusted(GetBool(ctx));
+		dukglue_push(ctx, event(ctx)->isTrusted());
+		return true;
+	}
+	
+	// readonly attribute DOMHighResTimeStamp timeStamp;
+	static int timeStamp(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		if(duk_is_valid_index(ctx, 1))
+			event(ctx)->timeStamp(GetDouble(ctx));
+		dukglue_push(ctx, event(ctx)->timeStamp());
+		return true;
+	}
+	
+	// undefined initEvent(DOMString type, optional boolean bubbles = false, optional boolean cancelable = false); // legacy
+	static int initEvent(duk_context* ctx){
+		DKDEBUGFUNC(ctx);
+		DKString _type = GetString(ctx, 1);
+		bool _bubbles = GetBool(ctx, 2);
+		bool _cancelable = GetBool(ctx, 3);
+		event(ctx)->initEvent(_type, _bubbles, _cancelable);
+		return DKTODO();
+	}
+};
+REGISTER_OBJECT(DKEventDUK, true)
+
+
+#endif //DKEventDUK_H
+#endif //HAVE_DKDuktape
