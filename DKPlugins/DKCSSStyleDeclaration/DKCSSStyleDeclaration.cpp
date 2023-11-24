3cd40d3cba75c718b36b028b99dc96f8c28077da
aquawicket <aquawicket@hotmail.com>
2023-11-24 04:24:18 -0500

    git commit

---
+++ DKPlugins/DKCSSStyleDeclaration/DKCSSStyleDeclaration.cpp
@@ -0,0 +1,1 @@
+#include "DKCSSStyleDeclaration/DKCSSStyleDeclaration.h"
---
+++ DKPlugins/DKCSSStyleDeclaration/DKMAKE.cmake
@@ -0,0 +1,5 @@
+# [IDL] https://drafts.csswg.org/cssom/#the-cssstyledeclaration-interface
+dk_depend(DKInterface)
+
+dk_generateCmake(DKCSSStyleDeclaration)
+dk_assets(DKCSSStyleDeclaration)
