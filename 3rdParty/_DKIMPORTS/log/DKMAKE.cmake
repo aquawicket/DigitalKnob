590d12c4aad68b2b07283e212231e2742e17254c
aquawicket <aquawicket@hotmail.com>
2023-11-23 22:19:17 -0500

    git commit

--- 3rdParty/_DKIMPORTS/log/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/log/DKMAKE.cmake
@@ -5,7 +5,6 @@ endif()
 
 dk_depend(android-ndk)
 
-
 dk_findLibrary(log)
 #dk_include(${ANDROID-NDK}/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include)
 #SET(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_LINK_EXECUTABLE} -llog")
