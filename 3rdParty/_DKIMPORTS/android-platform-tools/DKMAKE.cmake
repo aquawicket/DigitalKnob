ed53455fc38d4d100318a676ff150d2736c70ceb
aquawicket <aquawicket@hotmail.com>
2024-01-05 00:28:43 -0500

    git commit

--- 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
@@ -26,21 +26,21 @@ if(NOT ANDROID_HOST)
 	LINUX_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r33.0.3-linux.zip PATH ${ANDROID-SDK}/platform-tools)
 else()
 	ANDROID_HOST_dk_import(https://github.com/lzhiyong/android-sdk-tools/releases/download/33.0.3/android-sdk-tools-static-aarch64.zip PATH ${ANDROID-SDK}/termux)
-    dk_copy(${ANDROID-SDK}/termux/build-tools ${ANDROID-SDK}/build-tools/30.0.3)	# move termux/build-tools to android-sdk
-    dk_copy(${ANDROID-SDK}/termux/platform-tools ${ANDROID-SDK}/platform-tools)		# move termux/platform-tools to android-sdk
-    
-    ## Repackage Gradle's aapt2.jar with out version of aapt
+	dk_copy(${ANDROID-SDK}/termux/build-tools ${ANDROID-SDK}/build-tools/30.0.3)	# move termux/build-tools to android-sdk
+	dk_copy(${ANDROID-SDK}/termux/platform-tools ${ANDROID-SDK}/platform-tools)		# move termux/platform-tools to android-sdk
+	
+	## Repackage Gradle's aapt2.jar with out version of aapt
 	dk_set(AAPT2 /data/data/com.termux/files/home/.gradle/caches/modules-2/files-2.1/com.android.tools.build/aapt2/7.0.3-7396180/942684a205d274f6b23f6d066cafcc12a17ce9ff)
-    if(EXISTS ${AAPT2}/aapt2-7.0.3-7396180-linux.jar)
-        dk_info("Patching Gradle aapt2 .......")
-        dk_set(CURRENT_DIR ${AAPT2})
-        dk_executeProcess(unzip -o aapt2-7.0.3-7396180-linux.jar WORKING_DIRECTORY ${AAPT2})
-        dk_executeProcess(rm aapt2 WORKING_DIRECTORY ${AAPT2})
-        dk_executeProcess(cp ${ANDROID-SDK}/build-tools/30.0.3/aapt2 . WORKING_DIRECTORY ${AAPT2})
+	if(EXISTS ${AAPT2}/aapt2-7.0.3-7396180-linux.jar)
+		dk_info("Patching Gradle aapt2 .......")
+		dk_set(CURRENT_DIR ${AAPT2})
+		dk_executeProcess(unzip -o aapt2-7.0.3-7396180-linux.jar WORKING_DIRECTORY ${AAPT2})
+		dk_executeProcess(rm aapt2 WORKING_DIRECTORY ${AAPT2})
+		dk_executeProcess(cp ${ANDROID-SDK}/build-tools/30.0.3/aapt2 . WORKING_DIRECTORY ${AAPT2})
 		dk_executeProcess(rm aapt2-7.0.3-7396180-linux.jar WORKING_DIRECTORY ${AAPT2})
 		dk_executeProcess(zip aapt2-7.0.3-7396180-linux.jar ${AAPT2}/* WORKING_DIRECTORY ${AAPT2})
 		dk_executeProcess(rm -rf META-INF NOTICE aapt2 WORKING_DIRECTORY ${AAPT2})
-    endif()
+	endif()
 endif()
 
 # 34.0.3
