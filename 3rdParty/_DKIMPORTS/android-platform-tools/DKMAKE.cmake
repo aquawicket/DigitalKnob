8c5a49fef8fcbbc3ba52e1434c7a38b7305ec4de
aquawicket <aquawicket@hotmail.com>
2024-01-05 00:11:12 -0500

    git commit

--- 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
@@ -1,5 +1,6 @@
 # https://androidsdkoffline.blogspot.com/p/android-sdk-platform-tools.html
 # https://mirrors.cloud.tencent.com/AndroidSDK/
+# https://github.com/lzhiyong/android-sdk-tools
 
 if(NOT ANDROID)
 	dk_undepend(android-platform-tools)
@@ -22,15 +23,13 @@ dk_depend(android-sdk)
 WIN_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r33.0.3-windows.zip PATH ${ANDROID-SDK}/platform-tools)
 MAC_HOST_dk_import(https://mirrors.cloud.tencent.com/AndroidSDK/platform-tools_r33.0.3-darwin.zip PATH ${ANDROID-SDK}/platform-tools)
 if(NOT ANDROID_HOST)
-     LINUX_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r33.0.3-linux.zip PATH ${ANDROID-SDK}/platform-tools)
+	LINUX_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r33.0.3-linux.zip PATH ${ANDROID-SDK}/platform-tools)
 else()
-    ANDROID_HOST_dk_import(https://github.com/lzhiyong/android-sdk-tools/releases/download/33.0.3/android-sdk-tools-static-aarch64.zip PATH ${ANDROID-SDK}/termux)
-    # move termux/build-tools to android-sdk
-    dk_copy(${ANDROID-SDK}/termux/build-tools ${ANDROID-SDK}/build-tools/30.0.3)
-    # move termux/platform-tools to android-sdk
-    dk_copy(${ANDROID-SDK}/termux/platform-tools ${ANDROID-SDK}/platform-tools)
+	ANDROID_HOST_dk_import(https://github.com/lzhiyong/android-sdk-tools/releases/download/33.0.3/android-sdk-tools-static-aarch64.zip PATH ${ANDROID-SDK}/termux)
+    dk_copy(${ANDROID-SDK}/termux/build-tools ${ANDROID-SDK}/build-tools/30.0.3)	# move termux/build-tools to android-sdk
+    dk_copy(${ANDROID-SDK}/termux/platform-tools ${ANDROID-SDK}/platform-tools)		# move termux/platform-tools to android-sdk
     
-    ## fix aapt2 in gradle/cache
+    ## Repackage Gradle's aapt2.jar with out version of aapt
 	dk_set(AAPT2 /data/data/com.termux/files/home/.gradle/caches/modules-2/files-2.1/com.android.tools.build/aapt2/7.0.3-7396180/942684a205d274f6b23f6d066cafcc12a17ce9ff)
     if(EXISTS ${AAPT2}/aapt2-7.0.3-7396180-linux.jar)
         dk_info("Patching Gradle aapt2 .......")
@@ -42,3 +41,6 @@ else()
 		dk_executeProcess(rm -rf META-INF NOTICE aapt2 WORKING_DIRECTORY ${AAPT2})
     endif()
 endif()
+
+# 34.0.3
+
