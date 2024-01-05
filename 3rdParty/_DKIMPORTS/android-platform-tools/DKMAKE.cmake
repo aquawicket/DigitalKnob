e563502d4260cbcab6a5dd0f150f86dd1ba9bb15
aquawicket <aquawicket@hotmail.com>
2024-01-05 00:32:53 -0500

    git commit

--- 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/android-platform-tools/DKMAKE.cmake
@@ -44,7 +44,13 @@ else()
 endif()
 
 # 34.0.3
-#ANDROID_HOST_dk_import(https://github.com/lzhiyong/android-sdk-tools/releases/download/34.0.3/android-sdk-tools-static-aarch64.zip PATH ${ANDROID-SDK}/termux)
-#dk_copy(${ANDROID-SDK}/termux/build-tools ${ANDROID-SDK}/build-tools/30.0.3)	# move termux/build-tools to android-sdk
-#dk_copy(${ANDROID-SDK}/termux/platform-tools ${ANDROID-SDK}/platform-tools)		# move termux/platform-tools to android-sdk
+#WIN_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r34.0.3-windows.zip PATH ${ANDROID-SDK}/platform-tools)
+#MAC_HOST_dk_import(https://mirrors.cloud.tencent.com/AndroidSDK/platform-tools_r34.0.3-darwin.zip PATH ${ANDROID-SDK}/platform-tools)
+#if(NOT ANDROID_HOST)
+#	LINUX_HOST_dk_import(https://dl.google.com/android/repository/platform-tools_r34.0.3-linux.zip PATH ${ANDROID-SDK}/platform-tools)
+#else()
+	#ANDROID_HOST_dk_import(https://github.com/lzhiyong/android-sdk-tools/releases/download/34.0.3/android-sdk-tools-static-aarch64.zip PATH ${ANDROID-SDK}/termux)
+	#dk_copy(${ANDROID-SDK}/termux/build-tools ${ANDROID-SDK}/build-tools/30.0.3)	# move termux/build-tools to android-sdk
+	#dk_copy(${ANDROID-SDK}/termux/platform-tools ${ANDROID-SDK}/platform-tools)	# move termux/platform-tools to android-sdk
+#endif()
 
