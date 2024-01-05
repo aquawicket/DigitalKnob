c451b67ae8de27518cbbe9e542bf841d6e086231
aquawicket <aquawicket@hotmail.com>
2024-01-05 05:59:22 -0500

    git commit

--- build2.sh
+++ build2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # to run this script requires privledges
 # > chmod 777 build.sh
@@ -11,7 +11,6 @@ clear
 #################################
 #	Global variables
 #################################
-
 true=0
 false=1
 END_COLOR="\033[0m"
@@ -24,92 +23,111 @@ CYAN='\033[0;36'
 #	Functions
 #################################
 
-### error [string]
+### error <string> ###
 function error() {
 	echo -e "${RED} ERROR: $1 ${END_COLOR}"
 }
 
-### message [string]
+### message <string> ###
 function message() {
 	if [ -z "$1" ]; then
-		error "message [string] requires 1 parameter"
+		error "message <string> requires 1 parameter"
 		return $false
 	fi
 	
 	echo "$@"	
 }
 
-### file_exists
+### string_contains <string> <substring> ###
+function string_contains() {
+	if [ -z "$2" ]; then
+		error "string_contains <string] [substring] requires 2 parameters"
+		return $false
+	fi
+	
+	[[ $1 == *"$2"* ]]
+}
+
+### wait_for_key ###
 function wait_for_key() {
 	read -n 1 -s -r -p "Press any key to continue"
 	echo ""
 }
 
-### file_exists [file.ext]
+### file_exists <file> ###
 function file_exists() {
 	if [ -z "$1" ]; then
-		error "file_exists [file.ext] required 1 parameter"
+		error "file_exists <file> requires 1 parameter"
 		return $false
 	fi
 	
 	[ -e $1 ]
 }
 
-### file_contains [file.ext] [string]
+### file_contains <file.ext> <string> ###
 function file_contains() {
 	if [ -z "$2" ]; then
-		error "file_contains [file.ext] [string] required 2 parameters"
+		error "file_contains <file.ext> <string> requires 2 parameters"
 		return $false
 	fi
 		
 	grep -q "$2" "$1"
 }
 
-### append_file [string] [file.ext]
+### append_file <string> <file.ext> ###
 function append_file() {
 	if [ -z "$2" ]; then
-		error "append_file [string] [file.ext] required 2 parameters"
+		error "append_file <string> <file.ext> requires 2 parameters"
 		return $false
 	fi
 	
 	echo "$1" >> $2
 }
 
-### remove_from_file [string] [file.ext]
+### remove_from_file <string> <file.ext> ###
 function remove_from_file() {
 	if [ -z "$2" ]; then
-		error "remove_from_file [string] [file.ext] required 2 parameters"
+		error "remove_from_file <string> <file.ext> requires 2 parameters"
 		return $false
 	fi
 		
 	sed -i -e "/$1/d" $2
 }
 
-### get_dk_host
+### command_exists <command> ###
+command_exists() {
+	if [ -z "$1" ]; then
+		error "command_exists <command> requires 1 parameter"
+		return $false
+	fi
+	command -v "$1" >/dev/null 2>&1
+}
+
+### get_dk_host ###
 function get_dk_host() {
 	DK_HOST=$(uname -s)
 	echo "DK_HOST = $DK_HOST"
 }
 
-### get_dk_arch
+### get_dk_arch ###
 function get_dk_arch() {
 	DK_ARCH=$(uname -m)
 	echo "DK_ARCH = $DK_ARCH"
 }
 
-### get_dk_version
+### get_dk_version ###
 function get_dk_version() {
 	DK_VERSION=$(uname -v)
 	echo "DK_VERSION = $DK_VERSION"
 }
 
-### get_dk_ostype
+### get_dk_ostype ###
 function get_dk_ostype() {
 	DK_OSTYPE=$OSTYPE
 	echo "DK_OSTYPE = $DK_OSTYPE"
 }
 
-### get_dk_model
+### get_dk_model ###
 function get_dk_model() {
 	if [ -e /proc/device-tree/model ]; then
 		DK_MODEL=$(tr -d '\0' </proc/device-tree/model)
@@ -117,19 +135,19 @@ function get_dk_model() {
 	echo "DK_MODEL = $DK_MODEL"
 }
 
-### get_dk_machtype
+### get_dk_machtype ###
 function get_dk_machtype() {
 	DK_MACHTYPE=$MACHTYPE
 	echo "DK_MACHTYPE = $DK_MACHTYPE"
 }
 
-### get_dk_lang
+### get_dk_lang ###
 function get_dk_lang() {
 	DK_LANG=$LANG
 	echo "DK_LANG = $DK_LANG"
 }
 
-### get_dk_username
+### get_dk_username ###
 function get_dk_username() {
 	if [[ -n "$USER" ]]; then
 		DK_USERNAME=$USER
@@ -139,58 +157,58 @@ function get_dk_username() {
 	echo "DK_USERNAME = $DK_USERNAME"
 }
 
-### get_dk_logname
+### get_dk_logname ###
 function get_dk_logname() {
 	DK_LOGNAME=$LOGNAME
 	echo "DK_LOGNAME = $DK_LOGNAME"
 }
 
-### get_dk_home
+### get_dk_home ###
 function get_dk_home() {
 	DK_HOME=$HOME
 	echo "DK_HOME = $DK_HOME"
 }
 
-### get_dk_prefix
+### get_dk_prefix ###
 function get_dk_prefix() {
 	DK_PREFIX=$PREFIX
 	echo "DK_PREFIX = $DK_PREFIX"
 }
 
-### get_dk_term
+### get_dk_term ###
 function get_dk_term() {
 	DK_TERM=$TERM
 	echo "DK_TERM = $DK_TERM"
 }
 
-### get_dk_shell
+### get_dk_shell ###
 function get_dk_shell() {
 	DK_SHELL=$SHELL
 	echo "DK_SHELL = $DK_SHELL"
 }
 
-### get_dk_path
+### get_dk_path ###
 function get_dk_path() {
 	DK_PATH=$PATH
 	echo "DK_PATH = $DK_PATH"
 }
 
-### get_dk_pwd
+### get_dk_pwd ###
 function get_dk_pwd() {
 	DK_PWD=$PWD
 	echo "DK_PWD = $DK_PWD"
 }
 
-### load_dkenv
+### load_dkenv ###
 function load_dkenv() {
 	touch ~/.dkenv
 	source ~/.dkenv
 }
 
-### set_dk_root [path/to/digitalknob]
+### set_dk_root <path> ###
 function set_dk_root(){
 	if [ -z "$1" ]; then
-		error "set_dk_root [path/to/digitalknob] required 2 parameters"
+		error "set_dk_root <path> requires 1 parameter"
 		return $false
 	fi
 	
@@ -204,10 +222,11 @@ function set_dk_root(){
 	echo "DK_ROOT = $DK_ROOT"
 }
 
+### get_dk_root ###
 function get_dk_root() {
 	load_dkenv
 	
-	if [[ -n "$DK_ROOT" ]]; then
+	if [ -n "$DK_ROOT" ]; then
 		echo "DK_ROOT = $DK_ROOT"
 	else
 		echo "setting DK_ROOT to default directory"
@@ -215,6 +234,7 @@ function get_dk_root() {
 	fi
 }
 
+### clear_dk_root ###
 function clear_dk_root() {
 	export -n DK_ROOT
 	unset DK_ROOT
@@ -305,11 +325,10 @@ function main() {
 	# Windows (arm64)
 	# Windows (x86)
 	# Windows (x86_64)
-	
 }
 
 
-# If thit script is called with arguments, call the function, arguments, get the return value and exit
+# If this script is called with arguments, call the function with arguments, get the return value and exit
 if [ $# -ne 0 ]; then
 	echo "$1(${@:2}) ->"
 	echo ""
