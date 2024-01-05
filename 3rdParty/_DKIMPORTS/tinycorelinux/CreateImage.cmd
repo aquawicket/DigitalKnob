1706972fc9b15eadb25264b009ddb93aaaadb691
aquawicket <aquawicket@hotmail.com>
2024-01-05 02:36:52 -0500

    git commit

--- 3rdParty/_DKIMPORTS/tinycorelinux/CreateImage.cmd
+++ 3rdParty/_DKIMPORTS/tinycorelinux/CreateImage.cmd
@@ -1,10 +1,15 @@
 @echo off
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit ) :: keep window open
 
-set "TINYCORELINUX_DL=http://tinycorelinux.net/14.x/x86/release/CorePlus-current.iso"
-set "TINYCORELINUX_ISO=%cd%\CorePlus-current.iso"
+::set "TINYCORELINUX_DL=http://tinycorelinux.net/14.x/x86/release/CorePlus-current.iso"
+::set "TINYCORELINUX_ISO=%cd%\CorePlus-current.iso"
+
+set "TINYCORELINUX_DL=http://tinycorelinux.net/14.x/x86/release/TinyCore-current.iso"
+set "TINYCORELINUX_ISO=%cd%\TinyCore-current.iso"
+
 set "TINYCORELINUX_IMG=tinycore.img"
 set "QEMU=C:\Users\Administrator\digitalknob\Development\3rdParty\qemu"
+
 
 if NOT exist "%TINYCORELINUX_ISO%" (
 	call:download %TINYCORELINUX_DL% %TINYCORELINUX_ISO%
