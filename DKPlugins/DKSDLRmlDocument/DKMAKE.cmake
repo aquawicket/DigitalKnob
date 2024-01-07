75dd44667045d14420c8f216b1626440ef9a1ead
aquawicket <aquawicket@hotmail.com>
2024-01-06 19:19:16 -0500

    git commit

---
+++ DKPlugins/DKSDLRmlDocument/DKMAKE.cmake
@@ -0,0 +1,8 @@
+dk_depend(rmlui)
+dk_depend(DKAssets)
+dk_depend(DKCurl)
+dk_depend(DKSdlWindow)
+#dk_depend(DKRmlInterface)			//LINUX FIX:
+dk_depend(sdl_image)
+
+dk_generateCmake(DKSdlRmlDocument)
