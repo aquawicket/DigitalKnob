30b8c6c2b93ea2a571c717c618d6ef2d3100a4f8
aquawicket <aquawicket@hotmail.com>
2024-01-09 00:04:44 -0500

    git commit

--- DKCMake/other/RunTest.cmake.cmd
+++ DKCMake/other/RunTest.cmake.cmd
@@ -27,23 +27,17 @@
 
 set "CMAKE_DL=https://github.com/Kitware/CMake/releases/download/v3.21.1/cmake-3.21.1-windows-i386.msi"
 set "GIT_DL=https://github.com/git-for-windows/git/releases/download/v2.30.1.windows.1/Git-2.30.1-32-bit.exe"
-set "DIGITALKNOB=C:\Users\%USERNAME%\digitalknob"
-set "DKPATH=%DIGITALKNOB%\DK"
-set "DKDOWNLOAD=%DIGITALKNOB%\Download"
+set "DIGITALKNOB=C:/Users/%USERNAME%/digitalknob"
+set "DKCMAKE=%DIGITALKNOB%/Development/DKCMake"
+if exist "C:/Program Files/CMake/bin/cmake.exe" set "CMAKE=C:/Program Files/CMake/bin/cmake.exe"
+if exist "C:/Program Files (x86)/CMake/bin/cmake.exe" set "CMAKE=C:/Program Files (x86)/CMake/bin/cmake.exe"
+if not exist %CMAKE% ( ERROR "Could not locate CMAKE" )
 
-if exist "C:\Program Files\CMake\bin\cmake.exe" set "CMAKE=C:\Program Files\CMake\bin\cmake.exe"
-if exist "C:\Program Files (x86)\CMake\bin\cmake.exe" set "CMAKE=C:\Program Files (x86)\CMake\bin\cmake.exe"
-if NOT exist "%CMAKE%" (
-    echo "cannot find cmake, please update the repository"
-)
+:begin
+cls
 
-set "Test_cmake=C:\Users\%USERNAME%\digitalknob\Development\DKCMake\other\Test.cmake"
-if NOT exist "%Test_cmake%" (
-	echo "cannot find Test.cmake file, please update the repository"
-)
-
-"%CMAKE%" -P %Test_cmake%
-
-:end
-ECHO Done
-pause
+set "file=%DKCMAKE%/other/Test.cmake"
+"%CMAKE%" -DDKCMAKE=%DKCMAKE% -P "%file%" 
+ 
+pause 
+%DKEND% 
