188a4eff702b6c022cd61d43308e1a54b32aec80
aquawicket <aquawicket@hotmail.com>
2024-01-11 05:07:28 -0500

    git commit

--- 3rdParty/_DKIMPORTS/msys2/MINGW64.cmd
+++ 3rdParty/_DKIMPORTS/msys2/MINGW64.cmd
@@ -8,19 +8,45 @@ set "DIGITALKNOB=C:/Users/%USERNAME%/digitalknob"
 set "DKBRANCH=%DIGITALKNOB%/Development"
 set "MSYS2=%DKBRANCH%/3rdParty/msys2-x86_64-20221216"
 
-
-:: env MSYSTEM=MINGW64		  "Set each NAME to VALUE in the environment and run COMMAND"
+goto:main
+:: env MSYSTEM=MINGW64  "Set each NAME to VALUE in the environment and run COMMAND"
 :: /usr/bin/bash -li 	 
 
 ::   -l				= "invoke a new shell process"    
 ::   -i 			= "make the shell interactive"
 ::   -c <command> 	= "run a command"
 
-
+:: Opening a new interactive shell
 ::%MSYS2%/usr/bin/env MSYSTEM=MSYS /usr/bin/bash -li
 ::%MSYS2%/usr/bin/env MSYSTEM=MINGW32 /usr/bin/bash -li
-%MSYS2%/usr/bin/env MSYSTEM=MINGW64 /usr/bin/bash -lc /c/Users/Administrator/digitalknob/Development/build.sh
+::%MSYS2%/usr/bin/env MSYSTEM=MINGW64 /usr/bin/bash -li
 ::%MSYS2%/usr/bin/env MSYSTEM=CLANG32 /usr/bin/bash -li
 ::%MSYS2%/usr/bin/env MSYSTEM=CLANG64 /usr/bin/bash -li
 ::%MSYS2%/usr/bin/env MSYSTEM=CLANGARM64 /usr/bin/bash -li
 ::%MSYS2%/usr/bin/env MSYSTEM=UCRT64 /usr/bin/bash -li
+
+:: Run a script in a new launched shell
+::%MSYS2%/usr/bin/env MSYSTEM=MINGW64 /usr/bin/bash -lc /c/Users/Administrator/digitalknob/Development/build.sh
+
+::###### MINGW64 ######
+:console
+	TITLE DigitalKnob - CONSOLE
+	cls & echo You are in a Windows console environment & echo: & echo PATH = %PATH% & pause
+goto:eof 
+
+::###### MINGW64 ######
+:mingw64
+	TITLE DigitalKnob - MINGW64
+	%MSYS2%/usr/bin/env MSYSTEM=MINGW64 /usr/bin/bash -lc "clear && echo You are in a MINGW64 terminal environment && echo && echo PATH = $PATH && read -p 'press any key to continue' "
+goto:eof 
+
+::###### Main ######
+:main
+	echo This will alernate between different shell environments & echo:
+	pause
+	
+	call:console
+	call:mingw64
+	call:console
+	
+goto:eof 
