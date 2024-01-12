845b79b156c2b509ba1c1df599655817e2e11cc7
aquawicket <aquawicket@hotmail.com>
2024-01-12 01:11:23 -0500

    git commit

--- 3rdParty/_DKIMPORTS/msys2/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/msys2/DKMAKE.cmake
@@ -8,15 +8,15 @@ endif()
 
 
 ### INSTALL ###
-dk_set(MSYS2_VERSION x86_64-20221216)
-dk_set(MSYS2_DL https://github.com/msys2/msys2-installer/releases/download/2022-12-16/msys2-x86_64-20221216.exe)
+dk_set(MSYS2_VERSION msys2-x86_64-20231026.exe)
+dk_set(MSYS2_DL https://github.com/msys2/msys2-installer/releases/download/2023-10-26/msys2-x86_64-20231026.exe)
 dk_set(MSYS2_FOLDER msys2-${MSYS2_VERSION})
 dk_set(MSYS2 ${3RDPARTY}/${MSYS2_FOLDER})
 
 if(NOT EXISTS ${MSYS2}/msys2.exe)
 	dk_info("Installing ${MSYS2_FOLDER}")
 	dk_download(${MSYS2_DL} ${DKDOWNLOAD})
-	dk_command(${DKDOWNLOAD}/msys2-x86_64-20221216.exe install --root ${MSYS2} --confirm-command)
+	dk_command(${DKDOWNLOAD}/msys2-x86_64-20231026.exe install --root ${MSYS2} --confirm-command)
 endif()	
 
 
