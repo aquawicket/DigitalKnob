9797ff2f4fe5fe371f6b27e74a0ecdb3eaa76602
aquawicket <aquawicket@hotmail.com>
2024-01-30 07:21:03 -0500

    git commit

--- build.sh
+++ build.sh
@@ -399,13 +399,13 @@ echo " "
 #--------------------------------------------------------
 if [[ -n "$USERPROFILE" ]]; then
 	DIGITALKNOB="$USERPROFILE\digitalknob"
-	echo "DIGITALKNOB = $DIGITALKNOB"
 	DIGITALKNOB=$(sed 's.C:./c.g' <<< $DIGITALKNOB)
-	echo "DIGITALKNOB = $DIGITALKNOB"
 	DIGITALKNOB=$(sed 's.\\./.g' <<< $DIGITALKNOB)
+else
+	DIGITALKNOB="$HOME/digitalknob"
 fi
 
-#mkdir -p $DIGITALKNOB;
+mkdir -p $DIGITALKNOB;
 echo "DIGITALKNOB = $DIGITALKNOB"
 
 DKDOWNLOAD="$DIGITALKNOB/download"
