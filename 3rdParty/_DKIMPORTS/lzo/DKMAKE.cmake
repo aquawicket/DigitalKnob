8b86926008d14eed245453068ba16872584a6585
aquawicket <aquawicket@hotmail.com>
2024-01-31 05:44:46 -0500

    git commit

---
+++ 3rdParty/_DKIMPORTS/lzo/DKMAKE.cmake
@@ -0,0 +1,6 @@
+# Portable lossless data compression library
+# 
+# https://www.oberhumer.com/opensource/lzo
+# https://packages.msys2.org/base/lzo2
+
+dk_import(https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz)
