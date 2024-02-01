ed3b15e9c1fdff4223c7d375c4d3b3d5bdd0b796
aquawicket <aquawicket@hotmail.com>
2024-02-01 05:07:57 -0500

    git commit

---
+++ DKCMake/dev/cmake_echo.cmd
@@ -0,0 +1,68 @@
+:: This source file is part of digitalknob, the cross-platform C/C++/Javascript/Html/Css Solution
+::
+:: For the latest information, see https://github.com/aquawicket/DigitalKnob
+::
+:: Copyright(c) 2010 - 2023 Digitalknob Team, and contributors
+::
+:: Permission is hereby granted, free of charge, to any person obtaining a copy
+:: of this software and associated documentation files(the "Software"), to deal
+:: in the Software without restriction, including without limitation the rights
+:: to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+:: copies of the Software, and to permit persons to whom the Software is
+:: furnished to do so, subject to the following conditions :
+::
+:: The above copyright notice and this permission notice shall be included in all
+:: copies or substantial portions of the Software.
+::
+:: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+:: IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+:: FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+:: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+:: LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+:: OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+:: SOFTWARE.
+@echo off
+setlocal EnableDelayedExpansion
+if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit ) :: keep window open
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: cmake_echo <command> <file.ext>
+::
+:: Func:  Run code within th DKCMake codebase
+::
+:: eval_code:  the code to send to CMake
+:: error:    the error code if any
+::
+:: Example:  call cmake_echo dk_info("called from cmake_echo")
+::           echo cmake_echo returned: %error%
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+::###### cmake_echo ######
+:cmake_echo
+	if [%1] == [] 				echo "ERROR: cmake_echo() parameterv1 is invalid" & goto:eof
+	if [%2] == [] 				echo "ERROR: cmake_echo() parameterv2 is invalid" & goto:eof
+	
+	set "DKBRANCH=Development"
+	set "DIGITALKNOB=%HOMEDRIVE%%HOMEPATH%\digitalknob"
+	set "DKCMAKE=%DIGITALKNOB%\%DKBRANCH%\DKCMake"
+	
+	if exist "C:\Program Files\CMake\bin\cmake.exe" 		set "CMAKE=C:\Program Files\CMake\bin\cmake.exe"
+	if exist "C:\Program Files (x86)\CMake\bin\cmake.exe" 	set "CMAKE=C:\Program Files (x86)\CMake\bin\cmake.exe"
+	if not exist "%CMAKE%" 									echo "ERROR: Could not locate CMAKE" & goto:eof
+	
+	if not exist "%CMAKE%"		echo "ERROR: Could not locate CMAKE" 	& goto:eof
+	if not exist "%DKCMAKE%" 	echo "ERROR: Could not locate DKCMAKE" 	& goto:eof
+
+	:: cmake_echo begin
+	set commands=%1
+	set file=%2
+	call set commands=%%commands:"=%%
+	set "DKCOMMAND=%commands%"
+	call set DKCOMMAND=%%DKCOMMAND:^\=^/%%
+	echo DKCOMMAND = %DKCOMMAND%
+	call set DKCMAKE_DIR=%%DKCMAKE:^\=^/%%
+	
+	"%CMAKE%" "-DDKCMAKE=%DKCMAKE_DIR%" "-DDKCOMMAND=%DKCOMMAND%" -P "%DKCMAKE_DIR%/dev/cmake_echo.cmake"
+	goto:eof
+		
+	::echo return code: %ERRORLEVEL%
+goto:eof
