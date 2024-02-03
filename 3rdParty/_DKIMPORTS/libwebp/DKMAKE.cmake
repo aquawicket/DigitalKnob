47b6a002c5c914a8dff48eb7ef4700956980bb98
aquawicket <aquawicket@hotmail.com>
2024-02-03 02:33:38 -0500

    git commit

--- 3rdParty/_DKIMPORTS/libwebp/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libwebp/DKMAKE.cmake
@@ -15,10 +15,10 @@ dk_import(https://github.com/webmproject/libwebp.git BRANCH main)
 
 
 ### LINK ###
-dk_include				(${LIBWEBP})
+dk_include					(${LIBWEBP}																	WEBP_INCLUDE_DIR)
 if(MSVC)
-	WIN_dk_libDebug			(${LIBWEBP}/${OS}/${DEBUG_DIR}/libwebp.lib)
-	WIN_dk_libRelease		(${LIBWEBP}/${OS}/${RELEASE_DIR}/libwebp.lib)
+	WIN_dk_libDebug			(${LIBWEBP}/${OS}/${DEBUG_DIR}/libwebp.lib									WEBP_LIBRARY_DEBUG)
+	WIN_dk_libRelease		(${LIBWEBP}/${OS}/${RELEASE_DIR}/libwebp.lib								WEBP_LIBRARY_RELEASE)
 elseif(APPLE)
 	APPLE_dk_libDebug		(${LIBWEBP}/${OS}/WebP.build/${DEBUG_DIR}/webpdecode.build/libwebpdecode.a)
 	APPLE_dk_libDebug		(${LIBWEBP}/${OS}/WebP.build/${DEBUG_DIR}/webpdsp.build/libwebpdsp.a)
@@ -29,25 +29,25 @@ elseif(APPLE)
 	APPLE_dk_libRelease		(${LIBWEBP}/${OS}/WebP.build/${RELEASE_DIR}/webpencode.build/libwebpencode.a)
 	APPLE_dk_libRelease		(${LIBWEBP}/${OS}/WebP.build/${RELEASE_DIR}/webputils.build/libwebputils.a)
 else()
-	dk_libDebug		(${LIBWEBP}/${OS}/${DEBUG_DIR}/libwebp.a)
-	dk_libRelease	(${LIBWEBP}/${OS}/${RELEASE_DIR}/libwebp.a)
+	dk_libDebug				(${LIBWEBP}/${OS}/${DEBUG_DIR}/libwebp.a)
+	dk_libRelease			(${LIBWEBP}/${OS}/${RELEASE_DIR}/libwebp.a)
 endif()
 
 
 ### 3RDPARTY LINK ###
 if(MSVC)
-	WIN_dk_set		(LIBWEBP_CMAKE -DWEBP_INCLUDE_DIR=${LIBWEBP} -DWEBP_LIBRARY_DEBUG=${LIBWEBP}/${OS}/${DEBUG_DIR}/libwebpd.lib -DWEBP_LIBRARY_RELEASE=${LIBWEBP}/${OS}/${RELEASE_DIR}/libwebp.lib)
+	WIN_dk_set				(LIBWEBP_CMAKE -DWEBP_INCLUDE_DIR=${WEBP_INCLUDE_DIR} -DWEBP_LIBRARY_DEBUG=${WEBP_LIBRARY_DEBUG} -DWEBP_LIBRARY_RELEASE=${WEBP_LIBRARY_RELEASE})
 elseif(ANDROID)
-	ANDROID_dk_set	(LIBWEBP_CMAKE -DWEBP_INCLUDE_DIR=${LIBWEBP} -DWEBP_LIBRARY_DEBUG=${LIBWEBP}/${OS}/${DEBUG_DIR}/libwebp.a -DWEBP_LIBRARY_RELEASE=${LIBWEBP}/${OS}/${RELEASE_DIR}/libwebp.a)
+	ANDROID_dk_set			(LIBWEBP_CMAKE -DWEBP_INCLUDE_DIR=${WEBP_INCLUDE_DIR} -DWEBP_LIBRARY_DEBUG=${WEBP_LIBRARY_DEBUG} -DWEBP_LIBRARY_RELEASE=${WEBP_LIBRARY_RELEASE})
 else()
-	dk_set			(LIBWEBP_CMAKE -DWEBP_INCLUDE_DIR=${LIBWEBP} -DWEBP_LIBRARY_DEBUG=${LIBWEBP}/${OS}/${DEBUG_DIR}/libwebpd.a -DWEBP_LIBRARY_RELEASE=${LIBWEBP}/${OS}/${RELEASE_DIR}/libwebp.a)
+	dk_set					(LIBWEBP_CMAKE -DWEBP_INCLUDE_DIR=${WEBP_INCLUDE_DIR} -DWEBP_LIBRARY_DEBUG=${WEBP_LIBRARY_DEBUG} -DWEBP_LIBRARY_RELEASE=${WEBP_LIBRARY_RELEASE})
 endif()
 
 
 
 ### GENERATE ###
-dk_queueCommand(${DKCMAKE_BUILD} ${GIFLIB_CMAKE} ${LIBJPEG_TURBO_CMAKE} ${LIBPNG_CMAKE} ${SDL_CMAKE} ${TIFF_CMAKE} ${ZLIB_CMAKE} ${LIBWEBP})
+dk_queueCommand				(${DKCMAKE_BUILD} ${GIFLIB_CMAKE} ${LIBJPEG_TURBO_CMAKE} ${LIBPNG_CMAKE} ${SDL_CMAKE} ${TIFF_CMAKE} ${ZLIB_CMAKE} ${LIBWEBP})
 
 
 ### COMPILE ###
-dk_build(${LIBWEBP} webp)
+dk_build					(${LIBWEBP} webp)
