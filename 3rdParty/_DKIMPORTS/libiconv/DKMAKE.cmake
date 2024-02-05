895c1227233b95448b77b3dbebcacdcf07ecf6c2
aquawicket <aquawicket@hotmail.com>
2024-02-05 15:00:28 -0800

    git commit

--- 3rdParty/_DKIMPORTS/libiconv/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libiconv/DKMAKE.cmake
@@ -1,7 +1,7 @@
 # https://www.gnu.org/software/libiconv/
 # https://savannah.gnu.org/projects/libiconv
 # https://stackoverflow.com/a/57734435/688352
-
+# https://cmake.org/cmake/help/latest/module/FindIconv.html
 
 ### IMPORT ###
 dk_import(http://ftp.vim.org/ftp/gnu/libiconv/libiconv-1.14.tar.gz PATCH)
@@ -50,9 +50,10 @@ else()
 		-DLIBICONV_PATH=${LIBICONV} 
 		-DICONV_DIR=${LIBICONV}
 		-DICONV_INCLUDE_DIR=${LIBICONV_INCLUDE_DIR}
-		-DICONV_LIBRARIES=${LIBICONV}/${OS}/${DEBUG_DIR}
+		-DICONV_LIBRARIES=${LIBICONV_CHARSET_LIBRARY_DEBUG};${LIBICONV_LIBRARY_DEBUG}
 		-DIconv_INCLUDE_DIR=${LIBICONV_INCLUDE_DIR}
-		-DIconv_LIBRARY=${LIBICONV_LIBRARY_DEBUG})
+		-DIconv_LIBRARY=${LIBICONV_LIBRARY_DEBUG}
+		-DIconv_LIBRARIES=${LIBICONV_CHARSET_LIBRARY_DEBUG};${LIBICONV_LIBRARY_DEBUG})
 	RELEASE_dk_set(LIBICONV_CMAKE 
 		-DLIBICONV_PATH=${LIBICONV} 
 		-DICONV_DIR=${LIBICONV}
