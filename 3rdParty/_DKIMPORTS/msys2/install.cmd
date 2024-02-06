e1831baa14bb89ebfc53f6b4afad0708ee8bbdb5
aquawicket <aquawicket@hotmail.com>
2024-02-06 07:56:24 -0800

    git commit

--- 3rdParty/_DKIMPORTS/msys2/install.cmd
+++ 3rdParty/_DKIMPORTS/msys2/install.cmd
@@ -4,3 +4,4 @@ if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit ) :: k
 set "DIGITALKNOB=%HOMEDRIVE%%HOMEPATH%\digitalknob"
 %DIGITALKNOB%\Development\DKCMake\dev\cmake_eval "include('%DIGITALKNOB%/Development/3rdParty/_DKIMPORTS/msys2/DKMAKE.cmake')"
 
+cmake -E create_symlink C:\Users\aquawicket\digitalknob\Development\3rdParty\libjpeg-turbo-main\win64\Debug\jconfig.h C:\Users\aquawicket\digitalknob\Development\3rdParty\libjpeg-turbo-main\win64\Debug\jconfig.h
