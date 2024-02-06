60455f2c8d525aa2a402dce52979511f30e14982
aquawicket <aquawicket@hotmail.com>
2024-02-05 18:23:17 -0800

    git commit

--- 3rdParty/_DKIMPORTS/harfbuzz/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/harfbuzz/DKMAKE.cmake
@@ -1,3 +1,14 @@
 # https://github.com/harfbuzz/harfbuzz.git
 
 dk_import(https://github.com/harfbuzz/harfbuzz.git BRANCH main)
+
+dk_libDebug			(${HARFBUZZ}/${OS}/${DEBUG_DIR}/libharfbuzz.lib)
+dk_libRelease		(${HARFBUZZ}/${OS}/${RELEASE_DIR}/libharfbuzz.lib)
+
+dk_queueCommand(${DKCMAKE_BUILD} ${HARFBUZZ})
+#HARFBUZZ_LIBRARY 
+#HARFBUZZ_INCLUDE_DIR
+
+
+dk_build(${HARFBUZZ})
+
