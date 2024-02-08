cf28c5a6dc898160222f9e12592face530f8de3b
aquawicket <aquawicket@hotmail.com>
2024-02-08 15:20:58 -0800

    added dk_getCmakeCommandLine

--- DKCMake/DKFunctions.cmake
+++ DKCMake/DKFunctions.cmake
@@ -3571,6 +3571,38 @@ endfunction()
 
 
 ###############################################################################
+# dk_getCmakeCommandLine(RESULT)
+#	
+#	TODO
+#
+#	@RESULT			- TODO
+#
+function(dk_getCmakeCommandLine RESULT)
+	DKDEBUGFUNC(${ARGV})
+	if(NOT CMAKE_ARGC)
+		dk_info("dk_getCmakeCommandLine(): CMAKE_ARGC not set")
+		dk_info("CMAKE_ARG variables are only set in cmake script mode")
+		return()
+	endif()
+	
+	set(PASSED_PARAMETERS "") # it will contain all params string
+	set(ARG_NUM 1) # current index, starting with 1 (0 ignored)
+	#math(EXPR ARGC_COUNT "${CMAKE_ARGC}")
+	set(ARGC_COUNT "${CMAKE_ARGC}")
+	
+	while(ARG_NUM LESS ARGC_COUNT)	
+		set(CURRENT_ARG ${CMAKE_ARGV${ARG_NUM}})					# making current arg named "CMAKE_ARGV" + "CURRENT_INDEX"
+		set(PASSED_PARAMETERS ${PASSED_PARAMETERS} ${CURRENT_ARG})  # adding current param to the list
+		math(EXPR ARG_NUM "${ARG_NUM}+1") 							# incrementing current index
+	endwhile()
+	
+	dk_info("PASSED_PARAMETERS = ${PASSED_PARAMETERS}")
+	dk_wait()
+	
+	set(${RESULT} "${PASSED_PARAMETERS}" PARENT_SCOPE) #return RESULT
+endfunction()
+
+###############################################################################
 # dk_printSettings()
 #
 #	TODO
@@ -3579,7 +3611,10 @@ endfunction()
 #
 function(dk_printSettings)
 	DKDEBUGFUNC(${ARGV})
+	
+	dk_getCmakeCommandLine(CMAKE_COMMAND_LINE)
 	dk_buildLog("#################  HOST SYSTEM VARIABLES  ################")
+	dk_buildLog("CMAKE_COMMAND_LINE:            ${CMAKE_COMMAND_LINE}")
 	dk_buildLog("CMAKE_COMMAND:                 ${CMAKE_COMMAND}")
 	dk_buildLog("CMAKE_EXE:                     ${CMAKE_EXE}")
 	dk_buildLog("CMAKE_VERSION:                 ${CMAKE_VERSION}")
