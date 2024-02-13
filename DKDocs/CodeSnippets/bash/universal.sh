eea0b187af5c0d850763f46dd70acf47bf03ff4b
aquawicket <aquawicket@hotmail.com>
2024-02-13 15:57:10 -0800

    added universal file, ablility to run single script in unix and windows environments

--- DKDocs/CodeSnippets/bash/universal.sh
+++ DKDocs/CodeSnippets/bash/universal.sh
@@ -5,9 +5,11 @@ GOTO :WINDOWS
 
 ::UNIX
 echo "This universal script is running on Unix"
+read -p "Press any key to continue ..."
 exit $?
 
 
 :WINDOWS
 ECHO This universal script is running on Windows
 pause
+exit
