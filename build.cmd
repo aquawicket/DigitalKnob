c638e87e93b25b1814035ecc58c68f7bc6ee452f
aquawicket <aquawicket@hotmail.com>
2024-02-13 18:43:02 -0800

    added comments to build.cmd

--- build.cmd
+++ build.cmd
@@ -483,6 +483,8 @@ goto:eof
 ::dk_call()
 :dk_call
 	::TODO
+	:: Here we try to use DK_call to pass on a call while echoing the call passed
+	:: This works in bash script, yet we have issues with this in cmd
 	echo %*
 	%*
 goto:eof
