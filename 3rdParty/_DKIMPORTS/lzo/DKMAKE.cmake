bddfedd6998f329138d8e53c78198efe8493aebe
aquawicket <aquawicket@hotmail.com>
2024-02-19 12:08:16 -0800

    still working on libxml2 builds

--- 3rdParty/_DKIMPORTS/lzo/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/lzo/DKMAKE.cmake
@@ -2,5 +2,8 @@
 # 
 # https://www.oberhumer.com/opensource/lzo
 # https://packages.msys2.org/base/lzo2
+# https://github.com/eyeseaevan/lzo-2.10.git
+# https://github.com/eyeseaevan/lzo-2.10.git
 
-dk_import(https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz)
+#dk_import(https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz)
+dk_import(https://github.com/eyeseaevan/lzo-2.10.git)
