59f25dfdd0a7baa09aa81267521021f97c428876
aquawicket <aquawicket@hotmail.com>
2024-02-26 22:45:02 -0800

    opened up all os_arch selections in build.sh

--- build.sh
+++ build.sh
@@ -253,31 +253,31 @@ function Pick_OS() {
 	echo ""
 	echo " 2) Android (arm32)"
 	echo " 3) Android (arm64)"
-	#echo " 4) Android (x86)"
-	#echo " 5) Android (x86_64)"
+	echo " 4) Android (x86)"
+	echo " 5) Android (x86_64)"
 	echo " 6) Emscripten"
 	echo " 7) iOS (arm32)"
 	echo " 8) iOS (arm64)"
-	#echo " 9) iOS (x86)"
-	#echo " 10) iOS (x86_64)"
-	#echo "11) iOS-Simulator (arm32)"
-	#echo "12) iOS-Simulator (arm64)"
+	echo " 9) iOS (x86)"
+	echo " 10) iOS (x86_64)"
+	echo "11) iOS-Simulator (arm32)"
+	echo "12) iOS-Simulator (arm64)"
 	echo "13) iOS-Simulator (x86)"
 	echo "14) iOS-Simulator (x86_64)"
-	#echo "15) Linux (arm32)"
-	#echo "16) Linux (arm64)"
+	echo "15) Linux (arm32)"
+	echo "16) Linux (arm64)"
 	echo "17) Linux (x86)"
 	echo "18) Linux (x86_64)"
-	#echo "19) Mac (arm32)"
-	#echo "20) Mac (arm64)"
+	echo "19) Mac (arm32)"
+	echo "20) Mac (arm64)"
 	echo "21) Mac (x86)"
 	echo "22) Mac (x86_64)"
 	echo "23) Raspberry (arm32)"
 	echo "24) Raspberry (arm64)"
-	#echo "25) Raspberry (x86)"
-	#echo "26) Raspberry (x86_64)"
-	#echo "27) Windows (arm32)"
-	#echo "28) Windows (arm64)"
+	echo "25) Raspberry (x86)"
+	echo "26) Raspberry (x86_64)"
+	echo "27) Windows (arm32)"
+	echo "28) Windows (arm64)"
 	echo "29) Windows (x86)"
 	echo "30) Windows (x86_64)"
 	echo "31) Clear Screen"
@@ -292,40 +292,40 @@ function Pick_OS() {
 		TARGET_OS="android_arm32"
 	elif [ "$input" == "3" ]; then
 		TARGET_OS="android64"
-	#elif [ "$input" == "4" ]; then
-	#	TARGET_OS="android_x86"
-	#elif [ "$input" == "5" ]; then
-	#	TARGET_OS="android_x64"
+	elif [ "$input" == "4" ]; then
+		TARGET_OS="android_x86"
+	elif [ "$input" == "5" ]; then
+		TARGET_OS="android_x64"
 	elif [ "$input" == "6" ]; then
 		TARGET_OS="emscripten"
 	elif [ "$input" == "7" ]; then
 		TARGET_OS="ios32"
 	elif [ "$input" == "8" ]; then
 		TARGET_OS="ios64"
-	#elif [ "$input" == "9" ]; then
-	#	TARGET_OS="ios_x86"
-	#elif [ "$input" == "10" ]; then
-	#	TARGET_OS="ios_x64"
-	#elif [ "$input" == "11" ]; then
-	#	TARGET_OS="iossim32"
-	#elif [ "$input" == "12" ]; then
-	#	TARGET_OS="iossim64"
+	elif [ "$input" == "9" ]; then
+		TARGET_OS="ios_x86"
+	elif [ "$input" == "10" ]; then
+		TARGET_OS="ios_x64"
+	elif [ "$input" == "11" ]; then
+		TARGET_OS="iossim32"
+	elif [ "$input" == "12" ]; then
+		TARGET_OS="iossim64"
 	elif [ "$input" == "13" ]; then
 		TARGET_OS="iossim32"
 	elif [ "$input" == "14" ]; then
 		TARGET_OS="iossim64"
 	elif [ "$input" == "15" ]; then
-	#	TARGET_OS="linux_arm32"
-	#elif [ "$input" == "16" ]; then
-	#	TARGET_OS="linux_arm64"
+		TARGET_OS="linux_arm32"
+	elif [ "$input" == "16" ]; then
+		TARGET_OS="linux_arm64"
 	elif [ "$input" == "17" ]; then
 		TARGET_OS="linux32"
 	elif [ "$input" == "18" ]; then
 		TARGET_OS="linux64"
-	#elif [ "$input" == "19" ]; then
-	#	TARGET_OS="mac_arm32"
-	#elif [ "$input" == "20" ]; then
-	#	TARGET_OS="mac_arm64"
+	elif [ "$input" == "19" ]; then
+		TARGET_OS="mac_arm32"
+	elif [ "$input" == "20" ]; then
+		TARGET_OS="mac_arm64"
 	elif [ "$input" == "21" ]; then
 		TARGET_OS="mac32"
 	elif [ "$input" == "22" ]; then
@@ -334,14 +334,14 @@ function Pick_OS() {
 		TARGET_OS="raspberry32"
 	elif [ "$input" == "24" ]; then
 		TARGET_OS="raspberry64"
-	#elif [ "$input" == "25" ]; then
-	#	TARGET_OS="raspberry_x86"
-	#elif [ "$input" == "26" ]; then
-	#	TARGET_OS="raspberry_x64"
-	#elif [ "$input" == "27" ]; then
-	#	TARGET_OS="windows_arm32"
-	#elif [ "$input" == "28" ]; then
-	#	TARGET_OS="windows_arm64"
+	elif [ "$input" == "25" ]; then
+		TARGET_OS="raspberry_x86"
+	elif [ "$input" == "26" ]; then
+		TARGET_OS="raspberry_x64"
+	elif [ "$input" == "27" ]; then
+		TARGET_OS="windows_arm32"
+	elif [ "$input" == "28" ]; then
+		TARGET_OS="windows_arm64"
 	elif [ "$input" == "29" ]; then
 		TARGET_OS="windows32"
 	elif [ "$input" == "30" ]; then
