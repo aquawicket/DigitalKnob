0fa8d7ed7eab918161f86f596e4f4fd5c12c1183
aquawicket <aquawicket@hotmail.com>
2024-02-26 20:34:04 -0800

    enabled ogg, vorbis, flac for all output targets

--- DKCMake/DKDisabled.cmake
+++ DKCMake/DKDisabled.cmake
@@ -215,7 +215,7 @@ if(EMSCRIPTEN)
 	dk_disable(core_services)
 	dk_disable(core_video)
 	dk_disable(dukluv)
-	dk_disable(flac)				# ERROR: src/libFLAC/cpu.c:91:43 error: invalid output constraint '=a' in asm
+	#dk_disable(flac)				# ERROR: src/libFLAC/cpu.c:91:43 error: invalid output constraint '=a' in asm
 	dk_disable(fontconfig)
 	dk_disable(foundation)
 	dk_disable(game_controller)
@@ -241,7 +241,7 @@ if(EMSCRIPTEN)
 	dk_disable(msinttypes)
 	dk_disable(nasm)
 	dk_disable(ncurses)
-	dk_disable(ogg)
+	#dk_disable(ogg)
 	dk_disable(openblas)			# CMake Error: When cross compiling, a TARGET is required
 	dk_disable(opencv)				# ittnotify_config.h:376:12: error: call to undeclared function '__TBB_machine_fetchadd4'
 	dk_disable(openframeworks)
@@ -259,7 +259,7 @@ if(EMSCRIPTEN)
 	dk_disable(system_configuration)
 	dk_disable(tesseract)			# missing <allheaders.h>
 	dk_disable(uwebsockets)
-	dk_disable(vorbis)				# requires ogg
+	#dk_disable(vorbis)				# requires ogg
 	dk_disable(waave)
 	dk_disable(wasm3)
 	dk_disable(x265)
@@ -336,9 +336,9 @@ endif(IOS OR IOSSIM)
 
 # Disabled for iOS targets
 if(IOS AND NOT IOSSIM)  
-	dk_disable(flac)				# configure: error: C compiler cannot create executables
-	dk_disable(ogg)					# configure: error: C compiler cannot create executables
-	dk_disable(vorbis)				# configure: error: C compiler cannot create executables
+	#dk_disable(flac)				# configure: error: C compiler cannot create executables
+	#dk_disable(ogg)					# configure: error: C compiler cannot create executables
+	#dk_disable(vorbis)				# configure: error: C compiler cannot create executables
 endif(IOS AND NOT IOSSIM)
 
 
@@ -395,7 +395,7 @@ if(MAC)
 	dk_disable(DKVncClient)			# case value evaluates to 4294967295, which cannot be narrowed to type 'int'
 	dk_disable(aubio)				# breaks DKCefV8
 	dk_disable(dukluv)
-	dk_disable(flac)				# 'asm' undelcared identifier
+	#dk_disable(flac)				# 'asm' undelcared identifier
 	dk_disable(fontconfig)
 	dk_disable(gdal)
 	dk_disable(gzip)
