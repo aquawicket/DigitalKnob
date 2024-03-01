d5b02b00e24479319d41ec05b88ea823a88d126a
aquawicket <aquawicket@hotmail.com>
2024-02-29 18:43:36 -0800

    did some work to finding CMAKE_EXE

--- 3rdParty/_DKIMPORTS/cmake/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/cmake/DKMAKE.cmake
@@ -5,6 +5,10 @@
 # https://discourse.cmake.org/t/cmake-silent-install-with-options-help/1475/2
 # https://askubuntu.com/questions/355565/how-do-i-install-the-latest-version-of-cmake-from-the-command-line 	# How to get latest version on ubuntu
 
+if(EXISTS ${CMAKE_EXE})
+	dk_debug("CMAKE_EXE already set to: ${CMAKE_EXE}")
+	return()
+endif()
 
 ### DOWNLOAD ###
 # https://github.com/Kitware/CMake/releases
@@ -18,110 +22,99 @@ WIN_X86_64_HOST_dk_set		(CMAKE_DL https://github.com/Kitware/CMake/releases/down
 get_filename_component(CMAKE_DL_FILE ${CMAKE_DL} NAME)
 dk_removeExtension(${CMAKE_DL_FILE} CMAKE_FOLDER)
 
-
 if(MSYSTEM)
 	dk_depend(msys2)
-	
-	dk_remove(${MSYS2}/var/lib/pacman/db.lck NOERROR)
-	
-	if(CLANG32)
-		dk_command("pacman -S mingw-w64-clang-i686-cmake --needed --noconfirm")		# CLANG32
-		dk_set(CMAKE ${MSYS2}/clang32/share/cmake)
-		dk_set(CMAKE_EXE ${MSYS2}/clang32/bin/cmake)
-		dk_set(CMAKE_MODULES ${MSYS2}/clang32/share/cmake/Modules)
-	elseif(CLANG64)
-		dk_command("pacman -S mingw-w64-clang-x86_64-cmake --needed --noconfirm")	# CLANG64
-		dk_set(CMAKE ${MSYS2}/clang64/share/cmake)
-		dk_set(CMAKE_EXE ${MSYS2}/clang64/bin/cmake)
-		dk_set(CMAKE_MODULES ${MSYS2}/clang64/share/cmake/Modules)
-	elseif(CLANGARM64)
-		dk_command("pacman -S mingw-w64-clang-aarch64-cmake --needed --noconfirm")	# CLANGARM64
-		dk_set(CMAKE ${MSYS2}/clangarm64/share/cmake)
-		dk_set(CMAKE_EXE ${MSYS2}/clangarm64/bin/cmake)
-		dk_set(CMAKE_MODULES ${MSYS2}/clangarm64/share/cmake/Modules)
-	elseif(MINGW32)
-		dk_command("pacman -S mingw-w64-i686-cmake --needed --noconfirm")			# MINGW32
-		dk_set(CMAKE ${MSYS2}/mingw32/share/cmake)
-		dk_set(CMAKE_EXE ${MSYS2}/mingw32/bin/cmake)
-		dk_set(CMAKE_MODULES ${MSYS2}/mingw32/share/cmake/Modules)
-	elseif(MINGW64)
-		dk_command("pacman -S mingw-w64-x86_64-cmake --needed --noconfirm")			# MINGW64
-		dk_set(CMAKE ${MSYS2}/mingw64/share/cmake)
-		dk_set(CMAKE_EXE ${MSYS2}/mingw64/bin/cmake)
-		dk_set(CMAKE_MODULES ${MSYS2}/mingw64/share/cmake/Modules)
-	elseif(UCRT64)
-		dk_command("pacman -S mingw-w64-ucrt-x86_64-cmake --needed --noconfirm")	# UCRT64
-		dk_set(CMAKE ${MSYS2}/ucrt64/share/cmake)
-		dk_set(CMAKE_EXE ${MSYS2}/ucrt64/bin/cmake)
-		dk_set(CMAKE_MODULES ${MSYS2}/ucrt64/share/cmake/Modules)
+	if(NOT MSYS2)
+		dk_error("MSYS2:${MSYS2} is empty")
 	endif()
 	
-	#dk_set(CMAKE_EXE cmake)
+	execute_process(COMMAND which cmake.exe OUTPUT_VARIABLE CMAKE_EXE OUTPUT_STRIP_TRAILING_WHITESPACE)
+	dk_debug("which cmake.exe = ${CMAKE_EXE}")
 	
+	execute_process(COMMAND cygpath -m ${CMAKE_EXE} OUTPUT_VARIABLE CMAKE_EXE OUTPUT_STRIP_TRAILING_WHITESPACE)
+	dk_debug("cygpath -m = ${CMAKE_EXE}")
+	
+	if(NOT EXISTS ${CMAKE_EXE})
+		dk_remove(${MSYS2}/var/lib/pacman/db.lck NOERROR)
+		if(CLANG32)
+			dk_command("pacman -S mingw-w64-clang-i686-cmake --needed --noconfirm")		# CLANG32
+			dk_set(CMAKE_EXE ${MSYS2}/clang32/bin/cmake.exe)
+		elseif(CLANG64)
+			dk_command("pacman -S mingw-w64-clang-x86_64-cmake --needed --noconfirm")	# CLANG64
+			dk_set(CMAKE_EXE ${MSYS2}/clang64/bin/cmake.exe)
+		elseif(CLANGARM64)
+			dk_command("pacman -S mingw-w64-clang-aarch64-cmake --needed --noconfirm")	# CLANGARM64
+			dk_set(CMAKE_EXE ${MSYS2}/clangarm64/bin/cmake.exe)
+		elseif(MINGW32)
+			dk_command("pacman -S mingw-w64-i686-cmake --needed --noconfirm")			# MINGW32
+			dk_set(CMAKE_EXE ${MSYS2}/mingw32/bin/cmake.exe)
+		elseif(MINGW64)
+			dk_command("pacman -S mingw-w64-x86_64-cmake --needed --noconfirm")			# MINGW64
+			dk_set(CMAKE_EXE ${MSYS2}/mingw64/bin/cmake.exe)
+		elseif(UCRT64)
+			dk_command("pacman -S mingw-w64-ucrt-x86_64-cmake --needed --noconfirm")	# UCRT64
+			dk_set(CMAKE_EXE ${MSYS2}/ucrt64/bin/cmake.exe)
+		endif()
+	endif()
 else()
 	if(WIN_HOST)
 		dk_set(CMAKE_EXE ${3RDPARTY}/${CMAKE_FOLDER}/bin/cmake.exe)
 		if(NOT EXISTS ${CMAKE_EXE})
 			### INSTALL ###
 			dk_info("Installing CMake . . .")
-			dk_download(${CMAKE_DL} ${DKDOWNLOAD})
-			get_filename_component(CMAKE_DL_FILE ${CMAKE_DL} NAME)
-			
+			dk_download(${CMAKE_DL} ${DKDOWNLOAD})			
 			file(TO_NATIVE_PATH "${DKDOWNLOAD}/${CMAKE_DL_FILE}" CMAKE_INSTALL_FILE)
 			file(TO_NATIVE_PATH "${3RDPARTY}/${CMAKE_FOLDER}" CMAKE_INSTALL_PATH)
-			execute_process(COMMAND cmd /c echo MsiExec.exe /i "${CMAKE_INSTALL_FILE}" INSTALL_ROOT=${CMAKE_INSTALL_PATH})
 			execute_process(COMMAND cmd /c MsiExec.exe /i "${CMAKE_INSTALL_FILE}" INSTALL_ROOT=${CMAKE_INSTALL_PATH})
-		endif()
-		if(NOT EXISTS ${CMAKE_EXE})
-			dk_error("cannot find cmake")
 		endif()
 	else()
 		execute_process(COMMAND which cmake OUTPUT_VARIABLE CMAKE_EXE OUTPUT_STRIP_TRAILING_WHITESPACE)
-		#string(STRIP ${CMAKE_EXE} CMAKE_EXE)
+		dk_debug("which cmake = ${CMAKE_EXE}")
 		if(NOT EXISTS ${CMAKE_EXE})
-			dk_error("cannot find cmake")
+			MAC_HOST_dk_command(brew install cmake)
+			ANDROID_HOST_dk_command(apt -y install cmake)
+			LINUX_HOST_dk_command(sudo apt-get -y install cmake)
+			execute_process(COMMAND which cmake OUTPUT_VARIABLE CMAKE_EXE OUTPUT_STRIP_TRAILING_WHITESPACE)
+			dk_debug("which cmake = ${CMAKE_EXE}")
 		endif()
 	endif()
-	
-#	if(EXISTS /usr/bin/cmake)
-#		dk_set(CMAKE /usr/bin)
-#		dk_set(CMAKE_EXE /usr/bin/cmake)
-#	elseif(EXISTS /usr/local/bin/cmake)
-#		dk_set(CMAKE /usr/local/bin)
-#		dk_set(CMAKE_EXE /usr/local/bin/cmake)
-#	elseif(EXISTS /data/data/com.termux/files/usr/bin/cmake)
-#		dk_set(CMAKE /data/data/com.termux/files/usr/bin)
-#		dk_set(CMAKE_EXE /data/data/com.termux/files/usr/bin/cmake)
-
-#		### INSTALL ###
-#		dk_info("Installing CMake . . .")
-#		MAC_HOST_dk_command(brew install cmake)
-#		if("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Android")
-#			dk_set(APT "apt")
-#		else()
-#			dk_set(APT "apt-get")
-#		endif()
-#		LINUX_HOST_dk_command(sudo ${APT} -y install cmake)
-#	endif()
-#
-#	if(EXISTS /usr/bin/cmake)
-#		dk_set(CMAKE /usr/bin)
-#		dk_set(CMAKE_EXE /usr/bin/cmake)
-#	elseif(EXISTS /usr/local/bin/cmake)
-#		dk_set(CMAKE /usr/local/bin)
-#		dk_set(CMAKE_EXE /usr/local/bin/cmake)
-#	elseif(EXISTS /data/data/com.termux/files/usr/bin/cmake)
-#		dk_set(CMAKE /data/data/com.termux/files/usr/bin)
-#		dk_set(CMAKE_EXE /data/data/com.termux/files/usr/bin/cmake)
-#	else()
-#		dk_error("CMAKE NOT FOUND!")
-#	endif()
 endif()
 
 
+if(NOT CMAKE_COMMAND)
+	dk_error("CMAKE_COMMAND:${CMAKE_COMMAND} is empty")
+#else()
+#	dk_debug("CMAKE_COMMAND = ${CMAKE_COMMAND}")
+endif()
+
+if(NOT EXISTS ${CMAKE_COMMAND})
+	dk_error("CMAKE_COMMAND:${CMAKE_COMMAND} does not exist")
+#else()
+#	dk_debug("CMAKE_COMMAND = ${CMAKE_COMMAND}")
+endif()
+
+if(NOT CMAKE_EXE)
+	dk_warn("CMAKE_EXE:${CMAKE_EXE} is empty. setting to ${CMAKE_COMMAND}")
+	set(CMAKE_EXE "${CMAKE_COMMAND}" CACHE INTERNAL "" FORCE)
+	#dk_debug("CMAKE_EXE = ${CMAKE_EXE}")
+endif()
+
+if(NOT CMAKE_EXE)
+	dk_warn("CMAKE_EXE:${CMAKE_EXE} is empty")
+#else()
+#	dk_debug("CMAKE_EXE = ${CMAKE_EXE}")
+endif()
+
+if(NOT EXISTS ${CMAKE_EXE})
+	dk_error("CMAKE_EXE:${CMAKE_EXE} does not exist")
+#else()
+#	dk_debug("CMAKE_EXE = ${CMAKE_EXE}")
+endif()
+		
 ###### CMake variables #######
-dk_debug("CMAKE_COMMAND = ${CMAKE_COMMAND}")
-dk_debug("CMAKE = ${CMAKE}")
+#dk_debug("CMAKE_COMMAND = ${CMAKE_COMMAND}")
+#dk_command(${CMAKE_COMMAND} --version)
+
 dk_debug("CMAKE_EXE = ${CMAKE_EXE}")
 dk_command(${CMAKE_EXE} --version)
 
