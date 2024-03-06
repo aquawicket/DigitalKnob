b79d5806ab4b3c4560c248db0e5f3cb9e198ebdb
aquawicket <aquawicket@hotmail.com>
2024-03-05 23:00:27 -0800

    added more dk_func files

---
+++ DKCMake/functions/dk_getParameter.cmake
@@ -0,0 +1,32 @@
+include_guard()
+message(STATUS "****** LOADING: ${CMAKE_CURRENT_LIST_FILE} ******")
+
+
+###############################################################################
+# dk_getParameter(name RESULT ${ARGV})
+#
+#	get a parameter by name from within a function
+#
+#	@name		-The input MARKER name for the parameter
+#	@RESULT	-The value of the next parameter after the MARKER
+#	${ARGV}		-The arg list from the calling function
+#
+macro(dk_getParameter name RESULT)
+	#DKDEBUGFUNC(${ARGV})
+	#dk_debug("ARGV = ${ARGV}")
+	#dk_debug("ARGN = ${ARGN}")
+	set(index 0)
+	foreach(arg ${ARGN})
+		#dk_debug(index PRINTVAR)
+		#dk_debug(arg PRINTVAR)
+		#dk_debug("ARGV${index} = ${ARGV${index}}")
+		if("${ARGV${index}}" STREQUAL "${name}")
+		#if("${arg}" STREQUAL "${name}")
+			math(EXPR value "${index}+1")
+			set(${RESULT} ${ARGV${value}})
+			#dk_debug("dk_getParameter(${name}) = ${${RESULT}}")
+		endif()
+		math(EXPR index "${index}+1")
+	endforeach()
+	#dk_debug("dk_getParameter(${name}) = ${${RESULT}}")
+endmacro()
---
+++ DKCMake/functions/dk_remove.cmake
@@ -0,0 +1,29 @@
+include_guard()
+message(STATUS "****** LOADING: ${CMAKE_CURRENT_LIST_FILE} ******")
+
+
+###############################################################################
+# dk_remove(path [NOERROR])
+#
+#	Remove a file or directory
+#
+#	@path		- The full path to the file or direcotory to remove
+#   NOERROR     - if NOERROR is specified in the parameters, dk_error() messages will not be displayed
+#
+function(dk_remove path)
+	DKDEBUGFUNC(${ARGV})
+	dk_includes("${ARGN}" "NOERROR" has_NOERROR)
+	if(${has_NOERROR})
+		set(noerror true)
+	endif()
+	if(NOT EXISTS ${path})
+		if(NOT noerror)
+			dk_error("${path} does not exist")
+		endif()
+		return()
+	endif()
+	file(REMOVE_RECURSE ${path})
+	if(EXISTS ${path})
+		dk_error("failed to remove ${path}")
+	endif()
+endfunction()
