a512c593e67f012247b0c12075039b98fc071460
aquawicket <aquawicket@hotmail.com>
2024-03-06 03:15:43 -0800

    added missing dk_cmake function files

---
+++ DKCMake/functions/dk_rename.cmake
@@ -0,0 +1,40 @@
+include_guard()
+message(STATUS "****** LOADING: ${CMAKE_CURRENT_LIST_FILE} ******")
+
+
+###############################################################################
+# dk_rename(from to) OVERWRITE NOERROR
+#
+#	Rename file or directory or move a file or directory to another location
+#
+#	@from		- The source path to copy
+#	@to			- The destination path to copy to
+#	OVERWRITE	- if any of the parameters equals OVERWRITE, overwritting existing files is enabled
+#   NOERROR     - if any of the parameters equals NOERROR, dk_error() messages will not be displayed
+#
+function(dk_rename from to) # FLAGS: OVERWRITE, NOERROR
+	DKDEBUGFUNC(${ARGV})
+	dk_includes("${ARGN}" "OVERWRITE" includes)
+	if(${includes})
+		set(overwrite true)
+	endif()
+	dk_includes("${ARGN}" "NOERROR" includes)
+	if(${includes})
+		set(noerror true)
+	endif()
+	dk_info("Renameing ${from} to ${to}")
+	if(NOT EXISTS ${from})
+		if(NOT noerror)
+			dk_error("from:${from} not found")
+		endif()
+		return()
+	endif()
+	if(EXISTS ${to})
+		if(NOT ${overwrite})
+			dk_error("Cannot rename file. Destiantion exists and not set to overwrite")
+		endif()
+		dk_remove(${to})
+	endif()
+	file(RENAME ${from} ${to})
+endfunction()
+dk_createOsMacros("dk_rename")
---
+++ DKCMake/functions/dk_upxCompress.cmake
@@ -0,0 +1,17 @@
+include_guard()
+message(STATUS "****** LOADING: ${CMAKE_CURRENT_LIST_FILE} ******")
+
+
+###############################################################################
+# dk_upxCompress(path)
+#
+#	UPX compress an executable or dynamic library file
+#
+#	@path		- The full path to the binary file to compress with UPX
+#
+function(dk_upxCompress path)
+	DKDEBUGFUNC(${ARGV})
+	dk_info("UPX compressing ${path}...")
+	dk_info("Please wait...")
+	dk_executeProcess("${UPX_EXE} -9 -v ${path}")
+endfunction()
