b2af95c0482aecb26ca53b55a44fb78f2a3abcad
aquawicket <aquawicket@hotmail.com>
2024-03-16 02:29:17 -0700

    added missing cmake function file

---
+++ DKCMake/functions/dk_createSmartObject.cmake
@@ -0,0 +1,16 @@
+include_guard()
+
+###############################################################################
+# dk_createSmartObject(object)
+#
+#	TODO
+#	We can scan each plugin and attempt to create it's variable and infomation manually here
+#
+#	@object		- TODO
+#
+function(dk_createSmartObject object)
+	DKDEBUGFUNC(${ARGV})
+	dk_todo()
+	dk_verbose("dk_createSmartObject(${object})")
+	# We require something that can resolve to a full, valid path containing a DKMAKE.cmake file 
+endfunction()
