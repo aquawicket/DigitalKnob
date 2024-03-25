83d068cce688f1a7ef569f5831ac1ca55ad773e5
aquawicket <aquawicket@hotmail.com>
2024-03-25 00:24:30 -0700

    upgrading input_timeout

--- DKBatch/input_timeout.cmd
+++ DKBatch/input_timeout.cmd
@@ -9,9 +9,12 @@ if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 
 
 :::: INPUT_TIMEOUT ::::
+:input_timeout
 echo.
 echo A :::: INPUT_TIMEOUT :::: "%1"
 set DEADLINE=10
+set /a countdown=%DEADLINE%
+
 if "%1" NEQ "" goto %1
 
 
@@ -19,38 +22,47 @@ if "%1" NEQ "" goto %1
 :input_timeout_mainthread
 echo B :input_timeout_mainthread
 del input_timeout.tmp 2>nul >nul
+
 start /b cmd /c %0 :input_timeout_secondThread
 set n=0
-echo Enter Text (%DEADLINE% seconds timeout):
+echo Enter Text (%DEADLINE% second timeout):
 
 
 
 :input_timeout_loop
-::echo C :input_timeout_loop
-set /a second+=1
-ping -n 2 localhost > nul
-if !second! LSS %DEADLINE% (
-    if not exist input_timeout.tmp goto :input_timeout_loop
-    < input_timeout.tmp (
-        set /p input=
-    )
-	set result=!input!
-	goto input_timeout_result
-   
-) ELSE (
-	set result=default
-	goto input_timeout_result
-)
+	::echo C :input_timeout_loop
+	set /a second+=1
+	TITLE !countdown!
+	set /a countdown-=1
+	ping -n 2 localhost > nul
+	if !second! LSS %DEADLINE% (
+		TITLE !countdown!
+		if not exist input_timeout.tmp goto :input_timeout_loop
+		< input_timeout.tmp (
+			set /p input=
+		)
+		set result=!input!
+		goto input_timeout_result
+	   
+	) ELSE (
+		set result=default
+		goto input_timeout_result
+	)
 goto:eof
+
 
 
 :input_timeout_secondThread
-echo B :input_timeout_secondThread
-set /p var=
-> input_timeout.tmp echo !var!
+	echo B :input_timeout_secondThread
+	set /p var=
+	> input_timeout.tmp echo !var!
 goto:eof
 
 
+
 :input_timeout_result
-echo D :input_timeout_result
-echo result = %result%
+	echo D :input_timeout_result
+	echo.
+	echo result = %result%
+	TITLE %result%
+goto:eof
