00dd4b657be02f04c0d62dba79427b20668b8eac
aquawicket <aquawicket@hotmail.com>
2024-03-28 15:55:40 -0400

    dk_find_program working better

--- 3rdParty/_DKIMPORTS/make/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/make/DKMAKE.cmake
@@ -7,25 +7,25 @@ if(MSYSTEM)
 	
 	if(CLANG32)
 		dk_command(pacman -S mingw-w64-clang-i686-make --needed --noconfirm)			# CLANG32
-		dk_set(MAKE_PROGRAM ${MSYS2}/clang32/bin/mingw32-make.exe)
+		dk_find_program(MAKE_PROGRAM mingw32-make "${MSYS2}/clang32/bin")
 	elseif(CLANG64)
 		dk_command(pacman -S mingw-w64-clang-x86_64-make --needed --noconfirm)			# CLANG64
-		dk_set(MAKE_PROGRAM ${MSYS2}/clang64/bin/mingw32-make.exe)
+		dk_find_program(MAKE_PROGRAM mingw32-make "${MSYS2}/clang64/bin")
 	elseif(CLANGARM64)
 		dk_command(pacman -S mingw-w64-clang-aarch64-make --needed --noconfirm)			# CLANGARM64
-		dk_set(MAKE_PROGRAM ${MSYS2}/clangarm64/bin/mingw32-make.exe)
+		dk_find_program(MAKE_PROGRAM mingw32-make "${MSYS2}/clangarm64/bin")
 	elseif(MINGW32)
 		dk_command(pacman -S mingw-w64-i686-make --needed --noconfirm)					# MINGW32
-		dk_set(MAKE_PROGRAM ${MSYS2}/mingw32/bin/mingw32-make.exe)
+		dk_find_program(MAKE_PROGRAM mingw32-make "${MSYS2}/mingw32/bin")
 	elseif(MINGW64)
 		dk_command(pacman -S mingw-w64-x86_64-make --needed --noconfirm)				# MINGW64
-		dk_set(MAKE_PROGRAM ${MSYS2}/mingw64/bin/mingw32-make.exe)
+		dk_find_program(MAKE_PROGRAM mingw32-make "${MSYS2}/mingw64/bin")
 	elseif(UCRT64)
 		dk_command(pacman -S mingw-w64-ucrt-x86_64-make --needed --noconfirm)			# UCRT64
-		dk_set(MAKE_PROGRAM ${MSYS2}/ucrt64/bin/mingw32-make.exe)
+		dk_find_program(MAKE_PROGRAM mingw32-make "${MSYS2}/ucrt64/bin")
 	endif()
 endif()
 
 #if(EMSCRIPTEN)
-#	dk_set(MAKE_PROGRAM ${EMSDK}/mingw/4.6.2_32bi/mingw32-make.exe)
+#	dk_find_program(MAKE_PROGRAM mingw32-make "${EMSDK}/mingw/4.6.2_32bit")
 #endif()
--- DKCMake/functions/dk_find_program.cmake
+++ DKCMake/functions/dk_find_program.cmake
@@ -5,11 +5,22 @@ include_guard()
 #
 #	TODO
 #
-#	@filename	- TODO
-#	@OUTPUT		- TODO
+#	<VAR>				- TODO
+#	name				- TODO
+# 	[path1 path2 ...]	- TODO
 #
 function(dk_find_program VAR name)
+	dk_debug(" ")
 	dk_debug("dk_find_program(${ARGV})")
+	if(EXISTS ${${VAR}})
+		dk_debug("already FOUND ${name} at ${${VAR}}")
+		return()
+	endif()
+	
+	if("${${VAR}}" STREQUAL "${VAR}-NOTFOUND")
+		set(${VAR}_second_pass 1)
+	endif()
+	
 	if(${VAR})
 		dk_error("error {VAR} already set to ${VAR}")
 	endif()
@@ -19,16 +30,16 @@ function(dk_find_program VAR name)
 	else()
 		find_program(${VAR} "${name}")
 	endif()
-	dk_debug(VAR PRINTVAR)
 	
-	
-	if(VAR)
+	if(EXISTS ${${VAR}})
 		dk_debug("FOUND ${name} at ${${VAR}}")
-	else()
-		if(DEFINED ${VAR})
-			dk_error("COULD NOT FIND ${name}")
-		endif()
-		dk_notice("COULD NOT FIND ${name}")
+		dk_set(${VAR} "${${VAR}}")
+		return()
+	elseif(${VAR}_second_pass)
+		dk_error("COULD NOT FIND ${name}")
+		return()
 	endif()
+	
+	dk_notice("COULD NOT FIND ${name}")
 	set(${VAR} "${${VAR}}" PARENT_SCOPE)
 endfunction()
