b42e58d0b22f10ea409d34f9973caf55299f527a
aquawicket <aquawicket@hotmail.com>
2024-04-22 20:47:11 -0400

    code cleanup

--- DKBash/functions/DK.sh
+++ DKBash/functions/DK.sh
@@ -5,17 +5,8 @@
 #echo "basename: [$(basename "$0")]"
 #echo "dirname : [$(dirname "$0")]"
 #echo "pwd     : [$(pwd)]"
-
-#. DKBash/functions/dk_realpath.sh
-dk_realpath() {
-    [ $1 = /* ] && echo "$1" || echo "$PWD/${1#./}"
-}
-SCRIPT_PATH=$(dk_realpath $0)
-[ -n "$(command -v "cygpath")" ] && SCRIPT_PATH=$(cygpath -u "$SCRIPT_PATH")
-SCRIPT_DIR=$(dirname $SCRIPT_PATH)
-SCRIPT_NAME=$(basename $SCRIPT_PATH)
-
 #export PS4=$'+\e[33m ${BASH_SOURCE[0]:-nofile}:${BASH_LINENO[0]:-noline} ${FUNCNAME[0]:-nofunc}()\e[0m  '
+
 
 ###### Reload Main Script with bash ######
 if [ ${RELOAD_WITH_BASH-1} = 1 ]; then
@@ -31,29 +22,41 @@ if [ ${RELOAD_WITH_BASH-1} = 1 ]; then
 	fi
 fi
 
-[ -n "$DKINIT" ] && return || readonly DKINIT=1     # dk_includeGuard()
+###### include guard ######
+[ -n "$DKINIT" ] && return || readonly DKINIT=1
+
+
+###### get SCRIPT_ variables  ######
+#. DKBash/functions/dk_realpath.sh
+dk_realpath() {
+    [ $1 = /* ] && echo "$1" || echo "$PWD/${1#./}"
+}
+SCRIPT_PATH=$(dk_realpath $0)
+[ -n "$(command -v "cygpath")" ] && SCRIPT_PATH=$(cygpath -u "$SCRIPT_PATH")
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+SCRIPT_NAME=$(basename $SCRIPT_PATH)
+
 
 ###### Set and check posix mode ######
 $(set -o posix) && set -o posix && case :$SHELLOPTS: in
   *:posix:*) echo "POSIX mode enabled" ;;
   *)         echo "POSIX mode not enabled" ;;
 esac
+
+
+###### Set error trace options ######
 $(set -o pipefail) && set -o pipefail  	# trace ERR through pipes
 $(set -o errtrace) && set -o errtrace 	# trace ERR through 'time command' and other functions
 #$(set -o nounset) && set -o nounset  	# set -u : exit the script if you try to use an uninitialised variable
 #$(set -o errexit) && set -o errexit  	# set -e : exit the script if any statement returns a non-true
 
-###### Global Script Variables ######
-#export LOG_VERBOSE=1
-#export LOG_DEBUG=1
-#export TRACE_ON_WARNINGS=1
-#export HALT_ON_WARNINGS=0
-#export CONTINUE_ON_ERRORS=0
+
+###### set true and false variables ######
 readonly true=0
 readonly false=1
 
 
-###### Script internal setup ######
+###### get shell type ######
 [ -d "/proc" ] && shell_type=$(basename $(readlink /proc/$$/exe))
 [ "$SHELL" = "/bin/zsh" ] && shell_type="zsh"
 #echo "shell_type = $shell_type"
@@ -61,15 +64,16 @@ readonly false=1
 [ $shell_type = dash ] && export DKDASH=1 && export ext=sh
 [ $shell_type = zsh ] && export DKZSH=1 && export ext=bash
 [ $shell_type = bash ] && export DKBASH=1 && export ext=bash
-
 #[ $DKSH ]   && echo "DKSH"
 #[ $DKDASH ] && echo "DKDASH"
 #[ $DKZSH ] && echo "DKZSH"
 #[ $DKBASH ] && echo "DKBASH"
 
+
 ###### get DKBASH_DIR ######
 export BASH_SOURCE_DIR=$( cd -- "$(dirname "$BASH_SOURCE")" >/dev/null 2>&1 ; pwd -P )
 export DKBASH_DIR=$( cd -- "$(dirname "$BASH_SOURCE_DIR")" >/dev/null 2>&1 ; pwd -P )
+
 
 ###### Script loader ######
 . ${DKBASH_DIR}/functions/dk_load.${ext}
