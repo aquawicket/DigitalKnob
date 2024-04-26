b9d6a9eb8263f5a17327a73668aafe2385bd0639
aquawicket <aquawicket@hotmail.com>
2024-04-26 02:01:51 -0400

    readded dk_debugFunc for proper letter case

---
+++ DKCMake/functions/dk_debugFunc.cmake
@@ -0,0 +1,46 @@
+include_guard()
+
+##################################################################################
+# dk_debugFunc(${ARGV})
+#
+#	Prints the current file name, line number, function or macro and arguments
+#	Place this at the first line of every function you want to see debug output for.
+# 
+#	Example:
+#		function(MyFunction myArg1 myArg2)
+#			dk_debugFunc(${ARGV}) 
+#			## user code
+#		endfunction()
+#
+macro(dk_debugFunc)
+	#dk_debugFunc(${ARGV})
+	
+	if(DKDEBUGFUNC_ENABLED)
+		if(NOT CMAKE_CURRENT_FUNCTION_LIST_FILE)
+			set(CMAKE_CURRENT_FUNCTION_LIST_FILE "unknown")
+		endif()
+		get_filename_component(FILENAME ${CMAKE_CURRENT_FUNCTION_LIST_FILE} NAME)
+		#dk_getFilename(${CMAKE_CURRENT_FUNCTION_LIST_FILE} FILENAME)
+		if(${ARGC} LESS 1)
+			dk_updateLogInfo()
+			message(STATUS "${H_black}${FILENAME}:${CMAKE_CURRENT_FUNCTION_LIST_LINE}->${clr}${cyan}${CMAKE_CURRENT_FUNCTION}()${clr}")
+		else()
+			set(argIndex 1)
+			set(argString " {")
+			dk_getArgIdentifiers(${ARGV})
+			foreach(arg ${ARGV})
+				#set(argString "${argString}\"${ARGI${argIndex}}\":\"${arg}\"")
+				set(argString "${argString} ${arg}\"")
+				if(${argIndex} LESS ${ARGC})
+					set(argString "${argString},  ")
+				endif()
+				math(EXPR argIndex "${argIndex}+1")
+			endforeach()
+			set(argString "${argString}} ")
+			message(STATUS "${H_black}${FILENAME}:${CMAKE_CURRENT_FUNCTION_LIST_LINE}->${clr}${cyan}${CMAKE_CURRENT_FUNCTION}(${argString})${clr}")
+		endif()
+	endif()
+endmacro()
+#macro(DKDEBUGFUNC)
+#	dk_debugFunc(${ARGV})
+#endmacro()
