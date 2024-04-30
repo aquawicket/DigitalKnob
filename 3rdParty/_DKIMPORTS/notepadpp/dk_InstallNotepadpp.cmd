5d72e6c829f65fb6c498d844fade620467a29539
aquawicket <aquawicket@hotmail.com>
2024-04-30 01:58:24 -0400

    added dk_InstallNotepadpp.cmd to 3rdParty/notepadpp

---
+++ 3rdParty/_DKIMPORTS/notepadpp/dk_InstallNotepadpp.cmd
@@ -0,0 +1,18 @@
+@echo off
+call ../../../DKBatch/functions/DK.cmd
+call dk_includeGuard
+
+::####################################################################
+::# dk_cmakeNotepadpp()
+::#
+::#
+:dk_cmakeNotepadpp () {
+	call dk_debugFunc
+	
+	if "%DKIMPORTS_DIR%"==""  call dk_validateBranch
+    call dk_cmakeEval "dk_load('%DKIMPORTS_DIR%/notepadpp/DKMAKE.cmake')" "NOTEPADPP_EXE"
+	call dk_debug NOTEPADPP_EXE
+    call dk_checkError
+
+    cmd /k
+goto:eof
