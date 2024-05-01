1b452678e9cf3540ea1e3b140617470d4452c7a6
aquawicket <aquawicket@hotmail.com>
2024-05-01 03:51:17 -0400

    added dk_isEmpty.sh

---
+++ DKBash/functions/dk_isEmpty.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+[ -z "$DKINIT" ] && . ./DK.sh
+#dk_includeGuard
+
+##################################################################################
+# dk_isEmpty(directory)
+#
+#
+dk_isEmpty() {
+	#dk_debugFunc
+	
+	if [ -d "$1" ] && files=$(ls -qAH -- "$1") && [ -z "$files" ]; then
+		#printf '%s\n' "$dir is an empty directory"
+		return $true
+	else
+		#printf >&2 '%s\n' "$dir is not empty, or is not a directory" \
+        #           "or is not readable or searchable in which case" \
+        #            "you should have seen an error message from ls above."
+		return $false
+	fi
+}
+
+
+
+
+################################ DKTEST #########################################
+DKTEST () {
+	echo "PWD = $PWD"
+
+	mkdir empty
+	echo "The created empty folder is ...."
+    $(dk_isEmpty "$PWD/empty") && echo "Empty" || echo "NOT Empty"
+	echo "the current directory is ..."
+	$(dk_isEmpty "$PWD") && echo "Empty" || echo "NOT Empty"
+	
+	exec $SHELL
+}
+[ -n "$DKTEST" ] && DKTEST
