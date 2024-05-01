9d70e3fb7ad77337942bec3a741cc40e93460bb6
aquawicket <aquawicket@hotmail.com>
2024-04-30 20:07:11 -0400

    fixing dk_exit.sh

--- DKBash/functions/dk_exit.sh
+++ DKBash/functions/dk_exit.sh
@@ -10,7 +10,10 @@ dk_exit () {
 	dk_debugFunc
 	
 	echo "dk_exit $*"
-	exec $SHELL #$*
 	
-	#exit $*
+	# TODO: when open with icon, we can use exec to keep the window open
+	#exec $SHELL #$*
+	
+	# a pure exit is the default
+	exit $*
 }
