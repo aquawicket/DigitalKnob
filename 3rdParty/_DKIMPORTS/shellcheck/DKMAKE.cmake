6eeaf400e73043b1faaaec30ecd6c07fa693f15c
aquawicket <aquawicket@hotmail.com>
2024-05-05 12:52:50 -0400

    Added missing build file

---
+++ 3rdParty/_DKIMPORTS/shellcheck/DKMAKE.cmake
@@ -0,0 +1,8 @@
+# https://github.com/koalaman/shellcheck.git
+
+#scversion="stable" # or "v0.4.7", or "latest"
+#wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
+#cp "shellcheck-${scversion}/shellcheck" /usr/bin/
+#shellcheck --version
+
+#SHELLCHECK_VERSION="latest"
