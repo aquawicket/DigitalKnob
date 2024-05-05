7cdb2cb7c579a964d7b0c469ad17fb9f16e9964e
aquawicket <aquawicket@hotmail.com>
2024-05-05 10:44:37 -0400

    code cleanup

--- 3rdParty/_DKIMPORTS/shellcheck/install.sh
+++ 3rdParty/_DKIMPORTS/shellcheck/install.sh
@@ -11,41 +11,13 @@ dk_download "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHE
 
 
 ##### dk_install_extract(src_path dest_path) #####
-	dk_info "Extracting ${SHELLCHECK_DL_FILE}"
+dk_info "Extracting ${SHELLCHECK_DL_FILE}"
 	
-	dk_validate DK3RDPARTY_DIR "dk_validateBranch"
-	dest_path="${DK3RDPARTY_DIR}/shellcheck-${SHELLCHECK_VERSION}"
-	#dk_extract "${DKDOWNLOAD_DIR}/${SHELLCHECK_DL_FILE}" "${dest_path}"
-	dk_smartExtract "${DKDOWNLOAD_DIR}/${SHELLCHECK_DL_FILE}" "${dest_path}"
-	return
-	
-	
-	if dk_fileExists "${DKDOWNLOAD_DIR}/UNZIPPED"; then 
-		dk_remove "${DKDOWNLOAD_DIR}/UNZIPPED"
-	fi
-	dk_makeDirectory "${DKDOWNLOAD_DIR}/UNZIPPED"
-	
-	
-	
-	# We either have a root folder in /UNZIPPED, or multiple files without a root folder
-	cd "${DKDOWNLOAD_DIR}/UNZIPPED"
-	file_count=$(ls -1 | wc -l)
-	cd "${OLDPWD}"
-	dk_printVar file_count
+dk_validate DK3RDPARTY_DIR "dk_validateBranch"
+dest_path="${DK3RDPARTY_DIR}/shellcheck-${SHELLCHECK_VERSION}"
+dk_smartExtract "${DKDOWNLOAD_DIR}/${SHELLCHECK_DL_FILE}" "${dest_path}"
 
-	if [ ${file_count} -gt 2 ]; then ##NOTE: This should be "${count} GREATER 1" but msys has a readme file in it next to the inner msys folder and that messes things up for more than 1
-		#Zip extracted with no root folder, Rename UNZIPPED to dest_path
-		dk_rename "${DKDOWNLOAD_DIR}/UNZIPPED" "${dest_path}" # OVERWRITE
-	else 
-		if [ -e "${DKDOWNLOAD_DIR}/UNZIPPED/${dest_filename}" ]; then ##Zip extracted to expected folder. move
-			dk_rename "${DKDOWNLOAD_DIR}/UNZIPPED/${dest_filename}" "${dest_path}" # OVERWRITE
-			dk_remove "${DKDOWNLOAD_DIR}/UNZIPPED"
-		#file extracted to a root folder, but not named what we expected. rename
-		else
-			echo "TODO"
-		fi
-	fi
-
+	
 #cp "shellcheck-${scversion}/shellcheck" /usr/bin/
 #shellcheck --version
 
