de07864b9b99f3e777c55d0384178e760902edf6
aquawicket <aquawicket@hotmail.com>
2024-05-05 12:50:14 -0400

    correced dk_error file and line number

--- 3rdParty/_DKIMPORTS/shellcheck/install.sh
+++ 3rdParty/_DKIMPORTS/shellcheck/install.sh
@@ -4,21 +4,23 @@
 . ../../../DKBash/functions/DK
 
 SHELLCHECK_VERSION="v0.10.0"
-SHELLCHECK_DL_FILE="shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz"
+dk_validate HOST_OS "dk_getHostTriple"
+dk_printVar HOST_OS
+dk_printVar HOST_ARCH
+[ "${HOST_OS}_${HOST_ARCH}" == "linux_arm" ]    && SHELLCHECK_DL_FILE="shellcheck-${SHELLCHECK_VERSION}.linux.linux.armv6hf.tar.xz"
+[ "${HOST_OS}_${HOST_ARCH}" == "linux_arm64" ]  && SHELLCHECK_DL_FILE="shellcheck-${SHELLCHECK_VERSION}.linux.aarch64.tar.xz"
+[ "${HOST_OS}_${HOST_ARCH}" == "linux_x86_64" ] && SHELLCHECK_DL_FILE="shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz"
+[ "${HOST_OS}_${HOST_ARCH}" == "mac_x86_64" ]   && SHELLCHECK_DL_FILE="shellcheck-${SHELLCHECK_VERSION}.darwin.x86_64.tar.xz"
+[ "${HOST_OS}_${HOST_ARCH}" == "mac_arm64" ]    && SHELLCHECK_DL_FILE="shellcheck-${SHELLCHECK_VERSION}.darwin.aarch64.tar.xz"
+dk_assert SHELLCHECK_DL_FILE
 dk_printVar SHELLCHECK_DL_FILE
+
 dk_validate DKDOWNLOAD_DIR "dk_getDKPaths"
 dk_download "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/${SHELLCHECK_DL_FILE}" "${DKDOWNLOAD_DIR}/${SHELLCHECK_DL_FILE}"
 
-
-##### dk_install_extract(src_path dest_path) #####
-dk_info "Extracting ${SHELLCHECK_DL_FILE}"
-	
-dk_validate DK3RDPARTY_DIR "dk_validateBranch"
-dest_path="${DK3RDPARTY_DIR}/shellcheck-${SHELLCHECK_VERSION}"
+dest_path="${DKTOOLS_DIR}/shellcheck-${SHELLCHECK_VERSION}"
 dk_smartExtract "${DKDOWNLOAD_DIR}/${SHELLCHECK_DL_FILE}" "${dest_path}"
+SHELLCHECK_EXE="${dest_path}/shellcheck"
 
-	
 #cp "shellcheck-${scversion}/shellcheck" /usr/bin/
-#shellcheck --version
-
-exec $SHELL
+"${SHELLCHECK_EXE} --version"	
