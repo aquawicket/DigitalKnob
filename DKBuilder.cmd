9b9e0925e8129c44fd116d5ac43107ac44c04235
aquawicket <aquawicket@hotmail.com>
2024-05-06 19:38:27 -0400

    git commit

--- DKBuilder.cmd
+++ DKBuilder.cmd
@@ -1,12 +1,10 @@
 @echo off
 ::echo %~0(%*)
 
-::###### DK_SETUP ######
-::if not exist DKBatch mkdir DKBatch
-if not exist DKBatch\functions mkdir DKBatch\functions
-if not exist DKBatch\functions\DK.cmd powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKBatch/functions/DK.cmd', 'DKBatch\functions\DK.cmd')"
 
 ::###### DK_INIT ######
+if not exist "DKBatch\functions" mkdir DKBatch\functions
+if not exist "DKBatch\functions\DK.cmd" powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKBatch/functions/DK.cmd', 'DKBatch\functions\DK.cmd')"
 call "DKBatch/functions/DK.cmd"
 
 
