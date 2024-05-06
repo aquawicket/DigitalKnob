6daa14832ef8a612ad51aca86b5b8143e6e7dc18
aquawicket <aquawicket@hotmail.com>
2024-05-06 17:47:37 -0400

    code cleanup

--- DKBuilder.cmd
+++ DKBuilder.cmd
@@ -1,20 +1,18 @@
 @echo off
-::echo %~0(%*)
-
+echo %~0(%*)
 
 ::###### DK_SETUP ######
-::[ ! -e DKBash ] && mkdir DKBash
-::[ ! -e DKBash/functions ] && mkdir DKBash/functions
-::[ ! -e DKBash/functions/DK.sh ] && wget -P DKBash/functions https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKBash/functions/DK.sh
-
+::if not exist DKBatch mkdir DKBatch
+if not exist DKBatch\functions mkdir DKBatch\functions
+if not exist DKBatch\functions\DK.cmd powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKBatch/functions/DK.cmd', 'DKBatch\functions\DK.cmd')"
 
 ::###### DK_INIT ######
 call "DKBatch/functions/DK.cmd"
 
 
 ::###### Global variables ######
-call dk_printVar DKSCRIPT_PATH
-call dk_printVar DKBATCH_DIR
+::call dk_printVar DKSCRIPT_PATH
+::call dk_printVar DKBATCH_DIR
 
 
 ::###### Load Main Program ######
