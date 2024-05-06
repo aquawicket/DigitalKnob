ec27bb5bd0a52efa6e67cb31aae96da438d8e29f
aquawicket <aquawicket@hotmail.com>
2024-05-06 17:22:57 -0400

    code cleanup

--- DKBuilder.sh
+++ DKBuilder.sh
@@ -5,8 +5,8 @@
 ###### DK_SETUP ######
 [ ! -e DKBash ] && mkdir DKBash
 [ ! -e DKBash/functions ] && mkdir DKBash/functions
-[ ! -e DKBash/functions/DK ] && wget -P DKBash/functions https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKBash/functions/DK
-[ ! -e DKBash/functions/DK ] && curl -LO DKBash/functions/DK https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKBash/functions/DK
+#[ ! -e DKBash/functions/DK ] && wget -P DKBash/functions https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKBash/functions/DK
+[ ! -e DKBash/functions/DK ] && curl -Lo DKBash/functions/DK https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKBash/functions/DK
 
 ###### DK_INIT ######
 . DKBash/functions/DK
