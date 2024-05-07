6a93dce6df1996d7d0ae477f70fa8238ea1b905b
aquawicket <aquawicket@hotmail.com>
2024-05-07 03:09:12 -0400

    git commit

--- DKBash/tests/test/test_reloadSelf.sh
+++ DKBash/tests/test/test_reloadSelf.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-
-###### Load Function files ######
-#. ../functions/DK
-
-#rm -r -f "${DIGITALKNOB_DIR}/${DKSCRIPT_NAME}"
+echo ""
+echo "$0($*)"
 
 DKSCRIPT_PATH="$(cd $(dirname $(realpath $0)); pwd)/$(basename $0)"
 echo "DKSCRIPT_PATH = ${DKSCRIPT_PATH}"
-
+	
 DKSCRIPT_DIR="$(dirname "${DKSCRIPT_PATH}")"
 echo "DKSCRIPT_DIR = ${DKSCRIPT_DIR}"
 
@@ -16,8 +13,24 @@ echo "DKSCRIPT_NAME = ${DKSCRIPT_NAME}"
 
 DKPARENT_DIR="$(dirname "${DKSCRIPT_DIR}")"
 echo "DKPARENT_DIR = ${DKPARENT_DIR}"
+	
+	
+	
 
-cp -r "${DKSCRIPT_PATH}" "${DKPARENT_DIR}/${DKSCRIPT_NAME}"
-		
-
+if [ "$1" == "rename" ];then 
+	echo "RENAME"
+	sleep 3
+	
+	echo "mv ${DKSCRIPT_DIR}/test ${DKSCRIPT_DIR}/test_renamed"
+	mv "${DKSCRIPT_DIR}/test" "${DKSCRIPT_DIR}/test_renamed"
+	
+else
+	echo "MAIN"
+	
+	echo "cp -r ${DKSCRIPT_PATH} ${DKPARENT_DIR}/${DKSCRIPT_NAME}"
+	cp -r "${DKSCRIPT_PATH}" "${DKPARENT_DIR}/${DKSCRIPT_NAME}"
+	#. "${DKPARENT_DIR}/${DKSCRIPT_NAME}" rename
+	exec "${DKPARENT_DIR}/${DKSCRIPT_NAME}" rename
+	exit
+fi
 exec $SHELL
