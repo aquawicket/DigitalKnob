7e3f838dc1d3af06ff20a08a21d02b7751ab5748
unknown <aquawicket@hotmail.com>
2024-05-07 06:23:57 -0400

    git commit

--- DKBatch/functions/dk_call.cmd
+++ DKBatch/functions/dk_call.cmd
@@ -1,20 +1,16 @@
 @echo off
-call DK
+::call DK
 
 ::####################################################################
 ::# dk_call(<command args>)
 ::#
 ::#
 :dk_call () {
-	call dk_debugFunc
+	::call dk_debugFunc
 	::if "%1"=="" call dk_error "Incorrect number of parameters"
-
-    call dk_echo %magenta% > %* %clr%
-   
+    
+	::call dk_echo "%magenta% > %* %clr%"
+    if not exist "%DKBATCH_DIR%\functions\dk_load.cmd" powershell -Command "(New-Object Net.WebClient).DownloadFile('%DKHTTP%/dk_load.cmd', '%DKBATCH_DIR%\functions\dk_load.cmd')"
+	call dk_load %~1
 	call %*
 goto:eof
-
-
-
-:DKTEST ########################################################################
-	call dk_call dk_info "test message using dk_call"
