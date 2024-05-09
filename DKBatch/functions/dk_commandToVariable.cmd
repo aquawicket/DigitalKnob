ba8be6a6009d9eb3c84028b357b4753b6924c78d
unknown <aquawicket@hotmail.com>
2024-05-09 08:55:53 -0400

    git commit

--- DKBatch/functions/dk_commandToVariable.cmd
+++ DKBatch/functions/dk_commandToVariable.cmd
@@ -7,9 +7,9 @@ call DK
 ::#
 :dk_commandToVariable () {
 	call dk_debugFunc
-::	if "%~1" equ "" call dk_error "%__FUNCTION__%(%*): argument 1 is invalid"
-::	if "%~2" equ "" call dk_error "%__FUNCTION__%(%*): argument 2 is invalid"
-::	if "%~3" neq "" call dk_error "%__FUNCTION__%(%*): too many arguments"
+	::if "%~1" equ "" call dk_error "%__FUNCTION__%(): argument 1 is invalid"
+	if "%~2" equ "" call dk_error "%__FUNCTION__%(): argument 2 is invalid"
+	::if "%~3" neq "" call dk_error "%__FUNCTION__%(): too many arguments"
 	
     set "command="
 
