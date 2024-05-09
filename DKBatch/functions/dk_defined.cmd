ad81d5f15ad711b9940588c6d3ec2c236a8e1ea6
unknown <aquawicket@hotmail.com>
2024-05-08 20:06:45 -0400

    fixed dk_stringContains

--- DKBatch/functions/dk_defined.cmd
+++ DKBatch/functions/dk_defined.cmd
@@ -2,17 +2,63 @@
 call DK
 
 ::##################################################################################
-::# dk_defined(<variable>)
-::#
-::# Evaluates to true if the parameter is a variable that exists.
+::# dk_defined(<variable> <result>)
 ::#
 :dk_defined () {
 	call dk_debugFunc
-::	[ $# -ne 1 ] && return $false # Incorrect number of parameters
-	
-::	eval value='$'{$1+x} # value will = 'x' if the variable is defined
-::	[ -n "$value" ]
+	if "%~1" equ "" call dk_error "dk_defined() argument 1 is invalid"
 
-	if "%1"=="" echo %1 is NOT defined
-	if NOT "%1"=="" echo %1 IS defined
+	::set "variable=%1"
+	if defined %1 (
+		if "%~2" neq "" endlocal & set "%2=true"
+		(call )
+		goto:eof
+	)
+	
+	if "%~2" neq "" endlocal & set "%2=false"
+    (call)
 goto:eof
+
+
+
+
+
+
+:DKTEST ########################################################################
+
+    ::###### Using if return value
+	echo.
+	
+	set "_variable_=is defined"
+	call dk_defined _variable_ result
+	if "%result%" equ "true" (echo _variable_ is defined) else (echo _variable_ is NOT defined)
+	
+	set "_variable_="
+	call dk_defined _variable_ result
+	if "%result%" equ "true" (echo _variable_ is defined) else (echo _variable_ is NOT defined)
+	::FIXME: ERRORLEVEL is still 1 
+	
+	
+	::###### Using if ERRORLEVEL
+	echo.
+	
+	set "_variable_=is defined"
+	call dk_defined _variable_
+	if not ERRORLEVEL 1 (echo _variable_ is defined) else (echo _variable_ is NOT defined)
+	
+	set "_variable_="
+	call dk_defined _variable_
+	if not ERRORLEVEL 1 (echo _variable_ is defined) else (echo _variable_ is NOT defined)
+	::FIXME: ERRORLEVEL is still 1 
+	
+	
+	::###### Using && and || conditionals
+	echo.
+	
+	set "_variable_=is defined"
+	call dk_defined _variable_ && (echo _variable_ is defined) || (echo _variable_ is NOT defined)
+
+	set "_variable_="
+	call dk_defined _variable_ && (echo _variable_ is defined) || (echo _variable_ is NOT defined)
+	::FIXME: ERRORLEVEL is still 1
+	
--- DKBatch/functions/dk_stringContains.cmd
+++ DKBatch/functions/dk_stringContains.cmd
@@ -7,9 +7,66 @@ call DK
 ::#
 :dk_stringContains () {
 	call dk_debugFunc
+	if "%~1" equ "" call dk_error "dk_stringContains() argument 1 is invalid"
+	if "%~2" equ "" call dk_error "dk_stringContains() argument 2 is invalid"
 	
-    call set "haystack=%1"
-    call set "needle=%2"
-    if not "x!haystack:%needle%=!"=="x%haystack%" set "%3=1"
-    ::set "%3=0"
+	setlocal enableDelayedExpansion
+    call set "_haystack_=%~1"
+    call set "_needle_=%~2"
+    if not "x!_haystack_:%_needle_%=!"=="x%_haystack_%" (
+		if "%~3" neq "" endlocal & set "%3=true"
+		(call )
+		goto:eof
+	)
+	
+    if "%~3" neq "" endlocal & set "%3=false"
+    (call)
 goto:eof
+
+
+
+
+
+:DKTEST ########################################################################
+
+    ::###### Using if return value
+	echo.
+	
+	set "string=There is a needle in this haystack"
+	set "substring=needle"
+	call dk_stringContains "%string%" "%substring%" result
+	if "%result%" equ "true" (echo string contains substring) else (echo string does NOT contain substring)
+	
+	set "string=There is a needle in this haystack"
+	set "substring=straw"
+	call dk_stringContains "%string%" "%substring%" result
+	if "%result%" equ "true" (echo string contains substring) else (echo string does NOT contain substring)
+	::FIXME: ERRORLEVEL is still 1 
+	
+	
+	::###### Using if ERRORLEVEL
+	echo.
+	
+	set "string=There is a needle in this haystack"
+	set "substring=needle"
+	call dk_stringContains "%string%" "%substring%"
+	if not ERRORLEVEL 1 (echo string contains substring) else (echo string does NOT contain substring)
+	
+	set "string=There is a needle in this haystack"
+	set "substring=straw"
+	call dk_stringContains "%string%" "%substring%"
+	if not ERRORLEVEL 1 (echo string contains substring) else (echo string does NOT contain substring)
+	::FIXME: ERRORLEVEL is still 1 
+	
+	
+	::###### Using && and || conditionals
+	echo.
+	
+	set "string=There is a needle in this haystack"
+	set "substring=needle"
+	call dk_stringContains "%string%" "%substring%" && (echo string contains substring) || (echo string does NOT contain substring)
+
+	set "string=There is a needle in this haystack"
+	set "substring=straw"
+	call dk_stringContains "%string%" "%substring%" && (echo string contains substring) || (echo string does NOT contain substring)
+	::FIXME: ERRORLEVEL is still 1
