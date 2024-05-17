c898596c02c465fdc7944f9838fbe68f39ca629a
aquawicket <aquawicket@hotmail.com>
2024-05-16 23:56:57 -0400

    git commit

--- DKBatch/functions/dk_call.cmd
+++ DKBatch/functions/dk_call.cmd
@@ -7,12 +7,11 @@
 ::#
 :::dk_call () {
 	::call dk_debugFunc
-	::if "%~1" equ "" call dk_error "%__FUNCTION__%(%*): argument 1 is invalid"
 
     ::call dk_echo %magenta% > %* %clr%
    
 	if not exist "%DKBATCH_DIR%\functions\%~1.cmd" call dk_download "%DKHTTP%/%~1.cmd" "%DKBATCH_DIR%\functions\%~1.cmd"
-call %*
+    call %*
 
 
 
