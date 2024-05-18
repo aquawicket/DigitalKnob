6f00f144995df45ccdb2642761a8f39c38c21faa
aquawicket <aquawicket@hotmail.com>
2024-05-18 14:17:08 -0400

    git commit

---
+++ DKBatch/functions/dk_removeFromFile.cmd
@@ -0,0 +1,21 @@
+@echo off
+call DK
+
+
+::##################################################################################
+::# dk_removeFromFile(<string> <file>)
+::#
+::#
+:dk_removeFromFile () {
+	dk_debugFunc
+	[ $# -ne 2 ] && dk_error "${FUNCNAME}(): incorrect number of arguments"
+
+	sed -i -e "/$1/d" $2
+}
+
+
+
+DKTEST() { ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ###
+
+	dk_removeFromFile
+}
