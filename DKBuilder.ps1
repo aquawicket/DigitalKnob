4b5a044da9531345f3ec3b01ce8bfbdc0c8dba82
aquawicket <aquawicket@hotmail.com>
2024-06-04 17:22:39 -0400

    git commit

--- DKBuilder.ps1
+++ DKBuilder.ps1
@@ -3,7 +3,7 @@
 $DKHTTP_DKPOWERSHELL_FUNCTIONS_DIR = "https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKPowershell/functions"
 if (! (Test-Path "DKPowershell")) { New-Item -Path 'DKPowershell' -ItemType Directory }
 if (! (Test-Path "DKPowershell/functions")) { New-Item -Path 'DKPowershell/functions' -ItemType Directory }
-if (! (Test-Path "DKPowershell/functions/DK.ps1")) { Invoke-WebRequest -URI "$DKHTTP_DKPOWERSHELL_FUNCTIONS_DIR/functions/DK.ps1" -OutFile "DKPowershell\functions\DK.ps1" }
+if (! (Test-Path "DKPowershell/functions/DK.ps1")) { Invoke-WebRequest -URI "$DKHTTP_DKPOWERSHELL_FUNCTIONS_DIR/DK.ps1" -OutFile "DKPowershell\functions\DK.ps1" }
 
 . $PWD\DKPowershell\functions\DK.ps1
 
