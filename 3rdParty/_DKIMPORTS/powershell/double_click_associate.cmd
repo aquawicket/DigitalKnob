ee9149c2a6cfa16d5b84d47ed2084c1c22d2b65a
aquawicket <aquawicket@hotmail.com>
2024-06-04 09:20:26 -0400

    git commit

--- 3rdParty/_DKIMPORTS/powershell/double_click_associate.cmd
+++ 3rdParty/_DKIMPORTS/powershell/double_click_associate.cmd
@@ -6,7 +6,7 @@ call ../../../DKBatch/functions/DK.cmd
 ::	Seems to be a better place to change file associations. They take precidence over ftype and assoc commands
 	
 call dk_validate DKIMPORTS_DIR dk_validateBranch
-call dk_validate POWERSHELL_EXE "call %DKIMPORTS_DIR%\powershell\dk_install"
+call dk_validate POWERSHELL_EXE "call %DKIMPORTS_DIR%\powershell\dk_installPowershell"
 call dk_registryDeleteKey "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.ps1"
 call dk_registryDeleteKey "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.psm1"
 ftype dk_powershell=%POWERSHELL_EXE% "%%1"
