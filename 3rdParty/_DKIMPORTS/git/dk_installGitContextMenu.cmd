b239bfea5753bf87f835c72054ee3c23f91106a7
aquawicket <aquawicket@hotmail.com>
2024-06-04 15:12:15 -0400

    git commit

--- 3rdParty/_DKIMPORTS/git/dk_installGitContextMenu.cmd
+++ 3rdParty/_DKIMPORTS/git/dk_installGitContextMenu.cmd
@@ -4,7 +4,7 @@ call ../../../DKBatch/functions/DK.cmd
 
 call dk_validate DKIMPORTS_DIR dk_validateBranch
 call dk_validate GIT_EXE "call %DKIMPORTS_DIR%\git\dk_installGit"
-call dk_addContextMenu "GIT ADD" "%GITBASH_EXE%" "\"%GIT_EXE%\" \"%%%%%%%%1\""
+call dk_addContextMenu "GIT ADD" "%GITBASH_EXE%" "\"%GIT_EXE%\" add \"%%%%%%%%1\""
 
 call dk_info "complete"
 pause
