8c6c7f2f0afd74cf4f3375260f6ac0923a07c76c
aquawicket <aquawicket@hotmail.com>
2024-06-06 01:22:27 -0400

    git commit

--- 3rdParty/_DKIMPORTS/git/dk_installGit.cmd
+++ 3rdParty/_DKIMPORTS/git/dk_installGit.cmd
@@ -19,8 +19,9 @@ call ../../../DKBatch/functions/DK.cmd
 	call dk_convertToCIdentifier %GIT_FOLDER% GIT_FOLDER
     call dk_toLower %GIT_FOLDER% GIT_FOLDER
 	call dk_validate DKTOOLS_DIR "dk_getDKPaths"
-    call dk_set GIT_EXE "%DKTOOLS_DIR%\%GIT_FOLDER%\bin\git.exe"
-	call dk_set GITBASH_EXE "%DKTOOLS_DIR%\%GIT_FOLDER%\git-bash.exe"
+	call dk_set GIT "%DKTOOLS_DIR%\%GIT_FOLDER%"
+    call dk_set GIT_EXE "%GIT%\bin\git.exe"
+	call dk_set GITBASH_EXE "%GIT%\git-bash.exe"
         
     if exist "%GIT_EXE%" goto:git_installed
     call dk_echo   
@@ -28,15 +29,13 @@ call ../../../DKBatch/functions/DK.cmd
     call dk_download %GIT_DL% "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%"
     ::echo "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" /DIR=%DKTOOLS_DIR%\%GIT_FOLDER%
     ::"%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" /DIR="%DKTOOLS_DIR%\%GIT_FOLDER%" /VERYSILENT
-	call dk_info "%DKDOWNLOAD_DIR%\%GIT_DL_FILE% -y -o %DKTOOLS_DIR%\%GIT_FOLDER%"
-    "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" -y -o "%DKTOOLS_DIR%\%GIT_FOLDER%"
+    "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" -y -o "%GIT%"
     if not exist "%GIT_EXE%" call dk_error "cannot find git"
         
 	:git_installed	
 		
 		
-		
-		
+
 	::### Add Context Menu ###
 	call %DKIMPORTS_DIR%\git\dk_installGitContextMenu.cmd
 	
