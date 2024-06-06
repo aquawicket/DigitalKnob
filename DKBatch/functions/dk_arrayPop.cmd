ca6d8e8a833e5362a9321ed76161af2edf0f5085
aquawicket <aquawicket@hotmail.com>
2024-06-06 10:52:38 -0400

    git commit

--- DKBatch/functions/dk_arrayPop.cmd
+++ DKBatch/functions/dk_arrayPop.cmd
@@ -2,18 +2,53 @@
 call DK
 
 ::################################################################################
-::# dk_arrayPop(<array> <index>)
+::# dk_arrayPop(<array>)
 ::#
 ::#    reference: https://www.w3schools.com/js/js_array_methods.asp#mark_pop
 ::#
 :dk_arrayPop () {
 	call dk_debugFunc
-	if %__ARGC__% neq 2 (call dk_error "%__FUNCTION__%(%__ARGC__%): incorrect number of arguments")
+	if %__ARGC__% neq 1 (call dk_error "%__FUNCTION__%(%__ARGC__%): incorrect number of arguments")
 	
-	call dk_todo "dk_arrayPop"
+	call dk_arrayLength %~1 last_element
+	set /a last_element-=1
+	call dk_unset %~1[%last_element%]
 goto:eof
 
 
 
 
 :DKTEST ########################################################################
+
+	echo testing dk_arrayShift . . .
+	set "MyArray[0]=a"
+	set "MyArray[1]=b"
+	set "MyArray[2]=c"
+	set "MyArray[3]=d"
+	set "MyArray[4]=e"
+	
+	call dk_printArray MyArray
+	
+	call dk_arrayPop MyArray
+	call dk_printArray MyArray
+	
+	call dk_arrayPop MyArray
+	call dk_printArray MyArray
+	
+	call dk_arrayPop MyArray
+	call dk_printArray MyArray
+	
+	call dk_arrayPop MyArray
+	call dk_printArray MyArray
+	
+	call dk_arrayPop MyArray
+	call dk_printArray MyArray
+	
+	call dk_arrayPop MyArray
+	call dk_printArray MyArray
+	
+	call dk_arrayPop MyArray
+	call dk_printArray MyArray
+	
+	call dk_arrayPop MyArray
+	call dk_printArray MyArray
--- DKBatch/functions/dk_arrayPush.cmd
+++ DKBatch/functions/dk_arrayPush.cmd
@@ -2,7 +2,7 @@
 call DK
 
 ::################################################################################
-::# dk_arrayPush(<array> <index>)
+::# dk_arrayPush(<array> <value>)
 ::#
 ::#    reference: https://www.w3schools.com/js/js_array_methods.asp#mark_push
 ::#
@@ -10,10 +10,27 @@ call DK
 	call dk_debugFunc
 	if %__ARGC__% neq 2 (call dk_error "%__FUNCTION__%(%__ARGC__%): incorrect number of arguments")
 	
-	call dk_todo "dk_arrayPush"
+	call dk_arrayLength %~1 end_index
+	endlocal & call dk_set %~1[%end_index%] "%~2"
 goto:eof
 
 
 
 
 :DKTEST ########################################################################
+
+	echo testing dk_arrayUnshift . . .
+	set "MyArray[0]=a"
+	set "MyArray[1]=b"
+	set "MyArray[2]=c"
+	
+	call dk_printArray MyArray
+	
+	call dk_arrayPush MyArray d
+	call dk_printArray MyArray
+	
+	call dk_arrayPush MyArray e
+	call dk_printArray MyArray
+	
+	call dk_arrayPush MyArray f
+	call dk_printArray MyArray
