34d8684b746edae82bc726a50796cf7550d56e56
aquawicket <aquawicket@hotmail.com>
2024-06-06 03:21:51 -0400

    git commit

--- DKBatch/functions/dk_createFunctionList.cmd
+++ DKBatch/functions/dk_createFunctionList.cmd
@@ -7,18 +7,15 @@ call DK
 ::#
 ::#
 :dk_createFunctionList() {
-    :: create a list of all dk_functions
+    :: create a list of all dk_functions and store them in _functionList_
 	call dk_validate DKBATCH_FUNCTIONS_DIR "call dk_validateBranch"
     call dk_remove _functionList_
-    setlocal enableDelayedExpansion
-	for %%a in (%DKBATCH_FUNCTIONS_DIR%\*) do (
-        set "_func_=%%~na"
-        set "_match_=!_func_:dk_=!"
-        if not !_match_!==%%~na (
-			rem echo %%~na
-            echo %%~na >> %DKBATCH_FUNCTIONS_DIR%\_functionList_
-        )			
+	for %%a in (%DKBATCH_FUNCTIONS_DIR%\dk_*.cmd) do (
+		rem echo %%~na
+        echo %%~na >> %DKBATCH_FUNCTIONS_DIR%\_functionList_
     )
+	
+	if not exist %DKBATCH_FUNCTIONS_DIR%\_functionList_ (call dk_error "_functionList_ is missing")
 goto:eof
 
 
