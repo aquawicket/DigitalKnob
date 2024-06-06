b4f03730d6a7f353dfc8c3888d57447a8d85f7ae
aquawicket <aquawicket@hotmail.com>
2024-06-06 18:48:37 -0400

    git commit

--- DKBatch/functions/dk_load.cmd
+++ DKBatch/functions/dk_load.cmd
@@ -26,12 +26,10 @@ call DK
 		set "fpath=%DKBATCH_FUNCTIONS_DIR%\%~n1.cmd"
 	)
 
-	if exist "%fpath%" goto:eof
 	if not exist "%fpath%" echo Downloading %fn%
 	if not exist "%fpath%" call dk_download "%DKHTTP_DKBATCH_FUNCTIONS_DIR%/%fn%.cmd" "%fpath%"
 	if not exist "%fpath%" call dk_error "ERROR: %fpath%: file not found"
 	
-
 	:: Convert to windows line endings if only CR found
 	:: TODO
 	if exist "%fpath%" (
@@ -40,37 +38,38 @@ call DK
 		call dk_error "%fpath%: file not found"
 	)
 	
-	::set "dkload_list=;dk_getDirname;"
 	echo %dkload_list% | findstr ";%fn%;" && goto:eof
-		set "dkload_list=%dkload_list%;%fn%;" 			&:: Add to list
-		echo added %fn% to dkload_list
+	set "dkload_list=%dkload_list%;%fn%;" 			&:: Add to list
+	echo added %fn% to dkload_list
 	
-::		:: read file line by line and store matching lines in array
-		for /F "usebackq delims=" %%a in ("%fpath%") do (
-		    echo %%a | findstr "\<dk_*" >nul && (
-				set "temp=%%a"
-				call set "temp=%%temp:*dk_=dk_%%"
-				call set "temp=%%temp:*dk_load =%%"
-				call set "temp=%%temp:"= %%"
-				call set "temp=%%temp:(= %%"
-				call set "temp=%%temp:)= %%"
-				call set "temp=%%temp:#= %%"
-		    	call set "temp=%%temp:$= %%"
-				call set "temp=%%temp:{= %%"
-				call set "temp=%%temp:$= %%"
-				call set "temp=%%temp:,= %%"
-rem				call set "temp=%%temp:%%= %%"
-				call set "temp=%%temp: = %%"
-				setlocal enableDelayedExpansion
-				for %%i in (!temp!) do (
-					call set "temp=%%i"
-					goto:done
-				)
-				:done
-				endlocal
-				call echo temp = %%temp%%
-rem				call dk_load %%temp%%
-			) || echo.
-		)
+::if exist "%fpath%" goto:eof
+::	:: read file line by line and store matching lines in array
+	for /F "usebackq delims=" %%a in ("%fpath%") do (
+	    echo %%a | findstr "\<dk_*" >nul && (
+			set "temp=%%a"
+			call set "temp=%%temp:*dk_=dk_%%"
+			call set "temp=%%temp:*dk_load =%%"
+			call set "temp=%%temp:"= %%"
+			call set "temp=%%temp:(= %%"
+			call set "temp=%%temp:)= %%"
+			call set "temp=%%temp:#= %%"
+	    	call set "temp=%%temp:$= %%"
+			call set "temp=%%temp:{= %%"
+			call set "temp=%%temp:$= %%"
+			call set "temp=%%temp:,= %%"
+rem			call set "temp=%%temp:%%= %%"
+			call set "temp=%%temp: = %%"
+			setlocal enableDelayedExpansion
+			for %%i in (!temp!) do (
+				call set "temp=%%i"
+				goto:done
+			)
+			:done
+			endlocal
+			call echo temp = %%temp%%
+rem			call dk_load %%temp%%
+		) || echo.
+	)
+	
 rem		call dk_printArray lines
 goto:eof
