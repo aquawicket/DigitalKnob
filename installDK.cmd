a94a2cbbcd96a92e7b6f6f0efe591f00d198ede3
aquawicket <aquawicket@hotmail.com>
2024-06-08 18:04:55 -0400

    git commit

---
+++ installDK.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+::###### DK_INIT ######
+set "DKHTTP_DKBATCH_FUNCTIONS_DIR=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBatch/functions"
+if not exist "DKBatch\functions" mkdir DKBatch\functions
+if not exist "DKBatch\functions\DK.cmd" powershell -Command "(New-Object Net.WebClient).DownloadFile('%DKHTTP_DKBATCH_FUNCTIONS_DIR%/DK.cmd', 'DKBatch\functions\DK.cmd')"
+
+call "DKBatch/functions/DK.cmd" %*
+
+::###### Load Main Program ######
+call dk_validate DKBRANCH_DIR "call dk_validateBranch"
+call dk_validate GIT_EXE "call dk_validateGit"
+if NOT exist "%DKBRANCH_DIR%\.git" ("%GIT_EXE%" clone https://github.com/aquawicket/DigitalKnob.git "%DKBRANCH_DIR%")
