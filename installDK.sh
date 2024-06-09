6e860a448672b8406a00ac8afbe21f9a817c2fcf
aquawicket <aquawicket@hotmail.com>
2024-06-08 20:43:02 -0400

    git commit

--- installDK.sh
+++ installDK.sh
@@ -13,8 +13,8 @@ fi
 
 ###### Load Main Program ######
 dk_load dk_validate
-dk_load dk_validateBranch
 dk_load dk_validateGit
+dk_load dk_validateBranch
 dk_load dk_stringContains
 dk_load dk_getDKPaths
 dk_load dk_getHostTriple
@@ -26,8 +26,8 @@ dk_load dk_convertToCIdentifier
 dk_load dk_toLower
 
 ###### Run Program ######
-dk_validate DKBRANCH_DIR "dk_validateBranch"
 dk_validate GIT_EXE "dk_validateGit"
+dk_validate DKBRANCH_DIR "dk_validateBranch"
 if [ ! -e "$DKBRANCH_DIR/.git" ]; then
 	$GIT_EXE clone https://github.com/aquawicket/DigitalKnob.git $DKBRANCH_DIR
 	cd $DKBRANCH_DIR
