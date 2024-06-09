d41aaf7a1a6c753c3833a89e32a774bbc06f1087
aquawicket <aquawicket@hotmail.com>
2024-06-09 15:25:42 -0400

    git commit

---
+++ 3rdParty/_DKIMPORTS/powershell/dk_uninstallPowershellFileAssociations.cmd
@@ -0,0 +1,13 @@
+@echo off
+call ../../../DKBatch/functions/DK.cmd
+
+call dk_validate DKIMPORTS_DIR "call dk_validateBranch"
+call dk_validate POWERSHELL_EXE "call %DKIMPORTS_DIR%\powershell\dk_uninstallPowershell"
+
+::call dk_registryDeleteKey "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.ps1"
+::call dk_registryDeleteKey "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.psm1"
+::ftype dk_powershell=%POWERSHELL_EXE% "%%1"
+::assoc .ps1=dk_powershell
+::assoc .psm1=dk_powershell
+call dk_unsetFileAssoc .ps1 %POWERSHELL_EXE%
+call dk_unsetFileAssoc .psm1 %POWERSHELL_EXE%
