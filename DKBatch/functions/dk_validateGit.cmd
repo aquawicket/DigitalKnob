5472dc83ad906672b59a5447c8c6e1eedb842268
aquawicket <aquawicket@hotmail.com>
2024-06-08 20:10:20 -0400

    git commit

--- DKBatch/functions/dk_validateGit.cmd
+++ DKBatch/functions/dk_validateGit.cmd
@@ -13,20 +13,20 @@ call dk_set GIT_DL_WIN_X86_64 https://github.com/git-for-windows/git/releases/do
 ::#
 ::#
 :dk_validateGit () {
-	call dk_debugFunc
-	if %__ARGC__% neq 0 (call dk_error "%__FUNCTION__%(%__ARGC__%): incorrect number of arguments")
+    call dk_debugFunc
+    if %__ARGC__% neq 0 (call dk_error "%__FUNCTION__%(%__ARGC__%): incorrect number of arguments")
 	
-	call dk_validate HOST_ARCH "call dk_getHostTriple"
+    call dk_validate HOST_ARCH "call dk_getHostTriple"
     if "%HOST_ARCH%"=="arm32"  call dk_set GIT_DL ""
     if "%HOST_ARCH%"=="arm64"  call dk_set GIT_DL %GIT_DL_WIN_ARM64%
     if "%HOST_ARCH%"=="x86"    call dk_set GIT_DL %GIT_DL_WIN_X86%
     if "%HOST_ARCH%"=="x86_64" call dk_set GIT_DL %GIT_DL_WIN_X86_64%
         
     call dk_getFilename %GIT_DL% GIT_DL_FILE
-	call dk_removeExtension %GIT_DL_FILE% GIT_DL_NAME
+    call dk_removeExtension %GIT_DL_FILE% GIT_DL_NAME
     call dk_convertToCIdentifier %GIT_DL_NAME% GIT_FOLDER
     call dk_toLower %GIT_FOLDER% GIT_FOLDER
-	call dk_validate DKTOOLS_DIR "call dk_getDKPaths"
+    call dk_validate DKTOOLS_DIR "call dk_getDKPaths"
     call dk_set GIT_EXE %DKTOOLS_DIR%\%GIT_FOLDER%\bin\git.exe
         
     if exist "%GIT_EXE%" goto:eof
@@ -34,9 +34,7 @@ call dk_set GIT_DL_WIN_X86_64 https://github.com/git-for-windows/git/releases/do
     echo.   
     call dk_info "Installing git . . ."
     call dk_download %GIT_DL% "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%"
-    ::echo "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" /DIR=%DKTOOLS_DIR%\%GIT_FOLDER%
-    ::"%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" /DIR="%DKTOOLS_DIR%\%GIT_FOLDER%" /VERYSILENT
-	call dk_info "%DKDOWNLOAD_DIR%\%GIT_DL_FILE% -y -o %DKTOOLS_DIR%\%GIT_FOLDER%"
+    call dk_info "%DKDOWNLOAD_DIR%\%GIT_DL_FILE% -y -o %DKTOOLS_DIR%\%GIT_FOLDER%"
     "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" -y -o "%DKTOOLS_DIR%\%GIT_FOLDER%"
 	   
     if NOT exist "%GIT_EXE%" (call dk_error "cannot find git")
--- installDK.cmd
+++ installDK.cmd
@@ -14,12 +14,19 @@ call dk_load dk_validateGit
 call dk_load dk_stringContains
 call dk_load dk_getDKPaths
 call dk_load dk_getHostTriple
+call dk_load dk_getExtension
+call dk_load dk_removeExtension
+call dk_load dk_makeDirectory
+call dk_load dk_replaceAll
+call dk_load dk_convertToCIdentifier
+call dk_load dk_toLower
 
 ::###### Run Program ######
 call dk_validate DKBRANCH_DIR "call dk_validateBranch"
 call dk_validate GIT_EXE "call dk_validateGit"
 if NOT exist "%DKBRANCH_DIR%\.git" (
 	"%GIT_EXE%" clone https://github.com/aquawicket/DigitalKnob.git "%DKBRANCH_DIR%"
+	"%GIT_EXE%" checkout "%DKBRANCH%"
 ) else (
 	echo DigitalKnob is already installed at %DKBRANCH_DIR%
 )
