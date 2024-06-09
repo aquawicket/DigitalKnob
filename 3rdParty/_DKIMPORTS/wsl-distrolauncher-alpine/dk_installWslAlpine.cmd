63a7e44648de4cb4ef2fd7f51f540f5782345602
aquawicket <aquawicket@hotmail.com>
2024-06-09 19:21:20 -0400

    git commit

---
+++ 3rdParty/_DKIMPORTS/wsl-distrolauncher-alpine/dk_installWslAlpine.cmd
@@ -0,0 +1,23 @@
+@echo off
+call ../../../DKBatch/functions/DK.cmd
+
+::####################################################################
+::# dk_installWslAlpine()
+::#
+:dk_installWslAlpine () {
+	call dk_debugFunc
+	if %__ARGC__% neq 0 (call dk_error "%__FUNCTION__%(%__ARGC__%): incorrect number of arguments")
+	
+	call dk_set LAUNCHER_DL "https://github.com/agowa/WSL-DistroLauncher-Alpine/releases/download/1.3.2/launcher.exe"
+	call dk_getFilename %LAUNCHER_DL% LAUNCHER_DL_FILE
+	call dk_echo   
+    call dk_info "Installing WSL-Alpine Linux . . ."
+    call dk_download %LAUNCHER_DL% "%DKDOWNLOAD_DIR%\%LAUNCHER_DL_FILE%"
+	
+goto:eof
+
+
+
+:DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ###
+
+	call dk_installWslAlpine
