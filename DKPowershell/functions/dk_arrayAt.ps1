ef5533e7241ed790f5c208062c3d1f08b0efc73b
aquawicket <aquawicket@hotmail.com>
2024-06-10 08:43:26 -0400

    git commit

---
+++ DKPowershell/functions/dk_arrayAt.ps1
@@ -0,0 +1,29 @@
+if(!$DKINIT){ . $PWD\DK.ps1 }
+if(!$dk_arrayAt){ $dk_arrayAt = 1 } else{ return }
+
+dk_load dk_error
+################################################################################
+# dk_arrayAt(array, index)
+#
+#    https://www.w3schools.com/js/js_array_methods.asp#mark_at
+#    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/at
+#
+function Global:dk_arrayAt($arry, $index) {
+	dk_debugFunc
+	if($(__ARGC__) -ne 2){ dk_error "$(__FUNCTION__)($(__ARGC__)): incorrect number of arguments" }
+	
+	$value = Get-Variable -Name ($arry) -ValueOnly
+	return $value[$index]
+}
+
+
+
+
+function Global:DKTEST () { ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ###
+	dk_debugFunc
+	
+	$MyArray = @('a', 'b', 'c', 'd', 'e')
+
+	$element = dk_arrayAt MyArray 3
+	dk_echo "element = $element"
+}
