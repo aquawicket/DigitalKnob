9dedb2b21854ecce13c94199803066e466c1640b
aquawicket <aquawicket@hotmail.com>
2024-06-11 18:08:53 -0400

    git commit

--- DKPowershell/functions/dk_arrayLength.ps1
+++ DKPowershell/functions/dk_arrayLength.ps1
@@ -3,6 +3,7 @@ if(!$dk_arrayLength){ $dk_arrayLength = 1 } else{ return }
 
 dk_load dk_error
 dk_load dk_assert
+dk_load dk_warning
 ################################################################################
 # dk_arrayLength(array) -> rtn_var
 #
@@ -12,9 +13,19 @@ function Global:dk_arrayLength($array) {
 	dk_debugFunc
 	if($(__ARGC__) -ne 1){ dk_error "$(__FUNCTION__)($(__ARGC__)): incorrect number of arguments" }
 	
-	if(Test-Path variable:$array){ $_array_ = Get-Variable -Name ($array) -ValueOnly -ErrorAction SilentlyContinue } 
-	else { $_array_ = $array }
+	if($null -eq $array){ return 0 }
+	#if(!($array.count -gt 0)){ return 0 }
 	
+	if(Test-Path variable:$array){ $_array_ = Get-Variable -Name ($array) -ValueOnly -ErrorAction SilentlyContinue } 
+	else {
+		$_array_ = $array 
+		if(!($_array_.count -gt 1)){
+			dk_warning "dk_arrayLength($array): are you sure the parameter is an array?"
+			return 0 
+		}
+	}
+	if($null -eq $_array_){ return 0 }
+	#if(!($_array_.count -gt 0)){ return 0 }
 	return $_array_.count
 }
 
@@ -27,4 +38,20 @@ function Global:DKTEST(){ ######################################################
 	$myArray = @('element 1', 'element 2', 'element 3')
 	$length = dk_arrayLength myArray
 	dk_echo "length = $length"
+	
+	$myArrayB = @('element A', 'element B', 'element C')
+	$lengthB = dk_arrayLength myArrayB
+	dk_echo "lengthB = $lengthB"
+	
+	$myArrayC = @()
+	$lengthC = dk_arrayLength myArrayC
+	dk_echo "lengthC = $lengthC"
+	
+	$myArrayD = ""
+	$lengthD = dk_arrayLength myArrayD
+	dk_echo "lengthD = $lengthD"
+	
+	$lengthZ = dk_arrayLength myArrayZ
+	dk_echo "lengthZ = $lengthZ"
+	dk_printVar myArrayZ
 }
