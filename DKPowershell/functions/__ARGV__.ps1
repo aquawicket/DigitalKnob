81f5c58fc6d81850c8c9ebfc7c3d38d6ed0e14a3
aquawicket <aquawicket@hotmail.com>
2024-06-14 10:22:50 -0400

    git commit

--- DKPowershell/functions/__ARGV__.ps1
+++ DKPowershell/functions/__ARGV__.ps1
@@ -9,12 +9,18 @@ if(!$DKINIT){ . $PWD\DK.ps1 }
 function GLOBAL:__ARGV__($_FRAME_=1) {
 	$_ARGV_ = New-Object System.Collections.Generic.List[System.Object]
 	$boundParameters = $(Get-PSCallStack)[$_FRAME_].InvocationInfo.BoundParameters
-	foreach ($keyValue in $boundParameters.GetEnumerator()) {
+	foreach($keyValue in $boundParameters.GetEnumerator()) {
 		#echo "$($keyValue.Key) = $($keyValue.Value)"
 		#$_ARGV_.Add($($keyValue.Key))
 		$_ARGV_.Add($($keyValue.Value))
 	}
-	return $_ARGV_
+	$unboundArguments = $(Get-PSCallStack)[$_FRAME_].InvocationInfo.UnboundArguments
+#	foreach($keyValue in $unboundArguments.GetEnumerator()) {
+#		#echo "$($keyValue.Key) = $($keyValue.Value)"
+#		#$_ARGV_.Add($($keyValue.Key))
+#		$_ARGV_.Add($($keyValue.Value))
+#	}
+	return $_ARGV_+$unboundArguments
 }
 
 
