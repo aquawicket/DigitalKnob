883f917427c5a7000665662ae243bf24431c8cfa
aquawicket <aquawicket@hotmail.com>
2024-06-23 20:16:29 -0400

    git commit

--- DKJavascript/functions/DK.js
+++ DKJavascript/functions/DK.js
@@ -1,6 +1,1 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('DigitalKnob');
-}).listen(8080);
+console.log("DK.js")
---
+++ DKJavascript/functions/dk_echo.js
@@ -0,0 +1,31 @@
+console.log("dk_echo.js")
+//###### DKINIT ######
+var js = document.createElement("script");
+js.type = "text/javascript";
+js.src = "DK.js";
+document.body.appendChild(js);
+
+//################################################################################
+//# dk_echo(message)
+//#
+//#	    Print a message to the console
+//#
+//#     @msg	- The message to print
+//#
+//const dk_echo = function dk_echo(message){
+	//dk_debugFunc
+	
+//    console.log(message)
+//}
+
+
+
+//const DKTEST = function DKTEST() { //####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ###
+//    //dk_debugFunc
+//	
+//	console.log "This is a normal console.log commmand"
+//	dk_echo
+//	dk_echo
+//	dk_echo "This is a dk_echo line"
+//    dk_echo "%cyan%This is dk_echo with color %clr%"
+//}
