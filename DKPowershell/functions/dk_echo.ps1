37abf0f1c73e9badb267ab2524e2b9c3fb908ef2
aquawicket <aquawicket@hotmail.com>
2024-06-25 20:28:42 -0400

    git commit

--- DKPowershell/functions/dk_echo.ps1
+++ DKPowershell/functions/dk_echo.ps1
@@ -8,16 +8,14 @@ if(!$dk_echo){ $dk_echo = 1 } else{ return }
 #
 #     @msg	- The message to print
 #
-function Global:dk_echo($allArgs) {
+function Global:dk_echo($message) {
 	dk_debugFunc
+	if($(__ARGC__) -ne 1){ dk_error "$(__FUNCTION__)($(__ARGC__)): incorrect number of arguments" }
+	
 	# https://stackoverflow.com/a/818118
-	$allArgs = $PsBoundParameters.Values + $args 
-	
-	
-	#if($allArgs -eq ""){ echo "\n"; return }
+	# $allArgs = $PsBoundParameters.Values + $args 
 
-	#echo $allArgs
-	Write-Host $allArgs
+	Write-Host $message
 }
 
 
@@ -29,6 +27,8 @@ function Global:DKTEST (){ ####### DKTEST ####### DKTEST ####### DKTEST ####### 
 	
 	
 	echo "This is a normal echo commmand"
+	dk_echo ""
+	dk_echo ""
 	dk_echo "This is a dk_echo line"
 	dk_echo "${red}This is ${white}dk_echo ${blue}with color ${clr}"
 }
