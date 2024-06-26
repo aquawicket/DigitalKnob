46d0a0358e42936a599e143c5e9e5d8ca2c4d8e4
aquawicket <aquawicket@hotmail.com>
2024-06-26 15:35:38 -0400

    git commit

--- DKBuilder.cmd
+++ DKBuilder.cmd
@@ -1,10 +1,10 @@
-<!-- :
+<!-- ::################## ELEVATE ##################
 @echo off
-echo 0 = %0
-echo * = %*
-echo CD = %CD%
 
-net file 1>nul 2>nul & if errorlevel 0 goto :admin
+net session >nul 2>&1
+IF %ERRORLEVEL% EQU 0 goto :admin
+echo NOT AN ADMIN!
+pause
 if "%~1" == "elevated" goto :elevate_end
 if "%~2" == "gotPrivileges" goto :gotPrivileges
 	echo "elevating permissions . . ."
@@ -22,12 +22,25 @@ if "%~2" == "gotPrivileges" goto :gotPrivileges
 :elevate_end
 	echo elevate_end
 	setlocal & cd /d %~dp0
+--><job><script language="VBScript">
+		Set oShell = CreateObject( "WScript.Shell" )
+		DKSCRIPT_PATH=oShell.ExpandEnvironmentStrings("%DKSCRIPT_PATH%")
+		Set UAC = CreateObject("Shell.Application") 
+		args = "" 
+		For Each strArg in WScript.Arguments 
+		args = args & strArg & " "  
+		Next
+		args = "/c """ + DKSCRIPT_PATH + """ " + args 
+		UAC.ShellExecute "cmd", args, "", "runas", 1
+	</script></job>
 :admin	
-	echo admin
+	echo Administrator PRIVILEGES Detected!
 	cd /d %~dp0
-
-::###### DKBuilder ######
-
+::###################### ELEVATE ##################	
+	
+	
+	
+::################## DKBuilder ####################
 ::###### DK_INIT ######
 set "DKHTTP_DKBATCH_FUNCTIONS_DIR=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBatch/functions"
 set "DKBATCH_DIR=%CD%\DKBatch"
@@ -36,27 +49,6 @@ if not exist "%DKBATCH_FUNCTIONS_DIR%" mkdir "%DKBATCH_FUNCTIONS_DIR%"
 if not exist "%DKBATCH_FUNCTIONS_DIR%\DK.cmd" powershell -Command "(New-Object Net.WebClient).DownloadFile('%DKHTTP_DKBATCH_FUNCTIONS_DIR%/DK.cmd', '%DKBATCH_FUNCTIONS_DIR%\DK.cmd')"
 call "%DKBATCH_FUNCTIONS_DIR%\DK.cmd" %0
 
-
-
-
-
-
-
-
 ::###### Load Main Program ######
 call dk_source dk_buildMain
 call dk_buildMain
-
-
--->
-<job><script language="VBScript">
-	Set oShell = CreateObject( "WScript.Shell" )
-	DKSCRIPT_PATH=oShell.ExpandEnvironmentStrings("%DKSCRIPT_PATH%")
-	Set UAC = CreateObject("Shell.Application") 
-	args = "" 
-	For Each strArg in WScript.Arguments 
-	args = args & strArg & " "  
-	Next
-	args = "/c """ + DKSCRIPT_PATH + """ " + args 
-	UAC.ShellExecute "cmd", args, "", "runas", 1
-</script></job>
