9fda0797b6ee8558ec31f54fa93feab29d207ab5
aquawicket <aquawicket@hotmail.com>
2024-06-29 11:56:40 -0400

    git commit

---
+++ DKPowershell/functions/dk_keyboard.ps1
@@ -0,0 +1,40 @@
+if(!$DKINIT){ . $PWD/DK.ps1 }
+if(!$dk_keyboard){ $dk_keyboard = 1 } else{ return }
+
+##################################################################################
+# dk_keyboard()
+#
+#
+function Global:dk_keyboard() {
+	dk_debugFunc
+	if($(__ARGC__) -lt 0){ dk_error "$(__FUNCTION__)($(__ARGC__)): incorrect number of arguments" }
+	
+	Keyboard_Loop
+}
+
+function Global:Keyboard_Loop (){
+	Write-Host 0;
+	while (1) {
+		$keyCode = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown').VirtualKeyCode;
+		Write-Host keyCode = $keyCode
+		#    if %keyCode% equ 13 echo "Enter"
+		#    if %keyCode% equ 27 echo "Esc" 
+		#    if %keyCode% equ 35 echo "End" 
+		#    if %keyCode% equ 36 echo "Home" 
+		#    if %keyCode% equ 37 echo "LeftArrow" 
+		#    if %keyCode% equ 39 echo "RightArrow"
+	}
+}
+
+
+
+
+
+
+
+
+function Global:DKTEST (){ ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ###
+	dk_debugFunc
+	
+	dk_keyboard
+}
