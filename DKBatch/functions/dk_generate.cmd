590065eb25a9c699b202aa738ede4951fc28dde4
aquawicket <aquawicket@hotmail.com>
2024-07-01 07:59:39 -0400

    git commit

--- DKBatch/functions/dk_generate.cmd
+++ DKBatch/functions/dk_generate.cmd
@@ -35,68 +35,68 @@ call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
     set "DKLINK=Static"
 
     set "CMAKE_ARGS="
-    ::if "%TYPE%"=="Debug"           call dk_appendCmakeArgs -DDEBUG=ON & call dk_appendCmakeArgs -DRELEASE=OFF
-    if "%TYPE%"=="Debug"             call dk_appendCmakeArgs -DDEBUG=ON
-    ::if "%TYPE%"=="Release"         call dk_appendCmakeArgs -DDEBUG=OFF & call dk_appendCmakeArgs -DRELEASE=ON
-    if "%TYPE%"=="Release"           call dk_appendCmakeArgs -DRELEASE=ON
-    if "%TYPE%"=="All"               call dk_appendCmakeArgs -DDEBUG=ON & call dk_appendCmakeArgs -DRELEASE=ON
-    if "%DKLEVEL%"=="Build"          call dk_appendCmakeArgs -DBUILD=ON
-    if "%DKLEVEL%"=="Rebuild"        call dk_appendCmakeArgs -DREBUILD=ON
-    if "%DKLEVEL%"=="RebuildAll"     call dk_appendCmakeArgs -DREBUILDALL=ON
-    if "%DKLINK%"=="Static"          call dk_appendCmakeArgs -DSTATIC=ON
-    if "%DKLINK%"=="Shared"          call dk_appendCmakeArgs -DSHARED=OFF
-    ::if "%TARGET_OS%==emscripten" call dk_appendCmakeArgs -DEMSCRIPTEN=ON
+    ::if "%TYPE%"=="Debug"           call dk_append CMAKE_ARGS -DDEBUG=ON & call dk_append CMAKE_ARGS -DRELEASE=OFF
+    if "%TYPE%"=="Debug"             call dk_append CMAKE_ARGS -DDEBUG=ON
+    ::if "%TYPE%"=="Release"         call dk_append CMAKE_ARGS -DDEBUG=OFF & call dk_append CMAKE_ARGS -DRELEASE=ON
+    if "%TYPE%"=="Release"           call dk_append CMAKE_ARGS -DRELEASE=ON
+    if "%TYPE%"=="All"               call dk_append CMAKE_ARGS -DDEBUG=ON & call dk_append CMAKE_ARGS -DRELEASE=ON
+    if "%DKLEVEL%"=="Build"          call dk_append CMAKE_ARGS -DBUILD=ON
+    if "%DKLEVEL%"=="Rebuild"        call dk_append CMAKE_ARGS -DREBUILD=ON
+    if "%DKLEVEL%"=="RebuildAll"     call dk_append CMAKE_ARGS -DREBUILDALL=ON
+    if "%DKLINK%"=="Static"          call dk_append CMAKE_ARGS -DSTATIC=ON
+    if "%DKLINK%"=="Shared"          call dk_append CMAKE_ARGS -DSHARED=OFF
+    ::if "%TARGET_OS%==emscripten" call dk_append CMAKE_ARGS -DEMSCRIPTEN=ON
         
     call dk_set CMAKE_BINARY_DIR "%CMAKE_TARGET_PATH%/%TARGET_OS%/%TYPE%"
     call dk_printVar CMAKE_BINARY_DIR
         
-    call dk_appendCmakeArgs -S=%CMAKE_SOURCE_DIR%
-    call dk_appendCmakeArgs -B=%CMAKE_BINARY_DIR%
+    call dk_append CMAKE_ARGS -S=%CMAKE_SOURCE_DIR%
+    call dk_append CMAKE_ARGS -B=%CMAKE_BINARY_DIR%
 
     ::############ CMake Options ############
-    ::call dk_appendCmakeArgs -DCMAKE_VERBOSE_MAKEFILE=1
-    ::call dk_appendCmakeArgs -DCMAKE_COLOR_DIAGNOSTICS=ON
-    call dk_appendCmakeArgs -Wdev
-    ::call dk_appendCmakeArgs -Werror=dev
-    call dk_appendCmakeArgs -Wdeprecated
-    ::call dk_appendCmakeArgs -Werror=deprecated
-    ::call dk_appendCmakeArgs --graphviz=graphviz.txt
-    ::call dk_appendCmakeArgs --system-information system_information.txt
-    ::call dk_appendCmakeArgs --debug-trycompile
-    ::call dk_appendCmakeArgs --debug-output
-    ::call dk_appendCmakeArgs --trace
-    ::call dk_appendCmakeArgs --trace-expand
-    ::call dk_appendCmakeArgs --warn-uninitialized
-    call dk_appendCmakeArgs --warn-unused-vars
-    ::call dk_appendCmakeArgs --check-system-vars
+    ::call dk_append CMAKE_ARGS -DCMAKE_VERBOSE_MAKEFILE=1
+    ::call dk_append CMAKE_ARGS -DCMAKE_COLOR_DIAGNOSTICS=ON
+    call dk_append CMAKE_ARGS -Wdev
+    ::call dk_append CMAKE_ARGS -Werror=dev
+    call dk_append CMAKE_ARGS -Wdeprecated
+    ::call dk_append CMAKE_ARGS -Werror=deprecated
+    ::call dk_append CMAKE_ARGS --graphviz=graphviz.txt
+    ::call dk_append CMAKE_ARGS --system-information system_information.txt
+    ::call dk_append CMAKE_ARGS --debug-trycompile
+    ::call dk_append CMAKE_ARGS --debug-output
+    ::call dk_append CMAKE_ARGS --trace
+    ::call dk_append CMAKE_ARGS --trace-expand
+    ::call dk_append CMAKE_ARGS --warn-uninitialized
+    call dk_append CMAKE_ARGS --warn-unused-vars
+    ::call dk_append CMAKE_ARGS --check-system-vars
 	
-	if "%TARGET_OS%"=="android_arm32"      call dk_prependCmakeArgs -G Unix Makefiles
-	if "%TARGET_OS%"=="android_arm64"      call dk_prependCmakeArgs -G Unix Makefiles
-	if "%TARGET_OS%"=="emscripten"         call dk_prependCmakeArgs -G Unix Makefiles	
-	if "%TARGET_OS%"=="ios_arm32"          call dk_prependCmakeArgs -G Xcode
-	if "%TARGET_OS%"=="ios_arm64"          call dk_prependCmakeArgs -G Xcode
-	if "%TARGET_OS%"=="iossim_x86"         call dk_prependCmakeArgs -G Xcode
-	if "%TARGET_OS%"=="iossim_x86_64"      call dk_prependCmakeArgs -G Xcode
-	if "%TARGET_OS%"=="linux_x86"          call dk_prependCmakeArgs -G Unix Makefiles
-	if "%TARGET_OS%"=="linux_x86_64"       call dk_prependCmakeArgs -G Unix Makefiles
-	if "%TARGET_OS%"=="mac_x86"            call dk_prependCmakeArgs -G Xcode
-	if "%TARGET_OS%"=="mac_x86_64"         call dk_prependCmakeArgs -G Xcode
-	if "%TARGET_OS%"=="raspberry_arm32"    call dk_prependCmakeArgs -G Unix Makefiles
-	if "%TARGET_OS%"=="raspberry_arm64"    call dk_prependCmakeArgs -G Unix Makefiles
-	if "%TARGET_OS%"=="win_arm64_clang"    call dk_prependCmakeArgs -G MinGW Makefiles
-	if "%TARGET_OS%"=="win_x86_clang"      call dk_prependCmakeArgs -G MinGW Makefiles
-	if "%TARGET_OS%"=="win_x86_mingw"      call dk_prependCmakeArgs -G MinGW Makefiles
-	if "%TARGET_OS%"=="win_x86_64_clang"   call dk_prependCmakeArgs -G MinGW Makefiles
-	if "%TARGET_OS%"=="win_x86_64_mingw"   call dk_prependCmakeArgs -G MinGW Makefiles
-	if "%TARGET_OS%"=="win_x86_64_ucrt"    call dk_prependCmakeArgs -G MinGW Makefiles
+	if "%TARGET_OS%"=="android_arm32"      call dk_prepend CMAKE_ARGS -G Unix Makefiles
+	if "%TARGET_OS%"=="android_arm64"      call dk_prepend CMAKE_ARGS -G Unix Makefiles
+	if "%TARGET_OS%"=="emscripten"         call dk_prepend CMAKE_ARGS -G Unix Makefiles	
+	if "%TARGET_OS%"=="ios_arm32"          call dk_prepend CMAKE_ARGS -G Xcode
+	if "%TARGET_OS%"=="ios_arm64"          call dk_prepend CMAKE_ARGS -G Xcode
+	if "%TARGET_OS%"=="iossim_x86"         call dk_prepend CMAKE_ARGS -G Xcode
+	if "%TARGET_OS%"=="iossim_x86_64"      call dk_prepend CMAKE_ARGS -G Xcode
+	if "%TARGET_OS%"=="linux_x86"          call dk_prepend CMAKE_ARGS -G Unix Makefiles
+	if "%TARGET_OS%"=="linux_x86_64"       call dk_prepend CMAKE_ARGS -G Unix Makefiles
+	if "%TARGET_OS%"=="mac_x86"            call dk_prepend CMAKE_ARGS -G Xcode
+	if "%TARGET_OS%"=="mac_x86_64"         call dk_prepend CMAKE_ARGS -G Xcode
+	if "%TARGET_OS%"=="raspberry_arm32"    call dk_prepend CMAKE_ARGS -G Unix Makefiles
+	if "%TARGET_OS%"=="raspberry_arm64"    call dk_prepend CMAKE_ARGS -G Unix Makefiles
+	if "%TARGET_OS%"=="win_arm64_clang"    call dk_prepend CMAKE_ARGS -G MinGW Makefiles
+	if "%TARGET_OS%"=="win_x86_clang"      call dk_prepend CMAKE_ARGS -G MinGW Makefiles
+	if "%TARGET_OS%"=="win_x86_mingw"      call dk_prepend CMAKE_ARGS -G MinGW Makefiles
+	if "%TARGET_OS%"=="win_x86_64_clang"   call dk_prepend CMAKE_ARGS -G MinGW Makefiles
+	if "%TARGET_OS%"=="win_x86_64_mingw"   call dk_prepend CMAKE_ARGS -G MinGW Makefiles
+	if "%TARGET_OS%"=="win_x86_64_ucrt"    call dk_prepend CMAKE_ARGS -G MinGW Makefiles
 	
 	
-	if "%TARGET_OS%"=="win_arm64_clang"    call dk_prependCmakeArgs -DMSYSTEM=CLANGARM64
-	if "%TARGET_OS%"=="win_x86_clang"      call dk_prependCmakeArgs -DMSYSTEM=CLANG32
-	if "%TARGET_OS%"=="win_x86_mingw"      call dk_prependCmakeArgs -DMSYSTEM=MINGW32
-	if "%TARGET_OS%"=="win_x86_64_clang"   call dk_prependCmakeArgs -DMSYSTEM=CLANG64
-	if "%TARGET_OS%"=="win_x86_64_mingw"   call dk_prependCmakeArgs -DMSYSTEM=MINGW64
-	if "%TARGET_OS%"=="win_x86_64_ucrt"    call dk_prependCmakeArgs -DMSYSTEM=UCRT64
+	if "%TARGET_OS%"=="win_arm64_clang"    call dk_prepend CMAKE_ARGS -DMSYSTEM=CLANGARM64
+	if "%TARGET_OS%"=="win_x86_clang"      call dk_prepend CMAKE_ARGS -DMSYSTEM=CLANG32
+	if "%TARGET_OS%"=="win_x86_mingw"      call dk_prepend CMAKE_ARGS -DMSYSTEM=MINGW32
+	if "%TARGET_OS%"=="win_x86_64_clang"   call dk_prepend CMAKE_ARGS -DMSYSTEM=CLANG64
+	if "%TARGET_OS%"=="win_x86_64_mingw"   call dk_prepend CMAKE_ARGS -DMSYSTEM=MINGW64
+	if "%TARGET_OS%"=="win_x86_64_ucrt"    call dk_prepend CMAKE_ARGS -DMSYSTEM=UCRT64
 	
 	
 ::	###### CMAKE_TOOLCHAIN_FILE ######
@@ -104,7 +104,7 @@ call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
 ::	call dk_assertPath TOOLCHAIN
 ::	call dk_printVar TOOLCHAIN
 ::	call dk_set TOOLCHAIN_FILE "%%TOOLCHAIN:^\=^/%%"
-::	if exist %TOOLCHAIN% call dk_appendCmakeArgs -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE%
+::	if exist %TOOLCHAIN% call dk_append CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE%
     
 	
 ::	###### WSL CMake Fix ######
--- DKBatch/functions/dk_prepend.cmd
+++ DKBatch/functions/dk_prepend.cmd
@@ -2,18 +2,18 @@
 call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
 
 ::####################################################################
-::# dk_append(variable, string)
+::# dk_prepend(variable, string)
 ::#
 ::#
-:dk_append () {
+:dk_prepend () {
 	call dk_debugFunc
 	if %__ARGC__% lss 2 (call dk_error "%__FUNCTION__%(%__ARGC__%): incorrect number of arguments")
 	
 	setlocal enableDelayedExpansion
 	for /f "tokens=1,* delims= " %%a in ("%*") do set "ALL_BUT_FIRST=%%b"   &:: all_but_first argument
-	if defined %~1 set "append=!%~1! !ALL_BUT_FIRST!"
-	if not defined %~1 set "append=!ALL_BUT_FIRST!"
-	endlocal & set "%1=%append%"
+	if defined %~1 set "prepend=!ALL_BUT_FIRST! !%~1!"
+	if not defined %~1 set "prepend=!ALL_BUT_FIRST!"
+	endlocal & set "%1=%prepend%"
 	dk_printVar %1
 goto:eof
 
@@ -24,8 +24,8 @@ goto:eof
 	call dk_debugFunc
 	
 	call dk_echo "myVar = %myVar%"
-	call dk_append myVar "abc"
+	call dk_prepend myVar "abc"
 	call dk_echo "myVar = %myVar%"
-	call dk_append myVar "zyx"
+	call dk_prepend myVar "zyx"
 	call dk_echo "myVar = %myVar%"
 goto:eof
