436817a2884027712f35d036044520ae1b3539f2
aquawicket <aquawicket@hotmail.com>
2024-07-03 04:36:02 -0700

    git commit

--- DKCMake/functions/dk_gitUpdate.cmake
+++ DKCMake/functions/dk_gitUpdate.cmake
@@ -25,7 +25,7 @@ function(dk_checkGitRemote)
 		dk_printVar(DKBRANCH_DIR)
 		dk_command("${GIT_EXE}" clone https://github.com/aquawicket/DigitalKnob.git "${DKBRANCH_DIR}")
 	endif()
-	dk_comand(cd "${DKBRANCH_DIR}") #|| dk_error "cd $${DKBRANCH_DIR} failed!"
+	dk_command(cd "${DKBRANCH_DIR}") #|| dk_error "cd $${DKBRANCH_DIR} failed!"
 	dk_command("${GIT_EXE}" pull --all)
 	dk_command("${GIT_EXE}" checkout -- .)
 	dk_command("${GIT_EXE}" checkout "${DKBRANCH}" RESULT_VARIABLE checkout_result)
@@ -33,10 +33,10 @@ function(dk_checkGitRemote)
 		dk_info "${DKBRANCH} branch selected"
 	else()
 		dk_info("Remote has no ${DKBRANCH} branch. Creating...")
-		dk_comand("${GIT_EXE}" checkout -b "${DKBRANCH}" main)
+		dk_command("${GIT_EXE}" checkout -b "${DKBRANCH}" main)
 		dk_command("${GIT_EXE}" push --set-upstream origin "${DKBRANCH}")
 	endif()
-	#dk_comand(chmod +x "${DKBRANCH_DIR}"/build)
+	#dk_command(chmod +x "${DKBRANCH_DIR}"/build)
 endfunction()
 
 
