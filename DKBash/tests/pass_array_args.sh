b534477c9298cfc04b05b09c4521e8c51c2743d1
aquawicket <aquawicket@hotmail.com>
2024-07-05 18:26:21 -0700

    git commit

--- DKBash/tests/pass_array_args.sh
+++ DKBash/tests/pass_array_args.sh
@@ -2,76 +2,38 @@
 
 # https://stackoverflow.com/a/4017175
 
-
-takes_ary_as_arg (){
-	declare -a arg1_array=("${!1}")
+takes_array(){
+	eval local arg1=('${'$1'[@]}')
+	
 	echo ""
-	for ((i=0; i < ${#arg1_array[@]}; i++ )); do 
-		echo "arg1_array[$i] = ${arg1_array[$i]}";
-	done
-	echo "arg1_array = ${arg1_array[@]}"
-	echo "arg1_array size = ${#arg1_array[@]}"
-			
-	local arg2_array=("${!2}")
-    echo ""
-	for ((i=0; i < ${#arg2_array[@]}; i++ )); do 
-		echo "arg2_array[$i] = ${arg2_array[$i]}";
-	done
-	echo "arg2_array = ${arg2_array[@]}"
-	echo "arg2_array size = ${#arg2_array[@]}"
-	
-	local output_array=(
-        "This"
-        "is"
-        "a"
-		"returned"
-        "array"
-    )
-	
-	eval "$3=(${output_array[@]})"
-	#dk_printVar "${3}"
+	echo "array name = $1"
+	echo "$1[@] = ${arg1[@]}"
+	for ((i=0; i < ${#arg1[@]}; i++)); do echo "$1[$i] = ${arg1[$i]}"; done
+	echo "$1 size = ${#arg1[@]}"
 }
 
-takes_ary_as_arg2 (){
-	local arg1_array=("${!1}")
-	echo ""
-	for ((i=0; i < ${#arg1_array[@]}; i++ )); do 
-		echo "arg1_array[$i] = ${arg1_array[$i]}";
-	done
-	echo "arg1_array = ${arg1_array[@]}"
-	echo "arg1_array size = ${#arg1_array[@]}"
+returns_array(){
+	local rtn1=("1" "2" "3" "4")
+	eval $1="(${rtn1[@]})"
 }
+
 
 
 main (){
-	local myArry1=(
-        "This"
-        "is"
-        "array"
-        "1"
-    )
+	local array1=("a" "b" "c" "d")
+	takes_array array1
 	
-	local myArry2=(
-        "And"
-        "this"
-        "is"
-		"the" 
-		"second"
-        "array"
-    )
 	
-	#takes_ary_as_arg myArry1[@] myArry2[@] return_array
-	takes_ary_as_arg2 myArry1[@]
+	local array2=()
+	returns_array array2
+	echo ""
+	echo "array name = array2"
+	echo "array2[@] = ${array2[@]}"
+	for ((i=0; i < ${#array2[@]}; i++)); do echo "array2[$i] = ${array2[$i]}"; done
+	echo "array2 size = ${#array2[@]}"
+	
 	
 	echo ""
-	for ((i=0; i < ${#return_array[@]}; i++ )); do 
-		echo "return_array[$i] = ${return_array[$i]}";
-	done
-	#echo "return_array = ${return_array[@]}"
-	#echo "return_array size = ${#return_array[@]}"
-	
-	#echo ""
 	read -rp 'press enter to exit' 
 }
-
 main
