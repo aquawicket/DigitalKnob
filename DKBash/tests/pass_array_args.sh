a27bf31cc770a6113de0d4d6be88de6f3949b4a6
aquawicket <aquawicket@hotmail.com>
2024-07-05 19:46:09 -0700

    git commit

--- DKBash/tests/pass_array_args.sh
+++ DKBash/tests/pass_array_args.sh
@@ -2,10 +2,18 @@
 
 # https://stackoverflow.com/a/4017175
 
+is_array(){
+	if [[ "$(declare -p ${1})" =~ "declare -a" ]]; then
+		echo ${1} is an array
+		return ${true}
+	fi
+	echo ${1} is NOT an array
+	return ${false}
+}
+
 takes_array(){
 	eval local arg1=('${'$1'[@]}')
-	
-	echo ""
+
 	echo "array name = $1"
 	echo "$1[@] = ${arg1[@]}"
 	for ((i=0; i < ${#arg1[@]}; i++)); do echo "$1[$i] = ${arg1[$i]}"; done
@@ -14,6 +22,7 @@ takes_array(){
 
 returns_array(){
 	local rtn1=("1" "2" "3" "4")
+	
 	eval $1="(${rtn1[@]})"
 }
 
@@ -21,16 +30,23 @@ returns_array(){
 
 main (){
 	local array1=("a" "b" "c" "d")
+	echo ""
+	is_array array1
 	takes_array array1
 	
 	
 	local array2=()
 	returns_array array2
 	echo ""
+	is_array array2
 	echo "array name = array2"
 	echo "array2[@] = ${array2[@]}"
 	for ((i=0; i < ${#array2[@]}; i++)); do echo "array2[$i] = ${array2[$i]}"; done
 	echo "array2 size = ${#array2[@]}"
+	
+	local array3="string variable"
+	echo ""
+	is_array array3
 	
 	
 	echo ""
