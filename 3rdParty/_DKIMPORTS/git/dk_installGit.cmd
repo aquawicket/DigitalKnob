b7e5dd14b200efa2b9bb141e5c6bf42dcf9e3669
aquawicket <aquawicket@hotmail.com>
2024-07-06 19:39:24 -0700

    git commit

--- 3rdParty/_DKIMPORTS/git/dk_installGit.cmd
+++ 3rdParty/_DKIMPORTS/git/dk_installGit.cmd
@@ -1,46 +1,52 @@
 @echo off
 call ../../../DKBatch/functions/DK.cmd
 
+call dk_source dk_validate
+call dk_source dk_getBasename
+call dk_source dk_removeExtension
+call dk_source dk_convertToCIdentifier
+call dk_source dk_toLower
+
+call dk_set GIT_DL_WIN_X86 https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/PortableGit-2.44.0-32-bit.7z.exe
+call dk_set GIT_DL_WIN_X86_64 https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/PortableGit-2.44.0-64-bit.7z.exe
+
 ::####################################################################
 ::# dk_installGit()
 ::#
 ::#
 :dk_installGit () {
-	call dk_debugFunc
+    call dk_debugFunc
+    if %__ARGC__% neq 0 (call dk_error "%__FUNCTION__%(%__ARGC__%): incorrect number of arguments")
 	
-	call dk_validate HOST_ARCH "call dk_getHostTriple"
-::  if "%HOST_ARCH%"=="arm32" call dk_set GIT_DL ""
-::  if "%HOST_ARCH%"=="arm64"  call dk_set GIT_DL ""
-    if "%HOST_OS%_%HOST_ARCH%"=="win_x86"    call dk_set GIT_DL "https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/PortableGit-2.44.0-32-bit.7z.exe"
-    if "%HOST_OS%_%HOST_ARCH%"=="win_x86_64" call dk_set GIT_DL "https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/PortableGit-2.44.0-64-bit.7z.exe"
+    call dk_validate HOST_ARCH "call dk_getHostTriple"
+    if "%HOST_ARCH%"=="arm32"  call dk_set GIT_DL ""
+    if "%HOST_ARCH%"=="arm64"  call dk_set GIT_DL %GIT_DL_WIN_ARM64%
+    if "%HOST_ARCH%"=="x86"    call dk_set GIT_DL %GIT_DL_WIN_X86%
+    if "%HOST_ARCH%"=="x86_64" call dk_set GIT_DL %GIT_DL_WIN_X86_64%
         
     call dk_getBasename %GIT_DL% GIT_DL_FILE
-	call dk_getBasename %GIT_DL_FILE% GIT_FOLDER
-	call dk_convertToCIdentifier %GIT_FOLDER% GIT_FOLDER
+    call dk_removeExtension %GIT_DL_FILE% GIT_DL_NAME
+    call dk_convertToCIdentifier %GIT_DL_NAME% GIT_FOLDER
     call dk_toLower %GIT_FOLDER% GIT_FOLDER
-	call dk_validate DKTOOLS_DIR "call dk_getDKPaths"
-	call dk_set GIT "%DKTOOLS_DIR%\%GIT_FOLDER%"
-    call dk_set GIT_EXE "%GIT%\bin\git.exe"
-	call dk_set GITBASH_EXE "%GIT%\git-bash.exe"
+    call dk_validate DKTOOLS_DIR "call dk_getDKPaths"
+    call dk_set GIT_EXE %DKTOOLS_DIR%\%GIT_FOLDER%\bin\git.exe
         
-    if exist "%GIT_EXE%" goto:git_installed
+    if exist "%GIT_EXE%" goto:eof
+        
     call dk_echo   
     call dk_info "Installing git . . ."
     call dk_download %GIT_DL%
-    ::echo "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" /DIR=%DKTOOLS_DIR%\%GIT_FOLDER%
-    ::"%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" /DIR="%DKTOOLS_DIR%\%GIT_FOLDER%" /VERYSILENT
-    "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" -y -o "%GIT%"
-    if not exist "%GIT_EXE%" call dk_error "cannot find git"
-	:git_installed	
-		
-		
-
-	::### Add Context Menu ###
-	call %DKIMPORTS_DIR%\git\dk_installGitContextMenu.cmd
-	
-	::### Add File Associations ###
-	call %DKIMPORTS_DIR%\git\dk_installGitFileAssociations.cmd
-	
-	
+    call dk_info "%DKDOWNLOAD_DIR%\%GIT_DL_FILE% -y -o %DKTOOLS_DIR%\%GIT_FOLDER%"
+    "%DKDOWNLOAD_DIR%\%GIT_DL_FILE%" -y -o "%DKTOOLS_DIR%\%GIT_FOLDER%"
+	   
+    if NOT exist "%GIT_EXE%" (call dk_error "cannot find git")
+        
     ::call dk_checkError
+goto:eof
+
+
+
+:DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ###
+	
+    call dk_installGit
 goto:eof
