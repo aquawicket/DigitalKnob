f473cd379f801e065de853f8be75f3efe661cd19
aquawicket <aquawicket@hotmail.com>
2024-07-07 14:01:35 -0700

    git commit

--- DKPowershell/uninstallDKPowershell.cmd
+++ DKPowershell/uninstallDKPowershell.cmd
@@ -1,7 +1,14 @@
 @echo off
-call "%DKBATCH_FUNCTIONS_DIR%\DK.cmd"
+::call "%DKBATCH_FUNCTIONS_DIR%\DK.cmd"
 
 :uninstallDKPowershell
+	::###### DKINIT ######
+	set "DIGITALKNOB_DIR=%HOMEDRIVE%%HOMEPATH%\digitalknob"
+	set "DKBRANCH_DIR=%DIGITALKNOB_DIR%\Development"
+	set "DKBATCH_FUNCTION_DIR=%DKBRANCH_DIR%\DKBatch\functions"
+	set "DKBATCH_FUNCTION_DIR_=%DKBATCH_FUNCTION_DIR%\"
+	call "%DKBATCH_FUNCTION_DIR_%DK.cmd"
+	
 	echo Uninstalling .ps1 file associations . . .
 	
 	ftype dkpowershell=
