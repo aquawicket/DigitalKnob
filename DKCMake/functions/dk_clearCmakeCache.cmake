32680065ac3c3f6c90ba3b2be5c7247a8eaac7ef
aquawicket <aquawicket@hotmail.com>
2024-07-07 13:40:05 -0700

    git commit

---
+++ DKCMake/functions/dk_clearCmakeCache.cmake
@@ -0,0 +1,19 @@
+include(${DKCMAKE_FUNCTIONS_DIR}/DK.cmake)
+#include_guard()
+
+##################################################################################
+# dk_clearCmakeCache()
+#
+#
+function(dk_clearCmakeCache)
+	dk_debugFunc(${ARGV})
+
+endfunction()
+
+
+
+function(DKTEST) ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST #######
+	dk_debugFunc(${ARGV})
+	
+	dk_clearCmakeCache()
+endfunction()
---
+++ DKCMake/functions/dk_resetAll.cmake
@@ -0,0 +1,19 @@
+include(${DKCMAKE_FUNCTIONS_DIR}/DK.cmake)
+#include_guard()
+
+##################################################################################
+# dk_resetAll()
+#
+#
+function(dk_resetAll)
+	dk_debugFunc(${ARGV})
+
+endfunction()
+
+
+
+function(DKTEST) ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST #######
+	dk_debugFunc(${ARGV})
+	
+	dk_resetAll()
+endfunction()
