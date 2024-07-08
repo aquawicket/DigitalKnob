bbfdc322edd02d9aceffed6a72357e9c47498b79
aquawicket <aquawicket@hotmail.com>
2024-07-08 13:54:54 -0700

    git commit

--- DKBatch/functions/dk_removeSpecialChars.cmd
+++ DKBatch/functions/dk_removeSpecialChars.cmd
@@ -10,40 +10,66 @@ call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
 	::call dk_debugFunc
 	::if %__ARGC__% lss 1 call dk_error "%__FUNCTION__% not enough arguments")
 	::if %__ARGC__% gtr 2 call dk_error "%__FUNCTION__% too many arguments")
+@echo on
 	echo.
-	echo "%~1"
-	set "org=%~1"
+	
+	
+	
+	::echo "%~1"|find "&">nul
+	::if not errorlevel 1 goto :skip
+	::set "org=%~1"
+	set org=%*
+	setlocal enableDelayedExpansion	
+		if "" == %org:~0,1%%org:~-1% set "org=!org:~1,-1!"
+	endlocal & set "org=%org%"
+	
+	set "org=%org:"=""%"
+	set "org=%org:<=^<%"
+	set "org=%org:>=^>%"
+	set "org=%org:&=^&%"
+	
+	::set "org="%org%""
 	echo org = %org%
-	set "var=%~1"
+
+	set "var=%org%"
+	set "replaceWith=_"
 	
-::	setlocal enableDelayedExpansion
-::		set "var=!var:"=_!"
-::		if not "!var!"=="!org!" echo " characters removed
-::		set "org=!var!"
-::	endlocal
+::	echo %org%|find """">nul
+::	if errorlevel 1 goto:skip
+::	set "var=%var:"=_%"
+::	if not "%var%" equ "%org%"
+::	echo removed quote characters
+::	set "org=%var%"
+::	pause
+::	:skip
 	
-	set "var=%var:"=_%"
-	if not "%var%"=="%org%" echo removed quote characters
+	
+	
+	::echo %~1
+	::set "org=%~1"
+	::set "org=%~1%~2%~3%~4%~5%~6%~7%~8%~9"
+		
+	::setlocal enableDelayedExpansion	
+	::	if "" == %org:~0,1%%org:~-1% set "org=!org:~1,-1!"
+	::endlocal & set "org=%org%"
+	
+	
+	
+	set "org=%org:""=_%"
+	if not "%var%" equ "%org%" echo "" characters removed
 	set "org=%var%"
+	
 	
 	set "var=%var:^^=_%"
 	if not "%var%"=="%org%" echo ^^ characters removed
 	set "org=%var%"
 	
-	set "var=%var:&=_%"
-	if not "%var%"=="%org%" echo ^& characters removed
-	set "org=%var%"
-	
-	set "var=%var:<=_%"
+	set "var=%var:^<=_%"
 	if not "%var%"=="%org%" echo ^< characters removed
 	set "org=%var%"
 	
-	set "var=%var:>=_%"
+	set "var=%var:^>=_%"
 	if not "%var%"=="%org%" echo ^> characters removed
-	set "org=%var%"
-	
-	set "var=%var:|=_%"
-	if not "%var%"=="%org%" echo ^| characters removed
 	set "org=%var%"
 	
 	set "var=%var:`=_%"
@@ -57,8 +83,6 @@ call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
 	set "var=%var:;=_%"
 	if not "%var%"=="%org%" echo ; characters removed
 	set "org=%var%"
-	
-	::=
 	
 	set "var=%var:(=_%"
 	if not "%var%"=="%org%" echo ( characters removed
@@ -96,21 +120,45 @@ call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
 	if not "%var%"=="%org%" echo = characters removed
 	set "org=%var%"
 
+	set "var=%var:|=_%"
+	if not "%var%"=="%org%" echo ^| characters removed
+	set "org=%var%"
+	
+	echo "%var%"|find "*">nul
+	if not errorlevel 1 for /f "tokens=1* delims=*" %%A in ("%var%") do (set "var=%%A%replaceWith%%%B")
+	if not "%var%"=="%org%" echo * characters removed
+	set "org=%var%"
+	
+	
+	
+	
+	::set "var=%var:^&=_%"
+	::if not "%var%"=="%org%" echo ^& characters removed
+	::set "org=%var%"
+	
+	
 	:: Simple method to detect character in a string
 	::echo "%var%"|find "=">nul
 	::if not errorlevel 1 echo equal sign detected
+	
+	::setlocal enableDelayedExpansion	
+	::	set "var=!var:%=_!"
+	::	if not "!var!"=="!org!" echo %% characters removed
+	::	set "org=!var!"
+	::endlocal
 	
 	echo var = %var%
 goto:eof
 
 :replaceEqualSign variable replaceWith
     setlocal enableDelayedExpansion
-		set "replaceWith=%~2"
+		set "equal=="
+		set "with=%~2"
         set "_s=!%~1!#"
         set "_r="
         :_replaceEqualSign
-            for /F "tokens=1* delims==" %%A in ("%_s%") do (
-                if not defined _r ( set "_r=%%A" ) else ( set "_r=%_r%%~4%replaceWith%%%A" )
+            for /F "tokens=1* delims=%equal%" %%A in ("%_s%") do (
+                if not defined _r ( set "_r=%%A" ) else ( set "_r=%_r%%~4%with%%%A" )
                 set "_s=%%B"
             )
         if defined _s goto _replaceEqualSign
@@ -125,10 +173,8 @@ goto:eof
 
 	call dk_removeSpecialChars "abc xyz"
 	call dk_removeSpecialChars "abc ^ xyz"
-	rem call dk_removeSpecialChars "abc&xyz"
 	call dk_removeSpecialChars "abc < xyz"
 	call dk_removeSpecialChars "abc > xyz"
-	rem call dk_removeSpecialChars "abc|xyz"
 	call dk_removeSpecialChars "abc ` xyz"
 	call dk_removeSpecialChars "abc , xyz"
 	call dk_removeSpecialChars "abc ; xyz"
@@ -136,11 +182,14 @@ goto:eof
 	call dk_removeSpecialChars "abc ( xyz"
 	call dk_removeSpecialChars "abc ) xyz"
 	call dk_removeSpecialChars "abc ! xyz"
-	call dk_removeSpecialChars "abc " xyz"
 	call dk_removeSpecialChars "abc \ xyz"
 	call dk_removeSpecialChars "abc [ xyz"
 	call dk_removeSpecialChars "abc ] xyz"
 	call dk_removeSpecialChars "abc . xyz"
-	rem call dk_removeSpecialChars "abc*xyz"
+	call dk_removeSpecialChars "abc * xyz"
+	call dk_removeSpecialChars "abc & xyz"
 	call dk_removeSpecialChars "abc ? xyz"
+	::call dk_removeSpecialChars "abc % xyz"
+	call dk_removeSpecialChars "abc " xyz"
+	rem call dk_removeSpecialChars "abc ^| xyz"
 goto:eof
