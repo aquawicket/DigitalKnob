2b7b1a5e83fe4362510edf36610a60c79bcd7308
aquawicket <aquawicket@hotmail.com>
2024-07-12 17:34:07 -0700

    git commit

---
+++ DKBatch/functions/dk_toggleFullscreen.cmd
@@ -0,0 +1,25 @@
+@echo off
+call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
+
+::################################################################################
+::# dk_fullscreen()
+::#
+::#
+:dk_toggleFullscreen
+	call dk_debugFunc
+	if %__ARGC__% neq 0 call dk_error "%__FUNCTION__%:%__ARGV__% incorrect number of arguments"
+	
+	(title batchfs) ^& Mshta.exe vbscript:Execute("Set Ss=CreateObject(""WScript.Shell""):Ss.AppActivate ""batchfs"":Ss.SendKeys ""{F11}"":close") ^& !@getdim!"
+goto:eof
+
+
+
+
+::####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ######
+:DKTEST
+	call dk_debugFunc
+	
+	call dk_toggleFullscreen
+	call dk_sleep 3
+	call dk_toggleFullscreen
+goto:eof
