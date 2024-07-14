dcba6ce61084bf7217376cb6d3f1b35c783a5679
aquawicket <aquawicket@hotmail.com>
2024-07-14 09:44:01 -0700

    git commit

--- DKBash/functions/DK.sh
+++ DKBash/functions/DK.sh
@@ -304,20 +304,6 @@ dk_call(){
 	"${@}"
 }
 
-##################################################################################
-# dk_command()
-#
-#	run a unix command. Install it first if it's missing
-#
-#dk_command(){
-#	dk_debugFunc
-#	
-#	dk_commandExists ${1}  || dk_install ${1}
-#	dk_commandExists ${1}  || dk_error "${1}: command not found"
-#	#dk_echo "${*}"
-#	"${@}"
-#}
-
 
 ##################################################################################
 # run DK() function
