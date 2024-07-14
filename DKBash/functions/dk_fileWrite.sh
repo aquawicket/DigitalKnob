cd93301a1ae6eff1e3e4d2bce6f34066e03bcbad
aquawicket <aquawicket@hotmail.com>
2024-07-13 23:41:32 -0400

    git commit

--- DKBash/functions/dk_fileWrite.sh
+++ DKBash/functions/dk_fileWrite.sh
@@ -9,13 +9,14 @@ dk_fileWrite (){
 	dk_debugFunc
 	[ ${#} -ne 2 ] && dk_error "${FUNCNAME}(${#}): incorrect number of arguments"
 	
-	echo "${2}" > "${1}" 
+	echo ${2} > ${1} 
 }
 
 
 
 
 DKTEST (){ ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ###
+	dk_debugFunc
 
 	dk_fileWrite "dk_fileWrite_TEST.txt" "string written by dk_fileWrite"
 }
