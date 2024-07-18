e4f91eb22fd86a18dfceee9ac93cf4f21f0f4c82
aquawicket <aquawicket@hotmail.com>
2024-07-17 23:20:04 -0700

    git commit

--- DKBash/functions/__TIME__.sh
+++ DKBash/functions/__TIME__.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
+
 [ -z "${DKINIT}" ] && . "$(dirname ${0})/DK.sh"
 
-#from_nano_to_readable() {
-#   (( ${#} )) || { printf '%s\n' 'provide atleast one argument' >&2 ; }
-#    input="${1}"
-#    withNano="$(( ${input} % 1000000000 ))"
-#    withoutNano="$(date -d@"$(( ${input} / 1000000000 ))" +"%Y-%m-%d-%H:%M:%S")"
-#    printf '%s\n' "$withoutNano.$withNano"        
-#}
+
 
 ##################################################################################
 # __TIME__()
+#
+#
+#
 #
 __TIME__() {
 	#dk_debugFunc
@@ -30,66 +28,86 @@ __TIME__() {
 #	minutes="$((seconds/60))"
 	date_minutes="$(date -d@"$seconds" +"%M")"
 #	t_minutes="$((seconds/60%60))"
+
 #	#hours="$((seconds/60/60))"
 #	hours="$((seconds/3600))"
     date_hours="$(date -d@"$seconds" +"%H")"
 #   t_hours="$((seconds/60/60%24))"
+
 #	#days="$((seconds/60/60/24))"
 #	days="$((seconds/86400))"
 #	date_days="$(date -d@"$seconds" +"%d")"
 #	t_days="$((seconds/60/60%24))"
+
 #	#weeks="$((seconds/60/60/24/7))"
 #	weeks="$((seconds/604800))"
 #	#date_weeks="$(date -d@"$seconds" +"%d")"
 #	t_weeks="$((seconds/60/60/24/7%30))"
+
 #	#months="$((seconds/60/60/24/30))"
 #	months="$((seconds/2629746))"
 #	date_months="$(date -d@"$seconds" +"%m")"
 #	t_months="$((seconds/60/60/24%10))"
+
 #	#years="$((seconds/60/60/24/365))"
 #	years="$((seconds/31556952))"
 #	date_years="$(date -d@"$seconds" +"%Y")"
 #   t_years="$((seconds/60/60/24/7%12))" 
+
 #	decades="$((seconds/315569520))"
 #	#date_decades="$((date_years / 10))"
 #	#t_decades="$((seconds/60/60/24/365%10))"
+
 #	centuries="$((seconds/3155695200))"
 #	#date_centuries="$((date_years / 100))"
 #	#t_centuries="$((seconds/60/60/24/365%100))"
+
 #	timestamp="$(date -d@"$seconds" +"%Y-%m-%d-%H:%M:%S")"
 
 #	echo "      nanoseconds = ${nanoseconds}"
 #	echo " date_nanoseconds = ${date_nanoseconds}"
+
 #	echo "     microseconds = ${microseconds}"
 #	echo "date_microseconds = ${date_microseconds}"
+
 #	echo "     milliseconds = ${milliseconds}"
 #	echo "date_milliseconds = ${date_milliseconds}"
+
 #	echo "          seconds = ${seconds}"
 #	echo "     date_seconds = ${date_seconds}"
+
 #	echo "          minutes = ${minutes}"
 #	echo "     date_minutes = ${date_minutes}"
 #	echo "        t_minutes = ${t_minutes}"
+
 #	echo "            hours = ${hours}"
 #	echo "       date_hours = ${date_hours}"
 #	echo "          t_hours = ${t_hours}"
+
 #	echo "             days = ${days}"
 #	echo "        date_days = ${date_days}"
 #	echo "           t_days = ${t_days}"
+
 #	echo "            weeks = ${weeks}"
 #	#echo "       date_weeks = ${date_weeks}"
 #	echo "          t_weeks = ${t_weeks}"
+
 #	echo "           months = ${months}"
 #	echo "      date_months = ${date_months}"
 #	echo "         t_months = ${t_months}"
+
 #	echo "            years = ${years}"
 #	echo "       date_years = ${date_years}"
 #	echo "          t_years = ${t_years}"
+
 #	echo "          decades = ${decades}"
 #	echo "     date_decades = ${date_decades}"
 #	echo "        t_decades = ${t_decades}"
+
 #   echo "        centuries = ${centuries}"
 #	echo "   date_centuries = ${date_centuries}"
 #	echo "      t_centuries = ${t_centuries}"
+
 #	echo "        timestamp = ${timestamp}"
 	dk_return "${date_hours}:${date_minutes}:${date_seconds}.${date_milliseconds}"; return
 }
--- DKBatch/functions/__TIME__.cmd
+++ DKBatch/functions/__TIME__.cmd
@@ -1,19 +1,14 @@
 @echo off
-
-echo %~0 %*
 if "%~1" == "dk_getHour" echo dk_getHour
 call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
 
-::#from_nano_to_readable() {
-::#   (( ${#} )) || { printf '%s\n' 'provide atleast one argument' >&2 ; }
-::#    input="${1}"
-::#    withNano="$(( ${input} % 1000000000 ))"
-::#    withoutNano="$(date -d@"$(( ${input} / 1000000000 ))" +"%Y-%m-%d-%H:%M:%S")"
-::#    printf '%s\n' "$withoutNano.$withNano"        
-::#}
+
 
 ::##################################################################################
 ::# __TIME__()
+::#
+::#
+::#
 ::#
 :__TIME__
 	::call dk_debugFunc
@@ -21,8 +16,8 @@ call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
 ::    nanoseconds="$(date +%s%N)"
 ::    date_nanoseconds="$(( nanoseconds % 1000000000))"
 
-::   microseconds="$(( nanoseconds / 1000))"
-::   date_microseconds="$((microseconds % 1000))"
+::    microseconds="$(( nanoseconds / 1000))"
+::    date_microseconds="$((microseconds % 1000))"
       
 ::    milliseconds="$(( nanoseconds / 1000000))"
 ::    date_milliseconds="$((milliseconds % 1000000))"
@@ -30,69 +25,89 @@ call %DKBATCH_FUNCTIONS_DIR_%DK.cmd
 ::    seconds="$((nanoseconds / 1000000000))"
 ::    date_seconds="$(date -d@"$((    seconds % 1000000000))" +"%S")"
 
-::	minutes="$((seconds/60))"
-::	date_minutes="$(date -d@"$seconds" +"%M")"
-::	t_minutes="$((seconds/60%60))"
-::	::hours="$((seconds/60/60))"
-::	hours="$((seconds/3600))"
+::    minutes="$((seconds/60))"
+::	  date_minutes="$(date -d@"$seconds" +"%M")"
+::	  t_minutes="$((seconds/60%60))"
+
+::	  ::hours="$((seconds/60/60))"
+::	  hours="$((seconds/3600))"
 ::    date_hours="$(date -d@"$seconds" +"%H")"
-::   t_hours="$((seconds/60/60%24))"
-::	::days="$((seconds/60/60/24))"
-::	days="$((seconds/86400))"
-::	date_days="$(date -d@"$seconds" +"%d")"
-::	t_days="$((seconds/60/60%24))"
-::	::weeks="$((seconds/60/60/24/7))"
-::	weeks="$((seconds/604800))"
-::	::date_weeks="$(date -d@"$seconds" +"%d")"
-::	t_weeks="$((seconds/60/60/24/7%30))"
-::	::months="$((seconds/60/60/24/30))"
-::	months="$((seconds/2629746))"
-::	date_months="$(date -d@"$seconds" +"%m")"
-::	t_months="$((seconds/60/60/24%10))"
-::	::years="$((seconds/60/60/24/365))"
-::	years="$((seconds/31556952))"
-::	date_years="$(date -d@"$seconds" +"%Y")"
-::   t_years="$((seconds/60/60/24/7%12))" 
-::	decades="$((seconds/315569520))"
-::	::date_decades="$((date_years / 10))"
-::	::t_decades="$((seconds/60/60/24/365%10))"
-::	centuries="$((seconds/3155695200))"
-::	::date_centuries="$((date_years / 100))"
-::	::t_centuries="$((seconds/60/60/24/365%100))"
-::	timestamp="$(date -d@"$seconds" +"%Y-%m-%d-%H:%M:%S")"
+::    t_hours="$((seconds/60/60%24))"
+
+::	  ::days="$((seconds/60/60/24))"
+::	  days="$((seconds/86400))"
+::	  date_days="$(date -d@"$seconds" +"%d")"
+::	  t_days="$((seconds/60/60%24))"
+
+::	  ::weeks="$((seconds/60/60/24/7))"
+::	  weeks="$((seconds/604800))"
+::	  ::date_weeks="$(date -d@"$seconds" +"%d")"
+::	  t_weeks="$((seconds/60/60/24/7%30))"
+
+::	  ::months="$((seconds/60/60/24/30))"
+::	  months="$((seconds/2629746))"
+::	  date_months="$(date -d@"$seconds" +"%m")"
+::	  t_months="$((seconds/60/60/24%10))"
+
+::	  ::years="$((seconds/60/60/24/365))"
+::	  years="$((seconds/31556952))"
+::	  date_years="$(date -d@"$seconds" +"%Y")"
+::    t_years="$((seconds/60/60/24/7%12))" 
+
+::	  decades="$((seconds/315569520))"
+::	  ::date_decades="$((date_years / 10))"
+::	  ::t_decades="$((seconds/60/60/24/365%10))"
+
+::	  centuries="$((seconds/3155695200))"
+::	  ::date_centuries="$((date_years / 100))"
+::	  ::t_centuries="$((seconds/60/60/24/365%100))"
+
+::	  timestamp="$(date -d@"$seconds" +"%Y-%m-%d-%H:%M:%S")"
 
 ::	echo "      nanoseconds = ${nanoseconds}"
 ::	echo " date_nanoseconds = ${date_nanoseconds}"
+
 ::	echo "     microseconds = ${microseconds}"
 ::	echo "date_microseconds = ${date_microseconds}"
+
 ::	echo "     milliseconds = ${milliseconds}"
 ::	echo "date_milliseconds = ${date_milliseconds}"
+
 ::	echo "          seconds = ${seconds}"
 ::	echo "     date_seconds = ${date_seconds}"
+
 ::	echo "          minutes = ${minutes}"
 ::	echo "     date_minutes = ${date_minutes}"
 ::	echo "        t_minutes = ${t_minutes}"
+
 ::	echo "            hours = ${hours}"
 ::	echo "       date_hours = ${date_hours}"
 ::	echo "          t_hours = ${t_hours}"
+
 ::	echo "             days = ${days}"
 ::	echo "        date_days = ${date_days}"
 ::	echo "           t_days = ${t_days}"
+
 ::	echo "            weeks = ${weeks}"
 ::	::echo "       date_weeks = ${date_weeks}"
 ::	echo "          t_weeks = ${t_weeks}"
+
 ::	echo "           months = ${months}"
 ::	echo "      date_months = ${date_months}"
 ::	echo "         t_months = ${t_months}"
+
 ::	echo "            years = ${years}"
 ::	echo "       date_years = ${date_years}"
 ::	echo "          t_years = ${t_years}"
+
 ::	echo "          decades = ${decades}"
 ::	echo "     date_decades = ${date_decades}"
 ::	echo "        t_decades = ${t_decades}"
+
 ::  echo "        centuries = ${centuries}"
 ::	echo "   date_centuries = ${date_centuries}"
 ::	echo "      t_centuries = ${t_centuries}"
+
 ::	echo "        timestamp = ${timestamp}"
 	echo "%date_hours%:%date_minutes%:%date_seconds%.%date_milliseconds%"; return
 goto:eof
@@ -108,52 +123,57 @@ goto:eof
 	
 ::####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ######
 :DKTEST 
-	setlocal enableDelayedExpansion
-	set MacroWithArgs=for %%# in (1 2) do if %%#==2 ( for /f "tokens=1-2" %%1 in ("^!args^!") do (%\n%
-		call dk_echo "MacroWithArgs(%%~1, %%~2)" %\n%
-		echo ARG1 = %%~1 %\n%
-		echo ARG2 = %%~2 %\n%
-		for /f "usebackq tokens=*" %%a in (`__HOUR__`) do set "cmnd_out=%%a"
-	)) else set args=
-
-	%MacroWithArgs% arg1 arg2
-	pause
+	call debugFunc
 	
-	@echo date time = %date% %time%
-	@echo time = %time%
-	@echo hour = %hour%
-::	call :dk_getHour
-::	set hour=%time:~0,2%
-::	if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
-::	echo hour=%hour%
-	set min=%time:~3,2%
-	if "%min:~0,1%" == " " set min=0%min:~1,1%
-	echo min=%min%
-	set secs=%time:~6,2%
-	if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
-	set millisecs=%time:~9,2%
-	if "%millisecs:~0,1%" == " " set millisecs=0%millisecs:~1,1%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
-	set millisecs=%time:~9,2%
-	echo millisecs=%millisecs%
+	echo date time = %date% %time%
+	echo date = %date%
+	echo time = %time%
 
+::	set centuries=%time:~0,2%
+::	if "%centuries:~0,1%" == " " set centuries=0%centuries:~1,1%
+::	echo centuries=%centuries%
 
+::	set decades=%time:~0,2%
+::	if "%decades:~0,1%" == " " set decades=0%decades:~1,1%
+::	echo decades=%decades%
+
+::	set years=%time:~0,2%
+::	if "%years:~0,1%" == " " set years=0%years:~1,1%
+::	echo years=%years%
+
+::	set months=%time:~0,2%
+::	if "%months:~0,1%" == " " set months=0%months:~1,1%
+::	echo months=%months%
+
+::	set weeks=%time:~0,2%
+::	if "%weeks:~0,1%" == " " set weeks=0%weeks:~1,1%
+::	echo weeks=%weeks%
+	
+::	set days=%time:~0,2%
+::	if "%days:~0,1%" == " " set days=0%days:~1,1%
+::	echo days=%days%
+	
+	set hours=%time:~0,2%
+	if "%hours:~0,1%" == " " set hours=0%hours:~1,1%
+	echo hours=%hours%
+	
+	set minutes=%time:~3,2%
+	if "%minutes:~0,1%" == " " set minutes=0%minutes:~1,1%
+	echo minutes=%minutes%
+	
+	set seconds=%time:~6,2%
+	if "%seconds:~0,1%" == " " set seconds=0%seconds:~1,1%
+	echo seconds=%seconds%
+	
+	set milliseconds=%time:~9,2%
+	if "%milliseconds:~0,1%" == " " set milliseconds=0%milliseconds:~1,1%
+	echo milliseconds=%milliseconds%
+	
+::	set microseconds=%time:~9,2%
+::	if "%microseconds:~0,1%" == " " set microseconds=0%microseconds:~1,1%
+::	echo microseconds=%microseconds%
+
+::	set nanoseconds=%time:~9,2%
+::	if "%nanoseconds:~0,1%" == " " set nanoseconds=0%nanoseconds:~1,1%
+::	echo nanoseconds=%nanoseconds%
 goto:eof
