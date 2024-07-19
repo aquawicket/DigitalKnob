48d516549e2831884b3596c03792b4e3273295b9
aquawicket <aquawicket@hotmail.com>
2024-07-18 23:36:21 -0700

    git commit

---
+++ DKBatch/tests/comandSubstitution.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+(set \n=^^^
+%= This creates an escaped Line Feed - DO NOT ALTER =%
+)
+
+setlocal enableDelayedExpansion
+
+set $time=^&call:getTime^&echo
+
+:main
+	::for /f "usebackq tokens=*" %%a in (`time /t`) do echo %%a
+	
+	::call :getTime
+	::echo time is &call:getTime
+	echo ^> !$time:%$time%=%second%! ^<
+	::echo the time is !$time:%$time%:%second%! seconds
+	goto:main
+goto:eof
+
+
+  
+:getTime
+	::echo :getTime
+	:: https://stackoverflow.com/a/42199181/688352
+	for /F "skip=1 usebackq delims=" %%A in (`wmic Path Win32_LocalTime Get Second`) do for /F "delims=" %%B in ("%%A") do (set "second=%%A")
+goto:eof
+
+
+
