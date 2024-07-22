8136b83b25a15f389da9cfb5b73e15b8d0c77a5f
aquawicket <aquawicket@hotmail.com>
2024-07-21 23:35:03 -0700

    git commit

---
+++ DKPhp/functions/DK.php
@@ -0,0 +1,29 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+#include(${DKPHP_FUNCTIONS_DIR}/DK.php)
+#include_guard()
+
+##################################################################################
+# dk_realpath(path) -> rtn_var
+#
+#    PHP: https://www.php.net/manual/en/function.realpath.php
+#
+function DK(){
+	echo "DK()\n";
+	
+
+}
+
+
+
+
+
+
+
+####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ######
+function DKTEST(){ 
+	echo "DKTEST()\n";
+
+}
+
+?>
