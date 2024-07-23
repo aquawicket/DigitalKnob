3b87712cf387c14e321abb621df1a57b17a900fa
aquawicket <aquawicket@hotmail.com>
2024-07-22 19:26:55 -0700

    git commit

--- DKBatch/functions/dk_setDIGITALKNOB_DIR.cmd
+++ DKBatch/functions/dk_setDIGITALKNOB_DIR.cmd
@@ -5,6 +5,7 @@ call dk_source dk_debugFunc
 call dk_source dk_error
 call dk_source dk_warning
 call dk_source dk_setDKHOME_DIR
+call dk_source dk_setDKDESKTOP_DIR
 call dk_source dk_makeDirectory
 call dk_source dk_createShortcut
 ::####################################################################
@@ -15,12 +16,17 @@ call dk_source dk_createShortcut
     call dk_debugFunc
     if %__ARGC__% neq 0 call dk_error "%__FUNCTION__%:%__ARGV__% incorrect number of arguments"
 	
-	if defined DIGITALKNOB_DIR call dk_warning "DIGITALKNOB_DIR already set to %DIGITALKNOB_DIR%" && goto:eof
+    if defined DIGITALKNOB_DIR call dk_warning "DIGITALKNOB_DIR already set to %DIGITALKNOB_DIR%" && goto:eof
 	
-	if not defined DKHOME_DIR call dk_setDKHOME_DIR
-	set "DIGITALKNOB_DIR=%DKHOME_DIR%\digitalknob"
-	if not exist "%DIGITALKNOB_DIR%" call dk_makeDirectory "%DIGITALKNOB_DIR%"
-	if not exist "%DKDESKTOP%\digitalknob.lnk" call dk_createShortcut "%DKDESKTOP%\digitalknob.lnk" "%DIGITALKNOB_DIR%"
+    if not defined DKHOME_DIR call dk_setDKHOME_DIR
+    set "DIGITALKNOB_DIR=%DKHOME_DIR%\digitalknob"
+
+    :: create directory if it doesn't exist
+    if not exist "%DIGITALKNOB_DIR%" call dk_makeDirectory "%DIGITALKNOB_DIR%"
+
+    :: create desktop shortcut if it doesn't exist
+    if not defined DKDESKTOP_DIR call dk_setDKDESKTOP_DIR
+    if not exist "%DKDESKTOP%\digitalknob.lnk" call dk_createShortcut "%DKDESKTOP%\digitalknob.lnk" "%DIGITALKNOB_DIR%"
 goto:eof
 
 
