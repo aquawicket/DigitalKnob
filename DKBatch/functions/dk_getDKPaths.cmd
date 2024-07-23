1a0f7b3d30cc1b5d338286712ccd779dac81605b
aquawicket <aquawicket@hotmail.com>
2024-07-22 19:22:24 -0700

    git commit

--- DKBatch/functions/dk_getDKPaths.cmd
+++ DKBatch/functions/dk_getDKPaths.cmd
@@ -5,6 +5,7 @@ call dk_source dk_debugFunc
 call dk_source dk_error
 call dk_source dk_setDKHOME_DIR
 call dk_source dk_setDKDESKTOP_DIR
+call dk_source dk_setDIGITALKNOB_DIR
 call dk_source dk_setDKTOOLS_DIR
 call dk_source dk_setDKDOWNLOAD_DIR
 ::####################################################################
@@ -15,8 +16,8 @@ call dk_source dk_setDKDOWNLOAD_DIR
     call dk_debugFunc
     if %__ARGC__% neq 0 call dk_error "%__FUNCTION__%:%__ARGV__% incorrect number of arguments"
 	
-	if not defined DKHOME          call dk_setDKHOME_DIR
-	if not defined DKDESKTOP       call dk_setDKDESKTOP_DIR
+    if not defined DKHOME          call dk_setDKHOME_DIR
+    if not defined DKDESKTOP       call dk_setDKDESKTOP_DIR
     if not defined DIGITALKNOB_DIR call dk_setDIGITALKNOB_DIR
     if not defined DKTOOLS_DIR     call dk_setDKTOOLS_DIR
     if not defined DKDOWNLOAD_DIR  call dk_setDKDOWNLOAD_DIR
