ad7161c2a87c32368e64f117439beae137723751
aquawicket@hotmail.com <aquawicket>
2024-07-23 20:36:32 -0700

    git commit

--- DKPhp/dk_runPhpServer.cmd
+++ DKPhp/dk_runPhpServer.cmd
@@ -10,9 +10,9 @@ call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	call dk_validate PHP "call %DKIMPORTS_DIR%\php-src\dk_installPhp.cmd"
 	
 	call dk_validate DKPHP_DIR "call dk_validateBranch"
-	call dk_copy %PHP%\php.exe %DKPHP_DIR%\php_local.exe
-	call dk_copy %PHP%\php.exe %DKPHP_DIR%\php_web.exe
-	copy %PHP%\php8ts.dll %DKPHP_DIR%\php8ts.dll
+	call dk_copy %PHP%\php.exe %DKPHP_DIR%\php_local.exe OVERWRITE
+	call dk_copy %PHP%\php.exe %DKPHP_DIR%\php_web.exe OVERWRITE
+	call dk_copy %PHP%\php8ts.dll %DKPHP_DIR%\php8ts.dll OVERWRITE
 	
 	call dk_validate DKBRANCH_DIR "call dk_validateBranch"
 	cd %DKBRANCH_DIR%
