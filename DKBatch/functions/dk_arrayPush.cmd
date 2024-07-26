c41f063ad8112fedabc57e10c7590d2eee2579ca
aquawicket@hotmail.com <aquawicket>
2024-07-26 03:56:50 -0400

    git commit

--- DKBatch/functions/dk_arrayPush.cmd
+++ DKBatch/functions/dk_arrayPush.cmd
@@ -4,12 +4,23 @@ call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 ::################################################################################
 ::# dk_arrayPush(array, element1, element2, /* …, */ elementN)
 ::#
+::#    The push() method of Array instances adds the specified elements to the end of an array and returns the new length of the array.
+::#
+::#    PARAMETERS
+::#    element1, …, elementN
+::#        The element(s) to add to the end of the array.
+::#
+::#    RETURN VALUE
+::#    The new length property of the object upon which the method was called.
+::#
+::#    REFERENCE
 ::#    https://www.w3schools.com/js/js_array_methods.asp#mark_push
 ::#    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
 ::#
 :dk_arrayPush
 	call dk_debugFunc
-	if %__ARGC__% neq 2 call dk_error "%__FUNCTION__%:%__ARGV__% incorrect number of arguments"
+	if %__ARGC__% lss 2 call dk_error "%__FUNCTION__%:%__ARGV__% not enough arguments"
+	if %__ARGC__% gtr 3 call dk_error "%__FUNCTION__%:%__ARGV__% too many arguments"
 	
 	setlocal
 	call dk_arrayLength %~1 end_index
@@ -23,21 +34,18 @@ goto:eof
 :DKTEST
 	call dk_debugFunc
 	
-	::set "MyArray[0]=a"
-	::set "MyArray[1]=b"
-	::set "MyArray[2]=c"
+	call dk_arrayPush myArrayA "a b c"          &:: new_lengthA
+	call dk_printVar myArrayA
+	:: dk_printVar new_lengthA
+	call dk_echo
 	
-	call dk_printVar MyArray
+	call dk_arrayPush myArrayA "1 2 3" "d e f"  &:: new_lengthA
+	call dk_printVar myArrayA
+	:: dk_printVar new_lengthA
+	call dk_echo
 	
-	call dk_arrayPush MyArray a
-	call dk_printVar MyArray
-	
-	call dk_arrayPush MyArray b
-	call dk_printVar MyArray
-	
-	call dk_arrayPush MyArray c
-	call dk_printVar MyArray
-	
-	call dk_arrayPush MyArray d
-	call dk_printVar MyArray
+	call dk_arrayPush myArrayA "4 5 6" "h i j"  &:: new_lengthA
+	call dk_printVar myArrayA
+	:: dk_printVar new_lengthA
+	call dk_echo
 goto:eof
--- DKBatch/functions/dk_arrayShift.cmd
+++ DKBatch/functions/dk_arrayShift.cmd
@@ -3,13 +3,26 @@ call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 
 ::################################################################################
 ::# dk_arrayShift(array)
+::# dk_arrayShift(array, rtn_var)
 ::#
+::#	 Removes the first element from an array and returns that removed element. 
+::#    This method changes the length of the array.
+::#
+::#    PARAMETERS
+::#    array
+::#
+::#    RETURN VALUE
+::#    The removed element from the array; undefined if the array is empty.
+::#    
+::#    REFERENCE
 ::#    https://www.w3schools.com/js/js_array_methods.asp#mark_shift
 ::#    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
 ::#
 :dk_arrayShift
 	call dk_debugFunc
-	if %__ARGC__% neq 1 call dk_error "%__FUNCTION__%:%__ARGV__% incorrect number of arguments"
+	if %__ARGC__% lss 1 call dk_error "%__FUNCTION__%:%__ARGV__% not enough arguments"
+	if %__ARGC__% gtr 2 call dk_error "%__FUNCTION__%:%__ARGV__% too many arguments"
+	::dk_validateArgs array
 	
 	setlocal
 	set "_arry_=%~1"
@@ -22,8 +35,8 @@ call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 		set /a prev+=1
 		goto:dk_arrayShift_loop
 	)
-	::call dk_unset %_arry_%[%prev%]
-	endlocal & set "%_arry_%[%prev%]="
+	endlocal & call dk_unset %_arry_%[%prev%]
+	::endlocal & set "%_arry_%[%prev%]="
 goto:eof
 
 
@@ -36,29 +49,44 @@ goto:eof
 :DKTEST
 	call dk_debugFunc
 	
-	set "MyArray[0]=a"
-	set "MyArray[1]=b"
-	set "MyArray[2]=c"
-	set "MyArray[3]=d"
-	set "MyArray[4]=e"
+	set "myArrayA[0]=a b c"
+	set "myArrayA[1]=1 2 3"
+	set "myArrayA[2]=d e f"
+	set "myArrayA[3]=4 5 6"
+	set "myArrayA[4]=h i j"
 	
-	call dk_printVar MyArray
+	call dk_printVar myArrayA
+	call dk_echo
 	
-	call dk_arrayShift MyArray
-	call dk_printVar MyArray
+	call dk_arrayShift myArrayA removedA
+	call dk_printVar myArrayA
+	call dk_printVar removedA
+	call dk_echo
 	
-	call dk_arrayShift MyArray
-	call dk_printVar MyArray
+	call dk_arrayShift myArrayA removedA
+	call dk_printVar myArrayA
+	call dk_printVar removedA
+	call dk_echo
 	
-	call dk_arrayShift MyArray
-	call dk_printVar MyArray
+	call dk_arrayShift myArrayA removedA
+	call dk_printVar myArrayA
+	call dk_printVar removedA
+	call dk_echo
 	
-	call dk_arrayShift MyArray
-	call dk_printVar MyArray
+	call dk_arrayShift myArrayA removedA
+	call dk_printVar myArrayA
+	call dk_printVar removedA
+	call dk_echo
 	
-	call dk_arrayShift MyArray
-	call dk_printVar MyArray
+	call dk_arrayShift myArrayA removedA
+	call dk_printVar myArrayA
+	call dk_printVar removedA
+	call dk_echo
 	
-	call dk_arrayShift MyArray
-	call dk_printVar MyArray
+	:: FIXME:  out of array bounds past here
+::	call dk_arrayShift myArrayA removedA
+::	call dk_printVar myArrayA
+::	call dk_printVar removedA
+	call dk_echo
+	call dk_echo
 goto:eof
