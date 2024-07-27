4370232e858500fe65470af414b554df301d6226
aquawicket@hotmail.com <aquawicket>
2024-07-27 12:40:05 -0400

    git commit

---
+++ DKVb/uninstallDKVb.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+
+:uninstallDKVb
+	echo Uninstalling DKVb . . .
+
+	::###### DKINIT ######
+	set "DKBATCH_FUNCTIONS_DIR=..\DKBatch\functions"
+	set "DKBATCH_FUNCTIONS_DIR_=%DKBATCH_FUNCTIONS_DIR%\"
+	call "%DKBATCH_FUNCTIONS_DIR%\DK.cmd"
+	
+	call dk_echo "Uninstalling DKVb file associations . . ."
+	ftype dkvb=
+	assoc .vbs=
+	call dk_registryDeleteKey "HKEY_CLASSES_ROOT\dkvb"
+	pause
+goto:eof
