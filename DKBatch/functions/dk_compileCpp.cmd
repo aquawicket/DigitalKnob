6c64198777a3dc07eb033763b2996370f3ce1d37
aquawicket@hotmail.com <aquawicket>
2024-07-29 02:44:30 -0400

    git commit

--- DKBatch/functions/dk_compileCpp.cmd
+++ DKBatch/functions/dk_compileCpp.cmd
@@ -4,21 +4,23 @@ call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 call dk_source dk_debugFunc
 call dk_source dk_error
 ::####################################################################
-::# dk_compile(filepath)
+::# dk_compileCpp(filepath)
 ::#
 ::#
-:dk_compile
+:dk_compileCpp
 	call dk_debugFunc
 	if %__ARGC__% lss 1 call dk_error "%__FUNCTION__%:%__ARGV__% not enough arguments"
 	::if %__ARGC__% gtr 1 call dk_error "%__FUNCTION__%:%__ARGV__% too many arguments"
 
 	set "filepath=%~1"
+	set "appname=%~2"
+	if not defined appname set "appname=temp"
 	
 	call dk_validate DKIMPORTS_DIR "call dk_validateBranch"
 	call dk_validate GXX_EXE "call %DKIMPORTS_DIR%\gcc\dk_installGcc.cmd"
 	
 	::gcc -o [executable_name] [source_file].c
-	%GXX_EXE% -o temp -static "%filepath%"
+	%GXX_EXE% -o %appname% -static "%filepath%"
 goto:eof
 
 
