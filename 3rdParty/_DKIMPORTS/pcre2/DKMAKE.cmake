c57527ce5b7faa00bf924b3f47ee3bb4c9a94186
aquawicket@hotmail.com <aquawicket>
2024-07-29 14:03:07 -0400

    git commit

--- 3rdParty/_DKIMPORTS/pcre2/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/pcre2/DKMAKE.cmake
@@ -3,7 +3,8 @@ include(${DKCMAKE_FUNCTIONS_DIR}/DK.cmake)
 
 
 ### IMPORT ###
-dk_import(https://github.com/PCRE2Project/pcre2.git)
+dk_import(https://github.com/PCRE2Project/pcre2/archive/refs/heads/master.zip)
+#dk_import(https://github.com/PCRE2Project/pcre2.git)
 
 
 #TODO
