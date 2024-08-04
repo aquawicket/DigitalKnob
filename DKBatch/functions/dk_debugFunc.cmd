d32d3ed85b6615ee390bbc8096b7da202335f7c7
aquawicket <aquawicket>
2024-08-04 12:50:44 -0700

    git commit

--- DKBatch/functions/dk_debugFunc.cmd
+++ DKBatch/functions/dk_debugFunc.cmd
@@ -18,14 +18,37 @@ if not defined DKSTACK_marker          set /a "DKSTACK_marker=1"
 ::################################################################################
 ::# dk_debugFunc()
 ::#
+::#   This function is placed at the entry point of every function in the library. It provides debugging information for the parent function at execution time when enabled.
+::#   This function also implements a managed callstack for languages that do not have a implemented debugging callstack. Various other debugging tasks can be preformed 
+::#   such as argument count and type checking, parameter variable value watching, function execution timing and trigger breakpoints before and after function calls.
+::#	  Any functions named in the breakpoint list will pause execution when execution encounters the function call. Breakpoints may pause execution at the function entry point,
+::#   before the function is entered into the callstack, after entry and the trace is printed, or at the start of the next call, before the next calls callstack is entered.
 ::#
+::#   <TIMESTAMP> <ELAPSED_TIME>
+::#   1: If the previous function has a breakpoint set for the end of the call, pause execution here, before creating a new callstack entry.
+::#      OR, If the current function has a breakpoint set for before entry, pause execution here, before creating a new callstack entry.
+::#
+::#   2: Create a callstack entry containing the Timestamp, Current file, function, line number, arguments, argument count and argument values. 
+::#      As well as the calling function, file and line number
+::#
+::#   3: Check that the function paramter definition arguments match what the function received.  For example.  
+::#          dk_debugFunc(string, int, rtn:string)      "The function expects 3 arguments. Parameter 1 a string followed by an int amd a return string variable;
+::#      If the received arguments don't match the count or type defined, dk_debugFunc will throw an error and print the callstack.
+::#
+::#   4: If debug tracing is enabled, each function and parameter values will printed as the functions are called. Timestamp and Elapsed time since last callstack entry 
+::#      will also be printed to the debugging console. Any functions named in the exclusion list will not be printed. Any functions named in the inclusion list will always be printed.
+::#      unless debug tracing is explicitly turned off. 
+::#
+::#   5: If the current function has a breakpoint set for at entry, pause execution here, after creating and printing the new callstack entry. 
+::#   <TIMESTAMP>
+::#   
 :dk_debugFunc
 	if "%*" neq "" call dk_error "dk_debugFunc: too many arguments"
 	
 	call dk_callStack
 	:dk_callStackReturn
 	
-	:: find the first matching __CALLER__ in the stack.  If a match is found, replace that frame with the current frame, and remove all elements after
+	::TODO:  find the first matching __CALLER__ in the stack.  If a match is found, replace that frame with the current frame, and remove all elements after
 	set "DKSTACK[%DKSTACK_marker%].__FILE__=%__FILE__%"
 	set "DKSTACK[%DKSTACK_marker%].__FUNCTION__=%__FUNCTION__%"
 	set "DKSTACK[%DKSTACK_marker%].__ARGS__=%__ARGS__%"
