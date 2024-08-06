601e5cf6fa98d074056dc69bb07270a61e7b0e41
aquawicket <aquawicket>
2024-08-06 05:52:00 -0400

    git commit

---
+++ DKBatch/functions/dk_getDate.cmd
@@ -0,0 +1,37 @@
+@echo off
+call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
+
+::################################################################################
+::# dk_getDate(year month day)
+::#
+::#    REFERENCE: https://ss64.com/nt/syntax-gettime.html
+::#
+:dk_getDate
+	call dk_debugFunc
+	call dk_minMaxArgs 3
+	
+	setlocal
+	set t=2&if "%date%z" LSS "A" set t=1
+	for /f "skip=1 tokens=2-4 delims=(-)" %%a in ('echo/^|date') do (
+		for /f "tokens=%t%-4 delims=.-/ " %%d in ('date/t') do (
+			set %%a=%%d
+			set %%b=%%e
+			set %%c=%%f
+		)
+	)
+	endlocal & set "%1=%yy%" & set "%2=%mm%" & set "%3=%dd%"
+goto:eof
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+	call dk_debugFunc
+	call dk_minMaxArgs 0
+	
+	echo date = %date%
+	call dk_getDate year month day
+	echo date = %month%/%day%/%year%
+goto:eof	
--- DKBatch/functions/dk_getTime.cmd
+++ DKBatch/functions/dk_getTime.cmd
@@ -1,20 +1,28 @@
 @echo off
 call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 
+::################################################################################
+::# dk_getTime(hour minute second centisecond)
+::#
+::#    REFERENCE: https://ss64.com/nt/syntax-gettime.html
+::#
 :dk_getTime
+	call dk_debugFunc
+	call dk_minMaxArgs 4
+	
 	setlocal
 	for /f "tokens=1-3 delims=1234567890 " %%a in ("%time%") do set "delims=%%a%%b%%c"
 	for /f "tokens=1-4 delims=%delims%" %%G in ("%time%") do (
-		set _hh=%%G
-		set _min=%%H
-		set _ss=%%I
-		set _ms=%%J
+		set _hour=%%G
+		set _minute=%%H
+		set _second=%%I
+		set _centisecond=%%J
 	)
 	:: Strip any leading spaces
-	set _hh=%_hh: =%
+	set _hour=%_hour: =%
 	:: Ensure the hours have a leading zero
-	if 1%_hh% lss 20 set _hh=0%_hh%
-	endlocal & set %1=%_hh%:%_min%:%_ss%.%_ms%
+	if 1%_hour% lss 20 set _hour=0%_hour%
+	endlocal & set "%1=%_hour%" & set "%2=%_minute%" & set "%3=%_second%" & set "%4=%_centisecond%"
 goto:eof
 
 
@@ -26,6 +34,7 @@ goto:eof
 	call dk_debugFunc
 	call dk_minMaxArgs 0
 	
-	call dk_getTime myTime
-	call dk_printVar myTime
+	echo time = %time%
+	call dk_getTime hour minute second centisecond
+	echo time = %hour%:%minute%:%second%.%centisecond%
 goto:eof	
