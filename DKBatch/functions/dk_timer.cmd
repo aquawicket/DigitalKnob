b1b3fa9ca9b20b82721ca281ad22c04bbedfc8eb
aquawicket <aquawicket>
2024-08-06 02:58:33 -0700

    git commit

--- DKBatch/functions/dk_timer.cmd
+++ DKBatch/functions/dk_timer.cmd
@@ -28,33 +28,42 @@ call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 			set %%c=%%f
 		)
 	)
-	for /f "tokens=5-7 delims=:. " %%a in ('echo/^|time') do (
-	  set hh=%%a
-	  set nn=%%b
-	  set ss=%%c
-	)
+	set "year=%yy%"
 	set "month=%mm%"
 	set "day=%dd%"
-	set "year=%yy%"
-	set "hour=%hh%"
-	set "minute=%nn%"
-	set "second=%ss%"
-	echo DATE %month%/%day%/%year%
-	echo TIME %hour%:%minute%:%second%
-	
-	
-	
-	set /a dd=100%dd%%%100,mm=100%mm%%%100
-	set /a z=14-mm,z/=12,y=yy+4800-z,m=mm+12*z-3,j=153*m+2
-	set /a j=j/5+dd+y*365+y/4-y/100+y/400-2472633
-	set /a hh=100%hh%%%100,nn=100%nn%%%100,ss=100%ss%%%100
-	set /a j=j*86400+hh*3600+nn*60+ss
-	for /f "tokens=1-3 delims= " %%a in ('echo/%ID%') do (
-		set l=%%a&set f=%%b&set c=%%c
+	for /f "tokens=5-8 delims=:. " %%a in ('echo/^|time') do (
+	  set hour=%%a
+	  set minute=%%b
+	  set second=%%c
+	  set centisecond=%%d
 	)
-	if {%c%}=={} endlocal & set %1=0 0 %j% & goto :EOF
-	set /a l=j-c-f,f+=l
-	endlocal & set %1=%l% %f% %c%
+
+	echo TIMESTAMP %month%/%day%/%year%-%hour%:%minute%:%second%.%centisecond%
+	
+	
+	
+	set /a day=100%day% %% 100
+	set /a month=100%month% %% 100
+	set /a z=14-month
+	set /a z/=12
+	set /a y=year+4800-z
+	set /a m=month+12*z-3
+	set /a j=153*m+2
+	set /a j=j/5+day+y*365+y/4-y/100+y/400-2472633
+	set /a hour=100%hour% %% 100
+	set /a minute=100%minute% %% 100
+	set /a second=100%second% %% 100
+	set /a j=j*86400+hour*3600+minute*60+second
+	echo j = %j%
+	for /f "tokens=1-3 delims= " %%a in ('echo/%ID%') do (
+		set last=%%a
+		set first=%%b
+		set init=%%c
+	)
+	if {%init%}=={} endlocal & set %1=0 0 %j% & goto:eof
+	set /a last=j-init-first
+	set /a first+=last
+	endlocal & set %1=%last% %first% %init%
 goto:eof
 
 
