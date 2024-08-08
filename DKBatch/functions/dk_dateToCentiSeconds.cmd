0dda697bc235e56281638453060af26915c85068
aquawicket <aquawicket>
2024-08-07 20:19:45 -0400

    git commit

--- DKBatch/functions/dk_dateToCentiSeconds.cmd
+++ DKBatch/functions/dk_dateToCentiSeconds.cmd
@@ -1,8 +1,8 @@
 @echo off
 call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 
-::##################################################################################################
-::# dk_dateToCentiSeconds(centiseconds %centiseconds% %second% %minute% %hour% %day% %month% %year%)
+::#########################################################################################################
+::# dk_dateToCentiSeconds(seconds centiseconds %centisecond% %second% %minute% %hour% %day% %month% %year%)
 ::#
 ::#
 :dk_dateToCentiSeconds
@@ -10,27 +10,28 @@ call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	if %__ARGC__% LSS 1 call dk_error "%__FUNCTION__%(): not enough arguments"
 	
 	setlocal
-	if "%~2" equ "" (set "cs=0")    else (set /a "cs=100%~2%%100")
-	if "%~3" equ "" (set "ss=0")    else (set /a "ss=100%~3%%100")
-	if "%~4" equ "" (set "nn=0")    else (set /a "nn=100%~4%%100")
-	if "%~5" equ "" (set "hh=0")    else (set /a "hh=100%~5%%100")
-	if "%~6" equ "" (set "dd=1")    else (set /a "dd=100%~6%%100")
-	if "%~7" equ "" (set "mm=1")    else (set /a "mm=100%~7%%100")
-	if "%~8" equ "" (set "yy=1970") else (set /a "yy=10000%~8%%10000")
+	if "%~3" equ "" (set "cs=0")    else (set /a "cs=%~3")
+	::if "%~3" equ "" (set "cs=0")    else (set /a "cs=100%~3%%100")
+	if "%~4" equ "" (set "ss=0")    else (set /a "ss=100%~4%%100")
+	if "%~5" equ "" (set "nn=0")    else (set /a "nn=100%~5%%100")
+	if "%~6" equ "" (set "hh=0")    else (set /a "hh=100%~6%%100")
+	if "%~7" equ "" (set "dd=1")    else (set /a "dd=100%~7%%100")
+	if "%~8" equ "" (set "mm=1")    else (set /a "mm=100%~8%%100")
+	if "%~9" equ "" (set "yy=1970") else (set /a "yy=10000%~9%%10000")
 
 	set /a "dd=dd-1"
 	set /a "mm=mm-1"
 	set /a "yy=yy-1970"
 	
 	set /a "CentiSeconds_cs=%cs%"
-	set /a "Seconds_cs=%ss%*100+%CentiSeconds_cs%"
-	set /a "Minutes_cs=%nn%*60*100+%Seconds_cs%"
-	set /a "Hours_cs=%hh%*60*60*100+%Minutes_cs%"
-	set /a "Days_cs=%dd%*24*60*60*100+%Hours_cs%
-	set /a "Months_cs=(%mm%*304/10)*24*60*60*100+%Days_cs%"
-	::set /a "Years_cs=(%yy%*12*304/10)*24*60*60*100+%Months_cs%"
-	::if %Years_cs% gtr 9999999999 set /a "Years_cs=Years_cs+216000000"
-	endlocal & set %1=%Months_cs%
+	set /a "Seconds_ss=%ss%"
+	set /a "Minutes_ss=%nn%*60+%Seconds_ss%"
+	set /a "Hours_ss=%hh%*60*60+%Minutes_ss%"
+	set /a "Days_ss=%dd%*24*60*60+%Hours_ss%
+	set /a "Months_ss=(%mm%*304/10)*24*60*60+%Days_ss%"
+	set /a "Years_ss=(%yy%*12*304/10)*24*60*60+%Months_ss%"
+	if %Years_ss% gtr 99999999 set /a "Years_ss=Years_ss+2160000"
+	endlocal & set "%1=%Years_ss%" & set "%2=%CentiSeconds_cs%"
 goto:eof
 
 
@@ -46,18 +47,18 @@ goto:eof
 	
 	echo:
 	echo ###### dk_dateToCentiSeconds ######
-	::call dk_dateToCentiSeconds centiseconds %CentiSecond% %Second% %Minute% %Hour% %Day% %Month% %Year% 
-	::echo %Year%/%Month%/%Day% %Hour%:%Minute%:%Second%.%CentiSecond% = %centiseconds% centiseconds
-	call dk_dateToCentiSeconds centiseconds %CentiSecond% %Second% %Minute% %Hour% %Day% %Month%
-	echo %Month%/%Day% %Hour%:%Minute%:%Second%.%CentiSecond% = %centiseconds% centiseconds
-	call dk_dateToCentiSeconds centiseconds %CentiSecond% %Second% %Minute% %Hour% %Day%
-	echo %Day% %Hour%:%Minute%:%Second%.%CentiSecond% = %centiseconds% centiseconds
-	call dk_dateToCentiSeconds centiseconds %CentiSecond% %Second% %Minute% %Hour%
-	echo %Hour%:%Minute%:%Second%.%CentiSecond% = %centiseconds% centiseconds
-	call dk_dateToCentiSeconds centiseconds %CentiSecond% %Second% %Minute%
-	echo %Minute%:%Second%.%CentiSecond% = %centiseconds% centiseconds
-	call dk_dateToCentiSeconds centiseconds %CentiSecond% %Second%
-	echo :%Second%.%CentiSecond% = %centiseconds% centiseconds
-	call dk_dateToCentiSeconds centiseconds %CentiSecond%
-	echo .%CentiSecond% = %centiseconds% centiseconds
+	call dk_dateToCentiSeconds seconds centiseconds %CentiSecond% %Second% %Minute% %Hour% %Day% %Month% %Year% 
+	echo %Year%/%Month%/%Day% %Hour%:%Minute%:%Second%.%CentiSecond% = %seconds%.%centiseconds% seconds
+	call dk_dateToCentiSeconds seconds centiseconds %CentiSecond% %Second% %Minute% %Hour% %Day% %Month%
+	echo %Month%/%Day% %Hour%:%Minute%:%Second%.%CentiSecond% = %seconds%.%centiseconds% seconds
+	call dk_dateToCentiSeconds seconds centiseconds %CentiSecond% %Second% %Minute% %Hour% %Day%
+	echo %Day% %Hour%:%Minute%:%Second%.%CentiSecond% = %seconds%.%centiseconds% seconds
+	call dk_dateToCentiSeconds seconds centiseconds %CentiSecond% %Second% %Minute% %Hour%
+	echo %Hour%:%Minute%:%Second%.%CentiSecond% = %seconds%.%centiseconds% seconds
+	call dk_dateToCentiSeconds seconds centiseconds %CentiSecond% %Second% %Minute%
+	echo %Minute%:%Second%.%CentiSecond% = %seconds%.%centiseconds% seconds
+	call dk_dateToCentiSeconds seconds centiseconds %CentiSecond% %Second%
+	echo :%Second%.%CentiSecond% = %seconds%.%centiseconds% seconds
+	call dk_dateToCentiSeconds seconds centiseconds %CentiSecond% %Second%
+	echo .%CentiSecond% = %seconds%.%centiseconds% seconds
 goto:eof
