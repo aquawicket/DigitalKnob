32dc643a11b35697651374679bfacdc4e08de79d
aquawicket <aquawicket>
2024-08-12 17:02:26 -0400

    git commit

--- DKBatch/functions/dk_call.cmd
+++ DKBatch/functions/dk_call.cmd
@@ -11,8 +11,9 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	
 	if not exist "%DKBATCH_FUNCTIONS_DIR%\%~1.cmd" (
 		call dk_source "%~1"
-		if not exist "%DKBATCH_FUNCTIONS_DIR%\%~1.cmd" echo ERROR: failed to dowload %~1 & goto:eof
-		if not exist "%DKBATCH_FUNCTIONS_DIR%\dk_isCRLF.cmd" call dk_isCRLF "%~1" || call dk_fileToCRLF "%~1"
+		if not exist "%DKBATCH_FUNCTIONS_DIR%\%~1.cmd" echo ERROR: failed to dowload %~1.cmd & goto:eof
+		if not exist "%DKBATCH_FUNCTIONS_DIR%\dk_isCRLF.cmd" call dk_source dk_isCRLF
+		if exist "%DKBATCH_FUNCTIONS_DIR%\dk_isCRLF.cmd" call dk_isCRLF "%DKBATCH_FUNCTIONS_DIR%\%~1.cmd" || call dk_fileToCRLF "%DKBATCH_FUNCTIONS_DIR%\%~1.cmd"
 	)
 	
 	::echo "call %*"
