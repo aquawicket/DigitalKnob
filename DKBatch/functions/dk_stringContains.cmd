8bf438c56a0d47b3ff034b16d04d31eb3f6efbd3
aquawicket <aquawicket>
2024-08-12 16:08:51 -0400

    git commit

--- DKBatch/functions/dk_stringContains.cmd
+++ DKBatch/functions/dk_stringContains.cmd
@@ -9,7 +9,7 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	call dk_debugFunc 2 3
 	
 	setlocal enableDelayedExpansion
-	if "!!" neq "" call dk_error "%__FUNCTION__% requires delayed expansion"
+	if "!!" neq "" %dk_call% dk_error "%__FUNCTION__% requires delayed expansion"
 	
     set "_haystack_=%~1"
     set "_needle_=%~2"
@@ -32,44 +32,44 @@ goto:eof
 	call dk_debugFunc 0
 	
     ::###### Using if return value
-	call dk_echo
-	call dk_set string "There is a needle in this haystack"
-	call dk_set substring "needle"
-	call dk_stringContains "%string%" "%substring%" result
-	if "%result%" equ "true" (call dk_echo "string contains substring") else (call dk_echo "string does NOT contain substring")
+	%dk_call% dk_echo
+	%dk_call% dk_set string "There is a needle in this haystack"
+	%dk_call% dk_set substring "needle"
+	%dk_call% dk_stringContains "%string%" "%substring%" result
+	if "%result%" equ "true" (%dk_call% dk_echo "string contains substring") else (%dk_call% dk_echo "string does NOT contain substring")
 	
-	call dk_echo
-	call dk_set string "There is a needle in this haystack"
-	call dk_set substring "straw"
-	call dk_stringContains "%string%" "%substring%" result
-	if "%result%" equ "true" (call dk_echo "string contains substring") else (call dk_echo "string does NOT contain substring")
+	%dk_call% dk_echo
+	%dk_call% dk_set string "There is a needle in this haystack"
+	%dk_call% dk_set substring "straw"
+	%dk_call% dk_stringContains "%string%" "%substring%" result
+	if "%result%" equ "true" (%dk_call% dk_echo "string contains substring") else (%dk_call% dk_echo "string does NOT contain substring")
 	::FIXME: ERRORLEVEL is still 1 
 	
 	
 	::###### Using if ERRORLEVEL
-	call dk_echo
-	call dk_set string "There is a needle in this haystack"
-	call dk_set substring "needle"
-	call dk_stringContains "%string%" "%substring%"
-	if not ERRORLEVEL 1 (call dk_echo "string contains substring") else (call dk_echo "string does NOT contain substring")
+	%dk_call% dk_echo
+	%dk_call% dk_set string "There is a needle in this haystack"
+	%dk_call% dk_set substring "needle"
+	%dk_call% dk_stringContains "%string%" "%substring%"
+	if not ERRORLEVEL 1 (%dk_call% dk_echo "string contains substring") else (%dk_call% dk_echo "string does NOT contain substring")
 	
-	call dk_echo
-	call dk_set string "There is a needle in this haystack"
-	call dk_set substring "straw"
-	call dk_stringContains "%string%" "%substring%"
-	if not ERRORLEVEL 1 (call dk_echo "string contains substring") else (call dk_echo "string does NOT contain substring")
+	%dk_call% dk_echo
+	%dk_call% dk_set string "There is a needle in this haystack"
+	%dk_call% dk_set substring "straw"
+	%dk_call% dk_stringContains "%string%" "%substring%"
+	if not ERRORLEVEL 1 (%dk_call% dk_echo "string contains substring") else (%dk_call% dk_echo "string does NOT contain substring")
 	::FIXME: ERRORLEVEL is still 1 
 	
 	
 	::###### Using && and || conditionals
-	call dk_echo
-	call dk_set string "There is a needle in this haystack"
-	call dk_set substring "needle"
-	call dk_stringContains "%string%" "%substring%" && (call dk_echo "string contains substring") || (call dk_echo "string does NOT contain substring")
+	%dk_call% dk_echo
+	%dk_call% dk_set string "There is a needle in this haystack"
+	%dk_call% dk_set substring "needle"
+	%dk_call% dk_stringContains "%string%" "%substring%" && (%dk_call% dk_echo "string contains substring") || (%dk_call% dk_echo "string does NOT contain substring")
 
-	call dk_echo
-	call dk_set string "There is a needle in this haystack"
-	call dk_set substring "straw"
-	call dk_stringContains "%string%" "%substring%" && (call dk_echo "string contains substring") || (call dk_echo "string does NOT contain substring")
+	%dk_call% dk_echo
+	%dk_call% dk_set string "There is a needle in this haystack"
+	%dk_call% dk_set substring "straw"
+	%dk_call% dk_stringContains "%string%" "%substring%" && (%dk_call% dk_echo "string contains substring") || (%dk_call% dk_echo "string does NOT contain substring")
 	::FIXME: ERRORLEVEL is still 1
 goto:eof
