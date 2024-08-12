5c4a5133cd1ef6f919c313caa125cfcfa9998fd3
aquawicket <aquawicket>
2024-08-12 16:50:35 -0400

    git commit

---
+++ DKBatch/functions/dk_isCRLF.cmd
@@ -0,0 +1,60 @@
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
+
+::####################################################################
+::# dk_isCRLF(file rtn_var)
+::#
+::#
+:dk_isCRLF
+	call dk_debugFunc 1 2
+	setlocal
+
+	call type "%~1" | c:\Windows\System32\find.exe "" /v > "%~1.temp"
+	set size1=%~z1
+	rem add 2 in case the file doesn't have a trailing newline, since find will add it
+	set /a size1plus2=%size1%+2
+	call :setsize2 "%~1.temp%"
+	
+	for /f %%a in ('c:\Windows\System32\findstr /R /N "^" "%~1" ^| c:\Windows\System32\find /C ":"') do set lines=%%a
+	del "%~1.temp"
+	
+	if %size1plus2% equ %size2% (
+		if %lines% equ 2 (
+			rem echo File uses LF line endings!
+		) else (
+			rem echo File uses CRLF or has no line endings!
+			if defined "%~2" (endlocal & set "%2=true")
+			(call )
+			goto:eof
+		)
+	) else (
+		if %size1% lss %size2% (
+			rem echo File uses LF line endings!
+		) else (
+			rem echo File uses CR+LF line endings!
+			if defined "%~2" (endlocal & set "%2=true")
+			(call )
+			goto:eof
+		)
+	)
+	
+	if defined "%~2" (endlocal & set "%2=false")
+	(call)
+	endlocal
+goto:eof
+
+:setsize2
+	set size2=%~z1
+goto:eof
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+	call dk_debugFunc 0
+	
+	%dk_call% dk_isCRLF DK.cmd && %dk_call% dk_echo "DK.cmd is CRLF" || %dk_call% dk_echo "DK.cmd is NOT CRLF"
+	
+	%dk_call% dk_validate DKBASH_FUNCTIONS_DIR "%dk_call% dk_validateBranch"
+	%dk_call% dk_isCRLF %DKBASH_FUNCTIONS_DIR%\DK.sh && %dk_call% dk_echo "DK.sh is CRLF" || %dk_call% dk_echo "DK.sh is NOT CRLF"
+goto:eof
