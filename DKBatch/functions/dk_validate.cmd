ab4a815f711b48d5b6ed5bc02601ac92fbf78065
aquawicket <aquawicket>
2024-08-12 15:53:01 -0400

    git commit

--- DKBatch/functions/dk_validate.cmd
+++ DKBatch/functions/dk_validate.cmd
@@ -11,13 +11,13 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 
 	if defined %~1 if "%~1" neq "" (goto:eof)     &:: if the variable is already valid, return
 	
-	call dk_stringContains "%~2" "call" || call dk_error "dk_validate parameter 2 requires the use of call"
+	%dk_call% dk_stringContains "%~2" "call" || %dk_call% dk_error "dk_validate parameter 2 requires the use of call"
 	::call %~2
 	%~2
 	
 	if not defined %~1 dk_error "dk_validate was unable to set the variable with the code provided"
 	if "%~1"=="" dk_error "dk_validate called the code requested, but the variable is still invalid"
-	call dk_printVar "%~1"
+	%dk_call% dk_printVar "%~1"
 goto:eof
 
 
@@ -29,8 +29,8 @@ goto:eof
 :DKTEST
 	call dk_debugFunc 0
 	
-    call dk_validate DIGITALKNOB_DIR "call dk_getDKPaths"
-    echo DIGITALKNOB_DIR = %DIGITALKNOB_DIR%
-    echo DKTOOLS_DIR = %DKTOOLS_DIR%
-    echo DKDOWNLOAD_DIR = %DKDOWNLOAD_DIR%
+    %dk_call% dk_validate DIGITALKNOB_DIR "%dk_call% dk_getDKPaths"
+    %dk_call% dk_echo "DIGITALKNOB_DIR = %DIGITALKNOB_DIR%"
+    %dk_call% dk_echo "DKTOOLS_DIR = %DKTOOLS_DIR%"
+    %dk_call% dk_echo "DKDOWNLOAD_DIR = %DKDOWNLOAD_DIR%"
 goto:eof
