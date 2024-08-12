251475c198a8fcd0621e3c7fcb6fbfd1dc997a7d
aquawicket <aquawicket>
2024-08-12 17:15:51 -0400

    git commit

--- DKBatch/functions/dk_rename.cmd
+++ DKBatch/functions/dk_rename.cmd
@@ -30,7 +30,7 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	
 	:: the base directory of the %to% path must exist.    
 	%dk_call% dk_dirname "%_to_%" _parent_dir_
-	%dk_call% dk_printVar _parent_dir_
+	::%dk_call% dk_printVar _parent_dir_
 	%dk_call% dk_makeDirectory "%_parent_dir_%"
 	
 	::rename "%_from_%" "%_to_%"
