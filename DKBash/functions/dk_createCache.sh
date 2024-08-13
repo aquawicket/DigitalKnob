ee49cc357ea392422ae2ba6c7ab96374e2c1d65d
aquawicket <aquawicket>
2024-08-13 02:44:01 -0400

    git commit

--- DKBash/functions/dk_createCache.sh
+++ DKBash/functions/dk_createCache.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 [ -z "${DKINIT}" ] && . "$(dirname ${0})/DK.sh"
 
-
 ##################################################################################
 # dk_createCache()
 #
@@ -9,21 +8,23 @@
 dk_createCache() {
 	dk_debugFunc 0
 
-	
-	dk_echo "creating cache..."
+	dk_call dk_echo "creating cache..."
 	
 	# write variable values line by line
-	dk_fileWrite "${DKBRANCH_DIR}/cache" "${APP}"
-	dk_fileAppend "${DKBRANCH_DIR}/cache" "${TARGET_OS}"
-	dk_fileAppend "${DKBRANCH_DIR}/cache" "${TYPE}"
-	#dk_fileAppend "${DKBRANCH_DIR}/cache" "${DKENV}"
+	dk_call dk_fileWrite "${DKBRANCH_DIR}/cache" "${APP}"
+	dk_call dk_fileAppend "${DKBRANCH_DIR}/cache" "${TARGET_OS}"
+	dk_call dk_fileAppend "${DKBRANCH_DIR}/cache" "${TYPE}"
+	#dk_call dk_fileAppend "${DKBRANCH_DIR}/cache" "${DKENV}"
 }
 
 
 
 
-DKTEST() { ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+DKTEST() {
 	dk_debugFunc 0
 	
-	dk_createCache
+	dk_call dk_createCache
 }
