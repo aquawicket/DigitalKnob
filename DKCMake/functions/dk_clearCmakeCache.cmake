3b008abebd816cfbcc2d367fb86defb2cb27425f
aquawicket <aquawicket@hotmail.com>
2024-08-15 09:02:03 -0400

    git commit

--- DKCMake/functions/dk_clearCmakeCache.cmake
+++ DKCMake/functions/dk_clearCmakeCache.cmake
@@ -8,7 +8,23 @@ include(${DKCMAKE_FUNCTIONS_DIR}/DK.cmake)
 function(dk_clearCmakeCache)
 	dk_debugFunc(${ARGV})
 
+    dk_info("Deleteing CMake cache files. . .")
+	dk_validate(DIGITALKNOB_DIR "dk_getDKPaths()")
+    dk_cd("${DIGITALKNOB_DIR}")
+	
+	execute_process(COMMAND cmd /c for /r %i in (CMakeCache.*) do del "%i" WORKING_DIRECTORY ${WORKING_DIRECTORY})
+    execute_process(COMMAND cmd /c for /d /r %i in (*CMakeFiles*) do rd /s /q "%i" WORKING_DIRECTORY ${WORKING_DIRECTORY})
 endfunction()
+
+
+
+
+
+
+
+
+
+
 
 
 
--- DKCMake/functions/dk_pickUpdate.cmake
+++ DKCMake/functions/dk_pickUpdate.cmake
@@ -98,7 +98,11 @@ endfunction()
 
 
 
-function(DKTEST) ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+function(DKTEST)
 	dk_debugFunc(${ARGV})
 	
 	dk_pickUpdate()
