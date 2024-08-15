8f6b50838ca511c8d9d80c7e8c16cb61ac8cdd1c
aquawicket <aquawicket@hotmail.com>
2024-08-15 13:22:10 -0400

    git commit

--- DKCMake/functions/dk_exit.cmake
+++ DKCMake/functions/dk_exit.cmake
@@ -2,7 +2,7 @@ include(${DKCMAKE_FUNCTIONS_DIR}/DK.cmake)
 #include_guard()
 
 if(NOT DEFINED PAUSE_ON_EXIT) 
-	set(PAUSE_ON_EXIT 0 CACHE INTERNAL "")
+	set(PAUSE_ON_EXIT 1 CACHE INTERNAL "")
 endif()
 ###############################################################################
 # dk_exit(exit_code)
@@ -12,19 +12,21 @@ endif()
 function(dk_exit) # exit_code)
 	dk_debugFunc(${ARGV})
 	
-	if(NOT ARGV0)
-		set(exit_code 0)        ## default exit code is 0
-	else()
+	if(NOT exit_code)
+		set(exit_code 0 CACHE INTERNAL "")
+	endif()
+	if(${ARGV0} GREATER ${exit_code})
 		set(exit_code ${ARGV0})
 	endif()
 	
-	dk_echo("dk_exit ${exit_code}")
 	if(PAUSE_ON_EXIT EQUAL 1)
-		dk_echo("*** PAUSE_ON_EXIT ***")
+		dk_echo("*** PAUSE_ON_EXIT exit_code:${exit_code} ***")
 		dk_pause()
 	endif()
 	
 	cmake_language(EXIT ${exit_code})  # => 3.29
+	
+	### fallback methods ###
 	if(UNIX)
 		execute_process(COMMAND killall -9 cmake)
 	else()
@@ -42,5 +44,5 @@ function(DKTEST)
 	
 	#dk_exit()
 	#dk_exit(0)
-	dk_exit(123)
+	dk_exit(13)
 endfunction()
