07f572cdb8c0c59ac9df0e48d5d4f76bf07647ba
aquawicket <aquawicket@hotmail.com>
2024-08-15 11:51:49 -0400

    git commit

--- DKBash/functions/dk_pause.sh
+++ DKBash/functions/dk_pause.sh
@@ -3,17 +3,17 @@
 
 
 ##################################################################################
-# dk_pause()
+# dk_pause(pause_msg)
 #
 #	Pause execution and wait for keypress to continue
 #
 dk_pause() {
-	dk_debugFunc 0
+	dk_debugFunc 0 1
 
-	
+	[ -z "${1-}" ] && pause_msg="Press and any to continue..." || pause_msg="${1}"
 	#read -rp 'Press enter to continue...'; # key; echo ${key}
-	dk_echo "Press enter to continue..."; 
-	read -rp '';
+	dk_echo "${pause_msg}"
+	read -rp ''
 }
 
 
@@ -23,4 +23,8 @@ DKTEST() {
 	dk_debugFunc 0
 	
 	dk_pause
+	dk_pause "Pause with a custom message"
+	dk_pause "${red}Pause ${green}with ${blue}colorful ${yellow}message${clr}"
+	dk_echo  "pause with no message"
+	dk_pause " "
 }
