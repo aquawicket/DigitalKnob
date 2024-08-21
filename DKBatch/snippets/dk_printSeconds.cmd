5af5dce58a626f4363d711696dc868b73be94049
aquawicket <aquawicket@hotmail.com>
2024-08-20 21:44:42 -0400

    git commit

--- DKBatch/snippets/dk_printSeconds.cmd
+++ DKBatch/snippets/dk_printSeconds.cmd
@@ -9,29 +9,23 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	call dk_debugFunc 0
 	
 	for /f "tokens=1-3 delims=1234567890 " %%a in ("%time%") do set "delims=%%a%%b%%c" 
-	for /f "tokens=1-4 delims=%delims%" %%A in ("%time%") do (set "hour=%%A" & set "minute=%%B" & set "second=%%C" & set "centisecond=%%D")
+	for /f "tokens=1-4 delims=%delims%" %%A in ("%time%") do (set /a "hour=%%A" & set /a "minute=%%B" & set /a "second=%%C" & set /a "centisecond=%%D")
 	
 	
 	echo %hour%:%minute%:%second%.%centisecond%
-::	setlocal
-::	set /a "cs=100%centisecond%%%100"
-::	set /a "ss=100%second%%%100"
-::	set /a "nn=100%minute%%%100"
-::	set /a "hh=100%hour%%%100"
-::
-::	set /a "dd=dd-1"
-::	set /a "mm=mm-1"
-::	set /a "yy=yy-1970"
-::	
-::	set /a "CentiSeconds_cs=%cs%"
-::	set /a "Seconds_ss=%ss%"
-::	set /a "Minutes_ss=%nn%*60+%Seconds_ss%"
-::	set /a "Hours_ss=%hh%*60*60+%Minutes_ss%"
-::	set /a "Days_ss=%dd%*24*60*60+%Hours_ss%
-::	set /a "Months_ss=(%mm%*304/10)*24*60*60+%Days_ss%"
-::	set /a "Years_ss=(%yy%*12*304/10)*24*60*60+%Months_ss%"
-::	if %Years_ss% gtr 99999999 set /a "Years_ss=Years_ss+2160000"
-::	endlocal & set "%1=%Years_ss%" & set "%2=%CentiSeconds_cs%"
+	
+	set /a "centisecond=100%centisecond%%%100"
+	set /a "second=100%second%%%100"
+	set /a "minute=100%minute%%%100"
+	set /a "hour=100%hour%%%100"
+	echo %hour%:%minute%:%second%.%centisecond%
+	
+	
+	set /a "minutes_in_seconds=%minute%*60+%second%"
+	set /a "hours_in_seconds=%hour%*60*60+%minutes_in_seconds%"
+	set "total_seconds=%hours_in_seconds%"
+
+	echo %total_seconds%.%centisecond%
 goto:eof
 
 
--- DKBatch/snippets/time.cmd
+++ DKBatch/snippets/time.cmd
@@ -1,5 +1,42 @@
 @echo off
 
 for /L %%L in (-2147483648,1,2147483648) do @(
-	echo !time!
-)
+	for /f "tokens=1-4 delims=:." %%A in ("!time!") do (
+		rem set "hour=%%A" & set "minute=%%B" & set "second=%%C" & set "centisecond=%%D"
+		
+		rem   %%D = centiseconds
+		rem   %%C = seconds
+		rem   %%B = minutes
+		rem   %%A = hours
+		
+		
+		rem   Print centiseconds   00 - 59
+		rem echo %%D
+		set "centiseconds=%%D"
+
+
+		rem   Print seconds in centisecond resolution   0000 - 5999
+		set "seconds_in_seconds=%%C"
+		set "seconds_in_centiseconds=!seconds_in_seconds!%%D"
+		
+		
+		rem   Print minutes in centisecond resolution   0000 - ??????? 
+		set "second=%%C"
+		set /a "second=100!second!%%100"
+		set "minute=%%B"
+		set /a "minute=100!minute!%%100"
+		set /a "minutes_in_seconds=!minute!*60+!second!"
+		set /a "minutes_in_centiseconds=!minutes_in_seconds!%%D"
+
+		rem   Print hours in centisecond resolution    0000 - ???????
+		set "hour=%%A"
+		set /a "hour=100!hour!%%100"
+		set /a "hours_in_seconds=!hour!*60*60+!minutes_in_seconds!"
+		set /a "hours_in_centiseconds=!hours_in_seconds!%%D"
+		
+		
+		
+		
+		echo !hours_in_centiseconds!     !minutes_in_centiseconds!     !seconds_in_centiseconds!     !centiseconds!
+	)
+)	
