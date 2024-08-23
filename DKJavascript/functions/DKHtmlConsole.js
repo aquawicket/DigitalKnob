7320f498d498ad03ecb96689e3e1016e50017f78
aquawicket <aquawicket@hotmail.com>
2024-08-22 21:47:11 -0400

    git commit

--- DKJavascript/functions/DKHtmlConsole.js
+++ DKJavascript/functions/DKHtmlConsole.js
@@ -3,7 +3,7 @@
 function DKHtmlConsole() {}
 
 DKHtmlConsole.prototype.create = function DKHtmlConsole_create(top, left, width, height) {
-	var dkhtmlconsole = document.createElement("div");
+	var dkhtmlconsole = window.document.createElement("div");
 	this.dkhtmlconsole = dkhtmlconsole;
 	dkhtmlconsole.setAttribute("id", "DKHtmlConsole");
 	dkhtmlconsole.style.position = "absolute";
@@ -13,7 +13,10 @@ DKHtmlConsole.prototype.create = function DKHtmlConsole_create(top, left, width,
     dkhtmlconsole.style.height = height;
 	dkhtmlconsole.style.color = "black";
 	dkhtmlconsole.style.backgroundColor = "grey";
-	document.body.appendChild(dkhtmlconsole);
+	if(!window){ alert("windows is invalid"); return; }
+	if(!window.document){ alert("document is invalid"); return; }
+	if(!window.document.body){ alert("body is invalid"); return; }
+	window.document.body.appendChild(dkhtmlconsole);
 }
 
 DKHtmlConsole.prototype.log = function DKHtmlConsole_log(message) {
