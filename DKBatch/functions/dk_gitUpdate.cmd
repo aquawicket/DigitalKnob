29af92d9eb78b2aaf66d95a0d33dd7f5962c259c
aquawicket <aquawicket@hotmail.com>
2024-08-26 08:35:18 -0400

    git commit

--- DKBatch/functions/dk_gitUpdate.cmd
+++ DKBatch/functions/dk_gitUpdate.cmd
@@ -2,13 +2,17 @@
 if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 
 ::################################################################################
-::# dk_gitUpdate(NO_CONFIRM)
+::# dk_gitUpdate(url, branch, NO_CONFIRM)
 ::#
 ::#
 :dk_gitUpdate
-	call dk_debugFunc 0
+ setlocal
+	call dk_debugFunc 0 3
 	
-    if "%1" neq "NO_CONFIRM" (
+	if defined "%~1" (set "url=%~1") else (set "url=https://github.com/aquawicket/DigitalKnob.git")
+	if defined "%~2" (set "branch=%~2") else (set "branch=Development")
+	
+    if "%3" neq "NO_CONFIRM" (
         echo Git Update? Any local changes will be lost.
 		%dk_call% dk_confirm || goto:eof
     )
@@ -16,18 +20,19 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	%dk_call% dk_validate DKBRANCH_DIR "%dk_call% dk_validateBranch"
 	%dk_call% dk_validate GIT_EXE "%dk_call% dk_installGit"
 	
-    if NOT exist "%DKBRANCH_DIR%\.git" ("%GIT_EXE%" clone https://github.com/aquawicket/DigitalKnob.git "%DKBRANCH_DIR%")
+    if NOT exist "%DKBRANCH_DIR%\.git" ("%GIT_EXE%" clone %url% "%DKBRANCH_DIR%")
 
     cd "%DKBRANCH_DIR%"
     "%GIT_EXE%" pull --all
     "%GIT_EXE%" checkout -- .
 
-    "%GIT_EXE%" checkout %DKBRANCH%
+    "%GIT_EXE%" checkout %branch%
     if NOT "%ERRORLEVEL%" == "0" (
-        echo Remote has no %DKBRANCH% branch. Creating...
-        "%GIT_EXE%" checkout -b %DKBRANCH% main
-        "%GIT_EXE%" push --set-upstream origin %DKBRANCH%
+        echo Remote has no %branch% branch. Creating...
+        "%GIT_EXE%" checkout -b %branch% main
+        "%GIT_EXE%" push --set-upstream origin %branch%
     )
+ endlocal
 goto:eof
 
 
