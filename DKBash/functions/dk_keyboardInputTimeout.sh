ab8c32a3a1c2571e4e97b3978da41617c229647b
aquawicket <aquawicket@hotmail.com>
2024-08-26 05:46:07 -0400

    git commit

---
+++ DKBash/functions/dk_keyboardInputTimeout.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+[ -z "${DKINIT}" ] && . "$(dirname ${0})/DK.sh"
+
+#################################################################################
+# dk_keyboardInputTimeout(result, default, timeout)
+#
+#
+dk_keyboardInputTimeout() {
+	dk_debugFunc 3
+	
+	default=$2
+	timeout=$3
+	
+	read -t ${timeout} -e input || $(true)
+	input="${input:-${2}}"
+	dk_echo "input = ${input}"
+	eval $1='"${input}"'
+}
+
+
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+DKTEST() {
+	dk_debugFunc 0
+	
+	dk_call dk_echo "Type some input and press enter when done"
+	dk_call dk_keyboardInputTimeout input "default text" 10
+	dk_call dk_echo "you typed '${input}'"
+}
