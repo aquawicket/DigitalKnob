ca7ddb84b3e5c0c160e48d4bc6c4a3d54f5fc400
aquawicket <aquawicket@hotmail.com>
2024-08-27 07:55:34 -0400

    git commit

--- DKBatch/functions/dk_fileToCRLF.cmd
+++ DKBatch/functions/dk_fileToCRLF.cmd
@@ -13,7 +13,8 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	%dk_call% dk_rename "%~1" "%~1_toCRLF" OVERWRITE
 	if not exist "%~1_toCRLF" %dk_call% dk_error "failed to rename %~1"
 	if exist "%~1" %dk_call% dk_error "cannot rename file, destination already exists"
-		
+	
+	%dk_call% dk_info "Converting %~1 to CRLF line endings"
 	type "%~1_toCRLF" | find /V "" > "%~1"
 	if not exist "%~1" %dk_call% dk_error "failed to convert file to CRLF"
 	
