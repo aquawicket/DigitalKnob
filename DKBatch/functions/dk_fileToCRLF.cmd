2e8173d942996ac38a6e8a64873e74215c4cc47f
aquawicket <aquawicket@hotmail.com>
2024-08-27 07:25:48 -0400

    git commit

--- DKBatch/functions/dk_fileToCRLF.cmd
+++ DKBatch/functions/dk_fileToCRLF.cmd
@@ -10,13 +10,15 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd"
 	call dk_debugFunc 1
 	
 	%dk_call% dk_isCRLF "%~1" && %dk_call% dk_echo "%~1 already CRLF" && goto:eof
-	%dk_call% dk_rename "%~1" "%~1_TMP"
-	if not exist "%~1_TMP" %dk_call% dk_error "failed to rename %~1"
+	%dk_call% dk_rename "%~1" "%~1_toCRLF"
+	if not exist "%~1_toCRLF" %dk_call% dk_error "failed to rename %~1"
 	if exist "%~1" %dk_call% dk_error "cannot rename file, destination already exists"
 		
-	type "%~1_TMP" | find /V "" > "%~1"
-	%dk_call% dk_delete "%~1_TMP"
+	type "%~1_toCRLF" | find /V "" > "%~1"
 	if not exist "%~1" %dk_call% dk_error "failed to convert file to CRLF"
+	
+	%dk_call% dk_delete "%~1_toCRLF"
+	if exist "%~1_toCRLF" %dk_call% dk_error "failed to delete temporary file"
 goto:eof
 
 
