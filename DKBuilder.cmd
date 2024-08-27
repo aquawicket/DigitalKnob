af2505b247c546a7c4b8b8180dd50ccb0a7f9f5d
aquawicket <aquawicket@hotmail.com>
2024-08-27 07:13:28 -0400

    git commit

--- DKBuilder.cmd
+++ DKBuilder.cmd
@@ -5,6 +5,7 @@
 ::###### DK_INIT ######
 set "DKHTTP_DK_CMD=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBatch/functions/DK.cmd"
 set "DKBATCH_FUNCTIONS_DIR=%CD%\DKBatch\functions"
+set "DKBATCH_FUNCTIONS_DIR_=%DKBATCH_FUNCTIONS_DIR%\"
 set "DK_CMD=%DKBATCH_FUNCTIONS_DIR%\DK.cmd"
 if not exist "%DKBATCH_FUNCTIONS_DIR%" mkdir "%DKBATCH_FUNCTIONS_DIR%"
 if not exist "%DK_CMD%" powershell /? 1>nul && powershell -command "(New-Object Net.WebClient).DownloadFile('%DKHTTP_DK_CMD%', '%DK_CMD%')"
