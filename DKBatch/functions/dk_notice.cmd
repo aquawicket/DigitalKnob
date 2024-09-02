658ac06e30b0bb5936194384d05b52cdb22addd8
aquawicket <aquawicket@hotmail.com>
2024-09-02 05:49:43 -0400

    git commit

---
+++ DKBatch/functions/dk_notice.cmd
@@ -0,0 +1,28 @@
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
+
+::call dk_source dk_log
+::################################################################################
+::# dk_notice(message)
+::#
+::#	   Print a notice message to the console
+::#
+::#	   @message	- The message to print
+::#
+:dk_notice
+ setlocal
+	call dk_debugFunc 1
+	
+	%dk_call% dk_log NOTICE "%~1"
+goto:eof
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+ setlocal
+	call dk_debugFunc 0
+	
+	%dk_call% dk_notice "test dk_notice message"
+goto:eof
