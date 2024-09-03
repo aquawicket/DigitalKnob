52cf7bb9e8131129877cb3d20ab03fe01acf1ba6
aquawicket <aquawicket@hotmail.com>
2024-09-03 15:09:51 -0400

    git commit

--- 3rdParty/_DKIMPORTS/python3/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/python3/DKMAKE.cmake
@@ -5,21 +5,22 @@ dk_validate(HOST_TRIPLE "dk_getHostTriple()")
 # https://wiki.termux.com/wiki/Python
 
 
-WIN_HOST_dk_set	(PYTHON_DL https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-win32.zip)
-MAC_HOST_dk_set (PYTHON_DL ttps://www.python.org/ftp/python/3.11.8/python-3.11.8-macos11.pkg)
-dk_assert(PYTHON_DL)
+WIN_HOST_dk_set	(PYTHON3_DL https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-win32.zip)
+MAC_HOST_dk_set (PYTHON3_DL ttps://www.python.org/ftp/python/3.11.8/python-3.11.8-macos11.pkg)
+dk_assert(PYTHON3_DL)
 
-dk_findProgram(PYTHON3_EXE python "${PYTHON3}")
 
 if(NOT EXISTS ${PYTHON3_EXE})
 	ANDROID_HOST_dk_command(pkg install python -y)
 	LINUX_HOST_dk_command(apt-get install python3)
-	MAC_HOST_dk_import(${PYTHON_DL})
-	WIN_HOST_dk_import(${PYTHON_DL})
+	MAC_HOST_dk_import(${PYTHON3_DL})
+	WIN_HOST_dk_import(${PYTHON3_DL})
 	
 
 	dk_findProgram(PYTHON3_EXE python "${PYTHON3}")
 endif()
 
 
-
+if(NOT EXISTS ${PYTHON3_EXE})
+	dk_error("Could not find PYTHON3_EXE:${PYTHON3_EXE}")
+endif()
