0b8257524d4858a0edceca4dfed2149a9bbf4fc8
aquawicket <aquawicket@hotmail.com>
2024-09-05 16:16:19 -0400

    git commit

--- 3rdParty/_DKIMPORTS/gcc/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/gcc/DKMAKE.cmake
@@ -8,8 +8,9 @@ if(DEFINED ENV{MSYSTEM})
 endif()
 
 
-dk_validate(HOST_TRIPLE "dk_getHostTriple()")
-dk_validate(TARGET_TRIPLE "dk_getTargetTriple()")
+dk_validate(HOST_TRIPLE    "dk_getHostTriple()")
+dk_validate(TARGET_TRIPLE  "dk_getTargetTriple()")
+dk_validate(DKDOWNLOAD_DIR "dk_setDKDOWNLOAD_DIR()")
 
 dk_depend(msys2)
 dk_depend(pacman)
