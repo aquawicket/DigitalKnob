5a8984033dc3baa9f1efe20dfb9c1b8b7825a8b3
aquawicket <aquawicket@hotmail.com>
2024-09-05 15:38:09 -0400

    git commit

--- 3rdParty/_DKIMPORTS/clang/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/clang/DKMAKE.cmake
@@ -19,26 +19,32 @@ endif()
 		dk_command(${PACMAN_EXE} -S mingw-w64-clang-i686-clang --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})		# CLANG32
 		dk_set(CMAKE_C_COMPILER   ${MSYS2}/clang32/bin/clang.exe)
 		dk_set(CMAKE_CXX_COMPILER ${MSYS2}/clang32/bin/clang++.exe)
+		dk_set(CMAKE_RC_COMPILER  ${MSYS2}/clang32/bin/windres.exe)
 	elseif(win_x86_64_clang)
 		dk_command(${PACMAN_EXE} -S mingw-w64-clang-x86_64-clang --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})		# CLANG64
 		dk_set(CMAKE_C_COMPILER   ${MSYS2}/clang64/bin/clang.exe)
 		dk_set(CMAKE_CXX_COMPILER ${MSYS2}/clang64/bin/clang++.exe)
+		dk_set(CMAKE_RC_COMPILER  ${MSYS2}/clang64/bin/windres.exe)
 	elseif(win_arm64_clang)
 		dk_command(${PACMAN_EXE} -S mingw-w64-clang-aarch64-clang --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})	# CLANGARM64
 		dk_set(CMAKE_C_COMPILER   ${MSYS2}/clangarm64/bin/clang.exe)
 		dk_set(CMAKE_CXX_COMPILER ${MSYS2}/clangarm64/bin/clang++.exe)
+		dk_set(CMAKE_RC_COMPILER  ${MSYS2}/clangarm64/bin/windres.exe)
 	elseif(win_x86_mingw)
 		dk_command(${PACMAN_EXE} -S mingw-w64-i686-clang --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})				# MINGW32
 		dk_set(CMAKE_C_COMPILER   ${MSYS2}/mingw32/bin/clang.exe)
 		dk_set(CMAKE_CXX_COMPILER ${MSYS2}/mingw32/bin/clang++.exe)
+		dk_set(CMAKE_RC_COMPILER  ${MSYS2}/mingw32/bin/windres.exe)
 	elseif(win_x86_64_mingw)
 		dk_command(${PACMAN_EXE} -S mingw-w64-x86_64-clang --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})			# MINGW64
 		dk_set(CMAKE_C_COMPILER   ${MSYS2}/mingw64/bin/clang.exe)
 		dk_set(CMAKE_CXX_COMPILER ${MSYS2}/mingw64/bin/clang++.exe)
+		dk_set(CMAKE_RC_COMPILER  ${MSYS2}/mingw64/bin/windres.exe)
 	elseif(win_x86_64_ucrt)
 		dk_command(${PACMAN_EXE} -S mingw-w64-ucrt-x86_64-clang --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})		# UCRT64
 		dk_set(CMAKE_C_COMPILER   ${MSYS2}/ucrt64/bin/clang.exe)
 		dk_set(CMAKE_CXX_COMPILER ${MSYS2}/ucrt64/bin/clang++.exe)
+		dk_set(CMAKE_RC_COMPILER  ${MSYS2}/ucrt64/bin/windres.exe)
 	endif()
 #endif()
 
