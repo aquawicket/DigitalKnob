a2e8c4bf879fa312d2e2458b102ab4060576a4e0
aquawicket <aquawicket@hotmail.com>
2024-09-09 04:09:53 -0400

    git commit

--- DKBatch/functions/dk_gitCreatePatch.cmd
+++ DKBatch/functions/dk_gitCreatePatch.cmd
@@ -7,14 +7,13 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
 ::#
 :dk_gitCreatePatch
  setlocal
-    call dk_debugFunc 0
+    call dk_debugFunc 3
         
     %dk_call% dk_validate DKBRANCH_DIR "%dk_call% dk_validateBranch"
     cd %DKBRANCH_DIR%
     
     %dk_call% dk_validate GIT_EXE "%dk_call% dk_installGit"
 
-    ::::"%GIT_EXE%" --no-pager diff --compact-summary
 	"%GIT_EXE%"  diff --no-index --patch --output="%~3" "%~1" "%~2"
 %endfunction%
 
@@ -30,7 +29,8 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
     call dk_debugFunc 0
     
 	%dk_call% dk_selectFile fileA
-	%dk_call% dk_selectFile fileB	
+	%dk_call% dk_selectFile fileB
+	%dk_call% dk_saveFile patchFile
 	
-    %dk_call% dk_gitCreatePatch "%fileA%" "%fileB%" "new.patch"
+    %dk_call% dk_gitCreatePatch "%fileA%" "%fileB%" "%patchFile%"
 %endfunction%
---
+++ DKBatch/functions/dk_saveFile.cmd
@@ -0,0 +1,44 @@
+<# : chooser.bat
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
+
+::################################################################################
+::# dk_saveFile(rtn_var)
+::#
+:dk_saveFile
+ setlocal
+    call dk_debugFunc 1
+    
+    %dk_call% dk_validate POWERSHELL_EXE "%dk_call% dk_powershell"
+    for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+        endlocal & set %1=%%~I
+    )
+%endfunction%
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+ setlocal
+    call dk_debugFunc 0
+    
+    %dk_call% dk_saveFile myFile
+    %dk_call% dk_echo "myFile = %myFile%"
+%endfunction%
+
+
+
+
+
+
+: end begin PowerShell hybrid chimera #>
+Add-Type -AssemblyName System.Windows.Forms
+$f = new-object Windows.Forms.SaveFileDialog
+$f.InitialDirectory = pwd
+#$f.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+$f.ShowHelp = $true
+$f.Multiselect = $true
+[void]$f.ShowDialog()
+if ($f.Multiselect) { $f.FileNames } else { $f.FileName }
