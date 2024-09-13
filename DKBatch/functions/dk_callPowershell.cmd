16d3a8bd252e411cc6becc8232d6a9616b0b3286
aquawicket <aquawicket@hotmail.com>
2024-09-13 01:39:59 -0400

    git commit

--- DKBatch/functions/dk_callPowershell.cmd
+++ DKBatch/functions/dk_callPowershell.cmd
@@ -14,9 +14,7 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
 	:: https://stackoverflow.com/a/4732316/688352
     call %ComSpec% /c powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 	
-	for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
-	echo ALL_BUT_FIRST = %ALL_BUT_FIRST%
-	
+	for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b	
 	echo 'powershell . %DKPOWERSHELL_FUNCTIONS_DIR%\%~1.ps1^; %~1 %ALL_BUT_FIRST%'
 	for /f "delims=" %%Z in ('powershell . %DKPOWERSHELL_FUNCTIONS_DIR%\%~1.ps1^; %~1 %ALL_BUT_FIRST%') do (
 		echo %%Z
--- DKBatch/functions/dk_pinToQuickAccess.cmd
+++ DKBatch/functions/dk_pinToQuickAccess.cmd
@@ -7,13 +7,13 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
 ::#
 :dk_pinToQuickAccess
     call dk_debugFunc 1
-  setlocal   
-    :: METHOD 1:  call the dk_callPowershell function (FIXME: ERROR: runs twice)
-    %dk_call% dk_callPowershell dk_pinToQuickAccess "%~1"
-    
-    :: METHOD 2:  use dk_powershell
+  setlocal
+  
+	:: METHOD 1:  use dk_powershell
 	::%dk_call" dk_powershell "if(-not ($(New-Object -ComObject:Shell.Application).Namespace('shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}').Items() | ? {$_.Path -eq '%~1'})){$(New-Object -ComObject:Shell.Application).Namespace('%~1').Self.InvokeVerb('pintohome')}"
 
+    :: METHOD 2:  call the dk_callPowershell function
+    %dk_call% dk_callPowershell dk_pinToQuickAccess "%~1"
 %endfunction%
 
 
