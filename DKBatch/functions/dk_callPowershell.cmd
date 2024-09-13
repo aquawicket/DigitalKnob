51270da513ebb14c00d110c1534728b3b4831831
aquawicket <aquawicket@hotmail.com>
2024-09-13 02:08:53 -0400

    git commit

--- DKBatch/functions/dk_callPowershell.cmd
+++ DKBatch/functions/dk_callPowershell.cmd
@@ -4,6 +4,7 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
 ::####################################################################
 ::# dk_callPowershell(function, arguments..., return_args...)
 ::#
+::#   Reference: https://stackoverflow.com/questions/34451444/how-to-get-a-returned-value-from-powershell-and-get-it-in-a-batch-file
 ::#
 :dk_callPowershell
     call dk_debugFunc 1 99
@@ -14,13 +15,20 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
 	:: https://stackoverflow.com/a/4732316/688352
     call %ComSpec% /c powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 	
-	for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b	
-	echo 'powershell . %DKPOWERSHELL_FUNCTIONS_DIR%\%~1.ps1^; %~1 %ALL_BUT_FIRST%'
-	for /f "delims=" %%Z in ('powershell . %DKPOWERSHELL_FUNCTIONS_DIR%\%~1.ps1^; %~1 %ALL_BUT_FIRST%') do (
+	:: get last argument
+	for %%a in (%*) do set LAST_ARG=%%a
+	
+	:: get all but fisrt argument
+	for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST_ARGS=%%b	
+	echo 'powershell . %DKPOWERSHELL_FUNCTIONS_DIR%\%~1.ps1^; %~1 %ALL_BUT_FIRST_ARGS%'
+	
+	::for /f "delims=" %%Z in ('powershell $global:DKINIT^=1^; . %DKPOWERSHELL_FUNCTIONS_DIR%\%~1.ps1^; %~1 %ALL_BUT_FIRST_ARGS%') do (
+	for /f "delims=" %%Z in ('powershell . %DKPOWERSHELL_FUNCTIONS_DIR%\%~1.ps1^; %~1 %ALL_BUT_FIRST_ARGS%') do (
 		echo %%Z
 		set "rtn_value=%%Z"
 	)
-	echo "rtn_value = %rtn_value%"
+	
+	if "%LAST_ARG%" == "rtn_var" endlocal & set "%LAST_ARG%=%rtn_value%"
 %endfunction%
 
 
@@ -31,7 +39,8 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %0
     call dk_debugFunc 0
  setlocal
  
-	%dk_call% dk_callPowershell dk_test "parameter1" "parameter2"
+	%dk_call% dk_callPowershell dk_test "parameter1" "parameter2" rtn_var
+	echo rtn_var = %rtn_var%
 	::%dk_call% dk_callPowershell dk_confirm
     ::%dk_call% dk_callPowershell dk_debug "string from DKBatch"
 
