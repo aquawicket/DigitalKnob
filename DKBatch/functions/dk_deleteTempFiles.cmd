353bc36d95ecb7bcf22f3d41a3de36bef5b22e02
aquawicket <aquawicket@hotmail.com>
2024-09-15 16:41:18 -0400

    git commit

--- DKBatch/functions/dk_deleteTempFiles.cmd
+++ DKBatch/functions/dk_deleteTempFiles.cmd
@@ -10,14 +10,10 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
  setlocal
  
     %dk_call% dk_info "Deleteing .tmp files . . ."
-	%dk_call% dk_fixme "FIXME: Deleting .tmp files will also delete .tmpl files"
-	%return%
-	
     %dk_call% dk_validate DIGITALKNOB_DIR "%dk_call% dk_getDKPaths"
     cd "%DIGITALKNOB_DIR%"
-    for /r %%i in (*.tmp) do del "%%i"
-    for /r %%i in (*.TMP) do del "%%i"
-    ::%dk_call% dk_checkError
+	for /r %%Z in (*.tmp) do @if %%~xZ==.tmp del "%%Z"
+	for /r %%Z in (*.TMP) do @if %%~xZ==.TMP del "%%Z"
 %endfunction%
 
 
