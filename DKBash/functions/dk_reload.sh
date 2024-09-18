8c789165f1a48106c1f0f59b83e2d2730c951bd7
aquawicket <aquawicket@hotmail.com>
2024-09-18 18:38:47 -0400

    git commit

--- DKBash/functions/dk_reload.sh
+++ DKBash/functions/dk_reload.sh
@@ -13,24 +13,25 @@ dk_reload() {
 	dk_call dk_clearScreen
 	dk_call dk_info "reloading ${DKSCRIPT_PATH}. . ."
 	
-	
-	###### METHOD 1 ######
-	dk_call dk_fileWrite "${DKBASH_DIR}/reload" "${DKSCRIPT_PATH}"
-	dk_call dk_exit 0
-	
-	
+
 	###### METHOD 2 ######
 	#FIXME: need to unset any and all include guards here.
 	#       Or better yet, we need to clear the entire environment
 	dk_call dk_unset DKINIT
-	
 	if [ -n "$(command -v bash)" ]; then
-		echo "exec /bin/bash ${DKSCRIPT_PATH}"
+		#echo "exec /bin/bash ${DKSCRIPT_PATH}"
+		clear
 		exec /bin/bash "${DKSCRIPT_PATH}"
 	else
-		echo "exec ${DKSCRIPT_PATH}"
+		#echo "exec ${DKSCRIPT_PATH}"
+		clear
 		exec "${DKSCRIPT_PATH}"
 	fi
+	
+	
+	###### METHOD 1 ######
+	dk_call dk_fileWrite "${DKBASH_DIR}/reload" "${DKSCRIPT_PATH}"
+	dk_call dk_exit 0
 }
 
 
@@ -42,6 +43,6 @@ dk_reload() {
 DKTEST() {
 	dk_debugFunc 0
 	
-	dk_call dk_pause "Press any key to test dk_reload"
+	dk_call dk_pause "Press Enter key to test dk_reload"
 	dk_call dk_reload
 }
