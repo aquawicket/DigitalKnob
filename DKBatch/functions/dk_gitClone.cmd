6690a7f5ccc2bf8d718fc9ad29a12c67b74fd7fb
aquawicket <aquawicket@hotmail.com>
2024-09-20 07:54:46 -0400

    git commit

--- DKBatch/functions/dk_gitClone.cmd
+++ DKBatch/functions/dk_gitClone.cmd
@@ -10,8 +10,6 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
  setlocal
     
 	if exist "%~2\.git" %dk_call% dk_error "%~2\.git repository already exists" && %return%
-	::%dk_call% dk_isEmptyDirectory "%~2" || %dk_call% dk_rename "%~2" "%~2_BACKUP"
-	::if not exist "%~2_BACKUP" %dk_call% dk_fatal "dk_rename failed" && %return%
 	%dk_call% dk_isEmptyDirectory "%~2" || %dk_call% dk_copy "%~2" "%~2_BACKUP"
 	if not exist "%~2_BACKUP" %dk_call% dk_fatal "dk_copy failed" && %return%
 	%dk_call% dk_set DKBATCH_FUNCTIONS_DIR "%~2_BACKUP\DKBatch\functions"
@@ -20,20 +18,12 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 	
 	cd %DKTEMP_DIR%
 	%dk_call% dk_fileWrite %DKTEMP_DIR%\test.cmd "@echo off"
-	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "ping 127.0.0.1 -n 3 >null"
-	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "cd %DKTEMP_DIR%"
-	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "ping 127.0.0.1 -n 3 >null"
-	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "start rmdir %~2 /s /q"
-	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "ping 127.0.0.1 -n 3 >null"
-	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "del /F /Q %~2"
-	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "ping 127.0.0.1 -n 3 >null"
-	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "rd /S /Q %~2"
+	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "rmdir %~2 /s /q"
 	%dk_call% dk_fileAppend %DKTEMP_DIR%\test.cmd "%GIT_EXE% clone %~1 %~2"
 	
 	(goto) 2>nul & (
+	cd %DKTEMP_DIR%
 	start %DKTEMP_DIR%\test.cmd & exit
-	exit
-	exit
 	)
 %endfunction%
 
