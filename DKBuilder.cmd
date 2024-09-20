8a4805c3ae74720b5e3f8fa9bbd58b89ab5beb39
aquawicket <aquawicket@hotmail.com>
2024-09-20 19:23:46 -0400

    git commit

--- DKBuilder.cmd
+++ DKBuilder.cmd
@@ -1,5 +1,5 @@
-@echo off&setlocal enableDelayedExpansion&set "HDK=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBatch/functions/DK.cmd"&set "DKF=%HOMEDRIVE%%HOMEPATH%\digitalknob\Development\DKBatch\functions"&set "DK=!DKF!\DK.cmd"&if not exist "!DKF!" set "DKF=%CD%\DK"&mkdir "!DKF!"&set "DK=!DKF!\DK.cmd"&powershell -command "(New-Object Net.WebClient).DownloadFile('!HDK!','!DK!')" >nul 2>&1 || certutil -urlcache -split -f "!HDK!" "!DK!" >nul 2>&1 || curl -f "!HDK!" -o "!DK!" >nul 2>&1 || echo [31m DKINIT Failed :( [0m
-endlocal & set "DK=%DK%"
+@echo off&setlocal enableDelayedExpansion&set "HDK=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBatch/functions/DK.cmd"&set "DKF=%HOMEDRIVE%%HOMEPATH%\digitalknob\Development\DKBatch\functions"&set "DK=!DKF!\DK.cmd"&if not exist "!DKF!" set "DKF=%CD%\DK\functions"&mkdir "!DKF!"&set "DK=!DKF!\DK.cmd"&powershell -command "(New-Object Net.WebClient).DownloadFile('!HDK!','!DK!')" >nul 2>&1 || certutil -urlcache -split -f "!HDK!" "!DK!" >nul 2>&1 || curl -f "!HDK!" -o "!DK!" >nul 2>&1 || echo [31m DKINIT Failed :( [0m
+endlocal & set "DK=%DK%" & set "DKF=%DKF%"
 call "%DK%" %~0 %*
 
 ::####################
