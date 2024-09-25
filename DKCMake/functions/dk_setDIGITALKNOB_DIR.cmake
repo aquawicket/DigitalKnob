6e3d79fefcc81bc1b297f61c68039e64ef47c0e8
aquawicket <aquawicket@hotmail.com>
2024-09-25 05:52:56 -0700

    git commit Wed 09/25/2024

---
+++ DKCMake/functions/dk_setDIGITALKNOB_DIR.cmake
@@ -0,0 +1,37 @@
+include(${DKCMAKE_FUNCTIONS_DIR}/DK.cmake)
+#include_guard()
+
+####################################################################
+# dk_setDIGITALKNOB_DIR()
+#
+#
+function(dk_setDIGITALKNOB_DIR)
+    dk_debugFunc("\${ARGV}")
+
+    if(DEFINED DIGITALKNOB_DIR)
+		return()
+	endif()
+            
+    dk_setDKHOME_DIR()
+    #if(NOT DEFINED DIGITALKNOB set "DIGITALKNOB=D i g i t a l K n o b")
+    if(NOT DEFINED DIGITALKNOB)
+		set(DIGITALKNOB "digitalknob")
+	endif()
+    set(DIGITALKNOB_DIR "${DKHOME_DIR}\${DIGITALKNOB}")
+    if(NOT EXISTS "${DIGITALKNOB_DIR}")
+		dk_makeDirectory("${DIGITALKNOB_DIR}")
+	endif()
+endfunction()
+
+
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+function(DKTEST)
+    dk_debugFunc("\${ARGV}")
+   
+	dk_setDIGITALKNOB_DIR
+    dk_printVar(DIGITALKNOB_DIR)
+endfunction()
---
+++ DKCMake/functions/dk_setDKHOME_DIR.cmake
@@ -0,0 +1,35 @@
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+::####################################################################
+::# dk_setDKHOME_DIR()
+::#
+::#
+:dk_setDKHOME_DIR
+    call dk_debugFunc 0
+:: setlocal
+
+    if defined DKHOME_DIR %return%
+    
+    if not defined HOMEDRIVE %dk_call% dk_error "HOMEDRIVE is invalid"
+    if not defined HOMEPATH %dk_call% dk_error "HOMEPATH is invalid"
+    set "DKHOME_DIR=%HOMEDRIVE%%HOMEPATH%"
+    if not exist %DKHOME_DIR% %dk_call% dk_error "DKHOME_DIR:%DKHOME_DIR% does not exist"
+%endfunction%
+
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+    call dk_debugFunc 0
+ setlocal
+ 
+    %dk_call% dk_setDKHOME_DIR
+    %dk_call% dk_printVar DKHOME_DIR
+    
+    %dk_call% dk_setDKHOME_DIR
+    %dk_call% dk_printVar DKHOME_DIR
+%endfunction%
