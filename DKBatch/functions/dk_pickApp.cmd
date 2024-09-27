dfabdcd45d479d867e0e97ea19e4dca33feb9606
aquawicket <aquawicket@hotmail.com>
2024-09-27 04:15:21 -0700

    git commit Fri 09/27/2024

--- DKBatch/functions/dk_pickApp.cmd
+++ DKBatch/functions/dk_pickApp.cmd
@@ -7,7 +7,7 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 ::#
 :dk_pickApp APP
     call dk_debugFunc 0 1
- setlocal 
+ ::setlocal 
     
     %dk_call% dk_setTitle DigitalKnob - %APP% %TARGET_OS% %TYPE%
     
