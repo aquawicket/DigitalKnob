5d4dd4cf03a6a33b0005681e5aa5ec226ca2d7aa
aquawicket <aquawicket@hotmail.com>
2024-10-01 12:58:10 -0400

    git commit Tue 10/01/2024

--- DKBatch/functions/dk_getExtension.cmd
+++ DKBatch/functions/dk_getExtension.cmd
@@ -8,12 +8,27 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 :dk_getExtension
     call dk_debugFunc 2
  setlocal
- 
-    set "_input_=%1"
-    set "_input_=%_input_:"=%"
+
+	:: array of acceptable multi dot extensions
+	set "ext[1]=7z.exe"
+	set "ext[2]=tar.gz"
+	set "ext[3]=tar.xz"
+	
+	set "_input_=%~nx1"
+	set "_input_=%_input_:"=%"
     if [%_input_:~-1%] == [\] set "_input_=%_input_:~0,-1%"
     if [%_input_:~-1%] == [/] set "_input_=%_input_:~0,-1%"
-    for %%Z in ("%_input_%") do set "_extension_=%%~xZ"
+	
+	for /L %%i in (1,1,10) do (
+		set "_input_=!_input_:*.=!"
+		for /L %%i in (1,1,3) do if /I "!_input_!" equ "!ext[%%i]!" (
+			set "_extension_=.!ext[%%i]!"
+			goto:endloop
+		)
+	)
+	:endloop
+	:: If a multi-dot extension was not found, just return the single dot extension
+	if not defined _extension_ set "_extension_=.!_input_!"
     endlocal & set "%2=%_extension_%"
 %endfunction%
 
@@ -28,7 +43,28 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
     call dk_debugFunc 0
  setlocal
  
-    %dk_call% dk_set myPath "/test/test2/xfile.extension"
+    %dk_call% dk_set myPath "/test/test2/xfile.exten"
     %dk_call% dk_getExtension "%myPath%" extension
+    %dk_call% dk_printVar extension
+	
+	%dk_call% dk_getExtension "test.zip" extension
+    %dk_call% dk_printVar extension
+	
+	%dk_call% dk_getExtension "test.tar.gz" extension
+    %dk_call% dk_printVar extension
+	
+	%dk_call% dk_getExtension "test.tar.xz.tar.gz.tar.xz" extension
+    %dk_call% dk_printVar extension
+	
+	%dk_call% dk_getExtension "test.tar.x.gz" extension
+    %dk_call% dk_printVar extension
+	
+	%dk_call% dk_getExtension "test.tar.xz" extension
+    %dk_call% dk_printVar extension
+	
+	%dk_call% dk_getExtension "test.7z.exe.b" extension
+    %dk_call% dk_printVar extension
+	
+	%dk_call% dk_getExtension "test.7z.exe" extension
     %dk_call% dk_printVar extension
 %endfunction%
