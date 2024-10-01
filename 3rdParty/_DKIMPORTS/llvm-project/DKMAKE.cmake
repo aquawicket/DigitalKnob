e8b6d8b15f724a9f053b0444fa645f92b364f3ed
aquawicket <aquawicket@hotmail.com>
2024-10-01 03:34:57 -0400

    git commit Tue 10/01/2024

---
+++ 3rdParty/_DKIMPORTS/llvm-project/DKMAKE.cmake
@@ -0,0 +1,5 @@
+#!/usr/bin/cmake -P
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+# https://github.com/llvm/llvm-project
+
+dk_import(https://github.com/llvm/llvm-project/archive/d50d9613e33274146d9bccfd09e4e8d7654f7e4a.zip)
---
+++ 3rdParty/_DKIMPORTS/osxcross/DKMAKE.cmake
@@ -0,0 +1,6 @@
+#!/usr/bin/cmake -P
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+# https://github.com/tpoechtrager/osxcross
+
+
+dk_import(https://github.com/tpoechtrager/osxcross/archive/29fe6dd35522073c9df5800f8cd1feb4b9a993a8.zip)
