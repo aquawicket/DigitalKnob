fd4432ca966f4cc7d9233a10414d8ef83095d349
aquawicket <aquawicket@hotmail.com>
2024-10-01 18:30:52 -0400

    git commit Tue 10/01/2024

--- DKBash/functions/dk_getExtension.sh
+++ DKBash/functions/dk_getExtension.sh
@@ -8,9 +8,17 @@
 dk_getExtension() {
 	dk_debugFunc 1 2
 
+	
 	_filename_=$(dk_call dk_basename "${1}")
 	eval "_extension_=${_filename_##*.}"
 	
+	ext[0]="tar.gz"
+	ext[1]="tar.xz"
+	ext[2]="7z.exe"
+	for i in "${ext[@]}"; do
+		[[ "$1" == *"$i" ]] && _extension_="$i"
+	done
+
 	### return value ###
 	dk_call dk_printVar _extension_
 	[ ${#} -gt 1 ] && eval "${2}=${_extension_}" && return  # return value when using rtn_var parameter 
@@ -22,6 +30,28 @@ dk_getExtension() {
 DKTEST() {
 	dk_debugFunc 0
 	
-	dk_call dk_getExtension "TestFile.txt" extension
-	dk_call dk_echo "extension = ${extension}"
+	dk_call dk_set myPath "/test/test2/xfile.exten"
+    dk_call dk_getExtension "${myPath}" extension
+    dk_call dk_printVar extension
+	
+	dk_call dk_getExtension "test.zip" extension
+    dk_call dk_printVar extension
+	
+	dk_call dk_getExtension "test.tar.gz" extension
+    dk_call dk_printVar extension
+	
+	dk_call dk_getExtension "test.tar.xz.tar.gz.tar.xz" extension
+    dk_call dk_printVar extension
+	
+	dk_call dk_getExtension "test.tar.x.gz" extension
+    dk_call dk_printVar extension
+	
+	dk_call dk_getExtension "test.tar.xz" extension
+    dk_call dk_printVar extension
+	
+	dk_call dk_getExtension "test.7z.exe.b" extension
+    dk_call dk_printVar extension
+	
+	dk_call dk_getExtension "test.7z.exe" extension
+    dk_call dk_printVar extension
 }
