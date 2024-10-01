80d4d7cf971935651221a4ebb8696b93f62f25cc
aquawicket <aquawicket@hotmail.com>
2024-10-01 02:35:04 -0400

    git commit Tue 10/01/2024

---
+++ 3rdParty/_DKIMPORTS/filezilla/DKMAKE.cmake
@@ -0,0 +1,6 @@
+#!/usr/bin/cmake -P
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+# https://filezilla-project.org/
+
+
+dk_import("https://download.filezilla-project.org/client/FileZilla_3.67.1_win64_sponsored2-setup.exe")
