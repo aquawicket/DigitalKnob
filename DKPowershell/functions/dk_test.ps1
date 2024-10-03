739e9202961b72c197f39be6d56eb2caede95600
aquawicket <aquawicket@hotmail.com>
2024-10-03 14:23:33 -0400

    git commit Thu 10/03/2024

--- DKPowershell/functions/dk_test.ps1
+++ DKPowershell/functions/dk_test.ps1
@@ -8,28 +8,49 @@ if(!$dk_test){ $dk_test = 1 } else{ return }
 function GLOBAL:dk_test() {
 	dk_debugFunc 0 99
 
-	dk_call dk_echo "DKSCRIPT_PATH = ${DKSCRIPT_PATH}"
-	dk_call dk_echo "DKSCRIPT_DIR  = ${DKSCRIPT_DIR}"
-	dk_call dk_echo "DKSCRIPT_NAME = ${DKSCRIPT_NAME}"
-	dk_call dk_echo "DKSCRIPT_EXT  = ${DKSCRIPT_EXT}"
-	dk_call dk_echo "__FILE__      = $(__FILE__)"
-	dk_call dk_echo "__LINE__      = $(__LINE__)"
-	dk_call dk_echo "__FUNCTION__  = $(__FUNCTION__)"
-	dk_call dk_echo "__ARGC__      = $(__ARGC__)"
-	dk_call dk_echo "__ARGV__      = $(__ARGV__)"
-	dk_call dk_echo "__CALLER__    = $(__CALLER__)"
-	
-	$all_args = $PsBoundParameters.Values + ${args}
-	if($all_args){ dk_call dk_debug "all_args = $all_args"   }
-	if($args)    { dk_call dk_debug "args     = $args"       }
-	if($args[0]) { dk_call dk_debug "args[0]  = $($args[0])" }
-	if($args[1]) { dk_call dk_debug "args[1]  = $($args[1])" }
-	if($args[2]) { dk_call dk_debug "args[2]  = $($args[2])" }
-	if($args[3]) { dk_call dk_debug "args[3]  = $($args[3])" }
-	if($args[4]) { dk_call dk_debug "args[4]  = $($args[4])" }
-	if($args[5]) { dk_call dk_debug "args[5]  = $($args[5])" }
-	
-	return "return value from dk_test.ps1"
+    $all_args = $PsBoundParameters.Values + ${args}
+    
+    dk_call dk_echo "####################### dk_test.ps1 #######################"
+    dk_call dk_echo "                          0 = $0"
+    dk_call dk_echo "                          * = $*"
+    dk_call dk_echo "                   __TIME__ = $(__TIME__)"
+    dk_call dk_echo "                 __CALLER__ = $(__CALLER__)"
+    dk_call dk_echo "                   __FILE__ = $(__FILE__)"
+    dk_call dk_echo "                   __LINE__ = $(__LINE__)"
+    dk_call dk_echo "               __FUNCTION__ = $(__FUNCTION__ 1)"
+    dk_call dk_echo "                   __ARGC__ = $(__ARGC__ 1)"
+    dk_call dk_echo "                   __ARGV__ = $(__ARGV__ 1)"
+	dk_call dk_echo "                   all_args = $all_args"
+	dk_call dk_echo "                       args = $args"
+	dk_call dk_echo "                    args[0] = $($args[0])"
+	dk_call dk_echo "                    args[1] = $($args[1])"
+	dk_call dk_echo "                    args[2] = $($args[2])"
+	dk_call dk_echo "                    args[3] = $($args[3])"
+	dk_call dk_echo "                    args[4] = $($args[4])"
+	dk_call dk_echo "                    args[5] = $($args[5])"
+    dk_call dk_echo "              DKSCRIPT_PATH = ${DKSCRIPT_PATH}"
+    dk_call dk_echo "               DKSCRIPT_DIR = ${DKSCRIPT_DIR}"
+    dk_call dk_echo "              DKSCRIPT_NAME = ${DKSCRIPT_NAME}"
+    dk_call dk_echo "               DKSCRIPT_EXT = ${DKSCRIPT_EXT}"
+    dk_call dk_echo "              DKSCRIPT_ARGS = ${DKSCRIPT_ARGS}"
+    dk_call dk_echo "                 DKHOME_DIR = ${DKHOME_DIR}"
+    dk_call dk_echo "                DKCACHE_DIR = ${DKCACHE_DIR}"
+    dk_call dk_echo "              DKDESKTOP_DIR = ${DKDESKTOP_DIR}"
+    dk_call dk_echo "                DIGITALKNOB = ${DIGITALKNOB}"
+    dk_call dk_echo "            DIGITALKNOB_DIR = ${DIGITALKNOB_DIR}"
+    dk_call dk_echo "             DKDOWNLOAD_DIR = ${DKDOWNLOAD_DIR}"
+    dk_call dk_echo "                DKTOOLD_DIR = ${DKTOOLD_DIR}"
+    dk_call dk_echo "                   DKBRANCH = ${DKBRANCH}"
+    dk_call dk_echo "               DKBRANCH_DIR = ${DKBRANCH_DIR}"
+    dk_call dk_echo "             DK3RDPARTY_DIR = ${DK3RDPARTY_DIR}"
+    dk_call dk_echo "                 DKAPPS_DIR = ${DKAPPS_DIR}"
+    dk_call dk_echo "                 DKBASH_DIR = ${DKPOWERSHELL_DIR}"
+    dk_call dk_echo " DKPOWERSHELL_FUNCTIONS_DIR = ${DKPOWERSHELL_FUNCTIONS_DIR}"
+    dk_call dk_echo "DKPOWERSHELL_FUNCTIONS_DIR_ = ${DKPOWERSHELL_FUNCTIONS_DIR_}"
+    
+    $return_value="return value from dk_test.ps1"
+    
+    return "${return_value}"
 }
 
 
