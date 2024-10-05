887e59cc2b4a8a1d622bc97637dbc5eb77c23d4b
aquawicket <aquawicket@hotmail.com>
2024-10-05 13:04:30 -0400

    git commit Sat 10/05/2024

---
+++ DKBash/functions/dk_validateDK.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+[ -z "${DKINIT}" ] && . "${DKBASH_FUNCTIONS_DIR_}DK.sh"
+
+#####################################################################
+# dk_validateDK()
+#
+#
+dk_validateDK() {
+    dk_debugFunc 0
+	
+	dk_call dk_validate DIGITALKNOB_DIR "dk_call dk_DIGITALKNOB_DIR"
+	[ ! -n "${DKBRANCH}" ] 		&& export DKBRANCH="Development"
+	[ ! -n "${DKBRANCH_DIR}" ] 	&& export DKBRANCH_DIR="${DIGITALKNOB_DIR}\${DKBRANCH}"
+	
+	[ -e "${DKBRANCH_DIR}/.git" ] && [ "${DKSCRIPT_NAME}" neq "DKBuilder" ] && return
+	[ -e "${DKBRANCH_DIR}/.git" ] && [ "${DKSCRIPT_DIR}" = "${DKBRANCH_DIR}" ] && return
+
+    [ ! -e "${DKBRANCH_DIR}\.git" ] && dk_call dk_gitUpdate https://github.com/aquawicket/DigitalKnob.git ${DKBRANCH}
+	
+	[ "${DKSCRIPT_NAME}" = "DKBuilder" ] && export DKSCRIPT_PATH="${DKBRANCH_DIR}/${DKSCRIPT_NAME}/${DKSCRIPT_EXT}"
+	if [ ! -e "${DKSCRIPT_PATH}" ] dk_call dk_fatal "DKSCRIPT_PATH:${DKSCRIPT_PATH} does not exist" && return
+	
+	if "%DKSCRIPT_NAME%" equ "DKBuilder" set "DKSCRIPT_ARGS=%DKSCRIPT_PATH%"
+	for %%Z in ("%DKSCRIPT_PATH%") do set "DKSCRIPT_DIR=%%~dpZ"
+	if [%DKSCRIPT_DIR:~-1%] == [\] set "DKSCRIPT_DIR=%DKSCRIPT_DIR:~0,-1%"
+	if [%DKSCRIPT_DIR:~-1%] == [/] set "DKSCRIPT_DIR=%DKSCRIPT_DIR:~0,-1%"
+	export RELOADED=""
+	
+	#set "DKSCRIPT_DIR=%DKBRANCH_DIR%"
+	#set "DKSCRIPT_NAME="
+	#set "DKSCRIPT_EXT="
+	#set "DKBATCH_DIR="
+	#set "DKBATCH_FUNCTIONS_DIR=%DKBRANCH_DIR%\DKBatch\functions"
+	#set "DKBATCH_FUNCTIONS_DIR_="
+	#set "DKASSETS_DIR="
+	
+	exit 1
+}
+
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+DKTEST() {
+    dk_debugFunc 0
+    
+    dk_call dk_validateDK
+}
