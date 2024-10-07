af974e04eb7d7703f8c4cf9097722f202c0816b4
aquawicket <aquawicket@hotmail.com>
2024-10-07 16:37:52 -0400

    git commit Mon 10/07/2024

--- DKBuilder.cmd
+++ DKBuilder.cmd
@@ -1,15 +1,24 @@
 @echo off
 setlocal enableDelayedExpansion
+
 set "HDK=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBatch/functions/DK.cmd"
+
 set "DKF=%USERPROFILE%\digitalknob\Development\DKBatch\functions"
+
 if not exist "!DKF!" set "DKF=%USERPROFILE%\.dk\DKBatch\functions"
+
 attrib +h %USERPROFILE%\.dk
-::if not exist "!DKF!" set "DKF=%CD%\DKBatch\functions"
+
 set "DK=!DKF!\DK.cmd"
-mkdir "!DKF!" 2>nul&set "DK=!DKF!\DK.cmd" 
+mkdir "!DKF!" 2>nul
+set "DK=!DKF!\DK.cmd" 
+
 if not exist !DK! powershell -c "(New-Object Net.WebClient).DownloadFile('!HDK!','!DK!')" >nul 2>&1||certutil -urlcache -split -f "!HDK!" "!DK!" >nul 2>&1||curl -f "!HDK!" -o "!DK!" >nul 2>&1||echo DKINIT Failed
+
 endlocal & set "DK=%DK%"
 set "DKF=%DKF%"
+
+
 call "%DK%" %~0 %*
 
 ::takeown /F %DKF% /R /D "Y"
