7aa04d45c8edb2c201efd9b3684fe94caaaca2f2
aquawicket <aquawicket@hotmail.com>
2024-10-07 17:41:59 -0400

    git commit

--- DKBuilder.sh
+++ DKBuilder.sh
@@ -6,12 +6,15 @@ export DKF="${HOME}/digitalknob/Development/DKBash/functions"
 [ -e "${HOME}/.dk" ] 		|| mkdir "${HOME}/.dk"
 [ -e "${HOME}/.dk/DKBash" ] || mkdir "${HOME}/.dk/DKBash"
 [ -e "${DKF}" ] 			|| mkdir "${DKF}"
+[ -e "${DKF}" ] 			|| echo "DKF:${DKF} does not exist" || exit 1
 
 export DK="${DKF}/DK.sh"
 [ -e "${DK}" ] || [ -n "$(command -v "wget")" ] && wget -P "${DKF}" "${HDK}"
 [ -e "${DK}" ] || [ -n "$(command -v "curl")" ] && curl -Lo "${DK}" "${HDK}"
-[ -e "${DK}" ] && chmod 777 "${DK}"
-[ -e "${DK}" ] && . "${DK}"
+[ -e "${DK}" ] || echo "DK:${DK} does not exist" || exit 1
+
+chmod 777 "${DK}"
+. "${DK}"
 #"${DK}" $0 $*
 
 #####################
