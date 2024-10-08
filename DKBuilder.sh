3a8ac7a7d4193e79eb63c27c03ed5d69b90d70b4
aquawicket <aquawicket@hotmail.com>
2024-10-08 13:01:50 -0700

    git commit Tue 10/08/2024

--- DKBuilder.sh
+++ DKBuilder.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 export HDK="https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBash/functions/DK.sh"
 
-if (command -v wslpath &>/dev/null); then
-	(command -v sudo) && export SUDO_EXE="sudo" || export SUDO_EXE=" "
+(command -v sudo) && export SUDO_EXE="sudo" || export SUDO_EXE=" "
+
+###### WSL ######
+[ ! -e "${WSLPATH_EXE-}" ]	&& export WSLPATH_EXE=$(command -v "wslpath") || $(true)
+if [ -n "${WSLPATH_EXE-}" ]; then	
 	${SUDO_EXE} rm /etc/resolv.conf
 	${SUDO_EXE} sh -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
 	${SUDO_EXE} sh -c 'echo "[network]" > /etc/wsl.conf'
@@ -15,21 +18,20 @@ fi
 [ ! -e "${CMD_EXE}" ]	&& export CMD_EXE="/mnt/c/Windows/System32/cmd.exe"
 [ ! -e "${CMD_EXE}" ]	&& unset CMD_EXE
 [   -e "${CMD_EXE-}" ]	&& echo "CMD_EXE = ${CMD_EXE}"
-
+	
+######  USERPROFILE -> WSLPATH_EXE -> DKHOME_DIR ######
+[   -e "${WSLPATH_EXE}" ]	&& export USERPROFILE=$(${WSLPATH_EXE} -u $(${CMD_EXE} /c echo "%USERPROFILE%" | tr -d '\r'))
+[ ! -e "${WSLPATH_EXE}" ]	&& unset WSLPATH_EXE
+[   -e "${WSLPATH_EXE-}" ]	&& echo "WSLPATH_EXE = ${WSLPATH_EXE}"
+[   -e "${WSLPATH_EXE-}" ]	&& export DKHOME_DIR=$(wslpath -u $(${CMD_EXE} /c echo "%USERPROFILE%" | tr -d '\r'))
+	
 ######  USERPROFILE -> CYGPATH_EXE -> DKHOME_DIR ######
 [ ! -e "${CYGPATH_EXE-}" ]	&& export CYGPATH_EXE=$(command -v "cygpath") || $(true)
 [   -e "${CYGPATH_EXE}" ]	&& export USERPROFILE=$(${CYGPATH_EXE} -u $(${CMD_EXE} "/c echo %USERPROFILE% | tr -d '\r'"))
 [ ! -e "${CYGPATH_EXE}" ]	&& unset CYGPATH_EXE
 [   -e "${CYGPATH_EXE-}" ]	&& echo "CYGPATH_EXE = ${CYGPATH_EXE}"
 [   -e "${CYGPATH_EXE-}" ]	&& export DKHOME_DIR=$(cygpath -u $(${CMD_EXE} "/c echo %USERPROFILE% | tr -d '\r'"))
-	
-######  USERPROFILE -> WSLPATH_EXE -> DKHOME_DIR ######
-[ ! -e "${WSLPATH_EXE-}" ]	&& export WSLPATH_EXE=$(command -v "wslpath") || $(true)
-[   -e "${WSLPATH_EXE}" ]	&& export USERPROFILE=$(${WSLPATH_EXE} -u $(${CMD_EXE} /c echo "%USERPROFILE%" | tr -d '\r'))
-[ ! -e "${WSLPATH_EXE}" ]	&& unset WSLPATH_EXE
-[   -e "${WSLPATH_EXE-}" ]	&& echo "WSLPATH_EXE = ${WSLPATH_EXE}"
-[   -e "${WSLPATH_EXE-}" ]	&& export DKHOME_DIR=$(wslpath -u $(${CMD_EXE} /c echo "%USERPROFILE%" | tr -d '\r'))
-	
+
 ### HOME -> DKHOME_DIR ###
 [ ! -e "${DKHOME_DIR-}" ] 	&& export DKHOME_DIR="${HOME}"
 [ ! -e "${DKHOME_DIR}" ] 	&& echo "ERROR: DKHOME_DIR not found"
@@ -48,7 +50,6 @@ export DK="${DKF}/DK.sh"
 [ ! -e "${DK}" ] && (command -v wget &>/dev/null) && wget -P "${DKF}" "${HDK}"
 [ ! -e "${DK}" ] && (command -v curl &>/dev/null) && curl -Lo "${DK}" "${HDK}"
 [ ! -e "${DK}" ] && echo "DK:${DK} does not exist" && exit 1
-
 
 ${SUDO_EXE} chmod 777 "${DK}"
 . "${DK}"
