161f88a2eff2c3ab1ec39f65c37eb16c8f23ef3b
aquawicket <aquawicket@hotmail.com>
2024-10-10 01:53:26 -0700

    git commit Thu 10/10/2024

--- DKCMake/functions/dk_installPackage.cmake
+++ DKCMake/functions/dk_installPackage.cmake
@@ -25,6 +25,7 @@ function(dk_installPackage package)
 #
 	execute_process(COMMAND command -v apk OUTPUT_VARIABLE APK_EXE)
 	if(APK_EXE)
+		message("${APK_EXE} add ${package}")
 		execute_process(COMMAND ${APK_EXE} add ${package})				# Alpine Package Keeper (alpine linux)
 	endif()
 	
@@ -38,13 +39,35 @@ function(dk_installPackage package)
 		execute_process(COMMAND ${APT_EXE} -y install ${package})		# Apt (debian)
 	endif()
 	
-#		execute_process(COMMAND brew install ${package})				# Homebrew (MacOS)
-#		execute_process(COMMAND dnf install ${package})					# Dnf (yum)
-#		execute_process(COMMAND emerge ${package})						# Portage
-#		execute_process(COMMAND nix-env -i ${package})					# Nix
-#		execute_process(COMMAND ohpm install ${package})				# Ohpm
-#		execute_process(COMMAND pkg install ${package} -y)				# Termux
-#		execute_process(COMMAND pacman -S ${package} --noconfirm)		# Pacman
+	execute_process(COMMAND command -v brew OUTPUT_VARIABLE BREW_EXE)
+	if(BREW_EXE)
+		execute_process(COMMAND ${BREW_EXE} install ${package})			# Homebrew (MacOS)
+	endif()
+
+	execute_process(COMMAND command -v dnf OUTPUT_VARIABLE DNF_EXE)
+	if(DNF_EXE)
+		execute_process(COMMAND ${DNF_EXE} install ${package})			# Dnf (yum)
+	endif()
+	
+	execute_process(COMMAND command -v emerge OUTPUT_VARIABLE EMERGE_EXE)
+	if(EMERGE_EXE)
+		execute_process(COMMAND ${EMERGE_EXE} ${package})				# Portage
+	endif()
+	
+	execute_process(COMMAND command -v nix-env OUTPUT_VARIABLE NIX_ENV_EXE)
+	if(NIX_ENV_EXE)
+		execute_process(COMMAND ${NIX_ENV_EXE} -i ${package})					# Nix
+	endif()
+	
+	execute_process(COMMAND command -v ohpm OUTPUT_VARIABLE OHPM_EXE)
+	if(OHPM_EXE)
+		execute_process(COMMAND ohpm install ${package})				# Ohpm
+	endif()
+	
+	execute_process(COMMAND command -v pkg OUTPUT_VARIABLE PKG_EXE)
+	if(PKG_EXE)
+		execute_process(COMMAND pkg install ${package} -y)				# Termux
+	endif()
 
 	execute_process(COMMAND command -v pacman OUTPUT_VARIABLE PACMAN_EXE)
 	if(PACMAN_EXE)
@@ -60,17 +83,43 @@ function(dk_installPackage package)
 			execute_process(COMMAND ${PACMAN_EXE} -S mingw-w64-x86_64-${package} --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})			# MINGW64
 		elseif(win_x86_64_ucrt)
 			execute_process(COMMAND ${PACMAN_EXE} -S mingw-w64-ucrt-x86_64-${package} --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})	# UCRT64
+		else()
+			execute_process(COMMAND ${PACMAN_EXE} -S ${package} --needed --noconfirm --cachedir ${DKDOWNLOAD_DIR})							# DEFAULT
 		endif()
 	endif()
 
+	execute_process(COMMAND command -v swupd OUTPUT_VARIABLE SWUPD_EXE)
+	if(SWUPD_EXE)
+		execute_process(COMMAND ${SWUPD_EXE} bundle-add ${package})			# Swupd
+	endif()
+	
+	execute_process(COMMAND command -v tce-load OUTPUT_VARIABLE TCE_LOAD_EXE)
+	if(TCE_LOAD_EXE)
+		execute_process(COMMAND ${TCE_LOAD_EXE} -wil ${package})     			# Tiny core linux
+	endif()
+	
+	execute_process(COMMAND command -v winget OUTPUT_VARIABLE WINGET_EXE)
+	if(WINGET_EXE)
+		execute_process(COMMAND winget install ${package})				# WinGet
+	endif()
+	
+	execute_process(COMMAND command -v xbps-install OUTPUT_VARIABLE WINGET_EXE)
+	if(XBPS_INSTALL_EXE)
+		execute_process(COMMAND xbps-install ${package})				# Xbps
+	endif()
+	
+	execute_process(COMMAND command -v zypper OUTPUT_VARIABLE ZYPPER_EXE)
+	if(ZYPPER_EXE)
+		execute_process(COMMAND zypper in ${package})					# Zypper
+	endif()
+	
+#	dk_error("ERROR: no package managers found")
 
-#		execute_process(COMMAND swupd bundle-add ${package})			# Swupd
-#		execute_process(COMMAND tce-load -wil ${package})     			# Tiny core linux
-#		execute_process(COMMAND winget install ${package})				# WinGet
-#		execute_process(COMMAND xbps-install ${package})				# Xbps
-#		execute_process(COMMAND zypper in ${package})					# Zypper
-
-#		dk_error("ERROR: no package managers found")
+	execute_process(COMMAND command -v ${package} OUTPUT_VARIABLE PACKAGE_EXE)
+	if(EXISTS ${PACKAGE_EXE})
+		message("${package}_EXE installed successfully")
+		return()
+	endif()
 endfunction()
 
 
