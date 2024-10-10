c6d9f6739f8f4f61712f926de4d5deb80c16ef4a
aquawicket <aquawicket@hotmail.com>
2024-10-10 00:27:04 -0700

    git commit Thu 10/10/2024

--- DKCMake/functions/dk_clearCmakeCache.cmake
+++ DKCMake/functions/dk_clearCmakeCache.cmake
@@ -10,9 +10,15 @@ function(dk_clearCmakeCache)
 	dk_debugFunc("\${ARGV}")
 
     dk_info("Deleting CMake cache files. . .")
+	### TODO
+	dk_validate(DKBRANCH_DIR "dk_DKBRANCH_DIR()")
+	file(GLOB_RECURSE cmakecache_files LIST_DIRECTORIES true "${DKBRANCH_DIR}/" "*CMakeCache*")
+	foreach(item ${cmakecache_files})
+		message("${item}")
+	endforeach()
 	
-	execute_process(COMMAND cmd /c for /r %i in (CMakeCache.*) do del "%i" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
-    execute_process(COMMAND cmd /c for /d /r %i in (*CMakeFiles*) do rd /s /q "%i" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
+	#execute_process(COMMAND cmd /c for /r %i in (CMakeCache.*) do del "%i" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
+    #execute_process(COMMAND cmd /c for /d /r %i in (*CMakeFiles*) do rd /s /q "%i" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
 endfunction()
 
 
--- DKCMake/functions/dk_deleteTempFiles.cmake
+++ DKCMake/functions/dk_deleteTempFiles.cmake
@@ -10,17 +10,27 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 function(dk_deleteTempFiles)
 	dk_debugFunc("\${ARGV}")
 	
-	DKASSERT(DIGITALKNOB_DIR)
 	dk_info("Deleting Temporary files . . .")
-	if(WIN_HOST)
-		dk_executeProcess(for /r %%Z in (*.tmp) do @if %%~xZ==.tmp del "%%Z" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
-		dk_executeProcess(for /r %%Z in (*.TMP) do @if %%~xZ==.TMP del "%%Z" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
-	else()
-		dk_executeProcess("rm -rf `find . -name *.tmp`" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
-		dk_executeProcess(find . -name "*.TMP" -delete WORKING_DIRECTORY ${DIGITALKNOB_DIR})
-		dk_executeProcess(find . -name "*.tmp" -delete WORKING_DIRECTORY ${DIGITALKNOB_DIR})
-		
-	endif()
+	dk_validate(DKBRANCH_DIR "dk_DKBRANCH_DIR()")
+	
+	file(GLOB_RECURSE tmp_Files "${DKBRANCH_DIR}" "*.tmp")
+	foreach(file ${tmp_Files})
+		message("${file}")
+	endforeach()
+	
+	file(GLOB_RECURSE TMP_Files "${DKBRANCH_DIR}" "*.TMP")
+	foreach(file ${TMP_Files})
+		message("${file}")
+	endforeach()
+	
+	#if(WIN_HOST)
+	#	dk_executeProcess(for /r %%Z in (*.tmp) do @if %%~xZ==.tmp del "%%Z" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
+	#	dk_executeProcess(for /r %%Z in (*.TMP) do @if %%~xZ==.TMP del "%%Z" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
+	#else()
+	#	dk_executeProcess("rm -rf `find . -name *.tmp`" WORKING_DIRECTORY ${DIGITALKNOB_DIR})
+	#	dk_executeProcess(find . -name "*.TMP" -delete WORKING_DIRECTORY ${DIGITALKNOB_DIR})
+	#	dk_executeProcess(find . -name "*.tmp" -delete WORKING_DIRECTORY ${DIGITALKNOB_DIR})	
+	#endif()
 endfunction()
 
 
