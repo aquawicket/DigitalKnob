df65f752cfa876f4c8760bc961573924f5f0879d
aquawicket <aquawicket@hotmail.com>
2024-10-09 22:04:26 -0700

    git commit Wed 10/09/2024

--- DKBuilder.cmake
+++ DKBuilder.cmake
@@ -4,16 +4,16 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 ###### DKBuilder ######
 
 ###### DKINIT ######
-if(NOT EXISTS DKCMake)
-	file(MAKE_DIRECTORY DKCMake)
-endif()
-if(NOT EXISTS DKCMake/functions)
-	file(MAKE_DIRECTORY DKCMake/functions)
-endif()
-if(NOT EXISTS DKCMake/functions/DK.sh)
-	file(DOWNLOAD "https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKCMake/functions/DK.cmake" "DKCMake/functions/DK.cmake")
-endif()
-include(DKCMake/functions/DK.cmake)
+#if(NOT EXISTS DKCMake)
+#	file(MAKE_DIRECTORY DKCMake)
+#endif()
+#if(NOT EXISTS DKCMake/functions)
+#	file(MAKE_DIRECTORY DKCMake/functions)
+#endif()
+#if(NOT EXISTS DKCMake/functions/DK.sh)
+#	file(DOWNLOAD "https://raw.githubusercontent.com/aquawicket/Digitalknob/Development/DKCMake/functions/DK.cmake" "DKCMake/functions/DK.cmake")
+#endif()
+#include(DKCMake/functions/DK.cmake)
 
 
 ###### Load Main Program ######
