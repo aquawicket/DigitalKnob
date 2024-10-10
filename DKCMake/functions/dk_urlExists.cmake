78169016b14290a6b8a6bda1396aca8ef8bab00c
aquawicket <aquawicket@hotmail.com>
2024-10-10 05:29:44 -0700

    git commit Thu 10/10/2024

--- DKCMake/functions/dk_urlExists.cmake
+++ DKCMake/functions/dk_urlExists.cmake
@@ -8,18 +8,17 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 function(dk_urlExists url rtn_var)
     dk_debugFunc("\${ARGV}")
     
-    if(DISABLE_curl)
-		return()
-	endif()
-    
 	#for /f "usebackq tokens=*" %%f in (`curl -sI -o nul -w "%%{http_code}" "%~1"`) do set "httpCode=%%f"
-	file(DOWNLOAD ${url} STATUS status)
-	list(GET status 0 httpCode)
+	file(DOWNLOAD ${url} STATUS dl_status LOG dl_log)
+	dk_printVar(dl_status)
+	dk_printVar(dl_log)
+	list(GET dl_status 0 httpCode)
 
+	dk_echo("httpCode = ${httpCode}")
     if(${httpCode} EQUAL 0)
-        set(${rtn_var} TRUE PARENT_SCOPE)	
+        set(${rtn_var} 1 PARENT_SCOPE)	
     else()
-		set(${rtn_var} FALSE PARENT_SCOPE)
+		set(${rtn_var} 0 PARENT_SCOPE)
 	endif()
 endfunction()
 
@@ -43,7 +42,7 @@ function(DKTEST)
     
 	
     dk_echo()
-    set(url http://www.nonexisting.com/nofile.no)
+    set(url http://www.nXoXnXeXxXiXsXtXiXnXg.com/nofile.no)
     dk_urlExists(${url} url_exists)
     if(url_exists)
 		dk_echo("${url} exists") 
