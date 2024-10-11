245575ea7021163fd80216ce6e5c94056e8305cb
aquawicket <aquawicket@hotmail.com>
2024-10-11 17:09:42 -0400

    git commit Fri 10/11/2024

--- 3rdParty/_DKIMPORTS/mesa3d/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/mesa3d/DKMAKE.cmake
@@ -4,15 +4,13 @@ dk_load(dk_builder)
 # https://www.mesa3d.org
 
 if(NOT LINUX AND NOT RASPBERRY)
-	dk_undepend(mesa-common-dev)
+	dk_undepend(mesa3d)
 	dk_return()
 endif()
 
 
 ### INSTALL ###
 #dk_cd(/usr)
-#dk_depend(sudo)
-#dk_command(${SUDO_EXE} apt -y install mesa-common-dev)
 if("$ENV{WSL_DISTRO_NAME}" STREQUAL "Alpine")
 	dk_installPackage(mesa-dev)
 else()
--- DKPlugins/DKSDLWindow/DKMAKE.cmake
+++ DKPlugins/DKSDLWindow/DKMAKE.cmake
@@ -3,10 +3,12 @@ dk_depend(sdl)
 dk_depend(DKWindow)
 
 # DKCMake/DKGenerate.cmake will build android with or without GUI per the CMAKE_ANDROID_GUI variable.
-ANDROID_dk_set(CMAKE_ANDROID_GUI 1)
-ANDROID_dk_depend(openjdk)
-ANDROID_dk_depend(openjdk-8u41)
-ANDROID_dk_depend(android-cmdline-tools)
+if(ANDROID)
+	dk_set(CMAKE_ANDROID_GUI 1)
+	dk_depend(openjdk)
+	dk_depend(openjdk-8u41)
+	dk_depend(android-cmdline-tools)
+endif()
 
 # add SDL_main to the top of the DKPlugins.h file for iOS
 if(IOS)
