9b7c1cf37a82d6e6814ed543386a489f5b6face4
aquawicket <aquawicket@hotmail.com>
2024-10-11 20:36:29 -0400

    git commit Fri 10/11/2024

--- 3rdParty/_DKIMPORTS/libstdc++fs/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libstdc++fs/DKMAKE.cmake
@@ -3,5 +3,6 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 dk_load(dk_builder)
 # libstdc++fs
 
-dk_if(LINUX     "dk_lib(libstdc++fs.a)")
-dk_if(RASPBERRY "dk_lib(libstdc++fs.a)")
+if(LINUX OR RASPBERRY)
+	dk_lib(libstdc++fs.a)
+endif()
