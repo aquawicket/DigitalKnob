735734038888f7f4798799cc9d836b44c923c98c
aquawicket <aquawicket@hotmail.com>
2024-10-14 07:23:32 -0400

    git commit

--- DKBash/functions/dk_resetAll.sh
+++ DKBash/functions/dk_resetAll.sh
@@ -8,48 +8,41 @@
 dk_resetAll() {
 	dk_debugFunc 0
 
-	
-	dk_clearScreen
-	dk_echo
-	dk_echo
-	dk_info "Do you want to reset the entire local repository . . . ?"
-	dk_info "This will delete digitalknob, everything will be reset,"
-	dk_info "and the repository will be re-cloned. All libraries and tools"
-	dk_info "will be re downloaded and rebuild from start. Save any changes"
-	dk_info "you wish to commit or save beforehand."
-	dk_echo
+	dk_call dk_clearScreen
+	dk_call dk_echo
+	dk_call dk_echo
+	dk_call dk_info "Do you want to reset the entire local repository . . . ?"
+	dk_call dk_info "This will delete digitalknob, everything will be reset,"
+	dk_call dk_info "and the repository will be re-cloned. All libraries and tools"
+	dk_call dk_info "will be re downloaded and rebuild from start. Save any changes"
+	dk_call dk_info "you wish to commit or save beforehand."
+	dk_call dk_echo
 		
-	dk_confirm || return
+	dk_call dk_confirm || return
 		
-	# make sure script is running from DKBRANCH_DIR
-	if ! [ "${DKSCRIPT_DIR}" = "${DKBRANCH_DIR}" ]; then
-		dk_echo "${yellow}"
-		dk_echo "WARNING: this file isn't running from the branch directory"
-		dk_echo "Is must be in the branch directory to continue."
-		dk_echo "DKSCRIPT_DIR = ${DKSCRIPT_DIR}"
-		dk_echo "${clr}"			
-		dk_printVar DKBRANCH_DIR
-		return $(false);
-	fi
+#	# make sure script is running from DKBRANCH_DIR
+#	if ! [ "${DKSCRIPT_DIR}" = "${DKBRANCH_DIR}" ]; then
+#		dk_call dk_echo "${yellow}"
+#		dk_call dk_echo "WARNING: this file isn't running from the branch directory"
+#		dk_call dk_echo "Is must be in the branch directory to continue."
+#		dk_call dk_echo "DKSCRIPT_DIR = ${DKSCRIPT_DIR}"
+#		dk_call dk_echo "${clr}"			
+#		dk_call dk_printVar DKBRANCH_DIR
+#		return $(false);
+#	fi
 		
-	# Backup main script file to digitalknob/
-	#DIGITALKNOB_PARENT_DIR=$(dk_dirname "${DIGITALKNOB_DIR}")
-	#rm -r -f "${DIGITALKNOB_PARENT_DIR}/${DKSCRIPT_NAME}"
-	#cp "${DKSCRIPT_PATH}" "${DIGITALKNOB_PARENT_DIR}/${DKSCRIPT_NAME}"
-	#dk_echo "DELETING ${DIGITALKNOB_DIR} . . . ."
-	#rm -r -f "${DIGITALKNOB_DIR}" 2>/dev/null
+	# Backup main script file to ${DKCACHE_DIR}
+	dk_call dk_delete "${DKCACHE_DIR}/${DKSCRIPT_NAME}"
+	dk_call dk_copy "${DKSCRIPT_PATH}" "${DKCACHE_DIR}/${DKSCRIPT_NAME}"
+	dk_call dk_cd "${DKCACHE_DIR}"
+	dk_call dk_echo "DELETING ${DKBRANCH_DIR} . . . ."
+	dk_call dk_delete "${DKBRANCH_DIR}"
 	
-	rm -r -f "${DIGITALKNOB_DIR}/${DKSCRIPT_NAME}"
-	cp "${DKSCRIPT_PATH}" "${DIGITALKNOB_DIR}/${DKSCRIPT_NAME}"
-	cd "${DIGITALKNOB_DIR}"
-	dk_echo "DELETING ${DKBRANCH_DIR} . . . ."
-	rm -r -f "${DKBRANCH_DIR}" 2>/dev/null
+	dk_call dk_gitUpdate https://github.com/aquawicket/DigitalKnob.git Development #NO_CONFIRM
 	
-	dk_gitUpdate https://github.com/aquawicket/DigitalKnob.git Development #NO_CONFIRM
-	
-	dk_info "Restarting ${DKSCRIPT_NAME}. . ."
-	dk_sleep 3
-	dk_reload
+	dk_call dk_info "Restarting ${DKSCRIPT_NAME}. . ."
+	dk_call dk_sleep 3
+	dk_call dk_reload
 }
 
 
@@ -57,6 +50,7 @@ dk_resetAll() {
 
 ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
 DKTEST() {
+	dk_debugFunc 0
 
 	dk_resetAll
 }
