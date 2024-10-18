0bef49bc0a2756973da4222038bf82e2aa316200
aquawicket <aquawicket@hotmail.com>
2024-10-18 05:22:33 -0400

    git commit Fri 10/18/2024

--- 3rdParty/_DKIMPORTS/git/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/git/DKMAKE.cmake
@@ -6,13 +6,16 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 # https://github.com/git-for-windows/git
 
 dk_validate(host_triple "dk_host_triple()")
+dk_validate(DKIMPORTS_DIR "dk_DKBRANCH_DIR()")
+dk_getFileParam("${DKIMPORTS_DIR}/git/version.txt" VERSION)
+message("Git VERSION = ${VERSION}")
 
 ### DOWNLOAD ###
 if(WIN_X86_HOST)
-	dk_set		(GIT_DL https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/PortableGit-2.44.0-32-bit.7z.exe)
+	dk_set		(GIT_DL https://github.com/git-for-windows/git/releases/download/v${VERSION}.windows.1/PortableGit-${VERSION}-32-bit.7z.exe)
 endif()
 if(WIN_X86_64_HOST)
-	dk_set		(GIT_DL https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/PortableGit-2.44.0-64-bit.7z.exe)
+	dk_set		(GIT_DL https://github.com/git-for-windows/git/releases/download/v${VERSION}.windows.1/PortableGit-${VERSION}-64-bit.7z.exe)
 endif()
 if(WIN_HOST AND NOT GIT_DL)
 	dk_fatal("GIT_DL is invalid!")
