a908f66100e2ef04f2b6f3c1737d8474b16898ea
aquawicket <aquawicket@hotmail.com>
2024-10-18 10:58:33 -0400

    git commit Fri 10/18/2024

--- DKBash/functions/dk_reload.sh
+++ DKBash/functions/dk_reload.sh
@@ -11,13 +11,13 @@ dk_reload() {
 	dk_call dk_assertPath DKSCRIPT_PATH
 	dk_call dk_info "reloading ${DKSCRIPT_PATH}. . ."
 
-	dk_call dk_clearScreen
+	#dk_call dk_clearScreen
 	
 	###### METHOD 1 ######
 	# Clear the environment and reload script
-	BASH_EXE=bash
-	${BASH_EXE} -c "env -i HOME="$HOME" PATH="$PATH" ${BASH_EXE} -l -c "${DKSCRIPT_PATH}""
-	
+	BASH_EXE=${BASH}
+	exec env -i HOME="$HOME" PATH="$PATH" DKSCRIPT_PATH="${DKSCRIPT_PATH}" bash -l -c '${DKSCRIPT_PATH}'
+
 	###### METHOD 2 ######
 	# Replace the current process
 	#exec /bin/bash "${DKSCRIPT_PATH}"
