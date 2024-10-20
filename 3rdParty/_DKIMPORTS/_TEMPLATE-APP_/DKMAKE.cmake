e5fbbd290ab2094a7cc0df0f1c5c7303e4037f29
aquawicket <aquawicket@hotmail.com>
2024-10-19 20:17:16 -0400

    git commit Sat 10/19/2024

--- 3rdParty/_DKIMPORTS/_TEMPLATE-APP_/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/_TEMPLATE-APP_/DKMAKE.cmake
@@ -1,33 +1,25 @@
-#!/usr/bin/cmake -P
-include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
-dk_load(dk_builder)
-# to use this template, remove these lines
+########################################################################################
+# Remove these lines and rename all instaces of _TEMPLATE-APP_ to the name of your app
 dk_undepend(_TEMPLATE-APP_)
 dk_return()
-#########################################
+########################################################################################
 
 
+#!/usr/bin/cmake -P
+if(NOT DKCMAKE_FUNCTIONS_DIR_)
+	set(DKCMAKE_FUNCTIONS_DIR_ ${CMAKE_SOURCE_DIR}/../../../DKCMake/functions/)
+endif()
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+
+
+###### [app_name] ######
 # http://www.app.com
 # http://www.app.com/downloads/app-1.0.exe
-
 
 ### DEPENDS ###
 #dk_depend(depend_name)
 
-
-### VERSION ###
-#dk_set(APP_VERSION 1.0)
-#dk_set(APP_NAME "app-${APP_VERSION}.exe")
-#dk_set(APP_DL https://app.com/app.exe)
-#dk_set(APP "${ProgramFiles_x86}/${APP_NAME}")
-#dk_set(APP_EXE "${APP}/app.exe")
-
 ### INSTALL ###
-#IF(NOT EXISTS "${APP_EXE}")
-#	MESSAGE(STATUS "Installing ${APP_NAME}")
-#	dk_download(${APP_DL} ${DKDOWNLOAD_DIR}/${APP_NAME})
-#	dk_command(${DKDOWNLOAD_DIR}/${APP_NAME})
-#ENDIF()
-
-
-dk_import("http://www.app.com/downloads/app-1.0.exe")
+dk_validate		(DKIMPORTS_DIR "dk_DKBRANCH_DIR()")
+dk_getFileParam	(${DKIMPORTS_DIR}/[app_name]/[app_name].txt [APP_NAME]_IMPORT)
+dk_import		(${[APP_NAME]})
--- 3rdParty/_DKIMPORTS/_TEMPLATE-LIB_/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/_TEMPLATE-LIB_/DKMAKE.cmake
@@ -1,41 +1,39 @@
-#!/usr/bin/cmake -P
-include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
-dk_load(dk_builder)
-
-# to use this template, remove these lines
-#########################################
+########################################################################################
+# Remove these lines and rename all instaces of _TEMPLATE-APP_ to the name of your library
 dk_undepend(_TEMPLATE-LIB_)
 dk_return()
-#########################################
+########################################################################################
+
+#!/usr/bin/cmake -P
+if(NOT DKCMAKE_FUNCTIONS_DIR_)
+	set(DKCMAKE_FUNCTIONS_DIR_ ${CMAKE_SOURCE_DIR}/../../../DKCMake/functions/)
+endif()
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
-
-
-
-# https://github.com/organization/package
-
+###### [lib_name] ######
+# http://www.[lib_name].com
+# https://github.com/organization/[lib_name]
 
 ### DEPENDS ###
 #dk_depend(libname)
 
-
 ### INSTALL ###
-#dk_import(https://organization.com/download/package-1.0.zip)
-dk_import(https://github.com/organization/package.git)
-dk_import(https://github.com/organization/package/archive/refs/heads/master.zip)
-
+dk_validate		(DKIMPORTS_DIR "dk_DKBRANCH_DIR()")
+dk_getFileParam	(${DKIMPORTS_DIR}/[lib_name]/[lib_name].txt [LIB_NAME]_IMPORT)
+dk_import		(${[LIB_NAME]})
 
 ### LINK ###
-dk_include			(${_TEMPLATE_LIB_}/include)
-UNIX_dk_libDebug	(${_TEMPLATE_LIB_DEBUG_DIR}/libpackage.a)
-UNIX_dk_libRelease	(${_TEMPLATE_LIB_RELEASE_DIR}/libpackage.a)
-WIN_dk_libDebug		(${_TEMPLATE_LIB_DEBUG_DIR}/package.lib)
-WIN_dk_libRelease	(${_TEMPLATE_LIB_RELEASE_DIR}/package.lib)
+dk_include			(${[LIB_NAME]}/include)
+UNIX_dk_libDebug	(${[LIB_NAME]_DEBUG_DIR}/libpackage.a)
+UNIX_dk_libRelease	(${[LIB_NAME]_RELEASE_DIR}/libpackage.a)
+WIN_dk_libDebug		(${[LIB_NAME]_DEBUG_DIR}/package.lib)
+WIN_dk_libRelease	(${[LIB_NAME]_RELEASE_DIR}/package.lib)
 
 
 ### GENERATE ###
-dk_configure(${_TEMPLATE_LIB_})
+dk_configure(${[LIB_NAME]})
 
 
 ### COMPILE ###
-dk_build(${_TEMPLATE_LIB_} package)
+dk_build(${[LIB_NAME]} package)
