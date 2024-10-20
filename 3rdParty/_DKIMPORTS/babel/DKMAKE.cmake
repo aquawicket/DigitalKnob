647823c4b91dc9db2f2a3e9457bf25e448cb78b6
aquawicket <aquawicket@hotmail.com>
2024-10-19 21:07:01 -0400

    git commit Sat 10/19/2024

--- 3rdParty/_DKIMPORTS/babel/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/babel/DKMAKE.cmake
@@ -5,11 +5,9 @@ endif()
 include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
-dk_load(dk_builder)
+###### babel ######
 # https://github.com/babel/babel.git
 # https://unpkg.com/@babel/standalone@7.14.4/babel.min.js
 
-
 ### IMPORT ###
-#dk_import(https://github.com/babel/babel.git)
 dk_import(https://unpkg.com/@babel/standalone@7.14.4/babel.min.js)
