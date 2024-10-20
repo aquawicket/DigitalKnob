9185dcf5a6204748ff8e39966d433beb467deff0
aquawicket <aquawicket@hotmail.com>
2024-10-20 12:05:38 -0400

    git commit Sun 10/20/2024

--- 3rdParty/_DKIMPORTS/libuv/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libuv/DKMAKE.cmake
@@ -5,41 +5,42 @@ endif()
 include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
-dk_load(dk_builder)
+############ libuv ############
 # https://github.com/libuv/libuv.git
 
+dk_load				(dk_builder)
 
 ### DEPEND ###
-dk_depend(iphlpapi)
-dk_depend(userenv)
-
+dk_depend			(iphlpapi)
+dk_depend			(userenv)
 
 ### IMPORT ###
-#dk_import(https://github.com/libuv/libuv.git)
-#dk_import(https://github.com/libuv/libuv/archive/refs/tags/v1.44.2.zip)
-#dk_import(https://github.com/libuv/libuv.git TAG 2bc22c40eb68f3b38d1ae3ea68ef2f180cbe9fef)
-dk_import(https://github.com/libuv/libuv/archive/2bc22c40eb68f3b38d1ae3ea68ef2f180cbe9fef.zip)
-
+dk_validate			(DKIMPORTS_DIR "dk_DKBRANCH_DIR()")
+dk_getFileParam 	("${DKIMPORTS_DIR}/libuv/libuv.txt" LIBUV_DL)
+dk_import			(${LIBUV_DL})
 
 ### LINK ###
-dk_include			(${LIBUV}/include)
+dk_include			(${LIBUV}/include					LIBUV_INCLUDE_DIR)
 dk_include			(${LIBUV}/${triple})
-UNIX_dk_libDebug	(${LIBUV}/${triple}/${DEBUG_DIR}/libuv_a.a)
-UNIX_dk_libRelease	(${LIBUV}/${triple}/${RELEASE_DIR}/libuv_a.a)
-WIN_dk_libDebug		(${LIBUV}/${triple}/${DEBUG_DIR}/uv_a.lib)
-WIN_dk_libRelease	(${LIBUV}/${triple}/${RELEASE_DIR}/uv_a.lib)
-
+if(MSVC)
+	dk_libDebug		(${LIBUV_DEBUG_DIR}/uv_a.lib		LIBUV_DEBUG_LIBRARY)
+	dk_libRelease	(${LIBUV_RELEASE_DIR}/uv_a.lib		LIBUV_RELEASE_LIBRARY)
+else()
+	dk_libDebug		(${LIBUV_DEBUG_DIR}/libuv_a.a		LIBUV_DEBUG_LIBRARY)
+	dk_libRelease	(${LIBUV_RELEASE_DIR}/libuv_a.a		LIBUV_RELEASE_LIBRARY)
+endif()
+if(DEBUG)
+	set				(LIBUV_LIBRARY ${LIBUV_DEBUG_LIBRARY})
+endif()
+if(RELEASE)
+	set				(LIBUV_LIBRARY ${LIBUV_RELEASE_LIBRARY})
+endif()
 
 ### 3RDPARTY LINK ###
-UNIX_DEBUG_dk_set	(LIBUV_CMAKE -DLIBUV_INCLUDE_DIR=${LIBUV} -DLIBUV_LIBRARY=${LIBUV}/${triple}/${DEBUG_DIR}/uv_a.a)
-UNIX_RELEASE_dk_set	(LIBUV_CMAKE -DLIBUV_INCLUDE_DIR=${LIBUV} -DLIBUV_LIBRARY=${LIBUV}/${triple}/${RELEASE_DIR}/uv_a.a)
-WIN_DEBUG_dk_set	(LIBUV_CMAKE -DLIBUV_INCLUDE_DIR=${LIBUV} -DLIBUV_LIBRARY=${LIBUV}/${triple}/${DEBUG_DIR}/uv_a.lib)
-WIN_RELEASE_dk_set	(LIBUV_CMAKE -DLIBUV_INCLUDE_DIR=${LIBUV} -DLIBUV_LIBRARY=${LIBUV}/${triple}/${RELEASE_DIR}/uv_a.lib)
-
+dk_set				(LIBUV_CMAKE -DLIBUV_INCLUDE_DIR=${LIBUV_INCLUDE_DIR} -DLIBUV_LIBRARY=${LIBUV_LIBRARY})
 
 ### GENERATE ###
-dk_configure(${LIBUV})
-
+dk_configure		(${LIBUV})
 
 ### COMPILE ###
-dk_build(${LIBUV} uv_a)
+dk_build			(${LIBUV} uv_a)
