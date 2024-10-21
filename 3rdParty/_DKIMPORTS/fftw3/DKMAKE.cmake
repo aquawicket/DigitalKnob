9c6d44d6d2976ceb5e8c839cc721b5b82c8dd132
aquawicket <aquawicket@hotmail.com>
2024-10-21 15:56:44 -0400

    git commit Mon 10/21/2024

--- 3rdParty/_DKIMPORTS/fftw3/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/fftw3/DKMAKE.cmake
@@ -27,17 +27,17 @@ endif()
 ### GENERATE ###
 dk_configure(${FFTW3_DIR}
 	-DBUILD_TESTS=OFF				# "Build tests" ON
-	-DENABLE_OPENMP=ON 				# "Use OpenMP for multithreading" OFF
-	-DENABLE_THREADS=ON 			# "Use pthread for multithreading" OFF
-	-DWITH_COMBINED_THREADS=ON 		# "Merge thread library" OFF
-	-DENABLE_FLOAT=ON 				# "single-precision" OFF
-	-DENABLE_LONG_DOUBLE=ON 		# "long-double precision" OFF
-	-DENABLE_QUAD_PRECISION=ON 		# "quadruple-precision" OFF
-	-DENABLE_SSE=ON 				# "Compile with SSE instruction set support" OFF
-	-DENABLE_SSE2=ON		 		# "Compile with SSE2 instruction set support" OFF
-	-DENABLE_AVX=ON 				# "Compile with AVX instruction set support" OFF
-	-DENABLE_AVX2=ON 				# "Compile with AVX2 instruction set support" OFF
-	-DDISABLE_FORTRAN=ON) 			# "Disable Fortran wrapper routines" OFF
+	-DENABLE_OPENMP=OFF				# "Use OpenMP for multithreading" OFF
+	-DENABLE_THREADS=OFF 			# "Use pthread for multithreading" OFF
+	-DWITH_COMBINED_THREADS=OFF		# "Merge thread library" OFF
+	-DENABLE_FLOAT=OFF 				# "single-precision" OFF
+	-DENABLE_LONG_DOUBLE=OFF 		# "long-double precision" OFF
+	-DENABLE_QUAD_PRECISION=OFF		# "quadruple-precision" OFF
+	-DENABLE_SSE=OFF 				# "Compile with SSE instruction set support" OFF
+	-DENABLE_SSE2=OFF		 		# "Compile with SSE2 instruction set support" OFF
+	-DENABLE_AVX=OFF 				# "Compile with AVX instruction set support" OFF
+	-DENABLE_AVX2=OFF 				# "Compile with AVX2 instruction set support" OFF
+	-DDISABLE_FORTRAN=OFF) 			# "Disable Fortran wrapper routines" OFF
 
 ### COMPILE ###
 dk_build(${FFTW3_DIR})# fftw3)
