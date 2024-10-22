a510503f3482277f8f6123df6fe6146302939e69
aquawicket <aquawicket@hotmail.com>
2024-10-21 20:13:24 -0400

    git commit Mon 10/21/2024

--- 3rdParty/_DKIMPORTS/opus/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/opus/DKMAKE.cmake
@@ -5,26 +5,27 @@ endif()
 include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
-dk_load(dk_builder)
+############ opus ############
 # https://github.com/xiph/opus.git
 
+dk_load(dk_builder)
 
 ### IMPORT ###
-#dk_import(https://github.com/xiph/opus.git)
-dk_import(https://github.com/xiph/opus/archive/refs/heads/main.zip)
+dk_import(https://github.com/xiph/opus/archive/7db26934.zip)
 
 ### LINK ###
 dk_include			(${OPUS}/include)
 dk_include			(${OPUS}/${triple})
-UNIX_dk_libDebug	(${OPUS}/${triple}/${DEBUG_DIR}/libopus.a)
-UNIX_dk_libRelease	(${OPUS}/${triple}/${RELEASE_DIR}/libopus.a)
-WIN_dk_libDebug		(${OPUS}/${triple}/${DEBUG_DIR}/opus.lib)
-WIN_dk_libRelease	(${OPUS}/${triple}/${RELEASE_DIR}/opus.lib)
-
+if(MSVC)
+	dk_libDebug		(${OPUS_DEBUG_DIR}/opus.lib)
+	dk_libRelease	(${OPUS_RELEASE_DIR}/opus.lib)
+else()
+	dk_libDebug		(${OPUS_DEBUG_DIR}/libopus.a)
+	dk_libRelease	(${OPUS_RELEASE_DIR}/libopus.a)
+endif()
 
 ### GENERATE ###
 dk_configure(${OPUS})
-
 
 ### COMPILE ###
 dk_build(${OPUS})
