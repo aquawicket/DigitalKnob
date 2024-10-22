46ddf4ef7e8544670fe059be7d37e7c53b68cef3
aquawicket <aquawicket@hotmail.com>
2024-10-22 00:48:28 -0400

    git commit Tue 10/22/2024

--- 3rdParty/_DKIMPORTS/libwinpthread/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/libwinpthread/DKMAKE.cmake
@@ -5,6 +5,7 @@ endif()
 include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
+############ libwinpthread ############
 dk_load(dk_builder)
 #if(WIN_X86)
 #	dk_depend(mingw32)
@@ -13,21 +14,22 @@ dk_load(dk_builder)
 #endif()
 dk_depend(msys2)
 
-
-if(WIN_X86)
-	#dk_set(LIBWINPTHREAD ${MINGW32}/i686-w64-mingw32/lib)
+if(WIN_X86_CLANG)
+	dk_set(LIBWINPTHREAD ${MSYS2_DIR}/clang32/lib)
+elseif(WIN_X86_64_CLANG)
+	dk_set(LIBWINPTHREAD ${MSYS2_DIR}/clang64/lib)
+elseif(WIN_X86_MINGW)
 	dk_set(LIBWINPTHREAD ${MSYS2_DIR}/mingw32/lib)
-endif()
-if(WIN_X86_64)
-	#dk_set(LIBWINPTHREAD ${MINGW64}/x86_64-w64-mingw32/lib)
+elseif(WIN_X86_64_MINGW)
 	dk_set(LIBWINPTHREAD ${MSYS2_DIR}/mingw64/lib)
 endif()
 
 
 ### LINK ###
-WIN_dk_libDebug(${LIBWINPTHREAD}/libwinpthread.a)
-WIN_dk_libRelease(${LIBWINPTHREAD}/libwinpthread.a)
-
-
-### 3RDPARTY LINK ###
-WIN_dk_set(LIBWINPTHREAD_CMAKE -DLIBWINPTHREAD_LIBRARIES=${LIBWINPTHREAD})
+if(WIN)
+	dk_libDebug(${LIBWINPTHREAD}/libwinpthread.a)
+	dk_libRelease(${LIBWINPTHREAD}/libwinpthread.a)
+	
+### 3RDPARTY LINK ###	
+	dk_set(LIBWINPTHREAD_CMAKE -DLIBWINPTHREAD_LIBRARIES=${LIBWINPTHREAD})
+endif()
