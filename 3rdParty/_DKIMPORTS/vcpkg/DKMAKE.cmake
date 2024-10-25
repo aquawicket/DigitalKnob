e228b72e3a7513a69da8f0a38fa164d33612bc88
aquawicket <aquawicket@hotmail.com>
2024-10-25 19:34:31 -0400

    git commit Fri 10/25/2024

--- 3rdParty/_DKIMPORTS/vcpkg/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/vcpkg/DKMAKE.cmake
@@ -5,14 +5,15 @@ endif()
 include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
-dk_load(dk_builder)
+############ vcpkg ############
 # Getting Started  https://vcpkg.io/en/getting-started.html
 # Documentation    https://vcpkg.io/en/docs/README.html
+dk_load(dk_builder)
 
-
-#dk_import(https://github.com/Microsoft/vcpkg.git)
-dk_import(https://github.com/microsoft/vcpkg/archive/refs/heads/master.zip)
-
+dk_import(https://github.com/microsoft/vcpkg/archive/cff6ed45.zip)
 
 dk_cd(${VCPKG})
-WIN_dk_queueCommand(bootstrap-vcpkg.bat)
+
+if(WIN)
+	dk_queueCommand(bootstrap-vcpkg.bat)
+endif()
