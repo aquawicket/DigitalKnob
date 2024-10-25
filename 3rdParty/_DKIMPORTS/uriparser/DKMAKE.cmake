2c0ba072a27912e1188a7e885f9e5042e33369af
aquawicket <aquawicket@hotmail.com>
2024-10-25 18:58:59 -0400

    git commit Fri 10/25/2024

--- 3rdParty/_DKIMPORTS/uriparser/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/uriparser/DKMAKE.cmake
@@ -5,28 +5,28 @@ endif()
 include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
-dk_load(dk_builder)
+############ uriparser ############
 # https://github.com/uriparser/uriparser
+dk_load(dk_builder)
 
 ### INSTALL ###
-#dk_import(https://github.com/uriparser/uriparser.git)
-dk_import(https://github.com/uriparser/uriparser/archive/refs/heads/master.zip)
-
+dk_import(https://github.com/uriparser/uriparser/archive/2ab7b7df.zip)
 
 ### LINK ###
 dk_define			(URI_STATIC_BUILD)
 dk_include			(${URIPARSER}/include)
-UNIX_dk_libDebug	(${URIPARSER}/${triple}/${DEBUG_DIR}/liburiparser.a)
-UNIX_dk_libRelease	(${URIPARSER}/${triple}/${RELEASE_DIR}/liburiparser.a)
-WIN_dk_libDebug		(${URIPARSER}/${triple}/${DEBUG_DIR}/uriparser.lib)
-WIN_dk_libRelease	(${URIPARSER}/${triple}/${RELEASE_DIR}/uriparser.lib)
-
+if(MSVC)
+	dk_libDebug		(${URIPARSER}/${triple}/${DEBUG_DIR}/uriparser.lib)
+	dk_libRelease	(${URIPARSER}/${triple}/${RELEASE_DIR}/uriparser.lib)
+else()
+	dk_libDebug		(${URIPARSER}/${triple}/${DEBUG_DIR}/liburiparser.a)
+	dk_libRelease	(${URIPARSER}/${triple}/${RELEASE_DIR}/liburiparser.a)
+endif()
 
 ### GENERATE ###
 dk_configure(${URIPARSER}
 	-DURIPARSER_BUILD_DOCS=OFF
 	-DURIPARSER_BUILD_TESTS=OFF)
-
 
 ### COMPILE ###
 dk_build(${URIPARSER} uriparser)
