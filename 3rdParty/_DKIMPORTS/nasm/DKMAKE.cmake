b68ff11e62b78b2355e2747176a38fa1adcd0ffe
aquawicket <aquawicket@hotmail.com>
2024-10-29 15:58:04 -0400

    git commit Tue 10/29/2024

--- 3rdParty/_DKIMPORTS/nasm/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/nasm/DKMAKE.cmake
@@ -7,35 +7,49 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 ############ nasm ############
 # https://www.nasm.us
-dk_load(dk_builder)
+
 dk_validate(host_triple "dk_host_triple()")
 if(ANDROID_HOST)
 	return()
 endif()
 
-dk_depend(rpm)
-
-
-LINUX_X86_HOST_dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.i686.rpm)
-LINUX_X86_HOST_dk_set(NASM_EXE ${NASM_DIR}/nasm)
-
-if(LINUX_HOST)
-#import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.x86_64.rpm)
-dk_download(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.x86_64.rpm)
-dk_depend(sudo)
-dk_command(${SUDO_EXE} rpm -i ${DKDOWNLOAD_DIR}/nasm-2.16.01-0.fc36.x86_64.rpm)
-#dk_set(NASM_EXE ${NASM_DIR}/nasm)
+if(LINUX_X86_HOST)
+	#dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.i686.rpm)
+	#dk_depend(rpm)
+	#dk_command(${SUDO_EXE} rpm -i ${DKDOWNLOAD_DIR}/nasm-2.16.01-0.fc36.i686.rpm)
+	
+	dk_download(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.i686.rpm)
+	dk_validate(SUDO_EXE "dk_depend(sudo)")
+	dk_validate(ALIEN_EXE "dk_depend(alien)")
+	dk_command(${SUDO_EXE} ${ALIEN_EXE} -i ${DKDOWNLOAD_DIR}/linux/nasm-2.16.01-0.fc36.i686.rpm)
+	dk_set(NASM_EXE ${NASM_DIR}/nasm)
 endif()
 
-MAC_HOST_dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/macosx/nasm-2.16.01-macosx.zip)
-MAC_HOST_dk_set(NASM_EXE ${NASM_DIR}/nasm)
+if(LINUX_HOST)
+	#dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.x86_64.rpm)
+	#dk_depend(rpm)
+	#dk_command(${SUDO_EXE} rpm -i ${DKDOWNLOAD_DIR}/nasm-2.16.01-0.fc36.x86_64.rpm)
+	
+	dk_download(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/linux/nasm-2.16.01-0.fc36.x86_64.rpm)
+	dk_validate(SUDO_EXE "dk_depend(sudo)")
+	dk_validate(ALIEN_EXE "dk_depend(alien)")
+	dk_command(${SUDO_EXE} ${ALIEN_EXE} -i ${DKDOWNLOAD_DIR}/nasm-2.16.01-0.fc36.x86_64.rpm)
+	dk_set(NASM_EXE ${NASM_DIR}/nasm)
+endif()
 
-#WIN_HOST_dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/win32/nasm-2.16.01-win32.zip)
-if(WIN_HOST)
+if(MAC_HOST)
+	dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/macosx/nasm-2.16.01-macosx.zip)
+	dk_set(NASM_EXE ${NASM_DIR}/nasm)
+endif()
+
+if(WIN_HOST_X86)
+	dk_import(https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/win32/nasm-2.16.01-win32.zip)
+	dk_set(NASM_EXE ${NASM_DIR}/nasm.exe)
+endif()
+
+if(WIN_HOST_X86_64)
 	dk_import(https://github.com/microsoft/vcpkg/files/12073957/nasm-2.16.01-win64.zip)
 	dk_set(NASM_EXE ${NASM_DIR}/nasm.exe)
 endif()
 
-#dk_setEnv("PATH" "${NASM_EXE}")
-#dk_info("NASM = ${NASM}")
 dk_appendEnvPath(${NASM_DIR})
