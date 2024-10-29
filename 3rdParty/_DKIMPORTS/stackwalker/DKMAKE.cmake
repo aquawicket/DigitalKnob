0d80333ac6debc5a3edd58065e596803b0d7f421
aquawicket <aquawicket@hotmail.com>
2024-10-29 16:04:05 -0400

    git commit Tue 10/29/2024

--- 3rdParty/_DKIMPORTS/stackwalker/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/stackwalker/DKMAKE.cmake
@@ -5,32 +5,29 @@ endif()
 include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
-dk_load(dk_builder)
+############ stackwalker ############
 # https://www.codeproject.com/Articles/11132/Walking-the-callstack-2
 # https://github.com/ConnectedVision/connectedvision/tree/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0
 # https://github.com/ConnectedVision/connectedvision/raw/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0/stackwalker-2014-12-28.zip
 # https://www.codeproject.com/KB/threads/StackWalker/stackwalker.zip
 # FIXME: Let's replace this with boost::stacktrace for cross platform support.
 # https://www.boost.org/doc/libs/1_75_0/doc/html/stacktrace.html
+dk_load(dk_builder)
 if(NOT MSVC)
-	dk_undepend(stackwalker)
+	dk_disable(stackwalker)
 	dk_return()
 endif()
 
-
 ### IMPORT ###
 dk_import(https://github.com/ConnectedVision/connectedvision/raw/master/build_env/Conan/packages/StackWalker/2014.12.28/2.3.0/stackwalker-2014-12-28.zip FOLDER stackwalker-2014-12-28 PATCH)
-
 
 ### LINK ###
 WIN_dk_include		(${STACKWALKER}/StackWalker)
 WIN_dk_libDebug		(${STACKWALKER_DEBUG_DIR}/stackwalker.lib)
 WIN_dk_libRelease	(${STACKWALKER_RELEASE_DIR}/stackwalker.lib)
 
-
 ### GENERATE ###
 WIN_dk_configure	(${STACKWALKER})
-
 
 ### COMPILE ###
 WIN_dk_build		(${STACKWALKER})
