41415057d3589e5180dd44d1a9fd5a4f39cde13e
aquawicket <aquawicket@hotmail.com>
2024-11-03 18:17:54 -0500

    git commit Sun 11/03/2024

--- DKCSharp/snippets/runCSharpFile.cs
+++ DKCSharp/snippets/runCSharpFile.cs
@@ -16,9 +16,13 @@ namespace dynamiccompilation{
         static CSharpCodeProvider provider = new CSharpCodeProvider();
 
         static void Main(string[] args){
+			string[] arguments = Environment.GetCommandLineArgs();
+			System.Console.WriteLine("GetCommandLineArgs: {0}", string.Join(", ", arguments));
+			 
             CompilerParams.ReferencedAssemblies.AddRange(references);
-			int returnValue = CompileAndRun("helloWorld");
-			System.Console.WriteLine("returnValue = "+returnValue);
+			//int returnValue = CompileAndRun("dk_echo");
+			//System.Console.WriteLine("returnValue = "+returnValue);
+			CompileAndRunDKTEST("dk_echo");
             Console.ReadKey();
         }
 
@@ -37,11 +41,29 @@ namespace dynamiccompilation{
                 Module module = compile.CompiledAssembly.GetModules()[0];
                 returnValue = (int)module.GetType("NAMESPACE.CLASS").GetMethod(funcName).Invoke(null, new object[] {"Hello World!"});
             }
-			
             DateTime executionFinished = DateTime.Now;
             Console.WriteLine("Compile time: {0}", compilationFinished - start);
             Console.WriteLine("Execution time: {0}", executionFinished - compilationFinished);
 			return returnValue;
+        }
+		
+		static void CompileAndRunDKTEST(string funcName) {
+            DateTime start = DateTime.Now;
+            CompilerResults compile = provider.CompileAssemblyFromFile(CompilerParams, funcName+".cs");
+            DateTime compilationFinished = DateTime.Now;
+            if (compile.Errors.HasErrors) {
+                foreach (CompilerError ce in compile.Errors) {
+                    Console.WriteLine(ce.ToString());
+                }
+                Console.ReadKey();
+                return;
+            } else {
+                Module module = compile.CompiledAssembly.GetModules()[0];
+				module.GetType("NAMESPACE.CLASS").GetMethod("DKTEST").Invoke(null, new object[] {});
+            }
+            DateTime executionFinished = DateTime.Now;
+            Console.WriteLine("Compile time: {0}", compilationFinished - start);
+            Console.WriteLine("Execution time: {0}", executionFinished - compilationFinished);
         }
     }
 }
