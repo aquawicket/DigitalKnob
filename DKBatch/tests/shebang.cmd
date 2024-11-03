048b4bc0fd32efa61cd4184eecec117992c4273c
aquawicket <aquawicket@hotmail.com>
2024-11-02 20:25:44 -0400

    git commit Sat 11/02/2024

---
+++ DKBatch/tests/shebang.cmd
@@ -0,0 +1,9 @@
+@echo off
+set /p texte=< C:\Users\Administrator\digitalknob\Development\DKBash\functions\dk_debug.sh 
+echo %texte%
+
+set /p texte=< C:\Users\Administrator\digitalknob\Development\DKCMake\functions\dk_debug.cmake
+echo %texte%
+pause
+
+pause
