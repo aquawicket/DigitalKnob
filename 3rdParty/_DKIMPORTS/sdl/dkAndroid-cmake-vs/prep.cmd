e17539e2a768f485c5ec9e17e7bec9f3c391fd4a
aquawicket <aquawicket@hotmail.com>
2024-11-05 10:33:49 -0500

    git commit Tue 11/05/2024

--- 3rdParty/_DKIMPORTS/sdl/dkAndroid-cmake-vs/prep.cmd
+++ 3rdParty/_DKIMPORTS/sdl/dkAndroid-cmake-vs/prep.cmd
@@ -1,4 +1,6 @@
 @echo off
+if not defined DKBATCH_FUNCTIONS_DIR_ set "DKBATCH_FUNCTIONS_DIR_=..\..\..\DKBatch\functions\"
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 
 set "DIGITALKNOB_DIR=%USERPROFILE%\digitalknob"
 set "JAVA_HOME=%DIGITALKNOB_DIR%\Development\3rdParty\openjdk-11_windows-x64_bin"
--- 3rdParty/_DKIMPORTS/visualstudio/Install.cmd
+++ 3rdParty/_DKIMPORTS/visualstudio/Install.cmd
@@ -1,4 +1,5 @@
 @echo off
+if not defined DKBATCH_FUNCTIONS_DIR_ set "DKBATCH_FUNCTIONS_DIR_=..\..\..\DKBatch\functions\"
 if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 
 %dk_call% dk_validate DKDOWNLOAD_DIR "%dk_call% dk_DIGITALKNOB_DIR"
