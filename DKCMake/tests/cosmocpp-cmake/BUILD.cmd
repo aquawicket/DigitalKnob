dc463bdd91b64f1685f268668a33952d260174bd
aquawicket <aquawicket@hotmail.com>
2024-11-13 11:16:12 -0500

    git commit Wed 11/13/2024

--- DKCMake/tests/cosmocpp-cmake/BUILD.cmd
+++ DKCMake/tests/cosmocpp-cmake/BUILD.cmd
@@ -10,10 +10,10 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 @RD /S /Q "build"
 
 set "BASH_EXE=%DK3RDPARTY_DIR%\msys2-x86_64-20240727\usr\bin\bash.exe"
-%dk_call% dk_replaceAll "%DKTOOLS_DIR%\cmake-3.29.5-windows-x86_64\bin\cmake"          "\" "/" CMAKE_EXE
+%dk_call% dk_replaceAll "%DKTOOLS_DIR%\cmake-3.29.5-windows-x86_64\bin\cmake"         "\" "/" CMAKE_EXE
 %dk_call% dk_replaceAll "%DK3RDPARTY_DIR%\_DKIMPORTS\cosmocc\cosmo_toolchain.cmake"   "\" "/" CMAKE_TOOLCHAIN_FILE
-%dk_call% dk_replaceAll "%DKSCRIPT_DIR%"                                               "\" "/" CMAKE_SOURCE_DIR
-%dk_call% dk_replaceAll "%DKSCRIPT_DIR%/\build"                                        "\" "/" CMAKE_BINARY_DIR
+%dk_call% dk_replaceAll "%DKSCRIPT_DIR%"                                              "\" "/" CMAKE_SOURCE_DIR
+%dk_call% dk_replaceAll "%DKSCRIPT_DIR%/\build"                                       "\" "/" CMAKE_BINARY_DIR
 
 
 ::###### Configure helloWorld ######
