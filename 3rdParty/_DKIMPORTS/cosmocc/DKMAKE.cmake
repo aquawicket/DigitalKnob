e39adeb6e7aaef6574070b08db9f5ab7bff21b66
aquawicket <aquawicket@hotmail.com>
2024-11-12 21:33:11 -0500

    git commit Tue 11/12/2024

--- 3rdParty/_DKIMPORTS/cosmocc/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/cosmocc/DKMAKE.cmake
@@ -1,9 +1,13 @@
 #!/usr/bin/cmake -P
+include_guard()
+message("${ESC}[46m ${ESC}[30m ***${CMAKE_CURRENT_LIST_FILE}*** ${ESC}[0m")
 if(NOT DKCMAKE_FUNCTIONS_DIR_)
-	set(DKCMAKE_FUNCTIONS_DIR_ ${CMAKE_SOURCE_DIR}/../../../DKCMake/functions/)
+	set(DKCMAKE_FUNCTIONS_DIR_ ${CMAKE_SOURCE_DIR}/../../../DKCMake/functions/) # From this path to DK.cmake
+endif()
+if(NOT EXISTS ${DKCMAKE_FUNCTIONS_DIR_})
+	set(DKCMAKE_FUNCTIONS_DIR_ ${CMAKE_SOURCE_DIR}/../../../../../../../DKCMake/functions/) # From try compile to Dk.cmake 
 endif()
 include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
-
 
 ###### [cosmocc] ######
 # https://cosmo.zip
@@ -13,22 +17,71 @@ dk_load(dk_builder)
 dk_import(https://cosmo.zip/pub/cosmocc/cosmocc.zip)
 
 
-dk_set(COSMO_AR				${COSMOCC}/bin/cosmoar)
-dk_set(COSMO_C_COMPILER   	${COSMOCC}/bin/cosmocc)
-dk_set(COSMO_CXX_COMPILER 	${COSMOCC}/bin/cosmoc++)
-dk_set(COSMO_MAKE_PROGRAM 	${COSMOCC}/bin/make)
-#dk_set(COSMO_RC_COMPILER  	${COSMOCC}/bin/???)
-dk_set(COSMO_RANLIB			${COSMOCC}/bin/cosmoranlib)
+#dk_set(CMAKE_SYSTEM_NAME 				Generic)
+dk_set(COSMO		 					1)
+dk_set(COSMOPOLITAN 					1)
+#dk_set(BUILD_SHARED_LIBS 				OFF)
+#dk_set(CMAKE_SKIP_RPATH 				ON)
+#dk_set(CMAKE_CROSSCOMPILING 			OFF)
+
+dk_set(CMAKE_GENERATOR					"MSYS Makefiles")
+#dk_set(CMAKE_GENERATOR					"MinGW Makefiles")
+dk_set(CMAKE_FORCE_UNIX_PATHS 1)
+	
+#dk_set(COSMO_MAKE_PROGRAM 				"${MSYS2}/clang64/bin/mingw32-make.exe")
+dk_set(COSMO_ASM_COMPILER 				"${COSMOCC}/bin/cosmocc")
+dk_set(COSMO_C_COMPILER 				"${COSMOCC}/bin/cosmocc")
+dk_set(COSMO_CXX_COMPILER 				"${COSMOCC}/bin/cosmoc++")
+dk_set(COSMO_AR 						"${COSMOCC}/bin/cosmoar")
+dk_set(COSMO_RANLIB 					"${COSMOCC}/bin/${CMAKE_HOST_SYSTEM_PROCESSOR}-linux-cosmo-ranlib")
+dk_set(COSMO_LINKER 					"${COSMOCC}/bin/${CMAKE_HOST_SYSTEM_PROCESSOR}-linux-cosmo-ld")
+foreach(lang ASM C CXX)
+  dk_set(COSMO_${lang}_COMPILER_AR 		"${COSMOCC}/bin/${CMAKE_HOST_SYSTEM_PROCESSOR}-linux-cosmo-gcc-ar")
+  dk_set(COSMO_${lang}_COMPILER_RANLIB 	"${COSMOCC}/bin/${CMAKE_HOST_SYSTEM_PROCESSOR}-linux-cosmo-gcc-ranlib")
+endforeach()
+
+
+#dk_set(CMAKE_CXX_FLAGS_INIT 			"-fexceptions -frtti")
+dk_set(CMAKE_USER_MAKE_RULES_OVERRIDE 	${CMAKE_CURRENT_LIST_FILE})
+dk_set(CMAKE_ASM_OUTPUT_EXTENSION 		.o)
+dk_set(CMAKE_C_OUTPUT_EXTENSION 		.o)
+dk_set(CMAKE_CXX_OUTPUT_EXTENSION 		.o)
+
 
 
 ###### set GLOBAL CMAKE VARIABLES ######
 if(NOT CMAKE_C_COMPILER)
-	dk_set(CMAKE_C_COMPILER		${COSMO_C_COMPILER})
+	dk_set(CMAKE_C_COMPILER				${COSMO_C_COMPILER})
 endif()
 if(NOT CMAKE_CXX_COMPILER)
-	dk_set(CMAKE_CXX_COMPILER	${COSMO_CXX_COMPILER})
+	dk_set(CMAKE_CXX_COMPILER			${COSMO_CXX_COMPILER})
 endif()
-#dk_set(CMAKE_RC_COMPILER	${COSMO_RC_COMPILER})
+#if(NOT CMAKE_MAKE_PROGRAM)
+#	dk_set(CMAKE_MAKE_PROGRAM			${COSMO_MAKE_PROGRAM})
+#endif()
+if(NOT CMAKE_ASM_COMPILER)
+	dk_set(CMAKE_ASM_COMPILER			${COSMO_ASM_COMPILER})
+endif()
+if(NOT CMAKE_AR)
+	dk_set(CMAKE_AR						${COSMO_AR})
+endif()
+if(NOT CMAKE_RANLIB)
+	dk_set(CMAKE_RANLIB					${COSMO_RANLIB})
+endif()
+if(NOT CMAKE_LINKER)
+	dk_set(CMAKE_LINKER					${COSMO_LINKER})
+endif()
+foreach(lang ASM C CXX)
+	if(NOT CMAKE_${lang}_COMPILER_AR)
+		dk_set(CMAKE_${lang}_COMPILER_AR 		${COSMO_${lang}_COMPILER_AR})
+	endif()
+	if(NOT CMAKE_${lang}_COMPILER_RANLIB)
+		dk_set(CMAKE_${lang}_COMPILER_RANLIB 	${COSMO_${lang}_COMPILER_RANLIB})
+	endif()
+endforeach()
 
-dk_set(DKCONFIGURE_CC		${CMAKE_C_COMPILER})
-dk_set(DKCONFIGURE_CXX		${CMAKE_CXX_COMPILER})
+
+dk_set(DKCONFIGURE_CC					${CMAKE_C_COMPILER})
+dk_set(DKCONFIGURE_CXX					${CMAKE_CXX_COMPILER})
+
+dk_set(COSMO_BASH_EXPORTS				"export PATH=${COSMOCC}/bin:$PATH")
--- 3rdParty/_DKIMPORTS/windows-sdk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/windows-sdk/DKMAKE.cmake
@@ -7,6 +7,12 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 ############ windows-sdk ############
 # https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/
+#	Windows SDK for Windows 11 (10.0.22621.2428)	Released October 2023.
+#	Windows SDK for Windows 11 (10.0.22621.1778)	Released in May 2023.
+#	Windows SDK for Windows 11 (10.0.22621.755)		Released as part of Windows 11, version 22H2. Includes servicing update 10.0.22000.755 on October 25, 2022: ARM64 support 
+#	Windows SDK for Windows 11 (10.0.22000.194)		Released in Windows 11. w/ servicing update 10.0.22000.832 on July 29, 2022: Critical updates for developers building Arm64EC applications
+#	Windows 10 SDK version 2104 (10.0.20348.0)		intended for Windows Server development. For desktop development, see the release notes for changes you may benefit from by updating.	
+
 
 dk_import(https://go.microsoft.com/fwlink/?linkid=2272523)   # iso
 #dk_import(https://go.microsoft.com/fwlink/?linkid=2272610)  # installer
