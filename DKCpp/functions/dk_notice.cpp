e68c8225b9b2517c5c42a962b132f12d54056f40
aquawicket <aquawicket@hotmail.com>
2024-11-12 20:03:15 -0500

    git commit Tue 11/12/2024

---
+++ DKCpp/functions/dk_notice.cpp
@@ -0,0 +1,10 @@
+#include "dk_notice.hpp"
+
+
+//###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+#ifdef DKTEST
+int main() {
+	dk_notice("test dk_notice()");
+    return 0;
+}
+#endif
