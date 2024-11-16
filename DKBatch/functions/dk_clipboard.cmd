30980e644fb873a543f4158e919851d4a3e3adf6
aquawicket <aquawicket@hotmail.com>
2024-11-16 05:23:02 -0500

    git commit Sat 11/16/2024

---
+++ DKBatch/functions/dk_clipboard.cmd
@@ -0,0 +1,43 @@
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+
+::####################################################################
+::# dk_call()
+::#	dk_clipboard(set)
+::#
+::#
+:dk_clipboard
+	call dk_debugFunc 0 1
+ setlocal
+ 
+    if "%~1" equ "" goto clipboard_set
+	    
+	echo|set/p=%~1|clip
+	%return%
+		
+	:clipboard_set
+	%dk_call% dk_commandToVariable "powershell -command ""Get-Clipboard""" rtn_value
+		
+	for /f "delims=" %%Z in ('powershell -Command "Get-Clipboard"') do (
+		set "rtn_value=%%Z"
+	)
+	
+	endlocal & set "dk_clipboard=%rtn_value%"
+%endfunction%
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+    call dk_debugFunc 0
+ setlocal
+
+	:: set the clipboard
+	%dk_call% dk_clipboard "clipboard test"
+
+	:: get the clipboard
+	%dk_call% dk_clipboard
+	%dk_call% dk_echo "clipboard = %dk_clipboard%"
+
+%endfunction%
