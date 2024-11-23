017d0f7c9f0337c9aa58e54c16ee450786ffdbb2
aquawicket <aquawicket@hotmail.com>
2024-11-22 22:55:54 -0500

    git commit

---
+++ DKBatch/functions/dk_registryListKeys.cmd
@@ -0,0 +1,26 @@
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+::#################################################################################
+::# dk_registryListKeys(reg_path)
+::#
+::#
+:dk_registryListKeys
+    call dk_debugFunc 1
+ setlocal EnableDelayedExpansion
+ 
+	for /f "usebackq delims=" %%a in (`reg query %~1`) do (
+		echo %%a
+	)
+%endfunction%
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+    call dk_debugFunc 0
+ setlocal
+    
+    %dk_call% dk_registryListKeys "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules"
+%endfunction%
