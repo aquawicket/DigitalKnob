a27d26c7ecf46126c5928ffdbbb73fde3a020075
aquawicket <aquawicket@hotmail.com>
2024-11-23 01:10:42 -0500

    git commit Sat 11/23/2024

--- DKBuilder.cmd
+++ DKBuilder.cmd
@@ -15,9 +15,9 @@ mkdir "!DKF!" 2>nul
 set "DK=!DKF!\DK.cmd" 
 
 :: firewall
-netsh advfirewall firewall add rule name="powershell" dir=in action=allow program="C:\windows\system32\windowspowershell\v1.0\powershell.exe" enable=yes profile=any
-netsh advfirewall firewall add rule name="curl" dir=in action=allow program="C:\windows\system32\curl.exe" enable=yes profile=any
-netsh advfirewall firewall add rule name="git" dir=in action=allow program="C:\users\administrator\digitalknob\dktools\portablegit-2.46.2-64-bit\mingw64\libexec\git-core\git-remote-https.exe" enable=yes profile=any
+call :dk_firewallAllow "powershell" "C:\windows\system32\windowspowershell\v1.0\powershell.exe"
+call :dk_firewallAllow "curl" "C:\windows\system32\curl.exe"
+call :dk_firewallAllow "git" "C:\users\administrator\digitalknob\dktools\portablegit-2.46.2-64-bit\mingw64\libexec\git-core\git-remote-https.exe"
 
 if not exist !DK! powershell -c "(New-Object Net.WebClient).DownloadFile('!HDK!','!DK!')" >nul 2>&1||certutil -urlcache -split -f "!HDK!" "!DK!" >nul 2>&1||curl -f "!HDK!" -o "!DK!" >nul 2>&1||echo DKINIT Failed
 
@@ -36,3 +36,20 @@ call "%DK%" %~0 %*
 :: DIGITALKNOB_DIR					     		 C:/Users/aquawicket/digitalknob/
 :: DKHTTP_DIGITALKNOB	https://raw.githubusercontent.com/aquawicket/digitalknob/
 :: 
+%return%
+
+
+
+
+
+:dk_firewallAllow
+	::call dk_debugFunc 2
+
+	%dk_call% dk_registryContains "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" "%~2" && (
+		%dk_call% dk_warning "registry already contains a firewall rule for %~2"
+		%return%
+	)
+	
+	netsh advfirewall firewall add rule name="%~1" dir=in action=allow program="%~2" enable=yes profile=any
+	netsh advfirewall firewall add rule name="%~1" dir=out action=allow program="%~2" enable=yes profile=any
+%endfunction%
