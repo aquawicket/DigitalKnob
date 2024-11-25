12aa29fb87941020fa6303e56d6c59f1482f33dc
aquawicket <aquawicket@hotmail.com>
2024-11-25 12:14:45 -0500

    git commit Mon 11/25/2024

--- DKBatch/functions/dk_DKHOME_DIR.cmd
+++ DKBatch/functions/dk_DKHOME_DIR.cmd
@@ -6,37 +6,27 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 ::#
 ::#
 :dk_DKHOME_DIR
-	echo dk_DKHOME_DIR.cmd
     call dk_debugFunc 0 1
-	
-
 :: setlocal
 
-    ::if defined DKHOME_DIR %return%
-    
 	::############ SET ############
-::	if "%~1" neq "" ( 
-::		set "DKHOME_DIR=%~1"
-::		%return%
-::	)
-	
-	
+	if "%~1" neq "" ( 
+		set "DKHOME_DIR=%~1"
+	) else (
+
 	rem ############ GET ############
+		rem ###### WSLPATH_EXE ######
+		rem %dk_call% dk_set WSLPATH_EXE   "%WSL_EXE% wslpath"
+		rem %dk_call% dk_printVar WSLPATH_EXE
 
-	rem ###### WSLPATH_EXE ######
-	rem %dk_call% dk_set WSLPATH_EXE   "%WSL_EXE% wslpath"
-	rem %dk_call% dk_printVar WSLPATH_EXE
+		rem if not defined HOMEDRIVE        !dk_call! dk_warning "HOMEDRIVE is invalid"
+		rem if not defined HOMEPATH         !dk_call! dk_warning "HOMEPATH is invalid"
+		set "DKHOME_DIR=!USERPROFILE!"
+		rem	if exist "!WSLPATH_EXE!"        !dk_call! dk_commandToVariable "!WSLPATH_EXE! -u !DKHOME_DIR!" DKHOME_DIR
 
-	rem if not defined HOMEDRIVE        !dk_call! dk_warning "HOMEDRIVE is invalid"
-	rem if not defined HOMEPATH         !dk_call! dk_warning "HOMEPATH is invalid"
-		
-	rem ### DKHOME_DIR ###
-	set "DKHOME_DIR=!USERPROFILE!"
-		
-::	if exist "!WSLPATH_EXE!"        !dk_call! dk_commandToVariable "!WSLPATH_EXE! -u !DKHOME_DIR!" DKHOME_DIR
-
-	call dk_assertPath DKHOME_DIR
-	call dk_printVar DKHOME_DIR
+		!dk_call! dk_assertPath DKHOME_DIR
+		!dk_call! dk_printVar DKHOME_DIR
+	)
 %endfunction%
 
 
