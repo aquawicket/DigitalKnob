23a4ad1e884a29d25b4ec28f4e9ac951b29689bc
aquawicket <aquawicket@hotmail.com>
2024-11-29 14:59:08 -0500

    git commit Fri 11/29/2024

--- DKApps/DKCore/DKMAKE.cmake
+++ DKApps/DKCore/DKMAKE.cmake
@@ -7,26 +7,26 @@ dk_depend(DK)
 
 
 #[[ Work In Progress
-#!/usr/bin/cmake -P
-if(NOT DKCMAKE_FUNCTIONS_DIR_)
-	set(DKCMAKE_FUNCTIONS_DIR_ ${CMAKE_SOURCE_DIR}/../../DKCMake/functions/)
-endif()
-include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+	#!/usr/bin/cmake -P
+	if(NOT DKCMAKE_FUNCTIONS_DIR_)
+		set(DKCMAKE_FUNCTIONS_DIR_ ${CMAKE_SOURCE_DIR}/../../DKCMake/functions/)
+	endif()
+	include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
-############ CLEAR CMAKE CACHE ############
-#dk_clearCmakeCache()
+	############ CLEAR CMAKE CACHE ############
+	#dk_clearCmakeCache()
 
 
-############ GENERATE APP CMAKE ############
-set(DK_PROJECT_DIR ${CMAKE_SOURCE_DIR})
-dk_generateAppCmake(${DK_PROJECT_DIR} "zlib;DK")
+	############ GENERATE APP CMAKE ############
+	set(DK_PROJECT_DIR ${CMAKE_SOURCE_DIR})
+	dk_generateAppCmake(${DK_PROJECT_DIR} "zlib;DK")
 
 
-############ CONFIGURE APP ############
-dk_configure(${DK_PROJECT_DIR} -DDKCMAKE_FUNCTIONS_DIR=${DKCMAKE_FUNCTIONS_DIR} -DTRIPLE=${TRIPLE})
+	############ CONFIGURE APP ############
+	dk_configure(${DK_PROJECT_DIR} -DDKCMAKE_FUNCTIONS_DIR=${DKCMAKE_FUNCTIONS_DIR} -DTRIPLE=${TRIPLE})
 
 
-############ BUILD APP ############
-dk_build(${DK_PROJECT_DIR})
+	############ BUILD APP ############
+	dk_build(${DK_PROJECT_DIR})
 ]]
