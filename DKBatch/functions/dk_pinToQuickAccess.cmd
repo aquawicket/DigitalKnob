02afb8ad13791aebea3fd5a625ae0f8394ae8eb7
aquawicket <aquawicket@hotmail.com>
2024-11-29 21:48:18 -0500

    git commit Fri 11/29/2024

--- DKBatch/functions/dk_pinToQuickAccess.cmd
+++ DKBatch/functions/dk_pinToQuickAccess.cmd
@@ -9,12 +9,11 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
     call dk_debugFunc 1
   setlocal
   
-	:: METHOD 1:  use dk_powershell
-	::%dk_call" dk_powershell "if(-not ($(New-Object -ComObject:Shell.Application).Namespace('shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}').Items() | ? {$_.Path -eq '%~1'})){$(New-Object -ComObject:Shell.Application).Namespace('%~1').Self.InvokeVerb('pintohome')}"
-
-    :: METHOD 2:  call the dk_callDKPowershell function
+	:: METHOD 1:  call the dk_callDKPowershell function
     %dk_call% dk_callDKPowershell dk_pinToQuickAccess %*
 	
+	:: METHOD 2:  use dk_powershell
+	::%dk_call" dk_powershell "if(-not ($(New-Object -ComObject:Shell.Application).Namespace('shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}').Items() | ? {$_.Path -eq '%~1'})){$(New-Object -ComObject:Shell.Application).Namespace('%~1').Self.InvokeVerb('pintohome')}"
 %endfunction%
 
 
