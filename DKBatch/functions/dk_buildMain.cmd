96efdab5d6a4db0d6ee33ae01220acea48a00408
aquawicket <aquawicket@hotmail.com>
2024-11-29 20:27:30 -0500

    git commit Fri 11/29/2024

--- DKBatch/functions/dk_buildMain.cmd
+++ DKBatch/functions/dk_buildMain.cmd
@@ -10,7 +10,7 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
  setlocal
 
 	%dk_call% dk_validate DIGITALKNOB_DIR "%dk_call% dk_DIGITALKNOB_DIR"
-	%dk_call% dk_createShortcut "%DKDESKTOP_DIR%\digitalknob.lnk" "%DIGITALKNOB_DIR%" OVERWRITE
+	%dk_call% dk_createShortcut "%DKDESKTOP_DIR%\digitalknob.lnk" "%DIGITALKNOB_DIR%"   &::OVERWRITE
 	%dk_call% dk_pinToQuickAccess "%DIGITALKNOB_DIR%"
 
 	%dk_call% dk_assertPath DKSCRIPT_DIR
