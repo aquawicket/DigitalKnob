624ad57ce0ceb31028df661097f68977325d3ff4
aquawicket <aquawicket@hotmail.com>
2024-12-02 21:29:34 -0500

    git commit Mon 12/02/2024

---
+++ 3rdParty/_DKIMPORTS/blissos/DKMAKE.cmake
@@ -0,0 +1,11 @@
+#!/usr/bin/cmake -P
+if(NOT DKCMAKE_FUNCTIONS_DIR_)
+	set(DKCMAKE_FUNCTIONS_DIR_ ${CMAKE_SOURCE_DIR}/../../../DKCMake/functions/)
+endif()
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+
+
+
+############ blissOS ############
+dk_import(https://cytranet-dal.dl.sourceforge.net/project/blissos-x86/OTA/BlissOS14/FOSS/Surface/Bliss-Surface-v14.10.3-x86_64-OFFICIAL-foss-20241013.zip?viasf=1)
+
