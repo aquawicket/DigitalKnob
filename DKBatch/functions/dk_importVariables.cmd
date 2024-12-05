29a955cbcc9f974b2d373e5dd3cc644d4a0ac006
aquawicket <aquawicket@hotmail.com>
2024-12-04 21:11:54 -0500

    git commit Wed 12/04/2024

--- DKBatch/functions/dk_importVariables.cmd
+++ DKBatch/functions/dk_importVariables.cmd
@@ -205,7 +205,7 @@ if not defined DKINIT call "!DKBATCH_FUNCTIONS_DIR_!DK.cmd" %~0 %*
 	if defined PLUGIN_GIT (
 		rem # PLUGIN_GIT_FILENAME
 		set "PLUGIN_GIT_FILENAME="
-        !dk_call! dk_arrayAt PLUGIN_URL_ARRAY 3 PLUGIN_GIT_FILENAME									
+        !dk_call! array\dk_at PLUGIN_URL_ARRAY 3 PLUGIN_GIT_FILENAME									
 		!dk_call! dk_printVar PLUGIN_GIT_FILENAME 								&rem PLUGIN_GIT_FILENAME		: zlib
 		
 		rem # PLUGIN_GIT_NAME
