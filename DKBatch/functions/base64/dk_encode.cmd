026bcfd9c75cb314f5a229bede71092e8c7dd914
aquawicket <aquawicket@hotmail.com>
2024-12-04 20:34:32 -0500

    git commit Wed 12/04/2024

---
+++ DKBatch/functions/base64/dk_encode.cmd
@@ -0,0 +1,45 @@
+@echo off
+if not exist "%DKBATCH_FUNCTIONS_DIR_%" set "DKBATCH_FUNCTIONS_DIR_=..\"
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+
+::##################################################################################
+::# dk_fileB64Encode(inputFile)
+::# dk_fileB64Encode(inputFile, outputFile)
+::#
+::#    Encode input to base-64 output
+::#    https://github.com/base64code/examples
+::#
+:dk_fileB64Encode
+    call dk_debugFunc 1 2
+ setlocal
+ 
+    set "inputFile=%~1"
+    set "outputFile=%inputFile%.b64"
+    if %__ARGC__% equ 2 set "outputFile=%~2"
+    
+    if not exist "%inputFile%" %dk_call% dk_error "%inputFile% not found"
+    if exist "%outputFile%" %dk_call% dk_error "%outputFile% already exists and cannot be overwritten"
+    
+    certutil -encode -f "%inputFile%" "%outputFile%.tmp" 1>nul
+    type "%outputFile%.tmp"|find /v "CERTIFICATE-----">"%outputFile%"
+    del "%outputFile%.tmp"
+%endfunction%
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+    call dk_debugFunc 0
+ setlocal
+ 
+    ::%dk_call% dk_validate DKBRANCH_DIR "%dk_call% dk_DKBRANCH_DIR"
+    ::set "input=%DKBRANCH_DIR%\DKBuilder.cmd"
+    ::set "output=%DKBRANCH_DIR%\DKBuilder.cmd.b64"
+    
+    %dk_call% dk_selectFile input
+    %dk_call% dk_fileB64Encode "%input%"
+    ::%dk_call% dk_fileB64Encode "%input%" "test.b64"
+%endfunction%
--- DKBatch/functions/dk_call.cmd
+++ DKBatch/functions/dk_call.cmd
@@ -9,7 +9,7 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 :dk_call
     ::if exist "%DKBATCH_FUNCTIONS_DIR%\dk_debugFunc.cmd" call dk_debugFunc 1 99
  ::setlocal
-    
+   
     if "%~1"=="dk_debugFunc" echo [31m ERROR: dk_call cannot be used with dk_debugFunc [0m & %return%
 
     set "arg=%~1"
