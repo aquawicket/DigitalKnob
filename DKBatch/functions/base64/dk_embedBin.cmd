e8f75a909354cdc57d8bce45c92434cddc11b486
aquawicket <aquawicket@hotmail.com>
2024-12-09 11:52:53 -0500

    git commit Mon 12/09/2024

---
+++ DKBatch/functions/base64/dk_embedBin.cmd
@@ -0,0 +1,56 @@
+@echo off
+if not exist "%DKBATCH_FUNCTIONS_DIR_%" set "DKBATCH_FUNCTIONS_DIR_=..\"
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+::##################################################################################
+::# base64\dk_embedBin(inputFile)
+::# base64\dk_embedBin(inputFile, outputFile)
+::#
+::#    Encode input to base-64 output
+::#    https://github.com/base64code/examples
+::#
+:dk_embedBin
+    call dk_debugFunc 1 2
+ setlocal
+	
+    set "inputFile=%~1"
+	set "inputFilename=%~nx1"
+	set "outputFile=%~nx1.cmd"
+    if %__ARGC__% equ 2 set "outputFile=%~2"
+    
+    if not exist "%inputFile%" %dk_call% dk_error "%inputFile% not found"
+	if exist "%outputFile%" del %outputFile%
+    :: if exist "%outputFile%" %dk_call% dk_error "%outputFile% already exists and cannot be overwritten"
+    
+	set "tempCmd=%inputFile%.tmp"
+	(
+	echo ;;;===,,,@echo off
+	echo ;;;===,,,echo line2
+	echo ;;;===,,,findstr /v "^;;;===,,," "%%~f0" ^> %inputFilename%
+	echo ;;;===,,,%inputFilename%
+	echo ;;;===,,,exit /b
+
+	) >"%tempCmd%"
+	
+    copy /a %tempCmd% + /b %inputFile% /b %outputFile%
+	::del %tempCmd%
+%endfunction%
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+    call dk_debugFunc 0
+ setlocal
+ 
+    ::%dk_call% dk_validate DKBRANCH_DIR "%dk_call% dk_DKBRANCH_DIR"
+    ::set "input=%DKBRANCH_DIR%\DKBuilder.cmd"
+    ::set "output=%DKBRANCH_DIR%\DKBuilder.cmd.b64"
+	
+    %dk_call% dk_selectFile input
+    call dk_embedBin "%input%"
+	
+    ::%dk_call% base64.dk_encode "%input%" "test.b64"
+%endfunction%
