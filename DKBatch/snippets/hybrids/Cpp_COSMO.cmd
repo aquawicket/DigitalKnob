4098590eb3e84f45e48e621d3c21fbcc1f2a51df
aquawicket <aquawicket@hotmail.com>
2024-12-09 12:50:54 -0500

    git commit Mon 12/09/2024

--- DKBatch/snippets/hybrids/Cpp_COSMO.cmd
+++ DKBatch/snippets/hybrids/Cpp_COSMO.cmd
@@ -3,18 +3,36 @@
 	setlocal
 	cls
 	echo compiling %~n0.exe. . .																							 
-	set "outfile=%~dpn0.exe"
-	call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" > nul
-	"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.42.34433\bin\Hostx64\x64\cl.exe" %~dpf0 /TP /EHsc /link /out:%outfile% /nologo > nul
-	if not exist %outfile% (
+	set "SH_EXE=C:\Users\Administrator\digitalknob\DKTools\git-portable-2.46.2-64-bit\bin\bash.exe"
+	#set "COSMO_C_COMPILER=/C/Users/Administrator/digitalknob/Development/3rdParty/cosmocc-master/bin/cosmocc"
+	set "COSMO_C_COMPILER=/C/Users/Administrator/digitalknob/Development/3rdParty/cosmocc-master/bin/x86_64-unknown-cosmo-c++"
+
+	for %%Z in ("%~0") do set "APP_NAME=%%~nZ"
+	copy %~0 "%~n0.c"
+	set "C_FILE=%~n0.c"
+	set "COMPILER_EXE=%SH_EXE% %COSMO_C_COMPILER%"
+	set "APP_EXE=%CD%\build\%APP_NAME%.exe"
+	set "COMPILE_COMMAND=%COMPILER_EXE% -DDKTEST=1 -o %APP_EXE% %C_FILE%"
+	
+	::###### Setup build directory
+	if not exist "%CD%\build" mkdir "%CD%\build"
+	
+	::###### Compile Code ######
+	echo compiling ...
+	if exist %APP_EXE%  del %APP_EXE%
+
+	echo %COMPILE_COMMAND%
+	%COMPILE_COMMAND%
+	if not exist %APP_EXE% (
 		echo Compilation failed!
 		exit /b %ERRORLEVEL%
 	)
+	del %C_FILE%
+	
 	cls 
-	del "%~dpn0.obj"
-	%outfile%
+	%APP_EXE%
 	set exit_code=%ERRORLEVEL%
-	del %outfile%
+
 	echo exit_code = %exit_code% & pause
 	exit /b %exit_code%
 */
