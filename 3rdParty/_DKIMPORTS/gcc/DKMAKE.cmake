b2df62f6a513eebb360cf1d5fb786cb3bbd3e1fe
aquawicket <aquawicket@hotmail.com>
2024-12-14 23:12:50 -0500

    git commit Sat 12/14/2024

--- 3rdParty/_DKIMPORTS/gcc/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/gcc/DKMAKE.cmake
@@ -7,7 +7,7 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 # https://packages.msys2.org/package/mingw-w64-x86_64-gcc?repo=mingw64
 
-dk_load(dk_builder)
+dk_validate(target_triple "dk_target_triple")
 
 if(clang OR mingw OR ucrt)
 	dk_depend(msys2)
@@ -16,55 +16,55 @@ endif()
 dk_installPackage(gcc)
 dk_installPackage(g++)
 
-if(win_x86_clang)
-	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/clang32/bin/gcc.exe)
-	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/clang32/bin/g++.exe)
-	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/clang32/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
-elseif(win_x86_64_clang)
-	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/clang64/bin/gcc.exe)
-	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/clang64/bin/g++.exe)
-	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/clang64/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
-elseif(win_arm64_clang)
-	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/clangarm64/bin/gcc.exe)
-	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/clangarm64/bin/g++.exe)
-	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/clangarm64/bin/windres.exe)	# TODO:  move to DKIMPORTS/windres
-elseif(win_x86_mingw)
-	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/mingw32/bin/gcc.exe)
-	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/mingw32/bin/g++.exe)
-	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/mingw32/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
-elseif(win_x86_64_mingw)
-	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/mingw64/bin/gcc.exe)
-	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/mingw64/bin/g++.exe)
-	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/mingw64/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
-elseif(win_x86_64_ucrt)
-	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/ucrt64/bin/gcc.exe)
-	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/ucrt64/bin/g++.exe)
-	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/ucrt64/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
-elseif(LINUX_HOST)
-	if(EXISTS /usr/bin/gcc)
-		dk_set(GCC_C_COMPILER	/usr/bin/gcc)
-	elseif(EXISTS /usr/local/bin/gcc)
-		dk_set(GCC_C_COMPILER	/usr/local/bin/gcc)
-	endif()
-
-	if(EXISTS /usr/bin/g++)
-		dk_set(GCC_CXX_COMPILER	/usr/bin/g++)
-	elseif(EXISTS /usr/local/bin/g++)
-		dk_set(GCC_CXX_COMPILER	/usr/local/bin/g++)
-	endif()
-endif()
+#if(win_x86_clang)
+#	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/clang32/bin/gcc.exe)
+#	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/clang32/bin/g++.exe)
+#	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/clang32/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
+#elseif(win_x86_64_clang)
+#	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/clang64/bin/gcc.exe)
+#	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/clang64/bin/g++.exe)
+#	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/clang64/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
+#elseif(win_arm64_clang)
+#	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/clangarm64/bin/gcc.exe)
+#	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/clangarm64/bin/g++.exe)
+#	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/clangarm64/bin/windres.exe)	# TODO:  move to DKIMPORTS/windres
+#elseif(win_x86_mingw)
+#	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/mingw32/bin/gcc.exe)
+#	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/mingw32/bin/g++.exe)
+#	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/mingw32/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
+#elseif(win_x86_64_mingw)
+#	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/mingw64/bin/gcc.exe)
+#	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/mingw64/bin/g++.exe)
+#	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/mingw64/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
+#elseif(win_x86_64_ucrt)
+#	dk_set(GCC_C_COMPILER   	${MSYS2_DIR}/ucrt64/bin/gcc.exe)
+#	dk_set(GCC_CXX_COMPILER 	${MSYS2_DIR}/ucrt64/bin/g++.exe)
+#	dk_set(GCC_RC_COMPILER  	${MSYS2_DIR}/ucrt64/bin/windres.exe)		# TODO:  move to DKIMPORTS/windres
+#elseif(LINUX_HOST)
+#	if(EXISTS /usr/bin/gcc)
+#		dk_set(GCC_C_COMPILER	/usr/bin/gcc)
+#	elseif(EXISTS /usr/local/bin/gcc)
+#		dk_set(GCC_C_COMPILER	/usr/local/bin/gcc)
+#	endif()
+#
+#	if(EXISTS /usr/bin/g++)
+#		dk_set(GCC_CXX_COMPILER	/usr/bin/g++)
+#	elseif(EXISTS /usr/local/bin/g++)
+#		dk_set(GCC_CXX_COMPILER	/usr/local/bin/g++)
+#	endif()
+#endif()
 
 
 
 
 ###### set GLOBAL CMAKE VARIABLES ######
-if(NOT CMAKE_C_COMPILER)
-	dk_set(CMAKE_C_COMPILER		${GCC_C_COMPILER})
-endif()
-if(NOT CMAKE_CXX_COMPILER)
-	dk_set(CMAKE_CXX_COMPILER	${GCC_CXX_COMPILER})
-endif()
-dk_set(CMAKE_RC_COMPILER	${GCC_RC_COMPILER})			# TODO:  move to DKIMPORTS/windres
-
-dk_set(DKCONFIGURE_CC		${CMAKE_C_COMPILER})
-dk_set(DKCONFIGURE_CXX		${CMAKE_CXX_COMPILER})
+#if(NOT CMAKE_C_COMPILER)
+#	dk_set(CMAKE_C_COMPILER		${GCC_C_COMPILER})
+#endif()
+#if(NOT CMAKE_CXX_COMPILER)
+#	dk_set(CMAKE_CXX_COMPILER	${GCC_CXX_COMPILER})
+#endif()
+#dk_set(CMAKE_RC_COMPILER	${GCC_RC_COMPILER})			# TODO:  move to DKIMPORTS/windres
+#
+#dk_set(DKCONFIGURE_CC		${CMAKE_C_COMPILER})
+#dk_set(DKCONFIGURE_CXX		${CMAKE_CXX_COMPILER})
