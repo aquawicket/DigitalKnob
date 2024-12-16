0c6656c043c52502c573a7b9955dfce38ceec7f8
aquawicket <aquawicket@hotmail.com>
2024-12-16 02:14:19 -0500

    git commit Mon 12/16/2024

--- DKBatch/functions/dk_source.cmd
+++ DKBatch/functions/dk_source.cmd
@@ -21,8 +21,10 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
     
     echo downloading %~1.cmd to %DKBATCH_FUNCTIONS_DIR_%%~1.cmd
     
-	%dk_call% dk_dirname %DKBATCH_FUNCTIONS_DIR_%%~1.cmd source_dir
-	%dk_call% dk_makeDirectory %source_dir%
+	::%dk_call% dk_dirname %DKBATCH_FUNCTIONS_DIR_%%~1.cmd source_dir
+	for %%Z in ("%DKBATCH_FUNCTIONS_DIR_%%~1.cmd") do set "_dirname_=%%~dpZ"
+	if not exist %_dirname_%   mkdir %_dirname_%
+	::%dk_call% dk_makeDirectory %source_dir%
 	
     :: FIXME: causes infinate recursion loop
     :: Try dk_download
