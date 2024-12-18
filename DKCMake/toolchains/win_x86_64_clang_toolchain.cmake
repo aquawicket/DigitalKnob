c1dc903766799f8a0f3ebe8c369d0c4b38a20f25
aquawicket <aquawicket@hotmail.com>
2024-12-18 09:03:13 -0500

    git commit

--- DKCMake/toolchains/win_x86_64_clang_toolchain.cmake
+++ DKCMake/toolchains/win_x86_64_clang_toolchain.cmake
@@ -15,6 +15,6 @@ dk_set(CMAKE_RC_COMPILER  			"${${MSYSTEM}_BIN}/windres.exe")
 
 dk_append(CMAKE_C_FLAGS				-march=x86-64 -DMSYSTEM=${MSYSTEM} -DWIN -DWIN_X86_64 -D_WINDOWS -D_CRT_SECURE_NO_WARNINGS -D_USING_V110_SDK71_ -std=gnu17)
 dk_append(CMAKE_CXX_FLAGS			-march=x86-64 -DMSYSTEM=${MSYSTEM} -DWIN -DWIN_X86_64 -D_WINDOWS -D_CRT_SECURE_NO_WARNINGS -D_USING_V110_SDK71_ -std=gnu++17)
-dk_append(CMAKE_EXE_LINKER_FLAGS	-static) # -s)
+dk_append(CMAKE_EXE_LINKER_FLAGS	-static) # -mconsole -s)
 
 dk_append(DKCONFIGURE_FLAGS			--build=x86_64-w64-mingw32)
