1a1d4fe0d88649b39b0f9fbfb68062b3efe489bc
aquawicket <aquawicket@hotmail.com>
2024-12-23 06:35:01 -0500

    git commit Mon 12/23/2024

---
+++ DKCpp/functions/dk_basename.cpp
@@ -0,0 +1,20 @@
+#include "dk_basename.hpp"
+
+
+//###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+#ifndef DKMAIN
+#define DKMAIN = 1
+int main(int argc, char **argv) {
+	
+	/// Get DKSCRIPT_PATH
+	char* DKSCRIPT_PATH = argv[0];
+	printf("DKSCRIPT_PATH = %s\n", DKSCRIPT_PATH);
+	
+	/// Get DKSCRIPT_NAME
+	char* DKSCRIPT_NAME;
+	int rtn_code = dk_basename(DKSCRIPT_PATH, &DKSCRIPT_NAME);
+	printf("DKSCRIPT_NAME = %s\n", DKSCRIPT_NAME);
+	
+    return rtn_code;
+}
+#endif
---
+++ DKCpp/functions/dk_basename.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef dk_basename_hpp
+#define dk_basename_hpp
+
+#include "DK.hpp"
+
+
+//################################################################################
+//# dk_basename()
+//#
+#include <string.h>
+#include <stdio.h>
+#include <libgen.h>
+int dk_basename(char* path, char** _basename_){
+	//dk_debugFunc(1);
+	
+	// Method 1
+//	*_basename_ = basename(path);
+	
+	// Method 2
+	char *base = strrchr(path, '/');
+    if (base != NULL) {
+        base++; // Move pointer past the last '/'
+    } else {
+        base = path; // If no '/', the entire string is the basename
+    }
+	*_basename_ = base;
+	
+	return 0;
+};
+
+#endif //dk_basename_hpp
