8189e5e0b7a06e41c0f6011a4e9d4a6c84821187
aquawicket <aquawicket@hotmail.com>
2024-12-26 23:18:15 -0500

    git commit Thu 12/26/2024

--- DKBatch/functions/dk_validateDK.cmd
+++ DKBatch/functions/dk_validateDK.cmd
@@ -6,9 +6,9 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 ::#
 ::#
 :dk_validateDK
- ::setlocal
     call dk_debugFunc 0
-	
+ ::setlocal
+ 
 	%dk_call% dk_validate DIGITALKNOB_DIR "%dk_call% dk_DIGITALKNOB_DIR"
 	if not defined DKBRANCH          set "DKBRANCH=Development"
 	if not defined DKBRANCH_DIR      set "DKBRANCH_DIR=%DIGITALKNOB_DIR%\%DKBRANCH%"
@@ -18,10 +18,10 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
 
     if not exist "%DKBRANCH_DIR%\.git"  %dk_call% dk_gitUpdate https://github.com/aquawicket/DigitalKnob.git %DKBRANCH%
 	
-	if "%DKSCRIPT_NAME%" equ "DKBuilder" set "DKSCRIPT_PATH=%DKBRANCH_DIR%\%DKSCRIPT_NAME%%DKSCRIPT_EXT%"
-	if not exist %DKSCRIPT_PATH%  %dk_call% dk_fatal "DKSCRIPT_PATH:%DKSCRIPT_PATH% does not exist" && %return%
+	if "%DKSCRIPT_NAME%" == "DKBuilder" set "DKSCRIPT_PATH=%DKBRANCH_DIR%\%DKSCRIPT_NAME%%DKSCRIPT_EXT%"
+	if not exist %DKSCRIPT_PATH%  %dk_call% dk_fatal "DKSCRIPT_PATH:%DKSCRIPT_PATH% does not exist"
 	
-	if "%DKSCRIPT_NAME%" equ "DKBuilder" set "DKSCRIPT_ARGS=%DKSCRIPT_PATH%"
+	if "%DKSCRIPT_NAME%" == "DKBuilder" set "DKSCRIPT_ARGS=%DKSCRIPT_PATH%"
 	for %%Z in ("%DKSCRIPT_PATH%") do set "DKSCRIPT_DIR=%%~dpZ"
 	if [%DKSCRIPT_DIR:~-1%] == [\] set "DKSCRIPT_DIR=%DKSCRIPT_DIR:~0,-1%"
 	if [%DKSCRIPT_DIR:~-1%] == [/] set "DKSCRIPT_DIR=%DKSCRIPT_DIR:~0,-1%"
