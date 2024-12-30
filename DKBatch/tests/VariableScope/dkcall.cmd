d0ab276bfdc08b5b4806cdb219c4a6bc6ce353f1
aquawicket <aquawicket@hotmail.com>
2024-12-30 13:33:29 -0500

    git commit Mon 12/30/2024

--- DKBatch/tests/VariableScope/dkcall.cmd
+++ DKBatch/tests/VariableScope/dkcall.cmd
@@ -8,28 +8,18 @@
 	set "comand=%~1"
 	set "args=%*"
 	set "args=!args:%~1=!"
-
 	(set _1=%~1)
 	(set f1=%~f1)
 	(set n1=%~n1)
 	(set x1=%~x1)
 	(set np=%~p1)
-	
-	call setGlobal "STACK_%lvl%" "%comand% %args%"
-	
 	(set mark=-----)
 	(set indent=)
 	for /l %%x in (1, 1, %lvl%) do set "indent=!indent!%mark%"
-	
+	call setGlobal "STACK_%lvl%" "%comand% %args%"
 	
 	echo:
 	echo %indent%^> %comand%(%args%)
-	
-	::echo %indent% lvl = %lvl%
-	::echo %indent% local = %LOCAL_VAR%
-	::echo %indent% globalVar = %globalVarA%
-	::echo %indent% globalVarB = %globalVarB%
-	
 	if defined STACK_0 (echo 0: !STACK%_0!)
 	if defined STACK_1 (echo 1: !STACK%_1!)
 	if defined STACK_2 (echo 2: !STACK%_2!)
@@ -40,31 +30,16 @@
 	call %comand% %args%
 	:::::::::::::::::::::::::::::
 	
-	echo:
-	
-	
-	
 	call setGlobal STACK_%lvl% ""
 	(set /a lvl-=1)
 	call GLOBAL.cmd
 	
+	echo:
 	echo ^<%indent% %comand%(%args%)
 	if defined STACK_0 (echo 0: !STACK%_0!)
 	if defined STACK_1 (echo 1: !STACK%_1!)
 	if defined STACK_2 (echo 2: !STACK%_2!)
 	if defined STACK_3 (echo 3: !STACK%_3!)
 	if defined STACK_4 (echo 4: !STACK%_4!)
-	
-	
-	
-	::echo:
-	
-	
-	::echo %indent% lvl = %lvl%
-	::echo %indent% local = %LOCAL_VAR%
-	::echo %indent% globalVarA = %globalVarA%
-	::echo %indent% globalVarB = %globalVarB%
-	
-	
-	
+
 exit /b 0
--- DKBatch/tests/VariableScope/main.cmd
+++ DKBatch/tests/VariableScope/main.cmd
@@ -1,16 +1,11 @@
 @echo off
 echo ^@echo off > GLOBAL.cmd
-setlocal enableDelayedExpansion
-
-call :main
-exit /b 0
-
-
+if not defined dkcall  set "dkcall=call dkcall"
+if not defined lvl (set /a lvl=0)
 
 :main
 setlocal enableDelayedExpansion
-	if not defined dkcall  set "dkcall=call dkcall"
-	if not defined lvl (set /a lvl=0)
+
 	call setGlobal "STACK_0" "main"
 	
 	call setGlobal globalVarA GLOBALA
