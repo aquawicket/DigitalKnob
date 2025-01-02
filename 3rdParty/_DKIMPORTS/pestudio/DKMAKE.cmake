700dcffa927e28bf46f2314fc88cb68eb4b6c32d
aquawicket <aquawicket@hotmail.com>
2025-01-01 19:20:00 -0500

    git commit Wed 01/01/2025

---
+++ 3rdParty/_DKIMPORTS/pestudio/DKMAKE.cmake
@@ -0,0 +1,12 @@
+#!/usr/bin/cmake -P
+if(NOT DKCMAKE_FUNCTIONS_DIR_)
+	set(DKCMAKE_FUNCTIONS_DIR_ ../../../DKCMake/functions/)
+endif()
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+
+
+
+############ pestudio ############
+# https://www.winitor.com/
+# https://www.winitor.com/tools/pestudio/current/pestudio-9.59.zip
+dk_import(https://www.winitor.com/tools/pestudio/current/pestudio-9.59.zip)
