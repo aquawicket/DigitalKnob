855fd488a7d3079e1f240fc3fee906fcb03fb435
aquawicket <aquawicket@hotmail.com>
2025-01-03 17:12:08 -0500

    git commit Fri 01/03/2025

---
+++ DKBash/functions/dk_timeout.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+[ -z "${DKINIT-}" ] && . "${DKBASH_FUNCTIONS_DIR_-}DK.sh"
+
+##################################################################################
+# dk_timeout(seconds)
+#
+dk_timeout() {
+	dk_debugFunc 0 1
+	
+    [ -n "${1-}" ] && seconds="${1}" || seconds="5"
+	echo ""
+	read -t ${seconds} -n 1 -s -r -p "waiting ${seconds} seconds. Press any key to continue . . ." && return 0 || return 0
+}
+
+
+
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+DKTEST() {
+	dk_debugFunc 0
+	
+	dk_call dk_timeout 1
+	dk_call dk_timeout 2
+	dk_call dk_timeout 3
+	dk_call dk_timeout 4
+	dk_call dk_timeout 5
+	dk_call dk_timeout
+}
