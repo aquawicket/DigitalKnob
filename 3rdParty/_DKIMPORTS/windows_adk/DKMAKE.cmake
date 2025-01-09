47caef5cabaa98dc2cce0c73cbcedf2c7c5b2807
aquawicket <aquawicket@hotmail.com>
2025-01-09 10:47:32 -0500

    git commit Thu 01/09/2025

--- 3rdParty/_DKIMPORTS/windows_adk/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/windows_adk/DKMAKE.cmake
@@ -9,7 +9,63 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 # https://learn.microsoft.com/en-us/windows-hardware/get-started/adk-install
 
 
-dk_validate(DKDOWNLOAD_DIR "dk_DKDOWNLOAD_DIR()")
-dk_download(https://go.microsoft.com/fwlink/?linkid=2289980 "${DKDOWNLOAD_DIR}/adksetup.exe")
-dk_download(https://go.microsoft.com/fwlink/?linkid=2289981 "${DKDOWNLOAD_DIR}/adkwinpesetup.exe")
+
+
+
+
+###### Download the Windows ADK 10.1.26100.2454 (December 2024) ######
+# https://download.microsoft.com/download/2/d/9/2d9c8902-3fcd-48a6-a22a-432b08bed61e/ADK/adksetup.exe
+#if(NOT EXISTS "C:/Program Files (x86)/Windows Kits/10/Assessment and Deployment Kit")
+dk_validate(DKTOOLS_DIR "dk_DKTOOLS_DIR()")
+if(NOT EXISTS "${DKTOOLS_DIR}/ADK")
+	dk_validate(DKDOWNLOAD_DIR "dk_DKDOWNLOAD_DIR()")
+	if(NOT EXISTS "${DKDOWNLOAD_DIR}/ADK")
+		dk_download(https://go.microsoft.com/fwlink/?linkid=2289980 "${DKDOWNLOAD_DIR}/adksetup.exe")
+		execute_process(COMMAND cmd /c "${DKDOWNLOAD_DIR}/adksetup.exe" /layout "${DKDOWNLOAD_DIR}/ADK" /q)
+	endif()
+
+	dk_echo("Installing Windows ADK . . .  please wait")
+	execute_process(COMMAND cmd /c "${DKDOWNLOAD_DIR}/ADK/adksetup.exe" /installpath "${DKTOOLS_DIR}/ADK" /q)
+endif()
+
+
+
+###### Download the Windows PE add-on for the Windows ADK 10.1.26100.2454 (December 2024) ######
+# https://download.microsoft.com/download/5/5/6/556e01ec-9d78-417d-b1e1-d83a2eff20bc/ADKWinPEAddons/adkwinpesetup.exe
+#if(NOT EXISTS "C:/Program Files (x86)/Windows Kits/10/Assessment and Deployment Kit/Windows Preinstallation Environment")
+dk_validate(DKTOOLS_DIR "dk_DKTOOLS_DIR()")
+if(NOT EXISTS "${DKTOOLS_DIR}/ADKWinPEAddons")
+	dk_validate(DKDOWNLOAD_DIR "dk_DKDOWNLOAD_DIR()")
+	if(NOT EXISTS "${DKDOWNLOAD_DIR}/ADKWinPEAddons")
+		dk_download(https://go.microsoft.com/fwlink/?linkid=2289981 "${DKDOWNLOAD_DIR}/adkwinpesetup.exe")
+		execute_process(COMMAND cmd /c "${DKDOWNLOAD_DIR}/adkwinpesetup.exe" /layout "${DKDOWNLOAD_DIR}/ADKWinPEAddons" /q)
+	endif()
+	
+	dk_echo("Installing Windows PE add-on for the Windows ADK . . .  please wait")
+	dk_validate(DKTOOLS_DIR "dk_DKTOOLS_DIR()")
+	execute_process(COMMAND cmd /c "${DKDOWNLOAD_DIR}/ADKWinPEAddons/adkwinpesetup.exe" /installpath "${DKTOOLS_DIR}/ADK" /q)
+endif()
+
+set(DandISetEnv_bat "${DKTOOLS_DIR}/ADK/Assessment and Deployment Kit/Deployment Tools/DandISetEnv.bat")
+dk_getNativePath(${DandISetEnv_bat} DandISetEnv_bat)
+set(WinPE "${DKTOOLS_DIR}/ADK/WinPE")
+
+if(NOT EXISTS "${WinPE}")
+	set(copype_cmd "${DKTOOLS_DIR}/ADK/Assessment and Deployment Kit/Windows Preinstallation Environment/copype.cmd")
+	dk_getNativePath(${copype_cmd} copype_cmd)
+	dk_getNativePath(${WinPE} WinPE)
+	execute_process(COMMAND cmd /c call "${DandISetEnv_bat}" && "${copype_cmd}" amd64 "${WinPE}")
+endif()
+
+set(DKWinPE_iso "${DKTOOLS_DIR}/DKWinPE.iso")
+if(NOT EXISTS "${DKWinPE_iso}")
+	set(MakeWinPEMedia_cmd "${DKTOOLS_DIR}/ADK/Assessment and Deployment Kit/Windows Preinstallation Environment/MakeWinPEMedia.cmd")
+	dk_getNativePath(${MakeWinPEMedia_cmd} MakeWinPEMedia_cmd)
+	dk_getNativePath(${DKWinPE_iso} DKWinPE_iso)
+	set(WPE "${DKTOOLS_DIR}/ADK/Assessment and Deployment Kit/Windows Preinstallation Environment")
+	execute_process(COMMAND cmd /c call "${DandISetEnv_bat}" && "${MakeWinPEMedia_cmd}" /ISO "${WinPE}" "${DKWinPE_iso}" WORKING_DIRECTORY "${WPE}")
+endif()
+
+
+
 
