c0dc9885d135ef9f7a0e2111dbfb568dd3bae8b0
aquawicket <aquawicket@hotmail.com>
2025-01-17 05:05:44 -0500

    git commit Fri 01/17/2025

--- 3rdParty/_DKIMPORTS/python3/dk_install.cmd
+++ 3rdParty/_DKIMPORTS/python3/dk_install.cmd
@@ -9,24 +9,22 @@ if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
     %dk_call% dk_debugFunc 0
 	
 	%dk_call% dk_validate host_triple "%dk_call% dk_host_triple"
-	if defined win_x86_host           set "PYTHON3_DL=https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-win32.zip"
-	if defined win_x86_64_host        set "PYTHON3_DL=https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-win32.zip"
-	if not defined PYTHON3_DL         %dk_call% dk_error "PYTHON3_DL is invalid"
+	if defined ANDROID_HOST           set "PYTHON3_IMPORT=python"
+	if defined WIN_X86_HOST           set "PYTHON3_IMPORT=https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-win32.zip"
+	if defined WIN_X86_64_HOST        set "PYTHON3_IMPORT=https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-win32.zip"
+	if not defined PYTHON3_IMPORT     set "PYTHON3_IMPORT=python3"
 	
-	::%dk_call% dk_basename %PYTHON3_DL% PYTHON3_DL_FILE
-    ::%dk_call% dk_removeExtension %PYTHON3_DL_FILE% PYTHON3_FOLDER
-    ::%dk_call% dk_toLower %PYTHON3_FOLDER% PYTHON3_FOLDER
-	%dk_call% dk_importVariables %PYTHON3_DL% rtn_var
+	%dk_call% dk_isUrl %PYTHON3_IMPORT% && (
+		%dk_call% dk_importVariables %PYTHON3_IMPORT% rtn_var
+	)
 	
-	%dk_call% dk_validate DK3RDPARTY_DIR "%dk_call% dk_DK3RDPARTY_DIR"
-	%dk_call% dk_set PYTHON3 "%DK3RDPARTY_DIR%\%PYTHON3_FOLDER%"
 	%dk_call% dk_set PYTHON3_EXE "%PYTHON3%\python3.exe"
-
 	if exist "%PYTHON3_EXE%" %return%
 	
-	%dk_call% dk_download "%PYTHON3_DL%"
+	::install
+	%dk_call% dk_download "%PYTHON3_IMPORT%"
 	%dk_call% dk_validate DKDOWNLOAD_DIR "%dk_call% dk_DKDOWNLOAD_DIR"
-	%dk_call% dk_smartExtract "%DKDOWNLOAD_DIR%\%PYTHON3_DL_FILE%" "%PYTHON3%"
+	%dk_call% dk_smartExtract "%DKDOWNLOAD_DIR%\%PYTHON3_IMPORT_FILE%" "%PYTHON3%"
 	
 	if NOT exist "%PYTHON3_EXE%"  %dk_call% dk_error "cannot find PYTHON3_EXE:%PYTHON3_EXE%"
 %endfunction%
---
+++ DKBatch/functions/dk_isUrl.cmd
@@ -0,0 +1,96 @@
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+::####################################################################
+::# dk_isUrl(<in> rtn_var)
+::#
+::#  https://stackoverflow.com/a/17584764
+::#
+:dk_isUrl
+setlocal
+	%dk_call% dk_debugFunc 1 2
+ 
+	set "_arg0_=%~0"
+	
+	:: if "%_arg0_%" MATCHES "^(http|HTTP|https|HTTPS|ftp|FTP|ftps|FTPS)://" (exit /b 0)
+	echo "%~1" | findstr /i "http://" >nul 	&& exit /b 0
+	echo "%~1" | findstr /i "https://" >nul && exit /b 0
+	echo "%~1" | findstr /i "ftp://" >nul 	&& exit /b 0
+	echo "%~1" | findstr /i "ftps://" >nul 	&& exit /b 0
+	
+	exit /b 1
+%endfunction%
+
+
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+setlocal
+	%dk_call% dk_debugFunc 0
+
+    ::### http:// ###
+	%dk_call% dk_isUrl http://www.test.com 		&& echo http://www.test.com = true 		|| echo http://www.test.com = false
+	
+	%dk_call% dk_isUrl "http://www.test.com" 	&& echo "http://www.test.com" = true 	|| echo "http://www.test.com" = false
+	
+	::### HTTP:// ###
+	%dk_call% dk_isUrl HTTP://www.test.com 		&& echo HTTP://www.test.com = true 		|| echo HTTP://www.test.com = false
+	
+	%dk_call% dk_isUrl "HTTP://WWW.TEST.COM" 	&& echo HTTP://WWW.TEST.COM = true 		|| echo HTTP://WWW.TEST.COM = false
+	
+	::### https:// ###
+	%dk_call% dk_isUrl https://www.test.com		&& echo https://www.test.com = true 	|| echo https://www.test.com = false
+	
+	%dk_call% dk_isUrl "https://www.test.com"	&& echo "https://www.test.com" = true 	|| echo "https://www.test.com" = false
+	
+	::### HTTPS:// ###
+	%dk_call% dk_isUrl HTTPS://www.test.com		&& echo HTTPS://www.test.com = true 	|| echo HTTPS://www.test.com = false
+	
+	%dk_call% dk_isUrl "HTTPS://WWW.TEST.COM"	&& echo HTTPS://WWW.TEST.COM = true 	|| echo HTTPS://WWW.TEST.COM = false
+	
+	::### ftp:// ###
+	%dk_call% dk_isUrl ftp://www.test.com		&& echo ftp://www.test.com = true 		|| echo ftp://www.test.com = false
+	
+	%dk_call% dk_isUrl "ftp://www.test.com"		&& echo "ftp://www.test.com" = true 	|| echo "ftp://www.test.com" = false
+	
+	::### FTP:// ###
+	%dk_call% dk_isUrl FTP://WWW.TEST.COM		&& echo FTP://WWW.TEST.COM = true 		|| echo FTP://WWW.TEST.COM = false
+	
+	%dk_call% dk_isUrl "FTP://WWW.TEST.COM"		&& echo "FTP://WWW.TEST.COM" = true		|| echo "FTP://WWW.TEST.COM" = false
+	
+	::### ftps:// ###
+	%dk_call% dk_isUrl ftp://www.test.com		&& echo ftps://www.test.com = true 		|| echo ftps://www.test.com = false
+	
+	%dk_call% dk_isUrl "ftp://www.test.com"		&& echo "ftps://www.test.com" = true 	|| echo "ftps://www.test.com" = false
+	
+	::### FTPS:// ###
+	%dk_call% dk_isUrl FTPS://WWW.TEST.COM		&& echo FTPS://WWW.TEST.COM = true 		|| echo FTPS://WWW.TEST.COM = false
+	
+	%dk_call% dk_isUrl "FTPS://WWW.TEST.COM"	&& echo "FTPS://WWW.TEST.COM" = true 	|| echo "FTPS://WWW.TEST.COM" = false
+	
+	
+	%dk_call% dk_isUrl http://www.test.com && (
+		%dk_call% dk_echo "http://www.test.com = TRUE"
+	) || (
+		%dk_call% dk_echo "http://www.test.com = FALSE"
+	)
+	
+	%dk_call% dk_isUrl notAUrl && (
+		%dk_call% dk_echo "notAUrl = TRUE"
+	) || (
+		%dk_call% dk_echo "notAUrl = FALSE"
+	)
+	
+	::### NOT VALID ###
+	%dk_call% dk_echo ""
+	
+	%dk_call% dk_isUrl www.test.com && %dk_call% dk_echo www.test.com = true || %dk_call% dk_echo www.test.com = false
+	
+	%dk_call% dk_isUrl 'https://www.test.com'
+	%dk_call% dk_echo "'https://www.test.com' = %dk_isUrl%"
+
+%endfunction%
