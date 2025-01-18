f125a566c05a3dd5e4ed14e579435e69db95c224
aquawicket <aquawicket@hotmail.com>
2025-01-18 04:35:10 -0500

    git commit Sat 01/18/2025

--- DKBatch/functions/dk_commandToVariable.cmd
+++ DKBatch/functions/dk_commandToVariable.cmd
@@ -11,44 +11,34 @@ setlocal enableDelayedExpansion
 	%dk_call% dk_debugFunc 2 3
  
     set /a "i=0"
-
     for /f "usebackq delims=" %%Z in (`"%~1" %~2 ^& call echo %%^^errorlevel%%`) do (
-	rem for /f "delims=" %%Z in ('"%~1" %~2 ^& call echo %%^^ERRORLEVEL%%') do (
         set "line[!i!]=%%Z"
 		echo !i! = %%Z
 		set /a "i+=1"
     )
-	
-	
-
-
-
 	%COMSPEC% /c exit /b 0
+	:: Final errorlevel is stored in last line
 	set /a "i-=1"
 	set /a numLines=i-1
-	
-
-
-
-    
-    :: Final errorlevel is stored in last line
-
-    
 	set /a errorcode = !line[%i%]!
     set "line[%i%]="           &:: delete the error line from the array
-    
+    set "lastline=!line[%numLines%]!"
+
+
+	
     :: WARNING
     ::%dk_call% dk_todo "dk_commandToVariable only returns the last line, or, array item from the command.
-    
-    set "lastline=!line[%numLines%]!"
-	
+
+	::############### Print call details ###############
 					echo ###############################
 	if "%~3" neq "" echo ## command:^> '%~1 %~2'
 	if "%~3" equ "" echo ## command:^> '%~1'
 					echo ##  output: %lastline%
 					echo ## rtncode: %errorcode%
 					echo:##
-					
+	::##################################################
+
+	
     :: return the last line from the programs output
     if "%~3" neq "" endlocal & set "%3=%lastline%" && %return%
     endlocal & set "%2=%lastline%"
