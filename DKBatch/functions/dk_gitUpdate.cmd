01885b07bca3b5161f1bf3b76cd26c267ac00b9c
aquawicket <aquawicket@hotmail.com>
2025-01-19 17:39:26 -0500

    git commit Sun 01/19/2025

--- DKBatch/functions/dk_gitUpdate.cmd
+++ DKBatch/functions/dk_gitUpdate.cmd
@@ -12,8 +12,6 @@ setlocal
     if "%~1" neq "" (set "_url_=%~1") else (set "_url_=https://github.com/aquawicket/DigitalKnob.git")
     if "%~2" neq "" (set "_branch_=%~2") else (set "_branch_=Development")
     
-	echo _url_ = %_url_%
-	echo _branch_ = %_branch_%
     ::if "%3" neq "NO_CONFIRM" (
     ::    echo Git Update? Any local changes will be lost.
     ::    %dk_call% dk_confirm || %return%
@@ -24,34 +22,48 @@ setlocal
     
     if NOT exist "%DKBRANCH_DIR%\.git" (
 		if exist "%DKBRANCH_DIR%" (
+		
+			rem ###### Backup Branch directory and clone ######
 			%dk_call% dk_copy "%DKBRANCH_DIR%" "%DKBRANCH_DIR%_BACKUP" OVERWRITE
 			set "PATH=%DKBRANCH_DIR%_BACKUP\DKBatch\functions;%PATH%"
-			
-			echo _branch_ = %_branch_%
-			echo _branch_ = !_branch_!
-			
 			rd /s /q "%DKBRANCH_DIR%"
-			
-			echo _branch_ = %_branch_%
-			echo _branch_ = !_branch_!
 			"%GIT_EXE%" clone %_url_% "%DKBRANCH_DIR%"
+			"%GIT_EXE%" -C %DKBRANCH_DIR% pull --all
+			"%GIT_EXE%" -C %DKBRANCH_DIR% checkout -- .
+			"%GIT_EXE%" -C %DKBRANCH_DIR% checkout %_branch_%
+			
+			if NOT "%ERRORLEVEL%" == "0" (
+				%dk_call% dk_echo "Remote has no '%_branch_%' branch. Creating..."
+				"%GIT_EXE%" -C %DKBRANCH_DIR% checkout -b %_branch_% main
+				"%GIT_EXE%" -C %DKBRANCH_DIR% push --set-upstream origin %_branch_%
+			)
+			%return%
 		)
+		
+		rem ###### Clone into empty branch directory ######
+		"%GIT_EXE%" clone %_url_% "%DKBRANCH_DIR%"
+		"%GIT_EXE%" -C %DKBRANCH_DIR% pull --all
+		"%GIT_EXE%" -C %DKBRANCH_DIR% checkout -- .
+		"%GIT_EXE%" -C %DKBRANCH_DIR% checkout %_branch_%
+			
+		if NOT "%ERRORLEVEL%" == "0" (
+			%dk_call% dk_echo "Remote has no '%_branch_%' branch. Creating..."
+			"%GIT_EXE%" -C %DKBRANCH_DIR% checkout -b %_branch_% main
+			"%GIT_EXE%" -C %DKBRANCH_DIR% push --set-upstream origin %_branch_%
+		)
+		%return%
 	)
-
-    ::%dk_call% dk_cd "%DKBRANCH_DIR%"
-    "%GIT_EXE%" -C %DKBRANCH_DIR% pull --all
-    "%GIT_EXE%" -C %DKBRANCH_DIR% checkout -- .
-
-	echo _branch_ = %_branch_%
-	echo _branch_ = !_branch_!
-    "%GIT_EXE%" -C %DKBRANCH_DIR% checkout %_branch_%
-    if NOT "%ERRORLEVEL%" == "0" (
-		echo _branch_ = %_branch_%
-		echo _branch_ = !_branch_!
-        %dk_call% dk_echo "Remote has no '%_branch_%' branch. Creating..."
-        "%GIT_EXE%" -C %DKBRANCH_DIR% checkout -b %_branch_% main
-        "%GIT_EXE%" -C %DKBRANCH_DIR% push --set-upstream origin %_branch_%
-    )
+	
+	::###### Simple Update ######
+	"%GIT_EXE%" -C %DKBRANCH_DIR% pull --all
+	"%GIT_EXE%" -C %DKBRANCH_DIR% checkout -- .
+	"%GIT_EXE%" -C %DKBRANCH_DIR% checkout %_branch_%
+			
+	if NOT "%ERRORLEVEL%" == "0" (
+		%dk_call% dk_echo "Remote has no '%_branch_%' branch. Creating..."
+		"%GIT_EXE%" -C %DKBRANCH_DIR% checkout -b %_branch_% main
+		"%GIT_EXE%" -C %DKBRANCH_DIR% push --set-upstream origin %_branch_%
+	)
 %endfunction%
 
 
