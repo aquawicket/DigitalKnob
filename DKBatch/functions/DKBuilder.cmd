ae4da72a1985e167a3ab393c2f4073ca9424e404
aquawicket <aquawicket@hotmail.com>
2025-01-19 22:52:03 -0500

    git commit Sun 01/19/2025

--- DKBatch/functions/DKBuilder.cmd
+++ DKBatch/functions/DKBuilder.cmd
@@ -1,61 +1,60 @@
 @echo off
 
-set "ENABLE_dk_debug=1"
-
+:MAIN
 setlocal enableDelayedExpansion
 
-set "HDK=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBatch/functions/DK.cmd"
+	set "ENABLE_dk_debug=1"
+	set "HDK=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/DKBatch/functions/DK.cmd"
+	if not exist "%DKBATCH_FUNCTIONS_DIR_%" (set "DKBATCH_FUNCTIONS_DIR_=%USERPROFILE%\digitalknob\Development\DKBatch\functions")
+	if not exist "%DKBATCH_FUNCTIONS_DIR_%" (mkdir "%DKBATCH_FUNCTIONS_DIR_%" >nul 2>&1)
+	set "DK=%DKBATCH_FUNCTIONS_DIR_%\DK.cmd"
 
-set "DKF=%USERPROFILE%\digitalknob\Development\DKBatch\functions"
+	:: firewall
+	call :dk_firewallAllow powershell "%WINDIR%\system32\windowspowershell\v1.0\powershell.exe"
+	call :dk_firewallAllow curl "%WINDIR%\system32\curl.exe"
+	call :dk_firewallAllow git "C:\users\administrator\digitalknob\dktools\portablegit-2.46.2-64-bit\mingw64\libexec\git-core\git-remote-https.exe"
 
-if not exist "!DKF!" set "DKF=%USERPROFILE%\.dk\DKBatch\functions"
+	if not exist %DK% (
+		powershell -c "(New-Object Net.WebClient).DownloadFile('!HDK!','!DK!')" >nul 2>&1 || ^
+		certutil -urlcache -split -f "!HDK!" "!DK!" >nul 2>&1 || ^
+		curl -f "!HDK!" -o "!DK!" >nul 2>&1 || ^
+		echo DKINIT Failed
+	)
 
-attrib +h %USERPROFILE%\.dk
+	call "%DK%" %~0 %*
 
-set "DK=!DKF!\DK.cmd"
-mkdir "!DKF!" 2>nul
-set "DK=!DKF!\DK.cmd" 
-
-:: firewall
-call :dk_firewallAllow powershell "%WINDIR%\system32\windowspowershell\v1.0\powershell.exe"
-call :dk_firewallAllow curl "%WINDIR%\system32\curl.exe"
-call :dk_firewallAllow git "C:\users\administrator\digitalknob\dktools\portablegit-2.46.2-64-bit\mingw64\libexec\git-core\git-remote-https.exe"
-
-if not exist !DK! powershell -c "(New-Object Net.WebClient).DownloadFile('!HDK!','!DK!')" >nul 2>&1||certutil -urlcache -split -f "!HDK!" "!DK!" >nul 2>&1||curl -f "!HDK!" -o "!DK!" >nul 2>&1||echo DKINIT Failed
-
-endlocal & set "DK=%DK%"
-set "DKF=%DKF%"
-
-
-call "%DK%" %~0 %*
-
-::takeown /F %DKF% /R /D "Y"
-::####################
-%dk_call% dk_buildMain
-
-:: 		DKHOME	                                 C:/Users/aquawicket
-:: DKHTTP_HOME  	    https://raw.githubusercontent.com/aquawicket
-:: DIGITALKNOB_DIR					     		 C:/Users/aquawicket/digitalknob/
-:: DKHTTP_DIGITALKNOB	https://raw.githubusercontent.com/aquawicket/digitalknob/
-:: 
-%return%
-
-
-
-
-
-:dk_firewallAllow
-	call :dk_registryContains "HKLM\SYSTEM\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" "%~2" && exit /b 0
-	netsh advfirewall firewall add rule name="%~1" dir=in action=allow program="%~2" enable=yes profile=any
-	netsh advfirewall firewall add rule name="%~1" dir=out action=allow program="%~2" enable=yes profile=any
-%endfunction%
+	::takeown /F %DKF% /R /D "Y"
+	%dk_call% dk_buildMain
+exit /b %errorlevel%
 
 
 :dk_registryContains
-	setlocal EnableDelayedExpansion
-	for /f "usebackq delims=" %%a in (`reg query "%~1"`) do (
+setlocal enableDelayedExpansion
+	for /f "usebackq delims=" %%a in (`reg query %~1`) do (
 		set "str=%%a"
-		if not "x!str:%~2=!" == "x!str!" exit /b 0
+		if not "x!str:%~2=!x" == "x!str!x" (exit /b 0)
 	)
     exit /b 1
+%endfunction%
+
+
+:dk_firewallAllow
+setlocal enableDelayedExpansion
+	set "_file_=%~2"
+	set "_file_=%_file_:/=\%"
+	%dk_call% :dk_registryContains "HKLM\SYSTEM\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" "%_file_%" && (exit /b 0)
+	netsh advfirewall firewall add rule name="%~1" dir=in action=allow program="%_file_%" enable=yes profile=any >nul
+	netsh advfirewall firewall add rule name="%~1" dir=out action=allow program="%_file_%" enable=yes profile=any >nul
+	::###### Windows Firewall Control ######
+
+	if not exist "%WFC_EXE%" 	(set "WFC_EXE=C:\Program Files\Malwarebytes\Windows Firewall Control\wfc.exe")
+	if not exist "%WFCUI_EXE%" 	(set "WFCUI_EXE=C:\Program Files\Malwarebytes\Windows Firewall Control\wfcUI.exe")
+	if exist "%WFC_EXE%"		(set "WFC_APP=%WFC_EXE%")
+	if exist "%WFCUI_EXE%"		(set "WFC_APP=%WFCUI_EXE%")
+	if exist "%WFC_APP%"		(set cmnd="%WFC_APP%" -allow "%_file_%")
+	if not exist "%CMD_EXE%" (set "CMD_EXE=%COMSPEC%")
+	if not exist "%CMD_EXE%" (%dk_call% dk_error "CMD_EXE is invalid")
+	::echo cmnd ^> "%CMD_EXE%" /c "%cmnd%"
+	"%CMD_EXE%" /c "%cmnd%"
+	::######################################
 %endfunction%
