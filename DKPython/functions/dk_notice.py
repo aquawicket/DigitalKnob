710f039a1a499e730a249a0d984c11b0782a450d
aquawicket <aquawicket@hotmail.com>
2025-01-22 13:45:25 -0500

    git commit Wed 01/22/2025

---
+++ DKPython/functions/dk_notice.py
@@ -0,0 +1,27 @@
+import sys 
+sys.path.append(".")
+import DK
+
+from dk_log import *
+
+##################################################################################
+# dk_notice(<message>)
+#
+#    Print a notice message to the console
+#
+#    @message - The message to print
+#
+def dk_notice(*args):
+#    dk_call dk_debugFunc 1
+
+    dk_log(NOTICE, args[0])
+
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+def DKTEST():
+#    dk_call dk_debugFunc 0
+
+    dk_notice("test dk_notice message")
---
+++ DKPython/functions/dk_warning.py
@@ -0,0 +1,27 @@
+import sys 
+sys.path.append(".")
+import DK
+
+from dk_log import *
+
+##################################################################################
+# dk_warning(<message>)
+#
+#    Print a warning message to the console
+#
+#    @message - The message to print
+#
+def dk_warning(*args):
+#    dk_call dk_debugFunc 1
+
+    dk_log(WARNING, args[0])
+
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+def DKTEST():
+#    dk_call dk_debugFunc 0
+
+    dk_warning("test dk_warning message")
