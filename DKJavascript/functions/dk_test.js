9425a69c6192909417c96d612451c699470b0913
aquawicket <aquawicket@hotmail.com>
2025-01-22 18:50:15 -0500

    git commit Wed 01/22/2025

---
+++ DKJavascript/functions/dk_test.js
@@ -0,0 +1,20 @@
+//################################################################################
+//# dk_test()
+//#
+//#
+dk_test = function() {
+	//dk_debugFunc(0);
+	
+
+}
+
+
+
+
+
+//###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+DKTEST = function() {
+	//dk_debugFunc(0);
+	
+    var basename = dk_test();
+}
---
+++ DKPhp/functions/dk_test.php
@@ -0,0 +1,34 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include("DK.php");
+#include(${DKPHP_FUNCTIONS_DIR}/DK.php)
+#include_guard()
+
+##################################################################################
+# dk_test()
+#
+#
+function dk_test(){
+	echo "dk_test()\n";
+	#dk_debugFunc();
+#   if($(__ARGC__) -ne 1){ dk_error "$(__FUNCTION__)($(__ARGC__)): incorrect number of arguments" }	
+	
+
+	return "";
+}
+
+
+
+
+
+
+
+####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ####### DKTEST ######
+#function DKTEST(){ 
+	#dk_debugFunc();
+	
+	#dk_test = dk_test();
+
+#}
+
+?>
