3c03be4d3e39be978e289521879958c621125331
aquawicket <aquawicket@hotmail.com>
2025-01-23 14:39:38 -0500

    git commit Thu 01/23/2025

--- DKBatch/functions/dk_getFileLine.cmd
+++ DKBatch/functions/dk_getFileLine.cmd
@@ -11,37 +11,30 @@ setlocal enableDelayedExpansion
  
 	set "_filepath_=%~f1"
 	set "_filepath_=%_filepath_:/=\%"
+	%dk_call% dk_assertPath "%_filepath_%"
     
-    set /a "line=%~2" || for /f "delims=:" %%a in ('findstr /n /c:"%~2" "%_filepath_%"') do set "line=%%a"
-    
-    echo [91m  File: %_filepath_%: !line! [0m
-    set /a n=1
-    set /a min=!line!-15
-    set /a max=!line!+15
-    for /f "delims=" %%a in ('findstr /n /r /c:"^" "%_filepath_%"') do (
-        if !n! LSS !max! (
-            if !n! GTR !min! (
-                set str=%%a
-                set str=!str::=:    ! 
-                if !n! equ !line! (
-                    echo [30;41m^>  !str![0m
-                ) else (
-                    echo    !str!
-                )
-            )
-        )       
-        set /a n+=1
-    )
-    %dk_call% dk_echo
+    for /f "delims=:" %%a in ('findstr /n /c:"%~2" "%_filepath_%"') do (
+		if "!line!"=="" (
+			set "line=%%a"
+		) else (
+			set "line=!line!;%%a"
+		)
+	)
+ 
+	endlocal & set "dk_getFileLine=%line%"
+:: DEBUG 
+::	echo %line%: '%~2'
 %endfunction%
 
 
-
+:: FIND THIS LINE
 
 ::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
 :DKTEST
 setlocal
     %dk_call% dk_debugFunc 0
 
-    %dk_call% dk_getFileLine "../../README.md" "How to build"
+::    %dk_call% dk_getFileLine "../../README.md" "How to build"
+	%dk_call% dk_getFileLine "%~0" ":: FIND THIS LINE"
+	echo dk_getFileLine = %dk_getFileLine%
 %endfunction%
