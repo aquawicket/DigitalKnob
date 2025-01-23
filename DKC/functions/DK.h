0933b3175e505898b52d429b38ba0a202141e24f
aquawicket <aquawicket@hotmail.com>
2025-01-22 23:24:41 -0500

    git commit Wed 01/22/2025

--- DKC/functions/DK.h
+++ DKC/functions/DK.h
@@ -2,14 +2,43 @@
 #ifndef DK_h
 #define DK_h
 
+char* DKSCRIPT_PATH;
+char* DKSCRIPT_DIR;
+char* DKSCRIPT_NAME;
+char* DKSCRIPT_EXT;
+char* DKSCRIPT_ARGS;
+char* DKHOME_DIR;
+char* DKCACHE_DIR;
+char* DKDESKTOP_DIR;
+char* DIGITALKNOB;
+char* DIGITALKNOB_DIR;
+char* DKDOWNLOAD_DIR;
+char* DKTOOLS_DIR;
+char* DKBRANCH;
+char* DKBRANCH_DIR;
+char* DK3RDPARTY_DIR;
+char* DKAPPS_DIR;
+char* DKC_DIR;
+char* DKC_FUNCTIONS_DIR;
+char* DKC_FUNCTIONS_DIR_;
 
 //################################################################################
 //# DK()
 //#
 #include "dk_preprocessor.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int DK(){
 	printf("\x1B[104m \x1B[30m %s %s \x1B[0m \n\n", DKCOMPILER, DKCOMPILER_VERSION);
+	
+	
+	DKHOME_DIR = getenv("USERPROFILE");
+	//sprintf(DKCACHE_DIR, "s/.dk");/*, DKHOME_DIR);*/
+
+	//strcat(DKCACHE_DIR, "/.dk");
+	//strcat(DKCACHE_DIR, "/.dk");
+	
 	return 0;
 };
 int rtn = DK();
--- DKC/functions/dk_test.h
+++ DKC/functions/dk_test.h
@@ -8,50 +8,56 @@
 //################################################################################
 //# dk_test(args)
 //#
+#include <stdarg.h>
 #include <stdio.h>
-void dk_test(int argc, ...){
+#include <string.h>
+void dk_test(int __ARGC__, ...){
 	//dk_debugFunc 0 99
 	
+	
 	printf("################# dk_test.h ################\n");
-//    printf("                  0 = %s\n", args[0]);
-//	  printf("                  1 = %s\n", args[1]);
-//    printf("                  2 = %s\n", args[2]);
-//    printf("                  3 = %s\n", args[3]);
-//    printf("                  4 = %s\n", args[4]);
-//    printf("                  5 = %s\n", args[5]);
-//    printf("                  6 = %s\n", args[6]);
-//    printf("                  7 = %s\n", args[7]);
-//    printf("                  8 = %s\n", args[8]);
-//    printf("                  9 = %s\n", args[9]);
-//    printf("                  * = %s\n", args);
+	
+	//### args[n] ###
+	va_list ap;
+	va_start(ap, __ARGC__); /* Before C23: Requires the last fixed parameter (to get the address) */
+	char __ARGV__[256];
+	strcpy(__ARGV__,"");
+	for (int j = 0; j < __ARGC__; j++) { 
+		char* temp = va_arg(ap, char*);  /* Increments ap to the next argument. */
+		printf("               arg%d = %s\n", j, temp);
+		strcat(__ARGV__, temp);
+		strcat(__ARGV__, ";");
+    }
+    va_end(ap);
 //    printf("         ERRORLEVEL = %s\n", ERRORLEVEL);
 //    printf("                LVL = %s\n", LVL);
-//    printf("           __TIME__ = %s\n", __TIME__);
+    printf("           __DATE__ = %s\n", DKDATE);
+    printf("           __TIME__ = %s\n", DKTIME);
 //    printf("           __CMND__ = %s\n", __CMND__);/
-//    printf("           __FILE__ = %s\n", __FILE__);
-//    printf("           __LINE__ = %s\n", __LINE__);
-//    printf("           __FUNC__ = %s\n", __FUNC__);
-//    printf("           __ARGC__ = %s\n", __ARGC__);
-//    printf("           __ARGV__ = %s\n", __ARGV__);
-//    printf("      DKSCRIPT_PATH = %s\n", DKSCRIPT_PATH);
-//    printf("       DKSCRIPT_DIR = %s\n", DKSCRIPT_DIR);
-//    printf("      DKSCRIPT_NAME = %s\n", DKSCRIPT_NAME);
-//    printf("       DKSCRIPT_EXT = %s\n", DKSCRIPT_EXT);
-//    printf("      DKSCRIPT_ARGS = %s\n", DKSCRIPT_ARGS);
-//    printf("         DKHOME_DIR = %s\n", DKHOME_DIR);
-//    printf("        DKCACHE_DIR = %s\n", DKCACHE_DIR);
-//    printf("      DKDESKTOP_DIR = %s\n", DKDESKTOP_DIR);
-//    printf("        DIGITALKNOB = %s\n", DIGITALKNOB);
-//    printf("    DIGITALKNOB_DIR = %s\n", DIGITALKNOB_DIR);
-//    printf("     DKDOWNLOAD_DIR = %s\n", DKDOWNLOAD_DIR);
-//    printf("        DKTOOLS_DIR = %s\n", DKTOOLS_DIR);
-//    printf("           DKBRANCH = %s\n", DKBRANCH);
-//    printf("       DKBRANCH_DIR = %s\n", DKBRANCH_DIR);
-//    printf("     DK3RDPARTY_DIR = %s\n", DK3RDPARTY_DIR);
-//    printf("         DKAPPS_DIR = %s\n", DKAPPS_DIR);
-//    printf("            DKC_DIR = %s\n", DKC_DIR);
-//    printf("  DKC_FUNCTIONS_DIR = %s\n", DKC_FUNCTIONS_DIR);
-//    printf(" DKC_FUNCTIONS_DIR_ = %s\n", DKC_FUNCTIONS_DIR_);
+    printf("           __FILE__ = %s\n", __FILE__);
+    printf("           __LINE__ = %d\n", __LINE__);
+    printf("       __FUNCTION__ = %s\n", __FUNCTION__);
+    printf("           __ARGC__ = %d\n", __ARGC__);
+    printf("           __ARGV__ = %s\n", __ARGV__);
+    printf("      DKSCRIPT_PATH = %s\n", DKSCRIPT_PATH);
+    printf("       DKSCRIPT_DIR = %s\n", DKSCRIPT_DIR);
+    printf("      DKSCRIPT_NAME = %s\n", DKSCRIPT_NAME);
+    printf("       DKSCRIPT_EXT = %s\n", DKSCRIPT_EXT);
+    printf("      DKSCRIPT_ARGS = %s\n", DKSCRIPT_ARGS);
+    printf("         DKHOME_DIR = %s\n", DKHOME_DIR);
+    printf("        DKCACHE_DIR = %s\n", DKCACHE_DIR);
+    printf("      DKDESKTOP_DIR = %s\n", DKDESKTOP_DIR);
+    printf("        DIGITALKNOB = %s\n", DIGITALKNOB);
+    printf("    DIGITALKNOB_DIR = %s\n", DIGITALKNOB_DIR);
+    printf("     DKDOWNLOAD_DIR = %s\n", DKDOWNLOAD_DIR);
+    printf("        DKTOOLS_DIR = %s\n", DKTOOLS_DIR);
+    printf("           DKBRANCH = %s\n", DKBRANCH);
+    printf("       DKBRANCH_DIR = %s\n", DKBRANCH_DIR);
+    printf("     DK3RDPARTY_DIR = %s\n", DK3RDPARTY_DIR);
+    printf("         DKAPPS_DIR = %s\n", DKAPPS_DIR);
+    printf("            DKC_DIR = %s\n", DKC_DIR);
+    printf("  DKC_FUNCTIONS_DIR = %s\n", DKC_FUNCTIONS_DIR);
+    printf(" DKC_FUNCTIONS_DIR_ = %s\n", DKC_FUNCTIONS_DIR_);
     	
 //	setGlobal(GLOBAL_VAR, "This is a global variable");
 //	setReturn(RETURN_VAR, "This is a return variable");
