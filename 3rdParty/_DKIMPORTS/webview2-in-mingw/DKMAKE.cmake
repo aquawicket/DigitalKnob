adf8b839472852fb909893aa34935702c7bd487f
aquawicket <aquawicket@hotmail.com>
2025-01-24 18:00:10 -0500

    git commit Fri 01/24/2025

---
+++ 3rdParty/_DKIMPORTS/webview2-in-mingw/DKMAKE.cmake
@@ -0,0 +1,8 @@
+#!/usr/bin/cmake -P
+if(NOT DKCMAKE_FUNCTIONS_DIR_)
+	set(DKCMAKE_FUNCTIONS_DIR_ ../../../DKCMake/functions/)
+endif()
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+
+
+dk_import(https://github.com/jchv/webview2-in-mingw/archive/c003dd6e.zip)
