609d40ded97722dda80857dc4f8478a836012279
aquawicket <aquawicket@hotmail.com>
2025-01-25 03:09:44 -0500

    git commit Sat 01/25/2025

--- 3rdParty/_DKIMPORTS/webview2_in_mingw/DKMAKE.cmake
+++ 3rdParty/_DKIMPORTS/webview2_in_mingw/DKMAKE.cmake
@@ -6,29 +6,30 @@ include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
 
 
 ############ webview2_in_mingw ############
-dk_depend(libgcc)
-
+#dk_depend(libgcc)
 dk_import("https://github.com/jchv/webview2-in-mingw/archive/c003dd6e.zip" PATCH)
 
-# CFLAGS += -I./WebView/include
 dk_include("${WEBVIEW2_IN_MINGW}/WebView/include")
 
 dk_libDebug("${WEBVIEW2_IN_MINGW_DEBUG_DIR}/webview2.exe")
 dk_libRelease("${WEBVIEW2_IN_MINGW_RELEASE_DIR}/webview2.exe")
 
+if(WIN_X86)
+	set(ARCH "Win32")
+else(WIN_X86_64)
+	set(ARCH "x64")
+endif()
 
-# LDFLAGS += -L./WebView/x64 -lWebView2Loader -mwindows -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic
-dk_configure(${WEBVIEW2_IN_MINGW} -DLIBGCC_LIBRARY_DEBUG=${LIBGCC_LIBRARY_DEBUG})
+set(WEBVIEW2GUID_LIB		"${WEBVIEW2_IN_MINGW}/WebView/${ARCH}/WebView2Guid.lib")
+set(WEBVIEW2LOADER_DLL_LIB	"${WEBVIEW2_IN_MINGW}/WebView/${ARCH}/WebView2Loader.dll.lib")
+set(WEBVIEW2LOADER_DLL		"${WEBVIEW2_IN_MINGW}/WebView/${ARCH}/WebView2Loader.dll")
 
 
-
+dk_configure(${WEBVIEW2_IN_MINGW} -DARCH=${ARCH}) 
 dk_build(${WEBVIEW2_IN_MINGW})
 
-if(WIN_X86)
-	dk_copy(${WEBVIEW2_IN_MINGW}/WebView/Win32/WebView2Loader.dll ${WEBVIEW2_IN_MINGW_BUILD_DIR}/WebView2Loader.dll)
-elseif(WIN_X86_64)
-	dk_copy(${WEBVIEW2_IN_MINGW}/WebView/x64/WebView2Loader.dll ${WEBVIEW2_IN_MINGW_BUILD_DIR}/WebView2Loader.dll)
-endif()
+dk_copy(${WEBVIEW2_IN_MINGW}/WebView/${ARCH}/WebView2Loader.dll ${WEBVIEW2_IN_MINGW_BUILD_DIR}/WebView2Loader.dll)
+
 
 
 
