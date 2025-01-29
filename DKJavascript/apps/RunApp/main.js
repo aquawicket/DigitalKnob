1d2259e55d0740256c9e183b4947a2771b538a70
aquawicket <aquawicket@hotmail.com>
2025-01-29 04:16:53 -0500

    git commit Wed 01/29/2025

--- DKJavascript/apps/RunApp/main.js
+++ DKJavascript/apps/RunApp/main.js
@@ -1,3 +1,13 @@
+function RunFile() {
+	WshShell = new ActiveXObject("WScript.Shell");
+	WshShell.Run("c:/windows/system32/notepad.exe", 1, false);
+}
+	
+	
 function main(){
-	//TODO
+	window.resizeTo(350, 350);
+	window.moveTo(10, 10);
+		
+	var gui = '<input type="button" value="Run Notepad" onclick="RunFile();"/>'
+	document.body.innerHTML = gui;
 }
