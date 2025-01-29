74a9ad10d6b4ed8804209fc2a80d391fa2bebcbf
aquawicket <aquawicket@hotmail.com>
2025-01-29 04:31:11 -0500

    git commit Wed 01/29/2025

--- DKJavascript/apps/Console/main.js
+++ DKJavascript/apps/Console/main.js
@@ -1,3 +1,16 @@
+dk_source(DKJAVASCRIPT_DIR+"/functions/DKHtmlConsole.js");
+
 function main(){
-	//TODO
+	var DKHtml = 1;
+	dkTitle = "DigitalKnob - " + location.href;
+	document.title = dkTitle;
+
+	document.addEventListener("DOMContentLoaded", onDOMContentLoaded() );
+	function onDOMContentLoaded() {
+		dkconsole = new DKHtmlConsole;
+		dkconsole.create("","0px","0px","0px","","25%");
+
+		console.log(dkTitle + "<br></br>");
+		console.log("HOST = "+HOST);
+	}
 }
