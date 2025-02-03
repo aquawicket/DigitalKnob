b9071b018864792577b12b66d8ce18cafe14cb62
aquawicket <aquawicket@hotmail.com>
2025-02-03 01:34:37 -0500

    git commit Mon 02/03/2025

---
+++ DKBatch/functions/dk_currentDirectory.cmd
@@ -0,0 +1,38 @@
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+::################################################################################
+::# dk_currentDirectory(optional:<rtn_var>)
+::#
+::#    REFERENCE: https://ss64.com/nt/syntax-gettime.html
+::#
+:dk_currentDirectory
+setlocal
+	%dk_call% dk_debugFunc 1
+	set "dk_currentDirectory=%CD%"
+    
+	endlocal & (
+		set "dk_currentDirectory=%dk_currentDirectory%"
+		if "%1" neq "" set "%1=%dk_currentDirectory%"
+	)
+	
+::debug
+::	%dk_call% dk_printVar %1
+%endfunction%
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+setlocal
+	%dk_call% dk_debugFunc 0
+
+    %dk_call% dk_currentDirectory
+    %dk_call% dk_printVar dk_currentDirectory
+	
+	%dk_call% dk_currentDirectory myPath
+    %dk_call% dk_printVar myPath
+	%dk_call% dk_printVar dk_currentDirectory
+%endfunction%   
---
+++ DKBatch/functions/dk_nativePath.cmd
@@ -0,0 +1,38 @@
+@echo off
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+::####################################################################
+::# dk_nativePath(<pathname>, <rtn_var>:optional)
+::#
+::#
+:dk_nativePath
+setlocal
+	%dk_call% dk_debugFunc 1 2
+
+    set pathname=%1
+    set pathname=%pathname:"=%
+	set "pathname=%pathname:/=\%"
+    if "%pathname:~-1%"=="\" set pathname=%pathname:~0,-1%
+
+	for %%Z in ("%pathname%") do set "dk_nativePath=%%~fZ"
+::	set "dk_nativePath=%dk_nativePath:\=/%"
+	
+    endlocal & (
+		set "dk_nativePath=%dk_nativePath%"
+		if "%2" neq "" set "%2=%dk_nativePath%"
+	)	
+
+%endfunction%
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+setlocal
+	%dk_call% dk_debugFunc 0
+
+    %dk_call% dk_set myPath "C:/Windows/System32"
+    %dk_call% dk_nativePath "%myPath%" nativePath
+    %dk_call% dk_printVar nativePath
+%endfunction%
