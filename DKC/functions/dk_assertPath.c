bf780feaf99afc9dbf11781ecf493d7cb77cd42d
aquawicket <aquawicket@hotmail.com>
2025-02-08 15:22:15 -0500

    git commit

--- DKC/functions/dk_assertPath.c
+++ DKC/functions/dk_assertPath.c
@@ -1,3 +1,4 @@
+///$(which true);FLAGS="-g -Wall -Wextra --std=c17 -O1 -fsanitize=address,undefined";THIS_FILE="$(cd "$(dirname "$0")"; pwd -P)/$(basename "$0")";OUT_FILE="tmp/build-cache/$THIS_FILE";mkdir -p "$(dirname "$OUT_FILE")";test "$THIS_FILE" -ot "$OUT_FILE" || $(which clang || which gcc) $FLAGS "$THIS_FILE" -o "$OUT_FILE" || exit $?;exec bash -c "exec -a \"$0\" \"$OUT_FILE\" $([ $# -eq 0 ] || printf ' "%s"' "$@")"
 #include "dk_assertPath.h"
 
 //###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
