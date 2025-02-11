0f3f7eaa1618dbd0adb1b1655a2e1e570fe37194
aquawicket <aquawicket@hotmail.com>
2025-02-11 00:48:02 -0500

    git commit Tue 02/11/2025

---
+++ DKBatch/apps/Add3rdParty/Add3rdParty.cmd
@@ -0,0 +1,14 @@
+@echo off
+if not exist "%DKBATCH_FUNCTIONS_DIR_%"  set "DKBATCH_FUNCTIONS_DIR_=%CD%\..\..\functions\"
+if not defined DKINIT call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*
+
+:Add3rdParty
+	%dk_call% dk_inputBox
+	%dk_call% dk_printVar dk_inputBox
+	
+	%dk_call% dk_validate DKIMPORTS_DIR "%dk_call% dk_DKIMPORTS_DIR"
+	if exist "%DKIMPORTS_DIR%/%dk_inputBox%" (echo %dk_inputBox% already exists & %return%)
+	
+	echo creating %dk_inputBox% . . .
+	%dk_call% dk_copy "%DKIMPORTS_DIR%/_TEMPLATE-LIB_" "%DKIMPORTS_DIR%/%dk_inputBox%"
+%endfunction%
