208d63b7faf9862a0602676d2a3c2f3d868d6ad5
aquawicket <aquawicket@hotmail.com>
2025-02-14 08:16:57 -0500

    git commit Fri 02/14/2025

---
+++ 3rdParty/_DKIMPORTS/deno/DKMAKE.cmake
@@ -0,0 +1,33 @@
+#!/usr/bin/cmake -P
+if(NOT DKCMAKE_FUNCTIONS_DIR_)
+	set(DKCMAKE_FUNCTIONS_DIR_ ../../../DKCMake/functions/)
+endif()
+include(${DKCMAKE_FUNCTIONS_DIR_}DK.cmake)
+
+
+############ deno ############
+# https://github.com/denoland/deno
+
+
+
+### INSTALL ###
+dk_validate		(DKIMPORTS_DIR "dk_DKIMPORTS_DIR()")
+dk_getFileParam	(${DKIMPORTS_DIR}/debo/dkconfig.txt DENO_IMPORT)
+dk_import		(DENO_IMPORT)
+
+### LINK ###
+dk_include			(${DENO}/include)
+if(MSVC)
+	dk_libDebug		(${DENO_DEBUG_DIR}/deno.lib)
+	dk_libRelease	(${DENO_RELEASE_DIR}/deno.lib)
+else()
+	dk_libDebug		(${DENO_DEBUG_DIR}/libdeno.a)
+	dk_libRelease	(${DENO_RELEASE_DIR}/libdeno.a)
+endif()
+
+### GENERATE ###
+dk_configure(${DENO})
+
+
+### COMPILE ###
+dk_build(${DENO})
