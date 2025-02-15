9d62ae6d6b7f462fbf12866c73ed1112c702a1c6
aquawicket <aquawicket@hotmail.com>
2025-02-15 03:50:17 -0500

    git commit Sat 02/15/2025

---
+++ DKBatch/functions/dk_imgToIco.cmd
@@ -0,0 +1,39 @@
+@echo off
+if not defined DKINIT (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*)
+
+::####################################################################
+::# dk_imgToIco(<imgFile>, <icoFile>)
+::#
+::#
+:dk_imgToIco
+setlocal
+	%dk_call% dk_debugFunc 2 3
+ 
+    set "imgFile=%~1"
+	set "imgFile=%imgFile:/=\%"
+    set "icoFile=%~2"
+	set "icoFile=%icoFile:/=\%"
+    set "OVERWRITE=%~3"
+    
+    if not defined OVERWRITE if exist "%icoFile%" (%dk_call% dk_warning "%icoFile% already exists" && %return%)
+	if exist "%icoFile%" (%dk_call% dk_delete "%icoFile%")
+
+	%dk_call% dk_callDKPowershell dk_imgToIco "%imgFile% %icoFile%"
+    
+    if not exist %icoFile% (%dk_call% dk_fatal "Failed to create shortcut:%icoFile%")
+%endfunction%
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+setlocal
+	%dk_call% dk_debugFunc 0
+
+    %dk_call% dk_createShortcut "C:/Users/Administrator/Desktop/digitalknob.lnk" "C:/Users/Administrator/digitalknob"
+	
+	set "imgFile=C:\Users\Administrator\digitalknob\Development\DKCpp\DKApps\DKCore\icons\icon.png"
+	set "icoFile=C:\Users\Administrator\digitalknob\Development\DKCpp\DKApps\DKCore\icons\icon.ico"
+	%dk_call% dk_imgToIco "%imgFile%" "%icoFile%"
+%endfunction%
