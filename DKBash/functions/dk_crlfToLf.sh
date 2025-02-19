2a0f400bd568d94691435e4bd74719963932df68
aquawicket <aquawicket@hotmail.com>
2025-02-18 23:30:39 -0500

    git commit Tue 02/18/2025

---
+++ DKBash/functions/dk_crlfToLf.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+[ -z "${DKINIT-}" ] && . "${DKBASH_FUNCTIONS_DIR_-./}DK.sh"
+
+##################################################################################
+# dk_crlfToLf(file)
+#
+#	Convert a file with windows CRLF line endings to unix LF lined endings.
+#
+#	Reference: https://arueckauer.github.io/posts/2020/05/replace-crlf-with-lf-in-bash
+#
+dk_crlfToLf() {
+	dk_debugFunc 1
+	
+	_file_=$1
+	dk_call dk_assertPath ${_file_}
+	
+	#cat ${_file_} | sed 's/\r$//' > ${_file_}
+
+	sed -i 's/\r//g' ${_file_}
+}
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+DKTEST() {
+	dk_debugFunc 0
+	
+	dk_call dk_crlfToLf TODO
+}
---
+++ DKBatch/functions/dk_crlfToLf.cmd
@@ -0,0 +1,32 @@
+@echo off
+if not defined DKINIT (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*)
+
+::###############################################################################
+::# dk_crlfToLf(input_file, output_file)
+::#
+::#
+:dk_crlfToLf
+setlocal
+	%dk_call% dk_debugFunc 2
+	
+	set "_file_=%~1"
+	echo _file_ = %_file_%
+
+	set "_file_=%_file_:\=/%"
+	set "_file_=%_file_:C:=/c%"
+	%dk_call% dk_callDKBash dk_crlfToLf %_file_%
+%endfunction%
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+setlocal
+	%dk_call% dk_debugFunc 0
+	
+	%dk_call% dk_selectFile _file_
+	%dk_call% dk_crlfToLf "%_file_%"
+	
+%endfunction%
