1833763c084482d8eb43ec92365572908663b542
aquawicket <aquawicket@hotmail.com>
2025-02-19 19:29:49 -0500

    git commit Wed 02/19/2025

--- DKBatch/functions/dk_set.cmd
+++ DKBatch/functions/dk_set.cmd
@@ -6,19 +6,18 @@ if not defined DKINIT (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*)
 ::#
 ::#
 :dk_set
-::setlocal
-::    %dk_call% dk_debugFunc 1 2
-	::echo dk_set(%*)
-	set "name=%~1"
-	set "_args_=%*"
-	echo _args_ = '%_args_%'
-	if defined _args_ (set "_args_=!_args_:%name% =!")
-	
-	echo _args_ = '%_args_%'
-	if "%_args_:~-1%"==" " set "_args_=%_args_:~0,-1%"
-    set "%name%=%_args_%"
-	
+setlocal
+::	%dk_call% dk_debugFunc 1 2
 
+	set "_name_=%~1"
+	set "_args_=%*"
+
+	if defined _args_ (set "_args_=!_args_:%_name_% =!")
+	
+    endlocal & (
+		set "%_name_%=%_args_%"
+	)
+	
 ::DEBUG
 ::    %dk_call% dk_printVar %~1
 %endfunction%
@@ -35,6 +34,6 @@ setlocal
     %dk_call% dk_set myVar "value assigned with dk_set"
     %dk_call% dk_echo "myVar = %myVar%"
 	
-	%dk_call% dk_set myVarB "dk_info('test dk_info message')"
+	%dk_call% dk_set myVarB dk_info('test dk_info message')
 	%dk_call% dk_echo "myVarB = %myVarB%"
 %endfunction%
