fdefc90c2a5c52fd4144a8d9d925e00909d98a2c
aquawicket <aquawicket@hotmail.com>
2025-02-28 17:07:44 -0500

    git commit Fri 02/28/2025

---
+++ DKBatch/functions/dk_callDKCSharp.cmd
@@ -0,0 +1,107 @@
+@echo off
+if not defined DK_CMD (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*)
+
+::####################################################################
+::# dk_callDKCpp(function, arguments...)
+::# dk_callDKCpp(function, arguments..., rtn_var)
+::#
+::#
+dk_callDKCSharp
+setlocal enableDelayedExpansion
+    %dk_call% dk_debugFunc 1 99
+ 
+	::###### TODO ######
+	dk_todo("dk_callDKCSharp")
+ 
+ 
+	::###### DEFAULT ENVIRONMENT ######
+	:: clang, cosmocc, gcc, msvc 
+	set "default_target_os=%host_os%"
+	set "default_target_arch=%host_arch%"
+	set "default_target_env=csc"
+	
+	::###### _func_ ######
+	set "_func_=%~1"
+	
+	::###### _args_ ######
+	set _args_=%*
+	if defined _args_ (set _args_=!_args_:%~1 =!)
+	
+	::###### _last_arg_ ######
+	for %%a in (%*) do set _last_arg_=%%a
+	
+	
+	::###### DKC_FUNCTIONS_DIR ######
+	%dk_call% dk_validate DKCSHARP_FUNCTIONS_DIR  "%dk_call% dk_DKBRANCH_DIR"
+	%dk_call% dk_assertPath DKCSHARP_FUNCTIONS_DIR
+	
+	::###### DKHTTP_DKC_FUNCTIONS_DIR ######
+	%dk_call% dk_assertVar DKHTTP_DKBRANCH_DIR
+	if not defined DKHTTP_DKCSHARP_DIR         	 (set "DKHTTP_DKCSHARP_DIR=%DKHTTP_DKBRANCH_DIR%/DKC")
+	if not defined DKHTTP_DKCSHARP_FUNCTIONS_DIR  (set "DKHTTP_DKCSHARP_FUNCTIONS_DIR=%DKHTTP_DKCSHARP_DIR%/functions")
+	
+	::###### Download files if missing ######
+	if not exist %DKCSHARP_FUNCTIONS_DIR%/DK.cs    (%dk_call% dk_download "%DKHTTP_DKCSHARP_FUNCTIONS_DIR%/DK.cs" "%DKCSHARP_FUNCTIONS_DIR%/DK.cs")
+	if not exist %DKCSHARP_FUNCTIONS_DIR%/%~1.cs   (%dk_call% dk_download "%DKHTTP_DKCSHARP_FUNCTIONS_DIR%/%~1.cs" "%DKCSHARP_FUNCTIONS_DIR%/%~1.cs")
+
+	::###### target_os ######
+	if not defined target_os (set "target_os=%default_target_os%")
+	%dk_call% dk_debug "target_os = %target_os%"
+	
+	::###### target_arch ######
+	if not defined target_arch (set "target_arch=%default_target_arch%")
+	%dk_call% dk_debug "target_arch = %target_arch%"
+	
+	::###### target_env ######
+	if not defined target_env (set "target_env=%default_target_env%")
+	%dk_call% dk_debug "target_env = %target_env%"
+	
+    ::###### CSC_EXE ######
+	:: TODO:  note, reference dk_install.cmd for how to call DKCSharp methods from batch script
+	
+	::###### _c_file_ ######
+	set "_csharp_file_=%DKCSHARP_FUNCTIONS_DIR%/%_func_%.cs"
+	%dk_call% dk_assertPath _csharp_file_
+	
+	::###### DKC_BUILD_DIR ######
+	%dk_call% dk_validate DKCACHE_DIR "%dk_call% dk_DKCACHE_DIR"
+	set "DKCSHARP_BUILD_DIR=%DKCACHE_DIR%/DKCSHARP_BUILD_DIR"
+	if not exist "%DKCSHARP_BUILD_DIR%" (%dk_call% dk_makeDirectory "%DKCSHARP_BUILD_DIR%")
+	
+	::###### _app_exe_ ######
+	set "_app_exe_=%DKCSHARP_BUILD_DIR%/%_func_%.exe"
+	
+	::###### Compile Code ######
+	%dk_call% dk_echo "compiling ..."
+	if exist "%_app_exe_%" (%dk_call% dk_delete "%_app_exe_%")
+
+	%dk_call% dk_debug "COMPILER_EXE = %COMPILER_EXE%"
+	set "COMPILE_COMMAND=%COMPILER_EXE% -o %_app_exe_% -static %_csharp_file_%"
+	echo %COMPILE_COMMAND%
+	%COMPILE_COMMAND%
+	
+	if not exist "%_app_exe_%" (
+		%dk_call% dk_echo
+		%dk_call% dk_error "failed to compile %_csharp_file_%"
+		%return%
+	)
+	
+	::###### run executable ######
+	%dk_call% dk_commandToVariable %_app_exe_% %_args_%
+	endlocal & (
+		set "dk_callDKCpp=%dk_commandToVariable%"
+	)
+%endfunction%
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+setlocal
+	%dk_call% dk_debugFunc 0
+
+	%dk_call% dk_callDKCpp dk_test "arg 1" "arg 2" "arg 3"
+    %dk_call% dk_echo
+	%dk_call% dk_echo "dk_callDKCpp = %dk_callDKCpp%"
+%endfunction%
