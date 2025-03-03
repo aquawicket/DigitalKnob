d35f03325ac229052fbd705d01172231013600ab
aquawicket <aquawicket@hotmail.com>
2025-03-03 12:13:41 -0500

    git commit Mon 03/03/2025

--- 3rdParty/_DKIMPORTS/git/contextMenu/dk_install.cmd
+++ 3rdParty/_DKIMPORTS/git/contextMenu/dk_install.cmd
@@ -1,5 +1,5 @@
 @echo off
-if not defined DKBATCH_FUNCTIONS_DIR_ (set "DKBATCH_FUNCTIONS_DIR_=../../../DKBatch/functions/")
+if not defined DKBATCH_FUNCTIONS_DIR_ (set "DKBATCH_FUNCTIONS_DIR_=../../../../DKBatch/functions/")
 if not defined DK_CMD (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*)
 
 ::####################################################################
@@ -29,5 +29,5 @@ if not defined DK_CMD (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*)
 setlocal
 	%dk_call% dk_debugFunc 0
 	
-    %dk_call% dk_installGitContextMenu
+    call :dk_installGitContextMenu
 %endfunction%
