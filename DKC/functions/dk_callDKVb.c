ac9722bb7332ccec287f86d70a1870c06d26f833
aquawicket <aquawicket@hotmail.com>
2025-03-02 21:43:34 -0500

    git commit Sun 03/02/2025

---
+++ DKC/functions/dk_callDKVb.c
@@ -0,0 +1,18 @@
+///$(which true);FLAGS="-g -Wall -Wextra --std=c17 -O1 -fsanitize=address,undefined";THIS_FILE="$(cd "$(dirname "$0")"; pwd -P)/$(basename "$0")";OUT_FILE="tmp/build-cache/$THIS_FILE";mkdir -p "$(dirname "$OUT_FILE")";test "$THIS_FILE" -ot "$OUT_FILE" || $(which clang || which gcc) $FLAGS "$THIS_FILE" -o "$OUT_FILE" || exit $?;exec bash -c "exec -a \"$0\" \"$OUT_FILE\" $([ $# -eq 0 ] || printf ' "%s"' "$@")"
+
+#include "dk_callDKVb.h"
+#include "dk_echo.h"
+
+//###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+#ifndef DKMAIN
+#define DKMAIN 1
+int main(int argc, char** argv) {
+	//dk_debugFunc(0);
+	
+	char rtn_var[1035] = "undefined";
+	int exit_status = dk_callDKVb("dk_test", "FROM DKVb", rtn_var);
+	dk_echo("rtn_var = %s", rtn_var);
+	
+	return exit_status;
+}
+#endif
