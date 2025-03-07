fcb9205e9c92db39ec1576c6b4df6346d74f1387
aquawicket <aquawicket@hotmail.com>
2025-03-06 22:04:26 -0500

    git commit Thu 03/06/2025

--- DKBatch/functions/Base64/dk_embedBin.cmd
+++ DKBatch/functions/Base64/dk_embedBin.cmd
@@ -2,6 +2,8 @@
 if not exist "%DKBATCH_FUNCTIONS_DIR_%" (set "DKBATCH_FUNCTIONS_DIR_=../")
 if not defined DK_CMD (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*)
 
+set "dk_embedBin_DELETE_CMD_ON_EXTRACT=0"
+set "dk_embedBin_OVERWRITE_OUTPUT=1"
 ::##################################################################################
 ::# Base64::dk_embedBin(inputFile)
 ::# Base64::dk_embedBin(inputFile, outputFile)
@@ -15,8 +17,7 @@ setlocal
 
 	set "inputFile=%~1"
 	set "inputFilename=%~nx1"
-	set "outputFile=%~nx1.cmd"
-	if not "%~2"=="" (set "outputFile=%~2")
+	if "%~2"=="" (set "outputFile=%~nx1.cmd") else (set "outputFile=%~2")
 	if "%~3"=="OVERWRITE" (set "OVERWRITE=1") else (set "OVERWRITE=0")
 
 	if not exist "%inputFile%" (%dk_call% dk_error "%inputFile% not found")
@@ -24,21 +25,29 @@ setlocal
 		if not "%OVERWRITE%"=="1" (
 			%dk_call% dk_error "dk_embedBin Cannot embed file. Destiantion already exists and OVERWRITE is not set"
 		)
-		del %outputFile:/=\%
+		del "%outputFile:/=\%"
 	)
 
+	if "%dk_embedBin_DELETE_CMD_ON_EXTRACT%"=="1" (
+		set DELETE_CMD_ON_EXTRACT=echo ;;;===,,,del "%outputFile:/=\%"
+	)
+	if not "%dk_embedBin_OVERWRITE_OUTPUT%"=="1" (
+		set OVERWRITE_OUTPUT=echo ;;;===,,,if exist "%inputFilename%" echo ERROR: outputfile already exists ^& pause ^& exit /b %%errorlevel%%
+	)
 	set "tempCmd=%inputFile%.tmp"
 	(
 	echo ;;;===,,,@echo off
+	%OVERWRITE_OUTPUT%
 	echo ;;;===,,,echo decoding . . .
-	echo ;;;===,,,findstr /v "^;;;===,,," "%%~f0" ^> %inputFilename%
-	echo ;;;===,,,%inputFilename%
-	echo ;;;===,,,exit /b
+	echo ;;;===,,,findstr /v "^;;;===,,," "%%~f0" ^> "%inputFilename%"
+	echo ;;;===,,,start "" /b "%inputFilename%"
+	%DELETE_CMD_ON_EXTRACT%
+	echo ;;;===,,,exit /b %%errorlevel%%
 
 	) >"%tempCmd%"
 
-	copy /a %tempCmd:/=\% + /b %inputFile:/=\% /b %outputFile:/=\%
-	del %tempCmd:/=\%
+	copy /a "%tempCmd:/=\%" + /b "%inputFile:/=\%" /b "%outputFile:/=\%"
+	del "%tempCmd:/=\%"
 %endfunction%
 
 
@@ -54,8 +63,8 @@ setlocal
     ::set "input=%DKBRANCH_DIR%/DKBuilder.cmd"
     ::set "output=%DKBRANCH_DIR%/DKBuilder.cmd.b64"
 	
-    %dk_call% dk_selectFile input
-    %dk_call% dk_embedBin "%input%"
+    %dk_call% dk_selectFile
+    %dk_call% dk_embedBin "%dk_selectFile%"
 	
     ::%dk_call% Base64::dk_encode "%input%" "test.b64"
 %endfunction%
