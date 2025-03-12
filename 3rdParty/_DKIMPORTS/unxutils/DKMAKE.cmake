0cbb6b4b539c8e194a983d7fd869d3152a6b8fb8
aquawicket <aquawicket@hotmail.com>
2025-03-12 16:50:28 -0400

    git commit Wed 03/12/2025

---
+++ 3rdParty/_DKIMPORTS/unxutils/DKMAKE.cmake
@@ -0,0 +1,18 @@
+#!/usr/bin/cmake -P
+if(NOT EXISTS "$ENV{DKCMAKE_FUNCTIONS_DIR_}")
+	file(TO_CMAKE_PATH "$ENV{USERPROFILE}$ENV{HOME}/digitalknob/Development/DKCMake/functions" DKCMAKE_FUNCTIONS_DIR)
+	set(ENV{DKCMAKE_FUNCTIONS_DIR_} "${DKCMAKE_FUNCTIONS_DIR}/")
+endif()
+include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+include_guard()
+
+
+###### unxutils ######
+# https://unxutils.sourceforge.net
+
+
+### INSTALL ###
+dk_validate		(DKIMPORTS_DIR "dk_DKIMPORTS_DIR()")
+#dk_import		("https://unxutils.sourceforge.net/UnxUtils.zip")
+dk_import		("https://unxutils.sourceforge.net/UnxUpdates.zip")
+
--- DKBatch/functions/dk_findProgram.cmd
+++ DKBatch/functions/dk_findProgram.cmd
@@ -1,14 +1,15 @@
 @echo off
 if not defined DK_CMD (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*)
 
+set return_error=endlocal ^& call dk_returnError
 ::################################################################################
 ::dk_findProgram(<var> name [path1 path2 ...])
 ::#
 ::#
 :dk_findProgram
 setlocal enableDelayedExpansion
-	%dk_call% dk_debugFunc 2 9 
-
+	%dk_call% dk_debugFunc 2 9
+	
 	set "_var_=%~1
 	set "_val_=!%_var_%!"
 	if exist "%_val_%" (
@@ -23,14 +24,16 @@ setlocal enableDelayedExpansion
 		set "_recursive_=/R"
 	)
 
-	%dk_call% dk_commandToVariable where %_recursive_% %_pattern_% %_filename_%
+	%dk_call% dk_commandToVariable where %_recursive_% %_pattern_% %_filename_% 2>nul
 	if defined dk_commandToVariable (
 		%dk_call% dk_assertPath %dk_commandToVariable:\=/%
 		set "dk_findProgram=%dk_commandToVariable:\=/%"
 	) else (
-		%dk_call% dk_error "dk_findProgram: %_filename_% not found"
+		rem endlocal & %dk_call% dk_returnError "dk_findProgram: %_filename_% not found" & exit /b 1
+		::%return_error% "dk_findProgram: %_filename_% not found"
+		call dk_returnError "test"
 	)
-		
+
 	endlocal & (
 		set "%~1=%dk_findProgram%"
 	)
@@ -41,21 +44,26 @@ setlocal enableDelayedExpansion
 
 
 
+
+
+
+
 ::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
 :DKTEST
-setlocal
+setlocal enableDelayedExpansion
 	%dk_call% dk_debugFunc 0
-
+	
+	
 	%dk_call% dk_validate DKTOOLS_DIR "%dk_call% dk_DKTOOLS_DIR"
-	%dk_call% dk_findProgram PWSH_EXE "pwsh.exe" "%DKTOOLS_DIR%"
+	%dk_call% dk_findProgram PWSH_EXE "pwsh.exe" "%DKTOOLS_DIR%" || echo %red%LAST_ERROR = %LAST_ERROR%!LAST_ERROR!%clr%
 	%dk_call% dk_printVar PWSH_EXE
 	
-	%dk_call% dk_findProgram POWERSHELL_EXE "powershell.exe"
+	%dk_call% dk_findProgram POWERSHELL_EXE "powershell.exe" || echo %red%LAST_ERROR = %LAST_ERROR%!LAST_ERROR!%clr%
 	%dk_call% dk_printVar POWERSHELL_EXE
 	
-	%dk_call% dk_findProgram CMD_EXE "cmd.exe" "C:/Windows/System32"
+	%dk_call% dk_findProgram CMD_EXE "cmd.exe" "C:/Windows/System32" || echo %red%LAST_ERROR = %LAST_ERROR%!LAST_ERROR!%clr%
 	%dk_call% dk_printVar CMD_EXE
 	
-	%dk_call% dk_findProgram CMD_EXE "cmd.exe"
+	%dk_call% dk_findProgram CMD_EXE "cmd.exe" || echo %red%LAST_ERROR = %LAST_ERROR%!LAST_ERROR!%clr%
 	%dk_call% dk_printVar CMD_EXE
 %endfunction%
