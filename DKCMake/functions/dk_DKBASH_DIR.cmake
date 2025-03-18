735b8c8c02b1926bb7ed923326f373d2c39ec448
aquawicket <aquawicket@hotmail.com>
2025-03-18 14:00:02 -0400

    git commit Tue 03/18/2025

--- DKCMake/functions/dk_DKBASH_DIR.cmake
+++ DKCMake/functions/dk_DKBASH_DIR.cmake
@@ -11,16 +11,16 @@ function(dk_DKBASH_DIR)
 
 	###### SET ######
 	if(ARGN)
-		dk_set(DKBASH_DIR "${ARGN}")
-		
+		dk_set(ENV{DKBASH_DIR} "${ARGN}")
+
 	###### GET ######
 	else()
 		dk_validate(ENV{DKBRANCH_DIR} "dk_DKBRANCH_DIR()")
-		dk_set(DKBASH_DIR "$ENV{DKBRANCH_DIR}/DKBash")
+		set(ENV{DKBASH_DIR} "$ENV{DKBRANCH_DIR}/DKBash")
 	endif()
-	
-	dk_set(DKBASH_FUNCTIONS_DIR "${DKBASH_DIR}/functions")
-	dk_set(DKBASH_FUNCTIONS_DIR_ "${DKBASH_DIR}/functions/")
+
+	dk_set(DKBASH_FUNCTIONS_DIR "$ENV{DKBASH_DIR}/functions")
+	dk_set(DKBASH_FUNCTIONS_DIR_ "$ENV{DKBASH_DIR}/functions/")
 endfunction()
 
 
@@ -31,14 +31,14 @@ endfunction()
 ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
 function(DKTEST)
 	dk_debugFunc(0)
-	
+
 	dk_echo()
 	dk_echo("Test Getting DKBASH_DIR . . .")
 	dk_DKBASH_DIR()
-	dk_printVar(DKBASH_DIR)
-	
+	dk_printVar(ENV{DKBASH_DIR})
+
 	dk_echo()
 	dk_echo("Test Setting DKBASH_DIR . . .")
 	dk_DKBASH_DIR("C:/DK/DKBash")
-	dk_printVar(DKBASH_DIR)
+	dk_printVar(ENV{DKBASH_DIR})
 endfunction()
