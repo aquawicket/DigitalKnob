932444ab6433d0ce8eb8ee28a789481595fda5bf
aquawicket <aquawicket@hotmail.com>
2025-04-11 20:40:55 -0400

    git commit Fri 04/11/2025

---
+++ DKBatch/functions/Android/dk_UninstallPackage.cmd
@@ -0,0 +1,26 @@
+@echo off
+if not exist "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" for /F "tokens=*" %%A IN ('where /r "%USERPROFILE%" DK.cmd') do (set "DKBATCH_FUNCTIONS_DIR_=%%~dpA")
+if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
+
+::################################################################################
+::# dk_UninstallPackage(name)
+::#
+:dk_UninstallPackage
+setlocal
+	%dk_call% dk_debugFunc 0
+
+	%dk_call% dk_validate ADB_EXE "%dk_call% dk_ADB_EXE"
+	"%ADB_EXE%" shell pm uninstall %~1
+    "%ADB_EXE%" shell pm uninstall --user 0 %~1
+%endfunction%
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+setlocal
+	%dk_call% dk_debugFunc 0
+
+	%dk_call% Android/dk_UninstallPackage
+%endfunction%
