c8954fec010e746cd8244f0b6f60f96d045aa15f
aquawicket <aquawicket@hotmail.com>
2025-04-11 20:22:11 -0400

    git commit Fri 04/11/2025

---
+++ DKBatch/functions/Android/dk_listPackages.cmd
@@ -0,0 +1,25 @@
+@echo off
+if not exist "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" for /F "tokens=*" %%A IN ('where /r "%USERPROFILE%" DK.cmd') do (set "DKBATCH_FUNCTIONS_DIR_=%%~dpA")
+if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
+
+::################################################################################
+::# dk_listPackages()
+::#
+:dk_listPackages
+setlocal
+	%dk_call% dk_debugFunc 0
+
+	%dk_call% dk_validate ADB_EXE "%dk_call% dk_ADB_EXE"
+	"%ADB_EXE%" shell pm list packages -f
+%endfunction%
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+setlocal
+	%dk_call% dk_debugFunc 0
+
+	%dk_call% Android/dk_listPackages
+%endfunction%
