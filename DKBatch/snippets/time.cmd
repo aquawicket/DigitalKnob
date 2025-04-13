0d542f7876a56affc27d4e071c06d9221b83d666
aquawicket <aquawicket@hotmail.com>
2025-04-13 03:32:14 -0400

    git commit Sun 04/13/2025

--- DKBatch/snippets/time.cmd
+++ DKBatch/snippets/time.cmd
@@ -1,4 +1,5 @@
 @echo off
+if not defined DKBATCH_FUNCTIONS_DIR_ (set "DKBATCH_FUNCTIONS_DIR_=%CD:\=/%/../functions/")
 if not exist "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" for /F "tokens=*" %%A IN ('where /r "%USERPROFILE%" DK.cmd') do (set "DKBATCH_FUNCTIONS_DIR_=%%~dpA")
 if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 
