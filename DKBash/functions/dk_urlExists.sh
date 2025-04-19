8a9b54af5214fec1d54c6b3fb85303f9b97519de
aquawicket <aquawicket@hotmail.com>
2025-04-18 21:51:10 -0400

    git commit Fri 04/18/2025

--- DKBash/functions/dk_urlExists.sh
+++ DKBash/functions/dk_urlExists.sh
@@ -9,11 +9,26 @@
 dk_urlExists() {
 	dk_debugFunc 1
 
-	if curl --head --silent --fail $1 1> /dev/null 2>1; then
-		return $(true)
+	dk_call dk_httpResponse ${1}
+	
+	#echo dk_httpResponse = %dk_httpResponse%
+
+	if [ ${dk_httpResponse} -eq 200 ]; then
+		dk_urlExists=0
+	elif [ ${dk_httpResponse} -eq 301 ]; then
+		dk_urlExists=0
+	elif [ ${dk_httpResponse} -eq 302 ]; then
+		dk_urlExists=0
+	else
+		dk_urlExists=1
 	fi
 	
-	return $(false)
+
+	export dk_urlExists=${dk_urlExists}
+	#if ["$2" -ne "" ]; then "$2=${dk_urlExists}"
+	
+	dk_call dk_echo "dk_urlExists = ${dk_urlExists}"
+	return ${dk_urlExists}
 }
 
 
@@ -21,6 +36,22 @@ dk_urlExists() {
 ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
 DKTEST() {
 
+	###### Using if return value
+	dk_call dk_echo
+	url="http://www.google.com/index.html"
+	dk_call dk_urlExists ${url}
+	[ ${dk_urlExists} -eq 0 ] && dk_call dk_echo "${url} exists" || dk_call dk_echo "${url} does NOT exist"
+	dk_call dk_echo "dk_urlExists = ${dk_urlExists}"
+	
+	dk_call dk_echo
+	url="http://www.nonexisting.com/nofile.no"
+	dk_call dk_urlExists "${url}"
+	[ ${dk_urlExists} -eq 0 ] && dk_call dk_echo "${url%} exists" || dk_call dk_echo "${url} does NOT exist"
+	dk_call dk_echo "dk_urlExists = ${dk_urlExists}"
+	#FIXME: ERRORLEVEL is still 1 
+	
+	
+	
 	if dk_urlExists "http://www.google.com/index.html"; then
 		echo "The url exists"
 	else
