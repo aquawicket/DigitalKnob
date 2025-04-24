9900b1ec81640f3fa047e6b642388b59351a80e7
aquawicket <aquawicket@hotmail.com>
2025-04-24 18:06:17 -0400

    git commit Thu 04/24/2025

--- DKBatch/functions/dk_gitMergeBranch.cmd
+++ DKBatch/functions/dk_gitMergeBranch.cmd
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off&::########################################## DigitalKnob DKBatch ########################################################################
 if not exist "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" for /F "tokens=*" %%G IN ('where /r "%USERPROFILE%" DK.cmd') do (set "DKBATCH_FUNCTIONS_DIR_=%%~dpG")
 if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*) 
@@ -193,4 +192,4 @@ echo Bringing %branch% up to date with %destination%
  
 echo THE MERGE IS COMPLETE.
 %DKEND% 
->>>>>>> main
+
