b1e6ab363e8da074a5b04bd5cea18a10c817901b
aquawicket <aquawicket@hotmail.com>
2025-04-24 15:08:57 -0400

    git commit Thu 04/24/2025

--- DKBatch/DKINSTALL.cmd
+++ DKBatch/DKINSTALL.cmd
@@ -1,19 +1,67 @@
 @echo off
+echo line:2 %~0(%*)
+
+
+goto:after
+set "DE=1"
+setlocal enableDelayedExpansion
+set PRINT_DE_VAR_A=if ^!DE^! equ 1 (echo ^!DE^! equ 1) else (echo ^!DE^! neq 1)
+set PRINT_DE_VAR_B=if "^!DE^!" equ "1" (echo "^!DE^!" equ "1") else (echo "^!DE^!" neq "1")
+set PRINT_DE_VAR_C=if "!!DE!!" equ "1" (echo "3" equ "1") else (echo "3" neq "1")
+set PRINT_DE_VAR_D=if !!DE!! equ 1 (echo 4 equ 1) else (echo 4 neq 1)
+set PRINT_DE_STATUS=if ^!DE^! neq 1 (echo disabled_1) else if "^!DE^!" equ "1" (echo enabled_1) else if "!!DE!!" neq "1" (echo enabled_2) else (echo disabled_2)
+
+echo:
+setlocal enableDelayedExpansion
+echo ENABLED
+%PRINT_DE_VAR_A%
+%PRINT_DE_VAR_B%
+%PRINT_DE_VAR_C%
+%PRINT_DE_VAR_D%
+%PRINT_DE_STATUS% 
+echo DE = %DE% = !DE!
+
+echo:
+setlocal disableDelayedExpansion
+echo DISABLED
+%PRINT_DE_VAR_A%
+%PRINT_DE_VAR_B%
+%PRINT_DE_VAR_C%
+%PRINT_DE_VAR_D%
+%PRINT_DE_STATUS%
+echo DE = %DE% = !DE!
+
+echo:
+setlocal enableDelayedExpansion
+echo ENABLED
+%PRINT_DE_VAR_A%
+%PRINT_DE_VAR_B%
+%PRINT_DE_VAR_C%
+%PRINT_DE_VAR_D%
+%PRINT_DE_STATUS%
+echo DE = %DE% = !DE!
+:after
+
+
+
 if "%~1" equ "" (goto DKINSTALL)
 
 :runDKcmd
+	echo :runDKcmd
 	if not exist "%DKBATCH_FUNCTIONS_DIR%"	(set "DKBATCH_FUNCTIONS_DIR=%~1")
 	if not exist "%DKBATCH_FUNCTIONS_DIR_%"	(set "DKBATCH_FUNCTIONS_DIR_=%~1\")
-	if not exist "%ComSpec%"				(set "ComSpec=%~2")
-	if not exist "%DKCACHE_DIR%"			(set "DKCACHE_DIR=%~3")
-	if not exist "%DKSCRIPT_PATH%"			(set "DKSCRIPT_PATH=%~4")
+	if not exist "%CMD_EXE%"				(set "CMD_EXE=%~2")
+	if not exist "%DKSCRIPT_PATH%"			(set "DKSCRIPT_PATH=%~3")
 	if not defined DKSCRIPT_ARGS			(for /F "usebackq tokens=4*" %%a in ('%*') do set DKSCRIPT_ARGS=%%b)
-
+	if not exist "%TCC_EXE%"				(set "TCC_EXE=C:/Users/Administrator/digitalknob/DKTools/tcc-rt-master/tcc.exe")
+	
 	::###### run script ######
 	:: "%ComSpec%"	path to cmd.exe
 	:: /V:ON		enable delayed expansion
 	:: /K			keep the window open at the CMD prompt.
-	"%ComSpec%" /V:ON /K call "%DKSCRIPT_PATH%"
+	start "" "%CMD_EXE:/=\%" /V:ON /K "%DKSCRIPT_PATH%"
+	(start "" /b "%TCC_EXE:/=\%" /V:ON /K %DKSCRIPT_PATH% & pause)
+
 	::"%ComSpec%" /V:ON /K call "%DKSCRIPT_PATH%" %DKSCRIPT_ARGS%
 
 	::###### exit_code ######
@@ -37,6 +85,8 @@ if "%~1" equ "" (goto DKINSTALL)
 
 :DKINSTALL
 	if "%~1" neq "" (goto:eof)
+	echo :DKINSTALL
+	
 
 	::#################################################### DigitalKnob DKBatch ########################################################################
 	if not exist "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" for /F "tokens=*" %%A IN ('where /r "%USERPROFILE%" DK.cmd') do (set "DKBATCH_FUNCTIONS_DIR_=%%~dpA")
@@ -50,11 +100,11 @@ if "%~1" equ "" (goto DKINSTALL)
 	%dk_call% dk_validate CMD_EXE "%dk_call% dk_CMD_EXE"
 
 	:: Set the registry entry for the exxtension
-	ftype DKcmd="%ComSpec%" /c if exist "%~f0" ^
-	(echo DKcmd installed ^& "%ComSpec%" /c call "%~f0" "%DKBATCH_FUNCTIONS_DIR%" "%ComSpec%" "%DKCACHE_DIR%" "%%1" %%*) else ^
+	ftype DKcmd="%CMD_EXE:/=\%" /c if exist "%~f0" ^
+	(echo DKcmd installed ^& "%CMD_EXE:/=\%" /c call "%~f0" "%DKBATCH_FUNCTIONS_DIR%" "%CMD_EXE%" "%%1" %%*) else ^
 	(echo DKcmd not installed ^& "%%1" %%*)
 
-	%dk_call% dk_registrySetKey "HKCR\DKcmd\DefaultIcon" "" "REG_SZ" "%ComSpec%"
+	%dk_call% dk_registrySetKey "HKCR\DKcmd\DefaultIcon" "" "REG_SZ" "%CMD_EXE%"
 	assoc .cmd=DKcmd
 
 	%dk_call% dk_success "DKcmd install complete"
