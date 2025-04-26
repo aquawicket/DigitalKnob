2f62228c454a5290ebd61023e389607d44fc1f5e
aquawicket <aquawicket@hotmail.com>
2025-04-26 16:51:13 -0400

    git commit Sat 04/26/2025

--- DKJavascript/functions/dk_log.js
+++ DKJavascript/functions/dk_log.js
@@ -3,151 +3,151 @@ if(!dk_valid("dk_color"))	{ dk_source(DKJAVASCRIPT_DIR+"/functions/dk_color.js",
 
 
 //############ dk_log SETTINGS ############
-if(!dk_valid("dk_log_ENABLE"))			{ var dk_log_ENABLE = 1; }
+if(!dk_valid("dk_log_ENABLE"))				{ var dk_log_ENABLE = 1; }
 
 //### DEFAULT ###
-if(!dk_valid("DEFAULT"))				{ var DEFAULT="DEFAULT"; 			}
-if(!dk_valid("dk_log_DEFAULT_ENABLE"))	{ var dk_log_DEFAULT_ENABLE=1; 		}
-if(!dk_valid("dk_log_DEFAULT_COLOR"))	{ var dk_log_DEFAULT_COLOR=white; 	}
-if(!dk_valid("dk_log_DEFAULT_TAG"))		{ var dk_log_DEFAULT_TAG=""; 		}
-if(!dk_valid("dk_log_DEFAULT_TRACE"))	{ var dk_log_DEFAULT_TRACE=0;		}
-if(!dk_valid("dk_log_DEFAULT_LINE"))	{ var dk_log_DEFAULT_LINE=0; 		}
-if(!dk_valid("dk_log_DEFAULT_SOUND"))	{ var dk_log_DEFAULT_SOUND=0;		}
-if(!dk_valid("dk_log_DEFAULT_TIMEOUT"))	{ var dk_log_DEFAULT_TIMEOUT=0; 	}
-if(!dk_valid("dk_log_DEFAULT_PAUSE"))	{ var dk_log_DEFAULT_PAUSE=0; 		}
-if(!dk_valid("dk_log_DEFAULT_HALT"))	{ var dk_log_DEFAULT_HALT=0; 		}
+if(!dk_valid("DEFAULT"))					{ var DEFAULT="DEFAULT"; 					}
+if(!dk_valid("dk_log_DEFAULT_ENABLE"))		{ var dk_log_DEFAULT_ENABLE=1; 				}
+if(!dk_valid("dk_log_DEFAULT_COLOR"))		{ var dk_log_DEFAULT_COLOR=white; 			}
+if(!dk_valid("dk_log_DEFAULT_TAG"))			{ var dk_log_DEFAULT_TAG=""; 				}
+if(!dk_valid("dk_log_DEFAULT_TRACE"))		{ var dk_log_DEFAULT_TRACE=0;				}
+if(!dk_valid("dk_log_DEFAULT_LINE"))		{ var dk_log_DEFAULT_LINE=0; 				}
+if(!dk_valid("dk_log_DEFAULT_SOUND"))		{ var dk_log_DEFAULT_SOUND=0;				}
+if(!dk_valid("dk_log_DEFAULT_TIMEOUT"))		{ var dk_log_DEFAULT_TIMEOUT=0; 			}
+if(!dk_valid("dk_log_DEFAULT_PAUSE"))		{ var dk_log_DEFAULT_PAUSE=0; 				}
+if(!dk_valid("dk_log_DEFAULT_HALT"))		{ var dk_log_DEFAULT_HALT=0; 				}
 
 //### VERBOSE ###
-if(!dk_valid("VERBOSE"))				{ var VERBOSE="VERBOSE"; 			}
-var dk_log_VERBOSE_ENABLE=1;
-var dk_log_VERBOSE_COLOR=magenta;
-var dk_log_VERBOSE_TAG="VERBOSE: ";
-var dk_log_VERBOSE_TRACE=0;
-var dk_log_VERBOSE_LINE=0;
-var dk_log_VERBOSE_SOUND=0;
-var dk_log_VERBOSE_TIMEOUT=0;
-var dk_log_VERBOSE_PAUSE=0;
-var dk_log_VERBOSE_HALT=0;
+if(!dk_valid("VERBOSE"))					{ var VERBOSE="VERBOSE"; 					}
+if(!dk_valid("dk_log_VERBOSE_ENABLE"))		{ var dk_log_VERBOSE_ENABLE=1;				}
+if(!dk_valid("dk_log_VERBOSE_COLOR"))		{ var dk_log_VERBOSE_COLOR=magenta; 		}
+if(!dk_valid("dk_log_VERBOSE_TAG"))			{ var dk_log_VERBOSE_TAG="VERBOSE: "; 		}
+if(!dk_valid("dk_log_VERBOSE_TRACE"))		{ var dk_log_VERBOSE_TRACE=0;				}
+if(!dk_valid("dk_log_VERBOSE_LINE"))		{ var dk_log_VERBOSE_LINE=0; 				}
+if(!dk_valid("dk_log_VERBOSE_SOUND"))		{ var dk_log_VERBOSE_SOUND=0;				}
+if(!dk_valid("dk_log_VERBOSE_TIMEOUT"))		{ var dk_log_VERBOSE_TIMEOUT=0; 			}
+if(!dk_valid("dk_log_VERBOSE_PAUSE"))		{ var dk_log_VERBOSE_PAUSE=0; 				}
+if(!dk_valid("dk_log_VERBOSE_HALT"))		{ var dk_log_VERBOSE_HALT=0; 				}
 
 //### DEBUG ###
-if(!dk_valid("DEBUG"))					{ var DEBUG="DEBUG"; 				}
-var dk_log_DEBUG_ENABLE=1;
-var dk_log_DEBUG_COLOR=lblue;
-var dk_log_DEBUG_TAG="DEBUG: ";
-var dk_log_DEBUG_TRACE=0;
-var dk_log_DEBUG_LINE=0;
-var dk_log_DEBUG_SOUND=0;
-var dk_log_DEBUG_TIMEOUT=0;
-var dk_log_DEBUG_PAUSE=0;
-var dk_log_DEBUG_HALT=0;
+if(!dk_valid("DEBUG"))						{ var DEBUG="DEBUG"; 						}
+if(!dk_valid("dk_log_DEBUG_ENABLE"))		{ var dk_log_DEBUG_ENABLE=1;				}
+if(!dk_valid("dk_log_DEBUG_COLOR"))			{ var dk_log_DEBUG_COLOR=lblue; 			}
+if(!dk_valid("dk_log_DEBUG_TAG"))			{ var dk_log_DEBUG_TAG="DEBUG: "; 			}
+if(!dk_valid("dk_log_DEBUG_TRACE"))			{ var dk_log_DEBUG_TRACE=0;					}
+if(!dk_valid("dk_log_DEBUG_LINE"))			{ var dk_log_DEBUG_LINE=0; 					}
+if(!dk_valid("dk_log_DEBUG_SOUND"))			{ var dk_log_DEBUG_SOUND=0;					}
+if(!dk_valid("dk_log_DEBUG_TIMEOUT"))		{ var dk_log_DEBUG_TIMEOUT=0; 				}
+if(!dk_valid("dk_log_DEBUG_PAUSE"))			{ var dk_log_DEBUG_PAUSE=0; 				}
+if(!dk_valid("dk_log_DEBUG_HALT"))			{ var dk_log_DEBUG_HALT=0; 					}
 
 //### INFO ###
-if(!dk_valid("INFO"))					{ var INFO="INFO"; 					}
-var dk_log_INFO_ENABLE=1;
-var dk_log_INFO_COLOR=white;
-var dk_log_INFO_TAG="INFO: ";
-var dk_log_INFO_TRACE=0;
-var dk_log_INFO_LINE=0;
-var dk_log_INFO_SOUND=0;
-var dk_log_INFO_TIMEOUT=0;
-var dk_log_INFO_PAUSE=0;
-var dk_log_INFO_HALT=0;
+if(!dk_valid("INFO"))						{ var INFO="INFO"; 							}
+if(!dk_valid("dk_log_INFO_ENABLE"))			{ var dk_log_INFO_ENABLE=1;					}
+if(!dk_valid("dk_log_INFO_COLOR"))			{ var dk_log_INFO_COLOR=white;				}
+if(!dk_valid("dk_log_INFO_TAG"))			{ var dk_log_INFO_TAG="INFO: ";				}
+if(!dk_valid("dk_log_INFO_TRACE"))			{ var dk_log_INFO_TRACE=0;					}
+if(!dk_valid("dk_log_INFO_LINE"))			{ var dk_log_INFO_LINE=0; 					}
+if(!dk_valid("dk_log_INFO_SOUND"))			{ var dk_log_INFO_SOUND=0;					}
+if(!dk_valid("dk_log_INFO_TIMEOUT"))		{ var dk_log_INFO_TIMEOUT=0; 				}
+if(!dk_valid("dk_log_INFO_PAUSE"))			{ var dk_log_INFO_PAUSE=0; 					}
+if(!dk_valid("dk_log_INFO_HALT"))			{ var dk_log_INFO_HALT=0; 					}
 
 //### SUCCESS ###
-if(!dk_valid("SUCCESS"))				{ var SUCCESS="SUCCESS"; 			}
-var dk_log_SUCCESS_ENABLE=1;
-var dk_log_SUCCESS_COLOR=green;
-var dk_log_SUCCESS_TAG="SUCCESS: ";
-var dk_log_SUCCESS_TRACE=0;
-var dk_log_SUCCESS_LINE=0;
-var dk_log_SUCCESS_SOUND=0;
-var dk_log_SUCCESS_TIMEOUT=0;
-var dk_log_SUCCESS_PAUSE=0;
-var dk_log_SUCCESS_HALT=0;
+if(!dk_valid("SUCCESS"))					{ var SUCCESS="SUCCESS"; 					}
+if(!dk_valid("dk_log_SUCCESS_ENABLE"))		{ var dk_log_SUCCESS_ENABLE=1;				}
+if(!dk_valid("dk_log_SUCCESS_COLOR"))		{ var dk_log_SUCCESS_COLOR=green;			}
+if(!dk_valid("dk_log_SUCCESS_TAG"))			{ var dk_log_SUCCESS_TAG="SUCCESS: ";		}
+if(!dk_valid("dk_log_SUCCESS_TRACE"))		{ var dk_log_SUCCESS_TRACE=0;				}
+if(!dk_valid("dk_log_SUCCESS_LINE"))		{ var dk_log_SUCCESS_LINE=0; 				}
+if(!dk_valid("dk_log_SUCCESS_SOUND"))		{ var dk_log_SUCCESS_SOUND=0;				}
+if(!dk_valid("dk_log_SUCCESS_TIMEOUT"))		{ var dk_log_SUCCESS_TIMEOUT=0; 			}
+if(!dk_valid("dk_log_SUCCESS_PAUSE"))		{ var dk_log_SUCCESS_PAUSE=0; 				}
+if(!dk_valid("dk_log_SUCCESS_HALT"))		{ var dk_log_SUCCESS_HALT=0; 				}
 
 //### TODO ###
-if(!dk_valid("TODO"))					{ var TODO="TODO"; 					}
-var dk_log_TODO_ENABLE=1;
-var dk_log_TODO_COLOR=blue;
-var dk_log_TODO_TAG="TODO: ";
-var dk_log_TODO_TRACE=0;
-var dk_log_TODO_LINE=0;
-var dk_log_TODO_SOUND=0;
-var dk_log_TODO_TIMEOUT=1;
-var dk_log_TODO_PAUSE=0;
-var dk_log_TODO_HALT=0;
+if(!dk_valid("TODO"))						{ var TODO="TODO"; 							}
+if(!dk_valid("dk_log_TODO_ENABLE"))			{ var dk_log_TODO_ENABLE=1;					}
+if(!dk_valid("dk_log_TODO_COLOR"))			{ var dk_log_TODO_COLOR=blue;				}
+if(!dk_valid("dk_log_TODO_TAG"))			{ var dk_log_TODO_TAG="TODO: ";				}
+if(!dk_valid("dk_log_TODO_TRACE"))			{ var dk_log_TODO_TRACE=0;					}
+if(!dk_valid("dk_log_TODO_LINE"))			{ var dk_log_TODO_LINE=0; 					}
+if(!dk_valid("dk_log_TODO_SOUND"))			{ var dk_log_TODO_SOUND=0;					}
+if(!dk_valid("dk_log_TODO_TIMEOUT"))		{ var dk_log_TODO_TIMEOUT=1; 				}
+if(!dk_valid("dk_log_TODO_PAUSE"))			{ var dk_log_TODO_PAUSE=0; 					}
+if(!dk_valid("dk_log_TODO_HALT"))			{ var dk_log_TODO_HALT=0; 					}
 
 //### NOTICE ###
-if(!dk_valid("NOTICE"))					{ var NOTICE="NOTICE"; 				}
-var dk_log_NOTICE_ENABLE=1;
-var dk_log_NOTICE_COLOR=lyellow;
-var dk_log_NOTICE_TAG="NOTICE: ";
-var dk_log_NOTICE_TRACE=0;
-var dk_log_NOTICE_LINE=0;
-var dk_log_NOTICE_SOUND=0;
-var dk_log_NOTICE_TIMEOUT=0;
-var dk_log_NOTICE_PAUSE=0;
-var dk_log_NOTICE_HALT=0;
+if(!dk_valid("NOTICE"))						{ var NOTICE="NOTICE"; 						}
+if(!dk_valid("dk_log_NOTICE_ENABLE"))		{ var dk_log_NOTICE_ENABLE=1;				}
+if(!dk_valid("dk_log_NOTICE_COLOR"))		{ var dk_log_NOTICE_COLOR=lyellow;			}
+if(!dk_valid("dk_log_NOTICE_TAG"))			{ var dk_log_NOTICE_TAG="NOTICE: ";			}
+if(!dk_valid("dk_log_NOTICE_TRACE"))		{ var dk_log_NOTICE_TRACE=0;				}
+if(!dk_valid("dk_log_NOTICE_LINE"))			{ var dk_log_NOTICE_LINE=0; 				}
+if(!dk_valid("dk_log_NOTICE_SOUND"))		{ var dk_log_NOTICE_SOUND=0;				}
+if(!dk_valid("dk_log_NOTICE_TIMEOUT"))		{ var dk_log_NOTICE_TIMEOUT=1; 				}
+if(!dk_valid("dk_log_NOTICE_PAUSE"))		{ var dk_log_NOTICE_PAUSE=0; 				}
+if(!dk_valid("dk_log_NOTICE_HALT"))			{ var dk_log_NOTICE_HALT=0; 				}
 
 //### FIXME ###
-if(!dk_valid("FIXME"))					{ var FIXME="FIXME"; 				}
-var dk_log_FIXME_ENABLE=1;
-var dk_log_FIXME_COLOR=lyellow;
-var dk_log_FIXME_TAG="FIXME: ";
-var dk_log_FIXME_TRACE=0;
-var dk_log_FIXME_LINE=0;
-var dk_log_FIXME_SOUND=0;
-var dk_log_FIXME_TIMEOUT=3;
-var dk_log_FIXME_PAUSE=0;
-var dk_log_FIXME_HALT=0;
+if(!dk_valid("FIXME"))						{ var FIXME="FIXME"; 						}
+if(!dk_valid("dk_log_FIXME_ENABLE"))		{ var dk_log_FIXME_ENABLE=1;				}
+if(!dk_valid("dk_log_FIXME_COLOR"))			{ var dk_log_FIXME_COLOR=lyellow;			}
+if(!dk_valid("dk_log_FIXME_TAG"))			{ var dk_log_FIXME_TAG="FIXME: ";			}
+if(!dk_valid("dk_log_FIXME_TRACE"))			{ var dk_log_FIXME_TRACE=0;					}
+if(!dk_valid("dk_log_FIXME_LINE"))			{ var dk_log_FIXME_LINE=0; 					}
+if(!dk_valid("dk_log_FIXME_SOUND"))			{ var dk_log_FIXME_SOUND=0;					}
+if(!dk_valid("dk_log_FIXME_TIMEOUT"))		{ var dk_log_FIXME_TIMEOUT=3; 				}
+if(!dk_valid("dk_log_FIXME_PAUSE"))			{ var dk_log_FIXME_PAUSE=0; 				}
+if(!dk_valid("dk_log_FIXME_HALT"))			{ var dk_log_FIXME_HALT=0; 					}
 
 //### WARNING ###
-if(!dk_valid("WARNING"))				{ var WARNING="WARNING"; 			}
-var dk_log_WARNING_ENABLE=1;
-var dk_log_WARNING_COLOR=yellow;
-var dk_log_WARNING_TAG="WARNING: ";
-var dk_log_WARNING_TRACE=1;
-var dk_log_WARNING_LINE=0;
-var dk_log_WARNING_SOUND=0;
-var dk_log_WARNING_TIMEOUT=0;
-var dk_log_WARNING_PAUSE=0;
-var dk_log_WARNING_HALT=0;
+if(!dk_valid("WARNING"))					{ var WARNING="WARNING"; 					}
+if(!dk_valid("dk_log_WARNING_ENABLE"))		{ var dk_log_WARNING_ENABLE=1;				}
+if(!dk_valid("dk_log_WARNING_COLOR"))		{ var dk_log_WARNING_COLOR=yellow;			}
+if(!dk_valid("dk_log_WARNING_TAG"))			{ var dk_log_WARNING_TAG="WARNING: ";		}
+if(!dk_valid("dk_log_WARNING_TRACE"))		{ var dk_log_WARNING_TRACE=1;				}
+if(!dk_valid("dk_log_WARNING_LINE"))		{ var dk_log_WARNING_LINE=0; 				}
+if(!dk_valid("dk_log_WARNING_SOUND"))		{ var dk_log_WARNING_SOUND=0;				}
+if(!dk_valid("dk_log_WARNING_TIMEOUT"))		{ var dk_log_WARNING_TIMEOUT=0; 			}
+if(!dk_valid("dk_log_WARNING_PAUSE"))		{ var dk_log_WARNING_PAUSE=0; 				}
+if(!dk_valid("dk_log_WARNING_HALT"))		{ var dk_log_WARNING_HALT=0; 				}
 
 //### DEPRECATED ###
-if(!dk_valid("DEPRECATED"))				{ var DEPRECATED="DEPRECATED"; 		}
-var dk_log_DEPRECATED_ENABLE=1;
-var dk_log_DEPRECATED_COLOR=yellow;
-var dk_log_DEPRECATED_TAG="DEPRECATED: ";
-var dk_log_DEPRECATED_TRACE=0;
-var dk_log_DEPRECATED_LINE=0;
-var dk_log_DEPRECATED_SOUND=0;
-var dk_log_DEPRECATED_TIMEOUT=3;
-var dk_log_DEPRECATED_PAUSE=0;
-var dk_log_DEPRECATED_HALT=0;
+if(!dk_valid("DEPRECATED"))					{ var DEPRECATED="DEPRECATED"; 				}
+if(!dk_valid("dk_log_DEPRECATED_ENABLE"))	{ var dk_log_DEPRECATED_ENABLE=1;			}
+if(!dk_valid("dk_log_DEPRECATED_COLOR"))	{ var dk_log_DEPRECATED_COLOR=yellow;		}
+if(!dk_valid("dk_log_DEPRECATED_TAG"))		{ var dk_log_DEPRECATED_TAG="DEPRECATED: ";	}
+if(!dk_valid("dk_log_DEPRECATED_TRACE"))	{ var dk_log_DEPRECATED_TRACE=0;			}
+if(!dk_valid("dk_log_DEPRECATED_LINE"))		{ var dk_log_DEPRECATED_LINE=0; 			}
+if(!dk_valid("dk_log_DEPRECATED_SOUND"))	{ var dk_log_DEPRECATED_SOUND=0;			}
+if(!dk_valid("dk_log_DEPRECATED_TIMEOUT"))	{ var dk_log_DEPRECATED_TIMEOUT=3; 			}
+if(!dk_valid("dk_log_DEPRECATED_PAUSE"))	{ var dk_log_DEPRECATED_PAUSE=0; 			}
+if(!dk_valid("dk_log_DEPRECATED_HALT"))		{ var dk_log_DEPRECATED_HALT=0; 			}
 
 //### ERROR ###
-if(!dk_valid("ERROR"))					{ var ERROR="ERROR"; 				}
-var dk_log_ERROR_ENABLE=1;
-var dk_log_ERROR_COLOR=lred;
-var dk_log_ERROR_TAG="ERROR: ";
-var dk_log_ERROR_TRACE=0;
-var dk_log_ERROR_LINE=0;
-var dk_log_ERROR_SOUND=0;
-var dk_log_ERROR_TIMEOUT=1;
-var dk_log_ERROR_PAUSE=0;
-var dk_log_ERROR_HALT=0;
+if(!dk_valid("ERROR"))						{ var ERROR="ERROR"; 						}
+if(!dk_valid("dk_log_ERROR_ENABLE"))		{ var dk_log_ERROR_ENABLE=1;				}
+if(!dk_valid("dk_log_ERROR_COLOR"))			{ var dk_log_ERROR_COLOR=lred;				}
+if(!dk_valid("dk_log_ERROR_TAG"))			{ var dk_log_ERROR_TAG="ERROR: ";			}
+if(!dk_valid("dk_log_ERROR_TRACE"))			{ var dk_log_ERROR_TRACE=0;					}
+if(!dk_valid("dk_log_ERROR_LINE"))			{ var dk_log_ERROR_LINE=0; 					}
+if(!dk_valid("dk_log_ERROR_SOUND"))			{ var dk_log_ERROR_SOUND=0;					}
+if(!dk_valid("dk_log_ERROR_TIMEOUT"))		{ var dk_log_ERROR_TIMEOUT=1; 				}
+if(!dk_valid("dk_log_ERROR_PAUSE"))			{ var dk_log_ERROR_PAUSE=0; 				}
+if(!dk_valid("dk_log_ERROR_HALT"))			{ var dk_log_ERROR_HALT=0; 					}
 
 //### FATAL ###
-if(!dk_valid("FATAL"))					{ var FATAL="FATAL"; 				}
-var dk_log_FATAL_ENABLE=1;
-var dk_log_FATAL_COLOR=red;
-var dk_log_FATAL_TAG="FATAL: ";
-var dk_log_FATAL_TRACE=1;
-var dk_log_FATAL_LINE=0;
-var dk_log_FATAL_SOUND=1;
-var dk_log_FATAL_TIMEOUT=0;
-var dk_log_FATAL_PAUSE=0;
-var dk_log_FATAL_HALT=1;
+if(!dk_valid("FATAL"))						{ var FATAL="FATAL"; 						}
+if(!dk_valid("dk_log_FATAL_ENABLE"))		{ var dk_log_FATAL_ENABLE=1;				}
+if(!dk_valid("dk_log_FATAL_COLOR"))			{ var dk_log_FATAL_COLOR=red;				}
+if(!dk_valid("dk_log_FATAL_TAG"))			{ var dk_log_FATAL_TAG="FATAL: ";			}
+if(!dk_valid("dk_log_FATAL_TRACE"))			{ var dk_log_FATAL_TRACE=1;					}
+if(!dk_valid("dk_log_FATAL_LINE"))			{ var dk_log_FATAL_LINE=0; 					}
+if(!dk_valid("dk_log_FATAL_SOUND"))			{ var dk_log_FATAL_SOUND=1;					}
+if(!dk_valid("dk_log_FATAL_TIMEOUT"))		{ var dk_log_FATAL_TIMEOUT=1; 				}
+if(!dk_valid("dk_log_FATAL_PAUSE"))			{ var dk_log_FATAL_PAUSE=0; 				}
+if(!dk_valid("dk_log_FATAL_HALT"))			{ var dk_log_FATAL_HALT=1; 					}
 
 
 //##################################################################################
