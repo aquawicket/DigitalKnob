99e8000a3358deb97fd9b08ddb284740ff274ab5
aquawicket <aquawicket@hotmail.com>
2025-04-28 18:00:50 -0400

    git commit Mon 04/28/2025

---
+++ DKBatch/functions/dk_evalDKBash.cmd
@@ -0,0 +1,56 @@
+@echo off 
+if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" %~0 %*) 
+ 
+::############################################################################ 
+::# dk_evalDKBash() 
+::# 
+::# 
+:dk_evalDKBash 
+setlocal enableDelayedExpansion 
+	%dk_call% dk_debugFunc 0 
+ 
+	%dk_call% dk_validate DKCACHE_DIR "%dk_call% dk_DKCACHE_DIR"
+	echo %~1 > "%DKCACHE_DIR%/dk_evalDKBash_TEMP.sh"
+	
+	%dk_call% dk_validate BASH_EXE "%dk_call% dk_BASH_EXE"
+	set DKCOMMAND=%BASH_EXE% -c "%DKCACHE_DIR%/dk_evalDKBash_TEMP.sh";
+	%dk_call% dk_exec %DKCOMMAND%
+	endlocal & (
+		set "dk_evalDKBash=%dk_exec%"
+	)
+
+%endfunction% 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### 
+:DKTEST 
+setlocal enableDelayedExpansion 
+	%dk_call% dk_debugFunc 0 
+  
+	%dk_call% dk_evalDKBash "echo 'testing dk_evalDKBash'"
+	
+	::### Print Shell Path ad Version ###
+	%dk_call% dk_evalDKBash
+	(
+		"export ESC="";  # escape character
+		[ -n "${BASH-}" ] && export DKSHELL_PATH=${BASH-} || export DKSHELL_PATH=${SHELL-}
+		export DKSHELL=$(basename ${DKSHELL_PATH})
+		export DKSHELL_VERSION="$($DKSHELL_PATH --help 2>&1 | head -1)"
+		echo ""
+		echo "${ESC}[45m ${ESC}[30m ${DKSHELL} Version ${DKSHELL_VERSION} ${ESC}[0m"
+		echo "DKSHELL_PATH  ${DKSHELL_PATH}"
+		echo "DKSCRIPT_PATH  ${DKSCRIPT_PATH-}"
+		echo ""
+	)
+
+%endfunction% 
+ 
---
+++ DKBatch/tests/multiLinVar 2.cmd
@@ -0,0 +1,57 @@
+@echo off
+:: https://stackoverflow.com/a/62008572
+
+setlocal EnableDelayedExpansion
+
+::######## Original variable content ########
+set out=
+set NL=^
+
+
+::for /F "delims=" %%i in ('dir /b') do (
+::  if defined out set "out=!out!!NL!"
+::    set "out=!out!%%i"
+::)
+
+set "out=line1!NL!line2"
+
+echo:
+echo:
+echo ###### Original variable content ######
+set out
+::###########################################
+
+
+
+::######## Modified variable content ########
+set out=!out:^%NL%%NL%=^^^%NL%%NL%^%NL%%NL%!  &:: Replace every new-line by an escaped new-line
+echo:
+echo:
+echo ###### Modified variable content #####
+set out
+::###########################################
+
+
+::######### Actual variable content #########
+rem /* Use a `for` meta-variable rather than a normal environment variable to
+rem    pass the variable value beyond the `endlocal` barrier;
+rem    a standard `for` loop can be used here, because there are not going to be
+rem    wildcards `?` and `*` in the variable value since they have already been
+rem    resolved by `dir`; `for /F` cannot be used here due to the new-lines: */
+for %%j in ("!out!") do endlocal & set "data=%%~j"
+echo:
+echo:
+echo ###### Actual variable content ###### &:: Do not use `echo` to show true content of variable:
+set data
+::###########################################
+
+
+::######## Parsed variable content ##########
+echo:
+echo:
+echo ###### Parsed variable content ######
+echo %data%
+::###########################################
+
+pause
+exit /B 0
