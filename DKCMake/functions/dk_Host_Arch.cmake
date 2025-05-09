69624d43a3f6b5695a3d5c7214ac4de2ede38485
aquawicket <aquawicket@hotmail.com>
2025-05-08 21:03:09 -0400

    git commit Thu 05/08/2025

---
+++ DKCMake/functions/dk_Host_Arch.cmake
@@ -0,0 +1,76 @@
+#!/usr/bin/cmake -P
+include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+include_guard()
+
+
+#####################################################################
+# dk_Host_Arch()
+#
+#	  Host_Arch = Arm32, Arm64, X86, X86_64, Cosmopolitan
+#
+function(dk_Host_Arch)
+	dk_debugFunc(0 1)
+
+	###### SET ######
+	if(ARGV0)
+		dk_set(Host_Arch "${ARGV0}")
+	
+	###### GET ######
+	else()
+		### Set host_arch/HOST_ARCH
+		if(NOT CMAKE_HOST_SYSTEM_PROCESSOR)
+			if(CMAKE_HOST_WIN32)
+				if (DEFINED ENV{PROCESSOR_ARCHITEW6432})
+					dk_set(CMAKE_HOST_SYSTEM_PROCESSOR "$ENV{PROCESSOR_ARCHITEW6432}")
+				else()
+					dk_set(CMAKE_HOST_SYSTEM_PROCESSOR "$ENV{PROCESSOR_ARCHITECTURE}")
+				endif()
+			else()
+				execute_process(COMMAND uname -m OUTPUT_VARIABLE CMAKE_HOST_SYSTEM_PROCESSOR)
+			endif()
+		endif()
+		string(STRIP "${CMAKE_HOST_SYSTEM_PROCESSOR}" CMAKE_HOST_SYSTEM_PROCESSOR)
+		#if(COSMOPOLITAN)
+		#	dk_set(Host_Arch Cosmopolitan)
+		if("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "aarch64")
+			dk_set(Host_Arch Arm64)
+		elseif("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "AMD64")
+			dk_set(Host_Arch X86_64)
+		elseif("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "ARM64")
+			dk_set(Host_Arch Arm64)
+		elseif("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "EM64T")
+			dk_set(Host_Arch X86_64)
+		elseif("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "IA64")
+			dk_set(Host_Arch X86_64)
+		elseif("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "x86")
+			dk_set(Host_Arch X86)
+		elseif("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
+			dk_set(Host_Arch X86_64)
+		else()
+			dk_fatal("CMAKE_HOST_SYSTEM_PROCESSOR: Unknown arch: \"${CMAKE_HOST_SYSTEM_PROCESSOR}\"")
+		endif()
+	endif()
+	
+	dk_assertVar(Host_Arch)			#  Host_Arch = Arm64
+	dk_set(${Host_Arch}_Host 1)		# Arm64_Host = 1
+endfunction()
+
+
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+function(DKTEST)
+	dk_debugFunc(0)
+
+	###### GET ######
+    dk_Host_Arch()
+	dk_printVar(Host_Arch)
+	dk_printVar(${Host_Arch}_Host)
+	
+	###### SET ######
+	dk_Host_Arch("I686")
+	dk_printVar(Host_Arch)
+	dk_printVar(${Host_Arch}_Host)
+endfunction()
