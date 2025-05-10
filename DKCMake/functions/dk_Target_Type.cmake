73106ff0c549a1096c45a409e965835441c87d5a
aquawicket <aquawicket@hotmail.com>
2025-05-10 01:07:52 -0400

    git commit Sat 05/10/2025

---
+++ DKCMake/functions/dk_Target_Type.cmake
@@ -0,0 +1,67 @@
+#!/usr/bin/cmake -P
+include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+include_guard()
+
+##################################################################################
+# dk_Target_Type()
+#
+#	Target_Type = Debug, Release
+#
+function(dk_Target_Type)
+	dk_debugFunc(0 1)
+
+	###### SET ######
+	if(ARGV0)
+		dk_set(Target_Type "${ARGV0}")
+	
+	###### GET ######	
+	else()
+		dk_echo()
+		if(NOT Target_Type_Cache)
+			#d_k_Target_Type_Cache()
+		endif()
+		dk_echo(" 0) ${Target_Type_Cache}")
+		dk_echo()
+		dk_echo(" 1) Debug")
+		dk_echo(" 2) Release")
+		dk_echo(" 3) Go Back")
+		dk_echo(" 4) Exit")
+		dk_echo()
+		
+		dk_keyboardInput(input)
+		if("${input}" EQUAL "0")
+			dk_set(Target_Type ${Target_Type_Cache})
+		elseif("${input}" EQUAL "1")
+			dk_set(Target_Type "Debug")
+		elseif("${input}" EQUAL "2")
+			dk_set(Target_Type "Release")
+		elseif("${input}" EQUAL "3")
+			#dk_unset(ENV{UPDATE})
+		elseif("${input}" EQUAL "4")
+			dk_exit(0)
+		else()
+			dk_warning("invalid selection: '${input}'")
+		endif()
+	endif()
+	
+	dk_assertVar(Target_Type)
+	#dk_set(${Target_Type}_Target 1)	
+endfunction()
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+function(DKTEST)
+	dk_debugFunc(0)
+	
+	###### GET ######
+    dk_Target_Type()
+	dk_printVar(Target_Type)
+	#dk_printVar(${Target_Type}_Target)
+	
+	###### SET ######
+	dk_Target_Type("Debug")
+	dk_printVar(Target_Type)
+	#dk_printVar(${Target_Type}_Target)
+endfunction()
