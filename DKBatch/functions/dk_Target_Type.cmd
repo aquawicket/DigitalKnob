ed8902b0011b14450895409ad8ab138811dae30b
aquawicket <aquawicket@hotmail.com>
2025-05-12 05:39:58 -0400

    git commit Mon 05/12/2025

--- DKBatch/functions/dk_Target_Type.cmd
+++ DKBatch/functions/dk_Target_Type.cmd
@@ -18,10 +18,8 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
     echo 1) Debug
     echo 2) Release
     echo 3) All
-    echo 4) none
-    echo 5) Clear Screen
-    echo 6) Go Back
-    echo 7) Exit
+    echo 4) Go Back
+    echo 5) Exit
     
     %dk_call% dk_echo 
     %dk_call% dk_echo "Please select a build type."
@@ -32,10 +30,8 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
     if "%choice%" equ "1" (endlocal & set "%1=Debug"			& %return%)
     if "%choice%" equ "2" (endlocal & set "%1=Release"			& %return%)
     if "%choice%" equ "3" (endlocal & set "%1=All"				& %return%)
-    if "%choice%" equ "4" (endlocal & set "%1=none"			& %return%)
-    if "%choice%" equ "5" (%dk_call% dk_clearScreen			& %return%)
-    if "%choice%" equ "6" (%dk_call% dk_unset Target_Tuple	& %return%)
-    if "%choice%" equ "7" (%dk_call% dk_exit					& %return%)
+    if "%choice%" equ "4" (%dk_call% dk_unset Target_Tuple		& %return%)
+    if "%choice%" equ "5" (%dk_call% dk_exit					& %return%)
 
     %dk_call% dk_echo %choice%: invalid selection, please try again
     %dk_call% dk_unset Target_Type
@@ -49,5 +45,13 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 %setlocal%
 	%dk_call% dk_debugFunc 0
 
+    ::###### GET ######
     %dk_call% dk_Target_Type
+	%dk_call% dk_printVar Target_Type
+	%dk_call% dk_printVar %Target_Type%
+	
+	::###### SET ######
+	%dk_call% dk_Target_Type "Windows"
+	%dk_call% dk_printVar Target_Type
+	%dk_call% dk_printVar %Target_Type%
 %endfunction%
