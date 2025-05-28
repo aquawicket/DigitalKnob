9a4e6ca3a4c709bc6fcb0cf79bd90fba6c946af0
aquawicket <aquawicket@hotmail.com>
2025-05-28 19:22:44 -0400

    git commit Wed 05/28/2025

---
+++ 3rdParty/_DKIMPORTS/CMakeRC/DKINSTALL.cmake
@@ -0,0 +1,35 @@
+#/usr/bin/cmake -P
+if(NOT EXISTS "$ENV{DKCMAKE_FUNCTIONS_DIR_}")
+	set(ENV{DKCMAKE_FUNCTIONS_DIR_} "../../../DKCMake/functions/")
+endif()
+include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+
+
+###### CMakeRC ######
+# https://github.com/vector-of-bool/cmrc
+# https://github.com/vector-of-bool/cmrc/archive/refs/heads/master.zip
+# https://vector-of-bool.github.io/2017/01/21/cmrc.html
+
+### INSTALL ###
+dk_validate		(ENV{DKIMPORTS_DIR} "dk_DKIMPORTS_DIR()")
+dk_getFileParams("$ENV{DKIMPORTS_DIR}/CMakeRC/dkconfig.txt")
+dk_import		(${CMAKERC_IMPORT} NAME CMakeRC)
+
+
+
+### LINK ###
+dk_include			(${CMAKERC}/include)
+if(MSVC)
+	dk_libDebug		(${CMAKERC_DEBUG_DIR}/cmakerc.lib)
+	dk_libRelease	(${CMAKERC_RELEASE_DIR}/cmakerc.lib)
+else()
+	dk_libDebug		(${CMAKERC_DEBUG_DIR}/libcmakerc.a)
+	dk_libRelease	(${CMAKERC_RELEASE_DIR}/libcmakerc.a)
+endif()
+
+### GENERATE ###
+dk_configure(${CMAKERC})
+
+
+### COMPILE ###
+dk_build(${CMAKERC})
--- 3rdParty/_DKIMPORTS/embed/DKINSTALL.cmake
+++ 3rdParty/_DKIMPORTS/embed/DKINSTALL.cmake
@@ -13,3 +13,24 @@ include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
 dk_validate		(ENV{DKIMPORTS_DIR} "dk_DKIMPORTS_DIR()")
 dk_getFileParams("$ENV{DKIMPORTS_DIR}/embed/dkconfig.txt")
 dk_import		(${EMBED_IMPORT} NAME embed)
+
+dk_depend(tbb)
+
+
+### LINK ###
+dk_include			(${EMBED}/include)
+if(MSVC)
+	dk_libDebug		(${EMBED_DEBUG_DIR}/embed.lib)
+	dk_libRelease	(${EMBED_RELEASE_DIR}/embed.lib)
+else()
+	dk_libDebug		(${EMBED_DEBUG_DIR}/libembed.a)
+	dk_libRelease	(${EMBED_RELEASE_DIR}/libembed.a)
+endif()
+
+### GENERATE ###
+dk_set(CURRENT_PLUGIN "EMBED")
+dk_configure(${EMBED} ${TBB_CMAKE}) # -fexperimental-library
+
+
+### COMPILE ###
+dk_build(${EMBED})
