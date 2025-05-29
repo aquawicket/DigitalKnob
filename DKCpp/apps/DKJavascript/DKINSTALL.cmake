b357bb2758953d4406059d6996e90c35f967480e
aquawicket <aquawicket@hotmail.com>
2025-05-28 23:35:07 -0400

    git commit Wed 05/28/2025

--- DKCpp/apps/DKJavascript/DKINSTALL.cmake
+++ DKCpp/apps/DKJavascript/DKINSTALL.cmake
@@ -12,11 +12,22 @@ include_guard()
 #########################################################################
 
 
+###### DEFAULT TARGET ######
+dk_validate(Host_Os "dk_Host_Os()")
+dk_validate(Host_Arch "dk_Host_Arch()")
+dk_Target_Os("${Host_Os}")
+dk_Target_Arch("${Host_Arch}")
+dk_Target_Env ("Clang")
+dk_Target_Type("Debug")
+dk_Target_Tuple()
+
+############ APP ############
 dk_set(Target_App_Dir "${CMAKE_CURRENT_LIST_DIR}")
 dk_basename("${Target_App_Dir}")
 dk_set(Target_App "${dk_basename}")
 dk_validate(CONFIG_PATH "dk_CONFIG_PATH()")
 
+###### DEPENDENCIES ######
 dk_depend(DK)
 dk_depend(DKDuktape)
 dk_depend(DKDuktapeDebugger)
@@ -24,35 +35,40 @@ dk_depend(DKArchive)
 dk_depend(DKAssets)
 dk_depend(DKFmt)
 
-#if(HAVE_DK)
-	## copy app default files without overwrite
-	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/icon.png ${Target_App_Dir}/icon.png) 
-	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/assets.h ${Target_App_Dir}/assets.h)
-	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/main.cpp ${Target_App_Dir}/main.cpp)
-#endif()
+## copy app default files without overwrite
+dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/icon.png ${Target_App_Dir}/icon.png) 
+dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/main.cpp ${Target_App_Dir}/main.cpp)
 
 ###### CREATE ICONS ######
 if(EXISTS "${Target_App_Dir}/icon.png")
 	dk_createIcons("${Target_App_Dir}/icon.png")
-	if(Windows)
-		dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/Windows/icon.h ${Target_App_Dir}/Windows/icon.h)
-		dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/Windows/icon.rc ${Target_App_Dir}/Windows/icon.rc)
+endif()
+
+################# BACKUP USERDATA / INJECT ASSETS #####################
+if(EXISTS "${Target_App_Dir}/assets")
+	dk_copy(${Target_App_Dir}/assets/USER ${Target_App_Dir}/Backup/USER OVERWRITE NO_HALT)
+	dk_delete(${Target_App_Dir}/assets/USER NO_HALT)
+	#Compress the assets, they will be included by resource.rc
+	dk_info("Creating assets.zip . . .")
+	dk_compressAssets(${Target_App_Dir}/assets)
+	# Restore the backed up files
+	dk_copy(${Target_App_Dir}/Backup/ ${Target_App_Dir}/assets/ OVERWRITE NO_HALT)
+	dk_delete(${Target_App_Dir}/Backup NO_HALT)
+	#dummy assets.h file, or the builder wil complain about assets.h missing
+	dk_assertPath(DKCPP_PLUGINS_DIR)
+	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/assets.h ${Target_App_Dir}/assets.h OVERWRITE NO_HALT)
+endif()
+
+###################### Backup Executable ###########################
+if(BACKUP_APP_EXECUTABLES)
+	if(Debug)
+		dk_rename(${Target_App_Dir}/${Target_Tuple}/${Debug_Dir}/${Target_App}.exe ${Target_App_Dir}/${Target_Tuple}/${Debug_Dir}/${Target_App}.exe.backup OVERWRITE NO_HALT)
+	endif()
+	if(Release)
+		dk_rename(${Target_App_Dir}/${Target_Tuple}/${Release_Dir}/${Target_App}.exe ${Target_App_Dir}/${Target_Tuple}/${Release_Dir}/${Target_App}.exe.backup OVERWRITE NO_HALT)
 	endif()
 endif()
 
-####################### Assets ###################
-if(Windows)
-	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/win/assets.h ${Target_App_Dir}/assets.h)
-	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/win/assets.rc ${Target_App_Dir}/assets.rc)
-endif()
-
-####################### Other Resources ###################
-#if(Windows)
-#	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/win/resource.h ${Target_App_Dir}/resource.h)
-#	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/win/resource.rc ${Target_App_Dir}/resource.rc)
-#endif()
-
-	
 dk_generateAppCmake()
 
 #dk_clearCmakeCache()
@@ -62,9 +78,4 @@ dk_set(${CURRENT_PLUGIN} 			${CMAKE_SOURCE_DIR})
 dk_set(${CURRENT_PLUGIN}_CONFIG_DIR ${CMAKE_CURRENT_LIST_DIR}/${CONFIG_PATH})
 
 dk_configure(${CMAKE_CURRENT_LIST_DIR} -DDKCMAKE_FUNCTIONS_DIR=${DKCMAKE_FUNCTIONS_DIR} -DTUPLE=${TUPLE})
-
-if(BACKUP_APP_EXECUTABLES)
-	dk_backupExecutable()
-endif()
-
 dk_build(${CMAKE_CURRENT_LIST_DIR})
