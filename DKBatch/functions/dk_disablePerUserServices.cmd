9447c897eff3c527880cd9e6e70d4457d9acb591
aquawicket <aquawicket@hotmail.com>
2025-05-29 21:27:40 -0400

    git commit Thu 05/29/2025

--- DKBatch/functions/dk_disablePerUserServices.cmd
+++ DKBatch/functions/dk_disablePerUserServices.cmd
@@ -10,45 +10,14 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 ::#
 :dk_disablePerUserServices
 %setlocal%
+	%dk_call% dk_debugFunc 0
 
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\AarSvc" 								"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\BluetoothUserService"					"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\CDPUserSvc" 							"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\CaptureService" 						"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\CloudBackupRestoreSvc"					"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\ConsentUxUserSvc" 						"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\CredentialEnrollmentManagerUserSvc"		"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\DeviceAssociationBrokerSvc" 			"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\DevicePickerUserSvc" 					"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\DevicesFlowUserSvc" 					"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\NPSMSvc"								"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\P9RdrService" 							"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\PenService" 							"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\PrintWorkflowUserSvc"					"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\UdkUserSvc" 							"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\WpnUserService" 						"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\cbdhsvc" 								"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\webthreatdefusersvc"					"UserServiceFlags" "REG_DWORD" "0"
-	
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\AarSvc" 							"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\BluetoothUserService"				"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\CDPUserSvc" 						"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\CaptureService" 					"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\CloudBackupRestoreSvc"				"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\ConsentUxUserSvc" 					"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\CredentialEnrollmentManagerUserSvc"	"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\DeviceAssociationBrokerSvc" 		"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\DevicePickerUserSvc" 				"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\DevicesFlowUserSvc" 				"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\NPSMSvc"							"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\P9RdrService" 						"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\PenService" 						"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\PrintWorkflowUserSvc"				"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\UdkUserSvc" 						"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\WpnUserService" 					"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\cbdhsvc" 							"UserServiceFlags" "REG_DWORD" "0"
-	%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\webthreatdefusersvc"				"UserServiceFlags" "REG_DWORD" "0"
+	for /f "tokens=2" %%G in ('sc query state^=all type^=userservice ^| find "SERVICE_NAME"') do (
+		%dk_call% dk_registrySetKey "HKLM\SYSTEM\ControlSet001\Services\%%G" 		"UserServiceFlags" "REG_DWORD" "0"
+		%dk_call% dk_registrySetKey "HKLM\SYSTEM\CurrentControlSet\Services\%%G" 	"UserServiceFlags" "REG_DWORD" "0"
+	)
 
+	%dk_call% dk_notice "Restart for changes to take effect"
 %endfunction%
 
 
@@ -64,7 +33,8 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 ::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
 :DKTEST
 %setlocal%
-
+	%dk_call% dk_debugFunc 0
+	
 	%dk_call% dk_disablePerUserServices
 %endfunction%
 
