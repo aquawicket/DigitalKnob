8b2bedea2211423a7974dab98f92e3143080ae49
aquawicket <aquawicket@hotmail.com>
2025-05-30 18:59:48 -0400

    git commit Fri 05/30/2025

---
+++ DKCMake/functions/dk_gitClean.cmake
@@ -0,0 +1,33 @@
+#!/usr/bin/cmake -P
+### DK.cmake ############################################################
+if(NOT EXISTS "$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+	cmake_policy(SET CMP0009 NEW)
+	file(GLOB_RECURSE DK.cmake "/DK.cmake")
+	list(GET DK.cmake 0 DK.cmake)
+	get_filename_component(DKCMAKE_FUNCTIONS_DIR "${DK.cmake}" DIRECTORY)
+	set(ENV{DKCMAKE_FUNCTIONS_DIR_} "${DKCMAKE_FUNCTIONS_DIR}/")
+endif()
+include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+include_guard()
+#########################################################################
+
+###############################################################################
+# dk_gitClean()
+#
+#
+function(dk_gitClean)
+	dk_debugFunc(0)
+	
+	dk_validate(GIT_EXE "dk_installGit()")
+	dk_validate(ENV{DKBRANCH_DIR} "dk_DKBRANCH_DIR()")
+	execute_process(COMMAND "${GIT_EXE}" -C $ENV{DKBRANCH_DIR} clean -fdx)
+endfunction()
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+function(DKTEST)
+	dk_debugFunc(0)
+	
+	dk_gitClean()
+endfunction()
