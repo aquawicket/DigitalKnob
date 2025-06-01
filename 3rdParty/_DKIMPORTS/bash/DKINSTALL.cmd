ec7aa79b40b73eff750d5987ee6726e02e28bcd5
aquawicket <aquawicket@hotmail.com>
2025-06-01 16:28:28 -0400

    git commit Sun 06/01/2025

--- 3rdParty/_DKIMPORTS/bash/DKINSTALL.cmd
+++ 3rdParty/_DKIMPORTS/bash/DKINSTALL.cmd
@@ -8,14 +8,31 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 ::# DKINSTALL()
 ::#
 :DKINSTALL
-::%%setlocal%%
+%setlocal%
 	%dk_call% dk_debugFunc 0
 	
+	if exist "%BASH_EXE%" (%return%)
+	
+	
 	%dk_call% dk_validate DKIMPORTS_DIR "%dk_call% dk_DKIMPORTS_DIR"
-    %dk_call% dk_validate GIT_BASH_EXE "%dk_call% %DKIMPORTS_DIR%/git/DKINSTALL.cmd"
-	set "BASH_EXE=%GIT_BASH_EXE%"
+	
+	::###### GIT bash.exe ######
+	if not exist "%BASH_EXE%" (
+		%dk_call% dk_validate GIT "%dk_call% %DKIMPORTS_DIR%/git/DKINSTALL.cmd"
+		set "BASH_EXE=!GIT!/bin/bash.exe"
+	)
+	
+	::###### MSYS2 bash.exe ######
+	if not exist "%BASH_EXE%" (
+		%dk_call% dk_validate GIT_BASH_EXE "%dk_call% %DKIMPORTS_DIR%/msys2/DKINSTALL.cmd"
+		set "BASH_EXE=%MSYS2_BASH_EXE%"
+	)
 	
 	%dk_call% dk_assertVar BASH_EXE
+	::### return ###
+	endlocal & (
+		set "BASH_EXE=%BASH_EXE%"
+	)
 %endfunction%
 
 
