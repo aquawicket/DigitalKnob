665362703f968cc8055214b1187033c6d273bdd6
aquawicket <aquawicket@hotmail.com>
2025-06-03 13:15:03 -0400

    git commit Tue 06/03/2025

---
+++ DKPowershell/functions/dk_setConsoleIcon.ps1
@@ -0,0 +1,54 @@
+if( $env:DKPOWERSHELL_FUNCTIONS_DIR ){ . $env:DKPOWERSHELL_FUNCTIONS_DIR/DK.ps1 } else { . '/DK.ps1' }
+if(!$dk_setConsoleIcon){ $dk_setConsoleIcon = 1 } else{ return } #include guard
+
+
+################################################################################
+# dk_setConsoleIcon()
+#
+#    Function description
+#
+#    @arg1	- arg1 description
+#
+function Global:dk_setConsoleIcon() {
+	dk_debugFunc 0 99
+
+	# $all_args = $PsBoundParameters.Values + ${args};
+	# arg1 = $($args[0]);
+	# arg2 = $($args[1]);
+	
+	$iconFile = "C:\Users\Administrator\Desktop\icon.ico"
+	[System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') | Out-Null
+
+	# Verify the file exists
+	if ([System.IO.File]::Exists($iconFile) -eq $true) {
+		$ch = Invoke-Win32 'kernel32' ([IntPtr]) 'GetConsoleWindow'
+		$i = 0;
+		$size = 16;
+		while ($i -ne 4) {
+			$ico = New-Object System.Drawing.Icon($iconFile, $size, $size)
+			if ($ico -ne $null) {
+				Send-Message $ch 0x80 $i $ico.Handle | Out-Null
+			}
+			if ($i -eq 4) {
+				break
+			}
+			$i += 1
+			$size += 16
+		}
+	}
+  else {
+    Write-Host 'Icon file not found' -ForegroundColor 'Red'
+  }
+}
+
+
+
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST #####
+function Global:DKTEST() { 
+	dk_debugFunc 0
+	
+	dk_call dk_setConsoleIcon "C:\Users\Administrator\Desktop\icon.ico"
+}
