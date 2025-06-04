04d1898248646ef4b37669bdfd284093e8d1c1f1
aquawicket <aquawicket@hotmail.com>
2025-06-04 03:20:27 -0400

    git commit Wed 06/04/2025

--- DKCpp/apps/DKCore/DKINSTALL.cmake
+++ DKCpp/apps/DKCore/DKINSTALL.cmake
@@ -22,50 +22,53 @@ dk_Target_Type("Debug")
 dk_Target_Tuple()
 
 ############ APP ############
-dk_set(Target_App_Dir "${CMAKE_CURRENT_LIST_DIR}")
-dk_basename("${Target_App_Dir}")
-dk_set(Target_App "${dk_basename}")
-dk_validate(Target_Config "dk_Target_Config()")
+#dk_set(Target_App_Dir "${CMAKE_CURRENT_LIST_DIR}")
+#dk_basename("${Target_App_Dir}")
+#dk_set(Target_App "${dk_basename}")
+#dk_validate(Target_Config "dk_Target_Config()")
 
 ###### DEPENDENCIES ######
-dk_depend(DK)
+#dk_depend(DK)
 
 ## copy app default files without overwrite
-dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/icon.png ${Target_App_Dir}/icon.png) 
-dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/main.cpp ${Target_App_Dir}/main.cpp)
+#dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/icon.png ${Target_App_Dir}/icon.png) 
+#dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/main.cpp ${Target_App_Dir}/main.cpp)
 
 ###### CREATE ICONS ######
-if(EXISTS "${Target_App_Dir}/icon.png")
-	dk_createIcons("${Target_App_Dir}/icon.png")
-endif()
+#if(EXISTS "${Target_App_Dir}/icon.png")
+#	dk_createIcons("${Target_App_Dir}/icon.png")
+#endif()
 
 ################# BACKUP USERDATA / INJECT ASSETS #####################
-if(EXISTS "${Target_App_Dir}/assets")
-	dk_copy(${Target_App_Dir}/assets/USER ${Target_App_Dir}/Backup/USER OVERWRITE NO_HALT)
-	dk_delete(${Target_App_Dir}/assets/USER NO_HALT)
-	#Compress the assets, they will be included by resource.rc
-	dk_info("Creating assets.zip . . .")
-	dk_compressAssets(${Target_App_Dir}/assets)
-	# Restore the backed up files
-	dk_copy(${Target_App_Dir}/Backup/ ${Target_App_Dir}/assets/ OVERWRITE NO_HALT)
-	dk_delete(${Target_App_Dir}/Backup NO_HALT)
-	#dummy assets.h file, or the builder will complain about assets.h missing
-	dk_assertPath(DKCPP_PLUGINS_DIR)
-	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/assets.h ${Target_App_Dir}/assets.h OVERWRITE NO_HALT)
-endif()
+#if(EXISTS "${Target_App_Dir}/assets")
+#	dk_copy(${Target_App_Dir}/assets/USER ${Target_App_Dir}/Backup/USER OVERWRITE NO_HALT)
+#	dk_delete(${Target_App_Dir}/assets/USER NO_HALT)
+#	#Compress the assets, they will be included by resource.rc
+#	dk_info("Creating assets.zip . . .")
+#	dk_compressAssets(${Target_App_Dir}/assets)
+#	# Restore the backed up files
+#	dk_copy(${Target_App_Dir}/Backup/ ${Target_App_Dir}/assets/ OVERWRITE NO_HALT)
+#	dk_delete(${Target_App_Dir}/Backup NO_HALT)
+#	#dummy assets.h file, or the builder will complain about assets.h missing
+#	dk_assertPath(DKCPP_PLUGINS_DIR)
+#	dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/assets.h ${Target_App_Dir}/assets.h OVERWRITE NO_HALT)
+#endif()
 
 ###################### Backup Executable ###########################
-if(BACKUP_APP_EXECUTABLES)
-	dk_backupExecutable()
-endif()
+#if(BACKUP_APP_EXECUTABLES)
+#	dk_backupExecutable()
+#endif()
 
-dk_generateAppCmake()
+#dk_generateAppCmake()
 
 #dk_clearCmakeCache()
 
-dk_set(CURRENT_PLUGIN 				${Target_App})
-dk_set(${CURRENT_PLUGIN} 			${CMAKE_SOURCE_DIR})
-dk_set(${CURRENT_PLUGIN}_CONFIG_DIR ${CMAKE_CURRENT_LIST_DIR}/${Target_Config})
+#dk_set(CURRENT_PLUGIN 				${Target_App})
+#dk_set(${CURRENT_PLUGIN} 			${CMAKE_SOURCE_DIR})
+#dk_set(${CURRENT_PLUGIN}_CONFIG_DIR ${CMAKE_CURRENT_LIST_DIR}/${Target_Config})
 
-dk_configure(${CMAKE_CURRENT_LIST_DIR} -DDKCMAKE_FUNCTIONS_DIR=${DKCMAKE_FUNCTIONS_DIR} -DTUPLE=${TUPLE})
+#dk_configure(${CMAKE_CURRENT_LIST_DIR} -DDKCMAKE_FUNCTIONS_DIR=${DKCMAKE_FUNCTIONS_DIR} -DTUPLE=${TUPLE})
+
+dk_copy(${DKCPP_PLUGINS_DIR}/_DKIMPORT/CMakeLists.txt ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt) 
+dk_configure(${CMAKE_CURRENT_LIST_DIR})
 dk_build(${CMAKE_CURRENT_LIST_DIR})
