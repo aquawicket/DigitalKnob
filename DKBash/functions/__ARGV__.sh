1912e3822bd12435e04bdda25e2dd17a22959a67
aquawicket <aquawicket@hotmail.com>
2025-06-09 17:49:00 -0400

    git commit Mon 06/09/2025

--- DKBash/functions/__ARGV__.sh
+++ DKBash/functions/__ARGV__.sh
@@ -12,16 +12,13 @@ __ARGV__() {
 	[ -z ${1-} ] && local frame=0 || local frame=${1}
 	
 	local marker=0
-	
-	
 #	for (( i=0; i<frame; i++ )); do
 #		marker=$(($marker + ${BASH_ARGC[${i}]-}))
 #	done
-
-	local i=0
-	while [ i<frame ]; do
-		marker=$(($marker + ${BASH_ARGC[${i}]-}))
-		((i++))
+	local n=0
+	while [ ${n} -lt ${frame} ]; do
+		marker=$(($marker + ${BASH_ARGC[${n}]-}))
+		n=$((${n}+1))
 	done
 
 	
@@ -29,9 +26,16 @@ __ARGV__() {
 	local argv=()	
 	local argc=${BASH_ARGC[${frame}]-}
 	local begin=$(($marker+$argc-1))
-	for (( i=$begin; i>((begin-argc)); i-- )); do
-		argv+=(${BASH_ARGV[${i}]-})
+#	for (( i=$begin; i>((begin-argc)); i-- )); do
+#		argv+=(${BASH_ARGV[${i}]-})
+#	done
+	local n=$begin
+	while [ ${n} -gt $((${begin} - ${argc})) ]; do
+		argv+=(${BASH_ARGV[${n}]-})
+		n=$((${n}-1))
 	done
+
+
 	[ ${argc} = 0 ] && return
 	
 	local argv_string=${argv[@]}
