c0c7b8d3773c66ad4f6c7f6f658054fe04e5666a
aquawicket <aquawicket@hotmail.com>
2025-06-09 20:36:06 -0400

    git commit Mon 06/09/2025

--- DKBash/functions/dk_Host_Os.sh
+++ DKBash/functions/dk_Host_Os.sh
@@ -18,6 +18,7 @@ dk_Host_Os() {
 	
 	###### GET ######	
 	else
+		echo "OS = ${OS}"
 		if [ "${OS-}" = "Windows_NT" ]; then
 			[ -z "${Host_Os-}" ]     && Host_Os="Windows"
 		fi
@@ -48,19 +49,19 @@ dk_Host_Os() {
 #			[ -z "${Host_Os-}" ]		&& Host_Os=${Gcc_Os}
 #			[ -z "${Host_Env-}"	]		&& Host_Env=${Gcc_Env}
 #		fi
-#		if dk_call dk_commandExists bash; then
-#			Bash_Tuple=$(bash -c "builtin echo \${MACHTYPE}")
-#			remainder="${Bash_Tuple}"
-#			Bash_Arch="${remainder%%-*}"; remainder="${remainder#*-}"
-#			Bash_Vendor="${remainder%%-*}"; remainder="${remainder#*-}"
-#			Bash_Os="${remainder%%-*}"; remainder="${remainder#*-}"
+		if dk_call dk_commandExists bash; then
+			Bash_Tuple=$(bash -c "builtin echo \${MACHTYPE}")
+			remainder="${Bash_Tuple}"
+			Bash_Arch="${remainder%%-*}"; remainder="${remainder#*-}"
+			Bash_Vendor="${remainder%%-*}"; remainder="${remainder#*-}"
+			Bash_Os="${remainder%%-*}"; remainder="${remainder#*-}"
 #			Bash_Env="${remainder%%-*}"; remainder="${remainder#*-}"
 #			[ -z "${Host_Tuple-}" ]		&& Host_Tuple=${Bash_Tuple}
 #			[ -z "${Host_Arch-}" ]		&& Host_Arch=${Bash_Arch}
 #			[ -z "${Host_Vendor-}" ]	&& Host_Vendor=${Bash_Vendor}
-#			[ -z "${Host_Os-}" ]		&& Host_Os=${Bash_Os}
+			[ -z "${Host_Os-}" ]		&& Host_Os=${Bash_Os}
 #			[ -z "${Host_Env-}" ]		&& Host_Env=${Bash_Env}
-#		fi
+		fi
 #		if dk_call dk_commandExists uname; then
 #			Uname="$(uname)"         && dk_call dk_printVar Uname
 #			Uname_a="$(uname -a)"    && dk_call dk_printVar Uname_a
