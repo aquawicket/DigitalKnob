b0b2a2083428ea4724d75af69d84feb934f32fde
aquawicket <aquawicket@hotmail.com>
2025-06-13 18:20:36 -0400

    git commit Fri 06/13/2025

---
+++ DKBatch/functions/dk_depend.cmd
@@ -0,0 +1,47 @@
+@echo off&::########################################## DigitalKnob DKBatch ########################################################################
+if not exist "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" for /F "tokens=*" %%G IN ('where /r "%USERPROFILE%" DK.cmd') do (set "DKBATCH_FUNCTIONS_DIR_=%%~dpG")
+if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
+::#################################################################################################################################################
+
+
+::############################################################################
+::# dk_depend(plugin)
+::#
+::#   this will search for a "plugin" to run in the following search path
+::#  '3rdParty/_IMPORTS/'plugin'/DKINSTALL.cmd'
+::#
+:dk_depend
+%setlocal%
+	
+	set "_plugin_=%~1"
+	
+	%dk_call% dk_validate DKIMPORTS_DIR "%dk_call% dk_DKIMPORTS_DIR"
+	if exist "%DKIMPORTS_DIR%/%_plugin_%/DKINSTALL.cmd" (
+		call "%DKIMPORTS_DIR%/%_plugin_%/DKINSTALL.cmd"
+		
+		%dk_call% dk_success "found %_plugin_%"
+		%return%
+	)
+
+	%dk_call% dk_fatal "%DKIMPORTS%/%_plugin_%/DKINSTALL.cmd not found"
+%endfunction%
+
+
+
+
+
+
+
+
+
+
+
+::###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ######
+:DKTEST
+%setlocal%
+
+	%dk_call% dk_depend "git"
+	
+	
+%endfunction%
+
