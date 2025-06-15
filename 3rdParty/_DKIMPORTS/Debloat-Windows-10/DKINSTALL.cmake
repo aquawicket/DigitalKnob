5fa69ac9d5f4d8ed15c791a60ae4878b58fde4a2
aquawicket <aquawicket@hotmail.com>
2025-06-15 15:11:14 -0400

    git commit Sun 06/15/2025

--- 3rdParty/_DKIMPORTS/Debloat-Windows-10/DKINSTALL.cmake
+++ 3rdParty/_DKIMPORTS/Debloat-Windows-10/DKINSTALL.cmake
@@ -10,4 +10,4 @@ include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
 
 ### INSTALL ###
 dk_getFileParams("${CMAKE_CURRENT_LIST_DIR}/dkconfig.txt")
-dk_import		(${DEBLOAT-WINDOWS-10_IMPORT} NAME Debloat-Windows-10)
+dk_import		(${Debloat_Windows_10_Import})
