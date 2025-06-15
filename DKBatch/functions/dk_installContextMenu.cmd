33ef80773807bf10ec18b4b20ac163118de72e81
aquawicket <aquawicket@hotmail.com>
2025-06-15 11:41:09 -0400

    git commit Sun 06/15/2025

--- DKBatch/functions/dk_installContextMenu.cmd
+++ DKBatch/functions/dk_installContextMenu.cmd
@@ -10,14 +10,15 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 :dk_installContextMenu
 %setlocal%
 	%dk_call% dk_debugFunc 3
+	
     set "_menuTitle_=%~1"
     set "_icon_exe_=%~2"
 	set "_command_=%~3"
 	::set "_icon_exe_=%_icon_exe_:/=\%"
 	::set "_command_=%_command_:/=\%"
    
-    %dk_call% dk_info "Adding '%_menuTitle_%' context menu to Registry"
-	%dk_call% dk_info "using '%_icon_exe_%' for the icon"
+    %dk_call% dk_debug "Adding '%_menuTitle_%' context menu to Registry"
+	%dk_call% dk_debug "Using '%_icon_exe_%' for the icon"
    
 	%dk_call% dk_validate REG_EXE "%dk_call% dk_REG_EXE"
     ::### delete existing key ###
@@ -29,13 +30,13 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
     ::REG ADD "HKCR/*/shell/%_menuTitle_%" /ve /d "&%_menuTitle_%" /f
    
     ::### ICON ###
-echo %REG_EXE% ADD "HKCR\AllFilesystemObjects\shell\%_menuTitle_%" /v Icon /t REG_SZ /d "\"%_icon_exe_%\"" /f
+::echo %REG_EXE% ADD "HKCR\AllFilesystemObjects\shell\%_menuTitle_%" /v Icon /t REG_SZ /d "\"%_icon_exe_%\"" /f
      %REG_EXE% ADD "HKCR\AllFilesystemObjects\shell\%_menuTitle_%" /v Icon /t REG_SZ /d "\"%_icon_exe_%\"" /f
 ::echo %dk_call% dk_registrySetKey "HKCR\AllFilesystemObjects\shell\%_menuTitle_%" "Icon" "REG_SZ" "\"%_icon_exe_%\""
 ::	 %dk_call% dk_registrySetKey "HKCR\AllFilesystemObjects\shell\%_menuTitle_%" "Icon" "REG_SZ" "\"%_icon_exe_%\""
    
     ::### COMMAND <args> ###
-echo %REG_EXE% ADD "HKCR\AllFilesystemObjects\shell\%_menuTitle_%\command" /ve /d "%_command_%" /f
+::echo %REG_EXE% ADD "HKCR\AllFilesystemObjects\shell\%_menuTitle_%\command" /ve /d "%_command_%" /f
      %REG_EXE% ADD "HKCR\AllFilesystemObjects\shell\%_menuTitle_%\command" /ve /d "%_command_%" /f
 ::echo %dk_call% dk_registrySetKey "HKCR/AllFilesystemObjects/shell/%_menuTitle_%/command" "" "" "%_command_%"
 ::     %dk_call% dk_registrySetKey "HKCR/AllFilesystemObjects/shell/%_menuTitle_%/command" "" "" "%_command_%"
@@ -51,7 +52,6 @@ echo %REG_EXE% ADD "HKCR\AllFilesystemObjects\shell\%_menuTitle_%\command" /ve /
 %setlocal%
 	%dk_call% dk_debugFunc 0
 
-    %dk_call% dk_validate DKIMPORTS_DIR "%dk_call% dk_DKIMPORTS_DIR"
-    %dk_call% dk_validate GIT_EXE "%dk_call% %DKIMPORTS_DIR%/git/DKINSTALL.cmd"
+	%dk_call% dk_depend git
     %dk_call% dk_installContextMenu "GITADD" "GIT ADD" "GIT_EXE" "\"%GIT_EXE%\" add \"%%1\""
 %endfunction%
