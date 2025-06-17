e9f2aca22672dcdfc319917d2c3445734a903080
aquawicket <aquawicket@hotmail.com>
2025-06-17 14:16:39 -0400

    git commit Tue 06/17/2025

--- 3rdParty/_DKIMPORTS/cosmocc/DKINSTALL.cmake
+++ 3rdParty/_DKIMPORTS/cosmocc/DKINSTALL.cmake
@@ -1,12 +1,15 @@
 #!/usr/bin/cmake -P
-include_guard()
-if(NOT EXISTS "$ENV{DKCMAKE_FUNCTIONS_DIR_}")
-	set(ENV{DKCMAKE_FUNCTIONS_DIR_} "../../../DKCMake/functions/") # From this path to DK.cmake
-endif()
-if(NOT EXISTS $ENV{DKCMAKE_FUNCTIONS_DIR_})
-	set(ENV{DKCMAKE_FUNCTIONS_DIR_} ${CMAKE_SOURCE_DIR}/../../../../"../../../DKCMake/functions/") # From try compile to Dk.cmake 
+### DK.cmake ############################################################
+if(NOT EXISTS "$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+	cmake_policy(SET CMP0009 NEW)
+	file(GLOB_RECURSE DK.cmake "/DK.cmake")
+	list(GET DK.cmake 0 DK.cmake)
+	get_filename_component(DKCMAKE_FUNCTIONS_DIR "${DK.cmake}" DIRECTORY)
+	set(ENV{DKCMAKE_FUNCTIONS_DIR_} "${DKCMAKE_FUNCTIONS_DIR}/")
 endif()
 include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+include_guard()
+#########################################################################
 
 ###### [cosmocc] ######
 # https://cosmo.zip
