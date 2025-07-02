8c5316abf7a4ef73d03205d842fadfe5c8d59503
aquawicket <aquawicket@hotmail.com>
2025-07-02 01:44:13 -0400

    git commit Wed 07/02/2025

--- DKBatch/functions/dk_deleteDigitalKnob.cmd
+++ DKBatch/functions/dk_deleteDigitalKnob.cmd
@@ -12,16 +12,20 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 %setlocal%
  	%dk_call% dk_debugFunc 0 
 
-	::###### Deleting DIGITALKNOB_DIR ######
+	
 	echo ### Deleting DIGITALKNOB_DIR ###
 	%dk_call% dk_validate DIGITALKNOB_DIR "%dk_call% dk_DIGITALKNOB_DIR"
 	echo Do you want to delete the DigitalKnob folder ?
 	%dk_call% dk_confirm || (exit /b 0)
 	
-	::###### Create a deleter and run it in a new process and exit this script
-	echo ((goto) 2^>nul ^& cd "%SystemDrive%\" ^&^& rmdir /s /q "%DIGITALKNOB_DIR:/=\%") > "%TEMP%\delete_DK.cmd"
-	echo ((goto) 2^>nul ^& del "%TEMP%\delete_DK.cmd" ^& cmd /c exit /b 0) >> "%TEMP%\delete_DK.cmd"
-	start "" /MIN "%TEMP%\delete_DK.cmd" & exit
+	::###### Deleting DIGITALKNOB_DIR ######
+::	echo ((goto) 2^>nul ^& cd "%SystemDrive%\" ^&^& rmdir /s /q "%DIGITALKNOB_DIR:/=\%") > "%TEMP%\delete_DK.cmd"
+::	echo ((goto) 2^>nul ^& del "%TEMP%\delete_DK.cmd" ^& cmd /c exit /b 0) >> "%TEMP%\delete_DK.cmd"
+::	start "" /MIN "%TEMP%\delete_DK.cmd" & exit
+	
+	call :deleteParentFolder&exit /b
+	:deleteParentFolder
+	start /b "" cmd /c rd /s /q "%DIGITALKNOB_DIR:/=\%"&exit /b 11
 
 %endfunction%
 
