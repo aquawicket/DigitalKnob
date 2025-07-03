16c883f5bb9404615fa53b89ee85ce19797c6515
aquawicket <aquawicket@hotmail.com>
2025-07-03 02:00:52 -0400

    git commit Thu 07/03/2025

---
+++ 3rdParty/_DKIMPORTS/websocketd/DKUNINSTALL.cmake
@@ -0,0 +1,22 @@
+#/usr/bin/cmake -P
+### DK.cmake ############################################################
+if(NOT EXISTS "$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+	cmake_policy(SET CMP0009 NEW)
+	file(GLOB_RECURSE DK.cmake "/DK.cmake")
+	list(GET DK.cmake 0 DK.cmake)
+	get_filename_component(DKCMAKE_FUNCTIONS_DIR "${DK.cmake}" DIRECTORY)
+	set(ENV{DKCMAKE_FUNCTIONS_DIR_} "${DKCMAKE_FUNCTIONS_DIR}/")
+endif()
+include("$ENV{DKCMAKE_FUNCTIONS_DIR_}DK.cmake")
+include_guard()
+#########################################################################
+
+
+###### websocketd ######
+# https://github.com/joewalnes/websocketd/archive/8d6da30045855d1d9d81fd8a0936ef84981764f0.zip
+
+### UnINSTALL ###
+dk_getFileParams	("${CMAKE_CURRENT_LIST_DIR}/dkconfig.txt")
+dk_importVariables	(${WEBSOCKETD_IMPORT})
+
+dk_delete			("${WEBSOCKETD}")
