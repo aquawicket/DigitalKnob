3fc431acda4b99df1b7ce5ecc9b30b0cb588e8d0
aquawicket <aquawicket@hotmail.com>
2025-07-03 09:33:51 -0400

    git commit Thu 07/03/2025

---
+++ DKPowershell/functions/dk_fatal.ps1
@@ -0,0 +1,24 @@
+if(${env:DKPOWERSHELL_FUNCTIONS_DIR}){ . ${env:DKPOWERSHELL_FUNCTIONS_DIR}/DK.ps1; } else { . ${PSScriptRoot}/DK.ps1; }
+if(!$dk_fatal_ps1){ $dk_fatal_ps1 = 1; } else{ return; } #include guard
+
+################################################################################
+# dk_fatal(message)
+#
+#    Print a fatal message to the console
+#
+#    @message	- The message to print
+#
+function Global:dk_fatal($message) {
+	#dk_debugFunc 1;
+	
+	dk_call dk_log FATAL "$message"
+}
+
+
+
+###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### DKTEST ###### 
+function Global:DKTEST() { 
+	dk_debugFunc 0	
+	
+	dk_call dk_fatal "test dk_fatal message"
+}
