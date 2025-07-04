f6205a43638abcfdd9275d35962fb95683b697b6
aquawicket <aquawicket@hotmail.com>
2025-07-03 22:56:54 -0400

    git commit Thu 07/03/2025

--- DKPowershell/functions/dk_gitUpdate.ps1
+++ DKPowershell/functions/dk_gitUpdate.ps1
@@ -5,32 +5,51 @@ if(!$dk_gitUpdate_ps1){ $dk_gitUpdate_ps1 = 1; } else{ return; } #include guard
 # dk_gitUpdate(url, branch, NO_CONFIRM)
 #
 #
-function Global:dk_gitUpdate($url, $branch) {
-	dk_debugFunc 2 3
-
-	if($args[0]){ $url="$args[0]" }    else { $url="https://github.com/aquawicket/DigitalKnob.git" }
-	if($args[1]){ $branch="$args[1]" } else { $branch="Development" }
+function Global:dk_gitUpdate() {
+	dk_debugFunc 2 3;
 	
-	if(!($args[2] -eq "NO_CONFIRM")){
-		dk_call dk_info "Git Update? Any local changes will be lost."
-		if(!(dk_call dk_confirm)){ return 0 }
-	}
+	${dk_gitUpdate_BACKUP} = 1;
+	if($($args[0])){ ${_url_} = $($args[0]); }    		else { ${_url_} = "https://github.com/aquawicket/DigitalKnob.git"; }
+	if($($args[1])){ ${env:DKBRANCH} = $($args[1]); } 	else { ${env:DKBRANCH} = "Development"; }
 	
-	dk_call dk_validate GIT_EXE "dk_call dk_installGit"
+#	if(!($args[2] -eq "NO_CONFIRM")){
+#		dk_call dk_info "Git Update? Any local changes will be lost."
+#		if(!(dk_call dk_confirm)){ return 0 }
+#	}
+	
+	dk_call dk_validate GIT_EXE "dk_call dk_installGit";
 	if(!(dk_call dk_pathExists "${env:DKBRANCH_DIR}/.git")){
-		dk_call dk_printVar DKBRANCH_DIR
-		dk_call "${GIT_EXE}" clone "${url}" "${env:DKBRANCH_DIR}"
+		if(!(dk_call dk_pathExists "${env:DKBRANCH_DIR}")){
+			###### Backup Branch directory and clone ######
+			if("${dk_gitUpdate_BACKUP}" -eq "1"){
+				dk_call dk_copy "${env:DKBRANCH_DIR}" "${env:DKBRANCH_DIR}_BACKUP" OVERWRITE;
+			}
+			dk_call dk_delete "${env:DKBRANCH_DIR}";
+			dk_call "${GIT_EXE}" clone ${_url_} "${env:DKBRANCH_DIR}";
+			dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} pull --all;
+			dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} checkout -- .;
+			dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} checkout ${env:DKBRANCH} || (
+				dk_call dk_echo "Remote has no '${env:DKBRANCH}' branch. Creating...";
+				dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} checkout -b ${env:DKBRANCH} main;
+				dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} push --set-upstream origin ${env:DKBRANCH};
+			)
+			return;
+		}
+
+		dk_call "${GIT_EXE}" clone "${url}" "${env:DKBRANCH_DIR}";
 	}
+	
+	###### Update ######
 	#dk_call cd "${env:DKBRANCH_DIR}" #-or dk_call dk_error "cd $${DKBRANCH_DIR} failed!"
-	dk_call "${GIT_EXE}" pull --all
-	dk_call "${GIT_EXE}" checkout -- .
-	dk_call "${GIT_EXE}" checkout "${branch}"
+	dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} pull --all;
+	dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} checkout -- .;
+	dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} checkout ${env:DKBRANCH};
 	if( $? -eq "0" ){
-		dk_call dk_info "${branch} branch selected"
+		dk_call dk_info "${env:DKBRANCH} branch selected"
 	} else {
-		dk_call dk_info "Remote has no ${branch} branch. Creating..."
-		dk_call "${GIT_EXE}" checkout -b "${branch}" main
-		dk_call "${GIT_EXE}" push --set-upstream origin "${branch}"
+		dk_call dk_info "Remote has no ${env:DKBRANCH} branch. Creating..."
+		dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} checkout -b ${env:DKBRANCH} main
+		dk_call "${GIT_EXE}" -C ${env:DKBRANCH_DIR} push --set-upstream origin ${env:DKBRANCH}
 	}
 	#dk_call chmod +x "${env:DKBRANCH_DIR}"/build
 }
