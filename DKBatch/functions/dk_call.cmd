f3a2510c9a742c58f69584d26889ce8372473de2
aquawicket <aquawicket@hotmail.com>
2025-07-07 02:58:13 -0400

    git commit Mon 07/07/2025

--- DKBatch/functions/dk_call.cmd
+++ DKBatch/functions/dk_call.cmd
@@ -59,7 +59,7 @@ set "dk_call_IGNORE=dk_debugFunc"
 	if exist "%__CMND__:.cmd=%.cmd" (set __CMND__=%__CMND__:.cmd=%.cmd)
 	if exist "%DKBATCH_FUNCTIONS_DIR_%%__CMND__:.cmd=%.cmd" (set __CMND__=%DKBATCH_FUNCTIONS_DIR_%%__CMND__:.cmd=%.cmd)
 	if not exist "%__CMND__%" (
-		call dk_source "%__CMND__%"
+		call %DKBATCH_FUNCTIONS_DIR_%dk_source.cmd "%__CMND__%"
 	)
 	
 	if exist "%__CMND__:.cmd=%.cmd" (set __CMND__=%__CMND__:.cmd=%.cmd)
