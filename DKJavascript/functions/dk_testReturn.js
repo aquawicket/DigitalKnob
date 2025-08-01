57692b804ea36802f9008586066e70a0dab5a54a
aquawicket <aquawicket@hotmail.com>
2025-08-01 00:44:35 -0400

    git commit Fri 08/01/2025

--- DKJavascript/functions/dk_testReturn.js
+++ DKJavascript/functions/dk_testReturn.js
@@ -7,7 +7,15 @@ dk_source(DKJAVASCRIPT_DIR+"/functions/dk_debugFunc.js");
 dk_testReturn = function dk_testReturn_f(){
 	dk_debugFunc(1, 2);
 	
-	// TODO
+//	export dk_testReturn=${1/input/output};
+
+//	if [ -n "${2-}" ]; then
+//		export ${2}=${dk_testReturn};
+//	else
+//		builtin echo "${dk_testReturn}";
+//	fi
+	
+//	return $?;
 }
 
 
@@ -18,5 +26,20 @@ dk_testReturn = function dk_testReturn_f(){
 DKTEST = function DKTEST_f(){
 	dk_debugFunc(0);
 	
-	// TODO
+	//### Result as global variable
+	dk_echo("");
+//	dk_call dk_testReturn "inputA";
+//	dk_call dk_echo "dk_testReturn = ${dk_testReturn}";
+	
+	//### Result as parameter
+//	dk_call dk_echo
+//	dk_call dk_testReturn "inputB" resultB;
+//	dk_call dk_echo "resultB = ${resultB}";
+//	dk_call dk_echo "dk_testReturn = ${dk_testReturn}";
+	
+	//### Result as return value
+//	dk_call dk_echo
+//	resultC=$(dk_call dk_testReturn "inputC");
+//	dk_call dk_echo "resultC = ${resultC}";
+//	#dk_call dk_echo "dk_testReturn = ${dk_testReturn}";					#NOTE: export cannot be seen outside of command substituion
 }
--- DKPhp/functions/dk_testReturn.php
+++ DKPhp/functions/dk_testReturn.php
@@ -6,7 +6,18 @@
 function dk_testReturn(){
 	#dk_debugFunc 1 2
 
-	# TODO
+	if(!isset($argv)){
+		$argv = func_get_args();
+	}
+	if(isset($argv[0])){
+		global $dk_testReturn; $dk_testReturn = str_replace("input", "output", "$argv[0]");
+	}
+	
+	if(isset($argv[1])){
+		global ${$argv[1]}; ${$argv[1]} = $dk_testReturn;
+	} else {
+		dk_echo("$dk_testReturn\n");
+	}
 	
 	return 0;
 }
@@ -19,7 +30,24 @@ function dk_testReturn(){
 if(!function_exists('DKTEST')){ function DKTEST() {
 	#dk_call dk_debugFunc 0
 
-	# TODO
+	include_once(str_replace("\\","/",$_SERVER['USERPROFILE'])."/DigitalKnob/Development/DKPhp/functions/dk_echo.php");
+	
+	### Result as global variable
+	dk_echo("\n");
+	dk_testReturn("inputA");
+	dk_echo("dk_testReturn = ".$GLOBALS['dk_testReturn']."\n");
+	
+	### Result as parameter
+	dk_echo("\n");
+	dk_testReturn("inputB", "resultB");
+	dk_echo("resultB = ".$GLOBALS['resultB']."\n");
+	dk_echo("dk_testReturn = ".$GLOBALS['dk_testReturn']."\n");
+	
+	### Result as return value
+#	dk_echo("\n");
+#	$resultC=`dk_testReturn("inputC")`;
+#	dk_echo "resultC = ${resultC}";
+#	dk_echo("dk_testReturn = ".$GLOBALS['dk_testReturn']."\n");
 }}
 
 
