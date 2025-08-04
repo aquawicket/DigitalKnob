7d02a9ef1a83d2b3d452a32272d334225a3534c4
aquawicket <aquawicket@hotmail.com>
2025-08-04 02:56:41 -0400

    git commit Mon 08/04/2025

--- DKPowershell/functions/dk_basename.ps1
+++ DKPowershell/functions/dk_basename.ps1
@@ -10,15 +10,12 @@ function Global:dk_basename() {
 	dk_debugFunc 1 2;
 
 	$_path_ = $args[0];
-	if(Test-Path variable:$_path_){ $_path_ = Get-Variable -Name ($_path_) -ValueOnly } 
+	if(Test-Path variable:$_path_){ $_path_ = Get-Variable -Name ($_path_) -ValueOnly; } 
 	
 	${global:dk_basename} = Split-Path $_path_ -leaf;
 	
 	if($args[1]) {
-		Write-Host "args[1] = $($args[1])";
-		Write-Host "dk_basename = ${dk_basename}";
-		Set-Variable -Name "$($args[1])" -Value "${dk_basename}" -Scope Global;
-		Set-Variable -Name "resultD.test" -Value "${dk_basename}";
+		dk_call dk_set $args[1] ${dk_basename};
 	} else {
 		return ${dk_basename};
 	}
@@ -36,52 +33,25 @@ function Global:DKTEST() {
 	dk_debugFunc 0;
 	
 	### Result as global variable
-	dk_call dk_echo
+	dk_call dk_echo;
 	dk_call dk_basename "A:/directoryA/filenameA.extA";
 	dk_call dk_echo "dk_basename = ${dk_basename}";
 	
-	### Result as parameter name
-	dk_call dk_echo
-	dk_call dk_basename "B:/directoryB/filenameB.extB" resultB;
+	### Result as return value
+	dk_call dk_echo;
+	$resultB = dk_call dk_basename "B:/directoryB/filenameB.extB";
 	dk_call dk_echo "resultB = ${resultB}";
 	dk_call dk_echo "dk_basename = ${dk_basename}";
 	
-	### Result as return value
-	dk_call dk_echo
-	$resultC = dk_call dk_basename "C:/directoryC/filenameC.extC";
+	### Result as variable parameter
+	dk_call dk_echo;
+	dk_call dk_basename "C:/directoryC/filenameC.extC" resultC;
 	dk_call dk_echo "resultC = ${resultC}";
 	dk_call dk_echo "dk_basename = ${dk_basename}";
 	
-	### Result as parameter variable
-	$resultD = @{ "Key" = "Value" };
-	#$resultD['test'] = "success";
-	#$resultD.test = "success";
-	dk_call dk_echo
-	dk_call dk_basename "D:/directoryD/filenameD.extD" resultD.test;
-	dk_call dk_echo "resultD['test'] = $($resultD['test'])";
-	dk_call dk_echo "resultD.test = $($resultD.test)";
+	### Result as hashtable parameter
+	dk_call dk_echo;
+	dk_call dk_basename "D:/directoryD/filenameD.extD" resultD.data;
+	dk_call dk_echo "resultD.data = $($resultD.data)";
 	dk_call dk_echo "dk_basename = ${dk_basename}";
-	
-	
-	#$MyHashtable = @{};
-	#$MyHashtable.data = "old value"
-	#$MyHashtable.data;
-	$var = "MyHashtable.data"
-	
-	$myVar = "old var value";
-	$myVar;
-	$var = "myVar";
-	
-	$new_value = "new value";
-	
-	$var_arry = $var.Split(".");
-
-	if($var_arry[1]){
-		$(Get-Variable -Name $var_arry[0] -ValueOnly)[$var_arry[1]] = $new_value;
-	} else {
-		Set-Variable -Name $var_arry[0] -Value $new_value;
-	}
-	
-	#$MyHashtable.data;
-	$myVar;
 }
--- DKPowershell/functions/dk_toUpper.ps1
+++ DKPowershell/functions/dk_toUpper.ps1
@@ -8,11 +8,13 @@ if(!$dk_toUpper_ps1){ $dk_toUpper_ps1 = 1; } else{ return; } #include guard
 function Global:dk_toUpper() {
 	dk_debugFunc 1 2;
 
-	$str = $args[0];
-	${global:dk_toUpper} = ${str}.ToUpper();
+	$_str_ = $args[0];
+	#if(Test-Path variable:$_str_){ $_str_ = Get-Variable -Name ($_str_) -ValueOnly; } 
+	
+	${global:dk_toUpper} = ${_str_}.ToUpper();
 
 	if($args[1]) {
-		Set-Variable -Name $args[1] -Value ${dk_toUpper} -Scope Global;
+		dk_call dk_set $args[1] ${dk_toUpper};
 	} else {
 		return ${dk_toUpper};
 	}
@@ -25,19 +27,25 @@ function Global:DKTEST() {
 	dk_debugFunc 0;
 	
 	### Result as global variable
-	dk_call dk_echo
-	dk_call dk_toUpper "A CoNvErT tHiS sTrInG tO aLl UpPeRcAsE";
-	dk_call dk_echo "dk_toUpper = ${dk_toUpper}";
-	
-	### Result as parameter
-	dk_call dk_echo
-	dk_call dk_toUpper "B CoNvErT tHiS sTrInG tO aLl UpPeRcAsE" resultB;
-	dk_call dk_echo "resultB = ${resultB}";
+	dk_call dk_echo;
+	dk_call dk_toUpper "A:/directoryA/filenameA.extA";
 	dk_call dk_echo "dk_toUpper = ${dk_toUpper}";
 	
 	### Result as return value
-	dk_call dk_echo
-	$resultC=dk_call dk_toUpper "C CoNvErT tHiS sTrInG tO aLl UpPeRcAsE";
+	dk_call dk_echo;
+	$resultB=dk_call dk_toUpper "B:/directoryB/filenameB.extB";
+	dk_call dk_echo "resultB = ${resultB}";
+	dk_call dk_echo "dk_toUpper = ${dk_toUpper}";
+	
+	### Result as variable parameter
+	dk_call dk_echo;
+	dk_call dk_toUpper "C:/directoryC/filenameC.extC" resultC;
 	dk_call dk_echo "resultC = ${resultC}";
+	dk_call dk_echo "dk_toUpper = ${dk_toUpper}";
+	
+	### Result as hashtable parameter
+	dk_call dk_echo;
+	dk_call dk_toUpper "D:/directoryD/filenameD.extD" resultD.data;
+	dk_call dk_echo "resultD.data = $($resultD.data)";
 	dk_call dk_echo "dk_toUpper = ${dk_toUpper}";
 }
