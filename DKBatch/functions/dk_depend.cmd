525eea3880d1bf1cb4b1e99e2ffd924282c408b6
aquawicket <5370616+aquawicket@users.noreply.github.com>
2025-08-09 08:01:59 -0400

    Update dk_depend.cmd

--- DKBatch/functions/dk_depend.cmd
+++ DKBatch/functions/dk_depend.cmd
@@ -15,8 +15,12 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 	%dk_call% dk_debugFunc 1 99
 	
 	%dk_call% dk_validate DKIMPORTS_DIR "%dk_call% dk_DKIMPORTS_DIR"
+	if not exist "%DKIMPORTS_DIR%/%~1/DKINSTALL.cmd" (
+		%dk_call% dk_validate DKHTTP_DKIMPORTS_DIR "set DKHTTP_DKIMPORTS_DIR=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/3rdParty/_DKIMPORTS"
+		%dk_call% dk_download "!DKHTTP_DKIMPORTS_DIR!/%~1/DKINSTALL.cmd"  "%DKIMPORTS_DIR%/%~1/DKINSTALL.cmd"
+	)
+
 	%dk_call% dk_allButFirstArgs %*
-	
 	endlocal & (
 		%dk_call% "%DKIMPORTS_DIR%/%~1/DKINSTALL.cmd" %dk_allButFirstArgs%
 	)
