0a6f605dfddb3f7fea168184245eb5a9b494513a
aquawicket <5370616+aquawicket@users.noreply.github.com>
2025-08-09 07:59:46 -0400

    Update dk_installGit.cmd

--- DKBatch/functions/dk_installGit.cmd
+++ DKBatch/functions/dk_installGit.cmd
@@ -12,11 +12,11 @@ if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
 ::%setlocal%
 	%dk_call% dk_debugFunc 0
 	
-	%dk_call% dk_validate DKIMPORTS_DIR "%dk_call% dk_DKIMPORTS_DIR"
-	if not exist "%DKIMPORTS_DIR%/git/DKINSTALL.cmd" (
-		%dk_call% dk_validate DKHTTP_DKIMPORTS_DIR "set DKHTTP_DKIMPORTS_DIR=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/3rdParty/_DKIMPORTS"
-		%dk_call% dk_download "!DKHTTP_DKIMPORTS_DIR!/git/DKINSTALL.cmd"  "%DKIMPORTS_DIR%/git/DKINSTALL.cmd"
-	)
+::	%dk_call% dk_validate DKIMPORTS_DIR "%dk_call% dk_DKIMPORTS_DIR"
+::	if not exist "%DKIMPORTS_DIR%/git/DKINSTALL.cmd" (
+::		%dk_call% dk_validate DKHTTP_DKIMPORTS_DIR "set DKHTTP_DKIMPORTS_DIR=https://raw.githubusercontent.com/aquawicket/DigitalKnob/Development/3rdParty/_DKIMPORTS"
+::		%dk_call% dk_download "!DKHTTP_DKIMPORTS_DIR!/git/DKINSTALL.cmd"  "%DKIMPORTS_DIR%/git/DKINSTALL.cmd"
+::	)
 	%dk_call% dk_depend git
 %endfunction%
 
