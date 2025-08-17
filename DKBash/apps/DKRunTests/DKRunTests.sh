9949be20f37e2cecfb878c066f0ce4ba80c78d11
aquawicket <aquawicket@hotmail.com>
2025-08-17 16:41:20 -0400

    git commit Sun 08/17/2025

--- DKBash/apps/DKRunTests/DKRunTests.sh
+++ DKBash/apps/DKRunTests/DKRunTests.sh
@@ -15,21 +15,61 @@ fi
 
 
 #####################################
+RUNTEST() {
+	if ! [ "#${disabled/;${1};//}#" = "#${disabled}#" ]; then
+		echo "${yellow} ######### ${1} is disabled ######### ${clr}";
+		return;
+	fi
+	echo "######## ${DKBASH_FUNCTIONS_DIR_}${1} #######";
+	[ ! -e "${DKBASH_FUNCTIONS_DIR_}${1}" ] && return -1
+	dk_call dk_fileContains "${DKBASH_FUNCTIONS_DIR_}${1}" "DKTEST(" || return -1
+	
+	CURRENT_TEST_FILE="${1}";
+	echo "">> 								$(dirname $0)/log.txt
+	echo "######### ${1} #########">> 		$(dirname $0)/log.txt
+	#title TESTING "### %~nx1 ###"
+	echo "";
+	echo "${bg_magenta}${white}###### DKTEST MODE ###### ${1} ###### DKTEST MODE ######${clr}";
+	echo "";
+	. "${DKBASH_FUNCTIONS_DIR_}${1}";
+	DKTEST && (
+		echo "        ### passed status:$?">> 		$(dirname $0)/log.txt
+	) || (
+		echo "        ### FAILED status:$?">> 		$(dirname $0)/log.txt
+	)
+	echo "";
+	echo "${bg_magenta}${white}######## END TEST ####### ${1} ######## END TEST #######${clr}";
+	echo "";
+}
+
+#####################################
 DKRunTests() {
-	echo "DKRunTests()";
+	DISABLE __ARGC__.sh;
+	DISABLE __ARG__.sh;
+	DISABLE __ARGV__.sh;
+	
 	dk_call dk_validate DKBASH_FUNCTIONS_DIR_ "dk_call dk_DKBRANCH_DIR";
-	dk_call dk_getFiles ${DKBATCH_FUNCTIONS_DIR_};
+	dk_call dk_getFiles ${DKBASH_FUNCTIONS_DIR_} _files_;
+	
+	### Clear log.txt
+	echo "" > $(dirname $0)/log.txt
+	
+	#set "dk_log_ERROR_CALLBACK=call %~f0 :CALLBACK"
+	#set "dk_log_FATAL_CALLBACK=call %~f0 :CALLBACK"
+	
+	#set "READY=1"
+	for ((i=0; i<${#_files_[@]}; i++)); do
+		echo "RUNTEST ${_files_[$i]}";
+		RUNTEST "${_files_[$i]}";
+	done
 }
 
 #####################################
 DISABLE() {
-	echo "DISABLE()";
+	export disabled="${disabled-};${1};"
 }
 
-#####################################
-RUNTEST() {
-	echo "RUNTEST()";
-}
+
 
 #####################################
 CALLBACK() {
