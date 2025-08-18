36710fb9c645c5d101ca3e223b15e2298505da09
aquawicket <aquawicket@hotmail.com>
2025-08-17 21:29:49 -0400

    git commit

--- DKPowershell/apps/DKRunTests/DKRunTests.ps1
+++ DKPowershell/apps/DKRunTests/DKRunTests.ps1
@@ -44,7 +44,7 @@ function Global:DKRunTests() {
 	DISABLE dk_extract.ps1;
 	DISABLE dk_enterManually.ps1;
 	DISABLE dk_generate.ps1;
-	
+	DISABLE dk_import.ps1;
 	
 	dk_call dk_validate DKPOWERSHELL_FUNCTIONS_DIR_ "dk_call dk_DKBRANCH_DIR";
 	$_files_ = dk_call dk_getFiles ${DKPOWERSHELL_FUNCTIONS_DIR_};
