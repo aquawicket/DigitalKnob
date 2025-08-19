61c4b49f8cee537b800820e045e90c6e18416c73
aquawicket <aquawicket@hotmail.com>
2025-08-18 20:48:44 -0400

    git commit Mon 08/18/2025

---
+++ 3rdParty/_DKIMPORTS/pacman/DKINSTALL.cmd
@@ -0,0 +1,39 @@
+@echo off&::###### DK.cmd #########################################################################################################################
+if not exist "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" for /F "tokens=*" %%G IN ('where /r "%USERPROFILE%" DK.cmd') do (set "DKBATCH_FUNCTIONS_DIR_=%%~dpG")
+if not defined DK.cmd (call "%DKBATCH_FUNCTIONS_DIR_%DK.cmd" "%~0" %*)
+::#################################################################################################################################################
+
+
+::############ pacman ############
+::# https://wiki.archlinux.org/title/Pacman
+::# https://walterteng.com/using-zsh-on-windows
+
+%dk_call% dk_validate MSYS2 "dk_depend msys2"
+%dk_call% dk_findProgram PACMAN_EXE pacman.exe "%MSYS2_DIR%/usr/bin"
+
+::#	if NOT defined DKUPDATE (
+::#		if exist %PACMAN_EXE% (
+::#			%dk_call% dk_notice "PACMAN_EXE is already installed, returning"
+::#			%return%
+::#		)
+::# )
+
+::###### init the pacman keyring ######
+::#dk_delete("%MSYS2_GPGDir%")
+if not exist "%MSYS2_GPGDir%" (
+	set "PATH=%MSYS2%/usr/bin:%PATH%"
+	set "%MAKEPKG_LIBRARY%=%MSYS2%/usr/share/makepkg"
+	%dk_call% dk_findProgram BASH_EXE bash "%MSYS2%/usr/bin"
+	%dk_call% dk_exec %BASH_EXE% -c "pacman-key --init"
+	%dk_call% dk_exec %BASH_EXE% -c "pacman-key --populate msys2"
+	%dk_call% dk_killProcess gpg-agent.exe NO_HALT
+)
+
+if NOT EXIST "%MSYS2_DBPath%/sync" (
+	%dk_call% dk_exec "%PACMAN_EXE%" -Syu --noconfirm
+)
+
+%dk_call% dk_FirewallAllow "pacman" "%PACMAN_EXE%"
+
+#%dk_call% dk_installPackage pacman
+%dk_call% dk_assertPath PACMAN_EXE
