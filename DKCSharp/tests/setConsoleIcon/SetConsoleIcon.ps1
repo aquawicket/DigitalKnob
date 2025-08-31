24cc1c4f47da407895f28b5e982afb9cf88510ca
aquawicket <aquawicket@hotmail.com>
2025-08-30 22:37:44 -0400

    git commit Sat 08/30/2025

---
+++ DKCSharp/tests/setConsoleIcon/SetConsoleIcon.ps1
@@ -0,0 +1,14 @@
+$w=Add-Type -ReferencedAssemblies System.Drawing -Name WAPI -PassThru -MemberDefinition '
+[DllImport("kernel32.dll")] static extern bool AllocConsole();
+[DllImport("kernel32.dll")] static extern System.IntPtr GetConsoleWindow();
+[DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+   static extern System.IntPtr SendMessage(System.IntPtr hWnd, int Msg, int wParam, System.IntPtr lParam);
+public static void SetIcon() {
+	System.Drawing.Icon icon = new System.Drawing.Icon("icon.ico");
+	System.IntPtr handle = GetConsoleWindow();
+	if(handle == System.IntPtr.Zero) { AllocConsole(); }
+	System.IntPtr result01 = SendMessage(handle, 0x0080, 0, icon.Handle);
+	System.IntPtr result02 = SendMessage(handle, 0x0080, 1, icon.Handle);
+	System.IntPtr result03 = SendMessage(handle, 0x0080, 2, icon.Handle);
+}'
+$w::SetIcon()
