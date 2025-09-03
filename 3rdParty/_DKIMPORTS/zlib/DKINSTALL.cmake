d7e81b5869c744d40c6395706846bb8c10abdaa1
aquawicket <aquawicket@hotmail.com>
2025-09-02 22:13:11 -0400

    git commit Tue 09/02/2025

--- 3rdParty/_DKIMPORTS/zlib/DKINSTALL.cmake
+++ 3rdParty/_DKIMPORTS/zlib/DKINSTALL.cmake
@@ -24,28 +24,28 @@ include_guard()
 dk_import()
 
 ### DKBIN ###
-#set(ZLIB			 	"$ENV{DIGITALKNOB_DIR}/DKBIN/include/zlib")  	# C:/Users/Administrator/DigitalKnob/DKBIN/include/zlib
-#set(ZLIB_INC		 	"$ENV{DIGITALKNOB_DIR}/DKBIN/include/zlib")  	# C:/Users/Administrator/DigitalKnob/DKBIN/include/zlib
-#set(ZLIB_Config_Dir  	"${ZLIB_INC}/${Target_Config}")					# //DKBIN/include/zlib/Windows_X86_64_Clang (MULTI_CONFIG), //DKBIN/include/zlib/Windows_X86_64_Clang/Debug (SINGLE_CONFIG)
-#set(ZLIB_LIB		 	"$ENV{DIGITALKNOB_DIR}/DKBIN/lib/zlib")	   		# C:/Users/Administrator/DigitalKnob/DKBIN/lib/zlib
-#set(ZLIB_Debug_Dir		"${ZLIB_LIB}/${Target_Tuple}/${Debug_Dir}")		# C:/Users/Administrator/DigitalKnob/DKBIN/lib/zlib/Windows_X86_64_Clang/Debug
-#set(ZLIB_Release_Dir 	"${ZLIB_LIB}/${Target_Tuple}/${Release_Dir}")	# C:/Users/Administrator/DigitalKnob/DKBIN/lib/zlib/Windows_X86_64_Clang/Release
+#set(zlib			 	"$ENV{DIGITALKNOB_DIR}/DKBIN/include/zlib")  	# C:/Users/Administrator/DigitalKnob/DKBIN/include/zlib
+#set(zlib_INC		 	"$ENV{DIGITALKNOB_DIR}/DKBIN/include/zlib")  	# C:/Users/Administrator/DigitalKnob/DKBIN/include/zlib
+#set(zlib_Config_Dir  	"${zlib_INC}/${Target_Config}")					# //DKBIN/include/zlib/Windows_X86_64_Clang (MULTI_CONFIG), //DKBIN/include/zlib/Windows_X86_64_Clang/Debug (SINGLE_CONFIG)
+#set(zlib_LIB		 	"$ENV{DIGITALKNOB_DIR}/DKBIN/lib/zlib")	   		# C:/Users/Administrator/DigitalKnob/DKBIN/lib/zlib
+#set(zlib_Debug_Dir		"${zlib_LIB}/${Target_Tuple}/${Debug_Dir}")		# C:/Users/Administrator/DigitalKnob/DKBIN/lib/zlib/Windows_X86_64_Clang/Debug
+#set(zlib_Release_Dir 	"${zlib_LIB}/${Target_Tuple}/${Release_Dir}")	# C:/Users/Administrator/DigitalKnob/DKBIN/lib/zlib/Windows_X86_64_Clang/Release
 
 ### LINK ###
-dk_include				(${ZLIB}								ZLIB_INCLUDE_DIR)
-dk_include				(${ZLIB_Config_Dir}						ZLIB_INCLUDE_DIR2)
+dk_include				(${zlib}								ZLIB_INCLUDE_DIR)
+dk_include				(${zlib_Config_Dir}						ZLIB_INCLUDE_DIR2)
 if(MSVC)
 	if(Windows)	
-		dk_libDebug		(${ZLIB_Debug_Dir}/zd.lib				ZLIB_LIBRARY_DEBUG)
-		dk_libRelease	(${ZLIB_Release_Dir}/z.lib 				ZLIB_LIBRARY_RELEASE)
+		dk_libDebug		(${zlib_Debug_Dir}/zd.lib				ZLIB_LIBRARY_DEBUG)
+		dk_libRelease	(${zlib_Release_Dir}/z.lib 				ZLIB_LIBRARY_RELEASE)
 	endif()
 else()
 	if(Windows)
-		dk_libDebug		(${ZLIB_Debug_Dir}/libzlibstatic.a		ZLIB_LIBRARY_DEBUG)
-		dk_libRelease	(${ZLIB_Release_Dir}/libzlibstatic.a	ZLIB_LIBRARY_RELEASE)
+		dk_libDebug		(${zlib_Debug_Dir}/libzlibstatic.a		ZLIB_LIBRARY_DEBUG)
+		dk_libRelease	(${zlib_Release_Dir}/libzlibstatic.a	ZLIB_LIBRARY_RELEASE)
 	else()
-		dk_libDebug		(${ZLIB_Debug_Dir}/libz.a				ZLIB_LIBRARY_DEBUG)
-		dk_libRelease	(${ZLIB_Release_Dir}/libz.a				ZLIB_LIBRARY_RELEASE)
+		dk_libDebug		(${zlib_Debug_Dir}/libz.a				ZLIB_LIBRARY_DEBUG)
+		dk_libRelease	(${zlib_Release_Dir}/libz.a				ZLIB_LIBRARY_RELEASE)
 	endif()
 endif()
 if(Debug)
@@ -57,26 +57,26 @@ endif()
 
 ### 3RDPARTY LINK ###
 # https://cmake.org/cmake/help/latest/module/FindZLIB.html
-dk_append(ZLIB_CMAKE 
+dk_append(zlib_CMAKE 
 	"-DZLIB_INCLUDE_DIR=${ZLIB_INCLUDE_DIR}"
 	"-DZLIB_LIBRARY_DEBUG=${ZLIB_LIBRARY_DEBUG}" 
 	"-DZLIB_LIBRARY_RELEASE=${ZLIB_LIBRARY_RELEASE}")
 if(Windows AND MSVC)
-	dk_append(ZLIB_CMAKE 
+	dk_append(zlib_CMAKE 
 		"-DCMAKE_C_FLAGS=/I${ZLIB_INCLUDE_DIR} /I${ZLIB_INCLUDE_DIR2}" 
 		"-DCMAKE_CXX_FLAGS=/I${ZLIB_INCLUDE_DIR} /I${ZLIB_INCLUDE_DIR2}")
 else()
-	dk_append(ZLIB_CMAKE
+	dk_append(zlib_CMAKE
 		"-DCMAKE_C_FLAGS=-I${ZLIB_INCLUDE_DIR} -I${ZLIB_INCLUDE_DIR2}" 
 		"-DCMAKE_CXX_FLAGS=-I${ZLIB_INCLUDE_DIR} -I${ZLIB_INCLUDE_DIR2}")
 endif()
-dk_append(ZLIB_CMAKE
+dk_append(zlib_CMAKE
 	-DZLIB_LIBRARY=${ZLIB_LIBRARY}
 	-DZLIB_LIBRARIES=${ZLIB_LIBRARY}
 	"-DCMAKE_EXE_LINKER_FLAGS=${ZLIB_LIBRARY}")
 
 ### GENERATE ###
-dk_configure(${ZLIB} -DZLIB_BUILD_EXAMPLES=OFF)# -DUNIX=1 -DWIN32=0)
+dk_configure(${zlib} -DZLIB_BUILD_EXAMPLES=OFF)# -DUNIX=1 -DWIN32=0)
 
 ### COMPILE ###
-dk_build(${ZLIB} zlibstatic)
+dk_build(${zlib} zlibstatic)
