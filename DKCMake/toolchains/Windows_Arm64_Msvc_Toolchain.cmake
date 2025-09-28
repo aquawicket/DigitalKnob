6f568771c4f47824816f83f5a9a1e494b9f5aa0e
aquawicket <aquawicket@hotmail.com>
2025-09-28 14:56:13 -0400

    git commit Sun 09/28/2025

--- DKCMake/toolchains/Windows_Arm64_Msvc_Toolchain.cmake
+++ DKCMake/toolchains/Windows_Arm64_Msvc_Toolchain.cmake
@@ -1,5 +1,5 @@
 message("############################################################################")
-message("#################### Windows_X86_Msvc_Toolchain.cmake ######################")
+message("#################### Windows_Arm64_Msvc_Toolchain.cmake ####################")
 message("############################################################################")
 # https://learn.microsoft.com/en-us/cpp/build/projects-and-build-systems-cpp?view=msvc-170
 
@@ -11,12 +11,12 @@ dk_depend(visualstudio)
 
 dk_set(CMAKE_GENERATOR				${VS_GENERATOR})				# https://cmake.org/cmake/help/latest/variable/CMAKE_GENERATOR.html
 dk_set(CMAKE_MAKE_PROGRAM 			${VS_MAKE_PROGRAM})				# https://cmake.org/cmake/help/latest/variable/CMAKE_MAKE_PROGRAM.html
-dk_set(CMAKE_GENERATOR_PLATFORM		${VS_X86_GENERATOR_PLATFORM})	# https://cmake.org/cmake/help/latest/variable/CMAKE_GENERATOR_PLATFORM.html
-dk_set(CMAKE_CXX_COMPILER			${VS_X86_COMPILER})				# https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER.html
-dk_set(CMAKE_C_COMPILER				${VS_X86_COMPILER})				# https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER.html
-#dk_set(CMAKE_DUMPBIN				${VS_X86_DUMPBIN})
-#dk_set(CMAKE_LINKER				${VS_X86_LINKER})
-#dk_append(DKCONFIGURE_FLAGS		--build=i686-msvc)
+dk_set(CMAKE_GENERATOR_PLATFORM		${VS_ARM64_GENERATOR_PLATFORM})	# https://cmake.org/cmake/help/latest/variable/CMAKE_GENERATOR_PLATFORM.html
+dk_set(CMAKE_CXX_COMPILER			${VS_ARM64_COMPILER})			# https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER.html
+dk_set(CMAKE_C_COMPILER				${VS_ARM64_COMPILER})			# https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER.html
+#dk_set(CMAKE_DUMPBIN				${VS_ARM64_DUMPBIN})
+#dk_set(CMAKE_LINKER				${VS_ARM64_LINKER})
+#dk_append(DKCONFIGURE_FLAGS		--build=arm-msvc)
 
 ###### C Compiler Options ######
 # https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-by-category?view=msvc-170
@@ -26,7 +26,7 @@ dk_append(CMAKE_C_FLAGS
 	/D_USING_V110_SDK71_ 		# "
 	/D_WINDOWS 					# "
 	/DWIN 						# "
-	/DWIN_X86_64 				# "
+	/DWIN_ARM64 				# "
 	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
 	/std:c17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
 	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
@@ -41,7 +41,7 @@ dk_append(CMAKE_CXX_FLAGS
 	/D_USING_V110_SDK71_ 		# "
 	/D_WINDOWS 					# "
 	/DWIN 						# "
-	/DWIN_X86_64 				# "
+	/DWIN_ARM64 				# "
 	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
 	/std:c++17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
 	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
