0e0f0c8666ecd6d061906b5536f19892a60bfff3
aquawicket <aquawicket@hotmail.com>
2025-09-28 14:48:17 -0400

    git commit Sun 09/28/2025

--- DKCMake/toolchains/Windows_X86_64_Msvc_Toolchain.cmake
+++ DKCMake/toolchains/Windows_X86_64_Msvc_Toolchain.cmake
@@ -14,61 +14,55 @@ dk_set(CMAKE_MAKE_PROGRAM 			${VS_MAKE_PROGRAM})					# https://cmake.org/cmake/h
 dk_set(CMAKE_GENERATOR_PLATFORM		${VS_X86_64_GENERATOR_PLATFORM})	# https://cmake.org/cmake/help/latest/variable/CMAKE_GENERATOR_PLATFORM.html
 dk_set(CMAKE_CXX_COMPILER			${VS_X86_64_COMPILER})				# https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER.html
 dk_set(CMAKE_C_COMPILER				${VS_X86_64_COMPILER})				# https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER.html
-dk_set(CMAKE_DUMPBIN				${VS_X86_64_DUMPBIN})
-dk_set(CMAKE_LINKER					${VS_X86_64_LINKER})
+#dk_set(CMAKE_DUMPBIN				${VS_X86_64_DUMPBIN})
+#dk_set(CMAKE_LINKER				${VS_X86_64_LINKER})
 #dk_append(DKCONFIGURE_FLAGS		--build=x86_64-w64-msvc)
 
 ###### C Compiler Options ######
 # https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-by-category?view=msvc-170
 dk_append(CMAKE_C_FLAGS				
-	/DWIN 						# https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions?view=msvc-170
-	/DWIN_X86_64 				# "
-	/D_WINDOWS 					# "
-	/D_CRT_SECURE_NO_WARNINGS 	# "
-	/D_USING_V110_SDK71_ 		# "
-	/std:c17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
-	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
-	/Zm500 						# https://learn.microsoft.com/en-us/cpp/build/reference/zm-specify-precompiled-header-memory-allocation-limit?view=msvc-170
-	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
 	/bigobj						# https://learn.microsoft.com/en-us/cpp/build/reference/bigobj-increase-number-of-sections-in-dot-obj-file?view=msvc-170
+	/D_CRT_SECURE_NO_WARNINGS 	# https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions?view=msvc-170
+	/D_USING_V110_SDK71_ 		# "
+	/D_WINDOWS 					# "
+	/DWIN 						# "
+	/DWIN_X86_64 				# "
+	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
+	/std:c17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
+	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
+	/Zm500 						# https://learn.microsoft.com/en-us/cpp/build/reference/zm-specify-precompiled-header-memory-allocation-limit?view=msvc-170
 )
 
 ###### C++ Compiler Options ######
 # https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-by-category?view=msvc-170
 dk_append(CMAKE_CXX_FLAGS			
-	/DWIN 						# https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions?view=msvc-170
-	/DWIN_X86_64 				# "
-	/D_WINDOWS 					# "
-	/D_CRT_SECURE_NO_WARNINGS 	# "
-	/D_USING_V110_SDK71_ 		# "
-	/std:c++17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
-	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
-	/Zm500 						# https://learn.microsoft.com/en-us/cpp/build/reference/zm-specify-precompiled-header-memory-allocation-limit?view=msvc-170
-	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
 	/bigobj						# https://learn.microsoft.com/en-us/cpp/build/reference/bigobj-increase-number-of-sections-in-dot-obj-file?view=msvc-170
+	/D_CRT_SECURE_NO_WARNINGS 	# https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions?view=msvc-170
+	/D_USING_V110_SDK71_ 		# "
+	/D_WINDOWS 					# "
+	/DWIN 						# "
+	/DWIN_X86_64 				# "
+	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
+	/std:c++17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
+	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
+	/Zm500 						# https://learn.microsoft.com/en-us/cpp/build/reference/zm-specify-precompiled-header-memory-allocation-limit?view=msvc-170
 )
 
 ###### Linker Options ######
 # https://learn.microsoft.com/en-us/cpp/build/reference/linker-options?view=msvc-170
-dk_append(CMAKE_EXE_LINKER_FLAGS	
+dk_append(CMAKE_EXE_LINKER_FLAGS
 	/ENTRY:mainCRTStartup		# https://learn.microsoft.com/en-us/cpp/build/reference/entry-entry-point-symbol?view=msvc-170
+	/MANIFEST:NO				# https://learn.microsoft.com/en-us/cpp/build/reference/manifest-create-side-by-side-assembly-manifest?view=msvc-170
+#	/MANIFESTUAC:NO				# https://learn.microsoft.com/en-us/cpp/build/reference/manifestuac-embeds-uac-information-in-manifest?view=msvc-170
+	/MANIFESTUAC:"level='highestAvailable' uiAccess='true'"
+	/SAFESEH:NO					# https://learn.microsoft.com/en-us/cpp/build/reference/safeseh-image-has-safe-exception-handlers?view=msvc-170
+	/SUBSYSTEM:CONSOLE #,5.01	# https://learn.microsoft.com/en-us/cpp/build/reference/subsystem-specify-subsystem?view=msvc-170
 )
 
-list(APPEND DEBUG_LINK_FLAGS /MANIFEST:NO)
-list(APPEND DEBUG_LINK_FLAGS /MANIFESTUAC:NO)
-list(APPEND DEBUG_LINK_FLAGS /level='highestAvailable')
-list(APPEND DEBUG_LINK_FLAGS /uiAccess='true')
-#list(APPEND DEBUG_LINK_FLAGS /SUBSYSTEM:CONSOLE,5.01)
-list(APPEND DEBUG_LINK_FLAGS /SUBSYSTEM:CONSOLE)
-list(APPEND DEBUG_LINK_FLAGS /SAFESEH:NO)
-
-##list(APPEND RELEASE_LINK_FLAGS /FORCE) ## MySQL lib needs /FORCE due to zlib redefinitions
-list(APPEND RELEASE_LINK_FLAGS /INCREMENTAL:NO)
-list(APPEND RELEASE_LINK_FLAGS /OPT:NOREF)
-list(APPEND RELEASE_LINK_FLAGS /MANIFEST:NO)
-list(APPEND RELEASE_LINK_FLAGS /MANIFESTUAC:NO)
-list(APPEND RELEASE_LINK_FLAGS /level='highestAvailable')
-list(APPEND RELEASE_LINK_FLAGS /uiAccess='true')
-#list(APPEND RELEASE_LINK_FLAGS /SUBSYSTEM:CONSOLE,5.01)
-list(APPEND RELEASE_LINK_FLAGS /SUBSYSTEM:CONSOLE)
-list(APPEND RELEASE_LINK_FLAGS /SAFESEH:NO)
+###### Linker Options (Release) ######
+dk_append(CMAKE_EXE_LINKER_FLAGS_RELEASE
+	/INCREMENTAL:NO				# https://learn.microsoft.com/en-us/cpp/build/reference/incremental-link-incrementally?view=msvc-170
+#	/OPT:NOREF					# https://learn.microsoft.com/en-us/cpp/build/reference/opt-optimizations?view=msvc-170
+#	/FORCE 						# https://learn.microsoft.com/en-us/cpp/build/reference/force-force-file-output?view=msvc-170
+								# MySQL lib needs /FORCE due to zlib redefinitions
+)
--- DKCMake/toolchains/Windows_X86_Msvc_Toolchain.cmake
+++ DKCMake/toolchains/Windows_X86_Msvc_Toolchain.cmake
@@ -14,7 +14,6 @@ dk_set(CMAKE_MAKE_PROGRAM 			${VS_MAKE_PROGRAM})				# https://cmake.org/cmake/he
 dk_set(CMAKE_GENERATOR_PLATFORM		${VS_X86_GENERATOR_PLATFORM})	# https://cmake.org/cmake/help/latest/variable/CMAKE_GENERATOR_PLATFORM.html
 dk_set(CMAKE_CXX_COMPILER			${VS_X86_COMPILER})				# https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER.html
 dk_set(CMAKE_C_COMPILER				${VS_X86_COMPILER})				# https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER.html
-
 #dk_set(CMAKE_DUMPBIN				${VS_X86_DUMPBIN})
 #dk_set(CMAKE_LINKER				${VS_X86_LINKER})
 #dk_append(DKCONFIGURE_FLAGS		--build=i686-msvc)
@@ -22,35 +21,48 @@ dk_set(CMAKE_C_COMPILER				${VS_X86_COMPILER})				# https://cmake.org/cmake/help
 ###### C Compiler Options ######
 # https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-by-category?view=msvc-170
 dk_append(CMAKE_C_FLAGS
-	/DWIN 						# https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions?view=msvc-170
-	/DWIN_X86					# "
-	/D_WINDOWS					# "
-	/D_CRT_SECURE_NO_WARNINGS	# "
-	/D_USING_V110_SDK71_		# "
-	/std:c17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
-	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
-	/Zm500 						# https://learn.microsoft.com/en-us/cpp/build/reference/zm-specify-precompiled-header-memory-allocation-limit?view=msvc-170
-	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
 	/bigobj						# https://learn.microsoft.com/en-us/cpp/build/reference/bigobj-increase-number-of-sections-in-dot-obj-file?view=msvc-170
+	/D_CRT_SECURE_NO_WARNINGS 	# https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions?view=msvc-170
+	/D_USING_V110_SDK71_ 		# "
+	/D_WINDOWS 					# "
+	/DWIN 						# "
+	/DWIN_X86_64 				# "
+	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
+	/std:c17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
+	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
+	/Zm500 						# https://learn.microsoft.com/en-us/cpp/build/reference/zm-specify-precompiled-header-memory-allocation-limit?view=msvc-170
 )
 
 ###### C++ Compiler Options ######
 # https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-by-category?view=msvc-170
 dk_append(CMAKE_CXX_FLAGS			
-	/DWIN 						# https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions?view=msvc-170
-	/DWIN_X86 					# "
-	/D_WINDOWS 					# "
-	/D_CRT_SECURE_NO_WARNINGS 	# "
-	/D_USING_V110_SDK71_ 		# "
-	/std:c++17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
-	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
-	/Zm500 						# https://learn.microsoft.com/en-us/cpp/build/reference/zm-specify-precompiled-header-memory-allocation-limit?view=msvc-170
-	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
 	/bigobj						# https://learn.microsoft.com/en-us/cpp/build/reference/bigobj-increase-number-of-sections-in-dot-obj-file?view=msvc-170
+	/D_CRT_SECURE_NO_WARNINGS 	# https://learn.microsoft.com/en-us/cpp/build/reference/d-preprocessor-definitions?view=msvc-170
+	/D_USING_V110_SDK71_ 		# "
+	/D_WINDOWS 					# "
+	/DWIN 						# "
+	/DWIN_X86_64 				# "
+	/nologo 					# https://learn.microsoft.com/en-us/cpp/build/reference/nologo-suppress-startup-banner-c-cpp?view=msvc-170
+	/std:c++17 					# https://learn.microsoft.com/en-us/cpp/build/reference/std-specify-language-standard-version?view=msvc-170
+	/Zc:__cplusplus 			# https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus?view=msvc-170
+	/Zm500 						# https://learn.microsoft.com/en-us/cpp/build/reference/zm-specify-precompiled-header-memory-allocation-limit?view=msvc-170
 ) 
 
 ###### Linker Options ######
 # https://learn.microsoft.com/en-us/cpp/build/reference/linker-options?view=msvc-170
-dk_append(CMAKE_EXE_LINKER_FLAGS	
-	/ENTRY:WinMainCRTStartup	# https://learn.microsoft.com/en-us/cpp/build/reference/entry-entry-point-symbol?view=msvc-170
-) 	
+dk_append(CMAKE_EXE_LINKER_FLAGS
+	/ENTRY:mainCRTStartup		# https://learn.microsoft.com/en-us/cpp/build/reference/entry-entry-point-symbol?view=msvc-170
+	/MANIFEST:NO				# https://learn.microsoft.com/en-us/cpp/build/reference/manifest-create-side-by-side-assembly-manifest?view=msvc-170
+#	/MANIFESTUAC:NO				# https://learn.microsoft.com/en-us/cpp/build/reference/manifestuac-embeds-uac-information-in-manifest?view=msvc-170
+	/MANIFESTUAC:"level='highestAvailable' uiAccess='true'"
+	/SAFESEH:NO					# https://learn.microsoft.com/en-us/cpp/build/reference/safeseh-image-has-safe-exception-handlers?view=msvc-170
+	/SUBSYSTEM:CONSOLE #,5.01	# https://learn.microsoft.com/en-us/cpp/build/reference/subsystem-specify-subsystem?view=msvc-170
+)
+
+###### Linker Options (Release) ######
+dk_append(CMAKE_EXE_LINKER_FLAGS_RELEASE
+	/INCREMENTAL:NO				# https://learn.microsoft.com/en-us/cpp/build/reference/incremental-link-incrementally?view=msvc-170
+#	/OPT:NOREF					# https://learn.microsoft.com/en-us/cpp/build/reference/opt-optimizations?view=msvc-170
+#	/FORCE 						# https://learn.microsoft.com/en-us/cpp/build/reference/force-force-file-output?view=msvc-170
+								# MySQL lib needs /FORCE due to zlib redefinitions
+)
